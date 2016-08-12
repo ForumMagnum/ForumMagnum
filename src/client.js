da74e7f6a876e7fd71c43d4a8690b0288acceb68
Sacha Greif <sacha357@gmail.com>
2016-08-12 13:18:49 +0900

    more clean up

--- src/client.js
+++ src/client.js
@@ -1,5 +1,10 @@
 import '../packages/nova-i18n-en-us/lib/export.js';
 
+/*
+note: posts, comments, users, lib, core, etc. are already
+loaded through nova-base-routes
+*/
+
 import 'nova-rss';
 import 'nova-notifications';
 import 'nova-search';
