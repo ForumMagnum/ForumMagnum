f09e5399c4e7a4bce0ab46e42f231f3ac025ff24
Ben Albert Pace <benalbertpace@bens-mbp-7.attlocal.net>
2017-09-03 12:06:47 -0700

    I think these are right.

--- packages/lesswrong/components/sequences/Codex.jsx
+++ packages/lesswrong/components/sequences/Codex.jsx
@@ -2,7 +2,7 @@ import { Components, registerComponent } from 'meteor/vulcan:core';
 import React from 'react';
 
 const Codex = (props, context) => {
-  return <Components.CollectionsPage documentId={'2izXHCrmJ684AnZ5X'} />
+  return <Components.CollectionsPage documentId={'qTe79fDX6GHSrpfXN'} />
 };
 
 registerComponent('Codex', Codex);
