8b417864ab670a977582753e8cb880d3597b6d74
Discordius <panisnecis@gmail.com>
2017-09-05 18:14:59 -0700

    Implemented Tufte CSS and made initial changes for notifications menu

--- packages/lesswrong/components/posts/PostsCommentsThreadWrapper.jsx
+++ packages/lesswrong/components/posts/PostsCommentsThreadWrapper.jsx
@@ -5,8 +5,8 @@ import Comments from 'meteor/vulcan:comments';
 
 const PostsCommentsThreadWrapper = (props, /* context*/) => {
 
-  const {loading, results, terms: {postId}, userId} = props;
-
+  const {loading, results, loadMore, networkStatus, totalCount, terms: {postId}, userId} = props;
+  const loadingMore = networkStatus === 2;
   if (loading || !results) {
     return <div className="posts-comments-thread"><Components.Loading/></div>
   } else {
@@ -14,7 +14,7 @@ const PostsCommentsThreadWrapper = (props, /* context*/) => {
     const nestedComments = Utils.unflatten(resultsClone, {idProperty: '_id', parentIdProperty: 'parentCommentId'});
     return (
       <div className="posts-comments-thread-wrapper">
-        <Components.PostsCommentsThread terms={{postId: postId, userId: userId, view: "postVisits", limit: 1}} comments={nestedComments} />
+        <Components.PostsCommentsThread terms={{postId: postId, userId: userId, view: "postVisits", limit: 1}} comments={nestedComments} loadMoreComments={loadMore} totalComments={totalCount} commentCount={resultsClone.length} loadingMoreComments={loadingMore}/>
       </div>
     );
   }
@@ -24,8 +24,8 @@ const options = {
   collection: Comments,
   queryName: 'PostCommentsThreadQuery',
   fragmentName: 'CommentsList',
-  limit: 50,
-  totalResolver: false,
+  // limit: 50,
+  // totalResolver: false,
 };
 
 registerComponent('PostsCommentsThreadWrapper', PostsCommentsThreadWrapper, [withList, options]);
