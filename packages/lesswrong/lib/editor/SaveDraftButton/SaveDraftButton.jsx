23b49aa94a89517f5e05564ec713a3c76aaaf59c
Raymond Arnold <rarnold@spotify.com>
2017-09-09 17:45:43 -0700

    SaveToDraftButton pruned slightly

--- packages/lesswrong/lib/editor/SaveDraftButton/SaveDraftButton.jsx
+++ packages/lesswrong/lib/editor/SaveDraftButton/SaveDraftButton.jsx
@@ -1,20 +1,11 @@
 import React, { PropTypes, Component } from 'react';
 import { Components, registerComponent } from 'meteor/vulcan:core';
-import FlatButton from 'material-ui/FlatButton';
 import { Checkbox } from 'formsy-react-components';
 
 class SaveDraftButton extends Component {
   constructor(props, context) {
     super(props,context);
-    const document = this.props.document;
-    const fieldName = this.props.name;
-        
-    this.state = {
-      active: false,
-    };
   }
-
-  toggleEditor = () => {this.setState({active: !this.state.active})}
     
   render() {
     return (
