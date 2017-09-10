899100825f678c8a4a5cccb2e384eb750cb244f1
Raymond Arnold <rarnold@spotify.com>
2017-09-09 17:46:54 -0700

    comments on draft

--- packages/lesswrong/lib/editor/SaveDraftButton/SaveDraftButton.jsx
+++ packages/lesswrong/lib/editor/SaveDraftButton/SaveDraftButton.jsx
@@ -2,6 +2,9 @@ import React, { PropTypes, Component } from 'react';
 import { Components, registerComponent } from 'meteor/vulcan:core';
 import { Checkbox } from 'formsy-react-components';
 
+// Ray 20017-09-09: This component is currently mostly unnecessary but is planned to turn into a real "submit" button
+// i.e click to submit and safe-to-draft
+
 class SaveDraftButton extends Component {
   constructor(props, context) {
     super(props,context);
