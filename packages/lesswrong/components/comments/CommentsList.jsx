d2590799917e4f524917b0c4517cc522a5f0728c
Raymond Arnold <rarnold@spotify.com>
2017-09-11 17:10:28 -0700

    Added a softDelete button
     • CommentsItem has handleDelete function
     • CommentsItem check for Users.canDo softRemove.all
     • CommentsItem displays Delete or Undo Delete button
    
     • CommentsList wrapped in withEdit
     • CommentsList passes editMutation into CommentsNode
    
     • CommentsNode passes editMutation to children
    
     • CommentsViews displays Admin views if admin has softRemove access
    
     • permissions.js includes sunshineRegiment
     • lib/views now includes default view and deleted view
     • deleted view shows all posts, with deleted posts flagged, sorted by deleted

--- packages/lesswrong/components/comments/CommentsList.jsx
+++ packages/lesswrong/components/comments/CommentsList.jsx
@@ -1,13 +1,21 @@
-import { Components, replaceComponent } from 'meteor/vulcan:core';
+import { Components, replaceComponent, withEdit } from 'meteor/vulcan:core';
 import React from 'react';
 import { FormattedMessage } from 'meteor/vulcan:i18n';
+import { Comments } from "meteor/example-forum";
 
-const CommentsList = ({comments, currentUser, highlightDate}) => {
+const CommentsList = ({comments, currentUser, highlightDate, editMutation}) => {
   if (comments) {
     return (
       <div className="comments-list">
-        {comments.map(comment => <Components.CommentsNode currentUser={currentUser} comment={comment} key={comment._id} newComment={highlightDate && (new Date(comment.postedAt).getTime() > new Date(highlightDate).getTime())}/>)}
-        {/*hasMore ? (ready ? <Components.CommentsLoadMore loadMore={loadMore} count={count} totalCount={totalCount} /> : <Components.Loading/>) : null*/}
+        {comments.map(comment =>
+          <Components.CommentsNode
+            currentUser={currentUser}
+            comment={comment}
+            key={comment._id}
+            newComment={highlightDate && (new Date(comment.postedAt).getTime() > new Date(highlightDate).getTime())}
+            editMutation={editMutation}
+            />)
+        }
       </div>
     )
   } else {
@@ -24,4 +32,10 @@ const CommentsList = ({comments, currentUser, highlightDate}) => {
 
 CommentsList.displayName = "CommentsList";
 
-replaceComponent('CommentsList', CommentsList);
+const withEditOptions = {
+  collection: Comments,
+  fragmentName: 'CommentsList',
+};
+
+
+replaceComponent('CommentsList', CommentsList, [withEdit, withEditOptions]);
--- packages/lesswrong/lib/views.js
+++ packages/lesswrong/lib/views.js
@@ -2,42 +2,64 @@ import { Comments } from 'meteor/example-forum';
 import Users from "meteor/vulcan:users";
 import LWEvents from "./collections/lwevents/collection.js";
 
-Comments.addView("postCommentsTop", function (terms) {
+// Comments.addDefaultView(function (terms) {
+//   return {
+//     selector: {deleted: {$ne: true}}
+//   };
+// });
+
+// TODO Please change this to "default view ignores deleted"
+
+Comments.addView("postCommentsDeleted", function (terms) {
   return {
     selector: {postId: terms.postId},
+    options: {sort: {deleted:-1}}
+  };
+});
+
+Comments.addView("postCommentsTop", function (terms) {
+  return {
+    selector: { postId: terms.postId, deleted: {$ne:true}},
     options: {sort: {baseScore: -1, postedAt: -1}}
   };
 });
 
 Comments.addView("postCommentsNew", function (terms) {
   return {
-    selector: {postId: terms.postId},
+    selector: { postId: terms.postId, deleted: {$ne:true}},
     options: {sort: {postedAt: -1}}
   };
 });
 
 Comments.addView("postCommentsBest", function (terms) {
   return {
-    selector: {postId: terms.postId},
+    selector: { postId: terms.postId, deleted: {$ne:true}},
     options: {sort: {baseScore: -1}, postedAt: -1}
   };
 });
 
 Comments.addView("recentComments", function (terms) {
   return {
+    selector: { deleted:{$ne:true}},
     options: {sort: {postedAt: -1}, limit: terms.limit || 5},
   };
 });
 
 Users.addView("topContributors", function (terms) {
   return {
+    selector: { deleted: {$ne:true}},
     options: {sort: {karma: -1}, limit: 5},
   };
 });
 
 LWEvents.addView("postVisits", function (terms) {
   return {
-    selector: {documentId: terms.postId, userId: terms.userId, name: "post-view"},
+    selector: {
+      documentId: terms.postId,
+      userId: terms.userId,
+      name: "post-view",
+      deleted: {$ne:true}
+    },
     options: {sort: {createdAt: -1}, limit: terms.limit || 1},
   };
 });
