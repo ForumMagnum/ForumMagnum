e4025aee13c6fd07eb9078ddf8940db49b149132
Raymond Arnold <rarnold@spotify.com>
2017-09-17 16:59:58 -0700

    remove spaces

--- packages/lesswrong/components/common/DialogGroup.jsx
+++ packages/lesswrong/components/common/DialogGroup.jsx
@@ -2,28 +2,28 @@ import React, { Component, PropTypes } from 'react';
 import Dialog from 'material-ui/Dialog';
 import FlatButton from 'material-ui/FlatButton';
 import { registerComponent } from 'meteor/vulcan:core';
- 
+
 /**
  * Dialog group, with trigger-button and dialog-instance
  */
-  
+
 class DialogGroup extends Component {
-   
+
   state = {
     open: false,
   };
- 
+
   handleOpen = () => {
     this.setState({open: true});
   };
- 
+
   handleClose = () => {
     this.setState({open: false});
   };
- 
+
   render() {
     const actions = this.props.actions.map(action => <span onTouchTap={this.handleClose}>{action}</span>)
- 
+
     return (
       <span className="dialog-trigger-group">
         <span className="dialog-trigger" onClick={this.handleOpen}>{ this.props.trigger }</span>
@@ -40,7 +40,7 @@ class DialogGroup extends Component {
     );
   }
 }
- 
+
 DialogGroup.propTypes = {
     title: PropTypes.string.isRequired,
     trigger: PropTypes.node.isRequired,
@@ -51,5 +51,5 @@ DialogGroup.defaultProps = {
   children: undefined,
   actions: [ <FlatButton label="Okay" primary={true}/> ],
 }
- 
+
 registerComponent('DialogGroup', DialogGroup);
