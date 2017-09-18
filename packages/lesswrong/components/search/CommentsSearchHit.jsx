294f86cafab09acdc215fca4eb0aee27fa6d17ea
Discordius <panisnecis@gmail.com>
2017-09-17 17:47:22 -0700

    Fixed comment links in search results

--- packages/lesswrong/components/search/CommentsSearchHit.jsx
+++ packages/lesswrong/components/search/CommentsSearchHit.jsx
@@ -10,22 +10,24 @@ import Badge from 'material-ui/Badge';
 
 import React, { PureComponent } from 'react';
 
-const CommentsSearchHit = ({hit}) => <div className="search-results-comments-item recent-comments-item comments-item">
-    <Link to={"/"}>
-      <div className="comments-item-body recent-comments-item-body ">
-        <object><div className="comments-item-meta recent-comments-item-meta">
-          <div className="search-results-comments-item-username">{hit.authorDisplayName}</div>
-          <div className="search-results-comments-item-score">{hit.baseScore} points </div>
-          <div className="comments-item-date"> {moment(new Date(hit.postedAt)).fromNow()}</div>
-          <div className="comments-item-origin">
-            {/*on <span className="comments-item-origin-post-title">{hit.post.title}</span>*/}
-          </div>
-        </div></object>
-      <div className="recent-comments-item-text comments-item-text">
-        <Snippet attributeName="body" hit={hit} tagName="mark" />
-      </div>
-      </div>
-    </Link>
-</div>
+const CommentsSearchHit = ({hit}) => {
+  return <div className="search-results-comments-item recent-comments-item comments-item">
+      <Link to={"/posts/" + hit.postId + "/" + hit.postSlug + "/" + hit._id}>
+        <div className="comments-item-body recent-comments-item-body ">
+          <object><div className="comments-item-meta recent-comments-item-meta">
+            <div className="search-results-comments-item-username">{hit.authorDisplayName}</div>
+            <div className="search-results-comments-item-score">{hit.baseScore} points </div>
+            <div className="comments-item-date"> {moment(new Date(hit.postedAt)).fromNow()}</div>
+            <div className="comments-item-origin">
+              {/*on <span className="comments-item-origin-post-title">{hit.post.title}</span>*/}
+            </div>
+          </div></object>
+        <div className="recent-comments-item-text comments-item-text">
+          <Snippet attributeName="body" hit={hit} tagName="mark" />
+        </div>
+        </div>
+      </Link>
+  </div>
+}
 
 registerComponent("CommentsSearchHit", CommentsSearchHit);
