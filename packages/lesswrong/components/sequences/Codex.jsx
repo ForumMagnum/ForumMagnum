89458cc164b60980e6569c76fcb5d3b5385cd669
Discordius <panisnecis@gmail.com>
2017-09-20 19:09:09 -0700

    Made Codex accessible

--- packages/lesswrong/components/sequences/Codex.jsx
+++ packages/lesswrong/components/sequences/Codex.jsx
@@ -2,7 +2,7 @@ import { Components, registerComponent } from 'meteor/vulcan:core';
 import React from 'react';
 
 const Codex = (props, context) => {
-  return <Components.CollectionsPage documentId={'qTe79fDX6GHSrpfXN'} />
+  return <Components.CollectionsPage documentId={'2izXHCrmJ684AnZ5X'} />
 };
 
 registerComponent('Codex', Codex);
