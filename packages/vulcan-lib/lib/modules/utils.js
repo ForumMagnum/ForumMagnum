88ba757cb90c0cfa1edac1cb78df60aef28872c5
Discordius <panisnecis@gmail.com>
2017-09-22 00:13:37 -0700

    Added error message for app operation not allowed

--- packages/vulcan-lib/lib/modules/utils.js
+++ packages/vulcan-lib/lib/modules/utils.js
@@ -475,6 +475,7 @@ Utils.performCheck = (operation, user, checkedObject, context, documentId) => {
   }
 
   if (!operation(user, checkedObject, context)) {
+    console.log("App Operation not allows", operation, checkedObject);
     throw new Error(Utils.encodeIntlError({id: `app.operation_not_allowed`, value: operation.name}));
   }
 
