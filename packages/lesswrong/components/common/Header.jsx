b71493a82d95c5513f7c22e850bfc8a117b0ed93
Discordius <panisnecis@gmail.com>
2017-09-22 14:23:45 -0700

    Fixed recent user comments

--- packages/lesswrong/components/common/Header.jsx
+++ packages/lesswrong/components/common/Header.jsx
@@ -58,7 +58,7 @@ class Header extends Component {
           />
         <Drawer docked={false} width={200} open={this.state.open} onRequestChange={(open) => this.setState({open})} containerClassName="menu-drawer" >
           <MenuItem onTouchTap={this.handleClose} containerElement={<Link to={"/"}/>}> HOME </MenuItem>
-          <MenuItem onTouchTap={this.handleClose} containerElement={<Link to={"/sequences"}/>}> RATIONALITY:A-Z </MenuItem>
+          <MenuItem onTouchTap={this.handleClose} containerElement={<Link to={"/sequences"}/>}> RATIONALITY: A-Z </MenuItem>
           <MenuItem onTouchTap={this.handleClose} containerElement={<Link to={"/codex"}/>}> THE CODEX </MenuItem>
           <MenuItem onTouchTap={this.handleClose} containerElement={<Link to={"/hpmor"}/>}> HPMOR </MenuItem>
           <MenuItem onTouchTap={this.handleClose} containerElement={<Link to={"/meta"}/>}> META </MenuItem>
