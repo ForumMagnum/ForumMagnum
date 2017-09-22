4c32a70b28884f6e3e41c915b3dc977676c50af0
Raymond Arnold <rarnold@spotify.com>
2017-09-21 17:50:17 -0700

    sunshines can edit

--- packages/lesswrong/lib/collections/comments/permissions.js
+++ packages/lesswrong/lib/collections/comments/permissions.js
@@ -3,6 +3,7 @@ import { Comments } from "meteor/example-forum";
 
 const sunshineRegimentActions = [
   'comments.softRemove.all',
-  'comments.replyOnBlocked.all'
+  'comments.replyOnBlocked.all',
+  'comments.edit.all'
 ];
 Users.groups.sunshineRegiment.can(sunshineRegimentActions);
