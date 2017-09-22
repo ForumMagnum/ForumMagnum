d21975e9f288335aa2d1aeba4bfacaa9f42ba7f7
Raymond Arnold <rarnold@spotify.com>
2017-09-21 22:30:51 -0700

    warning if you don't have jq

--- launch_meteor.sh
+++ launch_meteor.sh
@@ -1,3 +1,16 @@
+if tput setaf 1 &> /dev/null; then
+  blue=$(tput setaf 153)
+  bold=$(tput bold)
+else
+  blue=""
+  bold=""
+fi
+
+
+command -v jq >/dev/null 2>&1 || { echo >&2 ${bold}${blue}"
+LessWrong2 requires the commandline tool 'jq' to launch a local server.
+Please install jq (on Mac, 'brew install jq')"; exit 1; }
+
 dbname=$(cat settings.json | jq -r .localServer.dbname)
 password=$(cat settings.json | jq -r .localServer.password)
 server=$(cat settings.json | jq -r .localServer.server)
