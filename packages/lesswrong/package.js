2119a133eb4e0147888c661c8216db1006572c4b
Discordius <panisnecis@gmail.com>
2017-10-03 11:18:44 -0700

    Testing!

--- packages/lesswrong/package.js
+++ packages/lesswrong/package.js
@@ -24,3 +24,19 @@ Package.onUse( function(api) {
   ], ['client']);
 
 });
+
+Package.onTest(function(api) {
+  // You almost definitely want to depend on the package itself,
+  // this is what you are testing!
+  api.use('lesswrong');
+  // You should also include any packages you need to use in the test code
+  api.use([
+    'fourseven:scss',
+    'vulcan:core',
+    'example-forum',
+    'vulcan:users',
+    'vulcan:voting',
+  ]);
+  // Finally add an entry point for tests
+  api.mainModule('./components/comments/comments.app-tests.js', 'client');
+});
