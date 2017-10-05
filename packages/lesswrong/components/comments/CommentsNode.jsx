c6a2bea09f6477e80a455e8d796779c25b9ce707
Max Harms <raelifin@gmail.com>
2017-10-04 17:06:40 -0700

    Comments are collapsable

--- packages/lesswrong/components/comments/CommentsNode.jsx
+++ packages/lesswrong/components/comments/CommentsNode.jsx
@@ -2,29 +2,48 @@ import { Components, replaceComponent } from 'meteor/vulcan:core';
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 
-const CommentsNode = ({ comment, currentUser, newComment, editMutation}) =>
-  <div className={newComment ? "comment-new" : "comment-old"}>
-    <div className={"comments-node"}>
-      <Components.CommentsItem
-        currentUser={currentUser}
-        comment={comment}
-        key={comment._id}
-        editMutation={editMutation}
-      />
-      {comment.childrenResults ?
-        <div className="comments-children">
-          {comment.childrenResults.map(comment =>
-            <CommentsNode currentUser={currentUser}
-              comment={comment}
-              key={comment._id}
-              newComment={newComment}
-              editMutation={editMutation}
-            />)}
+class CommentsNode extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      collapsed: false
+    };
+  }
+
+  toggleCollapse = () => {
+    this.setState({collapsed: !this.state.collapsed});
+  }
+
+  render() {
+    const {comment, currentUser, newComment, editMutation} = this.props;
+    return (
+      <div className={newComment ? "comment-new" : "comment-old"}>
+        <div className={"comments-node"}>
+          <Components.CommentsItem
+            collapsed={this.state.collapsed}
+            toggleCollapse={this.toggleCollapse}
+            currentUser={currentUser}
+            comment={comment}
+            key={comment._id}
+            editMutation={editMutation}
+          />
+        {!this.state.collapsed && comment.childrenResults ?
+            <div className="comments-children">
+              {comment.childrenResults.map(comment =>
+                <CommentsNode currentUser={currentUser}
+                  comment={comment}
+                  key={comment._id}
+                  newComment={newComment}
+                  editMutation={editMutation}
+                />)}
+            </div>
+            : null
+          }
         </div>
-        : null
-      }
-    </div>
-  </div>
+      </div>
+    )
+  }
+}
 
 CommentsNode.propTypes = {
   comment: PropTypes.object.isRequired, // the current comment
