1377421c0592944841b7a59cb738281258159173
Discordius <panisnecis@gmail.com>
2017-10-05 12:16:45 -0700

    Fixed CollectionsCard imgur import

--- packages/lesswrong/components/common/Home.jsx
+++ packages/lesswrong/components/common/Home.jsx
@@ -2,13 +2,14 @@ import { Components, registerComponent, withCurrentUser} from 'meteor/vulcan:cor
 import React from 'react';
 import { Link } from 'react-router';
 
+
 const testCollections = [
   {
     title: "Rationality: A-Z",
     id: "dummyId",
     user: {userName: "Eliezer_Yudkowsky", displayName: "EliezerYudkowsky", slug: "eliezer_yudkowsky"},
     summary: 'A set of essays by Eliezer Yudkowsky that serve as a long-form introduction to formative ideas behind Less Wrong, the Machine Intelligence Research Institute, the Center for Applied Rationality, and substantial parts of the effective altruism community.',
-    image: "http://i.imgur.com/dVXiZtw.png",
+    imageId: "dVXiZtw_xrmvpm.png",
     color: "#B1D4B4",
     big: true,
   },
@@ -17,7 +18,7 @@ const testCollections = [
     id: "dummyId2",
     user: {username: "Yvain", displayName: "Scott Alexander", slug: "yvain"},
     summary: "The Codex contains essays about science, medicine, philosophy, politics, and futurism. (There’s also one post about hallucinatory cactus-people, but it’s not representative)",
-    image: "http://i.imgur.com/ItFKgn4.png",
+    imageId: "ItFKgn4_rrr58y.png",
     color: "#88ACB8",
     big: false,
   },
@@ -26,7 +27,7 @@ const testCollections = [
     id: "dummyId3",
     user: {userName: "Eliezer_Yudkowsky", displayName: "EliezerYudkowsky", slug: "eliezer_yudkowsky"},
     summary: "In an Alternate Universe, Petunia married a scientist. Now Rationalist!Harry enters the wizarding world armed with Enlightenment ideals and the experimental spirit.",
-    image: "http://i.imgur.com/uu4fJ5R.png",
+    imageId: "uu4fJ5R_zeefim.png",
     color: "#757AA7",
     big: false,
   }
