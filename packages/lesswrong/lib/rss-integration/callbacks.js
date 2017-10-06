353d41ca61dcaa42e282a4d81a42c4b541cde4f1
Discordius <panisnecis@gmail.com>
2017-10-05 17:00:28 -0700

    Fixed "unnamed callback" warnings

--- packages/lesswrong/lib/rss-integration/callbacks.js
+++ packages/lesswrong/lib/rss-integration/callbacks.js
@@ -1,7 +1,7 @@
 import { addCallback, editMutation } from 'meteor/vulcan:core';
 import RSSFeeds from '../collections/rssfeeds/collection.js';
 
-const populateRawFeed = (feed) => {
+function populateRawFeed(feed) {
   const feedparser = require('feedparser-promised');
   const url = feed.url;
   feedparser.parse(url).then(currentPosts => {
