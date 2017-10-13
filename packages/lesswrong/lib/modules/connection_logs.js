3424e41bd8a07b822d01ac724e07b0bd8a9e9a3a
Discordius <panisnecis@gmail.com>
2017-10-13 01:39:40 -0700

    Improved connection logs

--- packages/lesswrong/lib/modules/connection_logs.js
+++ packages/lesswrong/lib/modules/connection_logs.js
@@ -57,5 +57,5 @@ Accounts.onLogin((login) => {
     currentUser: dummyUser,
     validate: false,
   })
-  console.log("New Login: ", login)
+  console.log("New Login: ", login && login.user && login.user.username);
 })
