19b017a4199fb1fc5bec25e8dc6f62bf3c805ac6
Raymond Arnold <rarnold@spotify.com>
2017-10-16 17:08:13 -0700

    showAdvanced2 - DisplayAdvancedPlugin

---
+++ packages/lesswrong/components/editor/editorPlugins/DisplayAdvancedPlugin.js
@@ -0,0 +1,13 @@
+
+import ToolbarButton from './HeadingsPlugin'
+
+import Plugin from './plugin'
+export default class DisplayAdvancedPlugin extends Plugin {
+  constructor(props: Props) {
+    super(props)
+  }
+  // name = "DisplayAdvanced"
+  // toolbarButtons = [
+  //   <ToolbarButton icon={<div>Full</div>} />
+  // ]
+}
