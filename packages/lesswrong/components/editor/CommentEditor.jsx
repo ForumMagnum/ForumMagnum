854067781574b61cea1c344cefd6df816d56692e
Discordius <panisnecis@gmail.com>
2017-10-24 23:45:38 -0700

    Draft JS Editor replacement (comments completed)

--- packages/lesswrong/components/editor/CommentEditor.jsx
+++ packages/lesswrong/components/editor/CommentEditor.jsx
@@ -6,21 +6,155 @@ import withEditor from './withEditor.jsx'
 
 import ls from 'local-storage';
 
+import { EditorState, convertFromRaw, convertToRaw } from 'draft-js';
+import Editor, { composeDecorators } from 'draft-js-plugins-editor';
+import createInlineToolbarPlugin, { Separator } from 'draft-js-inline-toolbar-plugin';
+import createMarkdownShortcutsPlugin from 'draft-js-markdown-shortcuts-plugin';
+
+import createImagePlugin from 'draft-js-image-plugin';
+import createAlignmentPlugin from 'draft-js-alignment-plugin';
+import createFocusPlugin from 'draft-js-focus-plugin';
+import createResizeablePlugin from 'draft-js-resizeable-plugin';
+import createLinkPlugin from 'draft-js-anchor-plugin';
+
+import {
+  ItalicButton,
+  BoldButton,
+  UnderlineButton,
+  CodeButton,
+  HeadlineOneButton,
+  HeadlineTwoButton,
+  HeadlineThreeButton,
+  UnorderedListButton,
+  OrderedListButton,
+  BlockquoteButton,
+  CodeBlockButton,
+} from 'draft-js-buttons';
+
+
+import { convertToHTML, convertFromHTML } from 'draft-convert';
+
+
+const htmlToDraft = convertFromHTML({
+  htmlToEntity: (nodeName, node, createEntity) => {
+    if (nodeName === 'a') {
+      return createEntity(
+        'LINK',
+        'MUTABLE',
+        {url: node.href}
+      )
+    }
+  },
+})
+
+const initialState = {
+    "entityMap": {
+        "0": {
+            "type": "image",
+            "mutability": "IMMUTABLE",
+            "data": {
+                "src": "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
+            }
+        }
+    },
+    "blocks": [{
+        "key": "9gm3s",
+        "text": "You can have images in your text field. This is a very rudimentary example, but you can enhance the image plugin with resizing, focus or alignment plugins.",
+        "type": "unstyled",
+        "depth": 0,
+        "inlineStyleRanges": [],
+        "entityRanges": [],
+        "data": {}
+    }, {
+        "key": "ov7r",
+        "text": " ",
+        "type": "atomic",
+        "depth": 0,
+        "inlineStyleRanges": [],
+        "entityRanges": [{
+            "offset": 0,
+            "length": 1,
+            "key": 0
+        }],
+        "data": {}
+    }, {
+        "key": "e23a8",
+        "text": "See advanced examples further down …",
+        "type": "unstyled",
+        "depth": 0,
+        "inlineStyleRanges": [],
+        "entityRanges": [],
+        "data": {}
+    }]
+};
+
 class CommentEditor extends Component {
   constructor(props, context) {
     super(props,context);
-    let editor = this.props.editor;
+    // let editor = this.props.editor;
     const document = this.props.document;
-    let state = document && document.content ? document.content : createEmptyState();
-
+    let state = {};
+    if (document && document.content) {
+      console.log("initial CommentEditor state", document.content);
+      try {
+        state = EditorState.createWithContent(convertFromRaw(document.content));
+      } catch(e) {
+        console.log("Invalid comment content, restoring from HTML instead", document);
+        state = document && document.htmlBody && EditorState.createWithContent(htmlToDraft(document.htmlBody))
+      }
+    } else if (document && document.htmlBody) {
+      console.log("initial CommentEditor state", document.htmlBody);
+      state = EditorState.createWithContent(htmlToDraft(document.htmlBody));
+    }
+    else {
+      state = EditorState.createWithContent(convertFromRaw(initialState));
+    }
     // Check whether we have a state from a previous session saved (in localstorage)
     const savedState = this.getSavedState();
-    if (savedState) { state = savedState }
+    if (savedState) {
+      try {
+        console.log("Restoring saved comment state: ", savedState);
+        state = EditorState.createWithContent(convertFromRaw(savedState))
+      } catch(e) {
+        console.log(e)
+      }
+    }
+
+    console.log("CommentEditor state", state);
 
     this.state = {
-      contentState: state,
+      editorState: state,
     };
-    editor.trigger.editable.add(state);
+    // editor.trigger.editable.add(state);
+    const linkPlugin = createLinkPlugin();
+    const inlineToolbarPlugin = createInlineToolbarPlugin({
+      structure: [
+        BoldButton,
+        ItalicButton,
+        UnderlineButton,
+        CodeButton,
+        linkPlugin.LinkButton,
+        Separator,
+        HeadlineOneButton,
+        HeadlineTwoButton,
+        UnorderedListButton,
+        OrderedListButton,
+        BlockquoteButton,
+        CodeBlockButton,
+      ]
+    });
+    const alignmentPlugin = createAlignmentPlugin();
+    const markdownShortcutsPlugin = createMarkdownShortcutsPlugin();
+    const focusPlugin = createFocusPlugin();
+    const resizeablePlugin = createResizeablePlugin();
+
+    const decorator = composeDecorators(
+      resizeablePlugin.decorator,
+      alignmentPlugin.decorator,
+      focusPlugin.decorator,
+    );
+    const imagePlugin = createImagePlugin({ decorator });
+    this.plugins = [inlineToolbarPlugin, alignmentPlugin, markdownShortcutsPlugin, focusPlugin, resizeablePlugin, imagePlugin, linkPlugin];
   }
 
   // Tries to retrieve a saved state from localStorage, depending on the available information
@@ -47,6 +181,7 @@ class CommentEditor extends Component {
   // Saves the passed state to localStorage, depending on the available information
   setSavedState = (state) => {
     const document = this.props.document;
+    console.log("setSavedState state: ", state);
     if (document && document._id) {
       ls.set(document._id, state);
     } else if (document && document.parentCommentId) {
@@ -60,14 +195,16 @@ class CommentEditor extends Component {
     //Add function for resetting form to form submit callbacks
     const document = this.props.document;
     const fieldName = this.props.name;
-    
+
+
+
     const resetEditor = (result) => {
       // On Form submit, create a new empty editable
-      let editor = this.props.editor;
-      let state = createEmptyState();
-      editor.trigger.editable.add(state);
+      // let editor = this.props.editor;
+      // let state = createEmptyState();
+      // editor.trigger.editable.add(state);
       this.setState({
-        contentState: state,
+        editorState: EditorState.createEmpty(),
       });
 
       if (document._id) { ls.remove(document._id) }
@@ -76,20 +213,46 @@ class CommentEditor extends Component {
       return result;
     }
     this.context.addToSuccessForm(resetEditor);
+
+    const submitRawContentState = (data) => {
+      data.content = convertToRaw(this.state.editorState.getCurrentContent())
+      return data;
+    }
+
+    this.context.addToSubmitForm(submitRawContentState);
+  }
+
+  focus = () => {
+    this.editor.focus();
   }
 
   render() {
-    const addValues = this.context.addToAutofilledValues;
-    let editor = this.props.editor;
-    const onChange = (state) => {
-      this.setSavedState(state);
-      addValues({content: state});
-      return state;
+    // const addValues = this.context.addToAutofilledValues;
+    const InlineToolbar = this.plugins[0].InlineToolbar;
+    const AlignmentTool = this.plugins[1].AlignmentTool;
+    // let editor = this.props.editor;
+    const onChange = (editorState) => {
+      const contentState = editorState.getCurrentContent();
+      this.setSavedState(convertToRaw(contentState));
+      // addValues({content: state.getCurrentContent()});
+      this.setState({editorState: editorState})
+      console.log("Editorstate Test: ", EditorState.createWithContent(contentState));
+      // const html = convertToHTML(state.getCurrentContent());
+      // console.log("CommentEditor onChange html: ", html)
+      return editorState;
     }
     return (
-      <div className="commentEditor">
-        <Editable editor={editor} id={this.state.contentState.id} onChange={onChange} />
-        <Toolbar editor={editor} />
+      <div className="commentEditor editor content-body comments-item-text" onClick={this.focus}>
+        {/* <Editable editor={editor} id={this.state.contentState.id} onChange={onChange} />
+        <Toolbar editor={editor} /> */}
+        <Editor
+          editorState={this.state.editorState}
+          onChange={onChange}
+          plugins={this.plugins}
+          ref={(element) => { this.editor = element; }}
+        />
+        <InlineToolbar />
+        <AlignmentTool />
       </div>
     )
   }
@@ -98,6 +261,7 @@ class CommentEditor extends Component {
 CommentEditor.contextTypes = {
   addToAutofilledValues: React.PropTypes.func,
   addToSuccessForm: React.PropTypes.func,
+  addToSubmitForm: React.PropTypes.func,
 };
 
 registerComponent('CommentEditor', CommentEditor, withEditor);
--- packages/lesswrong/lib/collections/comments/callbacks.js
+++ packages/lesswrong/lib/collections/comments/callbacks.js
@@ -1,5 +1,8 @@
+import React from 'react';
 import { Comments } from "meteor/example-forum";
-import { addCallback, editMutation, runCallbacks } from 'meteor/vulcan:core';
+import { addCallback, editMutation, runCallbacks, runCallbacksAsync } from 'meteor/vulcan:core';
+import { convertToHTML, convertFromHTML } from 'draft-convert';
+import { convertFromRaw } from 'draft-js';
 
 
 // function commentsSoftRemoveChildrenComments(comment) {
@@ -13,9 +16,85 @@ import { addCallback, editMutation, runCallbacks } from 'meteor/vulcan:core';
 //     });
 // }
 
+const draftToHTML = convertToHTML({
+  entityToHTML: (entity, originalText) => {
+    if (entity.type === 'image') {
+      let classNames = 'draft-image '
+      if (entity.data.alignment) {
+        classNames = classNames + entity.data.alignment;
+      }
+      let style = ""
+      if (entity.data.width) {
+        style = "width:" + entity.data.width + "%";
+      }
+      return `<figure><img src="${entity.data.src}" class="${classNames}" style="${style}" /></figure>`;
+    }
+    if (entity.type === 'LINK') {
+      return <a href={entity.data.url}>{originalText}</a>;
+    }
+    return originalText;
+  },
+  blockToHTML: (block) => {
+     const type = block.type;
+     if (type === 'atomic') {
+       return {start: '<span>', end: '</span>'};
+     }
+     if (type === 'blockquote') {
+       return <blockquote />
+     }
+    //  return <span/>;
+   },
+});
+
+
+
+
 function CommentsEditSoftDeleteCallback (comment, oldComment) {
   if (comment.deleted && !oldComment.deleted) {
     runCallbacksAsync('comments.softDelete.async', comment);
   }
 }
 addCallback("comments.edit.async", CommentsEditSoftDeleteCallback);
+
+function CommentsNewHTMLSerializeCallback (comment) {
+  if (comment.content) {
+    const contentState = convertFromRaw(comment.content);
+    const html = draftToHTML(contentState);
+    comment.htmlBody = html;
+    console.log("Comments New HTML serialization", html)
+  }
+  return comment
+}
+
+addCallback("comments.new.sync", CommentsNewHTMLSerializeCallback);
+
+function CommentsEditHTMLSerializeCallback (modifier, comment) {
+  if (modifier.$set && modifier.$set.content) {
+    const contentState = convertFromRaw(modifier.$set.content);
+    console.log("Comment Edit callback: ", modifier.$set.content);
+    modifier.$set.htmlBody = draftToHTML(contentState);
+    modifier.$set.plaintextBody = contentState.getPlainText();
+    console.log("Comments Edit HTML serialization", modifier.$set.htmlBody, modifier.$set.plaintextBody)
+  } else if (modifier.$set && modifier.$set.htmlBody) {
+    const contentState = convertFromRaw(modifier.$set.content);
+    modifier.$set.plaintextBody = contentState.getPlainText();
+  }
+  return modifier
+}
+
+addCallback("comments.edit.sync", CommentsEditHTMLSerializeCallback);
+
+// function commentsNewHTMLBodyAndPlaintextBody(comment) {
+//   if (comment.content) {
+//     const html = ReactDOMServer.renderToStaticMarkup(<Components.ContentRenderer state={comment.content} />);
+//     const plaintextBody = htmlToText.fromString(html);
+//     Comments.update(comment._id, {$set: {htmlBody: html, body: plaintextBody}});
+//   } else if (comment.htmlBody){
+//     const html = comment.htmlBody;
+//     const plaintextBody = htmlToText.fromString(html);
+//     Comments.update(comment._id, {$set: {body: plaintextBody}});
+//   }
+// }
+//
+// addCallback("comments.new.async", commentsNewHTMLBodyAndPlaintextBody);
+// addCallback("comments.edit.async", commentsNewHTMLBodyAndPlaintextBody);
