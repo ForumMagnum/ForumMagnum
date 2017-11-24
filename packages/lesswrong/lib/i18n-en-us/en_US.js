cad3baaa7f96a31cc06d455a60b8cbdeca8fff91
Raymond Arnold <rarnold@spotify.com>
2017-11-24 17:58:59 -0500

    clarifyPasswordText

--- packages/lesswrong/lib/i18n-en-us/en_US.js
+++ packages/lesswrong/lib/i18n-en-us/en_US.js
@@ -6,7 +6,7 @@ addStrings('en', {
 });
 
 addStrings('en', {
-  "Incorrect password": "Wrong password (If you've just come from LW 1.0 you'll need to reset your password)",
+  "Incorrect password": "Wrong password (If you've just come from LW 1.0 you'll need to reset your password, using the email address associated with your LW 1.0 account.)",
   "User not found": "User not found (Use the intercom if you think that user should exist already)",
   "User has no password set": "LW 1.0 account detected. Please reset your password with the link above",
   "Email already exists.": "Email already exists. If you have an LW 1.0 account, please reset your password with the link above"
