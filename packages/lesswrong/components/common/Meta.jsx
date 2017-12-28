a101f7bcae3e98597dc6d665c2c26458547c0b98
Discordius <panisnecis@gmail.com>
2017-12-28 14:41:42 -0800

    Removed Meta featured section

--- packages/lesswrong/components/common/Meta.jsx
+++ packages/lesswrong/components/common/Meta.jsx
@@ -5,12 +5,8 @@ import { Link } from 'react-router';
 const Meta = (props, context) => {
   const query = props.location && props.location.query;
   const recentPostsTerms = {view: 'top', limit: 10, ...query, meta: true}
-  const featuredPostsTerms = {view: 'featured', limit: 3, meta: true};
   return (
     <div className="home">
-        <Components.Section title="Featured Meta Posts">
-          <Components.PostsList terms={featuredPostsTerms} showHeader={false} showLoadMore={false} />
-        </Components.Section>
         <Components.Section title="Recent Meta Posts"
           titleComponent= {<div className="recent-posts-title-component">
             sorted by<br /> <Components.PostsViews />
