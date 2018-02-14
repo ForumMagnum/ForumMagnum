89dd39a6dc30079a3907a4047c1f31421b453133
Discordius <panisnecis@gmail.com>
2018-02-14 11:44:00 -0800

    Small changes to frontpage commenting guidelines

--- packages/lesswrong/components/comments/ModerationGuidelinesContent.jsx
+++ packages/lesswrong/components/comments/ModerationGuidelinesContent.jsx
@@ -9,15 +9,15 @@ const ModerationGuidelinesContent = ({ user, showFrontpageGuidelines = true, sho
     </div>
   }
   {showFrontpageGuidelines && <div>
-    <p><b>Sitewide commenting guidelines:</b></p>
+    <p><em>Frontpage commenting guidelines:</em></p>
     <p>
-      <em>Aim to explain, not persuade.</em> Write your true reasons for believing something, as opposed to the reasons you think are most likely to persuade readers of your comments. Try to offer concrete models, make predictions, and note what would change your mind.
+      <b>Aim to explain, not persuade.</b> Write your true reasons for believing something, as opposed to the reasons you think are most likely to persuade readers of your comments. Try to offer concrete models, make predictions, and note what would change your mind.
     </p>
     <p>
-      <em>Avoid identity politics.</em> Make personal statements instead of statements that try to represent a group consensus (“I think X is wrong” vs. “X is generally frowned upon”). Avoid stereotypical arguments that will cause others to round <em>you</em> off to someone else they’ve encountered before. Tell people how you think about a topic, instead of repeating someone else’s arguments (e.g. “But Nick Bostrom says…”).
+      <b>Avoid identity politics.</b> Make personal statements instead of statements that try to represent a group consensus (“I think X is wrong” vs. “X is generally frowned upon”). Avoid stereotypical arguments that will cause others to round you off to someone else they’ve encountered before. Tell people how <b>you</b> think about a topic, instead of repeating someone else’s arguments (e.g. “But Nick Bostrom says…”).
     </p>
     <p>
-      <em>Get curious.</em> If I disagree with someone, what might they be thinking; what are the moving parts of their beliefs? What model do I think they are running? Ask yourself - what about this topic do I not understand? What evidence could I get, or what evidence do I already have?
+      <b>Get curious.</b> If I disagree with someone, what might they be thinking; what are the moving parts of their beliefs? What model do I think they are running? Ask yourself - what about this topic do I not understand? What evidence could I get, or what evidence do I already have?
     </p>
 
   </div>}
