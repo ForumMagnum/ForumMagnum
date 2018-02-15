346d74c24abeb028726c53f8f09476ddb8f164a4
Raymond Arnold <rarnold@spotify.com>
2018-02-15 15:57:54 -0800

    deleteModal

--- packages/lesswrong/components/comments/CommentsItem/DeleteCommentMenuItem.jsx
+++ packages/lesswrong/components/comments/CommentsItem/DeleteCommentMenuItem.jsx
@@ -1,14 +1,18 @@
 import React, { PureComponent } from 'react';
 import { registerComponent, withMessages } from 'meteor/vulcan:core';
 import { MenuItem } from 'material-ui';
-import Users from 'meteor/vulcan:users';
 import PropTypes from 'prop-types';
 import withModerateComment from './withModerateComment.js'
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
 
 class DeleteCommentMenuItem extends PureComponent {
 
   constructor(props) {
     super(props);
+    this.state = {
+      open: false,
+    };
   }
 
   handleDelete = (event) => {
@@ -32,8 +36,37 @@ class DeleteCommentMenuItem extends PureComponent {
   }
 
   render() {
+    const modalActions = [
+      <span
+        className="comment-menu-item-delete-modal-button cancel"
+        onTouchTap={()=>{this.setState({open:false})}}>
+        Cancel
+      </span>,
+      <span
+        className="comment-menu-item-delete-modal-button cancel"
+        onTouchTap={()=>{this.setState({open:false})}}>
+        Cancel
+      </span>,
+      <span
+        className="comment-menu-item-delete-modal-button cancel"
+        onTouchTap={()=>{this.setState({open:false})}}>
+        Cancel
+      </span>,
+    ]
+
     if (!this.props.comment.deleted) {
-      return <MenuItem className="comment-menu-item-delete" onTouchTap={ this.handleDelete } primaryText="Delete" />
+      return <MenuItem className="comment-menu-item-delete" onTouchTap={()=>{this.setState({open:true})}} primaryText="Delete">
+        <Dialog
+          style={{zIndex:2101}}
+          title="Delete Comment"
+          actions={modalActions}
+          modal={false}
+          open={this.state.open}
+          onRequestClose={this.handleClose}
+        >
+          <input id="comment-menu-item-delete-reason"/>
+        </Dialog>
+      </MenuItem>
     } else if (this.props.comment.deleted) {
       return <MenuItem onTouchTap={ this.handleUndoDelete } primaryText="Undo Delete" />
     }
