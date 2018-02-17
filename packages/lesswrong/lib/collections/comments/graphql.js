d93e2d60f16c2d273355175d0dacedc1e352bc61
Raymond Arnold <rarnold@spotify.com>
2018-02-16 17:57:02 -0800

    graphql bisiness

--- packages/lesswrong/lib/collections/comments/graphql.js
+++ packages/lesswrong/lib/collections/comments/graphql.js
@@ -15,12 +15,23 @@ const specificResolvers = {
 
       if (Users.canModeratePost(context.currentUser, post)) {
 
-        let modifier = { $set: {
-          deleted: deleted,
-          deletedDate: deleted ? (comment.deletedDate || new Date()) : null,
-          deletedReason: deleted ? deletedReason : null,
-          deletedByUserId: deleted ? context.currentUser._id : null
-        }};
+        let set = {deleted: deleted}
+        let unset = {};
+        if (deleted) {
+          set.deletedPublic = deletedPublic;
+          set.deletedDate = comment.deletedDate || new Date();
+          set.deletedReason = deletedReason;
+          set.deletedByUserId = context.currentUser._id;
+        } else {
+          unset = {
+            deletedPublic: true,
+            deletedDate: true,
+            deletedReason: true,
+            deletedByUserId: true
+          }
+        }
+
+        let modifier = { $set: set, $unset: unset};
         modifier = runCallbacks('comments.moderate.sync', modifier);
         context.Comments.update({_id: commentId}, modifier);
         const updatedComment = context.Comments.findOne(commentId)
