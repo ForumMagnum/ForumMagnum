600a2556e6119f927675ed4bbe8e66b5af95086f
Discordius <panisnecis@gmail.com>
2018-02-17 16:18:30 -0800

    Change to how we query comments (also query deletedPublic comments)

--- packages/lesswrong/lib/collections/comments/views.js
+++ packages/lesswrong/lib/collections/comments/views.js
@@ -4,6 +4,7 @@ Comments.addDefaultView(terms => {
   const validFields = _.pick(terms, 'userId');
   return ({
     selector: {
+      $or: [{$and: [{deleted: true}, {deletedPublic: true}]}, {deleted: {$ne: true}}],
       ...validFields,
     }
   });
@@ -12,48 +13,50 @@ Comments.addDefaultView(terms => {
 
 Comments.addView("postCommentsDeleted", function (terms) {
   return {
-    selector: {postId: terms.postId},
+    selector: {
+      deleted: true,
+      postId: terms.postId
+    },
     options: {sort: {deleted:-1}}
   };
 });
 
 Comments.addView("postCommentsTop", function (terms) {
   return {
-    selector: { postId: terms.postId, deleted: {$ne:true}},
+    selector: { postId: terms.postId },
     options: {sort: {baseScore: -1, postedAt: -1}}
   };
 });
 
 Comments.addView("postCommentsNew", function (terms) {
   return {
-    selector: { postId: terms.postId, deleted: {$ne:true}},
+    selector: { postId: terms.postId },
     options: {sort: {postedAt: -1}}
   };
 });
 
 Comments.addView("postCommentsBest", function (terms) {
   return {
-    selector: { postId: terms.postId, deleted: {$ne:true}},
+    selector: { postId: terms.postId },
     options: {sort: {baseScore: -1}, postedAt: -1}
   };
 });
 
 Comments.addView("allRecentComments", function (terms) {
   return {
-    selector: { deleted:{$ne:true} },
     options: {sort: {postedAt: -1}, limit: terms.limit || 5},
   };
 });
 
 Comments.addView("recentComments", function (terms) {
   return {
-    selector: { deleted:{$ne:true}, score:{$gt:0}},
+    selector: { score:{$gt:0}},
     options: {sort: {postedAt: -1}, limit: terms.limit || 5},
   };
 });
 Comments.addView("topRecentComments", function (terms) {
   return {
-    selector: { deleted:{$ne:true}, score:{$gt:0}, postId:terms.postId},
+    selector: { score:{$gt:0}, postId:terms.postId},
     options: {sort: {baseScore: -1}, limit: terms.limit || 3},
   };
 });
@@ -62,7 +65,6 @@ Comments.addView("postCommentsUnread", function (terms) {
   return {
     selector: {
       postId: terms.postId,
-      deleted: {$ne:true},
     },
     options: {sort: {postedAt: -1}, limit: terms.limit || 5},
   };
