27d37783bc29a99634dadfe925c70ae6a47d9a2d
Discordius <panisnecis@gmail.com>
2018-03-18 22:53:54 -0700

    Commented out unnecessary RSS feed stuff

--- packages/lesswrong/components/feeds/editFeedButton.jsx
+++ packages/lesswrong/components/feeds/editFeedButton.jsx
@@ -1,35 +1,35 @@
-/*
-
-Button used to add a new feed to a user profile
-
-*/
-
-import React, { Component } from 'react';
-import { Components, registerComponent, withCurrentUser, getFragment } from 'meteor/vulcan:core';
-import { withRouter } from 'react-router';
-import RSSFeeds from '../../lib/collections/rssfeeds/collection.js';
-
-class editFeedButton extends Component {
-  render() {
-    return (
-        <Components.SmartForm
-          collection={RSSFeeds}
-          documentId={this.props.feed._id}
-          prefilledProps={ {userId: this.props.feed.userId} }
-          mutationFragment={getFragment('RSSFeedMutationFragment')}
-          successCallback={feed => {
-            this.props.closeModal();
-            this.props.flash("Successfully edited feed", 'success');
-          }}
-          removeSuccessCallback={({documentId, documentTitle}) => {
-            this.props.flash("Successfully deleted feed", "success");
-            // todo: handle events in collection callbacks
-            // this.context.events.track("post deleted", {_id: documentId});
-          }}
-          showRemove={true}
-        />
-      )
-    }
-}
-
-registerComponent('editFeedButton', editFeedButton, withCurrentUser, withRouter);
+// /*
+//
+// Button used to add a new feed to a user profile
+//
+// */
+//
+// import React, { Component } from 'react';
+// import { Components, registerComponent, withCurrentUser, getFragment } from 'meteor/vulcan:core';
+// import { withRouter } from 'react-router';
+// import RSSFeeds from '../../lib/collections/rssfeeds/collection.js';
+//
+// class editFeedButton extends Component {
+//   render() {
+//     return (
+//         <Components.SmartForm
+//           collection={RSSFeeds}
+//           documentId={this.props.feed._id}
+//           prefilledProps={ {userId: this.props.feed.userId} }
+//           mutationFragment={getFragment('RSSFeedMutationFragment')}
+//           successCallback={feed => {
+//             this.props.closeModal();
+//             this.props.flash("Successfully edited feed", 'success');
+//           }}
+//           removeSuccessCallback={({documentId, documentTitle}) => {
+//             this.props.flash("Successfully deleted feed", "success");
+//             // todo: handle events in collection callbacks
+//             // this.context.events.track("post deleted", {_id: documentId});
+//           }}
+//           showRemove={true}
+//         />
+//       )
+//     }
+// }
+//
+// registerComponent('editFeedButton', editFeedButton, withCurrentUser, withRouter);
--- packages/lesswrong/components/feeds/feedItem.jsx
+++ packages/lesswrong/components/feeds/feedItem.jsx
@@ -1,29 +1,28 @@
-import { Components, registerComponent, ModalTrigger } from 'meteor/vulcan:core';
-import React, { Component } from 'react';
-import { ListGroupItem } from 'react-bootstrap';
-
-//TODO: Migrate away from bootstrap
-
-class FeedItem extends Component {
-
-
-  render() {
-    currentUser = this.props.currentUser;
-    feed = this.props.feed;
-    return (
-      <ListGroupItem>
-          {feed.nickname + ": "} <a href={feed.url}> {feed.url} </a>
-          {feed.ownedByUser ? <span className="ownedByUserTag">owned by this user</span> : null}
-          {feed.rawFeed ? <div>Latest Post: <a href={feed.rawFeed[0].link}>feed.rawFeed[0].title</a></div> : null}
-          {"\n"}
-          {feed.displayFullContent ? <span className="displayFullContentTag">fully mirrored on LessWrong</span> : null}
-          <ModalTrigger label={"Edit"}>
-              <Components.editFeedButton feed={feed}/>
-          </ModalTrigger>
-      </ListGroupItem>
-    )
-  }
-
-}
-
-registerComponent('FeedItem', FeedItem);
+// import { Components, registerComponent, ModalTrigger } from 'meteor/vulcan:core';
+// import React, { Component } from 'react';
+// import { ListGroupItem } from 'react-bootstrap';
+//
+// //TODO: Migrate away from bootstrap
+//
+// class FeedItem extends Component {
+//
+//
+//   render() {
+//     const feed = this.props.feed;
+//     return (
+//       <ListGroupItem>
+//           {feed.nickname + ": "} <a href={feed.url}> {feed.url} </a>
+//           {feed.ownedByUser ? <span className="ownedByUserTag">owned by this user</span> : null}
+//           {feed.rawFeed ? <div>Latest Post: <a href={feed.rawFeed[0].link}>feed.rawFeed[0].title</a></div> : null}
+//           {"\n"}
+//           {feed.displayFullContent ? <span className="displayFullContentTag">fully mirrored on LessWrong</span> : null}
+//           <ModalTrigger label={"Edit"}>
+//               <Components.editFeedButton feed={feed}/>
+//           </ModalTrigger>
+//       </ListGroupItem>
+//     )
+//   }
+//
+// }
+//
+// registerComponent('FeedItem', FeedItem);
