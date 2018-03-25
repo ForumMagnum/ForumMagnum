4b9ffc539e14da08c872f97a8fc8ea94ef743125
Discordius <panisnecis@gmail.com>
2018-03-24 22:28:14 -0700

    Remove Winston

--- packages/lesswrong/lib/logging.js
+++ packages/lesswrong/lib/logging.js
@@ -1,16 +1,10 @@
 import { getSetting, addCallback } from 'meteor/vulcan:core';
-import winston from 'winston';
 import timber from 'timber';
 
 function initializeTimber() {
   getSetting('timber.apiKey');
   const transport = new timber.transports.HTTPS(getSetting('timber.apiKey'));
   timber.install(transport);
-
-  winston.remove(winston.transports.Console);
-  winston.add(winston.transports.Console, { formatter: timber.formatters.Winston });
-
-  winston.log('info',"Initializing Timber & Winston");
 }
 
 addCallback('graphql.init.before', initializeTimber)
