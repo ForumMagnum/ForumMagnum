9ffa2b6d8a8437f25c858f79585d58d50de314d9
James Lamine <jglamine@gmail.com>
2018-04-22 18:33:16 -0500

    Remove comment from server.js

--- packages/lesswrong/lib/events/server.js
+++ packages/lesswrong/lib/events/server.js
@@ -3,7 +3,6 @@ import Intercom from 'intercom-client';
 
 // Initiate Intercom on the server
 var intercomClient = new Intercom.Client({ token: getSetting("intercomToken") });
-// console.log(getSetting("intercomToken"));
 
 
 import './callbacks_async.js';
