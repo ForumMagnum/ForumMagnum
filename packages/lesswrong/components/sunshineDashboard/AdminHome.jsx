4532bea1e3aa64482510d2665d302383a8d29927
Raymond Arnold <rarnold@spotify.com>
2018-05-15 17:27:28 -0700

    Admin Dashboard Update

--- packages/lesswrong/components/sunshineDashboard/AdminHome.jsx
+++ packages/lesswrong/components/sunshineDashboard/AdminHome.jsx
@@ -50,6 +50,7 @@ addAdminColumn([
   },
   {
     name: 'createdAt',
+    label: 'Create Date',
     component: DateDisplay,
     order:2,
   },
@@ -62,6 +63,9 @@ addAdminColumn([
     order:4,
   },
 ])
+// Remove the ID and posts field from the users, so that the users fit in page reasonably
+delete AdminColumns[0]
+delete AdminColumns[1]
 
 const eventColumns = [
   {
@@ -93,6 +97,7 @@ const banColumns = [
     component: DateDisplay,
   },
 ]
+
 // columns={['_id', 'createdAt', 'expirationDate', 'type', 'user.username', 'ip']}
 class AdminHome extends PureComponent {
 
@@ -100,65 +105,87 @@ class AdminHome extends PureComponent {
     super(props);
     this.state = {
       sortUsersBy: {createdAt: -1},
+      view: 'LWUsersAdmin',
+      allUsersValue: 1,
     };
   }
 
   render() {
     return (
       <div className="admin-home page">
-        <Components.ShowIf check={Users.isAdmin} document={this.props.currentUser} failureComponent={<p className="admin-home-message"><FormattedMessage id="app.noPermission" /></p>}>
-          <h2>Admin Console</h2>
-          <div>
-            <div className="admin-new-ip-bans">
-              <h3>New IP Bans</h3>
-              <Components.SmartForm
-                collection={Bans}
-              />
-            </div>
-            <div className="admin-current-ip-bans">
-              <h3>Current IP Bans</h3>
-              <Components.Datatable
-                collection={Bans}
-                columns={banColumns}
-                options={{
-                  fragmentName: 'BansAdminPageFragment',
-                  terms: {},
-                  limit: 10,
-                }}
-                showEdit={true}
-              />
-            </div>
-            <div className="admin-recent-logins">
-              <h3>Recent Logins</h3>
-              <Components.Datatable
-                collection={LWEvents}
-                columns={eventColumns}
-                options={{
-                  fragmentName: 'lwEventsAdminPageFragment',
-                  terms: {view: "adminView", name: 'login'},
-                  limit: 10,
-                }}
-              />
-            </div>
-            <div className="admin-all-users">
-              <h3>All Users</h3>
-              <DropDownMenu value={1} onChange={this.handleChange} openImmediately={true}>
-                <MenuItem value={1} primaryText="Created At (Descending)" onTouchTap={(e) => {this.setState({sortUsersBy:{createdAt:-1}})}}/>
-                <MenuItem value={2} primaryText="Created At (Ascending)" onTouchTap={(e) => {this.setState({sortUsersBy:{createdAt:1}})}}/>
-                <MenuItem value={3} primaryText="Karma (Descending)" onTouchTap={(e) => {this.setState({sortUsersBy:{karma:-1}})}}/>
-                <MenuItem value={4} primaryText="Karma (Ascending)" onTouchTap={(e) => {this.setState({sortUsersBy:{karma:1}})}}/>
-              </DropDownMenu>
-              <Components.Datatable
-                collection={Users}
-                columns={AdminColumns}
-                options={{
-                  fragmentName: 'UsersAdmin',
-                  terms: {view: 'LWUsersAdmin', sort:this.state.sortUsersBy},
-                  limit: 20
-                }}
-                showEdit={true}
-                showNew={false}
-              />
+        <Components.ShowIf
+          check={Users.isAdmin}
+          document={this.props.currentUser}
+          failureComponent={
+            <p className="admin-home-message"><FormattedMessage id="app.noPermission" /></p>
+          }
+        >
+          <div className="admin-home-layout">
+            <h2>Admin Console</h2>
+            <div>
+              <div className="admin-recent-logins">
+                <h3>Recent Logins</h3>
+                <Components.Datatable
+                  collection={LWEvents}
+                  columns={eventColumns}
+                  options={{
+                    fragmentName: 'lwEventsAdminPageFragment',
+                    terms: {view: 'adminView', name: 'login'},
+                    limit: 10,
+                  }}
+                />
+              </div>
+              <div className="admin-all-users">
+                <h3>All Users</h3>
+                <DropDownMenu
+                  value={this.state.allUsersValue}
+                  onChange={this.handleChange}
+                  openImmediately={true}
+                >
+                  <MenuItem value={1} primaryText="Created At (Descending)"
+                    onTouchTap={(e) => {this.setState({allUsersValue:1, view:'LWUsersAdmin', sortUsersBy:{createdAt:-1}})}}/>
+                  <MenuItem value={2} primaryText="Created At (Ascending)"
+                    onTouchTap={(e) => {this.setState({allUsersValue:2, view:'LWUsersAdmin', sortUsersBy:{createdAt:1}})}}/>
+                  <MenuItem value={3} primaryText="Karma (Descending)"
+                    onTouchTap={(e) => {this.setState({allUsersValue:3, view:'LWUsersAdmin', sortUsersBy:{karma:-1}})}}/>
+                  <MenuItem value={4} primaryText="Karma (Ascending)"
+                    onTouchTap={(e) => {this.setState({allUsersValue:4, view:'LWUsersAdmin', sortUsersBy:{karma:1}})}}/>
+                  <MenuItem value={5} primaryText="Sunshines"
+                    onTouchTap={(e) => {this.setState({allUsersValue:5, view:'LWSunshinesList', sortUsersBy:{karma:1}})}} />
+                  <MenuItem value={6} primaryText="TrustLevel1"
+                    onTouchTap={(e) => {this.setState({allUsersValue:6, view:'LWTrustLevel1List', sortUsersBy:{karma:1}})}} />
+                </DropDownMenu>
+                <Components.Datatable
+                  collection={Users}
+                  columns={AdminColumns}
+                  options={{
+                    fragmentName: 'UsersAdmin',
+                    terms: {view: this.state.view, sort:this.state.sortUsersBy},
+                    limit: 20
+                  }}
+                  showEdit={true}
+                  showNew={false}
+                />
+              </div>
+              <div className="admin-new-ip-bans">
+                <h3>New IP Bans</h3>
+                <Components.SmartForm
+                  collection={Bans}
+                />
+              </div>
+              <div className="admin-current-ip-bans">
+                <h3>Current IP Bans</h3>
+                <Components.Datatable
+                  collection={Bans}
+                  columns={banColumns}
+                  options={{
+                    fragmentName: 'BansAdminPageFragment',
+                    terms: {},
+                    limit: 10,
+                  }}
+                  showEdit={true}
+                />
+              </div>
             </div>
           </div>
         </Components.ShowIf>
--- packages/lesswrong/lib/collections/users/views.js
+++ packages/lesswrong/lib/collections/users/views.js
@@ -1,5 +1,23 @@
 import Users from "meteor/vulcan:users";
 
+Users.addView('LWSunshinesList', function(terms) {
+  return {
+    selector: {groups:'sunshineRegiment'},
+    options: {
+      sort: terms.sort
+    }
+  }
+});
+
+Users.addView('LWTrustLevel1List', function(terms) {
+  return {
+    selector: {groups:'trustLevel1'},
+    options: {
+      sort: terms.sort
+    }
+  }
+});
+
 Users.addView('LWUsersAdmin', terms => ({
   options: {
     sort: terms.sort
