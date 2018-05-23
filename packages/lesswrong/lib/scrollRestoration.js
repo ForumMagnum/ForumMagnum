2610a2b85d38a7ce80127c80c98721705b453cdf
Raymond Arnold <rarnold@spotify.com>
2018-05-23 13:46:02 -0700

    fix comment error

--- packages/lesswrong/lib/scrollRestoration.js
+++ packages/lesswrong/lib/scrollRestoration.js
@@ -2,7 +2,8 @@ import { Meteor } from 'meteor/meteor';
 
 /* When refreshing the page, tell the browser to remember the scroll position.
  * Otherwise, users get scrolled to the top of the page.
- */ (See https://github.com/Discordius/Lesswrong2/issues/295#issuecomment-385866050)
+ */
+ // (See https://github.com/Discordius/Lesswrong2/issues/295#issuecomment-385866050)
 function rememberScrollPositionOnPageReload() {
   window.addEventListener('beforeunload', () => {
     if ('scrollRestoration' in window.history) {
