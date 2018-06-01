2975262c0681939c94a8ea38608d75ba494bedb0
Discordius <panisnecis@gmail.com>
2018-06-01 14:31:29 -0700

    Fixed intercomClient construction and sample_settings.json

--- packages/lesswrong/lib/events/server.js
+++ packages/lesswrong/lib/events/server.js
@@ -2,8 +2,11 @@ import { getSetting } from 'meteor/vulcan:lib';
 import Intercom from 'intercom-client';
 
 // Initiate Intercom on the server
-var intercomClient = new Intercom.Client({ token: getSetting("intercomToken") });
-
+const intercomToken = getSetting("intercomToken", null);
+let intercomClient = {}
+if (intercomToken) {
+  intercomClient = new Intercom.Client({ token: getSetting("intercomToken") });
+}
 
 import './callbacks_async.js';
 
