edb40079d34b7f6421ce8e929e25767d0c2ffccf
Discordius <panisnecis@gmail.com>
2018-06-02 12:35:16 -0700

    Removed Vulcan Accounts package from primary repository

--- packages/vulcan-accounts/imports/routes.js
+++
@@ -1,4 +0,0 @@
-import { addRoute } from 'meteor/vulcan:core';
-
-addRoute({name: 'resetPassword', path: '/reset-password/:token', componentName: 'AccountsResetPassword'});
-addRoute({name: 'enrollAccount', path: '/enroll-account/:token', componentName: 'AccountsEnrollAccount'});
