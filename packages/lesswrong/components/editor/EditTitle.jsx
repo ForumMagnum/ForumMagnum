e12c57a0843d6670a73e26509ddfb4e775fcea13
Discordius <panisnecis@gmail.com>
2018-06-02 12:02:29 -0700

    Updated EditTitle and EditUrl for new Vulcan version

--- packages/lesswrong/components/editor/EditTitle.jsx
+++ packages/lesswrong/components/editor/EditTitle.jsx
@@ -4,13 +4,13 @@ import PropTypes from 'prop-types';
 import { Textarea } from 'formsy-react-components';
 
 const EditTitle = (props) => {
+  console.log("EditTitle props:", props);
   return <Textarea
-            className="posts-edit-header-title"
-            name={ props.name }
-            value={ props.value }
-            placeholder={ props.placeholder }
-            layout="elementOnly"
-          />
+    className="posts-edit-header-title"
+    {...props.inputProperties}
+    placeholder={ props.placeholder }
+    layout="elementOnly"
+         />
 }
 
 registerComponent("EditTitle", EditTitle);
