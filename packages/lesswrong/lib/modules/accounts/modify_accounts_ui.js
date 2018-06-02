70c48e9297791aa08414ed2be486ed5b06809a62
Discordius <panisnecis@gmail.com>
2018-06-01 18:53:44 -0700

    Copied Vulcan Accounts over

---
+++ packages/lesswrong/lib/modules/accounts/modify_accounts_ui.js
@@ -0,0 +1,7 @@
+import { Accounts } from 'meteor/accounts-base';
+
+Accounts.ui._options = {
+  passwordSignupFields: 'USERNAME_AND_OPTIONAL_EMAIL',
+  onPostSignUpHook: () => {},
+  onSignedInHook: () => {},
+};
