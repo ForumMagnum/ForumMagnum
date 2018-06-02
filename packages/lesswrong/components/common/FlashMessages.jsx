1798e491960f56129d854df9b889ba1094a99bf6
Discordius <panisnecis@gmail.com>
2018-06-02 11:49:48 -0700

    Updated FlashMessages for new Vulcan version

--- packages/lesswrong/components/common/FlashMessages.jsx
+++ packages/lesswrong/components/common/FlashMessages.jsx
@@ -1,6 +1,7 @@
 import { Components, replaceComponent, withMessages } from 'meteor/vulcan:core';
 import React, { Component } from 'react';
 import Snackbar from 'material-ui/Snackbar';
+import { intlShape } from 'meteor/vulcan:i18n';
 
 class FlashMessages extends Component {
   handleRequestClose = () => {
@@ -14,21 +15,45 @@ class FlashMessages extends Component {
     }
   };
 
+  getProperties = (message) => {
+    if (typeof message === 'string') {
+      // if error is a string, use it as message
+      return {
+        message: message,
+        type: 'error'
+      }
+    } else {
+      // else return full error object after internationalizing message
+      const { id, messageString, properties } = message;
+      const translatedMessage = this.context.intl.formatMessage({ id, defaultMessage: messageString }, properties);
+      return {
+        ...message,
+        message: translatedMessage,
+      };
+    }
+  }
+
   render() {
     let messages = this.props.messages.filter(message => message.show);
+    let messageObject = messages.length > 0 && messages[0].id && this.getProperties(messages[0]);
     return (
       <div className="flash-messages">
         <Snackbar
-          open={messages.length > 0}
-          message={messages.length > 0 && messages[0].content}
+          open={!!messageObject}
+          message={messageObject && messageObject.message}
           autoHideDuration = {4000}
           onRequestClose={this.handleRequestClose}
         />
       </div>
     );
   }
-
 }
+
+FlashMessages.contextTypes = {
+  intl: intlShape
+}
+
+
 
 FlashMessages.displayName = "FlashMessages";
 
