78329f867540f99f6f10831d3a4f7e47eeaae2db
Discordius <panisnecis@gmail.com>
2018-06-04 12:50:46 -0700

    Don't overwrite old acccounts_ui options field

--- packages/lesswrong/lib/modules/accounts/modify_accounts_ui.js
+++ packages/lesswrong/lib/modules/accounts/modify_accounts_ui.js
@@ -1,6 +1,7 @@
 import { Accounts } from 'meteor/accounts-base';
 
 Accounts.ui._options = {
+  ...Accounts.ui._options,
   passwordSignupFields: 'USERNAME_AND_OPTIONAL_EMAIL',
   onPostSignUpHook: () => {},
   onSignedInHook: () => {},
