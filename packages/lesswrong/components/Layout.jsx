4eab7be4f8b1ac2864768c72e01aea4d0212d68a
Discordius <panisnecis@gmail.com>
2018-07-09 17:48:37 -0700

    Removed MUIPickersUtilProvider

--- packages/lesswrong/components/Layout.jsx
+++ packages/lesswrong/components/Layout.jsx
@@ -16,8 +16,6 @@ import { customizeTheme } from '../lib/modules/utils/theme';
 import Typekit from 'react-typekit';
 import { withStyles } from '@material-ui/core/styles';
 
-import MomentUtils from 'material-ui-pickers/utils/moment-utils';
-import MuiPickersUtilsProvider from 'material-ui-pickers/utils/MuiPickersUtilsProvider';
 
 
 const styles = theme => ({
@@ -48,35 +46,33 @@ const Layout = ({currentUser, children, currentRoute, params, client, classes}, 
     }
 
     return <div className="wrapper tk-warnock-pro" id="wrapper">
-      <MuiPickersUtilsProvider utils={MomentUtils}>
-        <V0MuiThemeProvider muiTheme={customizeTheme(currentRoute, userAgent, params, client.store)}>
-          <div>
-            <CssBaseline />
-            <Helmet>
-              <title>{getSetting('forumSettings.tabTitle', 'LessWrong 2.0')}</title>
-              <link name="material-icons" rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/icon?family=Material+Icons"/>
-              <link name="react-instantsearch" rel="stylesheet" type="text/css" href="https://unpkg.com/react-instantsearch-theme-algolia@4.0.0/style.min.css"/>
-              <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"/>
-              <meta httpEquiv="Accept-CH" content="DPR, Viewport-Width, Width"/>
-            </Helmet>
-            <Typekit kitId="jvr1gjm" />
-            {/* Deactivating this component for now, since it's been causing a good amount of bugs. TODO: Fix this properly */}
-            {/* {currentUser ? <Components.UsersProfileCheck currentUser={currentUser} documentId={currentUser._id} /> : null} */}
+      <V0MuiThemeProvider muiTheme={customizeTheme(currentRoute, userAgent, params, client.store)}>
+        <div>
+          <CssBaseline />
+          <Helmet>
+            <title>{getSetting('forumSettings.tabTitle', 'LessWrong 2.0')}</title>
+            <link name="material-icons" rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/icon?family=Material+Icons"/>
+            <link name="react-instantsearch" rel="stylesheet" type="text/css" href="https://unpkg.com/react-instantsearch-theme-algolia@4.0.0/style.min.css"/>
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"/>
+            <meta httpEquiv="Accept-CH" content="DPR, Viewport-Width, Width"/>
+          </Helmet>
+          <Typekit kitId="jvr1gjm" />
+          {/* Deactivating this component for now, since it's been causing a good amount of bugs. TODO: Fix this properly */}
+          {/* {currentUser ? <Components.UsersProfileCheck currentUser={currentUser} documentId={currentUser._id} /> : null} */}
 
-            {/* Sign up user for Intercom, if they do not yet have an account */}
-            {showIntercom(currentUser)}
-            <noscript className="noscript-warning"> This website requires javascript to properly function. Consider activating javascript to get access to all site functionality. </noscript>
-            <Components.Header {...this.props}/>
+          {/* Sign up user for Intercom, if they do not yet have an account */}
+          {showIntercom(currentUser)}
+          <noscript className="noscript-warning"> This website requires javascript to properly function. Consider activating javascript to get access to all site functionality. </noscript>
+          <Components.Header {...this.props}/>
 
-            <div className={classes.main}>
-              <Components.FlashMessages />
-              {children}
-              <Components.SunshineSidebar />
-            </div>
-            {/* <Components.Footer />  Deactivated Footer, since we don't use one. Might want to add one later*/ }
+          <div className={classes.main}>
+            <Components.FlashMessages />
+            {children}
+            <Components.SunshineSidebar />
           </div>
-        </V0MuiThemeProvider>
-      </MuiPickersUtilsProvider>
+          {/* <Components.Footer />  Deactivated Footer, since we don't use one. Might want to add one later*/ }
+        </div>
+      </V0MuiThemeProvider>
     </div>
 }
 
