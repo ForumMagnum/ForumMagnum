fae5a5faafa179426b74a503ffd2fd44a8fe8b44
Raymond Arnold <rarnold@spotify.com>
2018-07-11 16:39:52 -0700

    nonFrontpage posts only on unreviewed posts list

--- packages/lesswrong/components/sunshineDashboard/SunshineSidebar.jsx
+++ packages/lesswrong/components/sunshineDashboard/SunshineSidebar.jsx
@@ -6,7 +6,7 @@ const SunshineSidebar = (props) => {
   if (Users.canDo(props.currentUser, 'posts.moderate.all')) {
     return (
       <div className="sunshine-sidebar">
-        <Components.SunshineNewUsersList terms={{view:"sunshineNewUsers"}}/>
+        {/* <Components.SunshineNewUsersList terms={{view:"sunshineNewUsers"}}/> */}
         <Components.SunshineNewPostsList terms={{view:"sunshineNewPosts"}}/>
         <Components.SunshineReportedCommentsList terms={{view:"sunshineSidebarReports"}}/>
         <Components.SunshineNewCommentsList terms={{view:"sunshineNewCommentsList"}}/>
