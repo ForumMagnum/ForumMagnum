84e6ca913e4fe2d728dd0a891f8d9bd5e68db4cc
James Babcock <jimrandomh@gmail.com>
2018-07-21 19:30:40 -0400

    Add CommentsList to set of components with error boundaries

--- packages/lesswrong/components/comments/CommentsList.jsx
+++ packages/lesswrong/components/comments/CommentsList.jsx
@@ -1,43 +1,51 @@
 import { Components, replaceComponent, withEdit } from 'meteor/vulcan:core';
-import React from 'react';
+import React, { Component } from 'react';
 import { FormattedMessage } from 'meteor/vulcan:i18n';
 import { Comments } from "meteor/example-forum";
 
-const CommentsList = ({
-  comments,
-  currentUser,
-  highlightDate,
-  editMutation,
-  post,
-  frontPage,
-}) => {
-  if (comments) {
-    return (
-      <div className="comments-list">
-        {comments.map(comment =>
-          <Components.CommentsNode
-            currentUser={currentUser}
-            comment={comment}
-            key={comment._id}
-            highlightDate={highlightDate}
-            editMutation={editMutation}
-            post={post}
-            frontPage={frontPage}
-          />)
-        }
-      </div>
-    )
-  } else {
-    return (
-      <div className="comments-list">
-        <p>
-          <FormattedMessage id="comments.no_comments"/>
-        </p>
-      </div>
-    )
+class CommentsList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
   }
-
-};
+  
+  render() {
+    if (this.state.error) {
+      return <div className="errorText">Error rendering comments list: {this.state.error}</div>
+    }
+    
+    let {comments, currentUser, highlightDate, editMutation, post, frontPage} = this.props;
+    if (comments) {
+      return (
+        <div className="comments-list">
+          {comments.map(comment =>
+            <Components.CommentsNode
+              currentUser={currentUser}
+              comment={comment}
+              key={comment._id}
+              highlightDate={highlightDate}
+              editMutation={editMutation}
+              post={post}
+              frontPage={frontPage}
+            />)
+          }
+        </div>
+      )
+    } else {
+      return (
+        <div className="comments-list">
+          <p>
+            <FormattedMessage id="comments.no_comments"/>
+          </p>
+        </div>
+      )
+    }
+  }
+  
+  componentDidCatch(error, info) {
+    this.setState({error:error.toString()});
+  }
+}
 
 CommentsList.displayName = "CommentsList";
 
