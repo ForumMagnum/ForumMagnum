7ee7f0594f4594ac9c621cbbf86233b33336e13f
Sam Deere <dev@colophonemes.com>
2018-07-24 17:18:51 -0700

    Make Grid design responsive at smaller breakpoints

--- packages/lesswrong/components/common/Section.jsx
+++ packages/lesswrong/components/common/Section.jsx
@@ -14,50 +14,57 @@ const styles = (theme) => ({
     marginBottom: theme.spacing.unit * 4,
   },
   sectionTitleContainer: {
-    textAlign: 'right',
-    display: 'inline',
+    [theme.breakpoints.up('md')]: {
+      textAlign: 'right',
+      display: 'inline',
+    }
   },
   sectionTitle: {
-    display: 'inline',
-    position: 'relative',
-    top: (theme.spacing.unit) + BORDER_TOP_WIDTH,
-    '&:before': {
-      top: ((theme.spacing.unit) - BORDER_TOP_WIDTH) * -1,
-      position: 'absolute',
-      width: '100%',
-      borderTopStyle: 'solid',
+    [theme.breakpoints.down('sm')]: {
       borderTopWidth: BORDER_TOP_WIDTH,
-      content: '""'
+      borderTopStyle: 'solid',
+      paddingTop: theme.spacing.unit
+    },
+    [theme.breakpoints.up('md')]: {
+      display: 'inline',
+      position: 'relative',
+      top: theme.spacing.unit + BORDER_TOP_WIDTH,
+      '&:before': {
+        top: (theme.spacing.unit - BORDER_TOP_WIDTH) * -1,
+        position: 'absolute',
+        width: '100%',
+        borderTopStyle: 'solid',
+        borderTopWidth: BORDER_TOP_WIDTH,
+        content: '""'
+      }
     }
   },
   sectionTitleTop: {
-    paddingBottom: theme.spacing.unit,
-    marginBottom: theme.spacing.unit
+    [theme.breakpoints.up('md')]: {
+      paddingBottom: theme.spacing.unit,
+      marginBottom: theme.spacing.unit
+    }
   },
-  sectionTitleBottom: {
-    // TODO: do we need this class?
-  },
-  sectionContent: {
-    // TODO: do we need this class?
-  }
+  // left to provide overrides
+  sectionTitleBottom: {},
+  sectionContent: {}
 })
 
 const Section = ({contentStyle, title, /*titleWidth = 220, contentWidth = 715,*/ titleLink, titleComponent, children, classes}) => {
 
   return (
     <Grid container className={classes.section}>
-      <Grid item md={3} className={classes.sectionTitleContainer}>
+      <Grid item xs={12} md={3} className={classes.sectionTitleContainer}>
         {title && <div className={classes.sectionTitleTop}>
           <Typography variant="display1" className={classes.sectionTitle}>
-            {!titleLink && <span>{title}</span>}
-            {titleLink && <Link to={titleLink}>{title}</Link>}
+            {!titleLink ? <span>{title}</span> : <Link to={titleLink}>{title}</Link>}
           </Typography>
         </div>}
         {titleComponent && <div className={classes.sectionTitleBottom}>
           {titleComponent}
         </div>}
       </Grid>
-      <Grid item md={9}>
+      <Grid item xs={12} md={9}>
         <div className={classes.sectionContent}>
           {children}
         </div>
