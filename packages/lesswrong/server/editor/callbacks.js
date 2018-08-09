3b83da5bc22296c489f6621f3795417d215206fb
Discordius <panisnecis@gmail.com>
2018-08-08 18:28:35 -0700

    Removed async callbacks and moved everything to sync

--- packages/lesswrong/server/editor/callbacks.js
+++ packages/lesswrong/server/editor/callbacks.js
@@ -1,109 +1,70 @@
 import { addCallback } from 'meteor/vulcan:core';
 import { Posts, Comments } from 'meteor/example-forum';
 
-function PostsNewHTMLSerializeCallback (post) {
+async function PostsNewHTMLSerializeCallback (post) {
   if (post.content) {
-    const newPostFields = Posts.convertFromContent(post.content, post._id, post.slug);
-    post = {...post, ...newPostFields}
+    const newFields = await Posts.convertFromContentAsync(post.content, post._id, post.slug);
+    post = {...post, ...newFields}
   } else if (post.body) {
-    const newPostFields = Posts.convertFromMarkdown(post.body, post._id, post.slug)
-    post = {...post, ...newPostFields}
+    const newFields = await Posts.convertFromMarkdownAsync(post.body, post._id, post.slug)
+    post = {...post, ...newFields}
   } else if (post.htmlBody) {
-    const newPostFields = Posts.convertFromHTML(post.htmlBody, post._id, post.slug);
-    post = {...post, ...newPostFields}
+    const newFields = Posts.convertFromHTML(post.htmlBody, post._id, post.slug);
+    post = {...post, ...newFields}
   }
   return post
 }
 
 addCallback("posts.new.sync", PostsNewHTMLSerializeCallback);
 
-function CommentsNewHTMLSerializeCallback (comment) {
+async function CommentsNewHTMLSerializeCallback (comment) {
   if (comment.content) {
-    const newPostFields = Posts.convertFromContent(comment.content);
-    comment = {...comment, ...newPostFields}
+    const newFields = await Posts.convertFromContentAsync(comment.content);
+    comment = {...comment, ...newFields}
   } else if (comment.body) {
-    const newPostFields = Posts.convertFromMarkdown(comment.body)
-    comment = {...comment, ...newPostFields}
+    const newFields = await Posts.convertFromMarkdownAsync(comment.body)
+    comment = {...comment, ...newFields}
   } else if (comment.htmlBody) {
-    const newPostFields = Posts.convertFromHTML(comment.htmlBody);
-    comment = {...comment, ...newPostFields}
+    const newFields = Posts.convertFromHTML(comment.htmlBody);
+    comment = {...comment, ...newFields}
   }
   return comment
 }
 
 addCallback("comments.new.sync", CommentsNewHTMLSerializeCallback);
 
-function PostsEditHTMLSerializeCallback (modifier, post) {
+async function PostsEditHTMLSerializeCallback (modifier, post) {
   if (modifier.$set && modifier.$set.content) {
-    const newPostFields = Posts.convertFromContent(modifier.$set.content, post._id, post.slug)
-    modifier.$set = {...modifier.$set, ...newPostFields}
+    const newFields = await Posts.convertFromContentAsync(modifier.$set.content, post._id, post.slug)
+    modifier.$set = {...modifier.$set, ...newFields}
     delete modifier.$unset.htmlBody
   } else if (modifier.$set && modifier.$set.body) {
-    const newPostFields = Posts.convertFromMarkdown(modifier.$set.body, post._id, post.slug)
-    modifier.$set = {...modifier.$set, ...newPostFields}
+    const newFields = await Posts.convertFromMarkdownAsync(modifier.$set.body, post._id, post.slug)
+    modifier.$set = {...modifier.$set, ...newFields}
     delete modifier.$unset.htmlBody
   } else if (modifier.$set && modifier.$set.htmlBody) {
-    const newPostFields = Posts.convertFromHTML(modifier.$set.htmlBody, post._id, post.slug);
-    modifier.$set = {...modifier.$set, ...newPostFields}
+    const newFields = Posts.convertFromHTML(modifier.$set.htmlBody, post._id, post.slug);
+    modifier.$set = {...modifier.$set, ...newFields}
   }
   return modifier
 }
 
 addCallback("posts.edit.sync", PostsEditHTMLSerializeCallback);
 
-function CommentsEditHTMLSerializeCallback (modifier, comment) {
+async function CommentsEditHTMLSerializeCallback (modifier, comment) {
   if (modifier.$set && modifier.$set.content) {
-    const newPostFields = Comments.convertFromContent(modifier.$set.content)
-    modifier.$set = {...modifier.$set, ...newPostFields}
+    const newFields = await Comments.convertFromContentAsync(modifier.$set.content)
+    modifier.$set = {...modifier.$set, ...newFields}
     delete modifier.$unset.htmlBody
   } else if (modifier.$set && modifier.$set.body) {
-    const newPostFields = Comments.convertFromMarkdown(modifier.$set.body)
-    modifier.$set = {...modifier.$set, ...newPostFields}
+    const newFields = await Comments.convertFromMarkdownAsync(modifier.$set.body)
+    modifier.$set = {...modifier.$set, ...newFields}
     delete modifier.$unset.htmlBody
   } else if (modifier.$set && modifier.$set.htmlBody) {
-    const newPostFields = Comments.convertFromHTML(modifier.$set.htmlBody);
-    modifier.$set = {...modifier.$set, ...newPostFields}
+    const newFields = Comments.convertFromHTML(modifier.$set.htmlBody);
+    modifier.$set = {...modifier.$set, ...newFields}
   }
   return modifier
 }
 
 addCallback("comments.edit.sync", CommentsEditHTMLSerializeCallback);
-
-
-async function PostsHTMLSerializeCallbackAsync (newPost, oldPost) {
-  let newPostFields = {}
-
-  if (newPost.content !== oldPost.content) {
-    newPostFields = await Posts.convertFromContentAsync(newPost.content);
-  } else if (newPost.body !== oldPost.body) {
-    newPostFields = await Posts.convertFromMarkdownAsync(newPost.body, newPost._id, newPost.slug)
-  } else if (newPost.htmlBody !== oldPost.htmlBody) {
-    newPostFields = Posts.convertFromHTML(newPost.htmlBody, newPost._id, newPost.slug);
-  }
-
-  if (newPostFields) {
-    Posts.update({_id: newPost._id}, {$set: newPostFields})
-  }
-}
-
-addCallback("posts.edit.async", PostsHTMLSerializeCallbackAsync);
-addCallback("posts.new.async", PostsHTMLSerializeCallbackAsync);
-
-export async function CommentsHTMLSerializeCallbackAsync (newComment, oldComment) {
-  let newFields = {}
-
-  if (newComment.content !== oldComment.content) {
-    newFields = await Comments.convertFromContentAsync(newComment.content);
-  } else if (newComment.body !== oldComment.body) {
-    newFields = await Comments.convertFromMarkdownAsync(newComment.body)
-  } else if (newComment.htmlBody !== oldComment.htmlBody) {
-    newFields = Comments.convertFromHTML(newComment.htmlBody);
-  }
-
-  if (newFields) {
-    Comments.update({_id: newComment._id}, {$set: newFields})
-  }
-}
-
-addCallback("comments.edit.async", CommentsHTMLSerializeCallbackAsync);
-addCallback("comments.new.async", CommentsHTMLSerializeCallbackAsync);
