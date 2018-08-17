a3c9163ddfde27122c0e34d480187937c57bc40d
JP Addison <johnpaddison@gmail.com>
2018-08-17 12:21:51 -0700

    some silly things in the theme loading

--- packages/lesswrong/themes/forumTheme.js
+++ packages/lesswrong/themes/forumTheme.js
@@ -1,8 +1,8 @@
 import { getSetting } from 'meteor/vulcan:core'
 
-import lwTheme from '../themes/lesswrongTheme'
-import eaForumTheme from '../themes/eaForumTheme'
+import lwTheme from './lesswrongTheme'
+import eaForumTheme from './eaForumTheme'
 
-const forumTheme = getSetting('EAForum', false) ? eaForumTheme : lwTheme
+const forumTheme = getSetting('EAForum') ? eaForumTheme : lwTheme
 
 export default forumTheme
