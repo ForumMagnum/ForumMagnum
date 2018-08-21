fc8d0d6d2834312b4dfae13fc1ac5c2f6b5ab8c9
Discordius <panisnecis@gmail.com>
2018-08-20 17:30:46 -0700

    Factored out sequences navigation

--- packages/lesswrong/components/sequences/SequencesNavigation.jsx
+++ packages/lesswrong/components/sequences/SequencesNavigation.jsx
@@ -1,4 +1,4 @@
-import { Components, registerComponent, withDocument } from 'meteor/vulcan:core';
+import { Components, registerComponent, withDocument, Utils } from 'meteor/vulcan:core';
 import Sequences from '../../lib/collections/sequences/collection.js';
 import { Link } from 'react-router';
 import React from 'react';
@@ -9,68 +9,31 @@ const SequencesNavigation = ({
     loading,
     post
   }) => {
-    let prevPostUrl = ""
-    let nextPostUrl = ""
+    if (document && !loading) {
+      let title = document ? document.title : ""
+      let titleUrl = documentId ? "/s/" + documentId : ""
+      const { nextPost, prevPost } = Utils.getSequencePostLinks(document, post)
+      console.log("nextPost, prevPost: ", nextPost, prevPost, Utils.getSequencePostLinks(document, post))
+      return (
+        <div className="sequences-navigation-top">
+          <Components.SequencesNavigationLink
+            documentId={ prevPost && prevPost._id }
+            documentUrl={ "/s/" + document._id + (prevPost ? ("/p/" + prevPost._id) : "")}
+          direction="left" />
 
-    let prevPostId = ""
-    let nextPostId = ""
+          <div className="sequences-navigation-title">
+            {title ? <Link to={ titleUrl }>{ title }</Link> : <Components.Loading/>}
+          </div>
 
-    let title = document ? document.title : ""
-    let titleUrl = documentId ? "/s/" + documentId : ""
-
-    if (document && post && !loading) {
-      if (document.chapters) {
-        let currentChapter = false;
-        let currentPostIndex = false;
-        let currentChapterIndex = false;
-        let currentSequenceLength = document.chapters.length;
-        document.chapters.forEach((c) => {
-          if(c.posts && _.pluck(c.posts, '_id').indexOf(post._id) > -1) {
-            currentChapter = c
-            currentPostIndex = _.pluck(c.posts, '_id').indexOf(post._id);
-            currentChapterIndex = _.pluck(document.chapters, '_id').indexOf(c._id);
-          }
-        })
-        if (currentPostIndex || currentPostIndex === 0) {
-          if (currentPostIndex + 1 < currentChapter.posts.length) {
-            nextPostId = currentChapter.posts[currentPostIndex + 1]._id
-            nextPostUrl = "/s/" + document._id + "/p/" + nextPostId;
-          } else if (currentChapterIndex + 1 < currentSequenceLength && document.chapters[currentChapterIndex + 1].posts.length !== 0) {
-            nextPostId = document.chapters[currentChapterIndex + 1].posts[0]._id
-            nextPostUrl = "/s/" + document._id + "/p/" + nextPostId;
-          } else {
-            nextPostUrl = "/s/" + document._id;
-          }
-
-          if (currentPostIndex > 0) {
-            prevPostId = currentChapter.posts[currentPostIndex - 1]._id
-            prevPostUrl = "/s/" + document._id + "/p/" + prevPostId;
-          } else if (currentChapterIndex > 1) {
-            prevPostId = document.chapters[currentChapterIndex - 1].posts[document.chapters[currentChapterIndex-1].length - 1]._id
-            prevPostUrl = "/s/" + document._id + "/p/" + prevPostId;
-          } else {
-            prevPostUrl = "/s/" + document._id + "/p/" + document._id;
-          }
-        }
-      }
-    }
-    return (
-      <div className="sequences-navigation-top">
-        <Components.SequencesNavigationLink
-          documentId={ prevPostId }
-          documentUrl={ prevPostUrl }
-        direction="left" />
-
-        <div className="sequences-navigation-title">
-          {title ? <Link to={ titleUrl }>{ title }</Link> : <Components.Loading/>}
+          <Components.SequencesNavigationLink
+            documentId={ nextPost && nextPost._id }
+            documentUrl={ "/s/" + document._id + (nextPost ? ("/p/" + nextPost._id) : "")}
+          direction="right" />
         </div>
-
-        <Components.SequencesNavigationLink
-          documentId={ nextPostId }
-          documentUrl={ nextPostUrl }
-        direction="right" />
-      </div>
-    )
+      )
+    } else {
+      return <div><Components.Loading /></div>
+    }
   }
 
 const options = {
---
+++ packages/lesswrong/lib/collections/sequences/utils.js
@@ -0,0 +1,51 @@
+import { Utils } from 'meteor/vulcan:core'
+
+console.log("Adding sequence function to Utils")
+Utils.getSequencePostLinks = (sequence, post) => {
+  if (sequence && post) {
+    if (sequence.chapters) {
+      const { currentChapter, postIndex, chapterIndex } = Utils.getCurrentChapter(sequence, post)
+      let currentSequenceLength = sequence.chapters.length;
+      let nextPost, prevPost
+      if (postIndex || postIndex === 0) {
+        if (postIndex + 1 < currentChapter.posts.length) {
+          nextPost = currentChapter.posts[postIndex + 1]
+        } else if (chapterIndex + 1 < currentSequenceLength
+          && document.chapters[chapterIndex + 1].posts.length !== 0) {
+          nextPost = document.chapters[chapterIndex + 1].posts[0]
+        }
+
+        if (postIndex > 0) {
+          prevPost = currentChapter.posts[postIndex - 1]
+        } else if (chapterIndex > 1) {
+          prevPost = document.chapters[chapterIndex - 1].posts[document.chapters[chapterIndex-1].length - 1]
+        }
+      }
+      return {nextPost: nextPost, prevPost: prevPost, currentChapter: currentChapter, postIndex: postIndex, chapterIndex: chapterIndex}
+    }
+  }
+}
+
+/*
+  getCurrentChapter: Given a sequence and a current post, returns the current chapter,
+  the index of the current post in the chapter, and the index of the chapter in the sequence
+*/
+Utils.getCurrentChapter = (sequence, post) => {
+  const chapters = sequence.chapters
+  if (chapters) {
+    let result
+    chapters.forEach((c, i) => { //c: chapter, i: chapterIndex
+      const postIndex = _.pluck(c.posts, '_id').indexOf(post._id)
+      if (postIndex >= 0) {
+        result = {currentChapter: c, postIndex, chapterIndex: i}
+      }
+    })
+    if (result) {
+      return result
+    } else {
+      throw Error("No chapter found in sequence " + sequence._id + "that that includes post: " + post._id)
+    }
+  } else {
+    throw Error("Sequence has no chapters, can't find currentChapter")
+  }
+}
