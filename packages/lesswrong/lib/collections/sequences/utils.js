2a428226eed2715f8c5a9d73c32f7bab7b31f640
Discordius <panisnecis@gmail.com>
2018-08-20 18:16:04 -0700

    Add indent and default null values

--- packages/lesswrong/lib/collections/sequences/utils.js
+++ packages/lesswrong/lib/collections/sequences/utils.js
@@ -5,15 +5,14 @@ Utils.getSequencePostLinks = (sequence, post) => {
     if (sequence.chapters) {
       const { currentChapter, postIndex, chapterIndex } = Utils.getCurrentChapter(sequence, post)
       let currentSequenceLength = sequence.chapters.length;
-      let nextPost, prevPost
+      let nextPost = null, prevPost = null
       if (postIndex || postIndex === 0) {
         if (postIndex + 1 < currentChapter.posts.length) {
           nextPost = currentChapter.posts[postIndex + 1]
         } else if (chapterIndex + 1 < currentSequenceLength
-          && document.chapters[chapterIndex + 1].posts.length !== 0) {
+            && document.chapters[chapterIndex + 1].posts.length !== 0) {
           nextPost = document.chapters[chapterIndex + 1].posts[0]
         }
-
         if (postIndex > 0) {
           prevPost = currentChapter.posts[postIndex - 1]
         } else if (chapterIndex > 1) {
@@ -32,7 +31,7 @@ Utils.getSequencePostLinks = (sequence, post) => {
 Utils.getCurrentChapter = (sequence, post) => {
   const chapters = sequence.chapters
   if (chapters) {
-    let result
+    let result = null
     chapters.forEach((c, i) => { //c: chapter, i: chapterIndex
       const postIndex = _.pluck(c.posts, '_id').indexOf(post._id)
       if (postIndex >= 0) {
