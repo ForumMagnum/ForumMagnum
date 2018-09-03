3c10a56ee919bf5067bbbc2b0bc92d8ac6142502
Raymond Arnold <rarnold@spotify.com>
2018-09-03 16:32:54 -0700

    Search refactor (3 columns + styling)

--- packages/lesswrong/components/search/CommentsSearchHit.jsx
+++ packages/lesswrong/components/search/CommentsSearchHit.jsx
@@ -2,31 +2,32 @@ import { Components, registerComponent} from 'meteor/vulcan:core';
 import moment from 'moment';
 import { Link } from 'react-router';
 import { Snippet } from 'react-instantsearch/dom';
-
+import { withStyles } from '@material-ui/core/styles';
 import React, { PureComponent } from 'react';
+
+const styles = theme => ({
+  root: {
+    margin: theme.spacing.unit
+  }
+})
 
 const isLeftClick = (event) => {
   return event.button === 0 && !event.ctrlKey && !event.metaKey;
 }
 
-const CommentsSearchHit = ({hit, clickAction}) => {
-  return <div className="search-results-comments-item recent-comments-item comments-item">
-    <Link
-      to={"/posts/" + hit.postId + "/" + hit.postSlug + "#" + hit._id}
-      onClick={(event) => isLeftClick(event) && clickAction()}
-    >
-      <div className="comments-item-body recent-comments-item-body ">
-        <div className="comments-item-meta recent-comments-item-meta">
-          <div className="search-results-comments-item-username">{hit.authorDisplayName}</div>
-          <div className="search-results-comments-item-score">{hit.baseScore} points </div>
-          <div className="comments-item-date"> {moment(new Date(hit.postedAt)).fromNow()}</div>
-        </div>
-        <div className="recent-comments-item-text comments-item-text">
-          <Snippet attributeName="body" hit={hit} tagName="mark" />
-        </div>
-        </div>
-      </Link>
-  </div>
+const CommentsSearchHit = ({hit, clickAction, classes}) => {
+  return <Link to={"/posts/" + hit.postId + "/" + hit.postSlug + "#" + hit._id} onClick={(event) => isLeftClick(event) && clickAction()} className={classes.root}>
+    <div>
+      <Components.MetaInfo>{hit.authorDisplayName}</Components.MetaInfo>
+      <Components.MetaInfo>{hit.baseScore} points </Components.MetaInfo>
+      <Components.MetaInfo>
+        {moment(new Date(hit.postedAt)).fromNow()}
+      </Components.MetaInfo>
+    </div>
+    <div>
+      <Snippet attributeName="body" hit={hit} tagName="mark" />
+    </div>
+  </Link>
 }
 
-registerComponent("CommentsSearchHit", CommentsSearchHit);
+registerComponent("CommentsSearchHit", CommentsSearchHit, withStyles(styles));
