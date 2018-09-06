6b0a1245aa9d0e000ff970f9600b6acf10e3d1b5
Raymond Arnold <rarnold@spotify.com>
2018-09-06 15:43:10 -0700

    Add users to conversations

--- packages/lesswrong/components/search/UsersAutoCompleteHit.jsx
+++ packages/lesswrong/components/search/UsersAutoCompleteHit.jsx
@@ -2,18 +2,19 @@ import { Components, registerComponent } from 'meteor/vulcan:core';
 import React from 'react';
 import moment from 'moment';
 
-
 const UsersAutoCompleteHit = ({document, removeItem }) => {
   if (document) {
-    return <span className="users-search-hit-body">
-      <span className="users-search-hit-name">
+    return <div>
+      <Components.MetaInfo>
         {document.displayName}
-      </span>
-      <span className="users-search-hit-meta">
-        <span className="users-search-hit-karma">{document.karma} points </span>
-        <span className="users-search-hit-created-date"> {moment(new Date(document.createdAt)).fromNow()}</span>
-      </span>
-    </span>
+      </Components.MetaInfo>
+      <Components.MetaInfo>
+        {document.karma} points
+      </Components.MetaInfo>
+      <Components.MetaInfo>
+        {moment(new Date(document.createdAt)).fromNow()}
+      </Components.MetaInfo>
+    </div>
   } else {
     return <Components.Loading />
   }
