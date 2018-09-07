dd8dd43699570f18a439fa118c021588fc281ccd
Raymond Arnold <rarnold@spotify.com>
2018-09-06 18:48:31 -0700

    added propTypes

--- packages/lesswrong/components/comments/CommentsItem/CommentBody.jsx
+++ packages/lesswrong/components/comments/CommentsItem/CommentBody.jsx
@@ -2,6 +2,7 @@ import { Components, getRawComponent, registerComponent } from 'meteor/vulcan:co
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import { commentBodyStyles } from '../../../themes/stylePiping'
+import PropTypes from 'prop-types';
 
 const styles = theme => ({
   commentStyling: {
@@ -13,10 +14,16 @@ const CommentBody = ({comment, classes}) => {
   const htmlBody = {__html: comment.htmlBody};
   return (
     <div className={classes.commentStyling}>
-      {htmlBody && !comment.deleted && <div className="comment-body" dangerouslySetInnerHTML={htmlBody}></div>}
+      {!comment.deleted && <div className="comment-body" dangerouslySetInnerHTML={htmlBody}></div>}
       {comment.deleted && <div className="comment-body"><Components.CommentDeletedMetadata documentId={comment._id}/></div>}
     </div>
   )
 }
+
+CommentBody.propTypes = {
+  comment: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
+};
+
 
 registerComponent('CommentBody', CommentBody, withStyles(styles));
