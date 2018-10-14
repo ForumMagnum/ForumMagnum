cc3f732a9fc1e3234cbe5ad1769c365e9a6a3ce1
James Babcock <jimrandomh@gmail.com>
2018-10-14 19:00:41 -0400

    SequencesNavigationLink: Old->new material-UI
    ...and some style changes to compensate for differences between the old
    and new IconButton classes.

--- packages/lesswrong/components/sequences/SequencesNavigationLink.jsx
+++ packages/lesswrong/components/sequences/SequencesNavigationLink.jsx
@@ -1,18 +1,26 @@
 import { Components, registerComponent, withDocument} from 'meteor/vulcan:core';
 import { Posts } from '../../lib/collections/posts';
-import IconButton from 'material-ui/IconButton'
+import IconButton from '@material-ui/core/IconButton'
+import Tooltip from '@material-ui/core/Tooltip';
+import NavigateBefore from '@material-ui/icons/NavigateBefore'
+import NavigateNext from '@material-ui/icons/NavigateNext'
 import React from 'react';
 import { withRouter } from 'react-router';
 import { withStyles } from '@material-ui/core/styles';
+import classnames from 'classnames';
 
 const styles = theme => ({
+  root: {
+    padding: 0,
+    margin: 12,
+  },
   normal: {
-    "& .material-icons": {
+    "& svg": {
       color: "rgba(0,0,0, 0.5) !important"
     }
   },
   disabled: {
-    "& .material-icons": {
+    "& svg": {
       color: "rgba(0,0,0, 0.2) !important"
     }
   },
@@ -30,16 +38,21 @@ const SequencesNavigationLink = ({
   ) => {
     const post = (slug || documentId) && document
     const disabled = (!slug && !documentId);
-    return (
+    const button = (
       <IconButton
-        className={disabled ? classes.disabled : classes.normal}
+        classes={{
+          root: classnames(classes.root, disabled ? classes.disabled : classes.normal)
+        }}
         disabled={disabled}
-        iconClassName="material-icons"
-        tooltip={post && post.title}
         onClick={() => router.push(documentUrl)}>
-        { direction === "left" ? "navigate_before" : "navigate_next" }
+        { direction === "left" ? <NavigateBefore/> : <NavigateNext/> }
        </IconButton>
      )
+    if (post && post.title) {
+      return <Tooltip title={post.title}>{button}</Tooltip>
+    } else {
+      return button;
+    }
 };
 
 const options = {
