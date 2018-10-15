3766f3a7c75ab62d3fce841f5785eac10c4d2528
James Babcock <jimrandomh@gmail.com>
2018-10-14 21:05:13 -0400

    Add legacy breakpoints to theme.js. RecommendedReading SCSS->JSS.

--- packages/lesswrong/components/sequences/RecommendedReading.jsx
+++ packages/lesswrong/components/sequences/RecommendedReading.jsx
@@ -2,10 +2,59 @@ import { Components, registerComponent } from 'meteor/vulcan:core';
 import { withRouter, Link } from 'react-router';
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
+import { legacyBreakpoints } from '../../lib/modules/utils/theme';
+import classnames from 'classnames';
 
 const styles = theme => ({
   root: {
     position: "relative"
+  },
+  
+  post: {
+    width: 300,
+    display: "inline-block",
+    marginTop: -15,
+    paddingBottom: 55,
+  
+    [legacyBreakpoints.maxSmall]: {
+      width: "100%",
+      textAlign: "left",
+      paddingLeft: 25,
+      paddingBottom: 10,
+    },
+    [legacyBreakpoints.maxTiny]: {
+      paddingLeft: 5,
+    }
+  },
+  
+  prevPost: {
+  },
+  
+  nextPost: {
+    float: "right",
+    
+    [legacyBreakpoints.maxSmall]: {
+      paddingBottom: 50
+    }
+  },
+  
+  divider: {
+    position: "absolute",
+    height: "110px",
+    marginTop: "10px",
+    marginLeft: "auto",
+    marginRight: "auto",
+    width: "0px",
+    borderLeftStyle: "solid",
+    borderLeftWidth: "1px",
+    color: "rgba(0,0,0,0.3)",
+    left: 0,
+    right: 0,
+    top: 0,
+    
+    [legacyBreakpoints.maxSmall]: {
+      display: "none"
+    }
   },
   
   nextSequenceDirection: {
@@ -16,14 +65,15 @@ const styles = theme => ({
   clear: {
     clear: "both"
   }
-});
+})
 
 const RecommendedReading = ({sequence, chapter, post, previousPost, nextPost, nextTitle, nextLink, collectionTitle, classes}) => {
   return <div className={classes.root}>
-    {(previousPost && sequence) ? <div className="sequences-navigation-bottom-previous-post">
+    {(previousPost && sequence) ? <div className={classnames(classes.post, classes.prevPost)}>
       <Components.RecommendedReadingItem direction="Previous" post={previousPost} sequence={sequence}/>
     </div> : null}
-    <div className="sequences-navigation-bottom-divider"></div>
+    <div className={classes.divider}></div>
+    {/* TODO: This is currently unreachable (nextTitle is never passed). Hook it up, test the styles, then JSS-ify them. */}
     { nextTitle ?
       <div className="sequences-navigation-bottom-next-sequence">
         <Link className="sequences-navigation-next-sequence" to={nextLink || post.nextPageLink}>
@@ -31,7 +81,7 @@ const RecommendedReading = ({sequence, chapter, post, previousPost, nextPost, ne
         </Link>
       </div>
     : (
-      (nextPost && sequence) ? <div className="sequences-navigation-bottom-next-post">
+      (nextPost && sequence) ? <div className={classnames(classes.post, classes.nextPost)}>
         <Components.RecommendedReadingItem direction="Next" post={nextPost} sequence={sequence}/>
       </div> : null
     ) }
--- packages/lesswrong/lib/modules/utils/theme.js
+++ packages/lesswrong/lib/modules/utils/theme.js
@@ -119,5 +119,17 @@ export const customizeTheme = (router, userAgent, params, store) => {
     muiTheme.palette.accent1Color = "rgba(105,201,192,.5)"
     muiTheme.palette.accent2Color = "rgba(105,201,192,1)"
   }
+  
   return muiTheme
 }
+
+const lwTiny = "400px";
+const lwSmall = "715px";
+const lwMedium = "950px";
+export const legacyBreakpoints = {
+  maxTiny: "@media screen and (max-width: "+lwTiny+")",
+  maxSmall: "@media screen and (max-width: "+lwSmall+")",
+  minSmall: "@media screen and (min-width: "+lwSmall+")",
+  maxMedium: "@media screen and (max-width: "+lwMedium+")",
+  minMedium: "@media screen and (min-width: "+lwMedium+")",
+};
