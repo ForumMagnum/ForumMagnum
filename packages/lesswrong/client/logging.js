49e438b617763445ad716fe4d1bb203a118d20de
Discordius <panisnecis@gmail.com>
2018-10-15 16:30:36 -0700

    Capture console.error as errors

--- packages/lesswrong/client/logging.js
+++ packages/lesswrong/client/logging.js
@@ -1,4 +1,12 @@
 import * as Sentry from '@sentry/browser';
 
 // Initializing sentry on the client browser
-Sentry.init({ dsn: 'https://1ab1949fc8d04608b43132f37bb2a1b0@sentry.io/1301611' });
+Sentry.init({
+  dsn: 'https://1ab1949fc8d04608b43132f37bb2a1b0@sentry.io/1301611',
+  beforeBreadcrumb(breadcrumb, hint) {
+    if (breadcrumb.level === "error" && breadcrumb.message) {
+      Sentry.captureException(breadcrumb.message)
+    }
+    return breadcrumb
+  },
+});
