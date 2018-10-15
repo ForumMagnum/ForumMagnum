c3d6926c10e8d09984e9d66d28d89a00226ab0ae
James Babcock <jimrandomh@gmail.com>
2018-10-14 20:05:07 -0400

    RecommendedReading: Start SCSS->JSS

--- packages/lesswrong/components/sequences/RecommendedReading.jsx
+++ packages/lesswrong/components/sequences/RecommendedReading.jsx
@@ -1,17 +1,33 @@
 import { Components, registerComponent } from 'meteor/vulcan:core';
 import { withRouter, Link } from 'react-router';
 import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
 
-const RecommendedReading = ({sequence, chapter, post, previousPost, nextPost, nextTitle, nextLink, collectionTitle}) => {
-  return <div className="sequences-navigation-bottom-content">
+const styles = theme => ({
+  root: {
+    position: "relative"
+  },
+  
+  nextSequenceDirection: {
+    fontWeight: 600,
+    fontSize: "1.2rem",
+  },
+  
+  clear: {
+    clear: "both"
+  }
+});
+
+const RecommendedReading = ({sequence, chapter, post, previousPost, nextPost, nextTitle, nextLink, collectionTitle, classes}) => {
+  return <div className={classes.root}>
     {(previousPost && sequence) ? <div className="sequences-navigation-bottom-previous-post">
       <Components.RecommendedReadingItem direction="Previous" post={previousPost} sequence={sequence}/>
     </div> : null}
     <div className="sequences-navigation-bottom-divider"></div>
     { nextTitle ?
-      <div className="sequences-navigation-bottom-next-sequence next-only">
+      <div className="sequences-navigation-bottom-next-sequence">
         <Link className="sequences-navigation-next-sequence" to={nextLink || post.nextPageLink}>
-          <div className="sequences-navigation-next-sequence-direction">Next Sequence:</div> {nextTitle || post.nextPageTitle}
+          <div className={classes.nextSequenceDirection}>Next Sequence:</div> {nextTitle || post.nextPageTitle}
         </Link>
       </div>
     : (
@@ -19,9 +35,10 @@ const RecommendedReading = ({sequence, chapter, post, previousPost, nextPost, ne
         <Components.RecommendedReadingItem direction="Next" post={nextPost} sequence={sequence}/>
       </div> : null
     ) }
-    <div className="sequences-navigation-bottom-clear"></div>
+    <div className={classes.clear}></div>
   </div>
 };
 
 
-registerComponent('RecommendedReading', RecommendedReading,  withRouter);
+registerComponent('RecommendedReading', RecommendedReading,
+  withRouter, withStyles(styles, {name: "RecommendedReading"}));
