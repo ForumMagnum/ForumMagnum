2543c382816f5b4aed6720538c1d45240982f2ff
Discordius <panisnecis@gmail.com>
2018-10-15 18:46:23 -0700

    Add userId to google Analytics

--- packages/lesswrong/client/logging.js
+++ packages/lesswrong/client/logging.js
@@ -1,5 +1,5 @@
 import * as Sentry from '@sentry/browser';
-import { getSetting } from 'meteor/vulcan:core'
+import { getSetting, addCallback } from 'meteor/vulcan:core'
 
 const sentryUrl = getSetting('sentry.url');
 const sentryEnvironment = getSetting('sentry.environment');
@@ -15,3 +15,20 @@ Sentry.init({
   environment: sentryEnvironment
 });
 // Initializing sentry on the client browser
+
+function identifyUserToSentry(user) {
+  // Set user in sentry scope
+  Sentry.configureScope((scope) => {
+    scope.setUser({id: user._id, email: user.email, username: user.username});
+  });
+}
+
+addCallback('events.identify', identifyUserToSentry)
+
+function addUserIdToGoogleAnalytics(user) {
+  if (window && window.ga) {
+    window.ga('set', 'userId', user._id); // Set the user ID using signed-in user_id.
+  }
+}
+
+addCallback('events.identify', addUserIdToGoogleAnalytics)
