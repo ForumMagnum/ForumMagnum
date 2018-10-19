b22cbcd8d54a4adb7780b2fe2370caf2b771c4d9
Discordius <panisnecis@gmail.com>
2018-10-19 11:04:51 -0700

    Add release to sentry init

--- packages/lesswrong/client/logging.js
+++ packages/lesswrong/client/logging.js
@@ -3,6 +3,7 @@ import { getSetting, addCallback } from 'meteor/vulcan:core'
 
 const sentryUrl = getSetting('sentry.url');
 const sentryEnvironment = getSetting('sentry.environment');
+const sentryRelease = getSetting('sentry.release')
 
 Sentry.init({
   dsn: sentryUrl,
@@ -12,7 +13,8 @@ Sentry.init({
     }
     return breadcrumb
   },
-  environment: sentryEnvironment
+  environment: sentryEnvironment,
+  release: sentryRelease,
 });
 // Initializing sentry on the client browser
 
