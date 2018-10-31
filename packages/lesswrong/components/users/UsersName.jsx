0151c0b0f888feeb79726a2abfcc7ed61edafca9
Raymond Arnold <rarnold@spotify.com>
2018-10-30 17:06:44 -0700

    UsersNameDisplay

--- packages/lesswrong/components/users/UsersName.jsx
+++ packages/lesswrong/components/users/UsersName.jsx
@@ -1,28 +1,19 @@
-import { registerComponent, Components, getSetting, withDocument } from 'meteor/vulcan:core';
+import { registerComponent, Components } from 'meteor/vulcan:core';
 import React from 'react';
 import PropTypes from 'prop-types';
-import Users from 'meteor/vulcan:users';
-import { Link } from 'react-router';
 
-const UsersName = ({user, document, documentId, loading}) => {
+const UsersName = ({user, documentId}) => {
   if (documentId) {
     return <Components.UsersNameWrapper documentId={documentId} />
   } else {
-    return <Link className="users-name" to={Users.getProfileUrl(user)}>
-      {getSetting('AlignmentForum', false) ? (user.fullName || Users.getDisplayName(user)) : Users.getDisplayName(user)}
-    </Link>
+    return <Components.UsersNameDisplay user={user}/>
   }
 }
 UsersName.propTypes = {
-  user: PropTypes.object.isRequired,
+  user: PropTypes.object,
+  documentId: PropTypes.string,
 }
 
 UsersName.displayName = 'UsersName';
 
-const documentOptions = {
-  collection: Users,
-  queryName: 'UsersNameQuery',
-  fragmentName: 'UsersMinimumInfo',
-};
-
-registerComponent('UsersName', UsersName, [withDocument, documentOptions]);
+registerComponent('UsersName', UsersName);
--- packages/lesswrong/components/users/UsersNameWrapper.jsx
+++ packages/lesswrong/components/users/UsersNameWrapper.jsx
@@ -1,6 +1,7 @@
 import { Components, registerComponent, withDocument} from 'meteor/vulcan:core';
 import Users from 'meteor/vulcan:users';
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const UsersNameWrapper = ({document, documentId, loading}) => {
   if (!document && loading) {
@@ -12,6 +13,12 @@ const UsersNameWrapper = ({document, documentId, loading}) => {
 };
 
 UsersNameWrapper.displayName = "UsersNameWrapper";
+
+UsersNameWrapper.propTypes = {
+  documentId: PropTypes.string.isRequired,
+  document: PropTypes.object,
+  loading: PropTypes.bool
+}
 
 const options = {
   collection: Users,
