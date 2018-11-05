1004ec364388a10a7496e093733c5a667dc93bef
James Babcock <jimrandomh@gmail.com>
2018-11-05 12:53:47 -0500

    Add missing `withUser` in `SequencesPostsList`
    Fixes #1238 (can't vote on comment previews on Sequence pages).

--- packages/lesswrong/components/sequences/SequencesPostsList.jsx
+++ packages/lesswrong/components/sequences/SequencesPostsList.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { registerComponent, Components } from 'meteor/vulcan:core';
+import withUser from '../common/withUser';
 
-const SequencesPostsList = ({posts, chapter}) =>
+const SequencesPostsList = ({posts, chapter, currentUser}) =>
 
   <div className="sequences-posts-list">
-    {posts.map((post) => <Components.PostsItem key={post._id} post={post} chapter={chapter} />)}
+    {posts.map((post) => <Components.PostsItem key={post._id} post={post} chapter={chapter} currentUser={currentUser} />)}
   </div>
 
-registerComponent('SequencesPostsList', SequencesPostsList)
+registerComponent('SequencesPostsList', SequencesPostsList, withUser)
