2c5d33f67b0dbac053e719469d43bde2f7fe45e0
James Babcock <jimrandomh@gmail.com>
2018-11-19 19:56:10 -0800

    Ctrl+Left/Right navigates sequences

--- packages/lesswrong/components/sequences/CollectionsNavigation.jsx
+++ packages/lesswrong/components/sequences/CollectionsNavigation.jsx
@@ -1,47 +1,81 @@
 import { Components, registerComponent } from 'meteor/vulcan:core';
-import { Link } from 'react-router';
-import React from 'react';
+import { withRouter, Link } from 'react-router';
+import React, { PureComponent } from 'react';
 
-const CollectionsNavigation = ({
-  nextPostUrl,
-  prevPostUrl,
-  title,
-  titleUrl,
-  loading,
+class CollectionsNavigation extends PureComponent
+{
+  componentDidMount() {
+    if (!Meteor.isServer && document) {
+      document.addEventListener("keydown", this.handleKey);
+    }
+  }
   
-  // Two different ways of identifying the prev/next post for SequencesNavigation,
-  // depending on whether this is a collection or a sequence.
-  nextPostId, prevPostId,
-  nextPostSlug, prevPostSlug,
-}) => {
-
-  return (
-    <div className="sequences-navigation-top">
-      {loading ? <Components.Loading/> : <React.Fragment>
-        {prevPostUrl
-          ? <Components.SequencesNavigationLink
-              documentUrl={prevPostUrl}
-              direction="left"
-              documentId={prevPostId}
-              slug={prevPostSlug} />
-          : <Components.SequencesNavigationLinkDisabled
-              direction="left" />}
+  componentWillUnmount() {
+    if (!Meteor.isServer && document) {
+      document.removeEventListener("keydown", this.handleKey);
+    }
+  }
   
-        <div className="sequences-navigation-title">
-          {title ? <Link to={ titleUrl }>{ title }</Link> : <Components.Loading/>}
-        </div>
+  handleKey = (ev) => {
+    // Only if Ctrl and no other modifiers
+    if (ev.ctrlKey && !ev.shiftKey && !ev.altKey && !ev.metaKey) {
+      // Check the targe of the event; we don't want to navigate if you're
+      // trying to use Ctrl+Left/Right to move the cursor inside eg a comment
+      // box. Apply the hotkey if the target is either document.body (nothing
+      // selected) or is an <a> tag (a spurious selection because you opened
+      // a link in a new tab, usually).
+      if (ev.target === document.body || (ev.target && ev.target.tagName === 'A')) {
+        if (ev.keyCode == 37) { // Left
+          this.props.router.push(this.props.prevPostUrl);
+        } else if (ev.keyCode == 39) { // Right
+          this.props.router.push(this.props.nextPostUrl);
+        }
+      }
+    }
+  }
   
-        {nextPostUrl
-          ? <Components.SequencesNavigationLink
-              documentUrl={nextPostUrl}
-              direction="right"
-              documentId={nextPostUrl && nextPostId}
-              slug={nextPostUrl && nextPostSlug} />
-          : <Components.SequencesNavigationLinkDisabled
-              direction="right" />}
-      </React.Fragment>}
-    </div>
-  )
+  render() {
+    const {
+      nextPostUrl,
+      prevPostUrl,
+      title,
+      titleUrl,
+      loading,
+      
+      // Two different ways of identifying the prev/next post for SequencesNavigation,
+      // depending on whether this is a collection or a sequence.
+      nextPostId, prevPostId,
+      nextPostSlug, prevPostSlug,
+    } = this.props;
+    
+    return (
+      <div className="sequences-navigation-top">
+        {loading ? <Components.Loading/> : <React.Fragment>
+          {prevPostUrl
+            ? <Components.SequencesNavigationLink
+                documentUrl={prevPostUrl}
+                direction="left"
+                documentId={prevPostId}
+                slug={prevPostSlug} />
+            : <Components.SequencesNavigationLinkDisabled
+                direction="left" />}
+    
+          <div className="sequences-navigation-title">
+            {title ? <Link to={ titleUrl }>{ title }</Link> : <Components.Loading/>}
+          </div>
+    
+          {nextPostUrl
+            ? <Components.SequencesNavigationLink
+                documentUrl={nextPostUrl}
+                direction="right"
+                documentId={nextPostUrl && nextPostId}
+                slug={nextPostUrl && nextPostSlug} />
+            : <Components.SequencesNavigationLinkDisabled
+                direction="right" />}
+        </React.Fragment>}
+      </div>
+    )
+  }
 }
 
-registerComponent('CollectionsNavigation', CollectionsNavigation);
+registerComponent('CollectionsNavigation', CollectionsNavigation, withRouter);
