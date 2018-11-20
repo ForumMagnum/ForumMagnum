6cdb36684f7119c24ec391cbcae04eb8be0f3a80
JP Addison <johnpaddison@gmail.com>
2018-11-20 14:39:16 -0800

    todos and console logs in some scripts

--- packages/lesswrong/server/database-import/cleanup_scripts.js
+++ packages/lesswrong/server/database-import/cleanup_scripts.js
@@ -1,4 +1,5 @@
 /* global Vulcan */
+
 import Users from 'meteor/vulcan:users'
 import { Posts } from '../../lib/collections/posts'
 import { Comments } from '../../lib/collections/comments'
@@ -6,9 +7,10 @@ import { bulkUpdateWithJS } from '../scripts/utils'
 import { recalculateBaseScore } from 'meteor/vulcan:voting';
 
 Vulcan.renameDuplicateUsernames = async () => {
-  await bulkUpdateWithJS({
+  const result = await bulkUpdateWithJS({
     collection: Users,
     query: {username: /_duplicate/},
+    queryOptions: {limit: 1},
     updateFunction: document => {
       const newUsername = document.username.replace(/_duplicate.*/, '')
       return {
@@ -21,16 +23,20 @@ Vulcan.renameDuplicateUsernames = async () => {
 
     }
   })
+  console.log('result outer', result)
 }
 
 Vulcan.updateBaseScores = async () => {
-  for (collection of [Posts, Comments]) {
-    await bulkUpdateWithJS({
+  for (collection of [Posts]) { // [Posts, Comments]) {
+    const result = await bulkUpdateWithJS({
       collection,
+      query: {_id: 'pjt5x3ASwrPY2YyhJ'},           // TODO remove ------------------------
       updateFunction: document => {
+        // TODO why are votes not working to get joeys post back to 19
         const newBaseScore = recalculateBaseScore(document._id)
         return {$set: {baseScore: newBaseScore}}
       }
     })
+    console.log('result outer', result)
   }
 }
