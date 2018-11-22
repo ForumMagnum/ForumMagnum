e17a7e6617762ce5274e9c6ea17ca981f4f3afe9
James Babcock <jimrandomh@gmail.com>
2018-11-22 13:15:08 -0800

    Default to indexes in background; add removeObsoleteIndexes

--- packages/lesswrong/lib/collectionUtils.js
+++ packages/lesswrong/lib/collectionUtils.js
@@ -2,6 +2,19 @@
 export function ensureIndex(collection, index, options)
 {
   if (Meteor.isServer) {
-    collection._ensureIndex(index, options);
+    const mergedOptions = {background: true, ...options};
+    collection._ensureIndex(index, mergedOptions);
+  }
+}
+
+export function removeObsoleteIndexes(collection, indexes)
+{
+  if (Meteor.isServer) {
+    try {
+      for(let i=0; i<indexes.length; i++)
+        collection._dropIndex(indexes[i]);
+    } catch(e) {
+      // Swallow exception, to make this idempotent
+    }
   }
 }
