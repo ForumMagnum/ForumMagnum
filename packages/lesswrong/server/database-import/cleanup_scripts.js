cf8b1d813d40e92d332a419dfb0667aaae3f511c
JP Addison <johnpaddison@gmail.com>
2018-11-27 14:54:18 -0800

    clean up and document scripts

--- packages/lesswrong/server/database-import/cleanup_scripts.js
+++ packages/lesswrong/server/database-import/cleanup_scripts.js
@@ -2,12 +2,11 @@
 
 import Users from 'meteor/vulcan:users'
 import { Posts } from '../../lib/collections/posts'
-import { Comments } from '../../lib/collections/comments'
 import { bulkUpdateWithJS } from '../scripts/utils'
 import { recalculateBaseScore } from 'meteor/vulcan:voting';
 
 Vulcan.renameDuplicateUsernames = async () => {
-  const result = await bulkUpdateWithJS({
+  await bulkUpdateWithJS({
     collection: Users,
     query: {username: /_duplicate/},
     queryOptions: {limit: 1},
@@ -23,20 +22,16 @@ Vulcan.renameDuplicateUsernames = async () => {
 
     }
   })
-  console.log('result outer', result)
 }
 
 Vulcan.updateBaseScores = async () => {
   for (collection of [Posts]) { // [Posts, Comments]) {
-    const result = await bulkUpdateWithJS({
+    await bulkUpdateWithJS({
       collection,
-      query: {_id: 'pjt5x3ASwrPY2YyhJ'},           // TODO remove ------------------------
       updateFunction: document => {
-        // TODO why are votes not working to get joeys post back to 19
         const newBaseScore = recalculateBaseScore(document._id)
         return {$set: {baseScore: newBaseScore}}
       }
     })
-    console.log('result outer', result)
   }
 }
--- packages/lesswrong/server/scripts/utils.js
+++ packages/lesswrong/server/scripts/utils.js
@@ -1,9 +1,13 @@
-// TODO; really should have something to deal with write errors without blowing up
-
-// TODO; comment
+// Bulk apply a js function to a mongo collection; this is possibly less
+// performant, but much easier to write than the mongo update syntax.
+//
+// query and queryOptions are passed directly to Meteor's collection.find method.
+//
+// TODO: Make a slower version of this that handles errors better
+// Despite the presence of writeErrors in the result, the update appears to
+// crash and stop on some errors
 export const bulkUpdateWithJS = async ({collection, query={}, queryOptions={}, updateFunction}) => {
   const documents = collection.find(query, queryOptions)
-  console.log('docs', documents)
   const updates = documents.map(document => ({
     updateOne: {
       filter: {
@@ -12,12 +16,31 @@ export const bulkUpdateWithJS = async ({collection, query={}, queryOptions={}, u
       update: updateFunction(document)
     }
   }))
-  console.log('updates', updates)
-  const result = await collection.rawCollection().bulkWrite(updates)
-  console.log('result', result)
+  await collection.rawCollection().bulkWrite(updates)
 }
 
-// TODO; comment
+// Better dev experience for running async scripts from the meteor shell.
+//
+// When calling async functions from the command line, it is unavoidable that
+// the function will return instantly. The program will continue to run and to
+// log output in the main meteor program. We would like to know definitively
+// when the script exits. While we're at it, we can learn what errors caused
+// the script to crash, and get the total runtime.
+// Note that this is a function which takes a function and returns a function.
+// Use case is as follows:
+//
+// In some script file (remember to import the file in lesswrong/server.js!):
+//
+// import { wrapVulcanAsyncScript } from './utils'
+//
+// Vulcan.fixAllTheThings = wrapVulcanAsyncScript('fixAllTheThings', async (foo, bar) => {
+//   await fixThing1(foo)
+//   await fixThing2(bar)
+// })
+//
+// In the meteor shell:
+//
+// > Vulcan.fixAllTheThings('fee', 'bee')
 export const wrapVulcanAsyncScript = (name, scriptFunc) => async (...args) => {
   try {
     console.log(`================ ${name} ================`)
