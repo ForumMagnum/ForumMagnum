3d9a750ab3bd0edfe2380a1a144b1bd41c095b76
Discordius <panisnecis@gmail.com>
2018-12-02 20:36:09 -0800

    Merge akismet patch into master

--- packages/lesswrong/server/akismet.js
+++ packages/lesswrong/server/akismet.js
@@ -2,25 +2,40 @@ import LWEvents from '../lib/collections/lwevents/collection'
 import Reports from '../lib/collections/reports/collection.js'
 import { Posts } from '../lib/collections/posts/collection.js'
 import { Comments } from '../lib/collections/comments/collection.js'
-import { newMutation, getSetting, addCallback } from 'meteor/vulcan:core';
+import { newMutation, editMutation, getSetting, addCallback, runCallbacksAsync } from 'meteor/vulcan:core';
+import Users from 'meteor/vulcan:users';
 import akismet from 'akismet-api'
 
-async function checkForAkismetSpam({content, author, link}) {
+const SPAM_KARMA_THRESHOLD = 10 //Threshold after which you are no longer affected by spam detection
+
+async function constructAkismetReport({document, type = "post"}) {
+    const author = await Users.findOne(document.userId)
+
+    const link = (type === "post") ? 
+      Posts.getPageUrl(document, true) : 
+      (document.postId && Comments.getPageUrl(document, true)) // Don't get link if we create a comment without a post
     const events = await LWEvents.find({userId: author._id, name: 'login'}, {sort: {createdAt: -1}, limit: 1}).fetch()
     const ip = events && events[0] && events[0].properties && events[0].properties.ip
     const userAgent = events && events[0] && events[0].properties && events[0].properties.userAgent
     const referrer = events && events[0] && events[0].properties && events[0].properties.referrer
-    const spam = await client.checkSpam({
+    return {
       user_ip : ip,              // Required!
       user_agent : userAgent,    // Required!
       referrer : referrer || "",          // Required!
       permalink : link,
-      comment_type : 'post',
+      comment_type : (type === "post") ? 'blog-post' : 'comment',
       comment_author : author.displayName,
       comment_author_email : author.email,
-      comment_content : content,
-      is_test : true // Default value is false
-    })
+      comment_content : document.htmlBody, 
+      is_test: Meteor.isDevelopment
+    }
+}
+
+async function checkForAkismetSpam({document, type = "post"}) {
+    const akismetReport = constructAkismetReport({document, type})
+    const spam = await client.checkSpam(akismetReport)
+    // eslint-disable-next-line no-console
+    console.log("Checked document for spam: ", akismetReport, "result: ", spam)
     return spam
 }
 
@@ -46,20 +61,31 @@ client.verifyKey()
 
 async function checkPostForSpamWithAkismet(post, currentUser) {
   if (akismetKey) {
-    const spam = await checkForAkismetSpam({content: post.body, author: currentUser, link: Posts.getPageUrl(post, true)})
+    const spam = await checkForAkismetSpam({document: post,type: "post"})
     if (spam) {
       //eslint-disable-next-line no-console
-      console.log("Spam post detected, creating new Report")
+      console.log("Spam post detected, creating new Report", currentUser)
       newMutation({
         collection: Reports,
         document: {
           userId: currentUser._id,
           postId: post._id,
           link: Posts.getPageUrl(post),
-          description: "Akismet reported this as spam"
+          description: "Akismet reported this as spam",
+          reportedAsSpam: true,
         },
         validate: false,
       })
+      if ((currentUser.karma || 0) < SPAM_KARMA_THRESHOLD) {
+        // eslint-disable-next-line no-console
+        console.log("Deleting post from user below spam threshold", post)
+        editMutation({
+          collection: Posts,
+          documentId: post._id,
+          set: {status: 4}, // Sets status to spam
+          unset: {}
+        })
+      }
     } else {
       //eslint-disable-next-line no-console
       console.log('Post marked as not spam', post._id);
@@ -72,21 +98,35 @@ addCallback('posts.new.after', checkPostForSpamWithAkismet);
 
 async function checkCommentForSpamWithAkismet(comment, currentUser) {
     if (akismetKey) {
-      const spam = await checkForAkismetSpam({content: comment.body, author: currentUser, link: Comments.getPageUrl(comment, true)})
+      const spam = await checkForAkismetSpam({document: comment, type: "comment"})
       if (spam) {
         //eslint-disable-next-line no-console
-        console.log("Spam comment detected, creating new Report")
+        console.log("Spam comment detected, creating new Report", currentUser)
         newMutation({
           collection: Reports,
           document: {
             userId: currentUser._id,
             postId: comment.postId,
             commentId: comment._id,
-            link: Comments.getPageUrl(comment),
-            description: "Akismet reported this as spam"
+            link: comment.postId && Comments.getPageUrl(comment),
+            description: "Akismet reported this as spam",
+            reportedAsSpam: true
           },
           validate: false,
         })
+        if ((currentUser.karma || 0) < SPAM_KARMA_THRESHOLD) {
+          // eslint-disable-next-line no-console
+          console.log("Deleting comment from user below spam threshold", comment)
+          editMutation({
+            collection: Comments, 
+            documentId: comment._id,
+            set: {
+              deleted: true,
+              deletedDate: new Date(),
+              deletedReason: "Akismet spam detection"
+            }
+          })
+        }
       } else {
         //eslint-disable-next-line no-console
         console.log('Comment marked as not spam', comment._id);
@@ -96,4 +136,36 @@ async function checkCommentForSpamWithAkismet(comment, currentUser) {
   }
   
 addCallback('comments.new.after', checkCommentForSpamWithAkismet);
-  
+
+function runReportCloseCallbacks(newReport, oldReport) {
+  if (newReport.closedAt && !oldReport.closedAt) {
+    runCallbacksAsync('reports.close.async', newReport);
+  }
+}
+
+addCallback('reports.edit.async', runReportCloseCallbacks)
+
+async function akismetReportSpamHam(report) {
+  if (report.reportedAsSpam) {
+    let comment = {}
+    const post = await Posts.findOne(report.postId)
+    if (report.commentId) {
+      comment = Comments.findOne(report.commentId)
+    }
+    const akismetReportArguments = report.commentId ? {document: comment, type: "comment"} : {document: post, type: "post"}
+    const akismetReport = constructAkismetReport(akismetReportArguments)
+    if (report.markedAsSpam) {
+      client.submitSpam(akismetReport, (err) => {
+        // eslint-disable-next-line no-console
+        if (!err) { console.log("Reported Akismet correct positive", akismetReport)}
+      });
+    } else {
+      client.submitHam(akismetReport, (err) => {
+        // eslint-disable-next-line no-console
+        if (!err) { console.log("Reported Akismet false positive", akismetReport)}
+      })
+    }
+  }
+}
+
+addCallback('reports.close.async', akismetReportSpamHam)
