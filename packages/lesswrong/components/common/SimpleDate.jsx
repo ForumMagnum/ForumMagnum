9c7a789ba758201d26ae0333e1ad7a6f93cdc919
James Babcock <jimrandomh@gmail.com>
2018-12-07 16:46:36 -0800

    Push tooltip withTimezone into a place that doesn't have to execute
    The `withTimezone` HoC triggers some rerendering on page load (because
    of switching from the server time zone to the client time zone), and
    also date formatting with momentjs is a bit slow. Take the
    date-formatting that went into `FormatDate`'s tooltip, and refactor it
    so that it only has to happen (and only has to have a `withTimezone`)
    if the tooltip is actually visible.

--- packages/lesswrong/components/common/SimpleDate.jsx
+++ packages/lesswrong/components/common/SimpleDate.jsx
@@ -1,16 +1,11 @@
-import { registerComponent } from 'meteor/vulcan:core';
+import { Components, registerComponent } from 'meteor/vulcan:core';
 import React from 'react';
-import moment from 'moment-timezone';
-import Tooltip from '@material-ui/core/Tooltip';
-import withTimezone from '../common/withTimezone';
 
 /// A simple date, with no special cases, like "Jan 1, 2020". Hover over to
 /// also see the time.
-const SimpleDate = ({date, timezone}) => {
-  return <Tooltip title={moment(new Date(date)).tz(timezone).format('LLL z')}>
-      <span>{moment(new Date(date)).format("MMM DD, YYYY")}</span>
-  </Tooltip>
+const SimpleDate = ({date}) => {
+  return <Components.FormatDate date={date} format="MMM DD, YYYY"/>
 };
 
-registerComponent('SimpleDate', SimpleDate, withTimezone);
+registerComponent('SimpleDate', SimpleDate);
 
