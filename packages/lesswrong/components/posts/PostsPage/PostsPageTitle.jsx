b5ab5be658898ebc43ebbb18f685c74923b7dc3a
Raymond Arnold <rarnold@spotify.com>
2018-12-08 11:20:23 -0800

    Questions Page Tag

--- packages/lesswrong/components/posts/PostsPage/PostsPageTitle.jsx
+++ packages/lesswrong/components/posts/PostsPage/PostsPageTitle.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { withStyles } from '@material-ui/core/styles';
 import { registerComponent } from 'meteor/vulcan:core';
 import Typography from '@material-ui/core/Typography';
+import { Link } from 'react-router'
 
 const styles = theme => ({
   root: {
@@ -16,15 +17,23 @@ const styles = theme => ({
   },
   draft: {
     color: theme.palette.grey[500]
+  },
+  question: {
+    color: theme.palette.grey[600]
   }
 })
 
-const PostsPageTitle = ({classes, post}) =>
+const PostsPageTitle = ({classes, post}) => <div>
+  {post.question && <Typography variant="title">
+    <Link to="/questions" className={classes.question}>
+      [ Question ]
+    </Link>
+  </Typography>}
   <Typography variant="display3" className={classes.root}>
     {post.draft && <span className={classes.draft}>[Draft] </span>}
-    {post.question && <span>Question: </span>}
     {post.title}
   </Typography>
+</div>
 
 
 registerComponent('PostsPageTitle', PostsPageTitle, withStyles(styles, {name: "PostsPageTitle"}))
