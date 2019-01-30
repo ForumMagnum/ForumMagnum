4006213d8a4ee06c9937550d7a1495847c78cf53
Raymond Arnold <rarnold@spotify.com>
2019-01-29 19:32:34 -0800

    defensive programmer

--- packages/lesswrong/components/sequences/SequencesPostsList.jsx
+++ packages/lesswrong/components/sequences/SequencesPostsList.jsx
@@ -6,7 +6,8 @@ import withUser from '../common/withUser';
 const SequencesPostsList = ({posts, chapter, currentUser}) =>
 
   <div className="sequences-posts-list">
-    {posts.map((post) => <Components.PostsItem key={post._id} post={post} chapter={chapter} currentUser={currentUser} />)}
+    {console.log(posts)}
+    {posts.map((post) => post && <Components.PostsItem key={post._id} post={post} chapter={chapter} currentUser={currentUser} />)}
   </div>
 
 registerComponent('SequencesPostsList', SequencesPostsList, withUser)
