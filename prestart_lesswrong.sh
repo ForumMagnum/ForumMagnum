af73def35804899c2b35cff68e2dc0b4c459e49d
Discordius <panisnecis@gmail.com>
2019-02-11 15:40:26 -0800

    Add mongodb-org to apt stuff, hopefully

--- prestart_lesswrong.sh
+++ prestart_lesswrong.sh
@@ -26,6 +26,8 @@ elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
 
     if ! which mongo >/dev/null; then
       if which apt-get >/dev/null; then
+        apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
+        echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/4.0 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.0.list
         apt-get install mongodb-org
       fi
     fi
