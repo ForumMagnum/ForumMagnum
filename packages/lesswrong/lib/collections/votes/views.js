ed764c624c66110cb3a30e9a796fdf09cfad3fb2
James Babcock <jimrandomh@gmail.com>
2019-02-14 17:48:20 -0800

    Karma-change notifier respects AF karma distinction

--- packages/lesswrong/lib/collections/votes/views.js
+++ packages/lesswrong/lib/collections/votes/views.js
@@ -5,4 +5,4 @@ ensureIndex(Votes, {cancelled:1, userId:1, documentId:1});
 ensureIndex(Votes, {cancelled:1, documentId:1});
 
 // Used by getKarmaChanges
-ensureIndex(Votes, {authorId:1, votedAt:1, userId:1});
+ensureIndex(Votes, {authorId:1, votedAt:1, userId:1, afPower:1});
