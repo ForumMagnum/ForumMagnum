9df8d7ac1d342dbbc8a00939ed2f61a8b469036a
Discordius <panisnecis@gmail.com>
2019-02-17 20:31:06 -0800

    Oli reads about refactoring, remembers Jim saying "there is no way to make this file much nicer in this case", then spends 1.5 hours rewriting migration tools to prove a meaningless point (and to practice refactorings, I guess)

--- packages/lesswrong/server/migrations/migrationUtils.js
+++ packages/lesswrong/server/migrations/migrationUtils.js
@@ -47,29 +47,49 @@ function sleep(ms)
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-// Run a function, timing how long it took, then sleep for an amount of time
-// such that if you apply this to each of a series of batches, the fraction of
-// time spent not sleeping is equal to `loadFactor`. Used when doing a batch
-// migration or similarly slow operation, which can be broken into smaller
-// steps, to keep the database load low enough for the site to keep running.
-export async function runThenSleep(loadFactor, func)
-{
-  if (loadFactor <=0 || loadFactor > 1)
-    throw new Error(`Invalid loadFactor ${loadFactor}: must be in (0,1].`);
+// Takes in a function and a load-factor, returns a new function that runs the original
+// function, but then sleep afterwards, such that if you apply this
+// to each of a series of batches, the fraction of time spent not sleeping 
+// is equal to `loadFactor`. Used when doing a batch migration or similarly slow operation,
+// which can be broken into smaller steps, to keep the database load low 
+// enough for the site to keep running.
+
+export function throttleFunction(func, loadFactor) {
+  return async (...args) => {
+    if (loadFactor <=0 || loadFactor > 1)
+      throw new Error(`Invalid loadFactor ${loadFactor}: must be in (0,1].`);
   
-  const startTime = new Date();
-  try {
-    await func();
-  } finally {
-    const endTime = new Date();
-    const timeSpentMs = endTime-startTime;
-    
-    // loadFactor = timeSpentMs / (timeSpentMs + sleepTimeMs)
-    //   [Algebra happens]
-    // sleepTimeMs = timeSpentMs * (1/loadFactor - 1)
-    const sleepTimeMs = timeSpentMs * ((1/loadFactor) - 1);
-    await sleep(sleepTimeMs);
+    const startTime = new Date();
+    let returnValue = null
+    try {
+      returnValue = await func(...args);
+    } finally {
+      const endTime = new Date();
+      const timeSpentMs = endTime-startTime;
+      
+      // loadFactor = timeSpentMs / (timeSpentMs + sleepTimeMs)
+      //   [Algebra happens]
+      // sleepTimeMs = timeSpentMs * (1/loadFactor - 1)
+      const sleepTimeMs = timeSpentMs * ((1/loadFactor) - 1);
+      await sleep(sleepTimeMs);
+    }
+    return returnValue
   }
+}
+
+async function migrateBucket(percentile, fieldName, collection, batchOptions, defaultValue) {
+  const query = {
+    [fieldName]: null,
+    [batchOptions.fieldName]: {$lt: percentile.value},
+  }
+  const mutation = { $set: {
+    [fieldName]: defaultValue
+    }
+  }
+  const options = { multi: true }
+  const writeResult = await collection.update(query, mutation, options)
+  // eslint-disable-next-line no-console
+  console.log(`Bucket with max ${JSON.stringify(percentile.value)} done. Writeresult: ${JSON.stringify(writeResult)}`);
 }
 
 // Given a collection which has a field that has a default value (specified
@@ -80,8 +100,8 @@ export async function fillDefaultValues({ collection, fieldName, batchOptions, l
   if (!collection) throw new Error("Missing required argument: collection");
   if (!fieldName) throw new Error("Missing required argument: fieldName");
   const schema = collection.simpleSchema()._schema
-  const defaultValue = schema[fieldName].defaultValue;
   if (!schema) throw new Error(`Collection ${collection.collectionName} does not have a schema`);
+  const defaultValue = schema[fieldName].defaultValue;
   if (defaultValue === undefined) throw new Error(`Field ${fieldName} does not have a default value`);
   if (!schema[fieldName].canAutofillDefault) throw new Error(`Field ${fieldName} is not marked autofillable`);
 
@@ -108,28 +128,13 @@ export async function fillDefaultValues({ collection, fieldName, batchOptions, l
       { $project: {value: '$_id.max', _id: 0}}
     ]).toArray()
 
+    const throttledMigrateBucket = throttleFunction(loadFactor, migrateBucket)
+
     // Starting at the lowest percentile, modify everything
     for (const percentile of percentiles) {
-      await runThenSleep(loadFactor, async () => {
-        const query = {
-          [fieldName]: null,
-          [batchOptions.fieldName]: {$lt: percentile.value},
-        }
-        const mutation = { $set: {
-          [fieldName]: defaultValue
-          }
-        }
-        const options = { multi: true }
-        const writeResult = await collection.update(query, mutation, options)
-        // eslint-disable-next-line no-console
-        console.log(`Bucket with max ${JSON.stringify(percentile.value)} done. Writeresult: ${JSON.stringify(writeResult)}`);
-      });
+      await throttledMigrateBucket(percentile, fieldName, collection, batchOptions, defaultValue)
     }
     // Then clean up the stragglers that weren't in any of the buckets (because of measurement error)
-    const writeResult = await collection.update({[fieldName]: null}, {$set: {[fieldName]: defaultValue}}, { multi: true })
-    // eslint-disable-next-line no-console
-    console.log(`Cleaned up stragglers. Writeresult: ${JSON.stringify(writeResult)}`)
-    return
   }
   
   const writeResult = await collection.update({
@@ -143,7 +148,7 @@ export async function fillDefaultValues({ collection, fieldName, batchOptions, l
   });
 
   // eslint-disable-next-line no-console
-  console.log(`Done. ${writeResult.nModified} rows affected`);
+  console.log(`Done. ${JSON.stringify(writeResult)} rows affected`);
 }
 
 // Given a query which finds documents in need of a migration, and a function
@@ -169,59 +174,72 @@ export async function migrateDocuments({ description, collection, batchSize, unm
   if (!collection) throw new Error("Missing required argument: collection");
   if (!unmigratedDocumentQuery) throw new Error("Missing required argument: unmigratedDocumentQuery");
   if (!migrate) throw new Error("Missing required argument: migrate");
-  if (!batchSize || !(batchSize>0))
-    throw new Error("Invalid batch size");
+  if (!batchSize || !(batchSize>0)) throw new Error("Invalid batch size");
   
-  if (!description)
-    description = "Migration on "+collection.collectionName;
+  // Assign default values
+  description = description || `Migration on ${collection.collectionName}`
   
   // eslint-disable-next-line no-console
   console.log(`Beginning migration step: ${description}`);
-  
-  let previousDocumentIds = {};
-  let documentsAffected = 0;
-  let done = false;
-  
-  // eslint-disable-next-line no-constant-condition
-  while(!done) {
-    await runThenSleep(loadFactor, async () => {
-      let documents = collection.find(unmigratedDocumentQuery, {limit: batchSize}).fetch();
-      
-      if (!documents.length) {
-        done = true;
-        return;
-      }
-      
-      // Check if any of the documents returned were supposed to have been
-      // migrated by the previous batch's update operation.
-      let docsNotMigrated = _.filter(documents, doc => previousDocumentIds[doc._id]);
-      if (docsNotMigrated.length > 0) {
-        let errorMessage = `Documents not updated in migrateDocuments: ${_.map(docsNotMigrated, doc=>doc._id)}`;
-        
-        // eslint-disable-next-line no-console
-        console.error(errorMessage);
-        throw new Error(errorMessage);
-      }
-      
-      previousDocumentIds = {};
-      _.each(documents, doc => previousDocumentIds[doc._id] = true);
-      
-      // Migrate documents in the batch
-      try {
-        await migrate(documents);
-        documentsAffected += documents.length;
-        // eslint-disable-next-line no-console
-        console.log("Documents updated: ", documentsAffected)
-      } catch(e) {
-        // eslint-disable-next-line no-console
-        console.error("Error running migration");
-        // eslint-disable-next-line no-console
-        console.error(JSON.stringify(e));
-        throw(e);
-      }
-    });
+
+  // Set up migration functions
+  async function getDocuments() {
+    return collection.find(unmigratedDocumentQuery, {limit: batchSize}).fetch()
+  }
+  const throttledMigrateDocuments = throttleFunction(migrateWithDocumentIds, loadFactor)
+  const throttledGetDocuments = throttleFunction(getDocuments, loadFactor)
+
+  // Set up loop variables
+  let migratedDocumentIds = new Set()
+  let currentDocuments = await getDocuments() // Get initial batch of documents
+  if (currentDocuments.length < 1) {
+    // eslint-disable-next-line no-console
+    console.log(`No documents to migrate in ${description}`)
+    return
+  }
+
+  while (currentDocuments.length > 0) {
+    // Check whether we failed to migrate any previous documents
+    await checkDocuments(currentDocuments, migratedDocumentIds)
+    // Migrate the current batch, then sleep
+    migratedDocumentIds = await throttledMigrateDocuments(currentDocuments, migrate, migratedDocumentIds)
+    // Query for new documents, then sleep
+    currentDocuments = await throttledGetDocuments()
   }
   
   // eslint-disable-next-line no-console
-  console.log(`Finished migration step: ${description}. ${documentsAffected} documents affected.`);
+  console.log(`Finished migration step: ${description}. ${migratedDocumentIds.size} documents affected.`);
+}
+
+async function migrateWithDocumentIds(documents, migrate, migratedDocumentIds) {
+  // Migrate the current batch
+  try {
+    await migrate(documents)
+    // Add all migrated documents to migratedDocumentIds set
+    const newMigratedDocumentIds = new Set([...(_.pluck(documents, "_id")), ...migratedDocumentIds])
+    // eslint-disable-next-line no-console
+    console.log("Documents updated: ", newMigratedDocumentIds.size)
+    return newMigratedDocumentIds
+  } catch(e) {
+     // eslint-disable-next-line no-console
+     console.error("Error running migration");
+     // eslint-disable-next-line no-console
+     console.error(JSON.stringify(e));
+     throw(e);
+  }
+}
+
+function setIntersection(a, b) {
+  return new Set([...a].filter(x => b.has(x)));
+}
+
+async function checkDocuments(documents, migratedDocumentIds) {
+  // Check whether we failed to migrate any previous documents
+  const unmigratedDocuments = setIntersection(new Set(_.pluck(documents, '_id')), migratedDocumentIds)
+  if (unmigratedDocuments.size > 0) {
+    let errorMessage = `Documents not updated in migrateDocuments: ${[...unmigratedDocuments.values()]}`;
+    // eslint-disable-next-line no-console
+    console.error(errorMessage);
+    throw new Error(errorMessage);
+  }
 }
