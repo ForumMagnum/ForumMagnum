d205a589983a507ea4c90e7a349af6b4c76fb3d9
James Babcock <jimrandomh@gmail.com>
2019-02-22 14:01:59 -0500

    More flexible format for user page URLs
    You can now reach a user's user page via /u/<slug> or /user/<slug>
    (rather than just /users/<slug>). If you type something which isn't a
    slug (in the sense of Utils.slugify), it will be slugified. All
    non-canonical URLs are Javascript redirects to the canonical URL.

--- packages/lesswrong/components/users/UsersSingle.jsx
+++ packages/lesswrong/components/users/UsersSingle.jsx
@@ -1,10 +1,18 @@
-import { Components, registerComponent } from 'meteor/vulcan:core';
+import { Components, registerComponent, Utils } from 'meteor/vulcan:core';
 import React from 'react';
+import { withRouter } from 'react-router';
 
-const UsersSingle = (props, context) => {
-  return <Components.UsersProfile userId={props.params._id} slug={props.params.slug} />
+const UsersSingle = ({params, router}) => {
+  const slug = Utils.slugify(params.slug);
+  const canonicalUrl = `/users/${slug}`;
+  if (router.location.pathname !== canonicalUrl) {
+    router.replace(canonicalUrl);
+    return null;
+  } else {
+    return <Components.UsersProfile userId={params._id} slug={slug} />
+  }
 };
 
 UsersSingle.displayName = "UsersSingle";
 
-registerComponent('UsersSingle', UsersSingle);
+registerComponent('UsersSingle', UsersSingle, withRouter);
