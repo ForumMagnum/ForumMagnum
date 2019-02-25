b86bfcfeeeb61b2f4f60164e18a974129c899bec
James Babcock <jimrandomh@gmail.com>
2019-02-25 15:15:59 -0800

    Comment about what router.replace does

--- packages/lesswrong/components/users/UsersSingle.jsx
+++ packages/lesswrong/components/users/UsersSingle.jsx
@@ -7,6 +7,9 @@ const UsersSingle = ({params, router}) => {
   const slug = Utils.slugify(params.slug);
   const canonicalUrl = Users.getProfileUrlFromSlug(slug);
   if (router.location.pathname !== canonicalUrl) {
+    // A Javascript redirect, which replaces the history entry (so you don't
+    // have a redirector interfering with the back button). Does not cause a
+    // pageload.
     router.replace(canonicalUrl);
     return null;
   } else {
