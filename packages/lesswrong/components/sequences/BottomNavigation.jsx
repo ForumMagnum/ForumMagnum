235134f84f03a05d0cf663e48fb4f7e3ae8f8675
James Babcock <jimrandomh@gmail.com>
2019-02-25 17:49:18 -0800

    Remove an unused import

--- packages/lesswrong/components/sequences/BottomNavigation.jsx
+++ packages/lesswrong/components/sequences/BottomNavigation.jsx
@@ -1,5 +1,5 @@
 import { Components, registerComponent } from 'meteor/vulcan:core';
-import { withRouter, Link } from 'react-router';
+import { withRouter } from 'react-router';
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import { legacyBreakpoints } from '../../lib/modules/utils/theme';
