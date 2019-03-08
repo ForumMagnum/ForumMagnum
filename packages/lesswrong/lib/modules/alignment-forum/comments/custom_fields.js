f254dce2fed7e148b8db9fe5b7c0186d321ed65d
James Babcock <jimrandomh@gmail.com>
2019-03-07 16:39:44 -0800

    alignment-forum schemas: Array to dict style

--- packages/lesswrong/lib/modules/alignment-forum/comments/custom_fields.js
+++ packages/lesswrong/lib/modules/alignment-forum/comments/custom_fields.js
@@ -1,5 +1,5 @@
 import { Comments } from "../../../collections/comments";
-import { generateIdResolverSingle, generateIdResolverMulti } from '../../../modules/utils/schemaUtils'
+import { generateIdResolverSingle, generateIdResolverMulti, addFieldsDict } from '../../../modules/utils/schemaUtils'
 import { getSetting } from 'meteor/vulcan:core'
 import { schemaDefaultValue } from '../../../collectionUtils';
 
@@ -12,120 +12,94 @@ export const alignmentOptionsGroup = {
 
 const alignmentForum = getSetting('AlignmentForum', false)
 
-Comments.addField([
-  {
-    fieldName: 'af',
-    fieldSchema: {
-      type: Boolean,
-      optional: true,
-      label: "AI Alignment Forum",
-      ...schemaDefaultValue(false),
-      viewableBy: ['guests'],
-      editableBy: ['alignmentForum', 'admins'],
-      insertableBy: ['alignmentForum', 'admins'],
-      hidden: (props) => alignmentForum || !props.alignmentForumPost
-    }
+addFieldsDict(Comments, {
+  af: {
+    type: Boolean,
+    optional: true,
+    label: "AI Alignment Forum",
+    ...schemaDefaultValue(false),
+    viewableBy: ['guests'],
+    editableBy: ['alignmentForum', 'admins'],
+    insertableBy: ['alignmentForum', 'admins'],
+    hidden: (props) => alignmentForum || !props.alignmentForumPost
   },
 
-  {
-    fieldName: 'afDate',
-    fieldSchema: {
-      type: Date,
-      optional: true,
-      hidden: true,
-      defaultValue: false,
-      viewableBy: ['guests'],
-      insertableBy: ['alignmentForum'],
-      editableBy: ['alignmentForum'],
-    }
+  afDate: {
+    type: Date,
+    optional: true,
+    hidden: true,
+    defaultValue: false,
+    viewableBy: ['guests'],
+    insertableBy: ['alignmentForum'],
+    editableBy: ['alignmentForum'],
   },
 
-  {
-    fieldName: 'afBaseScore',
-    fieldSchema: {
-      type: Number,
-      optional: true,
-      label: "Alignment Base Score",
-      viewableBy: ['guests'],
-    }
+  afBaseScore: {
+    type: Number,
+    optional: true,
+    label: "Alignment Base Score",
+    viewableBy: ['guests'],
   },
 
-
-    {
-      fieldName: 'suggestForAlignmentUserIds',
-      fieldSchema: {
-        type: Array,
-        viewableBy: ['members'],
-        editableBy: ['alignmentForum', 'alignmentForumAdmins'],
-        optional: true,
-        label: "Suggested for Alignment by",
-        control: "UsersListEditor",
-        group: alignmentOptionsGroup,
-        resolveAs: {
-          fieldName: 'suggestForAlignmentUsers',
-          type: '[User]',
-          resolver: generateIdResolverMulti(
-            {collectionName: 'Users', fieldName: 'suggestForAlignmentUserIds'}
-          ),
-          addOriginalField: true
-        },
-      }
+  suggestForAlignmentUserIds: {
+    type: Array,
+    viewableBy: ['members'],
+    editableBy: ['alignmentForum', 'alignmentForumAdmins'],
+    optional: true,
+    label: "Suggested for Alignment by",
+    control: "UsersListEditor",
+    group: alignmentOptionsGroup,
+    resolveAs: {
+      fieldName: 'suggestForAlignmentUsers',
+      type: '[User]',
+      resolver: generateIdResolverMulti(
+        {collectionName: 'Users', fieldName: 'suggestForAlignmentUserIds'}
+      ),
+      addOriginalField: true
     },
+  },
+  'suggestForAlignmentUserIds.$': {
+    type: String,
+    optional: true
+  },
 
-    {
-      fieldName: 'suggestForAlignmentUserIds.$',
-      fieldSchema: {
-        type: String,
-        optional: true
-      }
+  reviewForAlignmentUserId: {
+    type: String,
+    optional: true,
+    group: alignmentOptionsGroup,
+    viewableBy: ['guests'],
+    editableBy: ['alignmentForumAdmins', 'admins'],
+    label: "AF Review UserId"
+  },
+
+  afDate: {
+    order:10,
+    type: Date,
+    optional: true,
+    label: "Alignment Forum",
+    defaultValue: false,
+    hidden: true,
+    viewableBy: ['guests'],
+    editableBy: ['alignmentForum', 'alignmentForumAdmins', 'admins'],
+    insertableBy: ['alignmentForum', 'alignmentForumAdmins', 'admins'],
+    group: alignmentOptionsGroup,
+  },
+
+  moveToAlignmentUserId: {
+    type: String,
+    optional: true,
+    hidden: true,
+    viewableBy: ['guests'],
+    editableBy: ['alignmentForum', 'alignmentForumAdmins', 'admins'],
+    group: alignmentOptionsGroup,
+    label: "Move to Alignment UserId",
+    resolveAs: {
+      fieldName: 'moveToAlignmentUser',
+      type: 'User',
+      resolver: generateIdResolverSingle(
+        {collectionName: 'Users', fieldName: 'moveToAlignmentUserId'}
+      ),
+      addOriginalField: true
     },
-
-    {
-      fieldName: 'reviewForAlignmentUserId',
-      fieldSchema: {
-        type: String,
-        optional: true,
-        group: alignmentOptionsGroup,
-        viewableBy: ['guests'],
-        editableBy: ['alignmentForumAdmins', 'admins'],
-        label: "AF Review UserId"
-      }
-    },
-
-      {
-        fieldName: 'afDate',
-        fieldSchema: {
-          order:10,
-          type: Date,
-          optional: true,
-          label: "Alignment Forum",
-          defaultValue: false,
-          hidden: true,
-          viewableBy: ['guests'],
-          editableBy: ['alignmentForum', 'alignmentForumAdmins', 'admins'],
-          insertableBy: ['alignmentForum', 'alignmentForumAdmins', 'admins'],
-          group: alignmentOptionsGroup,
-        }
-      },
-
-      {
-        fieldName: 'moveToAlignmentUserId',
-        fieldSchema: {
-          type: String,
-          optional: true,
-          hidden: true,
-          viewableBy: ['guests'],
-          editableBy: ['alignmentForum', 'alignmentForumAdmins', 'admins'],
-          group: alignmentOptionsGroup,
-          label: "Move to Alignment UserId",
-          resolveAs: {
-            fieldName: 'moveToAlignmentUser',
-            type: 'User',
-            resolver: generateIdResolverSingle(
-              {collectionName: 'Users', fieldName: 'moveToAlignmentUserId'}
-            ),
-            addOriginalField: true
-          },
-        }
-      },
-])
+  },
+})
--- packages/lesswrong/lib/modules/alignment-forum/posts/custom_fields.js
+++ packages/lesswrong/lib/modules/alignment-forum/posts/custom_fields.js
@@ -1,142 +1,113 @@
 import { Posts } from "../../../collections/posts";
 import { formGroups } from "../../../collections/posts/custom_fields.js"
-import { generateIdResolverMulti } from '../../../modules/utils/schemaUtils'
+import { generateIdResolverMulti, addFieldsDict } from '../../../modules/utils/schemaUtils'
 import { schemaDefaultValue } from '../../../collectionUtils';
 
-Posts.addField([
-  {
-    fieldName: 'af',
-    fieldSchema: {
-      order:10,
-      type: Boolean,
-      optional: true,
-      label: "Alignment Forum",
-      ...schemaDefaultValue(false),
-      viewableBy: ['guests'],
-      editableBy: ['alignmentForum'],
-      insertableBy: ['alignmentForum'],
-      control: 'checkbox',
-      group: formGroups.options,
+addFieldsDict(Posts, {
+  af: {
+    order:10,
+    type: Boolean,
+    optional: true,
+    label: "Alignment Forum",
+    ...schemaDefaultValue(false),
+    viewableBy: ['guests'],
+    editableBy: ['alignmentForum'],
+    insertableBy: ['alignmentForum'],
+    control: 'checkbox',
+    group: formGroups.options,
+  },
+
+  afDate: {
+    order:10,
+    type: Date,
+    optional: true,
+    label: "Alignment Forum",
+    hidden: true,
+    viewableBy: ['guests'],
+    editableBy: ['alignmentForum'],
+    insertableBy: ['alignmentForum'],
+    group: formGroups.options,
+  },
+
+  afBaseScore: {
+    type: Number,
+    optional: true,
+    label: "Alignment Base Score",
+    viewableBy: ['guests'],
+  },
+
+  afCommentCount: {
+    type: Number,
+    optional: true,
+    defaultValue: 0,
+    hidden:true,
+    label: "Alignment Comment Count",
+    viewableBy: ['guests'],
+  },
+
+  afLastCommentedAt: {
+    type: Date,
+    optional: true,
+    hidden: true,
+    viewableBy: ['guests'],
+    onInsert: () => {
+      return new Date();
     }
   },
 
-  {
-    fieldName: 'afDate',
-    fieldSchema: {
-      order:10,
-      type: Date,
-      optional: true,
-      label: "Alignment Forum",
-      hidden: true,
-      viewableBy: ['guests'],
-      editableBy: ['alignmentForum'],
-      insertableBy: ['alignmentForum'],
-      group: formGroups.options,
-    }
-  },
-
-  {
-    fieldName: 'afBaseScore',
-    fieldSchema: {
-      type: Number,
-      optional: true,
-      label: "Alignment Base Score",
-      viewableBy: ['guests'],
-    }
-  },
-
-  {
-    fieldName: 'afCommentCount',
-    fieldSchema: {
-      type: Number,
-      optional: true,
-      defaultValue: 0,
-      hidden:true,
-      label: "Alignment Comment Count",
-      viewableBy: ['guests'],
-    }
-  },
-
-  {
-    fieldName: 'afLastCommentedAt',
-    fieldSchema: {
-      type: Date,
-      optional: true,
-      hidden: true,
-      viewableBy: ['guests'],
-      onInsert: () => {
-        return new Date();
+  afSticky: {
+    order: 10,
+    type: Boolean,
+    optional: true,
+    label: "Sticky (Alignment)",
+    ...schemaDefaultValue(false),
+    group: formGroups.adminOptions,
+    viewableBy: ['guests'],
+    editableBy: ['alignmentForumAdmins', 'admins'],
+    insertableBy: ['alignmentForumAdmins', 'admins'],
+    control: 'checkbox',
+    onInsert: (post) => {
+      if(!post.afSticky) {
+        return false;
+      }
+    },
+    onEdit: (modifier, post) => {
+      if (!(modifier.$set && modifier.$set.afSticky)) {
+        return false;
       }
     }
   },
 
-  {
-    fieldName: 'afSticky',
-    fieldSchema: {
-      order: 10,
-      type: Boolean,
-      optional: true,
-      label: "Sticky (Alignment)",
-      ...schemaDefaultValue(false),
-      group: formGroups.adminOptions,
-      viewableBy: ['guests'],
-      editableBy: ['alignmentForumAdmins', 'admins'],
-      insertableBy: ['alignmentForumAdmins', 'admins'],
-      control: 'checkbox',
-      onInsert: (post) => {
-        if(!post.afSticky) {
-          return false;
-        }
-      },
-      onEdit: (modifier, post) => {
-        if (!(modifier.$set && modifier.$set.afSticky)) {
-          return false;
-        }
-      }
-    }
+  suggestForAlignmentUserIds: {
+    type: Array,
+    viewableBy: ['members'],
+    insertableBy: ['sunshineRegiment', 'admins'],
+    editableBy: ['alignmentForum', 'alignmentForumAdmins'],
+    optional: true,
+    label: "Suggested for Alignment by",
+    control: "UsersListEditor",
+    group: formGroups.adminOptions,
+    resolveAs: {
+      fieldName: 'suggestForAlignmentUsers',
+      type: '[User]',
+      resolver: generateIdResolverMulti(
+        {collectionName: 'Users', fieldName: 'suggestForAlignmentUserIds'}
+      ),
+      addOriginalField: true
+    },
+  },
+  'suggestForAlignmentUserIds.$': {
+    type: String,
+    optional: true
   },
 
-  {
-    fieldName: 'suggestForAlignmentUserIds',
-    fieldSchema: {
-      type: Array,
-      viewableBy: ['members'],
-      insertableBy: ['sunshineRegiment', 'admins'],
-      editableBy: ['alignmentForum', 'alignmentForumAdmins'],
-      optional: true,
-      label: "Suggested for Alignment by",
-      control: "UsersListEditor",
-      group: formGroups.adminOptions,
-      resolveAs: {
-        fieldName: 'suggestForAlignmentUsers',
-        type: '[User]',
-        resolver: generateIdResolverMulti(
-          {collectionName: 'Users', fieldName: 'suggestForAlignmentUserIds'}
-        ),
-        addOriginalField: true
-      },
-    }
+  reviewForAlignmentUserId: {
+    type: String,
+    optional: true,
+    viewableBy: ['guests'],
+    editableBy: ['alignmentForumAdmins', 'admins'],
+    insertableBy: ['alignmentForumAdmins', 'admins'],
+    group: formGroups.adminOptions,
+    label: "AF Review UserId"
   },
-
-  {
-    fieldName: 'suggestForAlignmentUserIds.$',
-    fieldSchema: {
-      type: String,
-      optional: true
-    }
-  },
-
-  {
-    fieldName: 'reviewForAlignmentUserId',
-    fieldSchema: {
-      type: String,
-      optional: true,
-      viewableBy: ['guests'],
-      editableBy: ['alignmentForumAdmins', 'admins'],
-      insertableBy: ['alignmentForumAdmins', 'admins'],
-      group: formGroups.adminOptions,
-      label: "AF Review UserId"
-    }
-  },
-
-]);
+});
