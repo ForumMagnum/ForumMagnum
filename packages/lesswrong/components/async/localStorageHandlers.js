015ee59118225c960bbf269a4303da320c1ad15e
James Babcock <jimrandomh@gmail.com>
2019-03-12 19:50:35 -0700

    Local-storage saving works with Markdown, HTML editors

--- packages/lesswrong/components/async/localStorageHandlers.js
+++ packages/lesswrong/components/async/localStorageHandlers.js
@@ -1,5 +1,12 @@
 import ls from 'local-storage';
 
+// Given a document and a field name, return:
+// {
+//   id: The name to use for storing drafts related to this document in
+//     localStorage. This may be combined with an editor-type prefix.
+//   verify: Whether to prompt before restoring a draft (as opposed to just
+//     always restoring it).
+// }
 const defaulGetDocumentStorageId = (doc, name) => {
   const { _id, conversationId } = doc
   if (_id && name) { return {id: `${_id}${name}`, verify: true}}
@@ -11,12 +18,14 @@ const defaulGetDocumentStorageId = (doc, name) => {
   }
 }
 
+// Return a wrapper around localStorage, with get, set, and reset functions
+// which handle the (document, field-name, prefix) => key mapping.
 export const getLSHandlers = (getLocalStorageId = null) => {
   const idGenerator = getLocalStorageId || defaulGetDocumentStorageId
   return {
-    get: ({doc, name}) => {
+    get: ({doc, name, prefix}) => {
       const { id, verify } = idGenerator(doc, name)
-      const savedState = ls.get(id)
+      const savedState = ls.get(prefix+id)
       if (verify && savedState && Meteor.isClient && window) {
         const result = window.confirm("We've found a previously saved state for this document, would you like to restore it?")
         return result ? savedState : null
@@ -24,7 +33,13 @@ export const getLSHandlers = (getLocalStorageId = null) => {
         return savedState
       }
     },
-    set: ({state, doc, name}) => {ls.set(idGenerator(doc, name).id, state)},
-    reset: ({doc, name}) => {ls.remove(idGenerator(doc, name).id)}
+    set: ({state, doc, name, prefix}) => {
+      const id = prefix+idGenerator(doc, name).id;
+      ls.set(id, state)
+    },
+    reset: ({doc, name, prefix}) => {
+      const id = prefix+idGenerator(doc, name).id;
+      ls.remove(id)
+    }
   }
 }
