e4e9ca4649a97eb45cffd856a93b427dc9b84495
Raymond Arnold <rarnold@ugps-Mac-8.localdomain>
2019-03-18 17:01:47 -0700

    Fancier Icons 'n shit

--- packages/lesswrong/components/common/TabNavigationMenu.jsx
+++ packages/lesswrong/components/common/TabNavigationMenu.jsx
@@ -3,103 +3,118 @@ import { withRouter, Link } from 'react-router';
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import classNames from 'classnames';
-import Home from '@material-ui/icons/Home';
-import LocalLibrary from '@material-ui/icons/LocalLibrary';
-import ListAlt from '@material-ui/icons/ListAlt';
-import Public from '@material-ui/icons/Public';
-import QuestionAnswer from '@material-ui/icons/QuestionAnswer';
+import Tooltip from '@material-ui/core/Tooltip';
 import Button from '@material-ui/core/Button';
-import Headroom from 'react-headroom'
-import { isMobile } from '../../lib/modules/utils/isMobile.js'
+
+import { compassIcon } from '../icons/compassIcon';
+import { questionsGlobeIcon } from '../icons/questionsGlobeIcon';
+import { communityGlobeIcon } from '../icons/communityGlobeIcon';
 
 
 const styles = (theme) => ({
   root: {
     position: "absolute",
     width:"100%",
-    [theme.breakpoints.up('md')]: {
+    zIndex: theme.zIndexes.tabNavigation,
+    [theme.breakpoints.up('lg')]: {
       top: 110,
-      left:5,
-      width:148,
-      borderRadius: 3,
+      left:0,
+      width:200,
     },
-    [theme.breakpoints.down('sm')]: {
+    [theme.breakpoints.down('md')]: {
       position: "fixed",
       left: 0,
-      bottom: 0,
-      zIndex:1000,
+      bottom: 0
     },
   },
   tabMenu: {
     display: "flex",
     justifyContent: "space-around",
-    zIndex:1000,
+    zIndex: theme.zIndexes.tabNavigation,
     backgroundColor: "white",
-    [theme.breakpoints.up('md')]: {
+    [theme.breakpoints.up('lg')]: {
       flexDirection: "column",
-      paddingTop: theme.spacing.unit,
-      paddingBottom: theme.spacing.unit,
     },
-    [theme.breakpoints.down('sm')]: {
+    [theme.breakpoints.down('md')]: {
       borderTop: "solid 1px rgba(0,0,0,.2)",
       backgroundColor: theme.palette.grey[200],
     },
   },
   navButtonWrapper: {
-    [theme.breakpoints.down('sm')]: {
+    width: 200,
+    [theme.breakpoints.down('md')]: {
       width: "20%",
     },
+    '&:hover': {
+      opacity:1
+    },
+    '&.selected': {
+      [theme.breakpoints.up('lg')]: {
+        backgroundColor: theme.palette.grey[200],
+      },
+      [theme.breakpoints.down('md')]: {
+        backgroundColor: theme.palette.grey[300],
+      },
+    }
   },
   navButtonInnerWrapper: {
-    [theme.breakpoints.down('sm')]: {
+    padding: theme.spacing.unit*2,
+    [theme.breakpoints.down('md')]: {
       padding:0,
-      paddingBottom: theme.spacing.unit/2,
       width: "100%",
     }
   },
   navButton: {
     display: "flex",
     alignItems: "center",
-    [theme.breakpoints.up('md')]: {
-      width: 116,
+    [theme.breakpoints.up('lg')]: {
+      width: 168,
       justifyContent: "flex-start",
       flexDirection: "row",
     },
-    [theme.breakpoints.down('sm')]: {
+    [theme.breakpoints.down('md')]: {
       flexDirection: "column",
       paddingTop: theme.spacing.unit,
     },
-    '&.selected': {
-      backgroundColor: theme.palette.grey[100],
-    }
   },
   icon: {
     display: "block",
-    color: theme.palette.grey[400],
-    marginBottom: theme.spacing.unit/2,
-    [theme.breakpoints.up('md')]: {
-      marginRight: theme.spacing.unit,
+    opacity: .25,
+    width: 30,
+    height: 32,
+    [theme.breakpoints.up('lg')]: {
+      marginRight: theme.spacing.unit*2,
       display: "inline",
     },
     '&.selected': {
-      color: theme.palette.grey[900],
+      opacity:.9,
     }
   },
   navText: {
     ...theme.typography.body2,
     color: theme.palette.grey[600],
-    [theme.breakpoints.down('sm')]: {
-      fontSize: 8,
+    [theme.breakpoints.down('md')]: {
+      fontSize: 10,
+      marginBottom: 3,
+      marginTop: 3,
     },
-    [theme.breakpoints.up('md')]: {
+    [theme.breakpoints.up('lg')]: {
       textTransform: "none !important",
-      paddingBottom: 5,
-      paddingLeft: 6,
     },
     '&.selected': {
       color: theme.palette.grey[900],
     }
   },
+  hideOnMobile: {
+    [theme.breakpoints.down('md')]: {
+      display: "none"
+    },
+  },
+  hideOnDesktop: {
+    [theme.breakpoints.up('lg')]: {
+      display: "none"
+    },
+  }
 })
 
 const TabNavigationMenu = ({
@@ -111,60 +126,79 @@ const TabNavigationMenu = ({
   
   return (
     <div className={classes.root}>
-      <Headroom downTolerance={10} upTolerance={12} disable={!isMobile()}>
-        <div className={classes.tabMenu}>
-          <Link to="/" className={classes.navButtonWrapper}>
+      <div className={classes.tabMenu}>
+        <Tooltip placement="right-start" title="Curated content, latest posts and comments.">
+          <Link to="/" className={classNames(classes.navButtonWrapper, {selected: pathname === "/"})}>
             <Button className={classes.navButtonInnerWrapper}>
               <div className={classes.navButton}>
-                <Home className={classNames(classes.icon, {selected: pathname === "/"})}/> 
+                <span className={classNames(classes.icon, {selected: pathname === "/"})}>
+                  { compassIcon }
+                </span>
+                {/* <Home className={classNames(classes.icon, {selected: pathname === "/"})}/>  */}
                 <span className={classNames(classes.navText, {selected: pathname === "/"})}>
                   Home
                 </span>
               </div>
             </Button>
           </Link>
-          <Link to="/questions" className={classes.navButtonWrapper}>
+        </Tooltip>
+        <Tooltip placement="right-start" title="Sequences of posts. Recommended reading for new users.">
+          <Link to="/library" className={classNames(classes.navButtonWrapper, {selected: pathname === "/library"})}>
             <Button className={classes.navButtonInnerWrapper}>
               <div className={classes.navButton}>
-                <QuestionAnswer className={classNames(classes.icon, {selected: pathname === "/questions"})}/> 
-                <span className={classNames(classes.navText, {selected: pathname === "/questions"})}>
-                  Questions
+                <span className={classNames(classes.icon, {selected: pathname === "/library"})}>
+                  <img src="/bookIcon.svg" />
                 </span>
-              </div>
-            </Button>
-          </Link>
-          <Link to="/library" className={classes.navButtonWrapper}>
-            <Button className={classes.navButtonInnerWrapper}>
-              <div className={classes.navButton}>
-                <LocalLibrary className={classNames(classes.icon, {selected: pathname === "/library"})}/> 
                 <span className={classNames(classes.navText, {selected: pathname === "/library"})}>
                   Library
                 </span>
               </div>
             </Button>
           </Link>
-          <Link to="/allPosts" className={classes.navButtonWrapper}>
+        </Tooltip>
+        <Tooltip placement="right-start" title="Ask and answer questions, ranging from simple newbie questions, to extensive outsourced research.">
+          <Link to="/questions" className={classNames(classes.navButtonWrapper, {selected: pathname === "/questions"})}>
             <Button className={classes.navButtonInnerWrapper}>
               <div className={classes.navButton}>
-                <ListAlt className={classNames(classes.icon, {selected: pathname === "/allPosts"})}/> 
+                <span className={classNames(classes.icon, {selected: pathname === "/questions"})}>
+                  { questionsGlobeIcon }
+                </span>
+                <span className={classNames(classes.navText, {selected: pathname === "/questions"})}>
+                  <span className={classes.hideOnMobile}>Open </span>Questions
+                </span>
+              </div>
+            </Button>
+          </Link>
+        </Tooltip>
+        <Tooltip placement="right-start" title="See all posts, filtered and sorted however you like.">
+          <Link to="/allPosts" className={classNames(classes.navButtonWrapper, classes.hideOnDesktop, {selected: pathname === "/allPosts"})}>
+            <Button className={classes.navButtonInnerWrapper}>
+              <div className={classes.navButton}>
+                <span className={classNames(classes.icon, {selected: pathname === "/allPosts"})}>
+                  <img src="/scrollIcon.svg" />
+                </span>
                 <span className={classNames(classes.navText, {selected: pathname === "/allPosts"})}>
                   All Posts
                 </span>
               </div>
             </Button>
           </Link>
-          <Link to="/community" className={classes.navButtonWrapper}>
+        </Tooltip>
+        <Tooltip placement="right-start" title="Events from around the world. Find a meetup near you.">
+          <Link to="/community" className={classNames(classes.navButtonWrapper, {selected: pathname === "/community"})}>
             <Button className={classes.navButtonInnerWrapper}>
               <div className={classes.navButton}>
-                <Public className={classNames(classes.icon, {selected: pathname === "/community"})}/> 
+                <span className={classNames(classes.icon, {selected: pathname === "/community"})}>
+                  { communityGlobeIcon }
+                </span>
                 <span className={classNames(classes.navText, {selected: pathname === "/community"})}>
-                  Community
+                  Community<span className={classes.hideOnMobile}> Events</span>
                 </span>
               </div>
             </Button>
           </Link>
-        </div>
-      </Headroom>
+        </Tooltip>
+      </div>
     </div>
   )
 };
