5c46c6040aa723e802235c634f12121ff0acc06e
James Babcock <jimrandomh@gmail.com>
2019-03-21 17:30:30 -0700

    Add timestamps to PM email

--- packages/lesswrong/server/emailComponents/PrivateMessagesEmail.jsx
+++ packages/lesswrong/server/emailComponents/PrivateMessagesEmail.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { registerComponent, Components } from 'meteor/vulcan:core';
+import { getSetting, registerComponent, Components } from 'meteor/vulcan:core';
 import { withStyles } from '@material-ui/core/styles';
 import './EmailUsername.jsx';
+import './EmailFormatDate.jsx';
 
 const styles = theme => ({
   message: {
@@ -9,10 +10,13 @@ const styles = theme => ({
 });
 
 const PrivateMessagesEmail = ({conversation, messages, participantsById, classes}) => {
-  const { EmailUsername } = Components;
+  const { EmailUsername, EmailFormatDate } = Components;
   return (<React.Fragment>
+    <p>You received {messages.length>1 ? "private messages" : "a private message"} on {getSetting('title')}.</p>
+    
     {messages.map((message,i) => <div className={classes.message} key={i}>
       <EmailUsername user={participantsById[message.userId]}/>
+      {" "}<EmailFormatDate date={message.createdAt}/>
       <div dangerouslySetInnerHTML={{__html: message.contents.html}}/>
     </div>)}
   </React.Fragment>);
