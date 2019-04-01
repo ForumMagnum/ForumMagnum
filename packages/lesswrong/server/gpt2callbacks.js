246dadc2c789f76725a67223b65f8c5cffe401b5
James Babcock <jimrandomh@gmail.com>
2019-04-01 12:56:16 -0700

    GPT2 can be banned from individual posts

--- packages/lesswrong/server/gpt2callbacks.js
+++ packages/lesswrong/server/gpt2callbacks.js
@@ -1,6 +1,6 @@
 import { addCallback, newMutation, getSetting } from 'meteor/vulcan:core';
 import Users from 'meteor/vulcan:users';
-import { Comments } from '../lib/collections/comments/collection.js'
+import { Comments, commentMutationOptions } from '../lib/collections/comments/collection.js'
 import { dataToMarkdown } from './editor/make_editable_callbacks.js';
 import request from 'request';
 
@@ -52,24 +52,33 @@ const ReplyToCommentWithGpt2 = async (parentComment) => {
     const parentCommentText = getCommentText(parentComment);
     const replyBody = await generateReply(parentCommentText);
     
-    // Create the reply
-    await newMutation({
-      collection: Comments,
-      document: {
-        userId: gpt2user._id,
-        contents: {
-          originalContents: {
-            type: "markdown",
-            data: replyBody,
-          }
-        },
-        parentCommentId: parentComment._id,
-        postId: parentComment.postId,
-        answerId: parentComment.answerId,
+    const document = {
+      userId: gpt2user._id,
+      contents: {
+        originalContents: {
+          type: "markdown",
+          data: replyBody,
+        }
       },
-      currentUser: gpt2user,
-      validate: false,
-    });
+      parentCommentId: parentComment._id,
+      postId: parentComment.postId,
+      parentAnswerId: parentComment.parentAnswerId,
+    }
+    
+    
+    // Create the reply
+    if (commentMutationOptions.newCheck(gpt2user, document)) {
+      await newMutation({
+        collection: Comments,
+        document: document,
+        context: {
+          currentUser: gpt2user,
+          Users: Users,
+        },
+        currentUser: gpt2user,
+        validate: true,
+      });
+    }
   } catch(e) {
     // eslint-disable-next-line no-console
     console.error(e);
