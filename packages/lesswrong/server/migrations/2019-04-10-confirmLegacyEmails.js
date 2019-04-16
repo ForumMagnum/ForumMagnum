563c02d7665ade4f19254778c962e3d4ca5a01b9
James Babcock <jimrandomh@gmail.com>
2019-04-15 17:11:53 -0700

    Use correct legacy schama for email validation import

--- packages/lesswrong/server/migrations/2019-04-10-confirmLegacyEmails.js
+++ packages/lesswrong/server/migrations/2019-04-10-confirmLegacyEmails.js
@@ -12,31 +12,43 @@ registerMigration({
       callback: (users) => {
         let updates = [];
         
-        // For users that were on old LessWrong...
         for(let user of users)
         {
+          // If the user is not a legacy user, no change
+          if (!user || !user.legacy || !user.legacyData)
+            continue;
+          
+          // Because all emails were verified on import, if the email address
+          // is unverified, that means verification was cleared (eg by an email
+          // address change) on import.
+          if (_.some(user.emails, email=>!email.verified))
+            continue;
+          
+          // If user.whenConfirmationEmailSent, either the email address was unnecessarily re-verified, or the email
+          // was un-verified by changing email address and then re-verified. Or a verification email was sent but not
+          // clicked; that case is hard to distinguish. In any case, leave things as-is.
+          if (user.whenConfirmationEmailSent)
+            continue;
+          
+          // If the email address matches legacyData.emailAddress, set its verified flag to legacyData.email_validated.
           const legacyData = user.legacyData;
           for (let i=0; i<user.emails.length; i++) {
-            const email = user.emails[i];
-            if (legacyData && email
-                // If their email address was verified...
-                && legacyData.has_subscribed === "t"
-                // And is still the same email address as before...
-                && legacyData.email === email.address
-                // And they haven't re-verified...
-                && !email.verified)
+            if (legacyData && legacyData.email
+              && user.emails && user.emails[i].address === legacyData.email)
             {
-              // Then mark their email address as verified.
-              updates.push({
-                updateOne: {
-                  filter: {_id: user._id},
-                  update: {
-                    $set: {
-                      [`emails[${i}].verified`]: true
+              const shouldBeVerified = legacyData.email_validated;
+              if(user.emails[i].verified != shouldBeVerified) {
+                updates.push({
+                  updateOne: {
+                    filter: {_id: user._id},
+                    update: {
+                      $set: {
+                        [`emails[${i}].verified`]: shouldBeVerified
+                      }
                     }
                   }
-                }
-              });
+                });
+              }
             }
           }
         }
