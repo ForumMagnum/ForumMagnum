e71df80bdd1a29a978c9de2db62a7cd675db28cb
Discordius <panisnecis@gmail.com>
2019-04-19 18:21:48 -0700

    Identify fullstory users

--- packages/lesswrong/client.js
+++ packages/lesswrong/client.js
@@ -1,10 +1,14 @@
+// Make sure to register settings before everything else
+import './lib/registerSettings.js'
+// Then import the google analytics stuff
+import './client/ga.js';
+
+// Then do the rest
 import './client/disconnect_meteor.js';
 import './client/themeProvider.js';
 import './client/logging.js';
 import './client/votingFragmentMatcher.js';
 export * from './lib/index.js';
 
-// After this line we can guarantee that registerSettings has been called
-import './client/ga.js';
 // Polyfills:
 import 'element-closest'
--- packages/lesswrong/client/ga.js
+++ packages/lesswrong/client/ga.js
@@ -1,5 +1,7 @@
 import { getSetting } from 'meteor/vulcan:core';
-import { addInitFunction } from 'meteor/vulcan:events';
+import { addInitFunction, addIdentifyFunction } from 'meteor/vulcan:events';
+import { setTimeout } from 'timers';
+
 
 function googleTagManagerInit() {
   const googleTagManagerId = getSetting('googleTagManager.apiKey')
@@ -9,10 +11,35 @@ function googleTagManagerInit() {
       'gtm.start':
         new Date().getTime(), event: 'gtm.js'
     }); var f = d.getElementsByTagName(s)[0],
-      j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
-        'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
+      j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; 
+      j.async = true; 
+      j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;         
+      f.parentNode.insertBefore(j, f);
     })(window, document, 'script', 'dataLayer', googleTagManagerId)
   }
 }
 
 addInitFunction(googleTagManagerInit)
+
+const identifyFullStoryUser = (currentUser) => {
+  const { karma = 0, afKarma = 0, frontpagePostCount = 0, voteCount = 0, createdAt } = currentUser
+  const fullStoryData = { karma, afKarma, frontpagePostCount, voteCount, createdAt }
+  global.FS.identify(currentUser._id, {
+    displayName: currentUser.displayName,
+    email: currentUser.email,
+    // Custom LessWrong variables
+    ...fullStoryData
+  });
+}
+
+const identifyFSCallback = (currentUser) => {
+  if (global.FS) {
+    identifyFullStoryUser(currentUser)
+  } else {
+    window.addEventListener('fullstory_loaded', function() {
+      identifyFullStoryUser(currentUser)
+    })
+  }
+}
+
+addIdentifyFunction(identifyFSCallback)
