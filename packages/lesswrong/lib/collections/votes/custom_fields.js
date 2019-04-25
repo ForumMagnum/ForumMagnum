010312eaf8cb27689be8995d06bd56144ac102b0
James Babcock <jimrandomh@gmail.com>
2019-04-24 19:03:10 -0700

    Remove now-empty votes/custom_fields.js

--- packages/lesswrong/lib/collections/votes/custom_fields.js
+++
@@ -1,6 +0,0 @@
-import { Votes } from './collection.js';
-import { VoteableCollections } from '../../modules/make_voteable.js';
-import { getWithLoader } from "../../loaders.js";
-import { addFieldsDict } from '../../modules/utils/schemaUtils'
-import { denormalizedCountOfReferences } from '../../modules/utils/schemaUtils.js';
-
