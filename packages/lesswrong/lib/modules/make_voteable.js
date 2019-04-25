432ef479cb8a3acc3c4aefecf01702dff2f993f9
James Babcock <jimrandomh@gmail.com>
2019-04-24 18:45:54 -0700

    make_voteable: Convert schema to dictionary style

--- packages/lesswrong/lib/modules/make_voteable.js
+++ packages/lesswrong/lib/modules/make_voteable.js
@@ -1,4 +1,5 @@
 import { Connectors } from 'meteor/vulcan:core'; // import from vulcan:lib because vulcan:core isn't loaded yet
+import { addFieldsDict } from './utils/schemaUtils'
 
 export const VoteableCollections = [];
 
@@ -6,50 +7,35 @@ export const makeVoteable = collection => {
 
   VoteableCollections.push(collection);
 
-  collection.addField([
-    /**
-      The document's base score (not factoring in the document's age)
-    */
-    {
-      fieldName: 'baseScore',
-      fieldSchema: {
-        type: Number,
-        optional: true,
-        defaultValue: 0,
-        canRead: ['guests'],
-        onInsert: document => {
-          // default to 0 if empty
-          return document.baseScore || 0;
-        }
+  addFieldsDict(collection, {
+    // The document's base score (not factoring in the document's age)
+    baseScore: {
+      type: Number,
+      optional: true,
+      defaultValue: 0,
+      canRead: ['guests'],
+      onInsert: document => {
+        // default to 0 if empty
+        return document.baseScore || 0;
       }
     },
-    /**
-      The document's current score (factoring in age)
-    */
-    {
-      fieldName: 'score',
-      fieldSchema: {
-        type: Number,
-        optional: true,
-        defaultValue: 0,
-        canRead: ['guests'],
-        onInsert: document => {
-          // default to 0 if empty
-          return document.score || 0;
-        }
+    // The document's current score (factoring in age)
+    score: {
+      type: Number,
+      optional: true,
+      defaultValue: 0,
+      canRead: ['guests'],
+      onInsert: document => {
+        // default to 0 if empty
+        return document.score || 0;
       }
     },
-    /**
-      Whether the document is inactive. Inactive documents see their score recalculated less often
-    */
-    {
-      fieldName: 'inactive',
-      fieldSchema: {
-        type: Boolean,
-        optional: true,
-        onInsert: () => false
-      }
+    // Whether the document is inactive. Inactive documents see their score
+    // recalculated less often
+    inactive: {
+      type: Boolean,
+      optional: true,
+      onInsert: () => false
     },
-
-  ]);
+  });
 }
