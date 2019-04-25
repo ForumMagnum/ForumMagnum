5f019e5de8b2096846a190441fecbe4edc2ec382
James Babcock <jimrandomh@gmail.com>
2019-04-24 18:53:11 -0700

    Move currentUserVotes,allVotes,voteCount fields from votes/custom_fields to make_voteable

--- packages/lesswrong/lib/modules/make_voteable.js
+++ packages/lesswrong/lib/modules/make_voteable.js
@@ -8,6 +8,61 @@ export const makeVoteable = collection => {
   VoteableCollections.push(collection);
 
   addFieldsDict(collection, {
+    currentUserVotes: {
+      type: Array,
+      optional: true,
+      viewableBy: ['guests'],
+      resolveAs: {
+        type: '[Vote]',
+        resolver: async (document, args, { Users, Votes, currentUser }) => {
+          if (!currentUser) return [];
+          const votes = await getWithLoader(Votes,
+            `votesByUser${currentUser._id}`,
+            {
+              userId: currentUser._id,
+              cancelled: false,
+            },
+            "documentId", document._id
+          );
+          
+          if (!votes.length) return [];
+          return Users.restrictViewableFields(currentUser, Votes, votes);
+        },
+      }
+    },
+    'currentUserVotes.$': {
+      type: Object,
+      optional: true
+    },
+    allVotes: {
+      type: Array,
+      optional: true,
+      viewableBy: ['guests'],
+      resolveAs: {
+        type: '[Vote]',
+        resolver: async (document, args, { Users, Votes, currentUser }) => {
+          const votes = await getWithLoader(Votes,
+            "votesByDocument",
+            {
+              cancelled: false,
+            },
+            "documentId", document._id
+          );
+          
+          if (!votes.length) return [];
+          return Users.restrictViewableFields(currentUser, Votes, votes);
+        },
+      }
+    },
+    'allVotes.$': {
+      type: Object,
+      optional: true
+    },
+    voteCount: {
+      type: Number,
+      optional: true,
+      viewableBy: ['guests'],
+    },
     // The document's base score (not factoring in the document's age)
     baseScore: {
       type: Number,
