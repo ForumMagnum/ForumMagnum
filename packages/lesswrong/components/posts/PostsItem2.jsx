a13653d841a48ff73dbefa68f2b47545b30b34f4
James Babcock <jimrandomh@gmail.com>
2019-04-26 12:09:35 -0700

    Fix display of event locations

--- packages/lesswrong/components/posts/PostsItem2.jsx
+++ packages/lesswrong/components/posts/PostsItem2.jsx
@@ -18,7 +18,6 @@ import Hidden from '@material-ui/core/Hidden';
 import { POSTED_AT_WIDTH, START_TIME_WIDTH } from './PostsItemDate.jsx';
 
 export const MENU_WIDTH = 18
-export const EVENT_WIDTH = 110
 export const KARMA_WIDTH = 42
 export const COMMENTS_WIDTH = 48
 
@@ -109,19 +108,14 @@ const styles = (theme) => ({
     }
   },
   event: {
-    width: EVENT_WIDTH,
-    justifyContent: "flex-end",
-    flex: 1,
+    maxWidth: 250,
     overflow: "hidden",
     whiteSpace: "nowrap",
     textOverflow: "ellipsis", // I'm not sure this line worked properly?
     marginRight: theme.spacing.unit*1.5,
     [theme.breakpoints.down('sm')]: {
-      justifyContent: "flex-start",
       width: "unset",
-      maxWidth: EVENT_WIDTH,
       marginLeft: 0,
-      flex: "unset"
     }
   },
   postedAt: {
@@ -132,20 +126,6 @@ const styles = (theme) => ({
       color: "rgba(0,0,0,.9)",
       [theme.breakpoints.down('sm')]: {
         width: "auto",
-      }
-    }
-  },
-  startTime: {
-    '&&': {
-      width: START_TIME_WIDTH,
-      justifyContent: "center",
-      fontWeight: 300,
-      fontSize: "1rem",
-      color: "rgba(0,0,0,.9)",
-      [theme.breakpoints.down('sm')]: {
-        justifyContent: "flex-start",
-        width: "auto",
-        flexGrow: 1,
       }
     }
   },
