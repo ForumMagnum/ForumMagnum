896c8c7ff551fe15e566515bf3028a6dd70e2dcd
James Babcock <jimrandomh@gmail.com>
2019-05-07 15:20:06 -0700

    Fix typo: subscriptions.create.async->subscription.create.async

--- packages/lesswrong/lib/collections/subscriptions/callbacks.js
+++ packages/lesswrong/lib/collections/subscriptions/callbacks.js
@@ -6,4 +6,4 @@ async function deleteOldSubscriptions(subscription) {
   Subscriptions.update({userId, documentId, collectionName, type}, {$set: {deleted: true}}, {multi: true})
 }
 
-addCallback("subscriptions.create.async", deleteOldSubscriptions);
+addCallback("subscription.create.async", deleteOldSubscriptions);
