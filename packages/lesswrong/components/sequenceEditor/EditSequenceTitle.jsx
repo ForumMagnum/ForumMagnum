c51d19c8378caf8ff651816507259ea7b2a5f962
James Babcock <jimrandomh@gmail.com>
2019-05-14 16:17:05 -0700

    Eliminate Formsy dependency
    Formsy is not compatible with Vulcan 1.13 forms (at least with the
    default config), and there was no good reason for us to be using it.
    Rewrite the two widgets that used Formsy to instead use MUI, and excise
    the dependency.

--- packages/lesswrong/components/sequenceEditor/EditSequenceTitle.jsx
+++ packages/lesswrong/components/sequenceEditor/EditSequenceTitle.jsx
@@ -1,6 +1,7 @@
 import { registerComponent } from 'meteor/vulcan:core';
 import React from 'react';
-import { Textarea } from 'formsy-react-components';
+import PropTypes from 'prop-types'
+import Input from '@material-ui/core/Input';
 import { withStyles } from '@material-ui/core/styles';
 
 const styles = theme => ({
@@ -14,19 +15,27 @@ const styles = theme => ({
   },
 });
 
-const EditSequenceTitle = ({classes, inputProperties, placeholder}) => {
+const EditSequenceTitle = ({classes, inputProperties, value, path, placeholder}, context) => {
   return <div className={classes.root}>
     <div className="sequences-image-scrim-overlay"></div>
     <div className="sequences-editor-title-wrapper">
-      <Textarea
+      <Input
         className="sequences-editor-title"
-        {...inputProperties}
-        placeholder={ placeholder }
-        layout="elementOnly"
+        placeholder={placeholder}
+        value={value}
+        onChange={(event) => {
+          context.updateCurrentValues({
+            [path]: event.target.value
+          })
+        }}
       />
     </div>
   </div>
 }
+
+EditSequenceTitle.contextTypes = {
+  updateCurrentValues: PropTypes.func,
+};
 
 registerComponent("EditSequenceTitle", EditSequenceTitle,
   withStyles(styles, {name: "EditSequenceTitle"}));
