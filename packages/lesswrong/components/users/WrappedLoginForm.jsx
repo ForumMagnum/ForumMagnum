b71a5d58dec85068979029c72d7641742799cef1
James Babcock <jimrandomh@gmail.com>
2019-05-20 10:50:50 -0700

    Move ReCaptcha handling from UsersAccountMenu to WrappedLoginForm
    This (hopefully) makes ReCaptcha work correctly when the login form is
    opened from places other than the main login menu.

--- packages/lesswrong/components/users/WrappedLoginForm.jsx
+++ packages/lesswrong/components/users/WrappedLoginForm.jsx
@@ -1,7 +1,28 @@
-import { registerComponent } from 'meteor/vulcan:core';
+import { Components, registerComponent, getSetting } from 'meteor/vulcan:core';
+import React, { Component } from 'react';
 
-const WrappedLoginForm = ({}) => {
-  return <Components.AccountsLoginForm />
+class WrappedLoginForm extends Component
+{
+  state = {
+    reCaptchaToken: null
+  };
+  
+  setReCaptchaToken = (token) => {
+    this.setState({reCaptchaToken: token})
+  }
+  
+  render() {
+    return <React.Fragment>
+      {getSetting('reCaptcha.apiKey')
+        && <Components.ReCaptcha verifyCallback={this.setReCaptchaToken} action="login/signup"/>}
+      <Components.AccountsLoginForm
+        onPreSignUpHook={(options) => {
+          const reCaptchaToken = this.state.reCaptchaToken
+          return {...options, profile: {...options.profile, reCaptchaToken}}
+        }}
+      />
+    </React.Fragment>;
+  }
 }
 
 registerComponent('WrappedLoginForm', WrappedLoginForm);
