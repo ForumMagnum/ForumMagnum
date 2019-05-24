480dac7e2ffde3e8150bb96eed36dae8e24f18dd
James Babcock <jimrandomh@gmail.com>
2019-05-24 16:02:33 -0700

    Sequence/collection cards can open in new tab

--- packages/lesswrong/components/sequences/SequencesGridItem.jsx
+++ packages/lesswrong/components/sequences/SequencesGridItem.jsx
@@ -1,7 +1,6 @@
 import {
   Components,
   registerComponent,
-  Utils
 } from 'meteor/vulcan:core';
 import NoSSR from 'react-no-ssr';
 import React, { PureComponent } from 'react';
@@ -15,7 +14,6 @@ import { legacyBreakpoints } from '../../lib/modules/utils/theme';
 const styles = theme => ({
   root: {
     ...theme.typography.postStyle,
-    cursor: 'pointer',
 
     width: "33%",
     padding: 15,
@@ -43,9 +41,6 @@ const styles = theme => ({
 
   topWithAuthor: {
     height: 68,
-  },
-
-  link: {
   },
 
   title: {
@@ -83,6 +78,7 @@ const styles = theme => ({
   },
 
   bottom: {
+    pointerEvents: "none",
   },
 
   image: {
@@ -114,18 +110,14 @@ class SequencesGridItem extends PureComponent {
     return '/s/' + this.props.sequence._id
   }
 
-  handleClick = (event) => {
-    const url = this.getSequenceUrl()
-    const navigate = this.props.router.push
-    Utils.manualClickNavigation(event, url, navigate)
-  }
-
   render() {
     const { sequence, showAuthor=false, classes } = this.props
+    const { LinkCard } = Components;
+    const url = this.getSequenceUrl()
 
-    return <div className={classes.root} onClick={this.handleClick}>
+    return <LinkCard className={classes.root} to={url}>
       <div className={classNames(classes.top, {[classes.topWithAuthor]: showAuthor})} style={{borderTopColor: sequence.color}}>
-        <Link key={sequence._id} className={classes.link} to={this.getSequenceUrl()}>
+        <Link key={sequence._id} to={url}>
           <Typography variant='title' className={classes.title}>
             {sequence.draft && <span className={classes.draft}>[Draft] </span>}
             {sequence.title}
@@ -147,7 +139,7 @@ class SequencesGridItem extends PureComponent {
           </NoSSR>
         </div>
       </div>
-    </div>;
+    </LinkCard>;
   }
 }
 
