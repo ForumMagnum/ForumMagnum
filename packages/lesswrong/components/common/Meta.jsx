06d90bd88ad660e625a997c6495e7cd51c74bb81
JP Addison <johnpaddison@gmail.com>
2019-05-30 11:11:52 -0700

    meta page use PostList2

--- packages/lesswrong/components/common/Meta.jsx
+++ packages/lesswrong/components/common/Meta.jsx
@@ -12,7 +12,7 @@ const Meta = ({location, currentUser}, context) => {
         titleComponent= {<div className="recent-posts-title-component">
           {currentUser && <div className="new-post-link"><Link to={{pathname:"/newPost", query: {meta: true}}}> new post </Link></div>}
           </div>} >
-          <Components.PostsList terms={recentPostsTerms} />
+          <Components.PostsList2 terms={recentPostsTerms} />
         </Components.Section>
     </div>
   )
