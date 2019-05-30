0ad2190080e9be6f517c55a3806546f59065d131
JP Addison <johnpaddison@gmail.com>
2019-05-30 11:36:48 -0700

    Excude meta posts from sunshine sidebar

--- packages/lesswrong/lib/collections/posts/views.js
+++ packages/lesswrong/lib/collections/posts/views.js
@@ -597,6 +597,7 @@ Posts.addView("sunshineNewPosts", function () {
     selector: {
       reviewedByUserId: {$exists: false},
       frontpageDate: viewFieldNullOrMissing,
+      meta: false,
     },
     options: {
       sort: {
