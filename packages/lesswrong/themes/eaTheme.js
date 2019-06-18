06ca5386a817638e90484ad980429b2ffa4ff490
JP Addison <johnpaddison@gmail.com>
2019-06-18 15:55:39 -0700

    protip, don't adjust all font spacing to fix a title

--- packages/lesswrong/themes/eaTheme.js
+++ packages/lesswrong/themes/eaTheme.js
@@ -50,7 +50,7 @@ const palette = {
 const basicText = {
   color: grey[800],
   // use ems (not rems) to preserve relative height even if font-size is changed
-  lineHeight: '1.25em',
+  lineHeight: '1.75em',
   fontWeight: 400,
   fontFamily: serifStack
 }
