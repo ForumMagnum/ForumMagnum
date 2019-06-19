df337eac6371de09b79a3fd28c749bebbca7c91d
James Babcock <jimrandomh@gmail.com>
2019-06-19 14:56:27 -0700

    Fold individual icons into PostsItemIcons.jsx

--- packages/lesswrong/components/posts/PostsItemIcons.jsx
+++ packages/lesswrong/components/posts/PostsItemIcons.jsx
@@ -1,24 +1,50 @@
 import React from 'react';
 import { registerComponent, Components, getSetting } from 'meteor/vulcan:core';
 import { withStyles } from '@material-ui/core/styles'
+import classNames from 'classnames';
+import Tooltip from '@material-ui/core/Tooltip';
+import StarIcon from '@material-ui/icons/Star';
+import PersonIcon from '@material-ui/icons/Person';
 
 const styles = theme => ({
   postIcon: {
     marginLeft: theme.spacing.unit,
     marginRight: theme.spacing.unit,
-  }
+  },
+  icon: {
+    fontSize: "1.2rem",
+    color: theme.palette.grey[500],
+    position: "relative",
+    top: 3,
+  },
+  alignmentIcon: {
+    fontSize: "1rem",
+    color: theme.palette.grey[500],
+    top: 0,
+  },
 });
 
 const PostsItemIcons = ({post, classes}) => {
-  const { PostsItemCuratedIcon, PostsItemAlignmentIcon, PostsItemPersonalIcon } = Components;
+  const { OmegaIcon } = Components;
   
   return <React.Fragment>
-    {post.curatedDate && <span className={classes.postIcon}><PostsItemCuratedIcon /></span>}
-    {!post.frontpageDate && <span className={classes.postIcon}><PostsItemPersonalIcon /></span>}
+    {post.curatedDate && <span className={classes.postIcon}>
+      <Tooltip title="Curated Post">
+        <StarIcon className={classes.icon}/>
+      </Tooltip>
+    </span>}
+    
+    {!post.frontpageDate && <span className={classes.postIcon}>
+      <Tooltip title="Personal Blogpost">
+        <PersonIcon className={classes.icon}/>
+      </Tooltip>
+    </span>}
     
     {getSetting('forumType') !== 'AlignmentForum' && post.af &&
       <span className={classes.postIcon}>
-        <PostsItemAlignmentIcon />
+        <Tooltip title="Crossposted from AlignmentForum.org">
+          <OmegaIcon className={classNames(classes.icon, classes.alignmentIcon)}/>
+        </Tooltip>
       </span>
     }
   </React.Fragment>
