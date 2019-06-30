c13f85dc17a589148d01d2da636890bec321725b
James Babcock <jimrandomh@gmail.com>
2019-06-29 20:20:43 -0700

    Refactor make_editable
    Remove commented-out field definitions; use addFieldsDict instead of
    addField.

--- packages/lesswrong/lib/editor/make_editable.js
+++ packages/lesswrong/lib/editor/make_editable.js
@@ -1,7 +1,7 @@
 import Users from 'meteor/vulcan:users'
 import { Utils } from 'meteor/vulcan:core'
 import { ContentType } from '../collections/revisions/schema'
-import { accessFilterMultiple } from '../modules/utils/schemaUtils.js';
+import { accessFilterMultiple, addFieldsDict } from '../modules/utils/schemaUtils.js';
 import SimpleSchema from 'simpl-schema'
 
 const RevisionStorageType = new SimpleSchema({
@@ -53,210 +53,78 @@ export const makeEditable = ({collection, options = {}}) => {
   editableCollectionsFields[collection.options.collectionName] = [
     ...(editableCollectionsFields[collection.options.collectionName] || []), 
     fieldName || "contents"
-  ] 
+  ]
 
-  collection.addField([
-    { 
-      fieldName: fieldName || "contents",
-      fieldSchema: {
-        type: RevisionStorageType,
-        inputType: 'UpdateRevisionDataInput',
-        optional: true,
-        viewableBy: ['guests'],
-        editableBy: ['members'],
-        insertableBy: ['members'],
-        group: formGroup,
-        ...permissions,
-        order,
-        control: 'EditorFormComponent',
-        resolveAs: {
-          type: 'Revision',
-          arguments: 'version: String',
-          resolver: async (doc, { version }, { currentUser, Revisions }) => {
-            const field = fieldName || "contents"
-            const { checkAccess } = Revisions
-            if (version) {
-              const revision = await Revisions.findOne({documentId: doc._id, version, fieldName: field})
-              return checkAccess(currentUser, revision) ? revision : null
-            }
-            return {
-              editedAt: (doc[field] && doc[field].editedAt) || new Date(),
-              userId: doc[field] && doc[field].userId,
-              originalContentsType: (doc[field] && doc[field].originalContentsType) || "html",
-              originalContents: (doc[field] && doc[field].originalContents) || {},
-              html: doc[field] && doc[field].html,
-              updateType: doc[field] && doc[field].updateType,
-              version: doc[field] && doc[field].version,
-              wordCount: doc[field] && doc[field].wordCount,
-            }
+  addFieldsDict(collection, {
+    [fieldName || "contents"]: {
+      type: RevisionStorageType,
+      inputType: 'UpdateRevisionDataInput',
+      optional: true,
+      viewableBy: ['guests'],
+      editableBy: ['members'],
+      insertableBy: ['members'],
+      group: formGroup,
+      ...permissions,
+      order,
+      control: 'EditorFormComponent',
+      resolveAs: {
+        type: 'Revision',
+        arguments: 'version: String',
+        resolver: async (doc, { version }, { currentUser, Revisions }) => {
+          const field = fieldName || "contents"
+          const { checkAccess } = Revisions
+          if (version) {
+            const revision = await Revisions.findOne({documentId: doc._id, version, fieldName: field})
+            return checkAccess(currentUser, revision) ? revision : null
           }
-        },
-        form: {
-          hintText:"Plain Markdown Editor",
-          multiLine:true,
-          fullWidth:true,
-          disableUnderline:true,
-          fieldName: fieldName || "contents",
-          commentEditor,
-          commentStyles,
-          getLocalStorageId,
-          enableMarkDownEditor,
-        },
+          return {
+            editedAt: (doc[field] && doc[field].editedAt) || new Date(),
+            userId: doc[field] && doc[field].userId,
+            originalContentsType: (doc[field] && doc[field].originalContentsType) || "html",
+            originalContents: (doc[field] && doc[field].originalContents) || {},
+            html: doc[field] && doc[field].html,
+            updateType: doc[field] && doc[field].updateType,
+            version: doc[field] && doc[field].version,
+            wordCount: doc[field] && doc[field].wordCount,
+          }
+        }
+      },
+      form: {
+        hintText:"Plain Markdown Editor",
+        multiLine:true,
+        fullWidth:true,
+        disableUnderline:true,
+        fieldName: fieldName || "contents",
+        commentEditor,
+        commentStyles,
+        getLocalStorageId,
+        enableMarkDownEditor,
       },
     },
-    {
-      fieldName: Utils.camelCaseify(`${fieldName}Revisions`),
-      fieldSchema: {
-        type: Object,
-        viewableBy: ['guests'],
-        optional: true,
-        resolveAs: {
-          type: '[Revision]',
-          arguments: 'limit: Int = 5',
-          resolver: async (post, { limit }, { currentUser, Revisions }) => {
-            const field = fieldName || "contents"
-            const resolvedDocs = await Revisions.find({documentId: post._id, fieldName: field}, {sort: {editedAt: -1}, limit}).fetch()
-            return accessFilterMultiple(currentUser, Revisions, resolvedDocs);
-          }
+    [Utils.camelCaseify(`${fieldName}Revisions`)]: {
+      type: Object,
+      viewableBy: ['guests'],
+      optional: true,
+      resolveAs: {
+        type: '[Revision]',
+        arguments: 'limit: Int = 5',
+        resolver: async (post, { limit }, { currentUser, Revisions }) => {
+          const field = fieldName || "contents"
+          const resolvedDocs = await Revisions.find({documentId: post._id, fieldName: field}, {sort: {editedAt: -1}, limit}).fetch()
+          return accessFilterMultiple(currentUser, Revisions, resolvedDocs);
         }
       }
     },
-    {
-      fieldName: Utils.camelCaseify(`${fieldName}Version`),
-      fieldSchema: {
-        type: String,
-        viewableBy: ['guests'],
-        optional: true,
-        resolveAs: {
-          type: 'String',
-          resolver: (post) => {
-            return post[fieldName || "contents"] && post[fieldName || "contents"].version
-          }
+    [Utils.camelCaseify(`${fieldName}Version`)]: {
+      type: String,
+      viewableBy: ['guests'],
+      optional: true,
+      resolveAs: {
+        type: 'String',
+        resolver: (post) => {
+          return post[fieldName || "contents"] && post[fieldName || "contents"].version
         }
       }
     }
-    // /**
-    //   Draft-js content
-    // */
-    // {
-    //   fieldName: Utils.camelCaseify(`${fieldName}Content`),
-    //   fieldSchema: {
-    //     type: Object,
-    //     optional: true,
-    //     ...permissions,
-    //     control: 'EditorFormComponent',
-    //     blackbox: true,
-    //     group: formGroup,
-    //     hidden: true,
-    //     order,
-    //     form: {
-          // hintText:"Plain Markdown Editor",
-          // multiLine:true,
-          // fullWidth:true,
-          // disableUnderline:true,
-          // fieldName: fieldName,
-          // commentEditor,
-          // commentStyles,
-          // getLocalStorageId,
-          // enableMarkDownEditor,
-    //     },
-    //   }
-    // },
-
-    // /**
-    //   Html Body field, made editable to allow access in edit form
-    // */
-    // {
-    //   fieldName: Utils.camelCaseify(`${fieldName}HtmlBody`),
-    //   fieldSchema: {
-    //     type: String,
-    //     optional: true,
-    //     viewableBy: ['guests'],
-    //     editableBy: ['admins'],
-    //     insertableBy: ['admins'],
-    //     control: "textarea",
-    //     group: adminFormGroup,
-    //     hidden: !adminFormGroup, // Only display htmlBody if admin form group is given
-    //   }
-    // },
-
-    /*
-      body: Stores a markdown version of the post
-    */
-
-    // {
-    //   fieldName: Utils.camelCaseify(`${fieldName}Body`),
-    //   fieldSchema: {
-    //     type: String,
-    //     viewableBy: ['guests'],
-    //     insertableBy: ['members'],
-    //     editableBy: ['members'],
-    //     control: "textarea",
-    //     optional: true,
-    //     hidden: true,
-    //     max: 1000000, //overwriting Vulcan's character limit
-    //   }
-    // },
-
-    // /*
-    //   htmlHighlight: stores an HTML highlight of an HTML body
-    // */
-
-    // {
-    //   fieldName: Utils.camelCaseify(`${fieldName}HtmlHighlight`),
-    //   fieldSchema: {
-    //     type: String,
-    //     optional: true,
-    //     hidden:true,
-    //     viewableBy: ['guests'],
-    //   }
-    // },
-
-    // /*
-    //   wordCount: count of words
-    // */
-
-    // {
-    //   fieldName: Utils.camelCaseify(`${fieldName}WordCount`),
-    //   fieldSchema: {
-    //     type: Number,
-    //     viewableBy: ['guests'],
-    //     optional: true,
-    //     hidden:true
-    //   }
-    // },
-
-    // /*
-    //   plaintextExcerpt: Version of the excerpt that is plaintext, used for the description head tags which are
-    //   used by Facebook and Google to extract previews of content.
-    // */
-
-    // {
-    //   fieldName: Utils.camelCaseify(`${fieldName}PlaintextExcerpt`),
-    //   fieldSchema: {
-    //     type: String,
-    //     viewableBy: ['guests'],
-    //     hidden: true,
-    //     optional: true
-    //   }
-    // },
-
-    // /*
-    //   lastEditedAs: Records whether the post was last edited in HTML, Markdown or Draft-JS, and displays the
-    //   appropriate editor when being edited, overwriting user-preferences
-    // */
-
-    // {
-    //   fieldName: Utils.camelCaseify(`${fieldName}LastEditedAs`),
-    //   fieldSchema: {
-    //     type: String,
-    //     viewableBy: ['guests'],
-    //     insertableBy: ['members'],
-    //     editableBy: ['members'],
-    //     optional: true,
-    //     hidden: true,
-    //     group: adminFormGroup,
-    //   }
-    // },
-  ])
+  });
 }
