4f330174166d8b32da1948455fc77fcb9cdbd450
Raymond Arnold <rarnold@DESKTOP-89H1FKK.localdomain>
2019-07-09 15:42:54 -0700

    remove extras

--- packages/lesswrong/components/shortform/ShortformPage.jsx
+++ packages/lesswrong/components/shortform/ShortformPage.jsx
@@ -1,21 +1,15 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import { Components, registerComponent } from 'meteor/vulcan:core';
-import withUser from '../common/withUser';
 
-class ShortformPage extends PureComponent {
-  state = { showShortformFeed: false }
+const ShortformPage = () => {
+  const { SingleColumnSection, ShortformThreadList, SectionTitle } = Components
 
-  render () {
-    // const { currentUser } = this.props
-    const { SingleColumnSection, ShortformThreadList, SectionTitle } = Components
-
-    return (
-      <SingleColumnSection>
-        <SectionTitle title="Shortform Content [Beta]"/>  
-        <ShortformThreadList terms={{view: 'shortform', limit:20}} />
-      </SingleColumnSection>
-    )
-  }
+  return (
+    <SingleColumnSection>
+      <SectionTitle title="Shortform Content [Beta]"/>  
+      <ShortformThreadList terms={{view: 'shortform', limit:20}} />
+    </SingleColumnSection>
+  )
 }
 
-registerComponent('ShortformPage', ShortformPage, withUser);
+registerComponent('ShortformPage', ShortformPage);
