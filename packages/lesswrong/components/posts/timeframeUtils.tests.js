c8183a1bc5d085136be990cde45cff6fb82b0863
JP Addison <johnpaddison@gmail.com>
2019-07-18 23:39:02 -0700

    shoulda checked the tests again

--- packages/lesswrong/components/posts/timeframeUtils.tests.js
+++ packages/lesswrong/components/posts/timeframeUtils.tests.js
@@ -3,22 +3,22 @@ import { getDateRange } from './timeframeUtils'
 describe('getDateRange', () => {
   it('handles days', () => {
     const result = getDateRange('2019-01-01', '2019-01-03', 'day')
-    result.should.deep.equal(['2019-01-03', '2019-01-02', '2019-01-01'])
+    result.should.deep.equal(['2019-01-02', '2019-01-01'])
   })
 
   it('handles weeks, basic case', () => {
     const result = getDateRange('2019-01-01', '2019-01-15', 'week')
-    result.should.deep.equal(['2019-01-15', '2019-01-08', '2019-01-01'])
+    result.should.deep.equal(['2019-01-08', '2019-01-01'])
   })
 
   it('handles months', () => {
     const result = getDateRange('2019-01-01', '2019-03-01', 'month')
-    result.should.deep.equal(['2019-03-01', '2019-02-01', '2019-01-01'])
+    result.should.deep.equal(['2019-02-01', '2019-01-01'])
   })
 
   it('handles years', () => {
-    const result = getDateRange('2018-01-01', '2019-01-01', 'year')
-    result.should.deep.equal(['2019-01-01', '2018-01-01'])
+    const result = getDateRange('2019-01-01', '2020-01-01', 'year')
+    result.should.deep.equal(['2019-01-01'])
   })
 
   // --- Sad cases --- //
@@ -28,19 +28,19 @@ describe('getDateRange', () => {
   // back than expected
   it('handles partial timeBlocks', () => {
     const result = getDateRange('2019-01-02', '2019-01-15', 'week')
-    result.should.deep.equal(['2019-01-15', '2019-01-08', '2019-01-01'])
+    result.should.deep.equal(['2019-01-08', '2019-01-01'])
   })
 
   it('handles reversed start and end dates', () => {
     (
       () => getDateRange('2019-01-03', '2019-01-01', 'day')
-    ).should.throw(Error, /got a after .* after the before/)
+    ).should.throw(Error, /got after .* after the before/)
   })
 
   it('handles malformed dates', () => {
     (
       () => getDateRange('01/01/2019', '2019-01-03', 'day')
-    ).should.throw(Error, /Invalid \w+Date/)
+    ).should.throw(Error, /Invalid after/)
   })
 
   it('handles malformed timeBlock', () => {
