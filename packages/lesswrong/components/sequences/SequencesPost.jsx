52dafa32a642291dda78ff94e79213486caf05ba
Discordius <panisnecis@gmail.com>
2019-07-19 19:05:49 -0700

    Fix sequences post params

--- packages/lesswrong/components/sequences/SequencesPost.jsx
+++ packages/lesswrong/components/sequences/SequencesPost.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 import { withRouter } from 'react-router';
 import { parseQuery } from '../../lib/routeUtil.js';
 
-const SequencesPost = ({params, router, location}) => {
+const SequencesPost = ({params, router, location, match: {params: { postId, sequenceId }}}) => {
   const query = parseQuery(location);
-  const version = query?.revision
-  return <Components.PostsPageWrapper documentId={params.postId} sequenceId={params.sequenceId} version={version} />
+  const version = query.revision
+  return <Components.PostsPageWrapper documentId={postId} sequenceId={sequenceId} version={version} />
 };
 
 registerComponent('SequencesPost', SequencesPost, withRouter);
