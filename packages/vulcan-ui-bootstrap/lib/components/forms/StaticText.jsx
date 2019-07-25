d5eac7de32ff7c00f6fad08d924d3a33f032cbdb
Discordius <panisnecis@gmail.com>
2019-07-24 18:13:52 -0700

    Add vulcan-ui-bootstrap back to packages

---
+++ packages/vulcan-ui-bootstrap/lib/components/forms/StaticText.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Components, registerComponent } from 'meteor/vulcan:core';
+
+const parseUrl = value => {
+  return value && value.toString().slice(0, 4) === 'http' ? (
+    <a href={value} target="_blank">
+      {value}
+    </a>
+  ) : (
+    value
+  );
+};
+
+const StaticComponent = ({ inputProperties, itemProperties }) => (
+  <Components.FormItem path={inputProperties.path} label={inputProperties.label} {...itemProperties}>
+    <div>{parseUrl(inputProperties.value)}</div>
+  </Components.FormItem>
+);
+
+registerComponent('FormComponentStaticText', StaticComponent);
