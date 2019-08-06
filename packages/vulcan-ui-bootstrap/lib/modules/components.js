b1ffe63fef344e931e65dc60bd5c2997859f9a54
James Babcock <jimrandomh@gmail.com>
2019-08-05 18:29:22 -0700

    Remove vulcan-ui-bootstrap components for unused datatypes

--- packages/vulcan-ui-bootstrap/lib/modules/components.js
+++ packages/vulcan-ui-bootstrap/lib/modules/components.js
@@ -1,14 +1,9 @@
 import '../components/forms/FormElement.jsx';
 import '../components/forms/Checkboxgroup.jsx';
-import '../components/forms/Date2.jsx';
 import '../components/forms/Default.jsx';
 import '../components/forms/Email.jsx';
-import '../components/forms/Radiogroup.jsx';
-import '../components/forms/SelectMultiple.jsx';
 import '../components/forms/Textarea.jsx';
-import '../components/forms/Time.jsx';
 import '../components/forms/Url.jsx';
-import '../components/forms/StaticText.jsx';
 import '../components/forms/FormComponentInner.jsx';
 import '../components/forms/FormControl.jsx'; // note: only used by old accounts package, remove soon?
 import '../components/forms/FormItem.jsx';
