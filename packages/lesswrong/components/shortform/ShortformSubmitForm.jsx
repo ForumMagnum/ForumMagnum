4e5278bb698b3e31c7fb2b29955563439b530bcb
Raymond Arnold <raemon777@gmail.com>
2019-08-07 16:21:25 -0700

    shortformSubmitRevert

--- packages/lesswrong/components/shortform/ShortformSubmitForm.jsx
+++ packages/lesswrong/components/shortform/ShortformSubmitForm.jsx
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import { Components, registerComponent } from 'meteor/vulcan:core';
 import { withStyles } from '@material-ui/core/styles';
 
@@ -6,43 +6,21 @@ const styles = theme => ({
   root: {
     marginLeft: 12,
     marginRight: 12
-  },
-  placeholderWrapper: {
-    marginTop: 12,
-    marginBottom: 24,
-    display: "flex",
-    justifyContent: "space-between"
-  },
-  placeholder: {
-    ...theme.typography.commentStyle,
-    ...theme.typography.body2,
-    fontWeight: 300,
-    color: theme.palette.grey[600]
-  },
-  placeholderSubmit: {
-    ...theme.typography.commentStyle,
-    ...theme.typography.body2,
-    color: theme.palette.grey[500],
-    marginRight: 8
   }
 }) 
 
-class ShortformSubmitForm extends PureComponent {
-  
-  render() {
-    const { classes, successCallback } = this.props
-    const { CommentsNewForm } = Components;
+const ShortformSubmitForm = ({ classes, successCallback}) => {
+  const { CommentsNewForm } = Components;
 
-    return (
-      <div className={classes.root}>
-        <CommentsNewForm
-          prefilledProps={{shortform: true}}
-          successCallback={successCallback}
-          type="comment"
-        />
-      </div>
-    );
-  }
+  return (
+    <div className={classes.root}>
+      <CommentsNewForm
+        prefilledProps={{shortform: true}}
+        successCallback={successCallback}
+        type="comment"
+      />
+    </div>
+  );
 }
 
 registerComponent('ShortformSubmitForm', ShortformSubmitForm, withStyles(styles, {name:"ShortformSubmitForm"}));
