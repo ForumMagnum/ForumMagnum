b2f583d683bf2e1b24bfbc2ff24f2a624ccbc56c
James Babcock <jimrandomh@gmail.com>
2019-08-07 17:24:30 -0700

    Re-merge vulcan-accounts custom fields
    This was caused by merging the Vulcan repo into the LW repo with one PR
    not synced from github.

--- packages/vulcan-accounts/imports/ui/components/LoginFormInner.jsx
+++ packages/vulcan-accounts/imports/ui/components/LoginFormInner.jsx
@@ -263,7 +263,8 @@ export class AccountsLoginFormInner extends TrackerComponent {
     switch (field) {
       case 'password': break;
       default:
-        value = value.trim();
+        if (typeof value === 'string')
+          value = value.trim();
         break;
     }
     this.setState({ [field]: value });
@@ -349,6 +350,15 @@ export class AccountsLoginFormInner extends TrackerComponent {
 
     if (this.showEnrollAccountForm()) {
       loginFields.push(this.getSetPasswordField());
+    }
+    
+    const { customSignupFields } = this.props;
+    if (customSignupFields && formState == STATES.SIGN_UP) {
+      for (let i=0; i<customSignupFields.length; i++)
+        loginFields.push({
+          ...customSignupFields[i],
+          onChange: this.handleChange.bind(this, customSignupFields[i].id)
+        });
     }
 
     return _.indexBy(loginFields, 'id');
@@ -536,13 +546,23 @@ export class AccountsLoginFormInner extends TrackerComponent {
    * Helper to get field values when switching states in the form.
    */
   getDefaultFieldValues() {
-    if (typeof localStorage !== 'undefined' && localStorage) {
-      const defaultFieldValues = JSON.parse(localStorage.getItem('accounts_ui') || null);
-      if (defaultFieldValues
-        && defaultFieldValues.passwordSignupFields === passwordSignupFields()) {
-        return defaultFieldValues;
+    let defaultFieldValues = {};
+
+    const { customSignupFields } = this.props;
+    if (customSignupFields) {
+      for (let i=0; i<customSignupFields.length; i++) {
+        defaultFieldValues[customSignupFields[i].id] = customSignupFields[i].defaultValue;
       }
     }
+    if (typeof localStorage !== 'undefined' && localStorage) {
+      const savedDefaultFieldValues = JSON.parse(localStorage.getItem('accounts_ui') || null);
+      if (savedDefaultFieldValues
+        && savedDefaultFieldValues.passwordSignupFields === passwordSignupFields()) {
+        defaultFieldValues = {...defaultFieldValues, ...savedDefaultFieldValues};
+      }
+    }
+    
+    return defaultFieldValues;
   }
 
   /**
@@ -634,7 +654,7 @@ export class AccountsLoginFormInner extends TrackerComponent {
       usernameOrEmail = null,
       password,
       formState,
-      onSubmitHook
+      onSubmitHook,
     } = this.state;
     let error = false;
     let loginSelector;
@@ -790,6 +810,9 @@ export class AccountsLoginFormInner extends TrackerComponent {
       formState,
       onSubmitHook
     } = this.state;
+    const {
+      customSignupFields
+    } = this.props;
 
     // add extra fields to options
     if (this.props.extraFields) {
@@ -834,6 +857,16 @@ export class AccountsLoginFormInner extends TrackerComponent {
       error = true;
     } else {
       options.password = password;
+    }
+    
+    if (customSignupFields) {
+      if (!options.profile)
+        options.profile = {};
+
+      for (let i=0; i<customSignupFields.length; i++) {
+        const fieldId = customSignupFields[i].id
+        options.profile[fieldId] = this.state[fieldId];
+      }
     }
 
     // set the signup locale
