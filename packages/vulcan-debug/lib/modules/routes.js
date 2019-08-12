54873f72bb161690388c8321ca5c5bf1c9ee2c1f
James Babcock <jimrandomh@gmail.com>
2019-08-11 19:32:07 -0700

    Remove a getDynamicComponent call that didn't do anything (file unconditionally imported elsewhere)

--- packages/vulcan-debug/lib/modules/routes.js
+++ packages/vulcan-debug/lib/modules/routes.js
@@ -1,8 +1,8 @@
-import {addRoute, getDynamicComponent} from 'meteor/vulcan:lib';
+import {addRoute} from 'meteor/vulcan:lib';
 
 addRoute([
   // {name: 'cheatsheet', path: '/cheatsheet', component: import('./components/Cheatsheet.jsx')},
-  {name: 'groups', path: '/groups', component: () => getDynamicComponent(import('../components/Groups.jsx')), layoutName: 'AdminLayout'},
+  {name: 'groups', path: '/groups', componentName: 'Groups', layoutName: 'AdminLayout'},
   {name: 'settings', path: '/settings', componentName: 'Settings', layoutName: 'AdminLayout'},
   {name: 'callbacks', path: '/callbacks', componentName: 'Callbacks', layoutName: 'AdminLayout'},
   {name: 'routes', path: '/routes', componentName: 'Routes', layoutName: 'AdminLayout'},
