149635f3119a64373b38fae4e56fc93c2db54afc
James Babcock <jimrandomh@gmail.com>
2019-08-14 19:59:27 -0700

    deleteOldSubscriptions: Fix run-order issue where callback that was supposed to delete old sub, deleted new sub too

--- packages/lesswrong/lib/collections/subscriptions/callbacks.js
+++ packages/lesswrong/lib/collections/subscriptions/callbacks.js
@@ -4,6 +4,7 @@ import { Subscriptions } from './collection'
 async function deleteOldSubscriptions(subscription) {
   const { userId, documentId, collectionName, type } = subscription
   Subscriptions.update({userId, documentId, collectionName, type}, {$set: {deleted: true}}, {multi: true})
+  return subscription;
 }
 
-addCallback("subscription.create.async", deleteOldSubscriptions);
+addCallback("subscription.create.before", deleteOldSubscriptions);
