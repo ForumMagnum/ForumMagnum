0479b7efbbb7ffcf33825e4f6b9d0bf499e4f9d1
James Babcock <jimrandomh@gmail.com>
2019-08-16 22:39:47 -0700

    Fix a legit broken import

--- packages/lesswrong/testing/client.tests.js
+++ packages/lesswrong/testing/client.tests.js
@@ -1,3 +1,3 @@
 import '../components/comments/CommentsItem/_comments-unit-tests.js';
 import '../components/posts/timeframeUtils.tests.js';
-import './moderation/moderation.client.tests.js';
+import './moderation/moderation.frontend.tests.js';
