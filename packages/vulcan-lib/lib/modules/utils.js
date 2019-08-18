360d9ebf65b5c36e3880c9ea0b5c56faa7776850
James Babcock <jimrandomh@gmail.com>
2019-08-17 22:23:05 -0700

    Don't extend String

--- packages/vulcan-lib/lib/modules/utils.js
+++ packages/vulcan-lib/lib/modules/utils.js
@@ -443,11 +443,6 @@ Utils.getRoutePath = routeName => {
   return Routes[routeName] && Routes[routeName].path;
 };
 
-String.prototype.replaceAll = function(search, replacement) {
-  var target = this;
-  return target.replace(new RegExp(search, 'g'), replacement);
-};
-
 Utils.isPromise = value => isFunction(get(value, 'then'));
 
 Utils.pluralize = s => {
