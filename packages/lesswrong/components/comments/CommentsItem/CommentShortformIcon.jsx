ee03bf2d64aafff7b632b64507577fb4b8d6fabf
Raymond Arnold <raemon777@gmail.com>
2019-08-18 12:56:08 -0700

    Show shortform icon in SingleLine comments

--- packages/lesswrong/components/comments/CommentsItem/CommentShortformIcon.jsx
+++ packages/lesswrong/components/comments/CommentsItem/CommentShortformIcon.jsx
@@ -19,9 +19,12 @@ const styles = theme => ({
   }
 });
 
-const CommentShortformIcon = ({comment, post, classes}) => {
+const CommentShortformIcon = ({comment, post, classes, simple}) => {
   // Top level shortform posts should show this icon/button, both to make shortform posts a bit more visually distinct, and to make it easier to grab permalinks for shortform posts.
   if (!comment.shortform || comment.topLevelCommentId) return null
+
+  if (simple) return <NotesIcon className={classes.icon} />
+
   return (
     <Tooltip title="Shortform Permalink">
       <Link to={Comments.getPageUrlFromIds({postId:post._id, postSlug:post.slug, commentId: comment._id})}>
