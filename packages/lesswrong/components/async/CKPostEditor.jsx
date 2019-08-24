24d8be2e165b8c35c06994b54aab31fd3f4c35a1
Discordius <panisnecis@gmail.com>
2019-08-23 20:30:14 -0700

    Hook up sidebar resizing

--- packages/lesswrong/components/async/CKPostEditor.jsx
+++ packages/lesswrong/components/async/CKPostEditor.jsx
@@ -15,20 +15,27 @@ const styles = theme => ({
   sidebar: {
     position: 'absolute',
     right: -350,
-    width: 300
+    width: 300,
+    [theme.breakpoints.down('md')]: {
+      position: 'absolute',
+      right: -100,
+      width: 50
+    },
+    [theme.breakpoints.down('sm')]: {
+      right: 0
+    }
   }
 })
 
-const refreshDisplayMode = ( editor ) => {
+const refreshDisplayMode = ( editor, sidebarElement ) => {
   const annotations = editor.plugins.get( 'Annotations' );
-  const sidebarElement = this.sidebarElementRef.current;
 
   if ( window.innerWidth < 1000 ) {
     sidebarElement.classList.remove( 'narrow' );
     sidebarElement.classList.add( 'hidden' );
     annotations.switchTo( 'inline' );
   }
-  else if ( window.innerWidth < 1300 ) {
+  else if ( window.innerWidth < 1400 ) {
     sidebarElement.classList.remove( 'hidden' );
     sidebarElement.classList.add( 'narrow' );
     annotations.switchTo( 'narrowSidebar' );
@@ -45,7 +52,7 @@ const Editor = ({ data, onSave, documentId, userId, formType, onInit, classes })
   const [layoutReady, setLayoutReady] = useState(false)
   useEffect(() => {
     setLayoutReady(true)
-  })
+  }, [])
 
   const sidebarRef = useRef(null)
   const presenceListRef = useRef(null)
@@ -55,12 +62,19 @@ const Editor = ({ data, onSave, documentId, userId, formType, onInit, classes })
       <script src='https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/latest.js?config=TeX-AMS_HTML' async></script>
     </Helmet>
     <div ref={presenceListRef} />
-    <div ref={sidebarRef} className={classes.sidebar} />
+    {/* Because of a bug in CKEditor, we call preventDefault on all click events coming out of the sidebar. See: https://github.com/ckeditor/ckeditor5/issues/1992 */}
+    <div ref={sidebarRef} className={classes.sidebar} onClick={(e) => e.preventDefault()}/>
     {layoutReady && <CKEditor
       editor={ PostEditor }
       onInit={ editor => {
           // Uncomment this line and the import above to activate the CKEDItor debugger
           // CKEditorInspector.attach(editor)
+
+          // We listen to the current window size to determine how to show comments
+          window.addEventListener( 'resize', () => refreshDisplayMode(editor, sidebarRef.current) );
+          // We then call the method once to determine the current window size
+          refreshDisplayMode(editor, sidebarRef.current)
+
           onInit(editor)
       } }
       config={{
