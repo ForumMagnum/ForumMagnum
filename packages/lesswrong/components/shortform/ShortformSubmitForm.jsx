065139a52a2db87ecda88ae919e352d1e6bcb32b
Raymond Arnold <raemon777@gmail.com>
2019-09-03 14:03:06 -0700

    improve shortform prompt

--- packages/lesswrong/components/shortform/ShortformSubmitForm.jsx
+++ packages/lesswrong/components/shortform/ShortformSubmitForm.jsx
@@ -5,7 +5,7 @@ import { withStyles } from '@material-ui/core/styles';
 const styles = theme => ({
   root: {
     marginLeft: 12,
-    marginRight: 12
+    marginRight: 12,
   }
 }) 
 
@@ -22,8 +22,8 @@ const ShortformSubmitForm = ({ classes, successCallback}) => {
         type="comment"
         formProps={{
           editorHintText: <div>
-            <p>Write your thoughts here! What have you been thinking about?</p>
-            <p>Incomplete, exploratory, formative, rambly, rough, boring, draft-stage, unedited, and even (gasp!) wrong thoughts are all welcome on Shortform.</p>
+            <div>Write your thoughts here! What have you been thinking about?</div>
+            <div>Exploratory, draft-stage, rough, and rambly thoughts are all welcome on Shortform.</div>
           </div>
         }}
       />
