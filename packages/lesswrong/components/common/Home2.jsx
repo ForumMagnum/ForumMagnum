f643f68e2f2ead75a1a90dbf82eeb159e97b9b0a
Discordius <panisnecis@gmail.com>
2019-09-04 21:16:03 -0700

    Add map to frontpage and fix small filter bug

--- packages/lesswrong/components/common/Home2.jsx
+++ packages/lesswrong/components/common/Home2.jsx
@@ -1,33 +1,32 @@
 import { Components, registerComponent } from 'meteor/vulcan:core';
-import React, { PureComponent } from 'react';
+import React from 'react';
 import withUser from '../common/withUser';
 import Users from 'meteor/vulcan:users';
+import { useLocation } from '../../lib/routeUtil';
 
-class Home2 extends PureComponent {
+const Home2 = ({currentUser}) => {
+  const { RecentDiscussionThreadsList, HomeLatestPosts, RecommendationsAndCurated, CommunityMapWrapper } = Components
 
-  render () {
-    const { currentUser } = this.props
-
-    const { RecentDiscussionThreadsList, HomeLatestPosts, RecommendationsAndCurated } = Components
-
-    const shouldRenderSidebar = Users.canDo(currentUser, 'posts.moderate.all') ||
-        Users.canDo(currentUser, 'alignment.sidebar')
-
-    return (
-      <React.Fragment>
-        {shouldRenderSidebar && <Components.SunshineSidebar/>}
-
-        <RecommendationsAndCurated configName="frontpage" />
-        <HomeLatestPosts />
-        <RecentDiscussionThreadsList
-          terms={{view: 'recentDiscussionThreadsList', limit:20}}
-          commentsLimit={4}
-          maxAgeHours={18}
-          af={false}
-        />
-      </React.Fragment>
-    )
-  }
+  const shouldRenderSidebar = Users.canDo(currentUser, 'posts.moderate.all') ||
+      Users.canDo(currentUser, 'alignment.sidebar')
+  const { lat, lng } = Users.getLocation(currentUser)
+  const { query } = useLocation()
+  const mapEventTerms = { view: 'nearbyEvents', lat, lng, filters: query?.filters || []}
+  
+  return (
+    <React.Fragment>
+      {shouldRenderSidebar && <Components.SunshineSidebar/>}
+      <CommunityMapWrapper terms={mapEventTerms} />
+      <RecommendationsAndCurated configName="frontpage" />
+      <HomeLatestPosts />
+      <RecentDiscussionThreadsList
+        terms={{view: 'recentDiscussionThreadsList', limit:20}}
+        commentsLimit={4}
+        maxAgeHours={18}
+        af={false}
+      />
+    </React.Fragment>
+  )
 }
 
 registerComponent('Home2', Home2, withUser);
