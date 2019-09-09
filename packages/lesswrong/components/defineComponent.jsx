cfca307a170ba74235275ce7e31457c280423a19
JP Addison <johnpaddison@gmail.com>
2019-09-09 16:26:13 -0700

    Revert "rename utils file"
    This reverts commit 9adfe25b50a8128e82605ec19f01144075c4ce90.

--- packages/lesswrong/components/defineComponent.jsx
+++ packages/lesswrong/components/defineComponent.jsx
@@ -1,16 +1,16 @@
-import { registerComponent, Components, getSetting } from 'meteor/vulcan:core';
+import { registerComponent } from 'meteor/vulcan:core';
 import { withStyles } from '@material-ui/core/styles';
 import compose from 'lodash/flowRight';
 
 export function defineComponent({name, component, split, styles, hocs})
 {
   component.displayName = name;
-
+  
   if (styles) {
     hocs = _.clone(hocs);
     hocs.push(withStyles(styles, {name: name}));
   }
-
+  
   if (split) {
     const hocsExecuted = hocs.map(hoc => {
       if(!Array.isArray(hoc)) return hoc;
@@ -22,12 +22,4 @@ export function defineComponent({name, component, split, styles, hocs})
   } else {
     registerComponent(name, component, ...hocs);
   }
-}
-
-// TODO; doc
-export const loadEventComponent = (componentName) => {
-  if (getSetting('hasEvents', true)) {
-    return Components[componentName]
-  }
-  return null
 }
