d36487da70aa3dcabe50d3407cc14c4271e23549
Discordius <panisnecis@gmail.com>
2019-09-13 16:46:02 -0700

    Lint

--- packages/lesswrong/components/posts/PostsEditPage.jsx
+++ packages/lesswrong/components/posts/PostsEditPage.jsx
@@ -1,7 +1,6 @@
 import React, { PureComponent } from 'react';
-import { Components, registerComponent, getSetting } from 'meteor/vulcan:core';
+import { Components, registerComponent } from 'meteor/vulcan:core';
 import { withLocation } from '../../lib/routeUtil'
-import { Helmet } from 'react-helmet';
 
 class PostsEditPage extends PureComponent {
 
@@ -9,7 +8,6 @@ class PostsEditPage extends PureComponent {
     const { query } = this.props.location;
     const postId = query.postId;
     const eventForm = !!(query.eventForm === "true");
-    const mapsAPIKey = getSetting('googleMaps.apiKey', null);
     
     return <div>
       <Components.PostsEditForm documentId={postId} eventForm={eventForm}/>
