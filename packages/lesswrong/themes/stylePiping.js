d5ee25794679cf4036b5383287150d0f2a4de43b
Raymond Arnold <raemon777@gmail.com>
2019-09-17 17:58:36 -0700

    ckEditor Styling Tweaks

--- packages/lesswrong/themes/stylePiping.js
+++ packages/lesswrong/themes/stylePiping.js
@@ -221,6 +221,60 @@ export const postHighlightStyles = theme => {
   return deepmerge(postBodyStyles(theme), postHighlightStyles, {isMergeableObject:isPlainObject})
 }
 
+export const ckEditorStyles = theme => {
+  return {
+    '& .ck': {
+      '--ck-spacing-standard': `${theme.spacing.unit}px`,
+      '&.ck-content': {
+        marginLeft: -theme.spacing.unit,
+        '--ck-focus-outer-shadow-geometry': "none",
+        '--ck-focus-ring': "solid 1px rgba(0,0,0,0)",
+        '--ck-focus-outer-shadow': "none",
+        '--ck-inner-shadow': "none"
+      },
+      '&.ck-sidebar, &.ck-presence-list': { //\u25B6
+        '& li': {
+          // By default ckEditor elements get the styles from postBodyStyles li elements
+          marginBottom: 'unset',
+          fontFamily: 'unset',
+          fontSize: 'unset',
+          fontWeight: 'unset',
+          lineHeight: 'unset'
+        },
+        '& .ck-comment:after': {
+          display:"none"
+        },
+        '& .ck-annotation__info-name, & .ck-annotation__info-time, & .ck-comment__input, & .ck-thread__comment-count, & .ck-annotation__main p, & .ck-annotation__info-name, & .ck-annotation__info-time, & .ck-presence-list__counter': {
+          ...commentBodyStyles(theme),
+        },
+        '& .ck-thread__comment-count': {
+          paddingLeft: theme.spacing.unit*2,
+          color: theme.palette.grey[600],
+          margin: 0,
+          paddingBottom: ".5em",
+          '&:before': {
+            content: '"\\25B6"'
+          }
+        },
+        '& .ck-annotation__main, & .ck-comment__input, & .ck-thread__input': {
+          width : "100%"
+        },
+        '& .ck-comment__wrapper': {
+          borderTop: 'solid 1px rgba(0,0,0,.15)',
+        },
+        '& .ck-annotation__info-name, & .ck-annotation__info-time': {
+          color: theme.palette.grey[600],
+          fontSize: "1rem"
+        },
+        '& .ck-annotation__user, & .ck-thread__user': {
+          display: "none"
+        },
+        '--ck-color-comment-count': theme.palette.primary.main,
+      } 
+    }
+  }
+}
+
 export const editorStyles = (theme, styleFunction) => ({
     '& .public-DraftStyleDefault-block': {
       marginTop: '1em',
@@ -228,11 +282,12 @@ export const editorStyles = (theme, styleFunction) => ({
     },
     '& code .public-DraftStyleDefault-block': {
       marginTop: 0,
-      marginBottom: 0,
+      marginBottom: 0,  
     },
     '& blockquote .public-DraftStyleDefault-block': {
       marginTop: 0,
       marginBottom: 0,
     },
-    ...styleFunction(theme)
+    ...styleFunction(theme),
+    ...ckEditorStyles(theme)
 })
