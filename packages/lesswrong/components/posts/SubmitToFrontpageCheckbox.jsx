0ab1b9db4a23da025b56d18f9bc3774200881c3c
JP Addison <johnpaddison@gmail.com>
2019-09-25 17:05:19 -0700

    moderators may promote to frontpage

--- packages/lesswrong/components/posts/SubmitToFrontpageCheckbox.jsx
+++ packages/lesswrong/components/posts/SubmitToFrontpageCheckbox.jsx
@@ -2,8 +2,40 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Tooltip from '@material-ui/core/Tooltip';
 import Checkbox from '@material-ui/core/Checkbox';
-import { registerComponent } from 'meteor/vulcan:core';
+import { registerComponent, getSetting } from 'meteor/vulcan:core';
 import { withStyles } from '@material-ui/core/styles';
+
+const forumDefaultCheckboxLabels = {
+  LessWrong: 'Moderators may promote to Frontpage',
+  AlignmentForum: 'Moderators may promote to Frontpage',
+  EAForum: 'Moderators may promote to Frontpage or Community'
+}
+
+const defaultCheckboxLabel = forumDefaultCheckboxLabels[getSetting('forumType')]
+
+const defaultTooltipLWAF = ({classes}) => <div className={classes.tooltip}>
+  <p>LW moderators will consider this post for frontpage</p>
+  <p className={classes.guidelines}>Things to aim for:</p>
+  <ul>
+    <li className={classes.guidelines}>
+      Usefulness, novelty and fun
+    </li>
+    <li className={classes.guidelines}>
+      Timeless content (minimize reference to current events)
+    </li>
+    <li className={classes.guidelines}>
+      Explain rather than persuade
+    </li>
+  </ul>
+</div>
+
+const forumDefaultTooltip = {
+  LessWrong: defaultTooltipLWAF,
+  AlignmentForum: defaultTooltipLWAF,
+  EAForum: () => "Uncheck this box if you want your post to stay on your personal blog."
+}
+
+const defaultTooltip = forumDefaultTooltip[getSetting('forumType')]
 
 const styles = theme => ({
   submitToFrontpageWrapper: {
@@ -23,12 +55,16 @@ const styles = theme => ({
       paddingRight: theme.spacing.unit*3,
     }
   },
+  checkboxRoot: {
+    display: 'flex',
+  },
   checkboxLabel: {
     fontWeight:500,
     fontFamily: theme.typography.commentStyle.fontFamily,
     fontSize: 16,
     color: "rgba(0,0,0,0.4)",
-    verticalAlign: 'middle'
+    verticalAlign: 'middle',
+    lineHeight: '1.25em'
   },
   tooltip: {
     '& ul': {
@@ -66,31 +102,15 @@ class SubmitToFrontpageCheckbox extends Component {
   }
 
   render() {
-    const { classes, label="Moderators may promote", tooltip } = this.props
+    const { classes, label=defaultCheckboxLabel, tooltip } = this.props
 
-    const displayedTooltip = tooltip || <div className={classes.tooltip}>
-      <p>LW moderators will consider this post for frontpage</p>
-      <p className={classes.guidelines}>Things to aim for:</p>
-      <ul>
-        <li className={classes.guidelines}>
-          Usefulness, novelty and fun
-        </li>
-        <li className={classes.guidelines}>
-          Timeless content (minimize reference to current events)
-        </li>
-        <li className={classes.guidelines}>
-          Explain rather than persuade
-        </li>
-      </ul>
-    </div>
+    const displayedTooltip = tooltip || defaultTooltip({classes})
 
     return <div className={classes.submitToFrontpageWrapper}>
       <Tooltip title={displayedTooltip}>
         <div className={classes.submitToFrontpage}>
-          <div>
-            <Checkbox checked={this.getCurrentValue()} onClick={this.handleClick}/>
-            <span className={classes.checkboxLabel}>{label}</span>
-          </div>
+          <Checkbox checked={this.getCurrentValue()} onClick={this.handleClick}/>
+          <span className={classes.checkboxLabel}>{label}</span>
         </div>
       </Tooltip>
     </div>
