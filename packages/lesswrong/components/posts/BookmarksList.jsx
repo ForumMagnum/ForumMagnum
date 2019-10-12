753178cfc8d10eef44733fb226afdd18e32b829b
Raymond Arnold <raemon777@gmail.com>
2019-10-11 19:52:47 -0700

    cleanup pr

--- packages/lesswrong/components/posts/BookmarksList.jsx
+++ packages/lesswrong/components/posts/BookmarksList.jsx
@@ -13,14 +13,13 @@ const BookmarksList = ({currentUser, limit=50 }) => {
     fragmentName: 'UserBookmarks',
     fetchPolicy: 'cache-then-network',
     documentId: currentUser._id,
-    ssr: true
   });
 
   let bookmarkedPosts = user?.bookmarkedPosts || []
   bookmarkedPosts = bookmarkedPosts.slice(0, limit)
 
   if (loading) return <Loading/>
-  console.log(bookmarkedPosts)
+
   return (
     <div>
       {bookmarkedPosts.map((post) => <PostsItem2 key={post._id} post={post} bookmark/>)}
