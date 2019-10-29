e996aaf3291a672c506a420c74342e4c58ac36ba
Raymond Arnold <raemon777@gmail.com>
2019-10-29 13:03:32 -0700

    revert PostsRevisionsList

--- packages/lesswrong/components/posts/PostsPage/PostsRevisionsList.jsx
+++ packages/lesswrong/components/posts/PostsPage/PostsRevisionsList.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { withStyles } from '@material-ui/core/styles';
-import { registerComponent, Components, useSingle } from 'meteor/vulcan:core';
+import { registerComponent, Components, withSingle } from 'meteor/vulcan:core';
 import MenuItem from '@material-ui/core/MenuItem';
 import { Posts } from '../../../lib/collections/posts/collection'
 import { QueryLink } from '../../../lib/reactRouterWrapper.js';
@@ -8,38 +8,30 @@ import { QueryLink } from '../../../lib/reactRouterWrapper.js';
 
 const styles = theme => ({
   version: {
-    marginRight: theme.spacing.unit,
-    fontWeight: 600,
+    marginRight: 5
   }
 })
 
-const PostsRevisionsList = ({documentId, classes}) => {
-
-  const { document, loading } = useSingle({
-    queryName: `PostsRevisionsList`,
-    collection: Posts,
-    fetchPolicy: 'network-only', // Ensure that we load the list of revisions a new every time we click (this is useful after editing a post)
-    fragmentName: 'PostsRevisionsList',
-    documentId: documentId,
-  });
-
-  console.log("revisions document", document)
-
+const PostsRevisionsList = ({document, loading, classes}) => {
   const { FormatDate } = Components
-
-  if (loading || !document) return <MenuItem disabled> Loading... </MenuItem>
-  if (!loading && !document) return null 
-
+  if (loading || !document) {return <MenuItem disabled> Loading... </MenuItem>} 
   const { revisions } = document
-
   return <React.Fragment>
     {revisions.map(({editedAt, version, user}) => 
-    <MenuItem key={version} value={version}>
+    <MenuItem key={version} component={QueryLink} query={{revision: version}} merge>
       <span className={classes.version}>v{version}</span> <FormatDate date={editedAt}/>
     </MenuItem>)}
   </React.Fragment>
 }
 
-registerComponent('PostsRevisionsList', PostsRevisionsList, 
+const queryOptions = {
+  queryName: `PostsRevisionsList`,
+  collection: Posts,
+  fetchPolicy: 'network-only', // Ensure that we load the list of revisions a new every time we click (this is useful after editing a post)
+  fragmentName: 'PostsRevisionsList'
+}
+
+
+registerComponent('PostsRevisionsList', PostsRevisionsList, [withSingle, queryOptions], 
   withStyles(styles, {name: "PostsRevisionsList"})
 )
