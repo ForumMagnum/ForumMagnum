0fd046baa8d8bc2b3a14dde7d39fba0585c3b872
Raemon <raemon777@gmail.com>
2019-10-30 11:46:50 -0700

    Reintroduce all plugins but math

--- public/lesswrong-editor/src/ckeditor.js
+++ public/lesswrong-editor/src/ckeditor.js
@@ -4,22 +4,22 @@
  */
 
 import BalloonBlockEditorBase from '@ckeditor/ckeditor5-editor-balloon/src/ballooneditor';
-// import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';
+import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';
 import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
 import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder';
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
-// import FontFamily from '@ckeditor/ckeditor5-font/src/fontfamily';
-// import FontSize from '@ckeditor/ckeditor5-font/src/fontsize';
+import FontFamily from '@ckeditor/ckeditor5-font/src/fontfamily';
+import FontSize from '@ckeditor/ckeditor5-font/src/fontsize';
 import Heading from '@ckeditor/ckeditor5-heading/src/heading';
-// import Highlight from '@ckeditor/ckeditor5-highlight/src/highlight';
+import Highlight from '@ckeditor/ckeditor5-highlight/src/highlight';
 import Image from '@ckeditor/ckeditor5-image/src/image';
 import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
 import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle';
 import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar';
 import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload';
-// import ImageResize from '@ckeditor/ckeditor5-image/src/imageresize';
+import ImageResize from '@ckeditor/ckeditor5-image/src/imageresize';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
 import Link from '@ckeditor/ckeditor5-link/src/link';
 import List from '@ckeditor/ckeditor5-list/src/list';
@@ -29,38 +29,14 @@ import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefrom
 import PresenceList from '@ckeditor/ckeditor5-real-time-collaboration/src/presencelist';
 import RealTimeCollaborativeComments from '@ckeditor/ckeditor5-real-time-collaboration/src/realtimecollaborativecomments';
 import RealTimeCollaborativeTrackChanges from '@ckeditor/ckeditor5-real-time-collaboration/src/realtimecollaborativetrackchanges';
-// import RemoveFormat from '@ckeditor/ckeditor5-remove-format/src/removeformat';
-// import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough';
+import RemoveFormat from '@ckeditor/ckeditor5-remove-format/src/removeformat';
+import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough';
 import Table from '@ckeditor/ckeditor5-table/src/table';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
-// import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline';
+import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline';
 import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter';
 import BlockToolbar from '@ckeditor/ckeditor5-ui/src/toolbar/block/blocktoolbar';
-// import Autosave from '@ckeditor/ckeditor5-autosave/src/autosave';
-
-// import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
-// import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter';
-// import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
-// import BlockToolbar from '@ckeditor/ckeditor5-ui/src/toolbar/block/blocktoolbar';
-// import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
-// import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
-// import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
-// import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder';
-// import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage';
-// import Heading from '@ckeditor/ckeditor5-heading/src/heading';
-// import Image from '@ckeditor/ckeditor5-image/src/image';
-// import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
-// import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle';
-// import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar';
-// import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload';
-// import Indent from '@ckeditor/ckeditor5-indent/src/indent';
-// import Link from '@ckeditor/ckeditor5-link/src/link';
-// import List from '@ckeditor/ckeditor5-list/src/list';
-// import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed';
-// import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
-// import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
-// import Table from '@ckeditor/ckeditor5-table/src/table';
-// import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
+import Autosave from '@ckeditor/ckeditor5-autosave/src/autosave';
 
 // import MathpreviewPlugin from 'ckeditor5-math-preview/src/mathpreview';
 
@@ -68,8 +44,7 @@ class CommentEditor extends BalloonBlockEditorBase {}
 class PostEditor extends BalloonBlockEditorBase {}
 class PostEditorCollaboration extends BalloonBlockEditorBase {}
 
-// Tables and MediaEmbeds are commented out for now, but will be added back in as soon as some minor
-// minor issues are debugged.
+// MediaEmbeds are commented out for now (will be added back in once we decide how to render them)
 
 // NOTE: If you make changes to this file, you must:
 // 1. navigate in your terminal to the corresponding folder ('cd ./public/lesswrong-editor')
@@ -86,35 +61,35 @@ const headingOptions = {
 };
 
 const postEditorPlugins = [
-	// Autosave,
-	// Alignment,
+	Autosave,
+	Alignment,
 	Autoformat,
 	BlockToolbar,
 	BlockQuote,
 	Bold,
 	CKFinder,
 	Essentials,
-	// FontFamily,
-	// FontSize,
+	FontFamily,
+	FontSize,
 	Heading,
-	// Highlight,
+	Highlight,
 	Image,
 	ImageCaption,
 	ImageStyle,
 	ImageToolbar,
 	ImageUpload,
-	// ImageResize,
+	ImageResize,
 	Italic,
 	Link,
 	List,
 	// MediaEmbed,
 	Paragraph,
 	PasteFromOffice,
-	// RemoveFormat,
-	// Strikethrough,
+	RemoveFormat,
+	Strikethrough,
 	Table,
 	TableToolbar,
-	// Underline,
+	Underline,
 	UploadAdapter,
 	// MathpreviewPlugin
 ];
@@ -190,8 +165,8 @@ PostEditorCollaboration.defaultConfig = {
 };
 
 CommentEditor.builtinPlugins = [
-	// Autosave,
-	// Alignment,
+	Autosave,
+	Alignment,
 	Autoformat,
 	BlockQuote,
 	Bold,
@@ -208,10 +183,10 @@ CommentEditor.builtinPlugins = [
 	List,
 	Paragraph,
 	PasteFromOffice,
-	// RemoveFormat,
-	// Strikethrough,
-	// Table,
-	// Underline,
+	RemoveFormat,
+	Strikethrough,
+	Table,
+	Underline,
 	UploadAdapter,
 	// MathpreviewPlugin
 ];
