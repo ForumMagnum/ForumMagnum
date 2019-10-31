abd6036d7d5ecde9110041f7510fe48a211f2d45
JP Addison <johnpaddison@gmail.com>
2019-10-31 14:27:38 -0700

    address pr comment 1

--- packages/lesswrong/components/posts/PostsPage/PostsPageTitle.jsx
+++ packages/lesswrong/components/posts/PostsPage/PostsPageTitle.jsx
@@ -5,30 +5,24 @@ import Typography from '@material-ui/core/Typography';
 import { Link } from 'react-router-dom';
 import { Posts } from '../../../lib/collections/posts';
 
-const styles = theme => {
-  // Bold titles need a little personal space
-  const margin = theme.typography.display3.fontWeight >= 600 ?
-    {marginTop: '0 !important', marginBottom: '.2em !important', marginLeft: '0 !important', marginRight: '.2em !important'} :
-    {margin: '0 !important'}
-  return {
-    root: {
-      ...theme.typography.postStyle,
-      ...theme.typography.display3,
-      ...theme.typography.headerStyle,
-      ...margin,
-      color: theme.palette.text.primary,
-      [theme.breakpoints.down('sm')]: {
-        fontSize: '2.5rem',
-      }
-    },
-    draft: {
-      color: theme.palette.grey[500]
-    },
-    question: {
-      color: theme.palette.grey[600],
-      display: "block",
-      marginTop: "1em"
+const styles = theme => ({
+  root: {
+    ...theme.typography.display3,
+    ...theme.typography.postStyle,
+    ...theme.typography.headerStyle,
+    margin: "0 !important",
+    color: theme.palette.text.primary,
+    [theme.breakpoints.down('sm')]: {
+      fontSize: '2.5rem',
     }
+  },
+  draft: {
+    color: theme.palette.grey[500]
+  },
+  question: {
+    color: theme.palette.grey[600],
+    display: "block",
+    marginTop: "1em"
   }
 }
 
