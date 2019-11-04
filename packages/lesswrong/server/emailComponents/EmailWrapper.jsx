5c09025159a343429b4da710895ad33b5290c1a0
JP Addison <johnpaddison@gmail.com>
2019-11-04 12:52:26 -0800

    fair enough linter, fair enough

--- packages/lesswrong/server/emailComponents/EmailWrapper.jsx
+++ packages/lesswrong/server/emailComponents/EmailWrapper.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { registerComponent, getSetting } from 'meteor/vulcan:core';
 import { Utils } from 'meteor/vulcan:lib';
 import { withStyles } from '@material-ui/core/styles';
-import { emailBodyStyles } from '../../themes/stylePiping'
+// import { emailBodyStyles } from '../../themes/stylePiping'
 
 const styles = theme => ({
   // root: emailBodyStyles(theme)
