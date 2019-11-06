a16be24c3ad5708f95c0dfdab8f6e12adedaa373
JP Addison <johnpaddison@gmail.com>
2019-11-05 16:34:52 -0800

    add posts to emailpreviewpage

--- packages/lesswrong/server/notificationBatching.js
+++ packages/lesswrong/server/notificationBatching.js
@@ -5,6 +5,7 @@ import { getNotificationTypeByNameServer } from './notificationTypesServer.jsx';
 import { EventDebouncer } from './debouncer.js';
 import toDictionary from '../lib/modules/utils/toDictionary.js';
 import Users from 'meteor/vulcan:users';
+import { Posts } from '../lib/collections/posts';
 import { Components, addGraphQLQuery, addGraphQLSchema, addGraphQLResolvers } from 'meteor/vulcan:core';
 import { UnsubscribeAllToken } from './emails/emailTokens.js';
 import { generateEmail, sendEmail, logSentEmail } from './emails/renderEmail.js';
@@ -92,28 +93,51 @@ export const wrapAndSendEmail = async ({user, subject, body}) => {
     await sendEmail(email);
     await logSentEmail(email, user);
   } catch(e) {
+    // TODO; really tho
+    console.error(e)
     Sentry.captureException(e);
   }
 }
 
 addGraphQLResolvers({
   Query: {
-    async EmailPreview(root, {notificationIds}, context) {
+    async EmailPreview(root, secondArg, context) {
+      // TODO; enforce arg presence
+      const {notificationIds, postId} = secondArg
+      console.log('gql')
+      console.log(' secondArg', secondArg)
+      console.log(' postId', postId)
+      console.log(' notificationIds', notificationIds)
       const { currentUser } = context;
       if (!Users.isAdmin(currentUser)) {
         throw new Error("This debug feature is only available to admin accounts");
       }
-      if (!notificationIds || !notificationIds.length) {
+      if (!notificationIds?.length && !postId) {
         return [];
       }
+      if (notificationIds?.length && postId) {
+        throw new Error("Please only specify notificationIds or postId in the query")
+      }
       
-      const notifications = await Notifications.find(
-        { _id: {$in: notificationIds} }
-      ).fetch();
-      const emails = await notificationBatchToEmails({
-        user: currentUser,
-        notifications
-      });
+      let emails
+      if (notificationIds?.length) {
+        const notifications = await Notifications.find(
+          { _id: {$in: notificationIds} }
+        ).fetch();
+        emails = await notificationBatchToEmails({
+          user: currentUser,
+          notifications
+        });
+        console.log('emails.length', emails.length)
+      }
+      if (postId) {
+        const post = Posts.findOne(postId)
+        emails = [{
+          user: currentUser,
+          subject: post.title,
+          body: <Components.NewPostEmail documentId={post._id} reason='cause I said so' />
+        }]
+      }
       const renderedEmails = await Promise.all(emails.map(async email => await wrapAndRenderEmail(email)));
       return renderedEmails;
     }
@@ -127,4 +151,4 @@ addGraphQLSchema(`
     text: String
   }
 `);
-addGraphQLQuery("EmailPreview(notificationIds: [String!]): [EmailPreview]");
+addGraphQLQuery("EmailPreview(notificationIds: [String], postId: String): [EmailPreview]");
