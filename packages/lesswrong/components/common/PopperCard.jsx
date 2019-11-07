de4885580c745b75fd885dba7f2d05cabecaffa9
James Babcock <jimrandomh@gmail.com>
2019-11-07 14:26:21 -0800

    Show tag description in hover when searching

--- packages/lesswrong/components/common/PopperCard.jsx
+++ packages/lesswrong/components/common/PopperCard.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { registerComponent, Components } from 'meteor/vulcan:core';
 import Card from '@material-ui/core/Card';
 
-const PopperCard = ({children, open, anchorEl}) => {
-  return <Components.LWPopper open={open} anchorEl={anchorEl} placement="bottom-start">
+const PopperCard = ({children, placement="bottom-start", open, anchorEl}) => {
+  return <Components.LWPopper open={open} anchorEl={anchorEl} placement={placement}>
     <Card>
       {children}
     </Card>
--- packages/lesswrong/components/tagging/TagSearchHit.jsx
+++ packages/lesswrong/components/tagging/TagSearchHit.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import { registerComponent } from 'meteor/vulcan:core';
+import { registerComponent, Components, useSingle } from 'meteor/vulcan:core';
 import { withStyles } from '@material-ui/core/styles';
+import withHover from '../common/withHover';
+import { Tags } from '../../lib/collections/tags/collection.js';
 
 const styles = {
   root: {
@@ -8,15 +10,40 @@ const styles = {
     padding: 8,
     cursor: "pointer",
   },
+  card: {
+    padding: 16,
+    width: 400,
+  },
 };
 
-const TagSearchHit = ({hit, onClick, classes}) => {
+const TagSearchHit = ({hit, onClick, hover, anchorEl, classes}) => {
+  const { PopperCard, ContentItemBody, Loading } = Components;
+  const { document: tag } = useSingle({
+    documentId: hit._id,
+    collection: Tags,
+    queryName: "tagSearchHit",
+    fragmentName: "TagFragment",
+    fetchPolicy: 'cache-then-network',
+  });
+  
   return (
-    <a className={classes.root} onClick={onClick} >
-      {hit.name}
-    </a>
+    <React.Fragment>
+      <PopperCard open={hover} anchorEl={anchorEl} placement="right-start">
+        <div className={classes.card}>
+          {!tag && <Loading/>}
+          {tag && <ContentItemBody
+            dangerouslySetInnerHTML={{__html: tag.description?.htmlHighlight}}
+            description={`tag ${tag.name}`}
+          />}
+        </div>
+      </PopperCard>
+      <a className={classes.root} onClick={onClick} >
+        {hit.name}
+      </a>
+    </React.Fragment>
   );
 }
 
 registerComponent("TagSearchHit", TagSearchHit,
+  withHover,
   withStyles(styles, {name: "TagSearchHit"}));
