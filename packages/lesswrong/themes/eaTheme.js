9c41ff7f890ef4601be8e2236e0fee327278ced9
JP Addison <johnpaddison@gmail.com>
2019-11-08 11:23:32 -0800

    display names for editors

--- packages/lesswrong/themes/eaTheme.js
+++ packages/lesswrong/themes/eaTheme.js
@@ -150,7 +150,7 @@ const theme = createLWTheme({
       main: {
         margin: '30px auto 15px auto',
         '@media (max-width: 959.95px)': {
-          marginTop: 0
+          marginTop: 36,
         }
       }
     },
@@ -255,6 +255,11 @@ const theme = createLWTheme({
     TabNavigationMenuSubItem: {
       root: {
         color: grey[800]
+      }
+    },
+    PostsPageTitle: {
+      root: {
+        lineHeight: 1.25
       }
     },
     PostsTimeBlock: {
