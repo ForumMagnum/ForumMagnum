cf6c20bfbab59f5cd8e1d04f507a4b8eb65e8452
Discordius <panisnecis@gmail.com>
2019-11-18 13:04:22 -0800

    Make expand message windows compatible

--- packages/lesswrong/lib/editor/ellipsize.jsx
+++ packages/lesswrong/lib/editor/ellipsize.jsx
@@ -102,7 +102,7 @@ export const commentExcerptFromHTML = (comment, currentUser, postPage) => {
     // This varies by the size of the comment or truncation amount, and reducing it by 1/4th seems about right.
     TruncateLength: Math.floor(truncationCharCount - (truncationCharCount/4)),
     TruncateBy: "characters",
-    Suffix: `... <span class="read-more"><a class="read-more-default">(Read more)</a><a class="read-more-tooltip">(Click to expand thread. ⌘F to Expand All)</a><span class="read-more-f-tooltip">Cmd/Ctrl F to expand all comments on this post</span></span>${styles}`,
+    Suffix: `... <span class="read-more"><a class="read-more-default">(Read more)</a><a class="read-more-tooltip">(Click to expand thread. ⌘/CTRL+F to Expand All)</a><span class="read-more-f-tooltip">Cmd/Ctrl F to expand all comments on this post</span></span>${styles}`,
   });
 };
 
