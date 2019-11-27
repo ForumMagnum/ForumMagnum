57eba915494cde2879328824c56a6fa39dfbce6b
Raemon <raemon777@gmail.com>
2019-11-27 14:05:35 -0800

    ckEditor blockquotes

--- packages/lesswrong/themes/createThemeDefaults.js
+++ packages/lesswrong/themes/createThemeDefaults.js
@@ -124,7 +124,6 @@ const createLWTheme = (theme) => {
         paddingLeft: spacingUnit*2,
         borderLeft: `solid 3px ${grey[300]}`,
         margin: 0,
-        ...body1FontSize
       },
       commentBlockquote: {
         fontWeight: 400,
@@ -135,7 +134,6 @@ const createLWTheme = (theme) => {
         borderLeft: `solid 3px ${grey[300]}`,
         margin: 0,
         marginLeft: spacingUnit*1.5,
-        ...body2FontSize
       },
       codeblock: {
         backgroundColor: grey[100],
