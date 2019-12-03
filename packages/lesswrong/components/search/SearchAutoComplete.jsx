d1f7804a5a94d46f3db02b4cab802af8d1c4e92e
James Babcock <jimrandomh@gmail.com>
2019-12-02 18:23:12 -0800

    Make connectAutoComplete use registerComponent HoC handling

--- packages/lesswrong/components/search/SearchAutoComplete.jsx
+++ packages/lesswrong/components/search/SearchAutoComplete.jsx
@@ -4,45 +4,42 @@ import { InstantSearch, Configure } from 'react-instantsearch-dom';
 import { connectAutoComplete } from 'react-instantsearch/connectors';
 import Autosuggest from 'react-autosuggest';
 
-const SearchAutoComplete = connectAutoComplete(
-  ({ hits, currentRefinement, refine, clickAction, placeholder, renderSuggestion, hitsPerPage=7, indexName }) =>
-  {
-    const algoliaAppId = getSetting('algolia.appId')
-    const algoliaSearchKey = getSetting('algolia.searchKey')
-    
-    const onSuggestionSelected = (event, { suggestion }) => {
-      event.preventDefault();
-      event.stopPropagation();
-      clickAction(suggestion._id)
-    }
-    return <InstantSearch
-      indexName={indexName}
-      appId={algoliaAppId}
-      apiKey={algoliaSearchKey}
-    >
-      <div className="posts-search-auto-complete">
-        <Autosuggest
-          suggestions={hits}
-          onSuggestionSelected={(event, {suggestion}) => {
-            event.preventDefault();
-            event.stopPropagation();
-            onSuggestionSelected(suggestion);
-          }}
-          onSuggestionsFetchRequested={({ value }) => refine(value)}
-          onSuggestionsClearRequested={() => refine('')}
-          getSuggestionValue={hit => hit.title}
-          renderSuggestion={renderSuggestion}
-          inputProps={{
-            placeholder: placeholder,
-            value: currentRefinement,
-            onChange: () => {},
-          }}
-          highlightFirstSuggestion
-        />
-        <Configure hitsPerPage={hitsPerPage} />
-      </div>
-    </InstantSearch>
+const SearchAutoComplete = ({ hits, currentRefinement, refine, clickAction, placeholder, renderSuggestion, hitsPerPage=7, indexName }) => {
+  const algoliaAppId = getSetting('algolia.appId')
+  const algoliaSearchKey = getSetting('algolia.searchKey')
+  
+  const onSuggestionSelected = (event, { suggestion }) => {
+    event.preventDefault();
+    event.stopPropagation();
+    clickAction(suggestion._id)
   }
-);
+  return <InstantSearch
+    indexName={indexName}
+    appId={algoliaAppId}
+    apiKey={algoliaSearchKey}
+  >
+    <div className="posts-search-auto-complete">
+      <Autosuggest
+        suggestions={hits}
+        onSuggestionSelected={(event, {suggestion}) => {
+          event.preventDefault();
+          event.stopPropagation();
+          onSuggestionSelected(suggestion);
+        }}
+        onSuggestionsFetchRequested={({ value }) => refine(value)}
+        onSuggestionsClearRequested={() => refine('')}
+        getSuggestionValue={hit => hit.title}
+        renderSuggestion={renderSuggestion}
+        inputProps={{
+          placeholder: placeholder,
+          value: currentRefinement,
+          onChange: () => {},
+        }}
+        highlightFirstSuggestion
+      />
+      <Configure hitsPerPage={hitsPerPage} />
+    </div>
+  </InstantSearch>
+}
 
-registerComponent("SearchAutoComplete", SearchAutoComplete);
+registerComponent("SearchAutoComplete", SearchAutoComplete, connectAutoComplete);
