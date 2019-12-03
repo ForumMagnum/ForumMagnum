081160ff4ed92878bc0dbf2c44570c35c179f8ac
Raemon <raemon777@gmail.com>
2019-12-02 16:38:03 -0800

    wip inlineComments

---
+++ packages/lesswrong/lib/collections/inlineComments/schema.js
@@ -0,0 +1,24 @@
+const schema = {
+  _id: {
+    optional: true,
+    type: String,
+    viewableBy: ['members'],
+  },
+  createdAt: {
+    optional: true,
+    type: Date,
+    viewableBy: ['members'],
+    onInsert: (document) => new Date(),
+  },
+  userId: {
+    ...foreignKeyField({
+      idFieldName: "userId",
+      resolverName: "user",
+      collectionName: "Users",
+      type: "User",
+    }),
+    optional: true,
+    canRead: ['guests'],
+    canCreate: ['members'],
+    hidden: true,
+  },
