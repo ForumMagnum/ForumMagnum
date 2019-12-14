876a2ee447fabeeb5e29e41ecbd3236e06c6ec94
James Babcock <jimrandomh@gmail.com>
2019-12-13 17:49:56 -0800

    Remove unused HoC `withAccess`

--- packages/vulcan-core/lib/modules/index.js
+++ packages/vulcan-core/lib/modules/index.js
@@ -15,7 +15,6 @@ export { default as Datatable } from './components/Datatable.jsx';
 export { default as ScrollToTop } from './components/ScrollToTop.jsx';
 
 export * from './containers/cacheUpdates.js';
-export { default as withAccess } from './containers/withAccess.js';
 export { default as withMulti, useMulti } from './containers/withMulti.js';
 export { default as withSingle, useSingle } from './containers/withSingle.js';
 export { default as withCreate, useCreate } from './containers/withCreate.js';
