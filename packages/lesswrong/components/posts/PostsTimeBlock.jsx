1d4fc2c6b23a5f5d58592621588d6eb1b74a367c
James Babcock <jimrandomh@gmail.com>
2019-12-13 17:56:01 -0800

    Fix some usages of withCurrentUser (should be withUser)

--- packages/lesswrong/components/posts/PostsTimeBlock.jsx
+++ packages/lesswrong/components/posts/PostsTimeBlock.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import {
-  withCurrentUser, Components, withList, registerComponent, getSetting
+  Components, withList, registerComponent, getSetting
 } from 'meteor/vulcan:core';
 import Typography from '@material-ui/core/Typography';
 import Hidden from '@material-ui/core/Hidden';
@@ -12,6 +12,7 @@ import { timeframeToTimeBlock } from './timeframeUtils'
 import { queryIsUpdating } from '../common/queryStatusUtils'
 import withTimezone from '../common/withTimezone';
 import { QueryLink } from '../../lib/reactRouterWrapper.js';
+import withUser from '../common/withUser.js';
 
 const styles = theme => ({
   root: {
@@ -216,5 +217,5 @@ registerComponent('PostsTimeBlock', PostsTimeBlock,
     ssr: true,
   }],
   withTimezone,
-  withCurrentUser, withStyles(styles, { name: "PostsTimeBlock" })
+  withUser, withStyles(styles, { name: "PostsTimeBlock" })
 );
