ac1d7d6dd918a9284236d64b73fdd69f3e58d52c
Discordius <panisnecis@gmail.com>
2019-12-22 13:49:48 -0800

    Also fix recent discussion thread

--- packages/lesswrong/components/comments/RecentDiscussionThread.jsx
+++ packages/lesswrong/components/comments/RecentDiscussionThread.jsx
@@ -105,7 +105,7 @@ const RecentDiscussionThread = ({
       setReadStatus(true);
       setMarkedAsVisitedAt(new Date());
       setExpandAllThreads(true);
-      recordPostView({post, type: "recentDiscussionClick"})
+      recordPostView({post, extraEventProperties: {type: "recentDiscussionClick"}})
     },
     [setReadStatus, setMarkedAsVisitedAt, setExpandAllThreads, recordPostView, post]
   );
