c69dc42269be2a1c50c4fc29e12fef2c10265d9e
Ruben Bloom <rmb042@gmail.com>
2019-12-25 23:16:18 -0800

    convert withHover to be Hook underneath and add tracking. Will see if it works.

--- packages/lesswrong/components/common/withHover.jsx
+++ packages/lesswrong/components/common/withHover.jsx
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import { hookToHoc } from '../../lib/hocUtils.js';
+import { withTracking } from '../../lib/analyticsEvents'
 
-const withHover = (WrappedComponent) => {
+export const HoverOpenContext = React.createContext('hoverOpen')
+
+const HoverManagerComponent = (WrappedComponent) => {
   return class HoverableComponent extends Component {
     state = { hover: false, anchorEl: null }
 
     handleMouseOver = (event) => {
       this.setState({ hover: true, anchorEl: event.currentTarget})
+      this.captureEvent("hoverEventTriggered")
     }
 
     handleMouseLeave = () => {
@@ -15,12 +20,17 @@ const withHover = (WrappedComponent) => {
     render () {
       const props = { hover: this.state.hover, anchorEl: this.state.anchorEl, ...this.props, stopHover: this.handleMouseLeave }
       return (
-        <span onMouseOver={this.handleMouseOver} onMouseLeave={this.handleMouseLeave}>
-          <WrappedComponent { ...props } />
-        </span>
+          <HoverOpenContext.Provider value={
+            <span onMouseOver={this.handleMouseOver} onMouseLeave={this.handleMouseLeave}>
+            <WrappedComponent {...props} />
+            </span>
+          }/>
       )
     }
   }
 }
 
+export const HoverManager = withTracking(HoverManagerComponent)
+export const useHover = () => React.useContext(HoverOpenContext)
+export const withHover = hookToHoc(useHover)
 export default withHover
