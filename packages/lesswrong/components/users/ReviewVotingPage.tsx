3b2286bd3a7488f7725d829930c2a3185664c585
Raemon <raemon777@gmail.com>
2020-01-02 17:46:32 -0800

    remove unused imports

--- packages/lesswrong/components/users/ReviewVotingPage.tsx
+++ packages/lesswrong/components/users/ReviewVotingPage.tsx
@@ -1,13 +1,6 @@
 import React, { useState, useReducer } from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import IconButton from '@material-ui/core/IconButton';
-import ArrowRightIcon from '@material-ui/icons/ArrowRight';
 import TextField from '@material-ui/core/TextField';
 import { find, reject, sortBy, sumBy } from 'lodash'
 import { registerComponent, Components, useMulti } from 'meteor/vulcan:core';
