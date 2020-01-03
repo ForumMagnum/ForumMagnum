6c8a20b370a0546bc3dc5db80398d4c8659b3c65
Ruben Bloom <rmb042@gmail.com>
2020-01-03 14:21:06 -0800

    Finish refactoring inView

--- packages/lesswrong/components/common/AnalyticsInViewTracker.jsx
+++ packages/lesswrong/components/common/AnalyticsInViewTracker.jsx
@@ -1,25 +1,28 @@
 import { registerComponent } from 'meteor/vulcan:core';
-import React, { useEffect } from 'react';
+import React, { useEffect, useCallback } from 'react';
 import { useIsInView, useTracking } from "../../lib/analyticsEvents";
 
 const AnalyticsInViewTracker = ({eventType, eventProps, observerProps, children, skip}) => {
-    const { ref, entry } = useIsInView(observerProps)
-    const { time, isIntersecting, intersectionRatio } = entry
+  const { setNode, entry } = useIsInView(observerProps)
 
-    const { captureEvent } = useTracking(eventType)
-    useEffect(() => {
-        !skip && !!entry && captureEvent(eventType||"inViewEvent", {
-            ...eventProps,
-            ...observerProps,
-            ...{ time, isIntersecting, intersectionRatio }
-        })
-    }, [entry])
+  const captureInViewEvent = useCallback(
+    useTracking({ eventType: eventType || "inViewEvent", eventProps: {...eventProps, ...observerProps}}).captureEvent,
+    // absolutely no reason for eventType or props to change for InView tracker once created, easiest way to prevent rerender because of object props
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [])
 
-    return (
-        <span ref={ref}>
-            { children }
-        </span>
-    )
+  useEffect(() => {
+    if (!skip && !!entry) {
+      const {time, isIntersecting, intersectionRatio} = entry
+      !skip && !!entry && captureInViewEvent(undefined, {time, isIntersecting, intersectionRatio})
+    }
+  }, [entry, captureInViewEvent, skip])
+
+  return (
+    <span ref={setNode}>
+      { children }
+    </span>
+  )
 }
 
 registerComponent('AnalyticsInViewTracker', AnalyticsInViewTracker)
--- packages/lesswrong/lib/analyticsEvents.js
+++ packages/lesswrong/lib/analyticsEvents.js
@@ -98,21 +98,29 @@ export function useTracking({eventType="unnamed", eventProps = {}, captureOnMoun
 export const withTracking = hookToHoc(useTracking)
 
 export function useIsInView({rootMargin='0px', threshold=0}={}) {
-  const [entry, setEntry] = useState(false)
-  const ref = useRef()
+  const [entry, setEntry] = useState(null)
+  const [node, setNode] = useState(null)
+
+  const observer = useRef(null)
+
   useEffect(() => {
-    const observer = new IntersectionObserver(([ entry ]) => {
+    if (observer.current && node) observer.current.disconnect()
+
+    observer.current = new window.IntersectionObserver(([ entry ]) => {
       setEntry(entry)
     }, {
       rootMargin,
       threshold
     })
-    if (ref.current) observer.observe(ref.current)
-    return () => {
-      observer.unobserve(ref.current)
-    }
-  }, [])
-  return { ref, entry }
+
+    const { current: currentObserver } = observer
+
+    if (node) currentObserver.observe(node)
+
+    return () => currentObserver.disconnect()
+  }, [node, rootMargin, threshold])
+
+  return { setNode, entry }
 }
 
 
