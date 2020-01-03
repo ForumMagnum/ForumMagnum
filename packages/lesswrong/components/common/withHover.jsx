4cbd4fe42467c93c94e05f035f931c306e670d4d
Ruben Bloom <rmb042@gmail.com>
2020-01-02 16:16:36 -0800

    Add useCallback to withHover

--- packages/lesswrong/components/common/withHover.jsx
+++ packages/lesswrong/components/common/withHover.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useRef } from 'react';
+import React, {useState, useRef, useCallback } from 'react';
 import { useTracking } from "../../lib/analyticsEvents";
 import { isMobile } from '../../lib/modules/utils/isMobile.js'
 
@@ -11,30 +11,29 @@ export const withHover = (WrappedComponent) => {
         const [hoverTrackingProps, setHoverTrackingProps] = useState()
 
 
-        const { captureEvent } = useTracking({eventType:"hoverEventTriggered"})
-
-        const captureHoverEvent = () => {
+        const captureHoverEvent = useCallback(() => {
             !isMobile() && captureEvent("hoverEventTriggered",
                 {...{timeToCapture: new Date() - mouseOverStart.current}, ...hoverTrackingProps})
             clearTimeout(delayTimer.current)
-        }
+        }, [captureEvent])
 
-        const handleMouseOver = (event) => {
+        const handleMouseOver = useCallback((event) => {
             setHover(true)
             setAnchorEl(event.currentTarget)
             mouseOverStart.current = new Date()
             clearTimeout(delayTimer.current)
             delayTimer.current = setTimeout(captureHoverEvent,500)
-        }
+        }, [captureHoverEvent])
 
-        const handleMouseLeave = () => {
+        const handleMouseLeave = useCallback(() => {
             setHover(false)
             setAnchorEl(null)
             clearTimeout(delayTimer.current)
             const hoverDuration = new Date() - mouseOverStart.current
-            if ( hoverDuration > 2000 ) captureEvent("hoverEventTriggered", {hoverEventType: "longHoverEvent", hoverDuration})
+            if ( hoverDuration > 2000 ) captureEvent("hoverEventTriggered",
+              {hoverEventType: "longHoverEvent", hoverDuration})
             mouseOverStart.current = undefined
-        }
+        },[captureEvent])
 
 
         const allProps = { hover, anchorEl, stopHover: handleMouseLeave, hoverTrackingProps, setHoverTrackingProps, ...props }
