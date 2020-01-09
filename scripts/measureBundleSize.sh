8392e49ad29091d719a86ec2bfd50edceb93d4e6
James Babcock <jimrandomh@gmail.com>
2020-01-08 16:46:02 -0800

    Set user-agent when measuring bundle size, to get less backcompat stuff

--- scripts/measureBundleSize.sh
+++ scripts/measureBundleSize.sh
@@ -6,9 +6,10 @@
 
 mkdir -p tmp
 local_frontpage_url=http://localhost:3000
+user_agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36"
 
 # Load the front page
-if ! curl --silent --show-error -o tmp/localFrontPage $local_frontpage_url; then
+if ! curl --silent --show-error --user-agent "$user_agent" -o tmp/localFrontPage $local_frontpage_url; then
   echo "Failed to download $local_frontpage_url; is a server running?"
   exit 1
 fi
@@ -24,7 +25,7 @@ rm -rf tmp/bundleSizeDownloads
 mkdir -p tmp/bundleSizeDownloads
 for scripturl in $(cat tmp/scriptFilenames); do
   scripturl_noslashes_nohash=$(echo "$scripturl" |sed -e 's/\//_/g' |sed -e 's/?hash=[a-z0-9]*//')
-  curl --silent --show-error -o "tmp/bundleSizeDownloads/${scripturl_noslashes_nohash}" "http://localhost:3000$scripturl"
+  curl --silent --show-error --user-agent "$user_agent" -o "tmp/bundleSizeDownloads/${scripturl_noslashes_nohash}" "http://localhost:3000$scripturl"
 done
 
 # Show total sizes
