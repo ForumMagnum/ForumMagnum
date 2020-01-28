7c09b5f8c7c78a05fab7a4b97d273d04227318a7
JP Addison <johnpaddison@gmail.com>
2020-01-27 17:00:32 -0800

    access EventVicinity from Components to protect from unimported case

--- packages/lesswrong/components/posts/PostsItem2.tsx
+++ packages/lesswrong/components/posts/PostsItem2.tsx
@@ -415,7 +415,7 @@ const PostsItem2 = ({
 
   const { PostsItemComments, PostsItemKarma, PostsTitle, PostsUserAndCoauthors, LWTooltip, 
     PostsPageActions, PostsItemIcons, PostsItem2MetaInfo, PostsItemTooltipWrapper,
-    BookmarkButton, EventVicinity, PostsItemDate, PostsItemNewCommentsWrapper, AnalyticsTracker } = (Components as ComponentTypes)
+    BookmarkButton, PostsItemDate, PostsItemNewCommentsWrapper, AnalyticsTracker } = (Components as ComponentTypes)
 
   const postLink = Posts.getPageUrl(post, false, sequenceId || chapter?.sequenceId);
 
@@ -496,7 +496,7 @@ const PostsItem2 = ({
                 </PostsItem2MetaInfo>}
 
                 { post.isEvent && <PostsItem2MetaInfo className={classes.event}>
-                  <EventVicinity post={post} />
+                  <Components.EventVicinity post={post} />
                 </PostsItem2MetaInfo>}
 
                 {showPostedAt && !resumeReading && <PostsItemDate post={post} />}
