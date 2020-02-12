7ba42d3b053409db87546672cd9c922fda611ee8
Sam Deere <dev@colophonemes.com>
2020-02-12 19:29:58 +0000

    Add dismissable handbook block to EAHome

--- packages/lesswrong/components/ea-forum/EAHome.jsx
+++ packages/lesswrong/components/ea-forum/EAHome.jsx
@@ -3,6 +3,7 @@ import React, { PureComponent } from 'react'
 import withUser from '../common/withUser'
 import Users from 'meteor/vulcan:users'
 
+
 const shouldShowHandbook = (currentUser, cookieSomething) => {
   // if hasSeenHandbook(currentUser) return false
   return true
@@ -11,7 +12,7 @@ const shouldShowHandbook = (currentUser, cookieSomething) => {
 class EAHome extends PureComponent {
   render () {
     const { currentUser } = this.props
-    const { RecentDiscussionThreadsList, HomeLatestPosts, ConfigurableRecommendationsList, } = Components
+    const { RecentDiscussionThreadsList, HomeLatestPosts, ConfigurableRecommendationsList, EAHomeHandbook } = Components
 
     const shouldRenderSidebar = Users.canDo(currentUser, 'posts.moderate.all')
     const recentDiscussionCommentsPerPost = (currentUser && currentUser.isAdmin) ? 4 : 3;
@@ -21,9 +22,7 @@ class EAHome extends PureComponent {
       <React.Fragment>
         {shouldRenderSidebar && <Components.SunshineSidebar/>}
 
-        {showHandbook && <div>
-          --------------------------------------------------------------------------------- EA Handbook ----------------------------------------------------------------------
-        </div>}
+        {showHandbook && <EAHomeHandbook />}
 
         <HomeLatestPosts />
 
---
+++ packages/lesswrong/components/ea-forum/EAHomeHandbook.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { Components, registerComponent } from 'meteor/vulcan:core'
+import { withStyles, createStyles } from '@material-ui/core/styles'
+import Typography from '@material-ui/core/Typography'
+import { withCookies } from 'react-cookie'
+import { withMessages } from '../common/withMessages';
+
+
+const styles = createStyles(theme => ({
+  root: {
+    minHeight: 300,
+    backgroundImage: `url(https://images.pexels.com/photos/414171/pexels-photo-414171.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500)`,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  dismissRoot: {
+    textAlign: 'right',
+    color: '#999'
+  }
+}))
+
+const COOKIE_NAME = 'hide_home_handbook'
+const END_OF_TIME = new Date('2038-01-18')
+const EAHomeHandbook = ({ classes, cookies, flash }) => {
+  const { SingleColumnSection } = Components
+  const hideHandbook = cookies.get(COOKIE_NAME)
+  if (hideHandbook) return null
+
+  const handleDismiss = () => {
+    cookies.set(COOKIE_NAME, 'true', {
+      expires: END_OF_TIME
+    })
+    flash({
+      messageString: `We won't show this again. If you want to read the Handbook, you can access it from the sidebar menu.`
+    })
+  }
+
+  return <SingleColumnSection>
+    <div className={classes.root}>
+      <Typography variant='h3'>EA Handbook</Typography>
+    </div>
+    <div className={classes.dismissRoot}>
+      <Typography>
+        <a onClick={handleDismiss}>Don't show this</a>
+      </Typography>
+    </div>
+  </SingleColumnSection>
+}
+registerComponent('EAHomeHandbook', EAHomeHandbook, withStyles(styles, { name: 'EAHomeHandbook' }), withCookies, withMessages)
