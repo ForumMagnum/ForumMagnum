5f7cff6176e8ffefb3125527a41c28b904710b75
James Babcock <jimrandomh@gmail.com>
2020-02-12 22:29:17 -0800

    Fix import-all-components unit test

--- packages/lesswrong/testing/components.tests.ts
+++ packages/lesswrong/testing/components.tests.ts
@@ -1,6 +1,6 @@
 import { importAllComponents } from '../lib/vulcan-lib/components';
 
-describe("Components", () => {
+describe("Components", function () {
   this.timeout(20000)
   it("doesn't crash when importing every component file", () => {
     importAllComponents();
