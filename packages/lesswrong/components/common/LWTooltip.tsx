5c189681d91d35fbc9c99643fe39d82e0f8cfd90
Raemon <raemon777@gmail.com>
2020-02-17 19:10:27 -0800

    tooltip maxWidth

--- packages/lesswrong/components/common/LWTooltip.tsx
+++ packages/lesswrong/components/common/LWTooltip.tsx
@@ -6,6 +6,8 @@ const styles = theme => ({
   root: {
     // inline-block makes sure that the popper placement works properly (without flickering). "block" would also work, but there may be situations where we want to wrap an object in a tooltip that shouldn't be a block element.
     display: "inline-block",
+  },
+  tooltip: {
     maxWidth: 300
   }
 })
@@ -35,7 +37,7 @@ const LWTooltip = ({classes, children, title, placement="bottom-start", hover, a
         }
       }}
     >
-      {title}
+      <div className={classes.tooltip}>{title}</div>
     </LWPopper>
     {children}
   </span>
