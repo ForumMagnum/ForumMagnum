169baab6ec5e8271b863a0ffdd6a549b8f06c989
James Babcock <jimrandomh@gmail.com>
2020-03-29 21:38:50 -0700

    Hookify BanUserFromPostItem

--- packages/lesswrong/components/comments/CommentsItem/BanUserFromPostMenuItem.tsx
+++ packages/lesswrong/components/comments/CommentsItem/BanUserFromPostMenuItem.tsx
@@ -1,65 +1,49 @@
 import React, { PureComponent } from 'react';
 import { registerComponent } from '../../../lib/vulcan-lib';
-import { withUpdate } from '../../../lib/crud/withUpdate';
-import { withMessages } from '../../common/withMessages';
+import { useUpdate } from '../../../lib/crud/withUpdate';
+import { useMessages } from '../../common/withMessages';
 import MenuItem from '@material-ui/core/MenuItem';
 import { Posts } from '../../../lib/collections/posts';
 import Users from '../../../lib/collections/users/collection';
-import withUser from '../../common/withUser';
+import { useCurrentUser } from '../../common/withUser';
 import * as _ from 'underscore';
 
-interface ExternalProps {
+const BanUserFromPostMenuItem = ({ comment, post }: {
   comment: CommentsList,
-  post: PostsList,
-}
-interface BanUserFromPostMenuItemProps extends ExternalProps, WithMessagesProps, WithUserProps, WithUpdateUserProps {
-}
+  post: PostsDetails,
+}) => {
+  const currentUser = useCurrentUser();
+  const { flash } = useMessages();
+  const {mutate: updatePost} = useUpdate({
+    collection: Posts,
+    fragmentName: 'PostsPage',
+  });
 
-class BanUserFromPostMenuItem extends PureComponent<BanUserFromPostMenuItemProps,{}> {
-
-  constructor(props: BanUserFromPostMenuItemProps) {
-    super(props);
-  }
-
-  handleBanUserFromPost = (event) => {
+  const handleBanUserFromPost = (event) => {
     event.preventDefault();
     if (confirm("Are you sure you want to ban this user from commenting on this post?")) {
-      const commentUserId = this.props.comment.userId
-      let bannedUserIds = _.clone(this.props.post.bannedUserIds) || []
+      const commentUserId = comment.userId
+      let bannedUserIds = _.clone(post.bannedUserIds) || []
       if (!bannedUserIds.includes(commentUserId)) {
         bannedUserIds.push(commentUserId)
       }
-      this.props.updateUser({
-        selector: {_id: this.props.comment.postId},
+      updatePost({
+        selector: {_id: comment.postId},
         data: {bannedUserIds:bannedUserIds}
-      }).then(()=>this.props.flash({messageString: `User ${this.props.comment.user.displayName} is now banned from commenting on ${this.props.post.title}`}))
+      }).then(()=>flash({messageString: `User ${comment.user.displayName} is now banned from commenting on ${post.title}`}))
     }
   }
 
-  render() {
-    const { currentUser, post} = this.props
-    if (Users.canModeratePost(currentUser, post)) {
-        return <MenuItem onClick={ this.handleBanUserFromPost }>
-          Ban user from this post
-        </MenuItem>
-      } else {
-        return null
-      }
+  if (Users.canModeratePost(currentUser, post)) {
+    return <MenuItem onClick={handleBanUserFromPost}>
+      Ban user from this post
+    </MenuItem>
+  } else {
+    return null
   }
 };
 
-const BanUserFromPostMenuItemComponent = registerComponent<ExternalProps>(
-  'BanUserFromPostMenuItem', BanUserFromPostMenuItem, {
-    hocs: [
-      withMessages,
-      withUpdate({
-        collection: Posts,
-        fragmentName: 'PostsPage',
-      }),
-      withUser
-    ]
-  }
-);
+const BanUserFromPostMenuItemComponent = registerComponent('BanUserFromPostMenuItem', BanUserFromPostMenuItem);
 
 declare global {
   interface ComponentTypes {
