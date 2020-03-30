2d68fd5b22aa008be3db7caa26c968d4311e6d04
James Babcock <jimrandomh@gmail.com>
2020-03-29 21:46:40 -0700

    Load larger fragment in CommentActions to resolve type error (fixing a few small actual bugs)

--- packages/lesswrong/components/comments/CommentsItem/CommentActions.tsx
+++ packages/lesswrong/components/comments/CommentsItem/CommentActions.tsx
@@ -2,16 +2,28 @@ import React from 'react';
 import { registerComponent, Components } from '../../../lib/vulcan-lib';
 import Divider from '@material-ui/core/Divider';
 import Users from '../../../lib/collections/users/collection';
+import Posts from '../../../lib/collections/posts/collection';
 import MenuItem from '@material-ui/core/MenuItem';
+import { useSingle } from '../../../lib/crud/withSingle';
 import { subscriptionTypes } from '../../../lib/collections/subscriptions/schema'
 
 const CommentActions = ({currentUser, comment, post, showEdit}: {
   currentUser: UsersCurrent, // Must be logged in
   comment: CommentsList,
-  post: PostsDetails,
+  post: PostsMinimumInfo,
   showEdit: ()=>void,
 }) => {
-  const { EditCommentMenuItem, ReportCommentMenuItem, DeleteCommentMenuItem, RetractCommentMenuItem, BanUserFromPostMenuItem, BanUserFromAllPostsMenuItem, MoveToAlignmentMenuItem, SuggestAlignmentMenuItem, BanUserFromAllPersonalPostsMenuItem, MoveToAnswersMenuItem, SubscribeTo, ToggleIsModeratorComment } = Components
+  const { EditCommentMenuItem, ReportCommentMenuItem, DeleteCommentMenuItem, RetractCommentMenuItem, BanUserFromPostMenuItem, BanUserFromAllPostsMenuItem, MoveToAlignmentMenuItem, SuggestAlignmentMenuItem, BanUserFromAllPersonalPostsMenuItem, MoveToAnswersMenuItem, SubscribeTo, ToggleIsModeratorComment, Loading } = Components
+  
+  const { document: postDetails, loading } = useSingle({
+    documentId: post._id,
+    collection: Posts,
+    fetchPolicy: "cache-first",
+    fragmentName: "PostsDetails",
+  });
+  
+  if (loading)
+    return <Loading/>
   
   return <>
     <EditCommentMenuItem comment={comment} showEdit={showEdit}/>
@@ -39,15 +51,15 @@ const CommentActions = ({currentUser, comment, post, showEdit}: {
       </MenuItem>
     }
     <ReportCommentMenuItem comment={comment}/>
-    <MoveToAlignmentMenuItem comment={comment} post={post}/>
-    <SuggestAlignmentMenuItem comment={comment} post={post}/>
-    { Users.canModeratePost(currentUser, post) && post.user && Users.canModeratePost(post.user, post) && <Divider />}
-    <MoveToAnswersMenuItem comment={comment} post={post}/>
-    <DeleteCommentMenuItem comment={comment} post={post}/>
+    <MoveToAlignmentMenuItem comment={comment} post={postDetails}/>
+    <SuggestAlignmentMenuItem comment={comment} post={postDetails}/>
+    { Users.canModeratePost(currentUser, postDetails) && postDetails.user && Users.canModeratePost(postDetails.user, postDetails) && <Divider />}
+    <MoveToAnswersMenuItem comment={comment} post={postDetails}/>
+    <DeleteCommentMenuItem comment={comment} post={postDetails}/>
     <RetractCommentMenuItem comment={comment}/>
-    <BanUserFromPostMenuItem comment={comment} post={post}/>
-    <BanUserFromAllPostsMenuItem comment={comment} post={post}/>
-    <BanUserFromAllPersonalPostsMenuItem comment={comment} post={post}/>
+    <BanUserFromPostMenuItem comment={comment} post={postDetails}/>
+    <BanUserFromAllPostsMenuItem comment={comment} post={postDetails}/>
+    <BanUserFromAllPersonalPostsMenuItem comment={comment} post={postDetails}/>
     <ToggleIsModeratorComment comment={comment}/>
   </>
 }
