a2568e5e7ef38bf2a882527011d7372fb7bb0a2a
Raymond Arnold <raymondarnold@raymonds-mbp.lan>
2020-04-29 10:44:53 -0700

    reword politics

--- packages/lesswrong/components/tagging/TagFilterSettings.tsx
+++ packages/lesswrong/components/tagging/TagFilterSettings.tsx
@@ -111,7 +111,7 @@ const TagFilterSettings = ({ filterSettings, setFilterSettings, classes }: {
         <p><b>Personal Blogposts</b> are posts that don't fit LessWrong's Frontpage Guidelines. They get less visibility by default. The frontpage guidelines are:</p>
         <ul>
           <li><em>Timelessness</em>. Will people still care about this in 5 years?</li>
-          <li><em>Avoid politics</em>. It's important to talk about sometimes, but we try to avoid it on LessWrong.</li>
+          <li><em>Avoid political topics</em>. They're important to discuss sometimes, but we try to avoid it on LessWrong.</li>
           <li><em>General Appeal</em>. Is this a niche post that only a small fraction of users will care about?</li>
         </ul>
       </div>}
