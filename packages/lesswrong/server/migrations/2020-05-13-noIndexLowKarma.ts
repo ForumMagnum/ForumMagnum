fb8fcc85adb82e7fd2a3bc3827dcf9e50e0a2e20
JP Addison <johnpaddison@gmail.com>
2020-05-19 12:09:47 -0700

    launch date by forum

--- packages/lesswrong/server/migrations/2020-05-13-noIndexLowKarma.ts
+++ packages/lesswrong/server/migrations/2020-05-13-noIndexLowKarma.ts
@@ -1,11 +1,19 @@
 import { registerMigration, forEachDocumentBatchInCollection } from './migrationUtils';
 import Posts from '../../lib/collections/posts/collection';
+import { getSetting } from '../vulcan-lib';
 import moment from 'moment'
+
+const launchDateByForum = {
+  LessWrong: 'TODO', // lw-look-here
+  AlignmentForum: "Don't",
+  EAForum: '2014-09-10'
+}
+const launchDate = launchDateByForum[getSetting('forumType') as string]
 
 export function makeLowKarmaSelector (karmaThreshold: number): MongoSelector<DbPost> {
   return {
     postedAt: {
-      $gt: moment.utc('2014-09-10').toDate(),
+      $gt: moment.utc(launchDate).toDate(),
       $lt: moment.utc().subtract(1, 'year').toDate()
     },
     isEvent: {$ne: true},
