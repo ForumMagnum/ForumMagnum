319d4f0983b7d18d4dce98dc3073eb32253b189b
JP Addison <johnpaddison@gmail.com>
2020-05-27 12:57:56 -0700

    auto-generated types need updating for some reason

--- packages/lesswrong/lib/generated/fragmentTypes.d.ts
+++ packages/lesswrong/lib/generated/fragmentTypes.d.ts
@@ -284,6 +284,7 @@ interface PostsPage extends PostsDetails { // fragment on Posts
 }
 
 interface PostsEdit extends PostsPage { // fragment on Posts
+  readonly coauthorUserIds: Array<string>,
   readonly moderationGuidelines: RevisionEdit,
   readonly contents: RevisionEdit,
   readonly customHighlight: RevisionEdit,
@@ -525,6 +526,7 @@ interface PostRelationsDefaultFragment { // fragment on PostRelations
 }
 
 interface TagRelsDefaultFragment { // fragment on TagRels
+  readonly createdAt: Date,
   readonly tagId: string,
   readonly postId: string,
   readonly deleted: boolean,
@@ -1387,6 +1389,7 @@ interface WithVoteTagRel_currentUserVotes { // fragment on Votes
 }
 
 interface TagsDefaultFragment { // fragment on Tags
+  readonly createdAt: Date,
   readonly name: string,
   readonly slug: string,
   readonly core: boolean,
