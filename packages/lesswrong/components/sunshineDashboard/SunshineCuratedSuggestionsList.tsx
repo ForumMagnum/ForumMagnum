2b5c71c9f19c711405622fb16116d99b244d5c8e
Raymond Arnold <raymondarnold@raymonds-mbp.lan>
2020-05-28 00:01:39 -0700

    Sunshine Curated appears after 3 days

--- packages/lesswrong/components/sunshineDashboard/SunshineCuratedSuggestionsList.tsx
+++ packages/lesswrong/components/sunshineDashboard/SunshineCuratedSuggestionsList.tsx
@@ -21,9 +21,9 @@ const SunshineCuratedSuggestionsList = ({ terms, belowFold }:{
     fragmentName: 'PostsList',
   });
   const curatedDate = new Date(curatedResults && curatedResults[0]?.curatedDate)
-  const twoDaysAgo = new Date(new Date().getTime()-(2*24*60*60*1000));
+  const threeDaysAgo = new Date(new Date().getTime()-(3*24*60*60*1000));
 
-  if (!belowFold && (curatedDate > twoDaysAgo)) return null
+  if (!belowFold && (curatedDate > threeDaysAgo)) return null
 
   if (loading) return <Components.Loading/>;
   
