d0c61c4b7db61ce6b1905e3035207cef2a52b580
JP Addison <johnpaddison@gmail.com>
2020-06-02 16:26:49 -0700

    eaify travis and deploy script

--- deploy.sh
+++ deploy.sh
@@ -9,15 +9,15 @@ yarn global add @sentry/cli;
 # Variables & Arguments
 settings_path="$1";
 deploy_config_path="$2";
-sentry_version=$(sentry-cli releases propose-version);
+# sentry_version=$(sentry-cli releases propose-version);
 
 # Environment variables and secrets provided via travis-ci
 git clone https://Discordius:$GITHUB_TOKEN@github.com/LessWrong2/LessWrong-Credentials.git
 
-# Set up sentry configurations
-sentry-cli releases new -p lesswrong $sentry_version;
-sentry-cli releases set-commits --auto $sentry_version;
-json -I -f $settings_path -e "this.public.sentry.release=\"$sentry_version\"";
+# # Set up sentry configurations
+# sentry-cli releases new -p lesswrong $sentry_version;
+# sentry-cli releases set-commits --auto $sentry_version;
+# json -I -f $settings_path -e "this.public.sentry.release=\"$sentry_version\"";
 
 # Deploy
 cp $deploy_config_path ./config.secret; # Have to copy this because mup deploy resolves in directory relative to config file
