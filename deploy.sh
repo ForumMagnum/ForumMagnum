de68c2e869e232e12a285eb3fe48ea6fdc53d6a8
JP Addison <johnpaddison@gmail.com>
2020-06-16 14:42:40 -0700

    clean up based on PR comments

--- deploy.sh
+++ deploy.sh
@@ -9,19 +9,11 @@ yarn global add @sentry/cli;
 # Variables & Arguments
 settings_path="$1";
 deploy_config_path="$2";
-echo 'deploy_config_path'
-# sentry_version=$(sentry-cli releases propose-version);
 
 git clone https://github.com/elasticdog/transcrypt.git
 # Environment variables and secrets provided via travis-ci
 ./transcrypt/transcrypt -c aes-256-cbc -p $TRANSCRYPT_SECRET -y
 
-# # Set up sentry configurations
-# sentry-cli releases new -p lesswrong $sentry_version;
-# sentry-cli releases set-commits --auto $sentry_version;
-# json -I -f $settings_path -e "this.public.sentry.release=\"$sentry_version\"";
-
 # Deploy
 cp $deploy_config_path ./config.secret; # Have to copy this because mup deploy resolves in directory relative to config file
-
 bash ./travis_wait "mup deploy --config config.secret --settings $settings_path";
