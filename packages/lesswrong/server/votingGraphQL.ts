28a0f80c107a32b02be5f06770d074657d4fcbb9
James Babcock <jimrandomh@gmail.com>
2020-06-22 19:09:10 -0700

    Improve comment on vote mutation

--- packages/lesswrong/server/votingGraphQL.ts
+++ packages/lesswrong/server/votingGraphQL.ts
@@ -23,14 +23,16 @@ addGraphQLMutation('vote(documentId: String, voteType: String, collectionName: S
 
 const voteResolver = {
   Mutation: {
-    // Voting mutation. Voting toggles, like the vote button UI: if the given
-    // vote type is different from the user's previous vote, change to the new
-    // vote type, but if it's the same as the user's previous vote, cancel that
-    // vote.
+    // vote: Voting mutation. Voting toggles, like the vote button UI: if the
+    // given vote type is different from the user's previous vote, change to the
+    // new vote type, but if it's the same as the user's previous vote, cancel
+    // that vote.
     //
     // The voteId argument is ignored (we don't actually want to give clients
     // control over database IDs), but still exists as an option for legacy
     // compatibility.
+    //
+    // Returns the document that was voted upon, with its score updated.
     async vote(root, {documentId, voteType, collectionName, voteId}, context: ResolverContext) {
       const { currentUser } = context;
       const collection = context[collectionName];
