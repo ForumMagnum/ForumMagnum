4c4e4b92ad53294e5f8225599c81368694a13d39
Raymond Arnold <raymondarnold@Raymonds-MacBook-Pro.local>
2020-07-04 11:30:35 -0700

    Add Filter Button is more clickable

--- packages/lesswrong/components/tagging/AddTagButton.tsx
+++ packages/lesswrong/components/tagging/AddTagButton.tsx
@@ -11,20 +11,23 @@ const styles = theme => ({
     ...theme.typography.commentStyle,
     color: theme.palette.grey[600],
     display: "inline-block",
-    textAlign: "center",
-    paddingLeft: 4
+    textAlign: "center"
   },
+  defaultButton: {
+    paddingLeft: 4
+  }
 });
 
-const AddTagButton = ({onTagSelected, classes, smallVariant}: {
+const AddTagButton = ({onTagSelected, classes, children}: {
   onTagSelected: (props: {tagId: string, tagName: string})=>void,
   classes: ClassesType,
-  smallVariant?: boolean
+  children: any
 }) => {
   const [isOpen, setIsOpen] = useState(false);
   const [anchorEl, setAnchorEl] = useState<HTMLElement|null>(null);
   const currentUser = useCurrentUser();
   const { captureEvent } = useTracking()
+  const { LWPopper, AddTag } = Components
 
   if (!userCanUseTags(currentUser)) {
     return null;
@@ -36,11 +39,11 @@ const AddTagButton = ({onTagSelected, classes, smallVariant}: {
       setIsOpen(true);
       captureEvent("addTagClicked")
     }}
-    className={smallVariant ? classes.small : classes.addTagButton}
+    className={classes.addTagButton}
   >
-    {smallVariant ? "+" : "+ Add Tag"}
+    {children ? children : <span className={classes.defaultButton}>+ Add Tag</span>}
 
-    <Components.LWPopper
+    <LWPopper
       open={isOpen}
       anchorEl={anchorEl}
       placement="bottom-start"
@@ -54,7 +57,7 @@ const AddTagButton = ({onTagSelected, classes, smallVariant}: {
         onClickAway={() => setIsOpen(false)}
       >
         <Paper>
-          <Components.AddTag
+          <AddTag
             onTagSelected={({tagId, tagName}: {tagId: string, tagName: string}) => {
               setAnchorEl(null);
               setIsOpen(false);
@@ -63,7 +66,7 @@ const AddTagButton = ({onTagSelected, classes, smallVariant}: {
           />
         </Paper>
       </ClickAwayListener>
-    </Components.LWPopper>
+    </LWPopper>
   </a>;
 }
 
