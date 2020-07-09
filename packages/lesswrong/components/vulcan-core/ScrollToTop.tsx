e131202a3e4d23d4d787ecab96fdb8a6b5c5c313
Discordius <panisnecis@gmail.com>
2020-07-09 12:58:07 -0700

    Fix scrollToTop

--- packages/lesswrong/components/vulcan-core/ScrollToTop.tsx
+++ packages/lesswrong/components/vulcan-core/ScrollToTop.tsx
@@ -1,23 +1,19 @@
-import React, {Component} from 'react';
-import {withLocation} from '../../lib/routeUtil';
+import React, {useEffect} from 'react';
 import {registerComponent} from '../../lib/vulcan-lib';
+import { useSubscribedLocation } from '../../lib/routeUtil';
 
 // Scroll restoration based on https://reacttraining.com/react-router/web/guides/scroll-restoration.
-export default class ScrollToTop extends Component<any> {
-  componentDidUpdate(prevProps) {
-    if (this.props.location.pathname !== prevProps.location.pathname) {
-      window.scrollTo(0, 0);
-    }
-  }
+export default function ScrollToTop() {
+  const { pathname } = useSubscribedLocation();
 
-  render() {
-    return null;
-  }
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
 }
 
-const ScrollToTopComponent = registerComponent('ScrollToTop', ScrollToTop, {
-  hocs: [withLocation]
-});
+const ScrollToTopComponent = registerComponent('ScrollToTop', ScrollToTop);
 
 declare global {
   interface ComponentTypes {
