643bb4e8e4d11a6de77faa220e10381cfcb88eb3
JP Addison <johnpaddison@gmail.com>
2020-07-16 18:52:01 +0100

    fix SiteLogo

--- packages/lesswrong/components/ea-forum/SiteLogo.tsx
+++ packages/lesswrong/components/ea-forum/SiteLogo.tsx
@@ -5,7 +5,7 @@
  */
 import React from 'react';
 import { registerComponent, Utils } from '../../lib/vulcan-lib';
-import { forumTitleSetting, PublicInstanceSetting } from '../../lib/instanceSettings';
+import { forumTitleSetting } from '../../lib/instanceSettings';
 
 const styles = theme => ({
   root: {
@@ -13,14 +13,18 @@ const styles = theme => ({
   }
 })
 
-export const logoUrlSetting = new PublicInstanceSetting<string | null>('logoUrl', null, "warning")
-
-const SiteLogo = ({classes}) => logoUrlSetting.get() ? <span/> : <img
-  className={classes.root}
-  src={Utils.getLogoUrl()}
-  title={forumTitleSetting.get()}
-  alt={`${forumTitleSetting.get()} Logo`}
-/>
+const SiteLogo = ({classes}) => {
+  console.log('SiteLog()')
+  console.log(' logourl', Utils.getLogoUrl())
+  return Utils.getLogoUrl()
+    ? <img
+      className={classes.root}
+      src={Utils.getLogoUrl()}
+      title={forumTitleSetting.get()}
+      alt={`${forumTitleSetting.get()} Logo`}
+    />
+    : <span/>
+}
 
 SiteLogo.displayName = "SiteLogo";
 const SiteLogoComponent = registerComponent(
