d04d6300a6db0013c2237d954b5c3d8a11998da5
Discordius <panisnecis@gmail.com>
2020-07-16 15:18:44 -0700

    Fix duplicate try

--- packages/lesswrong/server/rss-integration/cron.ts
+++ packages/lesswrong/server/rss-integration/cron.ts
@@ -6,77 +6,75 @@ import Users from '../../lib/collections/users/collection';
 import { asyncForeachSequential } from '../../lib/utils/asyncUtils';
 
 const runRSSImport = async () => {
-  try {
-    const feedparser = require('feedparser-promised');
-    const feeds = RSSFeeds.find({status: {$ne: 'inactive'}}).fetch()
-    await asyncForeachSequential(feeds, async feed => {
-      try {
-        // create array of all posts in current rawFeed object
-        let previousPosts = feed.rawFeed || [];
+  const feedparser = require('feedparser-promised');
+  const feeds = RSSFeeds.find({status: {$ne: 'inactive'}}).fetch()
+  await asyncForeachSequential(feeds, async feed => {
+    try {
+      // create array of all posts in current rawFeed object
+      let previousPosts = feed.rawFeed || [];
 
-        // check the feed for new posts
-        const url = feed.url;
-        const currentPosts = await feedparser.parse(url);
-        
-        let newPosts: Array<any> = currentPosts.filter(function (post) {
-          return !previousPosts.some(prevPost => {
-            return post.guid === prevPost.guid
-          })
+      // check the feed for new posts
+      const url = feed.url;
+      const currentPosts = await feedparser.parse(url);
+      
+      let newPosts: Array<any> = currentPosts.filter(function (post) {
+        return !previousPosts.some(prevPost => {
+          return post.guid === prevPost.guid
         })
+      })
 
-        // update feed object with new feed data (mutation)
-        var set: any = {};
-        set.rawFeed = currentPosts;
+      // update feed object with new feed data (mutation)
+      var set: any = {};
+      set.rawFeed = currentPosts;
 
-        await editMutation({
-          collection: RSSFeeds,
-          documentId: feed._id,
-          set: set,
+      await editMutation({
+        collection: RSSFeeds,
+        documentId: feed._id,
+        set: set,
+        validate: false,
+      })
+
+      await asyncForeachSequential(newPosts, async newPost => {
+        var body;
+
+        if (newPost['content:encoded'] && newPost.displayFullContent) {
+          body = newPost['content:encoded'];
+        } else if (newPost.description) {
+          body = newPost.description;
+        } else if (newPost.summary) {
+          body = newPost.summary;
+        } else {
+          body = "";
+        }
+
+        var post = {
+          title: newPost.title,
+          userId: feed.userId,
+          canonicalSource: feed.setCanonicalUrl ? newPost.link : undefined,
+          contents: {
+            originalContents: {
+              type: "html",
+              data: body
+            }
+          },
+          feedId: feed._id,
+          feedLink: newPost.link
+        };
+
+        let lwUser = Users.findOne({_id: feed.userId});
+
+        await newMutation({
+          collection: Posts,
+          document: post,
+          currentUser: lwUser,
           validate: false,
         })
-
-        await asyncForeachSequential(newPosts, async newPost => {
-          var body;
-
-          if (newPost['content:encoded'] && newPost.displayFullContent) {
-            body = newPost['content:encoded'];
-          } else if (newPost.description) {
-            body = newPost.description;
-          } else if (newPost.summary) {
-            body = newPost.summary;
-          } else {
-            body = "";
-          }
-
-          var post = {
-            title: newPost.title,
-            userId: feed.userId,
-            canonicalSource: feed.setCanonicalUrl ? newPost.link : undefined,
-            contents: {
-              originalContents: {
-                type: "html",
-                data: body
-              }
-            },
-            feedId: feed._id,
-            feedLink: newPost.link
-          };
-
-          let lwUser = Users.findOne({_id: feed.userId});
-
-          await newMutation({
-            collection: Posts,
-            document: post,
-            currentUser: lwUser,
-            validate: false,
-          })
-        })
-      } catch(error) {
-        //eslint-disable-next-line no-console
-        console.error('RSS error: ', error, feed);
-      }
-    })
-  } 
+      })
+    } catch(error) {
+      //eslint-disable-next-line no-console
+      console.error('RSS error: ', error, feed);
+    }
+  })
 }
 
 
