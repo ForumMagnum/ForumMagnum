860aaf92c9169301156a7f595c342e93ae8c1d18
JP Addison <johnpaddison@gmail.com>
2020-07-28 12:16:57 +0100

    hasEvents should be instance setting for reasonable testing

--- packages/lesswrong/components/users/UsersProfile.tsx
+++ packages/lesswrong/components/users/UsersProfile.tsx
@@ -13,8 +13,7 @@ import withUser from '../common/withUser';
 import Tooltip from '@material-ui/core/Tooltip';
 import { postBodyStyles } from '../../themes/stylePiping'
 import {AnalyticsContext} from "../../lib/analyticsEvents";
-import { forumTypeSetting } from '../../lib/instanceSettings';
-import { hasEventsSetting } from '../../lib/publicSettings';
+import { forumTypeSetting, hasEventsSetting } from '../../lib/instanceSettings';
 import Typography from '@material-ui/core/Typography';
 import { separatorBulletStyles } from '../common/SectionFooter';
 
