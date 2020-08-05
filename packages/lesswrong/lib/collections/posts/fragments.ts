2b2c46202ce0df067d27de0d1b80ffa9141e2a94
Raymond Arnold <raymondarnold@raymonds-mbp.lan>
2020-08-05 15:53:12 -0700

    Refactor post fragments

--- packages/lesswrong/lib/collections/posts/fragments.ts
+++ packages/lesswrong/lib/collections/posts/fragments.ts
@@ -103,13 +103,6 @@ registerFragment(`
       _id
       name
     }
-    bestAnswer {
-      ...CommentsList
-    }
-
-    lastPromotedComment {
-      ...CommentsList
-    }
   }
 `);
 
@@ -146,26 +139,39 @@ registerFragment(`
 `);
 
 registerFragment(`
-  fragment PostsList on Post {
+  fragment PostsListBase on Post {
     ...PostsBase
     ...PostsAuthors
-    contents {
-      htmlHighlight
-      wordCount
-      version
-    }
     moderationGuidelines {
       html
     }
     customHighlight {
       html
     }
-
+    lastPromotedComment {
+      ...CommentsList
+    }
+    bestAnswer {
+      ...CommentsList
+    }
     tags {
       ...TagPreviewFragment
     }
   }
 `);
+
+registerFragment(`
+  fragment PostsList on Post {
+    ...PostsListBase
+    contents {
+      htmlHighlight
+      wordCount
+      version
+    }
+  }
+`);
+
+
 
 registerFragment(`
   fragment PostsListTag on Post {
@@ -178,8 +184,7 @@ registerFragment(`
 
 registerFragment(`
   fragment PostsDetails on Post {
-    ...PostsBase
-    ...PostsAuthors
+    ...PostsListBase
 
     # Sort settings
     commentSortOrder
@@ -279,9 +284,6 @@ registerFragment(`
     ...PostsRevision
     ...PostSequenceNavigation
     
-    tags {
-      ...TagPreviewFragment
-    }
     tableOfContentsRevision(version: $version)
   }
 `)
@@ -332,9 +334,6 @@ registerFragment(`
     version
     contents {
       ...RevisionDisplay
-    }
-    tags {
-      ...TagPreviewFragment
     }
   }
 `)
