4829191910768b9fd61baf2b550337a35d3dd801
Habryka <habryka@Habrykas-iMac.local>
2020-09-11 18:36:12 -0700

    Fix lints

--- packages/lesswrong/components/tagging/TagProgressBar.tsx
+++ packages/lesswrong/components/tagging/TagProgressBar.tsx
@@ -68,13 +68,13 @@ const styles = (theme: ThemeType): JssStyles => ({
 
 });
 
-const TagProgressBar = ({classes}: {
+const TagProgressBar = ({ classes }: {
   classes: ClassesType,
 }) => {
 
   const { LWTooltip, PostsItem2MetaInfo, SeparatorBullet } = Components;
   const currentUser = useCurrentUser();
-  const {mutate: updateUser} = useUpdate({
+  const { mutate: updateUser } = useUpdate({
     collection: Users,
     fragmentName: 'UsersCurrent',
   });
@@ -108,7 +108,7 @@ const TagProgressBar = ({classes}: {
   const hideClickHandler = async () => {
     if (currentUser) {
       await updateUser({
-        selector: { _id: currentUser._id},
+        selector: { _id: currentUser._id },
         data: {
           hideTaggingProgressBar: true
         },
@@ -117,7 +117,7 @@ const TagProgressBar = ({classes}: {
         messageString: "Hid tagging progress bar from the frontpage",
         type: "success",
         action: () => void updateUser({
-          selector: { _id: currentUser._id},
+          selector: { _id: currentUser._id },
           data: {
             hideTaggingProgressBar: false
           },
@@ -131,58 +131,58 @@ const TagProgressBar = ({classes}: {
     }
   }
 
-  const allPostsTooltip =  processedTagsTotal < allTagsToProcessTotal ? 
-    `${allTagsToProcessTotal - processedTagsTotal} pages out of ${allTagsToProcessTotal} from the LW 1.0 Wiki still need processing` : 
+  const allPostsTooltip = processedTagsTotal < allTagsToProcessTotal ?
+    `${allTagsToProcessTotal - processedTagsTotal} pages out of ${allTagsToProcessTotal} from the LW 1.0 Wiki still need processing` :
     `All tags and wiki pages from the LW Wiki import have been processed!`
 
   return <div className={classes.root}>
-      <div className={classes.inner}>
-        <div className={classes.text}>
-          <Link className={classes.title} to={"/posts/ELN6FpRLoeLJPgx8z/importing-the-old-lw-wiki-help-wanted"}>
-            LW 1.0 Wiki Import Progress
+    <div className={classes.inner}>
+      <div className={classes.text}>
+        <Link className={classes.title} to={"/posts/ELN6FpRLoeLJPgx8z/importing-the-old-lw-wiki-help-wanted"}>
+          LW 1.0 Wiki Import Progress
           </Link>
-          <PostsItem2MetaInfo>	
-            <Link className={classes.allTagsBarColor} to={"/posts/ELN6FpRLoeLJPgx8z/importing-the-old-lw-wiki-help-wanted"}>	
-              How to Help
-            </Link>	
-            <SeparatorBullet/>
-            <a className={classes.allTagsBarColor} href="https://docs.google.com/spreadsheets/d/1uz_zORpS_FsTj82SRp_5CxL0o4-CIj4ZK_jLrBjjqJ8/edit#gid=2115128922">	
-              Process Pages
-            </a>	
-          </PostsItem2MetaInfo>
-          <LWTooltip title={<div>
-            <div>View all completely untagged posts, sorted by karma</div>
-            <div><em>(Click through to read posts, and then tag them)</em></div>
-          </div>}>
-          </LWTooltip>
-        </div>
-        { 
-          <LWTooltip className={classes.tooltip} title={allPostsTooltip}>
-            <LinearProgress 
-              classes={{root: classes.barRoot}} 
-              variant="determinate" 
-              value={(processedTagsTotal/allTagsToProcessTotal) * 100} 
-            />
-          </LWTooltip>
-        }
-        <div className={classes.secondaryInfo}>
-          <div className={classes.helpText}>
-            <span className={classes.allTagsBarColor}>{allTagsToProcessTotal - processedTagsTotal} pages from the LW 1.0 Wiki still need processing.{" "} </span>
-          </div>
-          <LWTooltip title={"Hide this progress bar from the frontpage"}>
-            <a 
-              className={classes.hideButton}
-              onClick={hideClickHandler}
-            > 
-              Hide 
+        <PostsItem2MetaInfo>
+          <Link className={classes.allTagsBarColor} to={"/posts/ELN6FpRLoeLJPgx8z/importing-the-old-lw-wiki-help-wanted"}>
+            How to Help
+            </Link>
+          <SeparatorBullet />
+          <a className={classes.allTagsBarColor} href="https://docs.google.com/spreadsheets/d/1uz_zORpS_FsTj82SRp_5CxL0o4-CIj4ZK_jLrBjjqJ8/edit#gid=2115128922">
+            Process Pages
             </a>
-          </LWTooltip>
-        </div>
+        </PostsItem2MetaInfo>
+        <LWTooltip title={<div>
+          <div>View all completely untagged posts, sorted by karma</div>
+          <div><em>(Click through to read posts, and then tag them)</em></div>
+        </div>}>
+        </LWTooltip>
       </div>
+      {
+        <LWTooltip className={classes.tooltip} title={allPostsTooltip}>
+          <LinearProgress
+            classes={{ root: classes.barRoot }}
+            variant="determinate"
+            value={(processedTagsTotal / allTagsToProcessTotal) * 100}
+          />
+        </LWTooltip>
+      }
+      <div className={classes.secondaryInfo}>
+        <div className={classes.helpText}>
+          <span className={classes.allTagsBarColor}>{allTagsToProcessTotal - processedTagsTotal} pages from the LW 1.0 Wiki still need processing.{" "} </span>
+        </div>
+        <LWTooltip title={"Hide this progress bar from the frontpage"}>
+          <a
+            className={classes.hideButton}
+            onClick={hideClickHandler}
+          >
+            Hide
+            </a>
+        </LWTooltip>
+      </div>
+    </div>
   </div>
 }
 
-const TagProgressBarComponent = registerComponent("TagProgressBar", TagProgressBar, {styles, hocs:[withErrorBoundary]});
+const TagProgressBarComponent = registerComponent("TagProgressBar", TagProgressBar, { styles, hocs: [withErrorBoundary] });
 
 declare global {
   interface ComponentTypes {
