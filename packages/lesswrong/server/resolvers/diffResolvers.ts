66790be8c252327d02b75c89dfe08fc891fb57bc
James Babcock <jimrandomh@gmail.com>
2020-09-30 17:28:15 -0700

    Fixed a bug which caused wiki-import revisions to not trim unchanged sections

--- packages/lesswrong/server/resolvers/diffResolvers.ts
+++ packages/lesswrong/server/resolvers/diffResolvers.ts
@@ -75,20 +75,32 @@ export const diffHtml = (before: string, after: string, trim: boolean): string =
   // Diff the revisions
   const diffHtmlUnsafe = diff(before, after);
   
-  const $ = cheerio.load(diffHtmlUnsafe)
-  if (trim) {
-    $('body').children().each(function(i, elem) {
-      const e = $(elem)
-      if (!e.find('ins').length && !e.find('del').length) {
-        e.remove()
-      }
-    })
-  }
-
+  let trimmed = trim ? trimHtmlDiff(diffHtmlUnsafe) : diffHtmlUnsafe;
+  
   // Sanitize (in case node-htmldiff has any parsing glitches that would
   // otherwise lead to XSS)
-  const diffHtml = sanitize($.html());
-  return diffHtml;
+  return sanitize(trimmed);
+}
+
+// Given an HTML diff (with <ins> and <del> tags), remove sections that don't
+// have changes to make an abridged view.
+export const trimHtmlDiff = (html: string): string => {
+  const $ = cheerio.load(html)
+  
+  // Does HTML contain a <body> tag? If so, look at children of the body tag.
+  // Otherwise look at the root.
+  const bodyTags = $('body');
+  const hasBodyTag = bodyTags.length > 0;
+  const rootElement = hasBodyTag ? bodyTags : $.root()
+  
+  rootElement.children().each(function(i, elem) {
+    const e = $(elem)
+    if (!e.find('ins').length && !e.find('del').length) {
+      e.remove()
+    }
+  })
+  
+  return $.html();
 }
 
 addGraphQLQuery('RevisionsDiff(collectionName: String!, fieldName: String!, id: String, beforeRev: String, afterRev: String!, trim: Boolean): String');
