2bd3f1d59a62cadbc3fd71e5594a5e84d19eeefa
Ruben Bloom <rmb042@gmail.com>
2020-10-16 15:04:17 -0700

    Reworked onto Master

--- packages/lesswrong/components/localGroups/WalledGardenPortal.tsx
+++ packages/lesswrong/components/localGroups/WalledGardenPortal.tsx
@@ -1,20 +1,23 @@
-import { Typography } from '@material-ui/core';
 import React, { useState } from 'react';
 import { Components, registerComponent } from '../../lib/vulcan-lib';
-import { postBodyStyles } from '../../themes/stylePiping';
 import { useCurrentUser } from '../common/withUser';
 import { useLocation } from "../../lib/routeUtil";
+import { commentBodyStyles } from '../../themes/stylePiping'
 
 const styles = (theme) => ({
-  body: {
-    ...theme.typography.body1,
-    ...postBodyStyles(theme)
+  welcomeText: {
+    ...commentBodyStyles(theme)
+  },
+  iframePositioning: {
+    position: "absolute",
+    width: "100%",
+    top: 0,
+    zIndex: theme.zIndexes.gatherTownIframe,
   },
   iframeStyling: {
     width: "100%",
-    height: 800,
+    height: "100vh",
     border: "none",
-    // maxWidth: "100vw"
   }
 })
 
@@ -36,46 +39,62 @@ function validateInviteCode(code: string) {
   return (code.length > 3)
 }
 
+
 const WalledGardenPortal = ({classes}:{classes:ClassesType}) => {
+  const { SingleColumnSection } = Components
   const { query, params: { slug } } = useLocation();
   const { inviteCode: inviteCodeQuery } = query;
   const currentUser = useCurrentUser();
 
   const isSunday =  (new Date()).getDay() === 0;
-  const authorizedToEnter = (currentUser && currentUser.walledGardenInvite) || (inviteCodeQuery && validateInviteCode(inviteCodeQuery)) || isSunday ;
+  const hasInvite = currentUser?.walledGardenInvite;
+  console.log({hasInvite, isSunday, inviteCodeQuery});
 
   const [onboarded, setOnboarded] = useState(false);
-  const hasInvite = currentUser?.walledGardenInvite;
 
-  console.log({isSunday, inviteCodeQuery, authorizedToEnter, hasInvite});
-
-  if (!authorizedToEnter) {
-    return <div>The Walled Garden is a private virtual space managed by the LessWrong team. It is closed right now.
-      Please return on Sunday when it is open to everyone. If you have a non-Sunday invite, you may need to log
-      in.</div>
-  } else {
+  const innerPortal = (onboarded:boolean)  => {
     return <div>
-      {onboarded
-        ? (<iframe className={classes.iframeStyling} src="https://gather.town/app/aPVfK3G76UukgiHx/lesswrong-campus"></iframe>)
-        : (<div>
-            <p>Welcome! The Garden is a locus for truthseeking.&nbsp;<br></br>
-              Get curious, go wherever the evidence takes you, be precise with your words, conduct experiments,
-              make bets, plan for your own fallibility, study many sciences and absorb their powers as your own.
-              Blah, blah, blah, this text is bad. Make better text!!<br></br>And, above, all in every motion,
-              figure out what is really actually true.</p>
-            < ul>
-              <li>Headphones</li>
-              <li>Interactions are voluntary</li>
-              <li>Restart to fix problems</li>
-              <li>Respawn</li>
-            </ul>
-            <a onClick={() => setOnboarded(true)}>
-              <b>Enter the Garden</b>
-            </a>
-        </div>)
+      { onboarded
+        ? <div className={classes.iframePositioning}>
+            <iframe className={classes.iframeStyling} src="https://gather.town/app/aPVfK3G76UukgiHx/lesswrong-campus"></iframe>
+        </div>
+        : <SingleColumnSection className={classes.welcomeText}>
+          <p>Welcome to the Walled Garden, a curated space for truthseekers!&nbsp;</p>
+          <p>Here you can socialize, co-work, play games, and attend events. The Garden is open to everyone on Sundays and
+            for public events. Otherwise, it is open by invite only.</p>
+          <ul>
+            <li>Please wear headphones, preferably with a microphone!</li>
+            <li>Restart your browser to solve most technical problems.</li>
+            <li>Respawn (<i>gear icon</i> &gt; <i>respawn</i>) if you get lost or stuck.</li>
+            <li>All interactions are voluntary. You are not compelled to talk to anyone.</li>
+            <li>Please report any issues, both technical and social, to the LessWrong team via Intercom (bottom right) or
+              email (team@lesswrong.com).
+            </li>
+          </ul>
+          <a onClick={() => setOnboarded(true)}>
+            <b>Enter the Garden</b>
+          </a>
+        </SingleColumnSection>
       }
     </div>
   }
+
+  if (currentUser?.walledGardenInvite) {
+    return innerPortal(onboarded)
+  }
+
+  if (isSunday) {
+    return innerPortal(onboarded)
+  }
+
+  if (validateInviteCode(inviteCodeQuery)) {
+    return innerPortal(onboarded)
+  }
+
+  return <SingleColumnSection className={classes.welcomeText}>
+    <p>The Walled Garden is a private virtual space managed by the LessWrong team.</p>
+    <p>It is closed right now. Please return on Sunday when it is open to everyone. If you have a non-Sunday invite, you may need to log in.</p>
+  </SingleColumnSection>
 }
 
 
