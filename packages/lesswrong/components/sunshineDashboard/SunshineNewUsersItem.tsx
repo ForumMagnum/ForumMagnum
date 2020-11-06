eca444e813b0399b81d1f2f07b2a0bf389c64038
James Babcock <jimrandomh@gmail.com>
2020-11-05 17:49:43 -0800

    Fix a crash in SunshineNewUsersItem from trying to in-place sort an immutable list

--- packages/lesswrong/components/sunshineDashboard/SunshineNewUsersItem.tsx
+++ packages/lesswrong/components/sunshineDashboard/SunshineNewUsersItem.tsx
@@ -159,8 +159,8 @@ const SunshineNewUsersItem = ({ user, classes, updateUser, allowContentPreview=t
     limit: 50
   });
 
-  const commentKarmaPreviews = comments ? comments.sort((c1, c2) => c2.baseScore - c1.baseScore) : []
-  const postKarmaPreviews = posts ? posts.sort((p1, p2) => p2.baseScore - p1.baseScore) : []
+  const commentKarmaPreviews = comments ? _.sortBy(comments, c=>c.baseScore) : []
+  const postKarmaPreviews = posts ? _.sortBy(posts, p=>p.baseScore) : []
 
   const { SunshineListItem, SidebarHoverOver, MetaInfo, SidebarActionMenu, SidebarAction, FormatDate, SunshineNewUserPostsList, SunshineNewUserCommentsList, CommentKarmaWithPreview, PostKarmaWithPreview, LWTooltip, Loading, NewConversationButton } = Components
 
