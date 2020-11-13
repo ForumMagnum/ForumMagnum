26ba1621033f2888577ded52ce998a6c8ba0e904
JP Addison <johnpaddison@gmail.com>
2020-11-13 16:26:56 +0000

    fix log warning from eaTheme

--- packages/lesswrong/themes/eaTheme.ts
+++ packages/lesswrong/themes/eaTheme.ts
@@ -301,11 +301,6 @@ const theme = createLWTheme({
         color: grey[800]
       }
     },
-    CommentPermalink: {
-      root: {
-        marginBottom: 24
-      }
-    },
     MuiSnackbarContent: {
       root: {
         backgroundColor: palette.lwTertiary.main
