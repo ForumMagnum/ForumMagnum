3f37d06f6d4c9ac6b8f6094a8387dbfbbc6dd8c0
James Babcock <jimrandomh@gmail.com>
2020-11-13 12:56:11 -0800

    Fix vote client-side display being broken in threads

--- packages/lesswrong/components/comments/CommentsNode.tsx
+++ packages/lesswrong/components/comments/CommentsNode.tsx
@@ -278,7 +278,7 @@ class CommentsNode extends Component<CommentsNodeProps,CommentsNodeState> {
     return false;
   }
 
-  commentTreesDiffer(oldComments, newComments) {
+  commentTreesDiffer(oldComments: CommentTreeNode<CommentsList>[], newComments: CommentTreeNode<CommentsList>[]) {
     if(!oldComments && newComments) return true;
     if(oldComments && !newComments) return true;
     if(!newComments) return false;
@@ -288,7 +288,7 @@ class CommentsNode extends Component<CommentsNodeProps,CommentsNodeState> {
     for(let i=0; i<oldComments.length; i++) {
       if(oldComments[i].item != newComments[i].item)
         return true;
-      if(this.commentTreesDiffer(oldComments[i].childComments, newComments[i].childComments))
+      if(this.commentTreesDiffer(oldComments[i].children, newComments[i].children))
         return true;
     }
     return false;
