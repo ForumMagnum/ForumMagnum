499cc9f07c261924dac0ed8887b3a14b1509cd55
Habryka <habryka@Habrykas-iMac.local>
2020-11-17 17:56:28 -0800

    More lints

--- packages/lesswrong/components/common/withOnPageScroll.tsx
+++ packages/lesswrong/components/common/withOnPageScroll.tsx
@@ -2,16 +2,11 @@ import React from 'react';
 import { isClient } from '../../lib/executionEnvironment';
 
 export const useOnPageScroll = (onScrollFn: ()=>void) => {
-  console.log("useOnPageScroll", onScrollFn)
-  console.log(onScrollFn)
   React.useEffect(() => {
     if (isClient) {
-      const newFunction = () => setTimeout(onScrollFn, 0)
-      document.addEventListener('scroll', newFunction)
-      
+      document.addEventListener('scroll', onScrollFn)
       return function cleanup() {
-        console.log("Cleaning up useOnPageScroll")
-        document.removeEventListener('scroll', newFunction);
+        document.removeEventListener('scroll', onScrollFn);
       };
     }
   }, [onScrollFn]);
