67f6df5b4330b4aa45b54b2a0ca17ab02a47bad5
James Babcock <jimrandomh@gmail.com>
2020-11-20 16:37:49 -0800

    Fix bug that broke ToC generation when certain empty heading blocks are empty

--- packages/lesswrong/server/tableOfContents.ts
+++ packages/lesswrong/server/tableOfContents.ts
@@ -108,7 +108,7 @@ export function extractTableOfContents(postHTML: string)
 
 function elementToToCText(cheerioTag: CheerioElement) {
   const tagHtml = cheerio(cheerioTag).html();
-  if (!tagHtml) throw Error("Tag does not exist");
+  if (!tagHtml) return null;
   const tagClone = cheerio.load(tagHtml);
   tagClone("style").remove();
   return tagClone.root().text();
