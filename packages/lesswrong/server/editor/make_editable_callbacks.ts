28d9b641a296dce9db9f193cee971452213bea47
Raemon <raemon777@gmail.com>
2020-11-24 18:42:59 -0800

    fix type error

--- packages/lesswrong/server/editor/make_editable_callbacks.ts
+++ packages/lesswrong/server/editor/make_editable_callbacks.ts
@@ -312,6 +312,7 @@ ensureIndex(Revisions, {documentId: 1, version: 1, fieldName: 1, editedAt: 1})
 
 async function buildRevision({ originalContents, currentUser }) {
   const { data, type } = originalContents;
+  console.log("buildRevision", originalContents)
   const html = await dataToHTML(data, type, !currentUser.isAdmin)
   const wordCount = await dataToWordCount(data, type)
   
@@ -411,6 +412,7 @@ export function addEditableCallbacks<T extends DbObject>({collection, options = 
   }
 
   async function editorSerializationEdit (docData, { oldDocument: document, newDocument, currentUser }) {
+    console.log("editorSerializationEdit", docData)
     if (docData[fieldName]?.originalContents) {
       if (!currentUser) { throw Error("Can't create document without current user") }
       
