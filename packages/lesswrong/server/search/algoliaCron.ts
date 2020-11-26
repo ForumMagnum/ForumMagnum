830d11cfe83f25871d9792d1488e71d885cdf075
JP Addison <johnpaddison@gmail.com>
2020-11-26 17:25:50 +0000

    warning comment about the cost of a setting

--- packages/lesswrong/server/search/algoliaCron.ts
+++ packages/lesswrong/server/search/algoliaCron.ts
@@ -2,6 +2,7 @@ import { DatabasePublicSetting } from '../../lib/publicSettings';
 import { addCronJob } from '../cronUtil';
 import { algoliaCleanAll, algoliaExportAll } from '../scripts/algoliaExport';
 
+// NB: This setting will set you back like $500/mo
 const algoliaAutoSyncIndexesSetting = new DatabasePublicSetting<boolean>('algolia.autoSyncIndexes', false)
 
 if (algoliaAutoSyncIndexesSetting.get()) {
