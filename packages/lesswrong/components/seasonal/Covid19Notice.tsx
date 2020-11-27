0c2cfd18f30e8e264cc2a00287a0d1aebf6f92eb
JP Addison <johnpaddison@gmail.com>
2020-11-27 08:38:15 +0000

    covid being widespread is now no longer a yet

--- packages/lesswrong/components/seasonal/Covid19Notice.tsx
+++ packages/lesswrong/components/seasonal/Covid19Notice.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Components, registerComponent } from '../../lib/vulcan-lib';
+import { registerComponent } from '../../lib/vulcan-lib';
 
 const styles = (theme: ThemeType): JssStyles => ({
   root: {
@@ -18,7 +18,7 @@ const Covid19Notice = ({ classes }: {
   return <div className={classes.root}>
     Check the number of COVID-19 cases in your area before hosting or attending in-person events.
     Do not host or attend gatherings in places where COVID-19 is widespread. In places where
-    COVID-19 is not yet widespread, turn away attendees who are coughing or have a fever, and
+    COVID-19 is not widespread, turn away attendees who are coughing or have a fever, and
     encourage attendees to wash their hands frequently.
   </div>
 }
