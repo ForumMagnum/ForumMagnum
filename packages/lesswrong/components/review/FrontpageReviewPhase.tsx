e787b25cc35994cf59021888b5c280e8ecb7f0cc
Habryka <habryka@Habrykas-iMac.local>
2020-11-27 10:48:43 -0800

    Build Progress Bar

--- packages/lesswrong/components/review/FrontpageReviewPhase.tsx
+++ packages/lesswrong/components/review/FrontpageReviewPhase.tsx
@@ -3,15 +3,53 @@ import { Components, registerComponent } from '../../lib/vulcan-lib';
 import { Link } from '../../lib/reactRouterWrapper';
 import { useCurrentUser } from '../common/withUser'
 import {AnalyticsContext} from "../../lib/analyticsEvents";
+import classNames from 'classnames';
 
 const styles = (theme: ThemeType): JssStyles => ({
   timeRemaining: {
-    marginTop: 6,
-    marginBottom: 4
   },
   learnMore: {
     color: theme.palette.lwTertiary.main
-  }
+  },
+  subtitle: {
+    width: "100%",
+    display: 'flex',
+    justifyContent: 'space-between'
+  },
+  reviewTimeline: {
+    ...theme.typography.commentStyle,
+    display: 'flex',
+    marginBottom: 6,
+    marginTop: -8
+  },
+  nominationBlock: {flexGrow: 1, marginRight: 2},
+  reviewBlock: {flexGrow: 2, marginRight: 2},
+  votingBlock: {flexGrow: 1},
+  blockText: {color: 'white', zIndex: 1},
+  progress: {
+    position: 'relative',
+    marginBottom: 2,
+    padding: 4,
+    backgroundColor: 'rgba(0,0,0,0.14)',
+    display: 'flex',
+    justifyContent: 'space-between',
+    cursor: 'pointer',
+    '&:hover': {
+      boxShadow: "0px 0px 10px rgba(0,0,0,.1)",
+      opacity: 0.9
+    }
+  },
+  activeProgress: {
+    backgroundColor: theme.palette.primary.light
+  },
+  coloredProgress: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    height: '100%',
+    backgroundColor: theme.palette.primary.main
+  },
+  nominationDate: {}
 })
 
 export const reviewAlgorithm = {
@@ -46,31 +84,57 @@ const FrontpageReviewPhase = ({classes, settings}) => {
 
   return (
     <div>
-      <LWTooltip placement="top-start" title={reviewTooltip}>
+      <div className={classes.reviewTimeline}>
+        <div className={classes.nominationBlock}>
+          <LWTooltip placement="top-start" title={reviewTooltip} className={classNames(classes.progress, classes.activeProgress)}>
+            <div className={classes.blockText}>Nominations</div>
+            <div className={classes.blockText}>Dec 14</div>
+            <div className={classes.coloredProgress} style={{width: '35%'}}/>
+          </LWTooltip>
+        </div>
+        <div className={classes.reviewBlock}>      
+          <div className={classes.progress}>
+            <div className={classes.blockText}>Reviews</div>
+            <div className={classes.blockText}>Jan 11</div>
+            <div className={classes.coloredProgress}/>
+          </div>
+        </div>
+        <div className={classes.votingBlock}>
+          <div className={classes.progress}>
+            <div className={classes.blockText}>Votes</div>
+            <div className={classes.blockText}>Jan 25</div>
+            <div className={classes.coloredProgress}/>
+          </div>
+        </div>
+      </div>
+      {/* <SectionSubtitle className={classes.subtitle}>
         <div>
-          <SectionSubtitle >
+          <LWTooltip placement="top-start" title={reviewTooltip}>
             <Link to={"/reviews"}>
               The LessWrong 2018 Review
             </Link>
-            <div className={classes.timeRemaining}>
-              <em>You have until Jan 13th to review and edit posts (<span className={classes.learnMore}>
-                <HoverPreviewLink href="/posts/qXwmMkEBLL59NkvYR/the-lesswrong-2018-review" innerHTML={"learn more"}/>
-              </span>)</em>
-            </div>
-          </SectionSubtitle>
+          </LWTooltip>
         </div>
-      </LWTooltip>
-      <AnalyticsContext listContext={"LessWrong 2018 Review"} capturePostItemOnMount>
+        <div className={classes.timeRemaining}>
+          <LWTooltip placement="top-start" title={reviewTooltip}>
+            <em>You have until Dec 14th to nominate posts (<span className={classes.learnMore}>
+              <HoverPreviewLink href="/posts/qXwmMkEBLL59NkvYR/the-lesswrong-2018-review" innerHTML={"learn more"}/>
+            </span>)</em>
+          </LWTooltip>
+        </div>
+      </SectionSubtitle> */}
+      
+      <AnalyticsContext listContext={"LessWrong 2019 Review"} capturePostItemOnMount>
         <RecommendationsList algorithm={reviewAlgorithm} />
       </AnalyticsContext>
-      <SectionFooter>
+      {/* <SectionFooter>
         <Link to={"/reviews"}>
           Reviews Dashboard
         </Link>
         {currentUser && <Link to={`/users/${currentUser.slug}/reviews`}>
           My Reviews
         </Link>}
-      </SectionFooter>
+      </SectionFooter> */}
     </div>
   )
 }
