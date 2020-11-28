0e83f14c13658eeac4449809476e4cf405221264
James Babcock <jimrandomh@gmail.com>
2020-11-28 11:13:48 -0800

    Remove broken Enzyme shallow-rendering unit tests

--- packages/lesswrong/components/comments/CommentsItem/_comments-unit-tests.tsx
+++ packages/lesswrong/components/comments/CommentsItem/_comments-unit-tests.tsx
@@ -1,4 +1,10 @@
-import React from 'react';
+// STUBBED
+// These unit tests were super brittle/broken because they use Enzyme shallow
+// rendering rather than real rendering, and were made without all the
+// infrastructure for setting up a proper React context for use in unit tests.
+// If we make a better React component unit testing setup, maybe bring these
+// back.
+/*import React from 'react';
 import { shallow, configure } from 'enzyme';
 import { expect } from 'meteor/practicalmeteor:chai';
 import { CommentsItem } from './CommentsItem'
@@ -22,4 +28,4 @@ describe('CommentsItem', () => {
     const commentsItem = shallow(<CommentsItemUntyped {...commentMockProps} />)
     expect(commentsItem.find(".comments-item-reply-link")).to.have.length(1);
   });
-});
+});*/
