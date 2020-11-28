e9cf638b959a23d0e3ba5733e707a3716fe6a6c9
lagerros <jacobklagerros@gmail.com>
2020-11-27 19:04:51 -0800

    new stuff

--- packages/lesswrong/components/books/BookLanding.tsx
+++ packages/lesswrong/components/books/BookLanding.tsx
@@ -3,7 +3,7 @@ import { Components, registerComponent } from '../../lib/vulcan-lib';
 import { Image } from 'cloudinary-react';
 import { cloudinaryCloudNameSetting } from '../../lib/publicSettings';
 import Row from 'react-bootstrap/Row';
-import { Button } from '@material-ui/core';
+import { Button, TextField } from '@material-ui/core';
 
 const styles = (theme: ThemeType): JssStyles => ({
 
@@ -12,7 +12,7 @@ const styles = (theme: ThemeType): JssStyles => ({
     position: "absolute",
     zIndex: "4",
     top: "0",
-    right: "-30px",
+    right: "-50px",
     width: "35%",
     maxWidth: "400px",
     height: "auto",
@@ -23,38 +23,39 @@ const styles = (theme: ThemeType): JssStyles => ({
     maxWidth: "1500px",
     display: "flex",
     alignItems: "center",
+    justifyContent: "center",
     paddingLeft: "5%",
     paddingRight: "5%",
-    position: "relative",
-    zIndex: "5",
+ //   position: "relative",
+ //   zIndex: "5",
     marginBottom: "50px"
   },
   textBody: {
 
   },
   quoteContainer: {
-    maxWidth: "400px",
-    float: "left",
+    maxWidth: "800px",
+    float: "none",
     backgroundColor: "white",
-    padding: "15px",
+    //padding: "15px",
     borderRightStyle: "solid",
     borderWidth: "10px",
-    height: "250px",
-    maxHeight: "250px",
+    height: "auto",
+   // maxHeight: "250px",
     alignItems: "center"
   },
   quote: {
-    fontSize: "36px",
+    fontSize: "54px",
     color: "#736b6b",
     marginBottom: "10px"
   },
   quoteAuthor: {
-    fontSize: "22px",
+    fontSize: "33px",
     color: "grey"
   },
   fullSetImage: {
     float: "left",
-    width: "50%",
+    width: "68%",
     height: "auto"
   },
   contentImage: { // should make this inherit basics from "fullSetImage", but not sure how. Or they should share a parent
@@ -63,17 +64,18 @@ const styles = (theme: ThemeType): JssStyles => ({
     height: "auto"
   },
   buyButtonRoot: {
-    background: "white", // 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    background: "#40ad48", // 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
     borderRadius: 3,
     border: 0,
-    color: 'grey',
+    color: 'white',
     height: 48,
-    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-    marginTop: "30px",
-    fontSize: "16px"
+   // boxShadow: '0 3px 5px 2px rgba(30, 25, 135, .3)',
+    marginTop: "60px",
+    fontSize: "30px"
   },
   buyButtonLabel: {
-    textTransform: 'capitalize'
+    textTransform: 'capitalize',
+    fontSize: "35px"
   },
   backgroundContainer: {
     position: 'relative'
@@ -102,8 +104,66 @@ const styles = (theme: ThemeType): JssStyles => ({
     textAlign: "justify",
     paddingRight: "50px",
     paddingLeft: "50px"
+  },
+
+  textField: {
+    fontSize: "22px"
+  },
+
+  coloredBand: {
+    width: "100%",
+    overflow: "hidden",
+    marginBottom: "3%"
+  },
+
+  colorQuoteContainer: {
+    margin: "5%",
+    marginTop: "5%",
+    marginBottom: "5%",
+    marginLeft: "14%"
+  },
+
+  bigColorQuote: {
+    alignItems: "center",
+    fontSize: "48px",
+    maxWidth: "600px",
+    display: "block",
+    marginBottom: "2%"
+  },
+
+  bigColorAuthor: {
+    fontSize: "38px",
+    color: "#aaa8ae",
+  },
+
+  largeImage: {
+    width: "100%",
+    height: "auto",
+    display: "block",
+    margin: "-13% 0px -13% 0px"
   }
 })
+
+const colorBandAndQuote = ({ classes , color, imageURL, quote, quoteAuthor}) => {
+  return (
+    <div>
+      <div className={classes.coloredBand} style={{"backgroundColor": color}}>
+        <img src={imageURL}
+              className={classes.largeImage}
+            />
+      </div>
+      <div className={classes.colorQuoteContainer}>
+        <div className={classes.bigColorQuote} style={{"color": color}}>
+          {quote}
+        </div>
+        <div className={classes.bigColorAuthor} >
+          {quoteAuthor}
+        </div>
+      </div>
+    </div>
+  )
+}
+
 
 const BookLanding = ({ classes }: {
   classes: ClassesType,
@@ -113,41 +173,64 @@ const BookLanding = ({ classes }: {
 
   return (
     <div>
-      <div className={classes.backgroundContainer}>
-        <div className={classes.row}>
-          <img src="https://res.cloudinary.com/lesswrong-2-0/image/upload/v1606266348/mockup_with_spines_w7bopz.png"
-            className={classes.fullSetImage}
-          />
-          <div>
-            <div className={classes.quoteContainer}
-              style={{borderRightColor: "white"}}
-            >
-              <div className={classes.quote}>
-              The rationality community is one of the brightest lights in the modern intellectual firmament.
-              </div>
-              <div className={classes.quoteAuthor}>
-              Bryan Caplan, George Mason University
-              </div>
+      <div className={classes.row}>
+        <img src="https://res.cloudinary.com/lesswrong-2-0/image/upload/v1606266348/mockup_with_spines_w7bopz.png"
+          className={classes.fullSetImage}
+        />
+        <div style={{float: "none", display: "block"}} >
+          <div className={classes.quoteContainer}
+            style={{borderRightColor: "white"}}
+          >
+            <div className={classes.quote}>
+            The rationality community is one of the brightest lights in the modern intellectual firmament.
+            </div>
+            <div className={classes.quoteAuthor}>
+            Bryan Caplan, George Mason University
             </div>
             <Button classes={{root: classes.buyButtonRoot, label: classes.buyButtonLabel}}>
               Buy the book set
             </Button>
           </div>
+
         </div>
-        <img src="https://res.cloudinary.com/lesswrong-2-0/image/upload/v1606266348/Site_side_stripe_i33qmp.png"
-          className={classes.sideStripe}
-        />
       </div>
-      <div className={classes.row}>
-        <div className={classes.mainTextBlock}>
-          <div className={classes.textColumn}>
+      {/* <img src="https://res.cloudinary.com/lesswrong-2-0/image/upload/v1606266348/Site_side_stripe_i33qmp.png"
+        className={classes.sideStripe}
+      /> */}
 
-              <strong>A Map that Reflects the Territory. </strong>
-              The greatest writing of the LessWrong community, collected into a beautifully
-              packaged book set, each book small enough to fit in your pocket.
 
-            {/* TODO: put the author text in columns instead</p>
-             */}
+
+      {colorBandAndQuote({classes,
+        color: '#40AD48',
+        imageURL: 'https://res.cloudinary.com/lesswrong-2-0/image/upload/v1606523337/Book%20landing%20page/Colored%20background/1_Epistemology_Marketing.png',
+        quote: 'Whenever there’s a cutting-edge new idea making the rounds, Eliezer was writing about it 5-10 years ago. A deep dive on Less Wrong will make you smarter.',
+        quoteAuthor: 'Tim Urban, Author "Wait but Why"'
+      })}
+
+      {colorBandAndQuote({classes,
+        color: '#c33e3f',
+        imageURL: 'https://res.cloudinary.com/lesswrong-2-0/image/upload/v1606523345/Book%20landing%20page/Colored%20background/2_Agency_Marketing.png',
+        quote: 'The rationality community is one of the brightest lights in the modern intellectual firmament.',
+        quoteAuthor: 'Bryan Caplan, George Mason University'
+      })}
+
+     {colorBandAndQuote({classes,
+        color: '#1c9fd9',
+        imageURL: 'https://res.cloudinary.com/lesswrong-2-0/image/upload/v1606523336/Book%20landing%20page/Colored%20background/3_Coordination_Marketing.png',
+        quote: 'Whenever there’s a cutting-edge new idea making the rounds, Eliezer was writing about it 5-10 years ago.',
+        quoteAuthor: 'Tim Urban, Author "Wait but Why"'
+      })}
+
+      <div className={classes.coloredBand} style={{"backgroundColor": "#d9ae1a"}}>
+        <img src="https://res.cloudinary.com/lesswrong-2-0/image/upload/v1606525655/Book%20landing%20page/Colored%20background/Curiosity_internals_2_wide.jpg"
+              className={classes.largeImage}
+            />
+      </div>
+
+
+      {/* <div className={classes.bigColorQuote} style={{"color": "#d9ae1a"}}>
+        The rationality community is one of the brightest lights in the modern intellectual firmament.
+      </div>
           </div>
           <div className={classes.textColumn}>
 
@@ -159,57 +242,70 @@ const BookLanding = ({ classes }: {
             <ul></ul>
             {/* Maybe use .map here instead? */}
 
-          </div>
-          <div className={classes.bodyText}>
-          </div>
-        </div>
-      </div>
-      <div className={classes.row}>
-        <img src="https://res.cloudinary.com/lesswrong-2-0/image/upload/v1606289257/Book%20landing%20page/Epistemology_internals_cropped_on_white.jpg"
-              className={classes.contentImage}
-            />
-        <div className={classes.quoteContainer}
-          style={{borderRightColor: "#4aa951"}}>
-          <div className={classes.quote}>
-              Whenever there’s a cutting-edge new idea making the rounds, Eliezer was writing about it 5-10 years ago. A deep dive on Less Wrong will make you smarter.
-          </div>
-          <div className={classes.quoteAuthor}>
-              Tim Urban, author of "Wait But Why"
-          </div>
-        </div>
-      </div>
+      {/*     </div>
+      //     <div className={classes.bodyText}>
+      //     </div>
+      //   </div>
+      // </div>
 
-      <div className={classes.row}>
-      <img src="https://res.cloudinary.com/lesswrong-2-0/image/upload/v1606289260/Book%20landing%20page/Curiosity_internals_white_cropped.jpg"
-              className={classes.contentImage}
-            />
-        <div className={classes.quoteContainer}
-          style={{borderRightColor: "#e8b10e"}}>
-          <div className={classes.quote}>
-            The rationality community is one of the brightest lights in the modern intellectual firmament.
-          </div>
-          <div className={classes.quoteAuthor}>
-              Tim Urban, author of "Wait But Why"
-          </div>
-        </div>
-      </div>
+      // <div className={classes.row}>
+      //   <img src="https://res.cloudinary.com/lesswrong-2-0/image/upload/v1606289257/Book%20landing%20page/Epistemology_internals_cropped_on_white.jpg"
+      //         className={classes.contentImage}
+      //       />
+      //   <div className={classes.quoteContainer}
+      //     style={{borderRightColor: "#4aa951"}}>
+      //     <div className={classes.quoteAuthor}>
+      //         Whenever there’s a cutting-edge new idea making the rounds, Eliezer was writing about it 5-10 years ago. A deep dive on Less Wrong will make you smarter.
+      //     </div>
+      //     <div className={classes.quote}>
+      //         Tim Urban, author of "Wait But Why"
+      //     </div>
+      //   </div>
+      // </div>
 
-      <div className={classes.row}>
-        <img src="https://res.cloudinary.com/lesswrong-2-0/image/upload/v1606289258/Book%20landing%20page/Agency_internals_copy.jpg"
-              className={classes.contentImage}
-            />
-        <div className={classes.quoteContainer}
-          style={{borderRightColor: "#eb5b50"}}>
-          <div className={classes.quote}>
-              Whenever there’s a cutting-edge new idea making the rounds, Eliezer was writing about it 5-10 years ago. A deep dive on Less Wrong will make you smarter.
-          </div>
-          <div className={classes.quoteAuthor}>
-              Tim Urban, author of "Wait But Why"
-          </div>
-        </div>
-      </div>
+      // <div className={classes.row}>
+      //   <img src="https://res.cloudinary.com/lesswrong-2-0/image/upload/v1606289260/Book%20landing%20page/Curiosity_internals_white_cropped.jpg"
+      //         className={classes.contentImage}
+      //       />
+      //   <div className={classes.quoteContainer}
+      //     style={{borderRightColor: "#e8b10e"}}>
+      //     <div className={classes.quoteAuthor}>
+      //       The rationality community is one of the brightest lights in the modern intellectual firmament.
+      //     </div>
+      //     <div className={classes.quote}>
+      //         Tim Urban, author of "Wait But Why"
+      //     </div>
+      //   </div>
+      // </div>
+
+      // <div className={classes.row}>
+      //   <img src="https://res.cloudinary.com/lesswrong-2-0/image/upload/v1606289258/Book%20landing%20page/Agency_internals_copy.jpg"
+      //         className={classes.contentImage}
+      //       />
+      //   <div className={classes.quoteContainer}
+      //     style={{borderRightColor: "#eb5b50"}}>
+      //     <div className={classes.quote}>
+      //         Whenever there’s a cutting-edge new idea making the rounds, Eliezer was writing about it 5-10 years ago. A deep dive on Less Wrong will make you smarter.
+      //     </div>
+      //     <div className={classes.quoteAuthor}>
+      //         Tim Urban, author of "Wait But Why"
+      //     </div>
+      //   </div>
+      // </div>
+
+      // <div className={classes.row} style={{paddingTop: "100px", float: "right", paddingRight: "370px"}}>
+
+      //     <div style={{fontSize: "20px", maxWidth: "400px", paddingRight: "25px"}}>
+      //         Get sent the 2-3 best LessWrong posts each week.
+      //     </div>
+
+      //   <form className={classes.root} noValidate autoComplete="off">
+      //     <TextField id="outlined-basic" label="Email" variant="outlined" />
+      //   </form>
+      // </div> */}
 
     </div>
+
   )
 }
 
