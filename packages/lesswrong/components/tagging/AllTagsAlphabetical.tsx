7312b9aac2de9fa96f81d6899999d23241c4d6ff
James Babcock <jimrandomh@gmail.com>
2020-11-29 21:23:20 -0800

    Fix warning

--- packages/lesswrong/components/tagging/AllTagsAlphabetical.tsx
+++ packages/lesswrong/components/tagging/AllTagsAlphabetical.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { registerComponent, Components } from '../../lib/vulcan-lib';
 import { useMulti } from '../../lib/crud/withMulti';
-import { Tags } from '../../lib/collections/tags/collection';
 import { Link } from '../../lib/reactRouterWrapper';
 import AddBoxIcon from '@material-ui/icons/AddBox';
 import _sortBy from 'lodash/sortBy';
