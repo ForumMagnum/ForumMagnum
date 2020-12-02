04f705e8d81dba24c01ef7a6d6738337fe5da316
JP Addison <johnpaddison@gmail.com>
2020-12-02 10:35:18 +0000

    link to post

--- packages/lesswrong/components/ea-forum/SmallpoxBanner.tsx
+++ packages/lesswrong/components/ea-forum/SmallpoxBanner.tsx
@@ -3,10 +3,12 @@ import { createStyles } from '@material-ui/core/styles'
 import { registerComponent, Components } from '../../lib/vulcan-lib';
 import { SECTION_WIDTH } from '../common/SingleColumnSection';
 import { cloudinaryCloudNameSetting } from '../../lib/publicSettings';
+import { Link } from '../../lib/reactRouterWrapper';
 
 const bannerHeight = 250
 const mobileImageId = 'Banner/Smallpox_Eradication_Day-17.png'
 const desktopImageId = 'Banner/Smallpox_Eradication_Day-16.png'
+const featuredPost = '/posts/jk7A3NMdbxp65kcJJ/500-million-but-not-a-single-one-more'
 
 const mobileImage = `https://res.cloudinary.com/${cloudinaryCloudNameSetting.get()}/image/upload/w_${SECTION_WIDTH},h_${bannerHeight}/${mobileImageId}`
 const desktopImage = `https://res.cloudinary.com/${cloudinaryCloudNameSetting.get()}/image/upload/w_${SECTION_WIDTH},h_${bannerHeight}/${desktopImageId}`
@@ -21,17 +23,19 @@ const SmallpoxBanner = ({ classes }) => {
   const { SingleColumnSection } = Components
   
   return <SingleColumnSection>
-    <picture>
-      {/* 960px is the md boundary */}
-      <source media="(max-width: 959px)" srcSet={mobileImage} />
-      <source media="(min-width: 960px)" srcSet={desktopImage} />
-      <img
-        className={classes.image}
-        src={desktopImage}
-        height={bannerHeight}
-        width='100%'
-      />
-    </picture>
+    <Link to={featuredPost}>
+      <picture>
+        {/* 960px is the md boundary */}
+        <source media="(max-width: 959px)" srcSet={mobileImage} />
+        <source media="(min-width: 960px)" srcSet={desktopImage} />
+        <img
+          className={classes.image}
+          src={desktopImage}
+          height={bannerHeight}
+          width='100%'
+        />
+      </picture>
+    </Link>
   </SingleColumnSection>
 }
 
