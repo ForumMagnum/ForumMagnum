hibukki/EAForum

basic banner image working

packages/lesswrong/components/ea-forum/EAHome.tsx
@@ -1,23 +1,28 @@
 import React from 'react'
 import { userHasEAHomeHandbook } from '../../lib/betas'
 import { PublicInstanceSetting } from '../../lib/instanceSettings'
+import { DatabasePublicSetting } from '../../lib/publicSettings'
 import { Components, registerComponent } from '../../lib/vulcan-lib'
 import { useCurrentUser } from '../common/withUser'
 
 const eaHomeSequenceIdSetting = new PublicInstanceSetting<string | null>('eaHomeSequenceId', null, "optional") // Sequence ID for the EAHomeHandbook sequence
+const showSmallpoxSetting = new DatabasePublicSetting<boolean>('showSmallpox', false)
 
 const EAHome = () => {
   const currentUser = useCurrentUser();
   const {
-    RecentDiscussionThreadsList, HomeLatestPosts, EAHomeHandbook, RecommendationsAndCurated
+    RecentDiscussionThreadsList, HomeLatestPosts, EAHomeHandbook, RecommendationsAndCurated, SmallpoxBanner
   } = Components
 
   const recentDiscussionCommentsPerPost = (currentUser && currentUser.isAdmin) ? 4 : 3;
   const shouldRenderEAHomeHandbook = userHasEAHomeHandbook(currentUser)
+  const shouldRenderSmallpox = showSmallpoxSetting.get()
 
   return (
     <React.Fragment>
       {shouldRenderEAHomeHandbook && <EAHomeHandbook documentId={eaHomeSequenceIdSetting.get()}/>}
+      
+      {shouldRenderSmallpox && <SmallpoxBanner/>}
 
       <HomeLatestPosts />
 

ForumMagnum/ForumMagnum

packages/lesswrong/components/ea-forum/EAHome.tsx
@@ -1,23 +1,28 @@
 import React from 'react'
 import { userHasEAHomeHandbook } from '../../lib/betas'
 import { PublicInstanceSetting } from '../../lib/instanceSettings'
+import { DatabasePublicSetting } from '../../lib/publicSettings'
 import { Components, registerComponent } from '../../lib/vulcan-lib'
 import { useCurrentUser } from '../common/withUser'
 
 const eaHomeSequenceIdSetting = new PublicInstanceSetting<string | null>('eaHomeSequenceId', null, "optional") // Sequence ID for the EAHomeHandbook sequence
+const showSmallpoxSetting = new DatabasePublicSetting<boolean>('showSmallpox', false)
 
 const EAHome = () => {
   const currentUser = useCurrentUser();
   const {
-    RecentDiscussionFeed, HomeLatestPosts, EAHomeHandbook, RecommendationsAndCurated
+    RecentDiscussionFeed, HomeLatestPosts, EAHomeHandbook, RecommendationsAndCurated, SmallpoxBanner
   } = Components
 
   const recentDiscussionCommentsPerPost = (currentUser && currentUser.isAdmin) ? 4 : 3;
   const shouldRenderEAHomeHandbook = userHasEAHomeHandbook(currentUser)
+  const shouldRenderSmallpox = showSmallpoxSetting.get()
 
   return (
     <React.Fragment>
       {shouldRenderEAHomeHandbook && <EAHomeHandbook documentId={eaHomeSequenceIdSetting.get()}/>}
+      
+      {shouldRenderSmallpox && <SmallpoxBanner/>}
 
       <HomeLatestPosts />
 