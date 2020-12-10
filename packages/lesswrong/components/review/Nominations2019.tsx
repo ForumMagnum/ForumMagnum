4c3dcb37740262b8dd6ae1a19138c889b15db1e8
Raemon <raemon777@gmail.com>
2020-12-09 23:29:43 -0800

    nomination styling

--- packages/lesswrong/components/review/Nominations2019.tsx
+++ packages/lesswrong/components/review/Nominations2019.tsx
@@ -6,10 +6,15 @@ const styles = (theme: ThemeType): JssStyles => ({
     ...theme.typography.body2,
     color: theme.palette.grey[600]
   },
+  meta: {
+    display: "flex",
+    alignItems: "flex-end",
+    justifyContent: "space-between",
+    marginBottom: 16,
+  },
   info: {
     ...theme.typography.body2,
-    ...theme.typography.commentStyle,
-    marginBottom: 16,
+    ...theme.typography.commentStyle
   }
 })
 
@@ -23,14 +28,15 @@ const Nominations2019 = ({classes}: {
   return (
     <div>
       <SingleColumnSection>
-        <SectionTitle title="Nominated Posts for the 2019 Review">
+        <SectionTitle title="Nominated Posts for the 2019 Review"/>
+        <div className={classes.meta}>
+          <div className={classes.info}>
+            <div>Posts need at least 2 nominations to continue into the Review Phase.</div>
+            <div>Nominate posts that you have personally found useful and important.</div>
+          </div>
           <a className={classes.setting} onClick={() => setSortBy(!sortByMost)}>
             Sort by: {sortByMost ? "most" : "fewest"} nominations
           </a>
-        </SectionTitle>
-        <div className={classes.info}>
-          <div>Posts need at least 2 nominations to continue into the Review Phase.</div>
-          <div>Nominate posts that you have personally found useful and important.</div>
         </div>
         <PostsList2 
           terms={{view:"nominations2019", sortByMost: sortByMost, limit: 50}} 
