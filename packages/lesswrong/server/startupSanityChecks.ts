8c45898245072158e6ff0e70244fdf75739f46ab
James Babcock <jimrandomh@gmail.com>
2020-12-10 23:43:14 -0800

    Fix an undefined-vs-null issue that prevented starting without a database

--- packages/lesswrong/server/startupSanityChecks.ts
+++ packages/lesswrong/server/startupSanityChecks.ts
@@ -9,13 +9,14 @@ const expectedDatabaseIdSetting = new PublicInstanceSetting<string | null>('expe
 onStartup(() => {
   const expectedDatabaseId = expectedDatabaseIdSetting.get();
   const databaseIdObject = DatabaseMetadata.findOne({ name: "databaseId" });
+  const databaseId = databaseIdObject?.value || null;
   
   // If either the database or the settings config file contains an ID, then
   // both must contain IDs and they must match.
-  if (expectedDatabaseId || databaseIdObject) {
-    if (expectedDatabaseId !== databaseIdObject?.value) {
+  if (expectedDatabaseId || databaseId) {
+    if (expectedDatabaseId !== databaseId) {
       console.error("Database ID in config file and in database don't match."); // eslint-disable-line no-console
-      console.error(`    Database ID: ${databaseIdObject?.value}`); // eslint-disable-line no-console
+      console.error(`    Database ID: ${databaseId}`); // eslint-disable-line no-console
       console.error(`    Expected database ID: ${expectedDatabaseId}`); // eslint-disable-line no-console
       console.error("If you are connecting to a production DB, you must use a matching config file. If you are *not* connecting to a production DB, you should not use a production config file."); // eslint-disable-line no-console
       process.exit(1);
