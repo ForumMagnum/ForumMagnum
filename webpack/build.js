ecafe962bc332b64b5ffeb5b58f42ed76e65712b
James Babcock <jimrandomh@gmail.com>
2020-12-11 15:08:12 -0800

    Convert from command-line invocation to script-based invocation of ESBuild

---
+++ webpack/build.js
@@ -0,0 +1,32 @@
+const esbuild = require("esbuild");
+
+esbuild.buildSync({
+  entryPoints: ['./src/platform/current/server/serverStartup.ts'],
+  bundle: true,
+  outfile: './build/server/js/bundle2.js',
+  platform: "node",
+  sourcemap: true,
+  define: {
+    "process.env.NODE_ENV": "\"development\"",
+    "webpackIsServer": true,
+  },
+  external: [
+    "akismet-api", "mongodb", "canvas", "express", "mz", "pg", "pg-promise",
+    "mathjax-node", "jsdom", "@sentry/node", "node-fetch", "later", "turndown",
+    "apollo-server", "apollo-server-express",
+  ],
+})
+
+esbuild.buildSync({
+  entryPoints: ['./src/client.js'],
+  bundle: true,
+  target: "es6",
+  sourcemap: true,
+  outfile: "./build/client/js/bundle.js",
+  define: {
+    "process.env.NODE_ENV": "\"development\"",
+    "webpackIsServer": false,
+    "global": "window",
+  },
+});
+
