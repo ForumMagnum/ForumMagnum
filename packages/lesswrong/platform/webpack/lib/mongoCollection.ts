d7af9b6bd8483a396b5996919fb2cb31c6baa356
James Babcock <jimrandomh@gmail.com>
2020-12-10 20:28:30 -0800

    Fix some minor type errors in Webpack stubs

--- packages/lesswrong/platform/webpack/lib/mongoCollection.ts
+++ packages/lesswrong/platform/webpack/lib/mongoCollection.ts
@@ -1,7 +1,13 @@
 
-class MongoCollection {
+export class MongoCollection {
   constructor(tableName: string) {
   }
   
   //TODO
+  views: any
+  defaultView: any
+  addView: any
+  aggregate: any
+  rawCollection: any
+  addDefaultView: any
 }
