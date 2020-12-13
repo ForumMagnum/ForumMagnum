2784e3cad5cce7c803de339121d097d05299fa24
James Babcock <jimrandomh@gmail.com>
2020-12-12 18:54:47 -0800

    useMeteor/useWebpack scripts handle node_modules

--- scripts/useMeteor.sh
+++ scripts/useMeteor.sh
@@ -3,3 +3,6 @@
 rm -f packages/lesswrong/platform/current
 ln -s ./meteor packages/lesswrong/platform/current
 
+rm -rf node_modules
+yarn install
+
--- scripts/useWebpack.sh
+++ scripts/useWebpack.sh
@@ -3,3 +3,7 @@
 rm -f packages/lesswrong/platform/current
 ln -s ./webpack packages/lesswrong/platform/current
 
+rm -rf node_modules
+(cd webpack && yarn install)
+ln -s webpack/node_modules node_modules
+
