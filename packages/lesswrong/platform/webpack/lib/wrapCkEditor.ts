92f67ec229af382141134bf95ac038fef0e68e9c
James Babcock <jimrandomh@gmail.com>
2020-12-15 15:37:50 -0800

    CkEditor under Webpack

--- packages/lesswrong/platform/webpack/lib/wrapCkEditor.ts
+++ packages/lesswrong/platform/webpack/lib/wrapCkEditor.ts
@@ -1,5 +1,9 @@
+const ckEditor = webpackIsServer ? {} : require('@lesswrong/lesswrong-editor');
 
-export const EditorWatchdog = null;
-export const CommentEditor = null;
-export const PostEditor = null;
-export const PostEditorCollaboration = null;
+const { EditorWatchdog, CommentEditor, PostEditor, PostEditorCollaboration } = ckEditor;
+export { EditorWatchdog, CommentEditor, PostEditor, PostEditorCollaboration };
+
+//export const EditorWatchdog = null;
+//export const CommentEditor = null;
+//export const PostEditor = null;
+//export const PostEditorCollaboration = null;
