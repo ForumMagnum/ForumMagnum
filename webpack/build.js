a0f372f8929419e181d72e7b4a3733fbfe30cf03
James Babcock <jimrandomh@gmail.com>
2020-12-14 16:55:55 -0800

    Webpack build.js: Better debug-vs-prod handling

--- webpack/build.js
+++ webpack/build.js
@@ -1,18 +1,21 @@
 const esbuild = require("esbuild");
 
+const isProduction = false;
+
 esbuild.buildSync({
   entryPoints: ['./src/platform/current/server/serverStartup.ts'],
   bundle: true,
   outfile: './build/server/js/bundle2.js',
   platform: "node",
   sourcemap: true,
+  minify: false,
   define: {
-    "process.env.NODE_ENV": "\"development\"",
+    "process.env.NODE_ENV": isProduction ? "\"production\"" : "\"development\"",
     "webpackIsServer": true,
   },
   external: [
     "akismet-api", "mongodb", "canvas", "express", "mz", "pg", "pg-promise",
-    "mathjax-node", "jsdom", "@sentry/node", "node-fetch", "later", "turndown",
+    "mathjax", "mathjax-node", "jsdom", "@sentry/node", "node-fetch", "later", "turndown",
     "apollo-server", "apollo-server-express", "graphql",
   ],
 })
@@ -23,8 +26,9 @@ esbuild.buildSync({
   target: "es6",
   sourcemap: true,
   outfile: "./build/client/js/bundle.js",
+  minify: false,
   define: {
-    "process.env.NODE_ENV": "\"development\"",
+    "process.env.NODE_ENV": isProduction ? "\"production\"" : "\"development\"",
     "webpackIsServer": false,
     "global": "window",
   },
