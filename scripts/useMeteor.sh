7f699cbaca892cefb348fa51274cf24e45ccedd7
James Babcock <jimrandomh@gmail.com>
2020-12-17 17:08:31 -0800

    useMeteor.sh/useWeback.sh don't have to redownload node_modules

--- scripts/useMeteor.sh
+++ scripts/useMeteor.sh
@@ -3,6 +3,18 @@
 rm -f packages/lesswrong/platform/current
 ln -s ./meteor packages/lesswrong/platform/current
 
-rm -rf node_modules
-yarn install
+if [ -d node_modules.meteor ]; then
+  echo "Switching to Meteor version"
+  # Remove node_modules (this will just be a symlink, belonging to webpack)
+  rm -rf node_modules
+  mv node_modules.meteor node_modules
+  yarn install
+elif [ -d node_modules/webpack ]; then
+  echo "Switching to Meteor version"
+  rm -rf node_modules
+  yarn install
+else
+  # Meteor already selected; nothing to do
+  echo "Meteor already selected"
+fi
 
--- scripts/useWebpack.sh
+++ scripts/useWebpack.sh
@@ -3,7 +3,22 @@
 rm -f packages/lesswrong/platform/current
 ln -s ./webpack packages/lesswrong/platform/current
 
-rm -rf node_modules
-(cd webpack && yarn install)
-ln -s webpack/node_modules node_modules
+if [ -d node_modules ] && [ ! -d node_modules/webpack ]; then
+  # If node_modules exists but belongs to Meteor, move it to
+  # node_modules.meteor, then set up symlinks and yarn install.
+  echo "Switching to Webpack version"
+  mv node_modules node_modules.meteor
+  ln -s webpack/node_modules node_modules
+  (cd webpack && yarn install)
+elif [ -d node_modules/webpack ]; then
+  # If node_modules exists and belongs to Webpack, do nothing
+  echo "Webpack already selected"
+else
+  # If node_modules doesn't exist or isn't a directory, remove it, set up
+  # symlinks and yarn install.
+  echo "Switching to Webpack version"
+  rm -rf node_modules
+  ln -s webpack/node_modules node_modules
+  (cd webpack && yarn install)
+fi
 
