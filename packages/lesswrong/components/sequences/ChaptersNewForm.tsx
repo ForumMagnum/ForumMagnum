1b533c919587f1f2cee19552dacaee1f74e700f1
James Babcock <jimrandomh@gmail.com>
2020-12-19 15:56:01 -0800

    Partially JSS-ify sequences editor

--- packages/lesswrong/components/sequences/ChaptersNewForm.tsx
+++ packages/lesswrong/components/sequences/ChaptersNewForm.tsx
@@ -1,16 +1,19 @@
 import { Components, registerComponent, getFragment } from '../../lib/vulcan-lib';
 import React from 'react';
 import Chapters from '../../lib/collections/chapters/collection';
+import { styles } from './CollectionsEditForm';
+import classNames from 'classnames';
 
 //TODO: Manage chapter removal to remove the reference from all parent-sequences
 
-const ChaptersNewForm = ({successCallback, cancelCallback, prefilledProps}: {
+const ChaptersNewForm = ({successCallback, cancelCallback, prefilledProps, classes}: {
   successCallback?: ()=>void,
   cancelCallback?: ()=>void,
   prefilledProps?: Record<string,any>,
+  classes: ClassesType,
 }) => {
   return (
-    <div className="chapters-new-form">
+    <div className={classNames(classes.newOrEditForm,classes.newForm)}>
       <h3>Add Chapter</h3>
       <Components.WrappedSmartForm
         collection={Chapters}
@@ -25,7 +28,7 @@ const ChaptersNewForm = ({successCallback, cancelCallback, prefilledProps}: {
   )
 }
 
-const ChaptersNewFormComponent = registerComponent('ChaptersNewForm', ChaptersNewForm);
+const ChaptersNewFormComponent = registerComponent('ChaptersNewForm', ChaptersNewForm, {styles});
 
 declare global {
   interface ComponentTypes {
