18f232a674ed6570fba7923721ca1a6aa6155335
James Babcock <jimrandomh@gmail.com>
2020-12-20 22:58:53 -0800

    Don't log Apollo state on startup

--- packages/lesswrong/platform/express/client/clientStartup.ts
+++ packages/lesswrong/platform/express/client/clientStartup.ts
@@ -1,8 +1,6 @@
 import { onStartupFunctions } from '../lib/executionEnvironment';
 
 async function clientStartup() {
-  console.log("In clientStartup");
-  console.log((window as any).__APOLLO_STATE__);
   require('../../../client.js');
   for (let startupFunction of onStartupFunctions)
     await startupFunction();
