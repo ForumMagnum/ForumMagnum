b35ae4f94a4ba862fd8529ab017c6b7771b508a4
James Babcock <jimrandomh@gmail.com>
2020-12-28 14:58:21 -0800

    Remove unused-and-stubbed component

--- packages/lesswrong/lib/components.ts
+++ packages/lesswrong/lib/components.ts
@@ -126,7 +126,6 @@ importComponent("HoverPreviewLink", () => require('../components/linkPreview/Hov
 importComponent(["PostLinkPreview", "PostLinkCommentPreview", "PostLinkPreviewSequencePost", "PostLinkPreviewSlug", "PostLinkPreviewLegacy", "CommentLinkPreviewLegacy", "PostLinkPreviewWithPost", "PostCommentLinkPreviewGreaterWrong", "DefaultPreview", "MozillaHubPreview", "MetaculusPreview"], () => require('../components/linkPreview/PostLinkPreview'));
 importComponent("LinkToPost", () => require('../components/linkPreview/LinkToPost'));
 
-importComponent("AccountsEnrollAccount", () => require('../components/users/EnrollAccount'));
 importComponent("UsersMenu", () => require('../components/users/UsersMenu'));
 importComponent("UsersEditForm", () => require('../components/users/UsersEditForm'));
 importComponent("UsersAccount", () => require('../components/users/UsersAccount'));
