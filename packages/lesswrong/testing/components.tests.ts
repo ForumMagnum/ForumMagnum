ad5d99fbece291dd561f75e95f25fac1bf1cf595
Oliver Habryka <panisnecis@gmail.com>
2020-12-29 20:18:18 -0800

    Log the current component and reactivate component tests

--- packages/lesswrong/testing/components.tests.ts
+++ packages/lesswrong/testing/components.tests.ts
@@ -3,8 +3,8 @@ import { importAllComponents } from '../lib/vulcan-lib/components';
 
 testStartup();
 
-// describe("Components", function () {
-//   it("doesn't crash when importing every component file", () => {
-//     importAllComponents();
-//   })
-// });
+describe("Components", function () {
+  it("doesn't crash when importing every component file", () => {
+    importAllComponents();
+  })
+});
