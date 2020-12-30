3dbe52ec5bf2fcfc597231195a44b1a071719772
Oliver Habryka <panisnecis@gmail.com>
2020-12-29 20:07:33 -0800

    What happens when we comment out the component tests?

--- packages/lesswrong/testing/components.tests.ts
+++ packages/lesswrong/testing/components.tests.ts
@@ -1,8 +1,8 @@
 import { importAllComponents } from '../lib/vulcan-lib/components';
 
-describe("Components", function () {
-  this.timeout(20000)
-  it("doesn't crash when importing every component file", () => {
-    importAllComponents();
-  })
-});
+// describe("Components", function () {
+//   this.timeout(20000)
+//   it("doesn't crash when importing every component file", () => {
+//     importAllComponents();
+//   })
+// });
