62d8fed6d88ffad3641da83dc316956cda901083
Discordius <panisnecis@gmail.com>
2021-01-03 17:29:55 -0800

    Try slightly different instructions

--- .platform/hooks/prebuild/yarn.sh
+++ .platform/hooks/prebuild/yarn.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 # need to install node first to be able to install yarn (as at prebuild no node is present yet)
-# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install 15
+node -e "console.log('Running Node.js ' + process.version)"
 # export NVM_DIR="$HOME/.nvm"
 
 
 # install
 cd /var/app/staging/
-
-nvm install 15
 
 echo "installed node version:"
 node --version
