533f151db19d65311a682ab1b41a5e4a257543bd
Discordius <panisnecis@gmail.com>
2021-01-03 16:58:15 -0800

    Adjust installation script

--- .platform/hooks/prebuild/yarn.sh
+++ .platform/hooks/prebuild/yarn.sh
@@ -2,11 +2,7 @@
 
 # need to install node first to be able to install yarn (as at prebuild no node is present yet)
 curl --silent --location https://rpm.nodesource.com/setup_15.x | sudo bash -
-sudo yum -y install nodejs
-
-# install yarn
-sudo wget https://dl.yarnpkg.com/rpm/yarn.repo -O /etc/yum.repos.d/yarn.repo
-sudo yum -y install yarn
+yum -y install nodejs
 
 # install
 cd /var/app/staging/
