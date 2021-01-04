9fb17bd957adf05f41eaf5341711b349c37743b4
Discordius <panisnecis@gmail.com>
2021-01-03 18:18:08 -0800

    Running nvm install instead of nvm use

--- scripts/runProduction.sh
+++ scripts/runProduction.sh
@@ -2,6 +2,7 @@ echo "Running Production Site"
 sudo chmod 755 ./
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
+nvm install
 nvm use
 yarn
 ./build.js -run --settings settings.json
