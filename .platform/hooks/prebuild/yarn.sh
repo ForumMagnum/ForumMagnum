053162c9d93b03e301c79733ab128cd36294309b
Discordius <panisnecis@gmail.com>
2021-01-03 17:12:36 -0800

    Make sure to add nvm to path

--- .platform/hooks/prebuild/yarn.sh
+++ .platform/hooks/prebuild/yarn.sh
@@ -2,6 +2,7 @@
 
 # need to install node first to be able to install yarn (as at prebuild no node is present yet)
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
 nvm install 15
 
 # install
