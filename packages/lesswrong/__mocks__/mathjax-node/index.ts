ccafee8662c38597d4d7da835e245eb97211ae10
James Babcock <jimrandomh@gmail.com>
2021-01-05 14:55:05 -0800

    Fix ESLint warnings in mocks

--- packages/lesswrong/__mocks__/mathjax-node/index.ts
+++ packages/lesswrong/__mocks__/mathjax-node/index.ts
@@ -1,5 +1,7 @@
 export default {
+    // eslint-disable-next-line no-console
     config: (conf) => console.log("Called mjAPI.config"),
+    // eslint-disable-next-line no-console
     start: () => console.log("Called mjAPI.start"),
     typeset: (conf) => ({html: "", css: ""})
 }
