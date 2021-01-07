d90e1870b6cb3695719af53fb393898c625693ca
Oliver Habryka <panisnecis@gmail.com>
2021-01-06 18:44:54 -0800

    Add settings file from runProduction script

--- scripts/runProduction.sh
+++ scripts/runProduction.sh
@@ -1,2 +1,4 @@
 echo "Running Production Site"
-./build.js -run --settings settings.json
+git clone https://Discordius:$GITHUB_CREDENTIALS_REPO_PAT@github.com/$GITHUB_CREDENTIALS_REPO_NAME.git Credentials 
+
+./build.js -run --settings ./Credentials/$SETTINGS_FILE_NAME
