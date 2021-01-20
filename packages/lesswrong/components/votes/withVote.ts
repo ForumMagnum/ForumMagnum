cb12955e2ef99e38c12f3c1e77315279822fc0b7
James Babcock <jimrandomh@gmail.com>
2021-01-19 21:47:43 -0800

    When multiple votes are inflight, a result from an earlier vote does not overwrite optimistic response from a later vote

--- packages/lesswrong/components/votes/withVote.ts
+++ packages/lesswrong/components/votes/withVote.ts
@@ -1,4 +1,4 @@
-import { useState, useCallback } from 'react';
+import { useState, useCallback, useRef } from 'react';
 import { useMessages } from '../common/withMessages';
 import { useMutation, gql } from '@apollo/client';
 import { setVoteClient } from '../../lib/voting/vote';
@@ -29,30 +29,47 @@ export const useVote = <T extends VoteableTypeClient>(document: T, collectionNam
 } => {
   const messages = useMessages();
   const [optimisticResponseDocument, setOptimisticResponseDocument] = useState<any>(null);
+  const mutationCounts = useRef({optimisticMutationIndex: 0, completedMutationIndex: 0});
   const collection = getCollection(collectionName);
   const query = getVoteMutationQuery(collection);
+  
   const [mutate] = useMutation(query, {
-    onCompleted: useCallback(() => {
-      setOptimisticResponseDocument(null)
+    onCompleted: useCallback((mutationResult) => {
+      if (++mutationCounts.current.completedMutationIndex == mutationCounts.current.optimisticMutationIndex) {
+        setOptimisticResponseDocument(null)
+      }
     }, []),
   });
   
   const vote = useCallback(async ({document, voteType, collectionName, currentUser}: {
     document: T, voteType: string|null, collectionName: VoteableCollectionName, currentUser: UsersCurrent
   }) => {
+    // Cast a vote. Because the vote buttons are easy to mash repeatedly (and
+    // the strong-voting mechanic encourages this), there could be multiple
+    // overlapping votes in-flight at once. We keep count of how many mutations
+    // we've sent out and how many responses we've gotten back, so that the
+    // result of an earlier vote does not overwrite the optimistic response of
+    // a later vote.
+    // FIXME: Currently the server is not guaranteed to process votes in the
+    // same order they're received (if they're in separate http requests), which
+    // means that if you double-click a vote button, you can get a weird result
+    // due to votes being processed out of order.
+    
     const newDocument = await setVoteClient({collection, document, user: currentUser, voteType });
 
     try {
-      void mutate({
+      mutationCounts.current.optimisticMutationIndex++;
+      setOptimisticResponseDocument(newDocument);
+      await mutate({
         variables: {
           documentId: document._id,
           voteType,
         },
       })
-      setOptimisticResponseDocument(newDocument);
     } catch(e) {
       const errorMessage = _.map(e.graphQLErrors, (gqlErr: any)=>gqlErr.message).join("; ");
       messages.flash({ messageString: errorMessage });
+      setOptimisticResponseDocument(null);
     }
   }, [messages, mutate, collection]);
   
