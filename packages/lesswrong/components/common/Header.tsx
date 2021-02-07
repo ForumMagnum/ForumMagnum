2d4802e68c60cbeb8d82c957b0fdec8b898ed0f5
James Babcock <jimrandomh@gmail.com>
2021-02-07 14:45:54 -0800

    Hookify Header

--- packages/lesswrong/components/common/Header.tsx
+++ packages/lesswrong/components/common/Header.tsx
@@ -1,6 +1,6 @@
-import React, { PureComponent } from 'react';
+import React, { useState } from 'react';
 import { Components, registerComponent } from '../../lib/vulcan-lib';
-import { withUpdateCurrentUser, WithUpdateCurrentUserProps } from '../hooks/useUpdateCurrentUser';
+import { useUpdateCurrentUser } from '../hooks/useUpdateCurrentUser';
 import { Link } from '../../lib/reactRouterWrapper';
 import NoSSR from 'react-no-ssr';
 import Headroom from '../../lib/react-headroom'
@@ -11,10 +11,11 @@ import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import TocIcon from '@material-ui/icons/Toc';
 import grey from '@material-ui/core/colors/grey';
-import withUser from '../common/withUser';
+import { useCurrentUser } from '../common/withUser';
 import withErrorBoundary from '../common/withErrorBoundary';
 import classNames from 'classnames';
-import { withTracking, AnalyticsContext } from '../../lib/analyticsEvents';
+import { AnalyticsContext } from '../../lib/analyticsEvents';
+import { useTracking } from "../../lib/analyticsEvents";
 import { forumTypeSetting, PublicInstanceSetting } from '../../lib/instanceSettings';
 
 const forumHeaderTitleSetting = new PublicInstanceSetting<string>('forumSettings.headerTitle', "LESSWRONG", "warning")
@@ -139,59 +140,45 @@ const styles = (theme: ThemeType): JssStyles => ({
   },
 });
 
-interface ExternalProps {
-  standaloneNavigationPresent: any,
-  toggleStandaloneNavigation: any,
+const Header = ({standaloneNavigationPresent, toggleStandaloneNavigation, toc, searchResultsArea, theme, classes}: {
+  standaloneNavigationPresent: boolean,
+  toggleStandaloneNavigation: ()=>void,
   toc: any,
-  searchResultsArea: any,
-}
-interface HeaderProps extends ExternalProps, WithUserProps, WithStylesProps, WithTrackingProps, WithUpdateCurrentUserProps {
-  theme: ThemeType,
-}
-interface HeaderState {
-  navigationOpen: boolean,
-  notificationOpen: boolean,
-  notificationHasOpened: boolean,
-  searchOpen: boolean,
-  unFixed: boolean,
-}
-class Header extends PureComponent<HeaderProps,HeaderState> {
-  state: HeaderState = {
-    navigationOpen: false,
-    notificationOpen: false,
-    notificationHasOpened: false,
-    searchOpen: false,
-    unFixed: true
-  }
+  searchResultsArea: React.RefObject<HTMLDivElement>,
+  theme?: ThemeType,
+  classes: ClassesType,
+}) => {
+  const [navigationOpen, setNavigationOpenState] = useState(false);
+  const [notificationOpen, setNotificationOpen] = useState(false);
+  const [notificationHasOpened, setNotificationHasOpened] = useState(false);
+  const [searchOpen, setSearchOpenState] = useState(false);
+  const [unFixed, setUnFixed] = useState(true);
+  const currentUser = useCurrentUser();
+  const { captureEvent } = useTracking()
+  const updateCurrentUser = useUpdateCurrentUser();
 
-  setNavigationOpen = (open) => {
-    const { captureEvent } = this.props
-    this.setState({navigationOpen: open})
+  const setNavigationOpen = (open: boolean) => {
+    setNavigationOpenState(open);
     captureEvent("navigationBarToggle", {open: open})
   }
 
-  handleNotificationToggle = () => {
-    const { notificationOpen } = this.state
-    const { captureEvent, currentUser } = this.props
+  const handleSetNotificationDrawerOpen = (isOpen: boolean): void => {
+    if (!currentUser) return;
+    if (isOpen) {
+      void updateCurrentUser({lastNotificationsCheck: new Date()});
+      setNotificationOpen(true);
+      setNotificationHasOpened(true);
+    } else {
+      setNotificationOpen(false);
+    }
+  }
+
+  const handleNotificationToggle = () => {
     if (!currentUser) return;
     const { lastNotificationsCheck } = currentUser
 
     captureEvent("notificationsIconToggle", {open: !notificationOpen, previousCheck: lastNotificationsCheck})
-    this.handleSetNotificationDrawerOpen(!notificationOpen);
-  }
-
-  handleSetNotificationDrawerOpen = (isOpen: boolean): void => {
-    const { updateCurrentUser, currentUser } = this.props;
-    if (!currentUser) return;
-    if (isOpen) {
-      void updateCurrentUser({lastNotificationsCheck: new Date()});
-      this.setState({
-        notificationOpen: true,
-        notificationHasOpened: true
-      })
-    } else {
-      this.setState({notificationOpen: false})
-    }
+    handleSetNotificationDrawerOpen(!notificationOpen);
   }
 
   // We do two things when the search is open:
@@ -199,17 +186,12 @@ class Header extends PureComponent<HeaderProps,HeaderState> {
   //  2) Hide the username on mobile so users with long usernames can still
   //     enter search queries
   // Called by SearchBar.
-  setSearchOpen = (isOpen) => {
-    const { captureEvent } = this.props
+  const setSearchOpen = (isOpen: boolean) => {
     if (isOpen) { captureEvent("searchToggle", {"open": isOpen}) }
-    this.setState({
-      searchOpen: isOpen
-    });
+    setSearchOpenState(isOpen);
   }
 
-  renderNavigationMenuButton = () => {
-    const {standaloneNavigationPresent, toggleStandaloneNavigation, classes, toc} = this.props
-    const { unFixed } = this.state
+  const renderNavigationMenuButton = () => {
     return <React.Fragment>
       <IconButton
         className={classNames(
@@ -218,19 +200,17 @@ class Header extends PureComponent<HeaderProps,HeaderState> {
         )}
         color="inherit"
         aria-label="Menu"
-        onClick={()=>this.setNavigationOpen(true)}
+        onClick={()=>setNavigationOpen(true)}
       >
-      {/* Show the ToC icon if there's a table of contents being displayed  */}
-        {toc && toc.sections ? (
-          <span>
-            <div className={classes.hideSmDown}>
-              <MenuIcon />
-            </div>
-            <div className={classes.hideMdUp}>
-              <TocIcon />
-            </div>
-          </span>
-        ) : <MenuIcon />}
+        {/* Show the ToC icon if there's a table of contents being displayed  */}
+        {toc?.sections ? <span>
+          <div className={classes.hideSmDown}>
+            <MenuIcon />
+          </div>
+          <div className={classes.hideMdUp}>
+            <TocIcon />
+          </div>
+        </span> : <MenuIcon />}
       </IconButton>
       {standaloneNavigationPresent && unFixed && <IconButton
         className={classNames(
@@ -246,86 +226,89 @@ class Header extends PureComponent<HeaderProps,HeaderState> {
     </React.Fragment>
   }
 
-  render() {
-    const { currentUser, classes, theme, toc, searchResultsArea } = this.props
-    const { notificationOpen, notificationHasOpened, navigationOpen, searchOpen } = this.state
-    const notificationTerms: NotificationsViewTerms = {view: 'userNotifications', userId: currentUser ? currentUser._id : "", type: "newMessage"}
-    const hasLogo = forumTypeSetting.get() === 'EAForum'
-
-    const {
-      SearchBar, UsersMenu, UsersAccountMenu, NotificationsMenuButton, NavigationDrawer,
-      NotificationsMenu, KarmaChangeNotifier, HeaderSubtitle, Typography
-    } = Components;
-
-    return (
-        <AnalyticsContext pageSectionContext="header">
-          <div className={classes.root}>
-            <Headroom
-              disableInlineStyles
-              downTolerance={10} upTolerance={10}
-              height={64}
-              className={classNames(
-                classes.headroom,
-                { [classes.headroomPinnedOpen]: searchOpen }
-              )}
-              onUnfix={() => this.setState({unFixed: true})}
-              onUnpin={() => this.setState({unFixed: false})}
-            >
-              <AppBar className={classes.appBar} position="static" color={theme.palette.headerType || "default"}>
-                  <Toolbar disableGutters={forumTypeSetting.get() === 'EAForum'}>
-                  {this.renderNavigationMenuButton()}
-                  <Typography className={classes.title} variant="title">
-                    <div className={classes.hideSmDown}>
-                      <div className={classes.titleSubtitleContainer}>
-                        <Link to="/" className={classes.titleLink}>
-                          {hasLogo && <div className={classes.siteLogo}><Components.SiteLogo/></div>}
-                          {forumHeaderTitleSetting.get()}
-                        </Link>
-                        <HeaderSubtitle />
-                      </div>
-                    </div>
-                    <div className={classes.hideMdUp}>
-                      <Link to="/" className={classes.titleLink}>
-                        {hasLogo && <div className={classes.siteLogo}><Components.SiteLogo/></div>}
-                        {forumShortTitleSetting.get()}
-                      </Link>
-                    </div>
-                  </Typography>
-                  <div className={classes.rightHeaderItems}>
-                    <NoSSR onSSR={<div className={classes.searchSSRStandin} />} >
-                      <SearchBar onSetIsActive={this.setSearchOpen} searchResultsArea={searchResultsArea} />
-                    </NoSSR>
-                    {currentUser && <div className={searchOpen ? classes.hideOnMobile : undefined}>
-                        <AnalyticsContext pageSectionContext="usersMenu">
-                          <UsersMenu color={getHeaderTextColor(theme)} />
-                        </AnalyticsContext>
-                      </div>}
-                    {!currentUser && <UsersAccountMenu color={getHeaderTextColor(theme)} />}
-                    {currentUser && <KarmaChangeNotifier documentId={currentUser._id}/>}
-                    {currentUser && <NotificationsMenuButton color={getHeaderTextColor(theme)} toggle={this.handleNotificationToggle} terms={{view: 'userNotifications', userId: currentUser._id}} open={notificationOpen}/>}
-                  </div>
-                </Toolbar>
-              </AppBar>
-              <NavigationDrawer
-                open={navigationOpen}
-                handleOpen={() => this.setNavigationOpen(true)}
-                handleClose={() => this.setNavigationOpen(false)}
-                toc={toc}
-              />
-            </Headroom>
-            {currentUser && <NotificationsMenu open={notificationOpen} hasOpened={notificationHasOpened} terms={notificationTerms} setIsOpen={this.handleSetNotificationDrawerOpen} />}
-          </div>
-        </AnalyticsContext>
-    )
+  const notificationTerms: NotificationsViewTerms = {
+    view: 'userNotifications',
+    userId: currentUser ? currentUser._id : "",
+    type: "newMessage"
   }
+  const hasLogo = forumTypeSetting.get() === 'EAForum'
+
+  const {
+    SearchBar, UsersMenu, UsersAccountMenu, NotificationsMenuButton, NavigationDrawer,
+    NotificationsMenu, KarmaChangeNotifier, HeaderSubtitle, Typography
+  } = Components;
+
+  return (
+    <AnalyticsContext pageSectionContext="header">
+      <div className={classes.root}>
+        <Headroom
+          disableInlineStyles
+          downTolerance={10} upTolerance={10}
+          height={64}
+          className={classNames(
+            classes.headroom,
+            { [classes.headroomPinnedOpen]: searchOpen }
+          )}
+          onUnfix={() => setUnFixed(true)}
+          onUnpin={() => setUnFixed(false)}
+        >
+          <AppBar className={classes.appBar} position="static" color={theme.palette.headerType || "default"}>
+            <Toolbar disableGutters={forumTypeSetting.get() === 'EAForum'}>
+              {renderNavigationMenuButton()}
+              <Typography className={classes.title} variant="title">
+                <div className={classes.hideSmDown}>
+                  <div className={classes.titleSubtitleContainer}>
+                    <Link to="/" className={classes.titleLink}>
+                      {hasLogo && <div className={classes.siteLogo}><Components.SiteLogo/></div>}
+                      {forumHeaderTitleSetting.get()}
+                    </Link>
+                    <HeaderSubtitle />
+                  </div>
+                </div>
+                <div className={classes.hideMdUp}>
+                  <Link to="/" className={classes.titleLink}>
+                    {hasLogo && <div className={classes.siteLogo}><Components.SiteLogo/></div>}
+                    {forumShortTitleSetting.get()}
+                  </Link>
+                </div>
+              </Typography>
+              <div className={classes.rightHeaderItems}>
+                <NoSSR onSSR={<div className={classes.searchSSRStandin} />} >
+                  <SearchBar onSetIsActive={setSearchOpen} searchResultsArea={searchResultsArea} />
+                </NoSSR>
+                {currentUser && <div className={searchOpen ? classes.hideOnMobile : undefined}>
+                    <AnalyticsContext pageSectionContext="usersMenu">
+                      <UsersMenu color={getHeaderTextColor(theme)} />
+                    </AnalyticsContext>
+                  </div>}
+                {!currentUser && <UsersAccountMenu color={getHeaderTextColor(theme)} />}
+                {currentUser && <KarmaChangeNotifier documentId={currentUser._id}/>}
+                {currentUser && <NotificationsMenuButton color={getHeaderTextColor(theme)} toggle={handleNotificationToggle} terms={{view: 'userNotifications', userId: currentUser._id}} open={notificationOpen}/>}
+              </div>
+            </Toolbar>
+          </AppBar>
+          <NavigationDrawer
+            open={navigationOpen}
+            handleOpen={() => setNavigationOpen(true)}
+            handleClose={() => setNavigationOpen(false)}
+            toc={toc}
+          />
+        </Headroom>
+        {currentUser && <NotificationsMenu
+          open={notificationOpen}
+          hasOpened={notificationHasOpened}
+          terms={notificationTerms}
+          setIsOpen={handleSetNotificationDrawerOpen}
+        />}
+      </div>
+    </AnalyticsContext>
+  )
 }
 
-const HeaderComponent = registerComponent<ExternalProps>('Header', Header, {
+const HeaderComponent = registerComponent('Header', Header, {
   styles,
   hocs: [
     withErrorBoundary,
-    withUpdateCurrentUser,
-    withUser, withTracking,
     withTheme(),
   ]
 });
