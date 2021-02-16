021fab9d2d2786a7b17146a92fb5b191fea73ee2
James Babcock <jimrandomh@gmail.com>
2021-02-15 20:50:08 -0800

    Fix a broken RSS link (missing slash)

--- packages/lesswrong/lib/rss_urls.ts
+++ packages/lesswrong/lib/rss_urls.ts
@@ -1,7 +1,7 @@
-import { siteUrlSetting } from './instanceSettings';
+import { getSiteUrl } from './vulcan-lib/utils';
 
 export const rssTermsToUrl = (terms) => {
-  const siteUrl = siteUrlSetting.get();
+  const siteUrl = getSiteUrl();
   const terms_as_GET_params = Object.keys(terms).map((k) => encodeURIComponent(k) + '=' + encodeURIComponent(terms[k])).join('&')
   return siteUrl+"feed.xml?"+terms_as_GET_params;
 }
