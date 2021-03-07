867ba1e2228f4aaeecc2d2e7715dd1cf072cef85
JanTrooper2 <JanGoergens@gmail.com>
2021-03-07 10:50:03 +0100

    implemented multiple conversations fix

--- packages/lesswrong/components/messaging/NewConversationButton.tsx
+++ packages/lesswrong/components/messaging/NewConversationButton.tsx
@@ -5,6 +5,8 @@ import { useNavigation } from '../../lib/routeUtil';
 import Conversations from '../../lib/collections/conversations/collection';
 import { forumTypeSetting } from '../../lib/instanceSettings';
 import qs from 'qs';
+import { useMulti } from '../../lib/crud/withMulti';
+import { Link } from '../../lib/reactRouterWrapper';
 
 // Button used to start a new conversation for a given user
 const NewConversationButton = ({ user, currentUser, children, templateCommentId }: {
@@ -29,10 +31,30 @@ const NewConversationButton = ({ user, currentUser, children, templateCommentId 
     history.push({pathname: `/inbox/${conversationId}`, ...search})
   }, [createConversation, user, currentUser, history, templateCommentId]);
 
+  // Check for already existing Conversation
+  // would like to put this in an extra function, but useMulti is not accessible from there 
+  let existingConversationObject;
+  const terms: ConversationsViewTerms = {view: 'userConversations', userId: currentUser._id, showArchive: true};
+  const { results, loading } = useMulti({  
+    terms,
+    collectionName: "Conversations",
+    fragmentName: 'conversationsListFragment',
+    fetchPolicy: 'cache-and-network',
+    limit: 200,
+  });
+  if (results != undefined){                //there is a reload happening at some point, after which the value gets assigned
+  results.forEach(conversation => {
+    if (conversation.title === null && conversation.participants.some(participant => participant._id === user._id)){
+      existingConversationObject = conversation;
+    }
+  })
+  }
+ // End of Check
+
   if (currentUser) {
     return (
-      <div onClick={newConversation}>
-        {children}
+      <div onClick={existingConversationObject === undefined ? newConversation : undefined}>
+        {existingConversationObject === undefined ? children : <Link to={`/inbox/${existingConversationObject._id}`}>Message</Link>}
       </div>
     )
   } else {
