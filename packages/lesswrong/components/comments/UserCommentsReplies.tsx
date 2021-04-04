d549c11820de185a1ce893151202fc4a4d246a28
Discordius <panisnecis@gmail.com>
2021-04-03 17:35:12 -0700

    Add a "comments and replies to comments" page

---
+++ packages/lesswrong/components/comments/UserCommentsReplies.tsx
@@ -0,0 +1,75 @@
+import { Components, registerComponent, slugify } from '../../lib/vulcan-lib';
+import React from 'react';
+import { useLocation } from '../../lib/routeUtil';
+import * as _ from 'underscore';
+import { useMulti } from '../../lib/crud/withMulti';
+import { getUserFromResults } from '../users/UsersProfile';
+
+const styles = (theme: ThemeType): JssStyles =>  ({
+  root: {
+    [theme.breakpoints.up('sm')]: {
+      marginRight: theme.spacing.unit*4,
+    }
+  }
+})
+
+const UserCommentsReplies = ({ classes }) => {
+  const { query } = useLocation();
+  const { SingleColumnSection, SectionTitle, Loading } = Components
+
+  const { params, pathname } = useLocation();
+  const slug = slugify(params.slug);
+
+  const {loading, results: userResults} = useMulti({
+    terms: {view: 'usersProfile', slug},
+    collectionName: "Users",
+    fragmentName: 'UsersProfile',
+    enableTotal: false,
+  });
+  const user = getUserFromResults(userResults)
+  const { loadingInitial, loadMoreProps, results } = useMulti({
+    terms: {view: 'allRecentComments', authorIsUnreviewed: null, limit: 50, userId: user?._id},
+    collectionName: "Comments",
+    fragmentName: 'CommentsListWithParentMetadata',
+    enableTotal: false,
+    skip: !user
+  });
+  
+  if (loadingInitial) return <Loading />
+  if (!results || results.length < 1) return <SingleColumnSection>
+    This user has not made any comments
+  </SingleColumnSection>
+
+  return (
+    <SingleColumnSection>
+      <SectionTitle title="All Your Comments + Rplies"/>
+      <div className={classes.root}>
+        {results.map(comment =>
+          <div key={comment._id}>
+            <Components.CommentsNode
+              treeOptions={{
+                condensed: false,
+                post: comment.post || undefined,
+                showPostTitle: true,
+              }}
+              comment={comment}
+              startThreadTruncated={true}
+              forceNotSingleLine
+              loadChildrenSeparately
+              loadDirectReplies
+            />
+          </div>
+        )}
+        <Components.LoadMore {...loadMoreProps} />
+      </div>
+    </SingleColumnSection>
+  )
+};
+
+const UserCommentsRepliesComponent = registerComponent('UserCommentsReplies', UserCommentsReplies, { styles });
+
+declare global {
+  interface ComponentTypes {
+    UserCommentsReplies: typeof UserCommentsRepliesComponent,
+  }
+}
