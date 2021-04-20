69585cc1d2aa8f8b2a624f1036ac33c8fd670f02
JP Addison <johnpaddison@gmail.com>
2021-04-20 15:31:18 +0100

    add transcrypt decrypt to deploy, use jpaddison as github user

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,8 @@
 FROM node:15.5.0
+# Transcrypt dependency
+RUN apt-get update && apt-get install -y bsdmainutils
+# Install transcrypt for EA Forum
+RUN curl -sSLo /usr/local/bin/transcrypt https://raw.githubusercontent.com/elasticdog/transcrypt/v2.1.0/transcrypt && chmod +x /usr/local/bin/transcrypt
 WORKDIR /usr/src/app
 COPY ./ ./
 RUN yarn
--- scripts/runProduction.sh
+++ scripts/runProduction.sh
@@ -1,4 +1,10 @@
 echo "Running Production Site"
-git clone https://Discordius:$GITHUB_CREDENTIALS_REPO_PAT@github.com/$GITHUB_CREDENTIALS_REPO_NAME.git Credentials 
+
+git clone https://jpaddison3:$GITHUB_CREDENTIALS_REPO_PAT@github.com/$GITHUB_CREDENTIALS_REPO_NAME.git Credentials
+
+# Decrypt credentials
+cd Credentials
+transcrypt -c aes-256-cbc -p $TRANSCRYPT_SECRET -y
+cd ..
 
 ./build.js -run --settings ./Credentials/$SETTINGS_FILE_NAME --production
