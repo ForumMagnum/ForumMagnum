78f75fd7bc972e9ae9408bcc8bcf10249b0ca811
JP Addison <johnpaddison@gmail.com>
2021-05-27 11:47:59 +0100

    Revert "types are good, pre-testing checkpoint" - unneccessary
    This reverts commit ab0cd3c92fe187e54466250b11227a59cdf532e8.

--- packages/lesswrong/server/authentication/auth0Accounts.ts
+++ packages/lesswrong/server/authentication/auth0Accounts.ts
@@ -2,55 +2,10 @@ import { Profile } from "passport-auth0";
 import Users from "../../lib/vulcan-users";
 import { slugify, Utils } from "../../lib/vulcan-lib/utils";
 import { updateMutator } from "../vulcan-lib/mutators";
-import { ManagementClient } from 'auth0'
-import { DatabaseServerSetting } from "../databaseSettings";
 
-export const auth0ClientIdSetting = new DatabaseServerSetting<string | null>('oAuth.auth0.appId', null)
-export const auth0SecretSetting = new DatabaseServerSetting<string | null>('oAuth.auth0.secret', null)
-export const auth0DomainSetting = new DatabaseServerSetting<string | null>('oAuth.auth0.domain', null)
-
-let auth0Client: ManagementClient
-
-/**
- * TODO;
- */
-function getAuth0Client() {
-  if (auth0Client) return auth0Client
-  const auth0ClientId = auth0ClientIdSetting.get()
-  const auth0Secret = auth0SecretSetting.get()
-  const auth0Domain = auth0DomainSetting.get()
-  if (!auth0ClientId || !auth0Secret || !auth0Domain) {
-    throw new Error('Cannot create Auth0 management client without required settings')
-  }
-  auth0Client = new ManagementClient({
-    domain: auth0Domain,
-    clientId: auth0ClientId,
-    clientSecret: auth0Secret
-  })
-  return auth0Client
-}
-
-async function userEmailVerified(profile: Profile) {
-  const auth0 = getAuth0Client()
-  const auth0User = await auth0.getUser({id: profile.id})
-  return auth0User.email_verified
-}
-
-// type MergeAccountResult = {
-//   error: Error,
-//   updatedUser: null
-// } | {
-//   error: null,
-//   updatedUser: DbUser
-// }
-
-export async function mergeAccountWithAuth0(user: DbUser, profile: Profile): Promise<DbUser> {
-  const verified = await userEmailVerified(profile)
-  if (!verified) {
-    throw new Error('Your EffectiveAltruism.org account email is unverified, cannot link with your Forum account')
-  }
+export async function mergeAccountWithAuth0(user: DbUser, profile: Profile) {
   delete profile._json
-  const {data: updatedUser} = await updateMutator({
+  return await updateMutator({
     collection: Users,
     documentId: user._id,
     // Annoying that typescript is concerned - `services` is a valid property
@@ -60,7 +15,6 @@ export async function mergeAccountWithAuth0(user: DbUser, profile: Profile): Pro
     validate: false
     // TODO: Soon we should delete passwords when we do this
   })
-  return updatedUser
 }
 
 export async function userFromAuth0Profile(profile: Profile): Promise<Partial<DbUser>> {
