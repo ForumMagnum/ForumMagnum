81bb642e306994bdc76b58841860d3fcb3767c19
JP Addison <johnpaddison@gmail.com>
2021-06-05 16:24:26 +0100

    overeager commit-push

--- packages/lesswrong/server/authenticationMiddlewares.ts
+++ packages/lesswrong/server/authenticationMiddlewares.ts
@@ -156,7 +156,6 @@ export const addAuthMiddlewares = (addConnectHandler) => {
       services: {
         google: profile
       },
-      // todo;
       emails: [{address: profile.emails?.[0].value, verified: true}],
       username: await Utils.getUnusedSlugByCollectionName("Users", slugify(profile.displayName)),
       displayName: profile.displayName,
