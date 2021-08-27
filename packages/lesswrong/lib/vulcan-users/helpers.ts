b9168c75b796bd243c6650ddf1cca31996a0c72e
Ruben Bloom <rmb042@gmail.com>
2021-08-27 10:40:53 -0700

    Keep a few changes introduced in PR

--- packages/lesswrong/lib/vulcan-users/helpers.ts
+++ packages/lesswrong/lib/vulcan-users/helpers.ts
@@ -73,5 +73,8 @@ export const userNumberOfItemsInPast24Hours = async function<T extends DbObject>
 };
 
 export const userFindByEmail = async function(email: string): Promise<DbUser|null> {
-  return await mongoFindOne("Users", { email: email });
+  const userByPlainEmail = await mongoFindOne("Users", { email: email });
+  if (userByPlainEmail) return userByPlainEmail;
+  const userByEmailsArray = await mongoFindOne("Users", { 'emails.address': email });
+  return userByEmailsArray;
 };
