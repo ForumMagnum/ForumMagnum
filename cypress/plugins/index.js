bba6addfec7d71d2b64169cd2e018de34bfc4015
jcmustin <jcmustin@gmail.com>
2021-08-29 18:08:17 -0700

    Add login test

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,22 +1,73 @@
 /// <reference types="cypress" />
-// ***********************************************************
-// This example plugins/index.js can be used to load plugins
-//
-// You can change the location of this file or turn off loading
-// the plugins file with the 'pluginsFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/plugins-guide
-// ***********************************************************
 
-// This function is called when a project is opened or re-opened (e.g. due to
-// the project's config changing)
+const { MongoClient, ObjectId } = require('mongodb');
+const { createHash } = require('crypto');
+const testAdmin = require('../fixtures/testAdmin.json');
+const testUser = require('../fixtures/testUser.json');
+const seedPosts = require('../fixtures/posts.json');
+const TEST_USER_LOGIN_TOKEN = "1234";
+const TEST_ADMIN_LOGIN_TOKEN = "0123";
+
+function hashLoginToken(loginToken) {
+  const hash = createHash('sha256');
+  hash.update(loginToken);
+  return hash.digest('base64');
+};
 
 /**
  * @type {Cypress.PluginConfig}
  */
 // eslint-disable-next-line no-unused-vars
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
-}
+  on('task', {
+    async dropAndSeedDatabase() {
+      MongoClient.connect("mongodb://localhost:27017", async function(err, client) {
+        if(err) {
+          console.error(err);
+          return;
+        }
+        await client.db().dropDatabase();
+      });
+      return null;
+    },
+    async createUser({user, loginToken}) {
+      MongoClient.connect("mongodb://localhost:27017", async function(err, client) {
+        if(err) {
+          console.error(err);
+          return;
+        }
+        const db = await client.db();
+        const insertedUser = await db.collection('users').insertOne(user);
+        await db.collection('users').updateOne({_id: insertedUser.insertedId}, {
+          $addToSet: {
+            "services.resume.loginTokens": {
+              when: new Date(),
+              hashedToken: hashLoginToken(loginToken),
+            },
+          }
+        });
+      });
+      return null;
+    },
+    createPost(title, body) {
+      MongoClient.connect(process.env.MONGO_URL, function(err, client) {
+        if(err) {
+          console.log(err);
+          return;
+        }
+        const db = client.db();
+        db.collection('users').updateOne({username: testAdmin.username}, {
+          $addToSet: {
+            "services.resume.loginTokens": TEST_ADMIN_LOGIN_TOKEN,
+          }
+        });
+        db.collection('users').updateOne({username: testUser.username}, {
+        $addToSet: {
+            "services.resume.loginTokens": TEST_USER_LOGIN_TOKEN,
+          }
+        });
+      });
+      return null;
+    }
+  });
+};
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,34 +1,8 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add("login", (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This is will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+/// <reference types="Cypress" />
+const crypto = require('crypto');
 
-Cypress.Commands.add('login', () => {
-  cy.setCookie('loginToken', '8d81affa90854269016c7f64fb5742f10694d7677e29c3c0a1d1059e32ea228a');
+Cypress.Commands.add('loginAs', user => {
+  const loginToken = crypto.randomBytes(12).toString('base64');
+  cy.task('createUser', {user, loginToken});
+  cy.setCookie('loginToken', loginToken);
 })
-
-Cypress.Commands.add('loginAdmin', () => {
-  cy.setCookie('loginToken', 'b9b019d99f015e68f07db28e3c2c4ce3ef4a2fdfc534c12d1ccdc66db6c9a034');
-})
-
