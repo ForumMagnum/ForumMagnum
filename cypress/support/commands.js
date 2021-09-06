565ba70d800d13d6975701d9788d0d363c32e254
jcmustin <jcmustin@gmail.com>
2021-09-06 18:48:10 +0100

    Add tests for posts and comments

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -5,4 +5,4 @@ Cypress.Commands.add('loginAs', user => {
   const loginToken = crypto.randomBytes(12).toString('base64');
   cy.task('createUser', {user, loginToken});
   cy.setCookie('loginToken', loginToken);
-})
+});
