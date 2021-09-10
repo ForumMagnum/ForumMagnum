db8816f10dfee2231791ac1877a7d2d4bf8483a1
jcmustin <jcmustin@gmail.com>
2021-09-10 12:04:37 +0100

    Add comment explaining test users

--- cypress/fixtures/users/index.js
+++ cypress/fixtures/users/index.js
@@ -2,6 +2,11 @@ const freshTestUser = require('./freshTestUser.json');
 const testUserWithContent = require('./testUserWithContent.json');
 const testAdmin = require('./testAdmin.json');
 
+/*
+The reason for a user with content and a user without is rate limiting.
+With the database having just been seeded, a user with e.g. existing posts
+can't create a new one without waiting about ten seconds.
+*/
 module.exports = [
   freshTestUser,
   testUserWithContent,
