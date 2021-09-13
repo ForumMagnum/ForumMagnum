19a9d9e6ed4e53e4e3a4a3668cdfe8b51e4a864c
jcmustin <jcmustin@gmail.com>
2021-09-13 22:05:05 +0100

    Add permissions

--- packages/lesswrong/lib/collections/featuredResources/schema.ts
+++ packages/lesswrong/lib/collections/featuredResources/schema.ts
@@ -1,21 +1,33 @@
 const schema: SchemaType<DbFeaturedResource> = {
   title: {
     type: String,
+    canRead: ['guests'],
+    canUpdate: ['sunshineRegiment', 'admins'],
   },
   body: {
     type: String,
+    canRead: ['guests'],
+    canUpdate: ['sunshineRegiment', 'admins'],
   },
   ctaText: {
     type: String,
+    canRead: ['guests'],
+    canUpdate: ['sunshineRegiment', 'admins'],
   },
   ctaUrl: {
     type: String,
+    canRead: ['guests'],
+    canUpdate: ['sunshineRegiment', 'admins'],
   },
   isActive: {
     type: Boolean,
+    canRead: ['guests'],
+    canUpdate: ['sunshineRegiment', 'admins'],
   },
   expiresAt: {
     type: Date,
+    canRead: ['guests'],
+    canUpdate: ['sunshineRegiment', 'admins'],
     optional: true,
   },
 }
