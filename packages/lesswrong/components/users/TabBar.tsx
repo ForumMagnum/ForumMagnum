29211b2860ec2f7181eaf6485564c7cef072d3e0
James Babcock <jimrandomh@gmail.com>
2021-09-13 15:12:37 -0700

    [WIP] Small-screen support for settings section sidebar

--- packages/lesswrong/components/users/TabBar.tsx
+++ packages/lesswrong/components/users/TabBar.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { Components, registerComponent } from '../../lib/vulcan-lib';
+import { useLocation, useNavigation } from '../../lib/routeUtil';
 import classNames from 'classnames';
 import Drawer from '@material-ui/core/Drawer';
 import MenuItem from '@material-ui/core/MenuItem';
+import * as _ from 'underscore';
 
 const styles = (theme: ThemeType): JssStyles => ({
   tabDrawerRoot: {
@@ -27,16 +29,36 @@ const TabBar = ({currentTab, setCurrentTab, tabs, children, classes}: {
   children: React.ReactNode,
   classes: ClassesType,
 }) => {
+  const { history } = useNavigation();
+  const { location } = useLocation();
+  const { hash } = location;
+  const [smallScreenMode, setSmallScreenMode] = useState(false);
+  const hashIsValidTab = _.any(tabs, tab=>tab.name===hash);
+  
+  useEffect(() => {
+    const windowIsNarrow = window && window.innerWidth<600;
+    if (smallScreenMode != windowIsNarrow)
+      setSmallScreenMode(windowIsNarrow);
+    if (smallScreenMode && hash && currentTab!==hash && hashIsValidTab) {
+      setCurrentTab(hash);
+    }
+  }, [smallScreenMode, hash, currentTab, setCurrentTab, hashIsValidTab]);
+  
   return <div>
     <Drawer
       variant="persistent"
       anchor="left"
-      open={true}
+      open={!smallScreenMode || !hash}
     >
       <div className={classes.tabsDrawer}>
         {tabs.map(tab => <MenuItem
           key={tab.name}
-          onClick={ev => setCurrentTab(tab.name)}
+          onClick={ev => {
+            if (smallScreenMode) {
+              history.push({...location, hash: tab.name});
+            }
+            setCurrentTab(tab.name)
+          }}
           selected={tab.name===currentTab}
         >
           {tab.label}
