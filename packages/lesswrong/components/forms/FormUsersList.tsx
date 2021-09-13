8240c2d94d96ba690ef9902265759d86ab01b6cc
James Babcock <jimrandomh@gmail.com>
2021-09-13 15:00:07 -0700

    Bring user list editor into new forms system, refactor form list-editors

--- packages/lesswrong/components/forms/FormUsersList.tsx
+++ packages/lesswrong/components/forms/FormUsersList.tsx
@@ -1,9 +1,29 @@
 import React from 'react';
-import { registerComponent, useStyles } from '../../lib/vulcan-lib/components';
+import { registerComponent, Components, useStyles } from '../../lib/vulcan-lib/components';
 import { useFormComponentContext, formCommonStyles, LWForm } from './formUtil';
+import { makeSortableListComponent } from './sortableList';
+import * as _ from 'underscore';
 
 const styles = (theme: ThemeType): JssStyles => ({
   ...formCommonStyles(theme),
+  
+  listEditor: {
+    display: "flex"
+  },
+  list: {
+    display: "flex",
+    flexWrap: "wrap"
+  },
+  item: {
+    listStyle: "none",
+    fontFamily: theme.typography.fontFamily
+  },
+});
+
+const SortableList = makeSortableListComponent({
+  renderItem: ({contents, removeItem, classes}) => <li className={classes.item}>
+    <Components.SingleUsersItemWrapper documentId={contents} removeItem={removeItem} />
+  </li>
 });
 
 export function FormUsersList<T, FN extends keyof T>({form, fieldName, label}: {
@@ -13,9 +33,29 @@ export function FormUsersList<T, FN extends keyof T>({form, fieldName, label}: {
 }) {
   const classes = useStyles(styles, "FormUsersList");
   const {value,setValue} = useFormComponentContext<string[],T>(form, fieldName);
+  const {ErrorBoundary, UsersSearchAutoComplete} = Components;
+  
   return <div className={classes.formField}>
-    {label}
-    { /*TODO*/ }
+    <span className={classes.leftColumn}>{label}</span>
+    <span className={classes.rightColumn}>
+      <span className={classes.listEditor}>
+        <ErrorBoundary>
+          <UsersSearchAutoComplete
+            clickAction={(added: string) => {
+              setValue([...value, added]);
+            }}
+            label={label}
+          />
+        </ErrorBoundary>
+        <SortableList
+          value={value}
+          setValue={setValue}
+          axis="xy"
+          className={classes.list}
+          classes={classes}
+        />
+      </span>
+    </span>
   </div>
 }
 
