bd8c034d91771d33612581c6c6b77172091b549b
Ben West <Xodarap@users.noreply.github.com>
2021-09-15 13:27:46 -0700

    working!

--- packages/lesswrong/lib/generated/databaseTypes.d.ts
+++ packages/lesswrong/lib/generated/databaseTypes.d.ts
@@ -289,6 +289,7 @@ interface DbPetrovDayLaunch extends DbObject {
   createdAt: Date
   launchCode: string
   hashedLaunchCode: string
+  userId: string
 }
 
 interface PostRelationsCollection extends CollectionBase<DbPostRelation, "PostRelations"> {
