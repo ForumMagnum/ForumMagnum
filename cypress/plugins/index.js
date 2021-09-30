1e9e8fec2494996e92abb99a4ded6372ec0fabba
jcmustin <jonathan.mustin@gmail.com>
2021-09-30 15:30:28 -0700

    Add clarifying comment about MongoClient callback

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -15,6 +15,22 @@ function hashLoginToken(loginToken) {
 };
 
 /**
+ * Note: 
+ * There are 2 broad ways to connect MongoClient to a database:
+ * 1. using a callback, e.g. MongoClient.connect(url, (err, client) => {...})
+ * 2. using a promise, e.g. const client = await MongoClient.connect(url)
+ * 
+ * For reasons I haven't pinned down, the callback version fails in the current
+ * setup, despite previously working in a prior implementation that used an
+ * in-memory DB. In particular, the associateLoginToken task fails to update
+ * the document of the current user.
+ * 
+ * Note that there are also instance method versions of both approaches above:
+ * 1. (new MongoClient(url)).connect((err, client) => {...})
+ * 2. const client = await (new Mongoclient(url)).connect()
+ * These show the same behavior: the callback version fails, and the promise version succeeds.
+ * 
+ * 
  * @type {Cypress.PluginConfig}
  */
 // eslint-disable-next-line no-unused-vars
@@ -36,8 +52,11 @@ module.exports = (on, config) => {
           lastSubthreadActivity: now,
         }));
       const client = new MongoClient(process.env.TESTING_DB_URL);
-      try{
-        await client.connect();
+      await client.connect(async (err, client) => {
+        if(err) {
+          console.error(err);
+          return;
+        }
         const isProd = (await client.db().collection('databasemetadata').findOne({name: 'publicSettings'}))?.value?.isProductionDB;
         if(isProd) {
           throw new Error('Cannot run tests on production DB.');
@@ -47,18 +66,17 @@ module.exports = (on, config) => {
         await db.collection('posts').insertMany(postsWithDates);
         await db.collection('comments').insertMany(commentsWithDates);
         await db.collection('users').insertMany(seedUsers);
-      } catch(err) {
-        console.error(err);
-        return undefined; //  Cypress tasks use undefined to signal failure (https://docs.cypress.io/api/commands/task#Usage)
-      } finally {
         await client.close();
-      }
+      });
       return null;
     },
     async associateLoginToken({user, loginToken}) {
       const client = new MongoClient(process.env.TESTING_DB_URL);
-      try{
-        await client.connect();
+      await client.connect(async (err, client) => {
+        if(err) {
+          console.error(err);
+          return;
+        }
         const db = await client.db();
         await db.collection('users').updateOne({username: user.username}, {
           $addToSet: {
@@ -68,12 +86,8 @@ module.exports = (on, config) => {
             },
           },
         });
-      } catch(err) {
-        console.error(err);
-        return undefined; // Cypress tasks use undefined to signal failure (https://docs.cypress.io/api/commands/task#Usage)
-      } finally {
-        await client.close();
-      }
+        await client.close()
+      });
       return null;
     },
   });
