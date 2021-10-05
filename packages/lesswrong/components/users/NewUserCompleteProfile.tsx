cff5b90b5ad2f61df1ab8cbfb0e3f5eb530ddeef
JP Addison <johnpaddison@gmail.com>
2021-10-05 10:43:10 -0400

    address pr comments

--- packages/lesswrong/components/users/NewUserCompleteProfile.tsx
+++ packages/lesswrong/components/users/NewUserCompleteProfile.tsx
@@ -3,7 +3,7 @@ import Button from "@material-ui/core/Button";
 import Checkbox from "@material-ui/core/Checkbox";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import TextField from "@material-ui/core/TextField";
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { forumTypeSetting, siteNameWithArticleSetting } from "../../lib/instanceSettings";
 import { Components, registerComponent } from "../../lib/vulcan-lib";
 import { useMessages } from "../common/withMessages";
@@ -44,7 +44,7 @@ function prefillUsername(maybeUsername: string | undefined | null): string {
 const NewUserCompleteProfile: React.FC<NewUserCompleteProfileProps> = ({ classes }) => {
   const currentUser = useCurrentUser()
   const [username, setUsername] = useState(prefillUsername(currentUser?.displayName))
-  const [newEmail, setNewEmail] = useState('')
+  const emailInput = useRef<HTMLInputElement>(null)
   const [subscribeToDigest, setSubscribeToDigest] = useState(false)
   const [validationError, setValidationError] = useState('')
   const [updateUser] = useMutation(gql`
@@ -86,8 +86,8 @@ const NewUserCompleteProfile: React.FC<NewUserCompleteProfileProps> = ({ classes
         subscribeToDigest,
         // We do this fancy spread so we avoid setting the email to an empty
         // string in the likely event that someone already had an email and
-        // wasn't show the set email field
-        ...(newEmail && {email: newEmail})
+        // wasn't shown the set email field
+        ...(!currentUser?.email && {email: emailInput.current?.value})
       }})
     } catch (err) {
       if (/duplicate key error/.test(err.toString?.())) {
@@ -100,7 +100,7 @@ const NewUserCompleteProfile: React.FC<NewUserCompleteProfileProps> = ({ classes
   }
   
   return <SingleColumnSection>
-      <div className={classes.root}>
+    <div className={classes.root}>
       <Typography variant='display3' gutterBottom className={classes.title}>
         Thanks for registering for {siteNameWithArticleSetting.get()}
       </Typography>
@@ -139,7 +139,8 @@ const NewUserCompleteProfile: React.FC<NewUserCompleteProfileProps> = ({ classes
         </Typography>
         <TextField
           label='Email'
-          onChange={(event) => setNewEmail(event.target.value)}
+          inputRef={emailInput}
+          // onChange={(event) => setNewEmail(event.target.value)}
         />
       </div>}
       
