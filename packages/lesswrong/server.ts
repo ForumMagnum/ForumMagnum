653c2b4201fec46505413fe03a40e21a2da411a6
JP Addison <johnpaddison@gmail.com>
2021-10-06 18:06:27 -0400

    fill emails messed up by bug

--- packages/lesswrong/server.ts
+++ packages/lesswrong/server.ts
@@ -32,6 +32,7 @@ import './server/scripts/debuggingScripts';
 import './server/scripts/rerunAFvotes';
 import './server/scripts/nullifyVotes';
 import './server/scripts/fixSSCDrafts';
+import './server/scripts/fillUserEmail';
 
 import './server/scripts/oneOffBanSpammers'
 import './server/scripts/ensureEmailInEmails';
