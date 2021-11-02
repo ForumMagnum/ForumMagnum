19e2e66373fee00be43d163e48b84ca270304149
JP Addison <johnpaddison@gmail.com>
2021-11-02 12:08:34 +0000

    deployment handles both LW and EA Forum

--- scripts/runProduction.sh
+++ scripts/runProduction.sh
@@ -1,10 +1,10 @@
 echo "Running Production Site"
 
 # lw-look here: you must define GITHUB_CREDENTIALS_REPO_USER in your AWS EBS config
-git clone https://GITHUB_CREDENTIALS_REPO_USER:$GITHUB_CREDENTIALS_REPO_PAT@github.com/$GITHUB_CREDENTIALS_REPO_NAME.git Credentials
+git clone https://$GITHUB_CREDENTIALS_REPO_USER:$GITHUB_CREDENTIALS_REPO_PAT@github.com/$GITHUB_CREDENTIALS_REPO_NAME.git Credentials
 
 # Decrypt credentials if encrypted
-if [ -z $TRANSCRYPT_SECRET ]; then
+if [ -n $TRANSCRYPT_SECRET ]; then
     cd Credentials
     transcrypt -c aes-256-cbc -p $TRANSCRYPT_SECRET -y
     cd ..
@@ -12,4 +12,3 @@ fi
 
 export NODE_OPTIONS="--max_old_space_size=2560 --heapsnapshot-signal=SIGUSR2"
 ./build.js -run --settings ./Credentials/$SETTINGS_FILE_NAME --production
-
