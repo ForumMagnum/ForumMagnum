ff3ad5d55db5cd88671407f4d5d0131b8e3d138d
Sarah Cheng <sarah.cheng@centreforeffectivealtruism.org>
2021-11-08 18:06:20 -0500

    small code cleanup

--- packages/lesswrong/server/callbacks/localgroupCallbacks.ts
+++ packages/lesswrong/server/callbacks/localgroupCallbacks.ts
@@ -1,14 +1,14 @@
 import { getCollectionHooks } from '../mutationCallbacks';
 
 
-getCollectionHooks("Localgroups").createValidate.add(async (validationErrors: Array<any>, {document: group}: {document: DbLocalgroup}) => {
+getCollectionHooks("Localgroups").createValidate.add((validationErrors: Array<any>, {document: group}: {document: DbLocalgroup}) => {
   if (!group.isOnline && !group.location)
     throw new Error("Location is required for local groups");
   
   return validationErrors;
 });
 
-getCollectionHooks("Localgroups").updateValidate.add(async (validationErrors: Array<any>, {oldDocument, newDocument}: {oldDocument: DbLocalgroup, newDocument: DbLocalgroup}) => {
+getCollectionHooks("Localgroups").updateValidate.add((validationErrors: Array<any>, {oldDocument, newDocument}: {oldDocument: DbLocalgroup, newDocument: DbLocalgroup}) => {
   if (!newDocument.isOnline && !newDocument.location)
     throw new Error("Location is required for local groups");
   
