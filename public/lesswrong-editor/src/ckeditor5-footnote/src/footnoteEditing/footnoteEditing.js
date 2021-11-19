2306252c5f8040b97f58033cc3161e9372837874
jcmustin <jonathan.mustin@gmail.com>
2021-11-19 15:17:11 -0800

    Add documentation to footnoteEditing.

--- public/lesswrong-editor/src/ckeditor5-footnote/src/footnoteEditing/footnoteEditing.js
+++ public/lesswrong-editor/src/ckeditor5-footnote/src/footnoteEditing/footnoteEditing.js
@@ -11,7 +11,7 @@ import InsertFootnoteCommand from '../insertfootnotecommand';
 import '../../theme/placeholder.css';
 import '../../theme/footnote.css';
 import ModelElement from '@ckeditor/ckeditor5-engine/src/model/element';
-import ViewElement from '@ckeditor/ckeditor5-engine/src/view/element';
+import RootElement from '@ckeditor/ckeditor5-engine/src/model/rootelement';
 import DocumentFragment from '@ckeditor/ckeditor5-engine/src/model/documentfragment';
 import { modelQueryElement, modelQueryElementsAll } from '../utils';
 import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
@@ -26,6 +26,9 @@ export default class FootnoteEditing extends Plugin {
 		return [ Widget, Autoformat ];
 	}
 
+	/**
+	 * @type {RootElement} The root element of the document.
+	 */
 	get rootElement() {
 		const rootElement = this.editor.model.document.getRoot();
 		if(!rootElement) {
@@ -42,8 +45,10 @@ export default class FootnoteEditing extends Plugin {
 
 		addFootnoteAutoformatting(this.editor, this.rootElement);
 
-		this._deleteModify();
+		this._handleDelete();
 
+		// The following callbacks are needed to map nonempty view elements
+		// to empty model elements. See https://ckeditor.com/docs/ckeditor5/latest/api/module_widget_utils.html#function-viewToModelPositionOutsideModelElement
 		this.editor.editing.mapper.on(
 			'viewToModelPosition',
 			// @ts-ignore -- the type signature of `on` here seem to be just wrong, given how it's used in the source code. 
@@ -56,58 +61,77 @@ export default class FootnoteEditing extends Plugin {
 		);
 	}
 
-	_deleteModify() {
+	/**
+	 * This method broadly deals with deletion of text, and updating the model
+	 * accordingly.
+	 * 1. If the footer footnotes section is deleted, all footnote references are removed.
+	 * 2. To avoid accidental deletion of content, deleting the number on a footnote while the
+	 * footnote still contains text will do nothing.
+	 * 3. 
+	 */
+	_handleDelete() {
 		const viewDocument = this.editor.editing.view.document;
 		const editor = this.editor;
 		this.listenTo( viewDocument, 'delete', (evt, data) => {
 			const doc = editor.model.document;
-			const deleteEle = doc.selection.getSelectedElement();
-			const lastPosition = doc.selection.getLastPosition();
+			const deletedElement = doc.selection.getSelectedElement();
+			const selectionEndpoint = doc.selection.getLastPosition();
 			if(!doc.selection.anchor || 
 				!doc.selection.focus || 
-				!lastPosition) {
+				!selectionEndpoint) {
 				throw new Error('Selection must have at least one range to perform delete operation.');
 			}
-			const positionParent = lastPosition.parent;
 		
-			// delete all footnoteReference references if footnotes section gets deleted
-			if (deleteEle !== null && deleteEle.name === ELEMENTS.footnoteSection) {
-				this._removeHolder(0);
+			// delete all footnote references if footnote section gets deleted
+			if (deletedElement !== null && deletedElement.name === ELEMENTS.footnoteSection) {
+				this._removeReferences(0);
 			}
 
-			if (!positionParent || positionParent.parent instanceof DocumentFragment || !positionParent.parent || positionParent.parent.name !== ELEMENTS.footnoteList) {
+			// selectionParent is the element containing the selection's endpoint.
+			const selectionParent = selectionEndpoint.parent;
+
+			// return unless the current selection is inside a footnoteList element.
+			if (!selectionParent || selectionParent.parent instanceof DocumentFragment || !selectionParent.parent || selectionParent.parent.name !== ELEMENTS.footnoteList) {
 				return;
 			}
 
 			// don't allow deleting a nonempty footnote without deleting text
-			if (positionParent.maxOffset > 1 && doc.selection.anchor.offset <= 1) {
+			if (selectionParent.maxOffset > 1 && doc.selection.anchor.offset <= 1) {
 				data.preventDefault();
 				evt.stop();
 			}
 
-			const entireParagraphSelected = (positionParent.maxOffset === doc.selection.anchor.offset && doc.selection.focus.offset === 0) ||
-				(positionParent.maxOffset === doc.selection.focus.offset && doc.selection.anchor.offset === 0);
+			const entireParagraphSelected = (selectionParent.maxOffset === doc.selection.anchor.offset && doc.selection.focus.offset === 0) ||
+				(selectionParent.maxOffset === doc.selection.focus.offset && doc.selection.anchor.offset === 0);
 
-			if(entireParagraphSelected && positionParent.index) {
+			// If the entire current paragraph is selected, and it's not the
+			// first paragraph of the current footnote, simply delete the
+			// paragraph.
+			if(entireParagraphSelected && selectionParent.index) {
 				editor.model.change(writer => {
-					writer.remove(positionParent);
+					writer.remove(selectionParent);
 				});
 				return;
 			}
 
-			if ((doc.selection.anchor.offset === 0 && positionParent.maxOffset === 1) || entireParagraphSelected) {
-				const footnoteList = positionParent.parent;
+			// if a) the footnote contains no text (other than its number) or b) the entire footnote is selected,
+			// remove the footnote and its references.
+			if (selectionParent.maxOffset === 1 || entireParagraphSelected) {
+				const footnoteList = selectionParent.parent;
 				const index = footnoteList.index;
 				const footnoteSection = footnoteList.parent;
 				if (
 					index === null || 
 					!footnoteSection || 
-					!(footnoteSection instanceof ModelElement)) 
-				throw new Error("footnoteList has an invalid parent section.")
+					!(footnoteSection instanceof ModelElement)
+				) {
+					throw new Error("footnoteList has an invalid parent.")
+				}
 
-				this._removeHolder(index+1);
+				this._removeReferences(index+1);
 				editor.model.change(writer => {
 					writer.remove(footnoteList);
+					// if only one footnote remains, remove the footnote section
 					if(footnoteSection.maxOffset === 0) {
 						writer.remove(footnoteSection);
 					}
@@ -127,7 +151,6 @@ export default class FootnoteEditing extends Plugin {
 				}
 				data.preventDefault();
 				evt.stop();
-				
 			}
 		} , { priority: 'high' });
 	}
@@ -137,7 +160,7 @@ export default class FootnoteEditing extends Plugin {
 	 * all references are deleted.
 	 * @param {number} footnoteId
 	 */
-	_removeHolder(footnoteId) {
+	_removeReferences(footnoteId) {
 		const removeList = [];
 		if(!this.rootElement) throw new Error('Document has no root element.');
 		const footnoteReferences = modelQueryElementsAll(this.editor, this.rootElement, e => e.name === ELEMENTS.footnoteReference);
