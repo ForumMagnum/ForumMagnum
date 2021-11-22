ForumMagnum/ForumMagnum

commit af4d89e6af05bf110e97c3b3cd96c8e91fd5767a

fix deepscan

packages/lesswrong/components/tagging/FilterMode.tsx
@@ -111,8 +111,8 @@ const FilterModeRawComponent = ({tagId="", label, mode, canRemove=false, onChang
 
   const otherValue = ["Hidden", -25,-10,0,10,25,"Required"].includes(mode) ? "" : (mode || "")
   return <span {...eventHandlers}>
-    <AnalyticsContext pageElementContext="tagFilterMode" tagId={tag?._id} tagName={tag?.name}>
-      {(tag && !isMobile()) ? <Link to={`tag/${tag.slug}`}>
+    <AnalyticsContext pageElementContext="tagFilterMode" tagId={tag._id} tagName={tag.name}>
+      {(!isMobile()) ? <Link to={`tag/${tag.slug}`}>
         {tagLabel}
       </Link>
       : tagLabel
@@ -172,7 +172,7 @@ const FilterModeRawComponent = ({tagId="", label, mode, canRemove=false, onChang
 
               onChange={ev => onChangeMode(parseInt(ev.target.value || "0"))}
             />
-            {canRemove && !tag?.suggestedAsFilter &&
+            {canRemove && !tag.suggestedAsFilter &&
               <div className={classes.removeLabel} onClick={ev => {if (onRemove) onRemove()}}>
                 <LWTooltip title={<div><div>This filter will no longer appear in Latest Posts.</div><div>You can add it back later if you want</div></div>}>
                   <a>Remove</a>

packages/lesswrong/components/comments/CommentPermalink.tsx
@@ -49,8 +49,10 @@ const CommentPermalink = ({ documentId, post, classes }: {
   const { Loading, Divider, CommentWithReplies, HeadTags } = Components;
 
   if (error || (!comment && !loading)) return <div>Comment not found</div>
+  
+  if (loading) return <Loading />
 
-  if (loading || !comment) {return null}
+  if (!comment) {return null}
 
   if (!documentId) return null
 
@@ -62,12 +64,12 @@ const CommentPermalink = ({ documentId, post, classes }: {
   // NB: classes.root is not in the above styles, but is used by eaTheme
   return <div className={classes.root}>
       <div className={classes.permalinkLabel}>Comment Permalink</div>
-      {loading ? <Loading /> : <div>
+      <div>
         <HeadTags ogUrl={ogUrl} canonicalUrl={canonicalUrl} image={socialPreviewImageUrl} 
         description={getCommentDescription(comment)} noIndex={true} />
         <CommentWithReplies key={comment._id} post={post} comment={comment} refetch={refetch} expandByDefault showTitle={false}/>
         <div className={classes.seeInContext}><a href={`#${documentId}`}>See in context</a></div>
-      </div>}
+      </div>
       {forumTypeSetting.get() !== 'EAForum' && <div className={classes.dividerMargins}><Divider /></div>}
     </div>
 }

packages/lesswrong/components/revisions/PostsRevisionSelect.tsx
@@ -43,7 +43,7 @@ const PostsRevisionSelect = ({ classes }: {
   if (!post) {return null;}
   
   return <SingleColumnSection>
-    <h1>{post && post.title}</h1>
+    <h1>{post.title}</h1>
     
     {(loadingPost || loadingRevisions) && <Loading/>}
     

commit 187c24b7ac0712a8674acd8a9598d249706ad3b0

Fix broken link when you click on Personal Blog in tag filters
Introduced in: af4d89e6af05bf110e97c3b3cd96c8e91fd5767a

packages/lesswrong/components/tagging/FilterMode.tsx
@@ -112,7 +112,7 @@ const FilterModeRawComponent = ({tagId="", label, mode, canRemove=false, onChang
   const otherValue = ["Hidden", -25,-10,0,10,25,"Required"].includes(mode) ? "" : (mode || "")
   return <span {...eventHandlers}>
     <AnalyticsContext pageElementContext="tagFilterMode" tagId={tag?._id} tagName={tag?.name}>
-      {(!isMobile()) ? <Link to={`tag/${tag?.slug}`}>
+      {(tag && !isMobile()) ? <Link to={`tag/${tag?.slug}`}>
         {tagLabel}
       </Link>
       : tagLabel