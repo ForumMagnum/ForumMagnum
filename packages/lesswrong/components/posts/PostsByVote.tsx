147ca8daf5b9fcf10ca77a67bba3d539bd58d222
Raemon <raemon777@gmail.com>
2021-11-30 01:29:02 -0800

    fix spacing

--- packages/lesswrong/components/posts/PostsByVote.tsx
+++ packages/lesswrong/components/posts/PostsByVote.tsx
@@ -3,30 +3,30 @@ import { useMulti } from '../../lib/crud/withMulti';
 import { registerComponent, Components } from '../../lib/vulcan-lib';
 
 const PostsByVote = ({postIds, year}: {postIds: Array<string>, year: number | '≤2020'}) => {
-    const { PostsItem2, ErrorBoundary, Loading } = Components
+  const { PostsItem2, ErrorBoundary, Loading } = Components
 
-    const before = year === '≤2020' ? '2021-01-01' : `${year + 1}-01-01`
-    const after = `${year}-01-01`
+  const before = year === '≤2020' ? '2021-01-01' : `${year + 1}-01-01`
+  const after = `${year}-01-01`
 
-    const { results: posts, loading } = useMulti({
-      terms: {
-        view: "nominatablePostsByVote",
-        postIds,
-        before,
-        ...(year === '≤2020' ? {} : {after}),
-      },
-      collectionName: "Posts",
-      fragmentName: "PostsList",
-      limit: 1000,
-    })
-    
-    if (loading) return <Loading/>
+  const { results: posts, loading } = useMulti({
+    terms: {
+      view: "nominatablePostsByVote",
+      postIds,
+      before,
+      ...(year === '≤2020' ? {} : {after}),
+    },
+    collectionName: "Posts",
+    fragmentName: "PostsList",
+    limit: 1000,
+  })
+  
+  if (loading) return <Loading/>
 
-    if (!posts || posts.length === 0) return <div>None</div>
+  if (!posts || posts.length === 0) return <div>None</div>
 
-    return <ErrorBoundary><div>
-          {posts.map(post=> <PostsItem2 key={post._id} post={post} />)}
-      </div></ErrorBoundary>
+  return <ErrorBoundary><div>
+        {posts.map(post=> <PostsItem2 key={post._id} post={post} />)}
+    </div></ErrorBoundary>
 }
 
 const PostsByVoteComponent = registerComponent("PostsByVote", PostsByVote);
