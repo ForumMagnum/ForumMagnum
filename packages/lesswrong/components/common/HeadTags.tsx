8e6f6e6b52024dd279a7086c7edc24cb5ab3b249
Ruben Bloom <rmb042@gmail.com>
2022-01-08 12:21:57 +1100

    Update twitter headers to be modern + Apple webclip preview

--- packages/lesswrong/components/common/HeadTags.tsx
+++ packages/lesswrong/components/common/HeadTags.tsx
@@ -65,7 +65,8 @@ const HeadTags = ({ogUrl: ogUrlProp, canonicalUrl: canonicalUrlProp, description
           {(noIndex || currentRoute?.noIndex) && <meta name='robots' content='noindex' />}
           <link rel='canonical' href={canonicalUrl}/>
           <link rel='shortcut icon' href={faviconUrlSetting.get()}/>
-
+          <link rel="apple-touch-icon" href={faviconUrlSetting.get()}/>
+  
           <link rel="alternate" type="application/rss+xml" href={rssUrl} />
         </Helmet>
       </React.Fragment>
