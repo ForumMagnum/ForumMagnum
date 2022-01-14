7134786bc2c0c897e81d7ed52673b756f0758d01
JP Addison <johnpaddison@gmail.com>
2022-01-14 14:47:48 +0000

    one more comment

--- packages/lesswrong/lib/publicSettings.ts
+++ packages/lesswrong/lib/publicSettings.ts
@@ -91,6 +91,7 @@ export const isProductionDBSetting = new DatabasePublicSetting<boolean>('isProdu
 // You will need to restart your server after changing these at present;
 // FrontpageReviewWidget reads them at startup.
 export const annualReviewStart = new DatabasePublicSetting('annualReview.start', "2021-11-30")
+// The following dates cut off their phase at the end of the day
 export const annualReviewNominationPhaseEnd = new DatabasePublicSetting('annualReview.nominationPhaseEnd', "2021-12-14")
 export const annualReviewReviewPhaseEnd = new DatabasePublicSetting('annualReview.reviewPhaseEnd', "2022-01-15")
 export const annualReviewEnd = new DatabasePublicSetting('annualReview.end', "2022-02-01")
