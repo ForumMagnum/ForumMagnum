8398dbe7e1b4ef60e7902d939dadcf28cfe92418
Raemon <raemon777@gmail.com>
2022-01-29 11:10:32 -0800

    paymentsCollection wip

---
+++ packages/lesswrong/lib/collections/payments/schema.ts
@@ -0,0 +1,21 @@
+// import { userOwns } from '../../vulcan-users/permissions';
+// import { schemaDefaultValue } from '../../collectionUtils';
+
+// const schema: SchemaType<DbPayment> = {
+//   userId: {
+//   },
+//   amount: {
+
+//   },
+//   recipientContactEmail: {
+
+//   },
+//   recipientPaymentEmail: {
+
+//   },
+//   reciptientPaymentInfo: {
+
+//   }
+// }
+
+// export default schema;
