6d059baf3d8c28a377d55cf2a8c5a9cf7f2dd5f8
Raemon <raemon777@gmail.com>
2022-01-30 19:06:46 -0800

    Update packages/lesswrong/components/payments/EditPaymentInfo.tsx
    Co-authored-by: Ruben Bloom <darkruby501@users.noreply.github.com>

--- packages/lesswrong/components/payments/EditPaymentInfo.tsx
+++ packages/lesswrong/components/payments/EditPaymentInfo.tsx
@@ -32,7 +32,7 @@ export const EditPaymentInfo = ({classes}: {
       <SectionTitle title={`Edit Payment for ${currentUser.displayName}`}/>
       <div className={classes.info}>
         <p>To receive prizes and donations through LessWrong, you must have a valid <a href="https://paypal.com/">PayPal account</a> and to enter your associated PayPal email/info here.</p>
-        <p>If you receive more than $600 in a year, you'll need to be entered into Center for Applied Rationality's payment system. CFAR will contact you are your LessWrong email address about next steps. (Make sure it's an email that you check regularly)</p>
+        <p>If you receive more than $600 in prizes in a single year, you will need to be entered into Center for Applied Rationality's (LessWrong's legal entity) payment system. CFAR will contact you via your submitted email address about next steps. (To ensure prompt payment, ensure it's an email that you check regularly)</p>
       </div>
       <Components.WrappedSmartForm
         layout="elementOnly"
