26287b44013be8cff4670b0d4474a288935d0794
Raemon <raemon777@gmail.com>
2022-02-02 12:15:41 -0800

    fix console errors

--- packages/lesswrong/server/migrations/2022-01-30-updateFinal2020ReviewVotes.ts
+++ packages/lesswrong/server/migrations/2022-01-30-updateFinal2020ReviewVotes.ts
@@ -22,7 +22,7 @@ registerMigration({
 
     let postsAllUsers = {}
     let postsHighKarmaUsers = {}
-    let postsAFUsers = {}
+    let postsAFUsers = {}å
 
     const posts = await Posts.find({reviewCount: {$gte: 1}}).fetch()
     const postIds = posts.map(post=>post._id)
@@ -37,11 +37,13 @@ registerMigration({
 
     for (let userId in votesByUserId) {
       let totalUserPoints = 0 
+      // eslint-disable-next-line no-console
       console.log(userId)
       const user = usersByUserId[userId][0]
       const votes = votesByUserId[userId].filter(vote => postIds.includes(vote.postId))
 
       const costTotal = votes.reduce((total,vote) => total + getCost(vote), 0)
+      // eslint-disable-next-line no-console
       console.log(userId, costTotal, (costTotal > 500) ? "500+" : "")
       for (let vote of votesByUserId[userId]) {
         if (!vote.qualitativeScore) continue
@@ -58,6 +60,7 @@ registerMigration({
       }
     }
 
+    // eslint-disable-next-line no-console
     console.log("Updating all karma...")
     for (let postId in postsAllUsers) {
       await Posts.update({_id:postId}, {$set: { 
@@ -66,6 +69,7 @@ registerMigration({
       }})
     }
 
+    // eslint-disable-next-line no-console
     console.log("Updating high karma...")
     for (let postId in postsHighKarmaUsers) {
       await Posts.update({_id:postId}, {$set: { 
@@ -73,7 +77,7 @@ registerMigration({
         finalReviewVoteScoreHighKarma: postsHighKarmaUsers[postId].reduce((x, y) => x + y, 0),
       }})
     }
-
+    // eslint-disable-next-line no-console
     console.log("Updating AF...")
     for (let postId in postsAFUsers) {
       await Posts.update({_id:postId}, {$set: { 
@@ -88,9 +92,7 @@ registerMigration({
 
     const authors = await Users.find({_id: {$in:authorIds}}).fetch() 
 
-    const getAuthor = post => {
-      return authors.filter(author => author._id === post.userId)[0].displayName
-    }
+    const getAuthor = (post) => authors.filter(author => author._id === post.userId)[0]
 
     const primaryColor = "#5f9b65" // ea-forum-look-here
     const errorColor = "#bf360c"
@@ -120,7 +122,7 @@ registerMigration({
 
     const donateButton = (post) => `<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_blank" style="text-align: center">
     <input type="hidden" name="cmd" value="_s-xclick" />
-    <input type="hidden" name="item_name" value='Best of LessWrong Prize, with special appreciation for ${getAuthor(post)}, author of "${post.title}".' />
+    <input type="hidden" name="item_name" value='Best of LessWrong Prize, with special appreciation for ${getAuthor(post).displayName}, author of "${post.title}".' />
     <input type="hidden" name="hosted_button_id" value="ZMFZULZHMAM9Y" />
     <input type="submit" value="Donate" border="0" name="submit" title="Donate via PayPal" alt="Donate with PayPal button" class="donate-button"/>
     <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
@@ -131,7 +133,13 @@ registerMigration({
         <td>
           <div class="title-row">
             <div class="title">
-              <a href="/posts/${post._id}/${post.slug}"><b>${post.title}</b></a>
+              <span class="post-title">
+                <a href="/posts/${post._id}/${post.slug}">${post.title}</a>
+              </span>
+              <span class="author">
+                <a href="/users/${getAuthor(post).slug}">${getAuthor(post).displayName}</a>
+              </span>
+
             </div>
             <div class="dots-row">
               ${post.finalReviewVotesHighKarma.sort((x,y) => x - y).map(vote=>voteDot(vote)).join("")}
@@ -140,18 +148,6 @@ registerMigration({
         </td>
         <td>${donateButton(post)}</td>
       </tr>`
-
-    const postTop15 = (post, i) =>  {
-      if (!post) return null
-      if (i > 14) return null 
-
-      return `<li>
-        <a href="/posts/${post._id}/${post.slug}">
-          <b>${post.title}</b>
-        </a> by ${getAuthor(post)}.
-      </li>
-      `
-    }
     
     const html = `<div>
         <style>
@@ -173,6 +169,7 @@ registerMigration({
           }
           .title {
             max-width: 350px;
+            line-height:1rem
           }
           .dot {
             margin-right:3px;
@@ -182,7 +179,9 @@ registerMigration({
           .dots-row {
             display:flex;
             align-items:center;
-            margin-left:15px;
+            margin-left:auto;
+            padding-top:8px;
+            padding-bottom:8px;
           }
           .donate-button {
             background:${primaryColor}; 
@@ -199,14 +198,22 @@ registerMigration({
             justify-content:space-between;
             flex-wrap:wrap;
           }
+          .author a  {
+            font-size: 14px;
+            white-space: pre;
+            line-height: 1rem;
+          }
+          .post-title a {
+            font-weight:600;
+            line-height:2rem;
+          }
         </style>
-        <ol>
-          ${finalPosts.map((post, i) => postTop15(post, i)).join("")}
-        </ol>
         <table>
           ${finalPosts.map((post, i) => postTableRow(post, i)).join("")}
         </table>
       </div>`
+
+    // eslint-disable-next-line no-console
     console.log(html)
   },
 });
