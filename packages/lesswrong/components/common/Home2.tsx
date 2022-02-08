c1caeade45c9e0e79c6f7f6102df903fc57112e9
Raemon <raemon777@gmail.com>
2022-02-07 22:48:52 -0800

    remove whitespace

--- packages/lesswrong/components/common/Home2.tsx
+++ packages/lesswrong/components/common/Home2.tsx
@@ -15,7 +15,6 @@ const Home2 = () => {
         <React.Fragment>
           <FrontpageBestOfLWWidget />
         
-          
           {reviewIsActive() && currentUser && <SingleColumnSection>
             <FrontpageReviewWidget />
           </SingleColumnSection>}
