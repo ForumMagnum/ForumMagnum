ForumMagnum/ForumMagnum

commit f1cd86f07f6a1575c08ddad79b4ef14e8a776c43

adjust margins around group icons

packages/lesswrong/components/localGroups/GroupLinks.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { registerComponent } from '../../lib/vulcan-lib';
 import LinkIcon from '@material-ui/icons/Link';
 import SvgIcon from '@material-ui/core/SvgIcon';
-import IconButton from '@material-ui/core/IconButton';
 import Tooltip from '@material-ui/core/Tooltip';
 import { createStyles } from '@material-ui/core/styles';
 import { forumTypeSetting } from '../../lib/instanceSettings';
+import classNames from 'classnames';
 
 
 // just the "f", used for the FB Group link
@@ -88,6 +88,7 @@ const styles = createStyles((theme: ThemeType): JssStyles => ({
     width: "17px",
     paddingTop: "0px",
     transform: "translateY(3px) rotate(-45deg)",
+    color: "rgba(0, 0, 0, 0.7)",
   },
 
   iconButton: {
@@ -95,6 +96,13 @@ const styles = createStyles((theme: ThemeType): JssStyles => ({
     width: '18px',
     height: '18px',
     verticalAlign: "baseline",
+  },
+  
+  noMargin: {
+    margin: 0,
+    '& :first-child': {
+      marginLeft: 0
+    }
   }
 }));
 
@@ -105,8 +113,9 @@ const tooltips: Partial<Record<string,string>> = {
   'MIRIx': "This is a MIRIx group"
 }
 
-const GroupLinks = ({ document, classes }: {
+const GroupLinks = ({ document, noMargin, classes }: {
   document: localGroupsBase|PostsBase,
+  noMargin?: Boolean,
   classes: ClassesType,
 }) => {
   const isEAForum = forumTypeSetting.get() === 'EAForum';
@@ -115,7 +124,7 @@ const GroupLinks = ({ document, classes }: {
   
   return(
     <div className={classes.root}>
-      {!isEAForum && <div className={classes.groupTypes}>
+      {!isEAForum && <div className={noMargin ? classNames(classes.groupTypes, classes.noMargin) : classes.groupTypes}>
         {document.types && document.types.map(type => {
           return (
             <Tooltip
@@ -130,7 +139,7 @@ const GroupLinks = ({ document, classes }: {
           )
         })}
       </div>}
-      <div className={classes.groupLinks}>
+      <div className={(noMargin && (isEAForum || !document.types.length)) ? classNames(classes.groupLinks, classes.noMargin) : classes.groupLinks}>
         {document.facebookLink
           && <Tooltip
             title={`Link to Facebook ${isEvent ? 'Event' : 'Group'}`}

packages/lesswrong/components/posts/PostsPage/PostsPagePostHeader.tsx
@@ -34,7 +34,7 @@ const styles = (theme: ThemeType): JssStyles => ({
   },
   groupLinks: {
     display: 'inline-block',
-    marginRight: 14
+    marginRight: 20
   },
   commentsLink: {
     marginRight: SECONDARY_SPACING,
@@ -153,7 +153,7 @@ const PostsPagePostHeader = ({post, classes}: {
             <PostsPageDate post={post} hasMajorRevision={hasMajorRevision} />
           </span>}
           {post.isEvent && <div className={classes.groupLinks}>
-            <Components.GroupLinks document={post} />
+            <Components.GroupLinks document={post} noMargin={true} />
           </div>}
           <a className={classes.commentsLink} href={"#comments"}>{ postGetCommentCountStr(post)}</a>
           <div className={classes.commentsLink}>

commit 141767f9b0285b93c42f6d2e27b7bec99e6e012f

Fix crash on event pages with no group-types field
Introduced in f1cd86f07f6a1575c08ddad79b4ef14e8a776c43. Probably only
affects some events from far in the past when this field didn't exist,
but showed up in Sentry nevertheless.

packages/lesswrong/components/localGroups/GroupLinks.tsx
@@ -164,7 +164,7 @@ const GroupLinks = ({ document, noMargin, classes }: {
           )
         })}
       </div>}
-      <div className={(noMargin && (isEAForum || !document.types.length)) ? classNames(classes.groupLinks, classes.noMargin) : classes.groupLinks}>
+      <div className={(noMargin && (isEAForum || !document.types?.length)) ? classNames(classes.groupLinks, classes.noMargin) : classes.groupLinks}>
         {document.facebookLink
           && <Tooltip
             title={`Link to Facebook ${isEvent ? 'Event' : 'Group'}`}