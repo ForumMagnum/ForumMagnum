ForumMagnum/ForumMagnum

commit d6fae38531980f89f807bd6aac669a20e8cb2f0e

Avoid creating duplicate accounts when someone uses oAuth and has account with an existing email.

packages/lesswrong/server/authenticationMiddlewares.ts
@@ -14,7 +14,7 @@ import { createMutator, updateMutator } from './vulcan-lib/mutators';
 import { combineUrls, getSiteUrl, slugify, Utils } from '../lib/vulcan-lib/utils';
 import pick from 'lodash/pick';
 import { forumTypeSetting } from '../lib/instanceSettings';
-import { userFromAuth0Profile, mergeAccountWithAuth0 } from './authentication/auth0Accounts';
+import { userFromAuth0Profile } from './authentication/auth0Accounts';
 import moment from 'moment';
 
 /**
@@ -54,6 +54,20 @@ export const expressSessionSecretSetting = new DatabaseServerSetting<string | nu
 type IdFromProfile<P extends Profile> = (profile: P) => string | number
 type UserDataFromProfile<P extends Profile> = (profile: P) => Promise<Partial<DbUser>>
 
+async function mergeAccount(idPath: string, user: DbUser, profile: Profile) {
+  return await updateMutator({
+    collection: Users,
+    documentId: user._id,
+    // This is the correct way to set a nested property according to Mongo, but
+    // it's very hard to get it to type correctly. TS thinks we're setting a
+    // completely different field, `services.auth0`, not setting a nested one.
+    set: {[idPath]: profile} as any,
+    // Normal updates are not supposed to update services
+    validate: false
+    // TODO: Soon we should delete passwords, and maybe(?) resume tokens when we do this
+  })
+}
+
 /**
  * Given the provider-appropriate ways to get user info from a profile, create
  * a function that handles successful logins from that provider
@@ -62,25 +76,25 @@ function createOAuthUserHandler<P extends Profile>(idPath: string, getIdFromProf
   return async (_accessToken: string, _refreshToken: string, profile: P, done: VerifyCallback) => {
     try {
       const profileId = getIdFromProfile(profile)
-      // Probably impossible, but if it is null, we just log the person in as a
-      // random user, which is bad, so we'll check anyway.
+      // Probably impossible
       if (!profileId) {
         return done(new Error('OAuth profile does not have a profile ID'))
       }
       let user = await Users.findOne({[idPath]: profileId})
       if (!user) {
-        const email = profile.emails?.[0]?.value
-        if (forumTypeSetting.get() === 'EAForum' && email) {
+        const email = profile.emails?.[0]?.value 
+        //FB and GitHub require verified emails, so this is secure
+        if (email) {
           // Collation here means we're using the case-insensitive index
           const matchingUsers = await Users.find({'emails.address': email}, {collation: {locale: 'en', strength: 2}}).fetch()
           if (matchingUsers.length > 1) {
-            throw new Error(`Multiple users found with email ${email}`)
+            throw new Error(`Multiple users found with email ${email}, please contact support`)
           }
           const user = matchingUsers[0]
           if (user) {
             // Forum only uses Auth0Profile
             // TODO: Guard with zod for isAUth0Profile
-            const { data: userUpdated } = await mergeAccountWithAuth0(user, profile as unknown as Auth0Profile)
+            const { data: userUpdated } = await mergeAccount(idPath, user, profile as unknown as Auth0Profile)
             if (user.banned && new Date(user.banned) > new Date()) {
               return done(new Error("banned"))
             }

packages/lesswrong/server/authentication/auth0Accounts.ts
@@ -3,20 +3,6 @@ import Users from "../../lib/vulcan-users";
 import { slugify, Utils } from "../../lib/vulcan-lib/utils";
 import { updateMutator } from "../vulcan-lib/mutators";
 
-export async function mergeAccountWithAuth0(user: DbUser, profile: Profile) {
-  return await updateMutator({
-    collection: Users,
-    documentId: user._id,
-    // This is the correct way to set a nested property according to Mongo, but
-    // it's very hard to get it to type correctly. TS thinks we're setting a
-    // completely different field, `services.auth0`, not setting a nested one.
-    set: {'services.auth0': profile} as any,
-    // Normal updates are not supposed to update services
-    validate: false
-    // TODO: Soon we should delete passwords, and maybe(?) resume tokens when we do this
-  })
-}
-
 export async function userFromAuth0Profile(profile: Profile): Promise<Partial<DbUser>> {
   const email = profile.emails?.[0].value
   const displayNameMatchesEmail = email === profile.displayName

packages/lesswrong/server/vulcan-lib/apollo-server/authentication.tsx
@@ -57,7 +57,7 @@ const passwordAuthStrategy = new GraphQLLocalStrategy(async function getUserPass
     return done(null, false, { message: 'Incorrect password.' });
   }
   
-  const match = await comparePasswords(password, user.services.password.bcrypt);
+  const match = await comparePasswords(password, user.services.password?.bcrypt);
 
   // If no immediate match, we check whether we have a match with their legacy password
   if (!match) {

commit ab42940b9d4dc9b16b743523fa73b22260570aba

OAuth mergeAccount: Don't put the profile into the ID field
Fixes bug introduced in d6fae38531980f89f807bd6aac669a20e8cb2f0e

packages/lesswrong/server/authenticationMiddlewares.ts
@@ -55,11 +55,11 @@ export const expressSessionSecretSetting = new DatabaseServerSetting<string | nu
 type IdFromProfile<P extends Profile> = (profile: P) => string | number
 type UserDataFromProfile<P extends Profile> = (profile: P) => Promise<Partial<DbUser>>
 
-async function mergeAccount(idPath: string, user: DbUser, profile: Profile) {
+async function mergeAccount(profilePath: string, user: DbUser, profile: Profile) {
   return await updateMutator({
     collection: Users,
     documentId: user._id,
-    set: {[idPath]: profile} as any,
+    set: {[profilePath]: profile} as any,
     // Normal updates are not supposed to update services
     validate: false
   })
@@ -69,7 +69,7 @@ async function mergeAccount(idPath: string, user: DbUser, profile: Profile) {
  * Given the provider-appropriate ways to get user info from a profile, create
  * a function that handles successful logins from that provider
  */
-function createOAuthUserHandler<P extends Profile>(idPath: string, getIdFromProfile: IdFromProfile<P>, getUserDataFromProfile: UserDataFromProfile<P>) {
+function createOAuthUserHandler<P extends Profile>(profilePath: string, getIdFromProfile: IdFromProfile<P>, getUserDataFromProfile: UserDataFromProfile<P>) {
   return async (_accessToken: string, _refreshToken: string, profile: P, done: VerifyCallback) => {
     try {
       const profileId = getIdFromProfile(profile)
@@ -77,7 +77,7 @@ function createOAuthUserHandler<P extends Profile>(idPath: string, getIdFromProf
       if (!profileId) {
         throw new Error('OAuth profile does not have a profile ID')
       }
-      let user = await Users.findOne({[idPath]: profileId})
+      let user = await Users.findOne({[`${profilePath}.id`]: profileId})
       if (!user) {
         const email = profile.emails?.[0]?.value 
         if (!email) {
@@ -92,7 +92,7 @@ function createOAuthUserHandler<P extends Profile>(idPath: string, getIdFromProf
           }
           const user = matchingUsers[0]
           if (user) {
-            const { data: userUpdated } = await mergeAccount(idPath, user, profile)
+            const { data: userUpdated } = await mergeAccount(profilePath, user, profile)
             if (user.banned && new Date(user.banned) > new Date()) {
               return done(new Error("banned"))
             }
@@ -122,8 +122,8 @@ function createOAuthUserHandler<P extends Profile>(idPath: string, getIdFromProf
 /**
  * Auth0 passes 5 parameters, not 4, so we need to wrap createOAuthUserHandler
  */
-function createOAuthUserHandlerAuth0(idPath: string, getIdFromProfile: IdFromProfile<Auth0Profile>, getUserDataFromProfile: UserDataFromProfile<Auth0Profile>) {
-  const standardHandler = createOAuthUserHandler(idPath, getIdFromProfile, getUserDataFromProfile)
+function createOAuthUserHandlerAuth0(profilePath: string, getIdFromProfile: IdFromProfile<Auth0Profile>, getUserDataFromProfile: UserDataFromProfile<Auth0Profile>) {
+  const standardHandler = createOAuthUserHandler(profilePath, getIdFromProfile, getUserDataFromProfile)
   return (accessToken: string, refreshToken: string, _extraParams: ExtraVerificationParams, profile: Auth0Profile, done: VerifyCallback) => {
     return standardHandler(accessToken, refreshToken, profile, done)
   }
@@ -270,7 +270,7 @@ export const addAuthMiddlewares = (addConnectHandler) => {
       callbackURL: `${getSiteUrl()}auth/google/callback`,
       proxy: true
     },
-    createOAuthUserHandler<GoogleProfile>('services.google.id', profile => profile.id, async profile => ({
+    createOAuthUserHandler<GoogleProfile>('services.google', profile => profile.id, async profile => ({
       email: profile.emails?.[0].value,
       services: {
         google: profile
@@ -295,7 +295,7 @@ export const addAuthMiddlewares = (addConnectHandler) => {
       callbackURL: `${getSiteUrl()}auth/facebook/callback`,
       profileFields: ['id', 'emails', 'name', 'displayName'],
     },
-      createOAuthUserHandler<FacebookProfile>('services.facebook.id', profile => profile.id, async profile => ({
+      createOAuthUserHandler<FacebookProfile>('services.facebook', profile => profile.id, async profile => ({
         email: profile.emails?.[0].value,
         services: {
           facebook: profile
@@ -316,7 +316,7 @@ export const addAuthMiddlewares = (addConnectHandler) => {
       callbackURL: `${getSiteUrl()}auth/github/callback`,
       scope: [ 'user:email' ], // fetches non-public emails as well
     },
-      createOAuthUserHandler<GithubProfile>('services.github.id', profile => parseInt(profile.id), async profile => ({
+      createOAuthUserHandler<GithubProfile>('services.github', profile => parseInt(profile.id), async profile => ({
         email: profile.emails?.[0].value,
         services: {
           github: profile
@@ -367,7 +367,7 @@ export const addAuthMiddlewares = (addConnectHandler) => {
         domain: auth0Domain,
         callbackURL: combineUrls(getSiteUrl(), 'auth/auth0/callback')
       },
-      createOAuthUserHandlerAuth0('services.auth0.id', profile => profile.id, userFromAuth0Profile)
+      createOAuthUserHandlerAuth0('services.auth0', profile => profile.id, userFromAuth0Profile)
     ));
   }
 