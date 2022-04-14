ddfaf859a52133270441a7303d21126707bdb11c
James Babcock <jimrandomh@gmail.com>
2022-04-14 14:14:22 -0700

    Fix Cypress test

--- cypress/integration/posts/edit_post.spec.js
+++ cypress/integration/posts/edit_post.spec.js
@@ -16,7 +16,7 @@ describe('Posts', function() {
     cy.get('.PostsPageActions-root').click();
     cy.contains('.PostActions-actions li', 'Edit').click();
     cy.get('.form-component-EditTitle').click().clear().type(newPostTitle);
-    cy.get('.input-contents .ck-editor__editable').click().clear().type(newPostBody);
+    cy.get('.input-contents .ck-editor__editable').click();
     
     // HACK: Clicking away from the title opens a flash-message. It auto-closes, but
     // while it's open, it can eat certain mouse clicks, causing them to close the
@@ -24,6 +24,7 @@ describe('Posts', function() {
     // affect the publish button). Wait until the message auto-closes.
     cy.wait(5000);
     
+    cy.get('.input-contents .ck-editor__editable').click().clear().type(newPostBody);
     cy.contains('Publish Changes').click();
     cy.contains('.PostsPageTitle-root', newPostTitle).should('exist');
     cy.contains('.PostsPage-postContent', newPostBody).should('exist');
