537f1c28055755a7d9aa6ae9e90c16844df2532c
Sarah Cheng <sarah.cheng@centreforeffectivealtruism.org>
2022-05-25 11:53:17 -0400

    WIP fix decrementing when deleting a comment; add logs
    Co-authored-by: jpaddison3 <jp@centreforeffectivealtruism.org>

--- packages/lesswrong/server/resolvers/commentResolvers.ts
+++ packages/lesswrong/server/resolvers/commentResolvers.ts
@@ -2,7 +2,8 @@ import { addGraphQLMutation, addGraphQLResolvers } from '../../lib/vulcan-lib';
 import { encodeIntlError} from '../../lib/vulcan-lib/utils';
 import { userCanModerateComment } from "../../lib/collections/users/helpers";
 import { accessFilterSingle } from '../../lib/utils/schemaUtils';
-import { moderateCommentsPostUpdate } from '../callbacks/commentCallbacks';
+import { updateMutator } from '../vulcan-lib';
+import { Comments } from '../../lib/collections/comments';
 
 const specificResolvers = {
   Mutation: {
@@ -29,10 +30,15 @@ const specificResolvers = {
           set.deletedReason = "";
           set.deletedByUserId = null;
         }
-        let modifier = { $set: set };
-        await context.Comments.rawUpdateOne({_id: commentId}, modifier);
-        const updatedComment = await context.Comments.findOne(commentId)
-        await moderateCommentsPostUpdate(updatedComment!, currentUser);
+        
+        const {data: updatedComment} = await updateMutator({
+          collection: Comments,
+          documentId: commentId,
+          set,
+          currentUser: currentUser,
+          validate: false,
+          context
+        });
         return await accessFilterSingle(context.currentUser, context.Comments, updatedComment, context);
       } else {
         throw new Error(encodeIntlError({id: `app.user_cannot_moderate_post`}));
