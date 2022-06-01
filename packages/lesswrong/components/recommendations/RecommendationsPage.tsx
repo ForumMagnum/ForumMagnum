1a81785f2f39c42172d97ad02fd4bcbd55457615
Raemon <raemon777@gmail.com>
2022-05-31 18:33:28 -0700

    return curated to recommendations-section, add to rec page

--- packages/lesswrong/components/recommendations/RecommendationsPage.tsx
+++ packages/lesswrong/components/recommendations/RecommendationsPage.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Components, registerComponent } from '../../lib/vulcan-lib';
 import {AnalyticsContext} from "../../lib/analyticsEvents";
+import { forumTypeSetting } from '../../lib/instanceSettings';
 
 const styles = (theme: ThemeType): JssStyles => ({
 });
@@ -8,12 +9,27 @@ const styles = (theme: ThemeType): JssStyles => ({
 const RecommendationsPage = ({classes}: {
   classes: ClassesType
 }) => {
-  const { ConfigurableRecommendationsList } = Components;
-  
+  const { ConfigurableRecommendationsList, PostsList2, SingleColumnSection, SectionTitle } = Components;
+
+  const showCurated = forumTypeSetting.get() === 'LessWrong'
+
   return (
-  <AnalyticsContext listContext={"recommendationsPage"} capturePostItemOnMount>
-    <ConfigurableRecommendationsList configName="recommendationspage" />
-  </AnalyticsContext>
+    <div>
+      <AnalyticsContext listContext={"recommendationsPage"} capturePostItemOnMount>
+        <ConfigurableRecommendationsList configName="recommendationspage" />
+      </AnalyticsContext>
+      {showCurated && <SingleColumnSection>
+        <SectionTitle title="Curated Posts"/>
+        <AnalyticsContext listContext={"curatedPosts"}>
+          <PostsList2
+            terms={{view:"curated", limit: 20}}
+            showNoResults={false}
+            boxShadow={false}
+            curatedIconLeft={true}
+          />
+        </AnalyticsContext>
+      </SingleColumnSection>}
+    </div>
   )
 };
 
