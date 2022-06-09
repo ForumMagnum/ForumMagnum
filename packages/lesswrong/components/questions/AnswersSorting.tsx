38244b6c2b17ab782fb3a4eea064182666508c97
Recursing <buonanno.lorenzo@gmail.com>
2022-06-09 21:04:48 +0200

    2 space indent

--- packages/lesswrong/components/questions/AnswersSorting.tsx
+++ packages/lesswrong/components/questions/AnswersSorting.tsx
@@ -13,65 +13,65 @@ export const sortingNames = {
 }
 
 const styles = (theme: ThemeType): JssStyles => ({
-    root: {
-        display: 'inline'
-    },
-    link: {
-        color: theme.palette.lwTertiary.main,
-    }
+  root: {
+    display: 'inline'
+  },
+  link: {
+    color: theme.palette.lwTertiary.main,
+  }
 })
 
 const AnswersSorting = ({ post, classes }: {
-    post?: PostsList,
-    classes: ClassesType,
+  post?: PostsList,
+  classes: ClassesType,
 }) => {
-    const [anchorEl, setAnchorEl] = useState<any>(null);
-    const { history } = useNavigation();
-    const location = useLocation();
+  const [anchorEl, setAnchorEl] = useState<any>(null);
+  const { history } = useNavigation();
+  const location = useLocation();
+  const { query } = location;
+
+  const handleClick = (event: React.MouseEvent) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleSortingClick = (sorting: string) => {
     const { query } = location;
+    const currentQuery = _.isEmpty(query) ? { answersSorting: "top" } : query;
+    setAnchorEl(null);
+    const newQuery = { ...currentQuery, answersSorting: sorting, postId: post ? post._id : undefined };
+    history.push({ ...location.location, search: `?${qs.stringify(newQuery)}` });
+  };
 
-    const handleClick = (event: React.MouseEvent) => {
-        setAnchorEl(event.currentTarget);
-    };
+  const handleClose = () => {
+    setAnchorEl(null);
+  }
 
-    const handleSortingClick = (sorting: string) => {
-        const { query } = location;
-        const currentQuery = _.isEmpty(query) ? { answersSorting: "top" } : query;
-        setAnchorEl(null);
-        const newQuery = { ...currentQuery, answersSorting: sorting, postId: post ? post._id : undefined };
-        history.push({ ...location.location, search: `?${qs.stringify(newQuery)}` });
-    };
+  let sortings = [...Object.keys(sortingNames)];
+  const currentSorting = query?.answersSorting || "top";
 
-    const handleClose = () => {
-        setAnchorEl(null);
-    }
-
-    let sortings = [...Object.keys(sortingNames)];
-    const currentSorting = query?.answersSorting || "top";
-
-    return <div className={classes.root}>
-        <a className={classes.link} onClick={handleClick}>
-            {sortingNames[currentSorting]}
-        </a>
-        <Menu
-            anchorEl={anchorEl}
-            open={Boolean(anchorEl)}
-            onClose={handleClose}
-        >
-            {sortings.map((sorting) => {
-                return <MenuItem key={sorting} onClick={() => handleSortingClick(sorting)} >
-                    {sortingNames[sorting]}
-                </MenuItem>
-            })}
-        </Menu>
-    </div>
+  return <div className={classes.root}>
+    <a className={classes.link} onClick={handleClick}>
+      {sortingNames[currentSorting]}
+    </a>
+    <Menu
+      anchorEl={anchorEl}
+      open={Boolean(anchorEl)}
+      onClose={handleClose}
+    >
+      {sortings.map((sorting) => {
+        return <MenuItem key={sorting} onClick={() => handleSortingClick(sorting)} >
+          {sortingNames[sorting]}
+        </MenuItem>
+      })}
+    </Menu>
+  </div>
 };
 
 const AnswersSortingComponent = registerComponent('AnswersSorting', AnswersSorting, { styles });
 
 declare global {
-    interface ComponentTypes {
-        AnswersSorting: typeof AnswersSortingComponent,
-    }
+  interface ComponentTypes {
+    AnswersSorting: typeof AnswersSortingComponent,
+  }
 }
 
