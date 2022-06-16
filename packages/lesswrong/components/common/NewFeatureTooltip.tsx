341e1990678c0c7b5e7a1d7943442a6ba8e38138
Will <williampayne23@gmail.com>
2022-06-16 17:00:27 +0100

    Added click to open and close function

--- packages/lesswrong/components/common/NewFeatureTooltip.tsx
+++ packages/lesswrong/components/common/NewFeatureTooltip.tsx
@@ -1,5 +1,5 @@
 import classNames from 'classnames';
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Components, registerComponent } from '../../lib/vulcan-lib';
 import { useHover } from './withHover';
 
@@ -14,7 +14,8 @@ const styles = (theme: ThemeType): JssStyles => ({
       height: 10,
       borderRadius: '50%',
       alignSelf: 'center',
-      position: 'absolute'
+      position: 'absolute',
+      cursor: 'pointer'
     },
     NewFeatureTooltipArrow: {
       pointerEvents: "none",
@@ -30,18 +31,64 @@ const styles = (theme: ThemeType): JssStyles => ({
     NewFeatureTooltip: {
       background: theme.palette.lwTertiary.main,
       color: theme.palette.primary.contrastText,
-      padding: 3,
+      padding: 5,
+      paddingRight: 12,
       borderRadius: 2,
       width: 100,
       marginLeft: 10,
       marginTop: -10,
       minHeight: 40
     },
+    closeButton: {cursor: 'pointer', position:'absolute', top:-10, right:1},
     wrapper: {
       display: 'flex',
       position: 'relative'
     }
   })
+
+
+
+const useTooltipToggle = () => {
+  const tooltipRef = useRef(null)
+  const [open, setOpen] = useState(false);
+  const [anchorEl, setAnchorEl] = useState();
+  /**
+   * Hook that alerts clicks outside of the passed ref
+   */
+  function useOutsideAlerter(ref, anchorEl) {
+    useEffect(() => {
+      /**
+       * Alert if clicked on outside of element
+       */
+      function handleClickOutside(event) {
+        if (ref.current && !ref.current.contains(event.target) && anchorEl && !anchorEl.contains(event.target)) {
+          setOpen(false)
+        }
+      }
+      // Bind the event listener
+      document.addEventListener("mousedown", handleClickOutside);
+      return () => {
+        // Unbind the event listener on clean up
+        document.removeEventListener("mousedown", handleClickOutside);
+      };
+    }, [ref, anchorEl]);
+  }
+
+  useOutsideAlerter(tooltipRef, anchorEl)
+
+  const closeEvents = {onClick : () => {
+    setOpen(false)
+  }}
+
+  const handleEvents = {onClick: (e) => {
+    setOpen(open => !open)
+    setAnchorEl(e.target);
+  }}
+
+  return {tooltipRef, anchorEl, open, closeEvents, handleEvents}
+
+}
+
 /**
  * 
  * @param props 
@@ -58,22 +105,24 @@ const NewFeatureTooltip = ({classes, children, className, text} :
   }) => {
 
   const {LWPopper} = Components
-  const { anchorEl, hover, eventHandlers } = useHover();
+  //const { anchorEl, hover, eventHandlers } = useHover();
+  const {tooltipRef, anchorEl, open, closeEvents, handleEvents} = useTooltipToggle()
 
   return <div className={classes.wrapper}>
     <LWPopper
-    
-    open={hover}
+    open={open}
     anchorEl={anchorEl}
     placement="right-start"
     allowOverflow>
-      <div>
+      <div ref={tooltipRef}>
         <div className={classes.NewFeatureTooltipArrow}></div>
-        <div className={classes.NewFeatureTooltip}>{text}</div>
+        <div className={classes.NewFeatureTooltip}>{text}
+        <div {...closeEvents} className={classes.closeButton}>&#x2715;</div> 
+        </div>
       </div>
     </LWPopper>
       {children}
-    <span className={classNames(classes.newFeatureHandle, className)} {...eventHandlers}/>
+    <span className={classNames(classes.newFeatureHandle, className)} {...handleEvents}/>
   </div>
 }
 
