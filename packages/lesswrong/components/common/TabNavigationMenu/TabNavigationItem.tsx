873b939709eaff834111c40fd971ebacafa45f3b
Sarah Cheng <sarah.cheng@centreforeffectivealtruism.org>
2022-06-29 10:59:44 -0400

    \o/ we fixed it!
    Co-authored-by: jpaddison3 <jp@centreforeffectivealtruism.org>

--- packages/lesswrong/components/common/TabNavigationMenu/TabNavigationItem.tsx
+++ packages/lesswrong/components/common/TabNavigationMenu/TabNavigationItem.tsx
@@ -85,14 +85,23 @@ const TabNavigationItem = ({tab, onClick, classes}: TabNavigationItemProps) => {
   // React router links don't handle external URLs, so use a
   // normal HTML a tag if the URL is external
   const externalLink = /https?:\/\//.test(tab.link);
-  const Element = externalLink ?
-    ({to, ...rest}) => <a href={to} target="_blank" rel="noopener noreferrer" {...rest} />
-    : Link;
+  // const Element = externalLink ?
+  //   ({to, ...rest}) => <a href={to} target="_blank" rel="noopener noreferrer" {...rest} />
+  //   : Link;
+
+  let handleClick = onClick
+  if (externalLink) {
+    handleClick = (e) => {
+      e.preventDefault()
+      window.open(tab.link, '_blank')?.focus()
+      onClick && onClick(e)
+    }
+  }
 
   return <LWTooltip placement='right-start' title={tab.tooltip || ''}>
     <MenuItemUntyped
-      onClick={onClick}
-      component={Element} to={tab.link}
+      onClick={handleClick}
+      component={Link} to={tab.link}
       disableGutters
       classes={{root: classNames({
         [classes.navButton]: !tab.subItem,
