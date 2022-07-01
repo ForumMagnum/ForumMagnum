c6e196c8372bfc6c411d59be5936697f5cda2446
jcmustin <jonathan.mustin@gmail.com>
2022-07-01 12:06:21 -0700

    Handle partial paste edge cases.

--- public/lesswrong-editor/src/ckeditor5-footnote/src/footnoteEditing/googleDocsFootnotesNormalizer.js
+++ public/lesswrong-editor/src/ckeditor5-footnote/src/footnoteEditing/googleDocsFootnotesNormalizer.js
@@ -25,7 +25,7 @@ export default class GoogleDocsFootnotesNormalizer {
 				id: /^ftnt_ref\d+$/
 			}
 		});
-		const backlinks = {};
+		const backLinks = {};
 		const references = {};
 		// @ts-ignore: DefinitelyTyped mistypes the return values of UpcastWriters range events
 		// as TypeScript Ranges, rather than CKEditor Ranges.
@@ -33,15 +33,19 @@ export default class GoogleDocsFootnotesNormalizer {
 			const { item, type } = value
 			if (type === 'elementStart' && footnoteReferenceMatcher.match(item)) {
 				const index = item.getAttribute('id').match(/ftnt_ref(\d+)/)[1];
-				references[index] = 
+				references[index] = {item, id: backLinks[index].id};
+			}
+		}
+		// @ts-ignore DefinitelyTyped mistyping of Range
+		for ( const value of documentRange ) {
+			const { item, type } = value
+			if (type === 'elementStart' && footnoteBacklinkMatcher.match(item)) {
+				const index = item.getAttribute('id').match(/ftnt(\d+)/)[1];
+				backLinks[index] = 
 					{ 
 						item,
 						id: Math.random().toString(36).slice(2),
 					};
-			}
-			if (type === 'elementStart' && footnoteBacklinkMatcher.match(item)) {
-				const index = item.getAttribute('id').match(/ftnt(\d+)/)[1];
-				backlinks[index] = {item, id: references[index].id};
 			}
 		}
 		for (const [ index, { item, id } ] of Object.entries(references)) {
@@ -49,54 +53,77 @@ export default class GoogleDocsFootnotesNormalizer {
 			writer.setAttribute(ATTRIBUTES.footnoteIndex, index, item);
 			writer.setAttribute(ATTRIBUTES.footnoteId, id, item);
 		}
-		if(!Object.keys(backlinks).length) {
+		if(!Object.keys(backLinks).length) {
 			return;
 		}
-		const previousItem = backlinks[1].item.parent.parent.previousSibling
+		if(!backLinks[1] || !backLinks[1].item) {
+			console.warn("Unexpected DOM structure: missing footnote with index 1.");
+			return;
+		}
+		const firstBacklink = backLinks[1].item;
+		if(
+			!firstBacklink.parent ||
+			!firstBacklink.parent.parent
+		) {
+			console.error("Unexpected DOM structure: expected footnote backlink to be nested within two parent tags.")
+			return;
+		}
+		const firstFootnote = firstBacklink.parent.parent;
+		const firstFootnoteIndex = firstFootnote.index;
+		const { previousSibling } = firstFootnote; 
 		const footnoteSectionRange = writer.createRange(
- 			writer.createPositionAfter(previousItem),
+ 			writer.createPositionBefore(firstFootnote),
 			// @ts-ignore: DefinitelyTyped mistyping of Range
  			writer.createPositionAt(documentRange.end),
 		)
-		const footnoteItems = Object.entries(backlinks).map(([ index, {item, id}]) => {
-			const end = backlinks.hasOwnProperty(parseInt(index) + 1) ? 
-				writer.createPositionBefore(backlinks[parseInt(index) + 1].item.parent.parent) :
-				// @ts-ignore: DefinitelyTyped mistyping of Range
-				documentRange.end;
+		const footnoteSectionParent = firstFootnote.parent;
+		if(!footnoteSectionParent) {
+			console.error("Unexpected DOM structure: expected footnote to have a parent element.")
+			return;
+		}
+		const footnoteItems = Object.entries(backLinks).map(([ index, {item, id}]) => {
+			if(!item.parent || !item.parent.parent) {
+				throw new Error("Unexpected Dom structure; expected footnote backLink to be nested within two parent tags.")
+			}
+			const footnoteContent = item.parent.parent;
 			const footnoteItemRange = writer.createRange(
 				writer.createPositionAfter(item),
-				end
+				writer.createPositionAfter(footnoteContent),
 			)
 			const footnoteBackLink = writer.createElement('span', {
 				[ATTRIBUTES.footnoteBackLink]: '',
 				[ATTRIBUTES.footnoteId]: id,
 			});
 			const footnoteContents = writer.createElement('div', 
-			{
-				[ATTRIBUTES.footnoteContent]: '',
-				[ATTRIBUTES.footnoteId]: id,
-				[ATTRIBUTES.footnoteIndex]: index,
-			}, 
-			// @ts-ignore DefinitelyTyped mistyping of Range
-			[...footnoteItemRange.getItems({ shallow: true})]
-				.filter(item => item.is('element'))
-				.map(element => writer.clone(element, true))
-		)
+				{
+					[ATTRIBUTES.footnoteContent]: '',
+					[ATTRIBUTES.footnoteId]: id,
+					[ATTRIBUTES.footnoteIndex]: index,
+				}, 
+				// @ts-ignore DefinitelyTyped mistyping of Range
+				[...footnoteItemRange.getItems({ shallow: true})]
+					.filter(item => item.is('element'))
+					.map(element => writer.clone(element, true)),
+			);
 			return writer.createElement('div', { 
 				[ATTRIBUTES.footnoteItem]: '',
 				[ATTRIBUTES.footnoteId]: id,
 				[ATTRIBUTES.footnoteIndex]: index,
-			}, [footnoteBackLink, footnoteContents])
-		})
+			}, [footnoteBackLink, footnoteContents]);
+		});
 
-		const footnoteSection = writer.createElement('div', {[ATTRIBUTES.footnoteSection]: ''}, footnoteItems)
+		const footnoteSection = writer.createElement('div', {[ATTRIBUTES.footnoteSection]: ''}, footnoteItems);
 
 		// @ts-ignore DefinitelyTyped mistyping of Range
 		for(const item of [...footnoteSectionRange.getItems()]) {
 			if (item.is('element')) {
-				writer.remove(item)
+				writer.remove(item);
 			}
 		}
-		writer.replace(previousItem, footnoteSection)
+		if(previousSibling && previousSibling.name === 'hr') {
+			writer.replace(previousSibling, footnoteSection);
+		} else {
+			writer.insertChild(firstFootnoteIndex, footnoteSection, footnoteSectionParent)
+		}
 	}
 }
