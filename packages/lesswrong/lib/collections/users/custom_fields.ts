56b01c82fe4df007ebea17a7a5ab98de8201a4e3
Sarah Cheng <sarah.cheng@centreforeffectivealtruism.org>
2022-07-06 20:28:14 -0400

    [EA Forum only] remove post author card

--- packages/lesswrong/lib/collections/users/custom_fields.ts
+++ packages/lesswrong/lib/collections/users/custom_fields.ts
@@ -261,6 +261,8 @@ addFieldsDict(Users, {
     order: 69,
   },
   
+  // We tested this on the EA Forum and it didn't encourage more PMs, but it led to some profile views.
+  // Hiding for now, will probably delete or test another version in the future.
   showPostAuthorCard: {
     type: Boolean,
     optional: true,
@@ -268,7 +270,7 @@ addFieldsDict(Users, {
     canRead: ['guests'],
     canUpdate: [userOwns, 'sunshineRegiment', 'admins'],
     canCreate: ['members'],
-    hidden: forumTypeSetting.get() !== 'EAForum',
+    hidden: true,
     control: 'checkbox',
     group: formGroups.siteCustomizations,
     order: 70,
