168cf4751b5ad98417013b990cd9c23212bbdfee
Raemon <raemon777@gmail.com>
2022-07-07 02:48:19 -0700

    properly set collection.title

--- packages/lesswrong/lib/generated/fragmentTypes.d.ts
+++ packages/lesswrong/lib/generated/fragmentTypes.d.ts
@@ -1260,6 +1260,11 @@ interface SequencesPageTitleFragment { // fragment on Sequences
   readonly _id: string,
   readonly title: string,
   readonly canonicalCollectionSlug: string,
+  readonly canonicalCollection: SequencesPageTitleFragment_canonicalCollection|null,
+}
+
+interface SequencesPageTitleFragment_canonicalCollection { // fragment on Collections
+  readonly title: string,
 }
 
 interface SequencesPageFragment extends SequencesPageTitleFragment { // fragment on Sequences
