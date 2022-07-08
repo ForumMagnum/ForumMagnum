e481bfeb5a68bdf1540355d0afa455fbb7ac6747
jcmustin <jonathan.mustin@gmail.com>
2022-07-08 14:26:54 -0700

    Add documentation

--- public/lesswrong-editor/src/ckeditor5-footnote/src/footnoteEditing/googleDocsFootnotesNormalizer.js
+++ public/lesswrong-editor/src/ckeditor5-footnote/src/footnoteEditing/googleDocsFootnotesNormalizer.js
@@ -5,15 +5,34 @@ import Matcher from '@ckeditor/ckeditor5-engine/src/view/matcher';
 import { ATTRIBUTES } from '../constants';
 import Range from '@ckeditor/ckeditor5-engine/src/view/range';
 import Element from '@ckeditor/ckeditor5-engine/src/view/element';
+import Writer from '@ckeditor/ckeditor5-engine/src/view/upcastwriter';
+import Document from '@ckeditor/ckeditor5-engine/src/view/document';
 
+/**
+ * This class is used for converting footnotes pasted in from Google Docs' Publish to Web view.
+ * Using Publish to Web is necessary because the default view doesn't let you select a document
+ * and its footnotes at the same time.
+ *
+ * Roughly, here's how this works: pasted text gets converted from an html string into view elements,
+ * which then get upcasted to model elements. This class gets injected at the view element stage,
+ * using the rarely-needed UpcastWriter to transform the data in preparation for the upcasting
+ * step. Specifically, this means adding attributes to each element — both element-type-specifying 
+ * attributes like `ATTRIBUTES.footnoteContent`, and context attributes like IDs and indices.
+ * From there, the upcast converters can in `converters.js` can do the rest.
+ */
 export default class GoogleDocsFootnotesNormalizer {
 	isActive() {
 		return true;
 	}
+	/**
+	 * 
+	 * @param {{content: {document: Document}}} data 
+	 */
 	execute(data) {
 		const writer = new UpcastWriter( data.content.document );
-
 		/**
+		 * Creates a range that spans the entire pasted text. Used
+		 * for traversing.
 		 * @type {Range}
 		*/
 		// @ts-ignore: DefinitelyTyped mistypes the return values of UpcastWriter's range events
@@ -63,42 +82,7 @@ export default class GoogleDocsFootnotesNormalizer {
 			console.error("Unexpected DOM structure: expected footnote to have a parent element.")
 			return;
 		}
-		const footnoteItems = Object.entries(backLinks).map(([ index, {item, id}]) => {
-			if(!item.parent || !item.parent.parent || !item.parent.parent.is('element')) {
-				throw new Error("Unexpected Dom structure; expected footnote backLink to be nested within two parent tags.")
-			}
-			const footnoteContent = item.parent.parent;
-			/**
-			 * @type {Range}
-			*/
-			// @ts-ignore DefinitelyTyped mistyping of Range
-			const footnoteItemRange = writer.createRange(
-				writer.createPositionAfter(item),
-				writer.createPositionAfter(footnoteContent),
-			)
-			const footnoteBackLink = writer.createElement('span', {
-				[ATTRIBUTES.footnoteBackLink]: '',
-				[ATTRIBUTES.footnoteId]: id,
-			});
-			const footnoteContents = writer.createElement('div', 
-				{
-					[ATTRIBUTES.footnoteContent]: '',
-					[ATTRIBUTES.footnoteId]: id,
-					[ATTRIBUTES.footnoteIndex]: index,
-				}, 
-				// @ts-ignore: startPosition isn't reuqired here.
-				[...footnoteItemRange.getItems({ shallow: true})]
-					.filter(item => item.is('element'))
-					// @ts-ignore: previous line is a type guard.
-					.map(element => writer.clone(element, true)),
-			);
-			return writer.createElement('div', { 
-				[ATTRIBUTES.footnoteItem]: '',
-				[ATTRIBUTES.footnoteId]: id,
-				[ATTRIBUTES.footnoteIndex]: index,
-			}, [footnoteBackLink, footnoteContents]);
-		});
-
+		const footnoteItems = this._createFootnoteItems(writer, backLinks)
 		const footnoteSection = writer.createElement('div', {[ATTRIBUTES.footnoteSection]: ''}, footnoteItems);
 
 		for(const item of [...footnoteSectionRange.getItems()]) {
@@ -114,7 +98,8 @@ export default class GoogleDocsFootnotesNormalizer {
 	}
 
 	/**
-	 * 
+	 * Finds all elements with an id indicating they're a footnote backLink.
+	 * Also generates a random alphanumeric footnote id for each.
 	 * @param {Range} documentRange
 	 * @returns {Object.<string, {item: Element, id: string}>}
 	 */
@@ -139,7 +124,9 @@ export default class GoogleDocsFootnotesNormalizer {
 	}
 
 	/**
-	 * 
+	 * Finds all anchor tags with an id indicating they're a footnote reference, and
+	 * with a backLink that has a matching index. (references to nonexistent footnotes
+	 * aren't parsed.)
 	 * @param {Range} documentRange
 	 * @param {Object.<string, {item: Element, id: string}>} backLinks
 	 * @returns {Object.<string, {item: Element, id: string}>}
@@ -168,5 +155,59 @@ export default class GoogleDocsFootnotesNormalizer {
 			}
 			return acc;
 		}, {})
+	}
+
+	/**
+	 * For each unprocessed backLink, creates a new element composed of a
+	 * footnoteBackLink element and a footnoteContent element, all wrapped in a footnoteItem.
+	 *
+	 * Presumes a structure s.t. each backlink is wrapped in two elements, the outer of which
+	 * contains the entire contents of the footnote.
+	 *
+	 * @param {Writer} writer
+	 * @param {Object.<string, {item: Element, id: string}>} backLinks 
+	 * @returns {Element[]}
+	 */
+	_createFootnoteItems(writer, backLinks) {
+		return Object.entries(backLinks).map(([ index, {item, id}]) => {
+			if(!item.parent || !item.parent.parent || !item.parent.parent.is('element')) {
+				throw new Error("Unexpected Dom structure; expected footnote backLink to be nested within two parent tags.")
+			}
+			const footnoteContent = item.parent.parent;
+			/**
+			 * These range bounds (start after the backLink, end
+			 * after the footnote content) are used to capture
+			 * all of the footnote except the backLink, which we
+			 * generate ourselves.
+			 * @type {Range}
+			*/
+			// @ts-ignore DefinitelyTyped mistyping of Range
+			const footnoteContentRange = writer.createRange(
+				writer.createPositionAfter(item),
+				writer.createPositionAfter(footnoteContent),
+			)
+			const newFootnoteBackLink = writer.createElement('span', {
+				[ATTRIBUTES.footnoteBackLink]: '',
+				[ATTRIBUTES.footnoteId]: id,
+			});
+			const newFootnoteContent = writer.createElement('div', 
+				{
+					[ATTRIBUTES.footnoteContent]: '',
+					[ATTRIBUTES.footnoteId]: id,
+					[ATTRIBUTES.footnoteIndex]: index,
+				},
+				// @ts-ignore: startPosition isn't required here.
+				[...footnoteContentRange.getItems({ shallow: true})]
+					.filter(item => item.is('element'))
+					// @ts-ignore: previous line is a type guard.
+					.map(element => writer.clone(element, true)),
+			);
+			return writer.createElement('div', { 
+				[ATTRIBUTES.footnoteItem]: '',
+				[ATTRIBUTES.footnoteId]: id,
+				[ATTRIBUTES.footnoteIndex]: index,
+			}, [newFootnoteBackLink, newFootnoteContent]);
+		});
+
 	}
 }
