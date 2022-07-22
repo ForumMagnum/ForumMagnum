3c793763566c499dc815f472d9305ebbca4e9759
JP Addison <johnpaddison@gmail.com>
2022-07-22 18:16:29 -0400

    reset instead of clear store, but always push ourselves to new url

--- packages/lesswrong/components/users/UsersEditForm.tsx
+++ packages/lesswrong/components/users/UsersEditForm.tsx
@@ -101,8 +101,11 @@ const UsersEditForm = ({terms, classes}: {
         hideFields={["paymentEmail", "paymentInfo"]}
         successCallback={async (user) => {
           flash(`User "${userGetDisplayName(user)}" edited`);
-          await client.clearStore()
-          history.push(userGetProfileUrl(user));
+          try {
+            await client.resetStore()
+          } finally {
+            history.push(userGetProfileUrl(user))
+          }
         }}
         queryFragment={getFragment('UsersEdit')}
         mutationFragment={getFragment('UsersEdit')}
