49ca6c2c615e28bcba47bbc8efa192d9240ca3d0
James Babcock <jimrandomh@gmail.com>
2022-08-03 10:55:14 -0700

    Fix Cypress test failures

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,14 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
   watchForFileChanges: true,
   defaultCommandTimeout: 15000,
+  
+  // Disable frame isolation. Something (maybe in Sentry or Intercom?) tries to
+  // make a cross-frame request, which fails because (I think) the test
+  // environment isn't https, or something like that. This caused some tests to
+  // fail that were clearly things that were working. TODO: Figure out why this
+  // is necessary and maybe take it out.
+  chromeWebSecurity: false,
+  
   retries: {
     runMode: 2,
     openMode: 0,
