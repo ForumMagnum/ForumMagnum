3aebc59e87546869b45dd3b86f7a7d88b4c997ee
Ollie Etherington <ollie@etherington.xyz>
2022-08-05 11:00:03 -0700

    Show curated posts on the recommendations page

--- packages/lesswrong/components/recommendations/RecommendationsPage.tsx
+++ packages/lesswrong/components/recommendations/RecommendationsPage.tsx
@@ -1,18 +1,15 @@
 import React from 'react';
 import { Components, registerComponent } from '../../lib/vulcan-lib';
 import {AnalyticsContext} from "../../lib/analyticsEvents";
-import { forumTypeSetting } from '../../lib/instanceSettings';
 
 const RecommendationsPage = ({classes}: {
   classes: ClassesType
 }) => {
   const { ConfigurableRecommendationsList, RecommendationsPageCuratedList } = Components;
 
-  const isLW = forumTypeSetting.get() === 'LessWrong'
-
   return (
     <AnalyticsContext pageSectionContext={"recommendationsPage"} capturePostItemOnMount>
-      {isLW && <RecommendationsPageCuratedList/>}
+      <RecommendationsPageCuratedList/>
       <ConfigurableRecommendationsList configName="recommendationspage" />
     </AnalyticsContext>
   )
