74422f42ccc73f9706a5b4b7a959aa39f945f0fe
Raemon <raemon777@gmail.com>
2022-08-23 17:16:53 -0700

    hide recommendations from lw

--- packages/lesswrong/components/recommendations/RecommendationsAndCurated.tsx
+++ packages/lesswrong/components/recommendations/RecommendationsAndCurated.tsx
@@ -211,6 +211,8 @@ const RecommendationsAndCurated = ({
     const renderBookmarks = ((currentUser?.bookmarkedPostsMetadata?.length || 0) > 0) && !settings.hideBookmarks
     const renderContinueReading = currentUser && (continueReading?.length > 0) && !settings.hideContinueReading
     
+    const renderRecommendations = !settings.hideFrontpage && forumTypeSetting.get() !== "LessWrong" // temporarily hiding from LessWrong during ACX Everywhere season
+
     const bookmarksLimit = (settings.hideFrontpage && settings.hideContinueReading) ? 6 : 3 
 
     return <SingleColumnSection className={classes.section}>
@@ -250,7 +252,7 @@ const RecommendationsAndCurated = ({
 
         <div className={classes.subsection}>
           <div className={classes.posts}>
-            {!settings.hideFrontpage && 
+            {renderRecommendations && 
               <AnalyticsContext listContext="frontpageFromTheArchives" pageSubSectionContext="frontpageFromTheArchives" capturePostItemOnMount>
                 <RecommendationsList algorithm={frontpageRecommendationSettings} />
               </AnalyticsContext>
