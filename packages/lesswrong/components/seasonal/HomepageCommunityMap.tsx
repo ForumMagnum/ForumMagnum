9f0e43dee607be2bbf5c5a18c9ed7ec77c75900f
Raemon <raemon777@gmail.com>
2022-08-23 17:00:52 -0700

    add map to frontpage

---
+++ packages/lesswrong/components/seasonal/HomepageCommunityMap.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { useUserLocation } from '../../lib/collections/users/helpers';
+import { forumTypeSetting } from '../../lib/instanceSettings';
+import { registerComponent, Components } from '../../lib/vulcan-lib';
+import { useCurrentUser } from '../common/withUser';
+
+const styles = (theme: ThemeType): JssStyles => ({
+  root: {
+
+  }
+});
+
+export const HomepageCommunityMap = ({classes}: {
+  classes: ClassesType,
+}) => {
+  const { CommunityMapWrapper } = Components
+
+  const currentUser = useCurrentUser()
+  const currentUserLocation = useUserLocation(currentUser)
+  const mapEventTerms: PostsViewTerms = {
+    view: 'events',
+    filters: [],
+  }
+
+  if (forumTypeSetting.get() !== "LessWrong") return null
+
+  return <div className={classes.root}>
+    <CommunityMapWrapper
+      terms={mapEventTerms}
+      mapOptions={currentUserLocation.known && {center: currentUserLocation, zoom: 5}}
+      showUsers
+    />
+  </div>;
+}
+
+const HomepageCommunityMapComponent = registerComponent('HomepageCommunityMap', HomepageCommunityMap, {styles});
+
+declare global {
+  interface ComponentTypes {
+    HomepageCommunityMap: typeof HomepageCommunityMapComponent
+  }
+}
+
