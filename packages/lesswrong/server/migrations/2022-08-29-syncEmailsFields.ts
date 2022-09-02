ec697bf9fd76e65b0317604497fc9930204832be
Raemon <raemon777@gmail.com>
2022-09-02 16:26:47 -0700

    add messageUsersABoutMerging migration

--- packages/lesswrong/server/migrations/2022-08-29-syncEmailsFields.ts
+++ packages/lesswrong/server/migrations/2022-08-29-syncEmailsFields.ts
@@ -1,40 +1,53 @@
+import React from 'react';
 import { registerMigration } from './migrationUtils';
 import Users from '../../lib/collections/users/collection';
+import { wrapAndSendEmail } from '../emails/renderEmail';
+import { userGetProfileUrl } from '../../lib/collections/users/helpers';
+import Conversations from '../../lib/collections/conversations/collection';
+import { createMutator } from '../vulcan-lib/mutators';
+import Messages from '../../lib/collections/messages/collection';
 
 registerMigration({
   name: "syncEmailsFields",
   dateWritten: "2022-08-29",
   idempotent: true,
   action: async () => {
-    const usersMissingEmail = await Users.find({email: null, emails:{$exists:true}}).fetch()
+    const totalUsersMissingEmail =  await Users.find({email: null})
+    const totalRecentUsersMissingEmail =  await Users.find({email: null, lastNotificationsCheck: {$exists:true}})
+
+    console.log(`There are ${totalUsersMissingEmail.count()} users missing an email`)
+    console.log(`There are ${totalRecentUsersMissingEmail.count()} users missing an email who've checked notifications in past 3 years`)
+
+    const usersEmailsToEmail = await Users.find({email: null, emails:{$ne:null}}).fetch()
+    const usersGoogleToEmail = await Users.find({email: null, 'services.google.email':{$ne:null}}).fetch()
+    const usersGithubToEmail = await Users.find({email: null, 'services.github.email':{$ne:null}}).fetch()
+    const usersFacebookToEmail = await Users.find({email: null, 'services.facebook.email':{$ne:null}}).fetch()
+    
+    console.log(`There are ${usersEmailsToEmail.length} users who's emails.address fields could fill in email`)
+    console.log(`There are ${usersGoogleToEmail.length} users who's google address could fill in email`)
+    console.log(`There are ${usersGithubToEmail.length} users who's github address fields could fill in email`)
+    console.log(`There are ${usersFacebookToEmail.length} users who's facebook address fields could fill in email`)
+
+    const usersMissingEmails = [...usersEmailsToEmail, ...usersGoogleToEmail, ...usersGithubToEmail, ...usersFacebookToEmail]
 
     // eslint-disable-next-line no-console
-    console.log(`setting emails for ${usersMissingEmail.length} users`)
-
-    let syncedUserCount = 0 
-
-    const duplicateEmails: Array<string> = [] 
-
-    for (const user of usersMissingEmail) {
+    console.log(`setting emails for ${usersMissingEmails.length} users`)
+    for (const user of usersMissingEmails) {
       const firstEmail = user.emails?.[0]
-      const email = firstEmail?.address ?? firstEmail?.value
+      const email = firstEmail?.address || firstEmail?.value || user.services?.google?.email || user.services?.facebook?.email || user.services?.github?.email
+
       if (email) {
-        const duplicateUsers = await Users.find({email}).fetch()
-        if (duplicateUsers.length === 0) {
-          await Users.rawUpdateOne({_id: user._id}, {$set: {email}})
-          // eslint-disable-next-line no-console
+          // await Users.rawUpdateOne({_id: user._id}, {$set: {email}})
           console.log("setting email to:", email)
-          syncedUserCount += 1
-        } else {
-          duplicateEmails.push(email)
-        }
       }
     }
+
     // eslint-disable-next-line no-console
-    console.log(`Synced users: ${syncedUserCount}`)
-    // eslint-disable-next-line no-console
-    console.log(`Users who were not synced due to duplicate emails: ${usersMissingEmail.length - syncedUserCount}`)
-    // eslint-disable-next-line no-console
-    console.log(duplicateEmails)
+    const newNumberOfMissingEmailsToEmail = await Users.find({email: null, emails:{$ne:null}}).fetch()
+    console.log(`Synced users: ${newNumberOfMissingEmailsToEmail.length}`)
+    console.log(`done`)
+
+
+
   }  
 })
---
+++ packages/lesswrong/server/migrations/2022-09-02-messageUsersAboutMerging.ts
@@ -0,0 +1,81 @@
+import { registerMigration } from './migrationUtils';
+import Users from '../../lib/collections/users/collection';
+import { userGetProfileUrl } from '../../lib/collections/users/helpers';
+import Conversations from '../../lib/collections/conversations/collection';
+import { createMutator } from '../vulcan-lib/mutators';
+import Messages from '../../lib/collections/messages/collection';
+
+
+const sendDuplicateEmailMessage = async (email) => {
+  const currentUser = await Users.findOne({displayName:"RubyTest"})
+  const users = await Users.find({email}).fetch()
+  if (users.length < 2 || !currentUser) return
+  const user = users[0]
+  const userIds = users.map(user => user._id)
+  const adminUserIds = [
+    'r38pkCm7wF4M44MDQ', // Raemon
+    'grecHJcgkb3KW5wnM', // RobertM
+    'qgdGA4ZEyW7zNdK84' // Ruby
+  ]
+  const participantIds = [...userIds, ...adminUserIds]
+  
+  // eslint-disable-next-line no-console
+  console.log(`Sending messages to ${participantIds}`)
+
+  const conversationData = {
+    participantIds: participantIds,
+    title: `Duplicate accounts with a shared email`
+  }
+  const conversation = await createMutator({
+    collection: Conversations,
+    document: conversationData,
+    currentUser: currentUser,
+    validate: false
+  });
+  const userProfiles = users.map(user=>`<li key=${user._id}><a href=${userGetProfileUrl(user)}>${user.displayName}</a></li>`).join("")
+  const message = `
+    <p>Hey ${user.displayName},</p>
+    <p>You've got multiple LessWrong accounts sharing the email ${user.email}. They are:</p>
+    <ul>
+      ${userProfiles}
+    </ul>
+    <p>I'm guessing you ended up with multiple accounts accidentally. We can merge them into a single user (which would then be listed as the author of all the posts, comments, and have the appropriate karma). Which user do you prefer to get merged into? (essentially this just means "which username do you want to keep?)</p>
+  `
+
+  const messageData = {
+    userId: currentUser._id,
+    contents: {
+      originalContents: {
+        type: "html",
+        data: message
+      }
+    },
+    conversationId: conversation.data._id
+  }
+
+  await createMutator({
+    collection: Messages,
+    document: messageData,
+    currentUser,
+    validate: false
+  })
+}
+
+registerMigration({
+  name: "messageUsersAboutMerging",
+  dateWritten: "2022-08-29",
+  idempotent: true,
+  action: async () => {
+    const emails = await Users.aggregate([
+      {"$match":   { "lastNotificationsCheck": {$exists: true}, "karma": {$gte:0}}}, // filter out users who aren't actively involved with LW or aren actively bad
+      {"$group" :  { "_id": "$email", "count": { "$sum": 1 } } },
+      {"$match":   {"_id" :{ "$ne" : null } , "count" : {"$gt": 1} }},
+      {"$sort":    {"count": -1}},
+      {"$project": {"email" : "$_id", "_id" : 0} }
+    ]).toArray()
+    
+    console.log("EMAILS:", emails)
+
+    await sendDuplicateEmailMessage("raemon777@gmail.com")
+  }
+})
