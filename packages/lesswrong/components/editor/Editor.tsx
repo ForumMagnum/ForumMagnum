ForumMagnum/ForumMagnum

commit f0cb371a81e0bfc89eb4fc158183612871e44bd9

fix ck editor performance for large documents, and use native ckeditor placeholder functionality

packages/lesswrong/components/editor/Editor.tsx
@@ -281,7 +281,7 @@ export class Editor extends Component<EditorProps,EditorComponentState> {
       markdownImgErrs: false
     }
     
-    this.throttledSetCkEditor = _.throttle((value) => this.setContents("ckEditorMarkup", value), autosaveInterval);
+    this.throttledSetCkEditor = _.debounce((getValue) => this.setContents("ckEditorMarkup", getValue()), autosaveInterval);
     this.debouncedCheckMarkdownImgErrs = _.debounce(this.checkMarkdownImgErrs, checkImgErrsInterval);
   }
 
@@ -436,18 +436,8 @@ export class Editor extends Component<EditorProps,EditorComponentState> {
         formType: formType,
         userId: currentUser?._id,
         onChange: (event, editor) => {
-          const data: string = editor.getData();
-          // If transitioning from empty to nonempty or nonempty to empty,
-          // bypass throttling. These cases don't have the performance
-          // implications that motivated having throttling in the first place,
-          // and this prevents an annoying delay in the blank-document
-          // placeholder text appearing/disappeaering.
-          if (isBlank({type: "ckEditorMarkup", value: data}) || isBlank(this.props.value)) {
-            this.throttledSetCkEditor.cancel();
-            this.setContents("ckEditorMarkup", data);
-          } else {
-            this.throttledSetCkEditor(data)
-          }
+          // The getData call needs to be wrapped in a closure to avoid `this` reference errors
+          this.throttledSetCkEditor(() => editor.getData());
         },
         onInit: editor => this.setState({ckEditorReference: editor})
       }
@@ -457,10 +447,7 @@ export class Editor extends Component<EditorProps,EditorComponentState> {
       // requires _id because before the draft is saved, ckEditor loses track of what you were writing when turning collaborate on and off (and, meanwhile, you can't actually link people to a shared draft before it's saved anyhow)
       // TODO: figure out a better solution to this problem.
 
-      const showPlaceholder = isBlank({type: "ckEditorMarkup", value});
-
       return <div className={classNames(this.getHeightClass(), classes.ckEditorStyles)}>
-        { this.renderPlaceholder(showPlaceholder, isCollaborative)}
         { isCollaborative
           ? <Components.CKPostEditor key="ck-collaborate"
               {...editorProps}

packages/lesswrong/components/editor/CKCommentEditor.tsx
@@ -5,6 +5,7 @@ import { getCkEditor, ckEditorBundleVersion } from '../../lib/wrapCkEditor';
 import { generateTokenRequest } from '../../lib/ckEditorUtils';
 import { ckEditorUploadUrlSetting, ckEditorWebsocketUrlSetting } from '../../lib/publicSettings'
 import { ckEditorUploadUrlOverrideSetting, ckEditorWebsocketUrlOverrideSetting } from '../../lib/instanceSettings';
+import { defaultEditorPlaceholder } from '../../lib/editor/make_editable';
 
 // Uncomment the import and the line below to activate the debugger
 // import CKEditorInspector from '@ckeditor/ckeditor5-inspector';
@@ -48,7 +49,8 @@ const CKCommentEditor = ({ data, collectionName, fieldName, onSave, onChange, on
             return onSave && onSave( editor.getData() )
           }
         },
-        initialData: data || ""
+        initialData: data || "",
+        placeholder: defaultEditorPlaceholder
       }}
       data={data}
     />

packages/lesswrong/components/editor/CKPostEditor.tsx
@@ -7,6 +7,7 @@ import { CollaborativeEditingAccessLevel, accessLevelCan } from '../../lib/colle
 import { ckEditorUploadUrlSetting, ckEditorWebsocketUrlSetting } from '../../lib/publicSettings'
 import { ckEditorUploadUrlOverrideSetting, ckEditorWebsocketUrlOverrideSetting } from '../../lib/instanceSettings';
 import { CollaborationMode } from './EditorTopBar';
+import { defaultEditorPlaceholder } from '../../lib/editor/make_editable';
 
 // Uncomment this line and the reference below to activate the CKEditor debugger
 // import CKEditorInspector from '@ckeditor/ckeditor5-inspector';
@@ -176,7 +177,8 @@ const CKPostEditor = ({ data, collectionName, fieldName, onSave, onChange, docum
         presenceList: {
           container: presenceListRef.current
         },
-        initialData: initData
+        initialData: initData,
+        placeholder: defaultEditorPlaceholder
       }}
     />}
   </div>

packages/lesswrong/lib/editor/make_editable.tsx
@@ -68,6 +68,10 @@ const defaultOptions: MakeEditableOptions = {
   revisionsHaveCommitMessages: false,
 }
 
+export const defaultEditorPlaceholder = `Write here. Select text for formatting options.
+We support LaTeX: Cmd-4 for inline, Cmd-M for block-level (Ctrl on Windows).
+You can switch between rich text and markdown in your user settings.`
+
 export const editableCollections = new Set<CollectionNameString>()
 export const editableCollectionsFields: Record<CollectionNameString,Array<string>> = {} as any;
 export const editableCollectionsFieldOptions: Record<CollectionNameString,Record<string,MakeEditableOptions>> = {} as any;

packages/lesswrong/themes/stylePiping.ts
@@ -443,6 +443,9 @@ export const ckEditorStyles = (theme: ThemeType): JssStyles => {
           ...hrStyles(theme)
         },
       },
+      '& .ck-placeholder:before': {
+        whiteSpace: 'break-spaces'
+      },
       '&.ck-sidebar, &.ck-presence-list': {
         '& li': {
           // By default ckEditor elements get the styles from postBodyStyles li elements

commit 0a112b5c620e3ea7468b085679785dad64420dc9

Fix a bug that caused editors in forms to not clear if submitted during the autosave delay window
Bug introduced in: https://github.com/ForumMagnum/ForumMagnum/commit/f0cb371a81e0bfc89eb4fc158183612871e44bd9
Caused by a subtle timing interaction between rerenders causing CkEditor
to update its contents, and CkEditor onChange events reporting state
changes. Deferring setValue() can cause exogenous editor changes to get
reverted.

packages/lesswrong/components/editor/Editor.tsx
@@ -444,8 +444,17 @@ export class Editor extends Component<EditorProps,EditorComponentState> {
         formType: formType,
         userId: currentUser?._id,
         onChange: (event, editor) => {
-          // The getData call needs to be wrapped in a closure to avoid `this` reference errors
-          this.throttledSetCkEditor(() => editor.getData());
+          // If transitioning from empty to nonempty or nonempty to empty,
+          // bypass throttling. These cases don't have the performance
+          // implications that motivated having throttling in the first place,
+          // and this prevents a timing bug with form-clearing on submit.
+          const data: string = editor.getData();
+          if (isBlank({type: "ckEditorMarkup", value: data}) || isBlank(this.props.value)) {
+            this.throttledSetCkEditor.cancel();
+            this.setContents("ckEditorMarkup", data);
+          } else {
+            this.throttledSetCkEditor(() => editor.getData())
+          }
         },
         onInit: editor => this.setState({ckEditorReference: editor})
       }