4c8b4d20d46826ad98d06f7f9dc0e9c989ab3d6d
JP Addison <johnpaddison@gmail.com>
2022-09-07 11:03:12 -0400

    extract function to measure height in js and show collapsed button if too high
    Co-authored-by: s-cheng <sarah.cheng@centreforeffectivealtruism.org>

---
+++ packages/lesswrong/components/common/useCheckMeritsCollapse.ts
@@ -0,0 +1,21 @@
+import React, { RefObject, useEffect, useRef, useState } from 'react';
+
+export function useCheckMeritsCollapse<T extends HTMLElement>(
+  {ref, height, deps}:
+  {ref: RefObject<T>, height: number, deps: any[]}
+) {
+  // this tracks whether the contents of the tab actually overflow
+  const [meritsCollapse, setMeritsCollapse] = useState(false)
+  const resizeObserver = useRef<ResizeObserver|null>(null)
+  useEffect(() => {
+    if (ref.current) {
+      // show/hide the collapse-related buttons depending on whether or not the content all fits
+      resizeObserver.current = new ResizeObserver(elements => {
+        setMeritsCollapse(Math.round(elements[0].contentRect.height) >= height)
+      })
+      resizeObserver.current.observe(ref.current)
+    }
+    return () => resizeObserver.current?.disconnect()
+  }, [ref, ...deps])
+  return meritsCollapse
+}
