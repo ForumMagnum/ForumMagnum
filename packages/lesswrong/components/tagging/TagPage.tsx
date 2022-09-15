1e75dd1ddbc06ef2a1215eefdcc1f910092f8d1a
Will Howard <w.howard256+github@gmail.com>
2022-09-15 17:27:58 +0100

    Allow setting an explicit descriptionTruncationCount on the EA Forum
    Co-authored-by: Sarah Cheng <sarah.cheng@centreforeffectivealtruism.org>

--- packages/lesswrong/components/tagging/TagPage.tsx
+++ packages/lesswrong/components/tagging/TagPage.tsx
@@ -15,6 +15,7 @@ import { MAX_COLUMN_WIDTH } from '../posts/PostsPage/PostsPage';
 import { EditTagForm } from './EditTagPage';
 import { useTagBySlug } from './useTag';
 import { forumTypeSetting, taggingNameCapitalSetting, taggingNamePluralCapitalSetting, taggingNamePluralSetting } from '../../lib/instanceSettings';
+import { min } from "lodash";
 
 const isEAForum = forumTypeSetting.get() === 'EAForum'
 
@@ -263,40 +264,44 @@ const TagPage = ({classes}: {
     captureEvent("readMoreClicked", {tagId: tag._id, tagName: tag.name, pageSectionContext: "wikiSection"})
   }
 
+  const readMoreHtml = "<span>...<p><a>(Read More)</a></p></span>"
   const htmlWithAnchors = tag.tableOfContents?.html ?? tag.description?.html ?? ""
   let description = htmlWithAnchors;
-  // EA Forum wants to truncate much less than LW
-  if(isEAForum) {
-    description = htmlWithAnchors;
-    for (let matchString of [
-        'id="Further_reading"',
-        'id="Bibliography"',
-        'id="Related_entries"',
-        'class="footnotes"',
-      ]) {
-      if(htmlWithAnchors.includes(matchString)) {
-        const truncationLength = htmlWithAnchors.indexOf(matchString);
-        /**
-         * The `truncate` method used below uses a complicated criterion for what
-         * counts as a character. Here, we want to truncate at a known index in
-         * the string. So rather than using `truncate`, we can slice the string
-         * at the desired index, use `parseFromString` to clean up the HTML,
-         * and then append our footer 'read more' element.
-         */
-        description = truncated ?
-          new DOMParser().parseFromString(
-            htmlWithAnchors.slice(0, truncationLength), 
-            'text/html'
-          ).body.innerHTML + "<span>...<p><a>(Read More)</a></p></span>" :
-          htmlWithAnchors;
-        break;
-      }
-    }
-  } else {
-    description = (truncated && !tag.wikiOnly)
-    ? truncate(htmlWithAnchors, tag.descriptionTruncationCount || 4, "paragraphs", "<span>...<p><a>(Read More)</a></p></span>")
+
+  // The default is to truncate after tag.descriptionTruncationCount or 4 paragraphs.
+  // The EA Forum prefers to truncate less so only truncate here if descriptionTruncationCount
+  // is set, otherwise truncate on specific breakpoints (e.g. "Further reading")
+  if (!isEAForum || tag.descriptionTruncationCount) {
+    description = truncated && !tag.wikiOnly
+    ? truncate(htmlWithAnchors, tag.descriptionTruncationCount || 4, "paragraphs", readMoreHtml)
     : htmlWithAnchors
   }
+
+  if(isEAForum && truncated) {
+    const breakpointArray = [
+      'id="Further_reading"',
+      'id="Bibliography"',
+      'id="Related_entries"',
+      'class="footnotes"',
+    ];
+    const matchedIndices = breakpointArray.map(matchString => htmlWithAnchors.indexOf(matchString)).filter(index => index > -1)
+    const truncationLength = matchedIndices.length > 0 ? min(matchedIndices) : 0;
+    if (truncationLength > 0) {
+      /**
+       * The `truncate` method used above uses a complicated criterion for what
+       * counts as a character. Here, we want to truncate at a known index in
+       * the string. So rather than using `truncate`, we can slice the string
+       * at the desired index, use `parseFromString` to clean up the HTML,
+       * and then append our footer 'read more' element.
+       */
+      const semanticallyTruncatedDesc =
+          new DOMParser().parseFromString(htmlWithAnchors.slice(0, truncationLength), "text/html").body.innerHTML +
+          readMoreHtml;
+      // description may already have been truncated (if descriptionTruncationCount is set), so only use semanticallyTruncatedDesc if it's actually shorter
+      description = description.length > semanticallyTruncatedDesc.length ? semanticallyTruncatedDesc : description;
+    }
+  }
+
   const headTagDescription = tag.description?.plaintextDescription || `All posts related to ${tag.name}, sorted by relevance`
   
   const tagFlagItemType = {
