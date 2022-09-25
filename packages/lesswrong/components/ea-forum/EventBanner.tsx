5b832c663449803ce186a5934974c65ff52cbff3
JP Addison <johnpaddison@gmail.com>
2022-09-25 19:44:46 -0400

    get higher resolution images because retina

--- packages/lesswrong/components/ea-forum/EventBanner.tsx
+++ packages/lesswrong/components/ea-forum/EventBanner.tsx
@@ -15,8 +15,8 @@ const mobileImageId = eventBannerMobileImageSetting.get()
 const desktopImageId = eventBannerDesktopImageSetting.get()
 const featuredPost = eventBannerLinkSetting.get()
 
-const mobileImage = `https://res.cloudinary.com/${container}/image/upload/w_${SECTION_WIDTH},h_${bannerHeight}/${mobileImageId}`
-const desktopImage = `https://res.cloudinary.com/${container}/image/upload/w_${SECTION_WIDTH},h_${bannerHeight}/${desktopImageId}`
+const mobileImage = `https://res.cloudinary.com/${container}/image/upload/w_${SECTION_WIDTH*2},h_${bannerHeight*2}/${mobileImageId}`
+const desktopImage = `https://res.cloudinary.com/${container}/image/upload/w_${SECTION_WIDTH*2},h_${bannerHeight*2}/${desktopImageId}`
 
 const styles = createStyles((theme: ThemeType): JssStyles => ({
   link: {
