23af957447b046722142bfdaf1e990cc07883f55
Robert Mushkatblat <bobert.mushky@gmail.com>
2022-09-30 17:31:52 -0700

    testing some changes

--- packages/lesswrong/components/posts/PostsEditForm.tsx
+++ packages/lesswrong/components/posts/PostsEditForm.tsx
@@ -85,9 +85,9 @@ const PostsEditForm = ({ documentId, classes }: {
   
   // If we have access to the post but only readonly access and only because
   // it's published, don't show the edit form.
-  if (document.userId !== currentUser._id && !userIsSharedOn(currentUser, document) && !userCanDo(currentUser, 'posts.edit.all')) {
-    return <Components.ErrorAccessDenied/>
-  }
+  // if (document.userId !== currentUser._id && !userIsSharedOn(currentUser, document) && !userCanDo(currentUser, 'posts.edit.all')) {
+  //   return <Components.ErrorAccessDenied/>
+  // }
 
   if (isNotHostedHere(document)) {
     return <ForeignCrosspostEditForm post={document} />;
