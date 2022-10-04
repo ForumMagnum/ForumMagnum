40827189d01ac160912aee304747e98b9da9f0dc
Will Howard <w.howard256+github@gmail.com>
2022-10-04 12:17:57 +0100

    Make algolia comment search work for subforums, topic discussion, events, groups (#5810)

--- packages/lesswrong/lib/collections/tags/schema.ts
+++ packages/lesswrong/lib/collections/tags/schema.ts
@@ -11,7 +11,7 @@ import { forumTypeSetting, taggingNamePluralSetting, taggingNameSetting } from '
 import { SORT_ORDER_OPTIONS, SettingsOption } from '../posts/sortOrderOptions';
 import omit from 'lodash/omit';
 import { formGroups } from './formGroups';
-import { TagCommentType } from '../comments/helpers';
+import { TagCommentType } from '../comments/types';
 
 addGraphQLSchema(`
   type TagContributor {
