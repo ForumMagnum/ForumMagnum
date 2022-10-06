fb662d92d76bf1f292c2f7082d917bd8a6056297
James Babcock <jimrandomh@gmail.com>
2022-10-05 22:12:38 -0700

    Fuzzy/partial matching of blockquotes for side-comments

--- packages/lesswrong/server/sideComments.ts
+++ packages/lesswrong/server/sideComments.ts
@@ -1,6 +1,30 @@
 import cheerio from 'cheerio';
 import { Comments } from '../lib/collections/comments/collection';
 
+export interface QuoteShardSettings {
+  minLength: number
+}
+const defaultQuoteShardSettings: QuoteShardSettings = {
+  minLength: 20,
+};
+
+
+/**
+ * Given HTML (probably for a post), make it so every top-level block element
+ * (ie, paragraphs, list items, tables, images) has an ID, adding new numeric
+ * IDs of the form "block123" if necessary.
+ *
+ * This is deterministic for a given HTML input, but is not guaranteed to be
+ * stable if the post is edited (ie, inserting a paragraph will increment the IDs
+ * of subsequent paragraphs). These IDs shouldn't be used for permalinks; for
+ * that, instead use the IDs attached to section-heading blocks by the table of
+ * contents generation code.
+ *
+ * These are used for side-comment alignment; that is, if a comment contains a
+ * blockquote that matches something in the post, this ID is used for tracking
+ * that match and potentially displaying a comment marker in
+ * the right margin.
+ */
 export function addBlockIDsToHTML(html: string): string {
   //@ts-ignore
   const parsedPost = cheerio.load(html, null, false);
@@ -14,42 +38,120 @@ export function addBlockIDsToHTML(html: string): string {
   return parsedPost.html();
 }
 
-export function getCommentQuotedBlockID(postHTML: string, commentHTML: string): string|null {
+/**
+ * Given the HTML of a post body which has IDs on every block (from
+ * addBlockIDsToHTML), and the HTML of a comment which might contain
+ * blockquotes taken from it, return the ID of the first block which the comment
+ * quotes from.
+ */
+export function getCommentQuotedBlockID(postHTML: string, commentHTML: string, options: QuoteShardSettings): string|null {
+  const quoteShards = commentToQuoteShards(commentHTML, options);
+  if (!quoteShards?.length) return null;
+  
   //@ts-ignore
   const parsedPost = cheerio.load(postHTML, null, false);
-  //@ts-ignore
+  
+  return findQuoteInPost(parsedPost, quoteShards);
+}
+
+/**
+ * Given a comment (as HTML) which might contain blockquotes, extract a list of
+ * "quote shards". A quote shard is an HTML substring which, if it matches an
+ * HTML substring found in the post, will cause the comment to match that part
+ * of the post.
+ *
+ * This incorporates some slightly complicated heuristics. In particular:
+ *  * Split paragraphs, list items, etc into their own blocks.
+ *  * If a blockquote-paragraph contains "..." (three dots), "…" (U+2026 Unicode
+ *    ellipsis character), or either of those wrapped in parens or square
+ *    brackets, split there.
+ *  * If there are matching square brackets containing up to 30 characters,
+ *    split into a before-the-bracket shard and an after-the-bracket shard.
+ *  * Discard shards shorter than 20 characters.
+ */
+export function commentToQuoteShards(commentHTML: string, options?: QuoteShardSettings): string[] {
+  options = options||defaultQuoteShardSettings;
+  const result: string[] = [];
+  
+  // Parse the HTML into cheerio
+  //@ts-ignore (cheerio type annotations sadly don't quite match the actual imported library)
   const parsedComment = cheerio.load(commentHTML, null, false);
   
-  return getParsedCommentQuotedBlockID(parsedPost, parsedComment);
-}
-
-function getParsedCommentQuotedBlockID(parsedPost, parsedComment): string|null {
+  // Find blockquote elements
   const blockquotes = parsedComment('blockquote');
   for (let i=0; i<blockquotes.length; i++) {
-    const blockquote = blockquotes[i];
-    const quotedBlockID = findQuoteBlockID(parsedPost, blockquote);
-    if (quotedBlockID) return quotedBlockID;
+    addQuoteShardsFromElement(result, cheerio(blockquotes[i]), options);
   }
   
-  return null;
+  return result;
 }
 
-export function findQuoteBlockID(parsedPost, blockquote): string|null {
-  const blockquoteText = cheerio(blockquote).text();
-  let markedElements = parsedPost('p,li,blockquote');
+function addQuoteShardsFromElement(outQuoteShards: string[], blockquoteElement: any, options: QuoteShardSettings): void {
+  // HACK: Rather than do this in full generality, we first assume that a
+  // blockquote element either contains only other block types or no block
+  // types.
+  // If the element has children that are of block types, recurse into them
+  let pos = blockquoteElement[0].firstChild;
+  let hasBlockChildren = false;
+  while (pos) {
+    if (pos.type==='tag' && ['p','li','blockquote'].includes(pos.name)) {
+      addQuoteShardsFromElement(outQuoteShards, cheerio(pos), options);
+      hasBlockChildren = true;
+    }
+    pos = pos.nextSibling;
+  }
+  if (hasBlockChildren) {
+    return;
+  }
   
+  // Split on ellipses
+  // HACK: Rather than handle this in full generality, we just ignore the case
+  // where ellipses may be inside child elements (eg an italicized section or
+  // a link caption). So we split the HTML *as strings*, which in that case
+  // produces mismatched tags. This is okay because quote shards are never
+  // rendered anywhere, only string-matched to produce a boolean result.
+  const quoteHtml = cheerio(blockquoteElement).html() || "";
+  
+  const ellipsizedSections = quoteHtml.split(/(\[\.\.\.\])|(\[\u2026\])|(\.\.\.)|(\u2026)/);
+  for (let section of ellipsizedSections) {
+    if (section) {
+      const trimmed = section.trim();
+      if (trimmed.length >= options.minLength) {
+        outQuoteShards.push(trimmed);
+      }
+    }
+  }
+}
+
+/**
+ * Given a post (as a cheerio parse tree) and a list of quote shards, return the
+ * ID of the first block which matches a quote shard (or null if no match is
+ * found).
+ */
+function findQuoteInPost(parsedPost, quoteShards: string[]): string|null {
+  let markedElements = parsedPost('p,li,blockquote');
   for (let i=0; i<markedElements.length; i++) {
     const blockID = cheerio(markedElements[i]).attr("id");
     if (blockID) {
-      const blockText = cheerio(markedElements[i]).text();
-      if(blockText.length>0 && blockquoteText===blockText) {
-        return blockID;
+      const markedHtml = cheerio(markedElements[i]).html()||"";
+      for (let quoteShard of quoteShards) {
+        if (markedHtml.indexOf(quoteShard) >= 0) {
+          return blockID;
+        }
       }
     }
   }
   return null;
 }
 
+/**
+ * Given a post, fetch all the comments on that post, check them for blockquotes,
+ * line those quotes up to sections of the post, and return a mapping from block
+ * IDs to arrays of comment IDs.
+ *
+ * This function is potentially quite slow, if there are a lot of comments and/or
+ * the post is very long. FIXME: Build caching for this.
+ */
 export async function getPostBlockCommentLists(context: ResolverContext, post: DbPost): Promise<Record<string,string[]>> {
   const postHTML = post.contents?.html;
   //@ts-ignore
@@ -63,13 +165,12 @@ export async function getPostBlockCommentLists(context: ResolverContext, post: D
   let result: Record<string,string[]> = {};
   for (let comment of comments) {
     //@ts-ignore
-    const parsedComment = cheerio.load(comment.contents?.html, null, false);
-    const blockID = getParsedCommentQuotedBlockID(parsedPost, parsedComment);
+    const quoteShards = commentToQuoteShards(comment.contents?.html);
+    const blockID = findQuoteInPost(parsedPost, quoteShards);
     if (blockID) {
       if (blockID in result) result[blockID].push(comment._id);
       else result[blockID] = [comment._id];
     }
   }
-  console.log("getPostBlockCommentLists: "+JSON.stringify(result));
   return result;
 }
--- packages/lesswrong/testing/sideComments.tests.ts
+++ packages/lesswrong/testing/sideComments.tests.ts
@@ -1,26 +1,65 @@
 import { testStartup } from './testMain';
 import chai from 'chai';
-import { getCommentQuotedBlockID } from '../server/sideComments';
+import { QuoteShardSettings, getCommentQuotedBlockID, commentToQuoteShards } from '../server/sideComments';
 
 testStartup();
+
+const testQuoteShardSettings: QuoteShardSettings = {
+  minLength: 5,
+};
 
 describe('side-comment blockquote matching', () => {
   it('finds exact-match blockquotes', () => {
     chai.assert.equal(
       getCommentQuotedBlockID(
-        '<p id="firstParagraph">Lorem ipsum</p>'
-        +'<p id="secondParagraph">dolor sit amet</p>',
-        '<blockquote>Lorem ipsum</blockquote><p>asdf</p>'
+        '<div><p id="firstParagraph">Lorem ipsum</p>'
+          + '<p id="secondParagraph">dolor sit amet</p></div>',
+        '<div><blockquote>Lorem ipsum</blockquote><p>asdf</p></div>',
+        testQuoteShardSettings
       ),
       'firstParagraph'
     );
     chai.assert.equal(
       getCommentQuotedBlockID(
-        '<p id="firstParagraph">Lorem ipsum</p>'
-        +'<p id="secondParagraph">dolor sit amet</p>',
-        '<blockquote>Lxxxm ixxxm</blockquote><p>asdf</p>'
+        '<div><p id="firstParagraph">Lorem ipsum</p>'
+          + '<p id="secondParagraph">dolor sit amet</p></div>',
+        '<div><blockquote>Lxxxm ixxxm</blockquote><p>asdf</p></div>',
+        testQuoteShardSettings
       ),
       null
+    );
+  });
+  
+  it('splits comments into quote shards', () => {
+    // Split on paragraphs
+    chai.assert.deepEqual(
+      commentToQuoteShards("<blockquote><p>Lorem ipsum dolor sit amet</p><p>adipiscing lorem amor asdf dolor sit amet</p></blockquote>"),
+      ["Lorem ipsum dolor sit amet", "adipiscing lorem amor asdf dolor sit amet"],
+    );
+    
+    // Split on "..." (in various forms)
+    const ellipsesVariants = ["...", " ... ", "\u2026", "[...]", "[\u2026]"];
+    for (const ellipses of ellipsesVariants) {
+      chai.assert.deepEqual(
+        commentToQuoteShards("<blockquote>Lorem ipsum dolor sit amet"+ellipses+"adipiscing lorem amor asdf dolor sit amet</blockquote>"),
+        ["Lorem ipsum dolor sit amet", "adipiscing lorem amor asdf dolor sit amet"],
+      );
+    }
+    
+    // Split on "[substituted word]"
+    // TODO not implemented
+    //for (const ellipses of ellipsesVariants) {
+    //  chai.assert.deepEqual(
+    //    commentToQuoteShards("<blockquote>The quick brown fox [Ahri] jumped over the lazy dog.</blockquote>", testQuoteShardSettings),
+    //    ["The quick brown fox", "jumped over the lazy dog."]
+    //  );
+    //}
+  });
+  
+  it('handles HTML inside quote shards', () => {
+    chai.assert.deepEqual(
+      commentToQuoteShards("<blockquote><p>Lorem <i>ipsum</i> dolor sit amet</p><p>adipiscing lorem amor asdf dolor sit amet</p></blockquote>"),
+      ["Lorem <i>ipsum</i> dolor sit amet", "adipiscing lorem amor asdf dolor sit amet"],
     );
   });
 });
