11253c6bfee38ee72550a0bb8eb2ac4b7dc2c059
Will Howard <w.howard256+github@gmail.com>
2022-10-11 17:00:15 +0100

    see what happens without hostname

--- packages/lesswrong/server/datadog/tracer.ts
+++ packages/lesswrong/server/datadog/tracer.ts
@@ -1,6 +1,7 @@
 import tracer from "dd-trace";
 tracer.init({
-  hostname: "172.17.0.1", logInjection: true
+  // hostname: "172.17.0.1",
+  // logInjection: true
 }); // initialized in a different file to avoid hoisting.
 tracer.use('express', {
   service: 'eaforum'
