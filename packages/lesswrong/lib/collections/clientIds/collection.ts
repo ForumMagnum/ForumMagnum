db9ed48517ff9a2fe267eefba586218a0334fc69
James Babcock <jimrandomh@gmail.com>
2022-10-12 12:28:22 -0700

    Add indexes to clientIds collection

--- packages/lesswrong/lib/collections/clientIds/collection.ts
+++ packages/lesswrong/lib/collections/clientIds/collection.ts
@@ -1,6 +1,6 @@
 import schema from './schema';
 import { createCollection } from '../../vulcan-lib';
-import { addUniversalFields, getDefaultResolvers } from '../../collectionUtils'
+import { addUniversalFields, getDefaultResolvers, ensureIndex } from '../../collectionUtils'
 
 export const ClientIds: ClientIdsCollection = createCollection({
   collectionName: "ClientIds",
@@ -13,3 +13,6 @@ addUniversalFields({
   collection: ClientIds,
   createdAtOptions: {viewableBy: ['admins']},
 });
+
+ensureIndex(ClientIds, {clientId: 1});
+ensureIndex(ClientIds, {userIds: 1});
