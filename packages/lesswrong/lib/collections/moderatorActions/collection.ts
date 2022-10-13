7dae5ae899cb24e1d541afcc57d9074a7f7eb83b
Robert Mushkatblat <bobert.mushky@gmail.com>
2022-10-13 15:34:39 -0700

    remove unused mutation options

--- packages/lesswrong/lib/collections/moderatorActions/collection.ts
+++ packages/lesswrong/lib/collections/moderatorActions/collection.ts
@@ -1,32 +1,14 @@
 import schema from './schema';
-import { userCanDo } from '../../vulcan-users/permissions';
 import { createCollection } from '../../vulcan-lib';
 import { addUniversalFields, getDefaultResolvers } from '../../collectionUtils'
-import { getDefaultMutations, MutationOptions } from '../../vulcan-core/default_mutations';
-
-const options: MutationOptions<DbModeratorAction> = {
-  newCheck: (user: DbUser|null, document: DbModeratorAction|null) => {
-    if (!user || !document) return false;
-    return userCanDo(user, 'bans.new');
-  },
-
-  editCheck: (user: DbUser|null, document: DbModeratorAction|null) => {
-    if (!user || !document) return false;
-    return userCanDo(user, `bans.edit.all`)
-  },
-
-  removeCheck: (user: DbUser|null, document: DbModeratorAction|null) => {
-    if (!user || !document) return false;
-    return userCanDo(user, `bans.remove.all`)
-  },
-}
+import { getDefaultMutations } from '../../vulcan-core/default_mutations';
 
 export const ModeratorActions: ModeratorActionsCollection = createCollection({
   collectionName: 'ModeratorActions',
   typeName: 'ModeratorAction',
   schema,
   resolvers: getDefaultResolvers('ModeratorActions'),
-  mutations: getDefaultMutations('ModeratorActions', options),
+  mutations: getDefaultMutations('ModeratorActions'),
   logChanges: true,
 });
 
