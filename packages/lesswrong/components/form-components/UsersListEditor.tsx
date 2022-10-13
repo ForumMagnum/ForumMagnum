55fd317fcb6bd2dd890ab8a242a8abe8f3cedf35
Robert Mushkatblat <bobert.mushky@gmail.com>
2022-10-13 11:04:33 -0700

    lint

--- packages/lesswrong/components/form-components/UsersListEditor.tsx
+++ packages/lesswrong/components/form-components/UsersListEditor.tsx
@@ -32,8 +32,6 @@ const UsersListEditor = ({value, path, label, classes}: {
   classes: ClassesType,
 }, context) => {
   const { updateCurrentValues } = context;
-
-  console.log({ context });
   
   const setValue = useCallback((newValue: string[]) => {
     updateCurrentValues({[path]: newValue});
