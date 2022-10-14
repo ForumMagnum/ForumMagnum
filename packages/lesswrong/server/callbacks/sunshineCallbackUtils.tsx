f2900ece4fdd548e719687dc097f90509280da80
Robert Mushkatblat <bobert.mushky@gmail.com>
2022-10-14 12:14:40 -0700

    rename param to override

--- packages/lesswrong/server/callbacks/sunshineCallbackUtils.tsx
+++ packages/lesswrong/server/callbacks/sunshineCallbackUtils.tsx
@@ -10,7 +10,7 @@ import { forumTypeSetting } from "../../lib/instanceSettings";
  * 
  * It's important this this only be be added to async callbacks on posts and comments, so that postCount and commentCount have time to update first
  */
-export async function triggerReviewIfNeeded(userId: string, newModAction?: true) {
+export async function triggerReviewIfNeeded(userId: string, override?: true) {
   const user = await Users.findOne({ _id: userId });
   if (!user)
     throw new Error("user is null");
@@ -28,7 +28,7 @@ export async function triggerReviewIfNeeded(userId: string, newModAction?: true)
   } else if (snoozed) {
     const contentCount = getCurrentContentCount(user);
     needsReview = contentCount >= user.snoozedUntilContentCount;
-  } else if (newModAction && forumTypeSetting.get() === 'LessWrong') {
+  } else if (override && forumTypeSetting.get() === 'LessWrong') {
     needsReview = true;
   }
 
