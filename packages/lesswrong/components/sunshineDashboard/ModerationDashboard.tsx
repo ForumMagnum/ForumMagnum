6f96fb65c3d28239fd3971ac6153c1860d4d18ca
Robert Mushkatblat <bobert.mushky@gmail.com>
2022-10-21 12:50:01 -0700

    remove unused import

--- packages/lesswrong/components/sunshineDashboard/ModerationDashboard.tsx
+++ packages/lesswrong/components/sunshineDashboard/ModerationDashboard.tsx
@@ -1,5 +1,5 @@
 import classNames from 'classnames';
-import React, { useMemo, useState } from 'react';
+import React, { useState } from 'react';
 import { useMulti } from '../../lib/crud/withMulti';
 
 import { Components, registerComponent } from "../../lib/vulcan-lib/components";
