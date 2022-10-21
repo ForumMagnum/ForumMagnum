be5e8eb5072b6b657b48a25e7d337300a411d1b0
Will Howard <w.howard256+github@gmail.com>
2022-10-21 09:29:07 +0100

    more cleanup, remove unused packages

--- packages/lesswrong/server/serverStartup.ts
+++ packages/lesswrong/server/serverStartup.ts
@@ -17,20 +17,6 @@ import process from 'process';
 import chokidar from 'chokidar';
 import fs from 'fs';
 
-let serverStartupComplete = false;
-export const isServerStartupComplete = () => serverStartupComplete;
-
-export const waitForServerStartup = async (timeoutMs=10000) => {
-  const startTime = new Date().getTime();
-  return new Promise(resolve => {
-    if (isServerStartupComplete()) resolve(true);
-    setInterval(() => {
-      if (isServerStartupComplete()) resolve(true);
-      if (new Date().getTime() - startTime > timeoutMs) resolve(false);
-    }, 100);
-  })
-}
-
 async function serverStartup() {
   // eslint-disable-next-line no-console
   console.log("Starting server");
@@ -59,7 +45,7 @@ async function serverStartup() {
       // See https://mongodb.github.io/node-mongodb-native/3.6/api/MongoClient.html
       // for various options that could be tuned here
       
-      // A deprecation warning says to use this option
+      // A deprecation warning says to use this option 
       useUnifiedTopology: true,
     });
     await client.connect();
@@ -116,8 +102,6 @@ async function serverStartup() {
       startWebserver();
     }
   }
-  
-  serverStartupComplete = true;
   
   /*if (process.stdout.isTTY) {
     console.log("Output is a TTY");
