d6b8bc189d40a0220f070882f13b60ef0a2691a5
Will Howard <w.howard256+github@gmail.com>
2022-10-24 13:50:58 +0100

    work out why tests are failing

--- packages/lesswrong/unitTests/schemacheck.test.ts
+++ packages/lesswrong/unitTests/schemacheck.test.ts
@@ -5,6 +5,6 @@ import { makeMigrations } from "../server/scripts/makeMigrations";
 describe('Schema check', () => {
   it('Has an accepted_schema.sql file which matches the schema defined in code', async () => {
     require('../server.ts');
-    await makeMigrations({writeSchemaChangelog: false, writeAcceptedSchema: false, generateMigrations: false, rootPath: path.join(__dirname, "../../../")});
+    await makeMigrations({writeSchemaChangelog: true, writeAcceptedSchema: true, generateMigrations: false, rootPath: path.join(__dirname, "../../../")});
   });
 })
