6edd6686dabbbeffa6aa296cd59fd888588543fe
Will Howard <w.howard256+github@gmail.com>
2022-10-24 13:55:13 +0100

    extend timeout

--- packages/lesswrong/unitTests/schemacheck.test.ts
+++ packages/lesswrong/unitTests/schemacheck.test.ts
@@ -6,5 +6,5 @@ describe('Schema check', () => {
   it('Has an accepted_schema.sql file which matches the schema defined in code', async () => {
     require('../server.ts');
     await makeMigrations({writeSchemaChangelog: true, writeAcceptedSchema: true, generateMigrations: false, rootPath: path.join(__dirname, "../../../")});
-  });
+  }, 15000);
 })
