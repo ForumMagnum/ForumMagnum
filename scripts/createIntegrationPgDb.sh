f637a6c150e68f843bbcd6f4a922c27282d85821
Ollie Etherington <ollie@etherington.xyz>
2022-10-31 12:23:19 +0000

    Add timeout to createIntegrationPgDb.sh

--- scripts/createIntegrationPgDb.sh
+++ scripts/createIntegrationPgDb.sh
@@ -3,7 +3,7 @@
 set -euxo pipefail
 
 yarn ea-start-testing-db &
-./scripts/waitForServer.sh
+./scripts/timeout.sh 60 ./scripts/waitForServer.sh
 curl -X POST http://localhost:3000/api/dropAndCreatePg \
 	-d '{"seed": false, "templateId": "jest_template"}' \
 	-H "Content-Type: application/json"
