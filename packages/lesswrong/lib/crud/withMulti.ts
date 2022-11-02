ac94725623d1fd29553dd11efe151860d44d5e6d
Will Howard <w.howard256+github@gmail.com>
2022-11-02 14:45:51 +0000

    Fix hover preview links

--- packages/lesswrong/lib/crud/withMulti.ts
+++ packages/lesswrong/lib/crud/withMulti.ts
@@ -294,7 +294,10 @@ export function useMulti<
   const { query: locationQuery, location } = useLocation();
   const { history } = useNavigation();
 
-  let defaultLimit: number = (((locationQuery && queryLimitName && parseInt(locationQuery[queryLimitName])) || terms.limit) ?? initialLimit)
+  const locationQueryLimit = locationQuery && queryLimitName && !isNaN(parseInt(locationQuery[queryLimitName])) ? parseInt(locationQuery[queryLimitName]) : undefined;
+  const termsLimit = terms?.limit;
+  const defaultLimit: number = locationQueryLimit ?? termsLimit ?? initialLimit
+
   const [ limit, setLimit ] = useState(defaultLimit);
   const [ lastTerms, setLastTerms ] = useState(_.clone(terms));
   
