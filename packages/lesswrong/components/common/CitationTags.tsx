88b0589f6a09ae1e84aa4dc390be56900c921b50
Ollie Etherington <ollie@etherington.xyz>
2022-11-09 12:10:49 +0000

    Add doc comment for citation tags

--- packages/lesswrong/components/common/CitationTags.tsx
+++ packages/lesswrong/components/common/CitationTags.tsx
@@ -2,6 +2,17 @@ import React from "react";
 import { Helmet } from "react-helmet";
 import { registerComponent } from "../../lib/vulcan-lib";
 
+/**
+ * This component adds citation meta tags for use by sites such as Google Scholar.
+ *
+ * There are several different standards in use for citation tags - for now we're
+ * just using the 'Highwire Press' standard which seems to be the most widespread,
+ * but others could be added in the future if required (other popular options are
+ * 'Eprints', 'BE Press', 'PRISM' and 'Dublin Core').
+ *
+ * The primary author is separate from the coauthors as, even though Highwire Press
+ * doesn't make a distinction, some of the other formats do.
+ */
 const CitationTags = ({title, author, coauthors, date}: {
   title?: string,
   author?: string,
