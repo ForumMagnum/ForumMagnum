75b34a5c165359a034e87c4343b7946bfc406912
JP Addison <johnpaddison@gmail.com>
2022-11-10 11:24:51 -0500

    Rewrite docstring of schema hash migration
    Co-authored-by: oetherington <ollie@etherington.xyz>

--- packages/lesswrong/server/migrations/20221021T091915.schema_hash.ts
+++ packages/lesswrong/server/migrations/20221021T091915.schema_hash.ts
@@ -1,13 +1,5 @@
 /**
- * Generated on 2022-10-21T09:19:15.910Z by `yarn makemigrations`
- * The following schema changes were detected:
- * ***Diff too large to display***
- * (run `git diff --no-index schema/accepted_schema.sql schema/schema_to_accept.sql` to see this more clearly)
- *
- * - [ ] Write a migration to represent these changes
- * - [ ] Rename this file to something more readable if you wish
- * - [ ] Uncomment `acceptsSchemaHash` below
- * - [ ] Run `yarn acceptmigrations` to update the accepted schema hash (running makemigrations again will also do this)
+ * This migration exists purely to add the first schema hash.
  */
 export const acceptsSchemaHash = "f53cd3ea6892d347ab969d1af8f4c835";
 
