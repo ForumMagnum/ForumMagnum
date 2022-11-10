5acecd110d186da77ff8f5731b369049b06c58ef
Ollie Etherington <ollie@etherington.xyz>
2022-11-10 22:55:00 +0000

    Code cleanup

--- packages/lesswrong/server/branchDb.ts
+++ packages/lesswrong/server/branchDb.ts
@@ -11,7 +11,7 @@ export const getBranchDbName = async (): Promise<string | undefined> => {
   }
 
   try {
-    const {stdout, stderr} = (await exec("git branch --show-current"));
+    const {stdout, stderr} = await exec("git branch --show-current");
     if (stderr || !stdout) {
       throw new Error(`Failed to read git branch: ${stderr}`);
     }
