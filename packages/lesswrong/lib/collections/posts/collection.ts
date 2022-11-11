092ab68d97c91ed52048d050be1eb085bc597dde
Ollie Etherington <ollie@etherington.xyz>
2022-11-11 12:20:35 +0000

    Remove debug collectionType: 'switching'

--- packages/lesswrong/lib/collections/posts/collection.ts
+++ packages/lesswrong/lib/collections/posts/collection.ts
@@ -48,7 +48,7 @@ interface ExtendedPostsCollection extends PostsCollection {
 export const Posts: ExtendedPostsCollection = createCollection({
   collectionName: 'Posts',
   typeName: 'Post',
-  collectionType: 'switching',
+  collectionType: 'pg',
   schema,
   resolvers: getDefaultResolvers('Posts'),
   mutations: getDefaultMutations('Posts', options),
