14b5eb583e696e9d4c74a03d51d80136f4587c83
Robert Mushkatblat <bobert.mushky@gmail.com>
2022-11-15 16:28:12 -0800

    remove set -u from runProduction

--- scripts/runProduction.sh
+++ scripts/runProduction.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Running Production Site"
 
-set -eux
+set -ex
 
 # lw-look here: you must define GITHUB_CREDENTIALS_REPO_USER in your AWS EBS config
 git clone https://$GITHUB_CREDENTIALS_REPO_USER:$GITHUB_CREDENTIALS_REPO_PAT@github.com/$GITHUB_CREDENTIALS_REPO_NAME.git Credentials
