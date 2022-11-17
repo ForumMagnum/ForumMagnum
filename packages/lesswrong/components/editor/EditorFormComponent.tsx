507024d87f18accc013abcda2b1abdcaa9503d26
James Babcock <jimrandomh@gmail.com>
2022-11-17 15:48:52 -0800

    Secondary post authors in collab editor can access version history
    Co-authored-by: jpaddison3 <jp@centreforeffectivealtruism.org>

--- packages/lesswrong/components/editor/EditorFormComponent.tsx
+++ packages/lesswrong/components/editor/EditorFormComponent.tsx
@@ -202,9 +202,10 @@ export const EditorFormComponent = ({form, formType, formProps, document, name, 
     />
     {!hideControls && <Components.EditorTypeSelect value={contents} setValue={wrappedSetContents} isCollaborative={isCollaborative(document, fieldName)}/>}
     {!hideControls && collectionName==="Posts" && fieldName==="contents" && !!document._id &&
-    <Components.PostVersionHistoryButton
-      postId={document._id}
-    />
+      <Components.PostVersionHistoryButton
+        post={document}
+        postId={document._id}
+      />
     }
   </div>
 }
