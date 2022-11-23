6d1d316a281b34705df579fb8066a02422f7a316
Robert Mushkatblat <bobert.mushky@gmail.com>
2022-11-23 15:17:29 -0800

    clean up views file

--- packages/lesswrong/lib/collections/commentApprovals/views.ts
+++ packages/lesswrong/lib/collections/commentApprovals/views.ts
@@ -1,30 +1,4 @@
 import { ensureIndex } from '../../collectionIndexUtils';
 import CommentApprovals from './collection';
 
-interface NoViewTerms extends ViewTermsBase {
-  view?: undefined;
-}
-
-// interface ActiveCommentModeratorActionsViewTerms extends ViewTermsBase {
-//   view: 'activeCommentModeratorActions';
-//   limit: number;
-// }
-
-// declare global {
-//   type CommentModeratorActionsViewTerms =
-//     | NoViewTerms
-//     | ActiveCommentModeratorActionsViewTerms
-// }
-
-// CommentApprovals.addView('activeCommentModeratorActions', function (terms: ActiveCommentModeratorActionsViewTerms) {
-//   return {
-//     selector: {
-//       $or: [
-//         { endedAt: { $exists: false } },
-//         { endedAt: null }
-//       ]
-//     },
-//     options: { sort: { createdAt: -1 }, limit: terms.limit }
-//   };
-// })
 ensureIndex(CommentApprovals, { commentId: 1, createdAt: -1 })
