f4dc44c5a576473f41e327ba1ad1e8fe02b4150d
James Babcock <jimrandomh@gmail.com>
2022-11-24 19:33:24 -0800

    Prevent spurious scroll when viewing a side comment that the page is permalinked to

--- packages/lesswrong/components/comments/CommentsNode.tsx
+++ packages/lesswrong/components/comments/CommentsNode.tsx
@@ -152,7 +152,7 @@ const CommentsNode = ({
 
   const {hash: commentHash} = useLocation();
   useEffect(() => {
-    if (comment && commentHash === ("#" + comment._id)) {
+    if (!noHash && comment && commentHash === ("#" + comment._id)) {
       setTimeout(() => { //setTimeout make sure we execute this after the element has properly rendered
         scrollIntoView()
       }, 0);
