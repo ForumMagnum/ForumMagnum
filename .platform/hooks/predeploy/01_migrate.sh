6335191557c9053c1249ce4365f9da6f9037aee7
Ollie Etherington <ollie@etherington.xyz>
2022-11-29 12:26:51 +0000

    Temporary debug commit

--- .platform/hooks/predeploy/01_migrate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [[ $EB_IS_COMMAND_LEADER == "true" ]]; then
-	echo "Leading instance - running migrations"
-	EB_APP_USER=$(/opt/elasticbeanstalk/bin/get-config platformconfig -k AppUser)
-	EB_APP_CURRENT_DIR=$(/opt/elasticbeanstalk/bin/get-config platformconfig -k AppDeployDir)
-	su -l ${EB_APP_USER} -c "cd $EB_APP_CURRENT_DIR; FORUM_MAGNUM_MIGRATE_CI=1 node migrate up"
-else
-	echo "Skipping migrations - not leading instance";
-fi
