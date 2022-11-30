2e488324292c642fc1a9aeae0fe229bf533a8839
Robert Mushkatblat <bobert.mushky@gmail.com>
2022-11-30 15:54:58 -0800

    remove collections import

--- packages/lesswrong/integrationTests/notificationCallbacks.tests.ts
+++ packages/lesswrong/integrationTests/notificationCallbacks.tests.ts
@@ -1,7 +1,7 @@
 import "./integrationTestSetup";
 import ReviewVotes from "../lib/collections/reviewVotes/collection";
-import { Collections } from "../lib/vulcan-lib";
-console.log({ Collections, ReviewVotes })
+// import { Collections } from "../lib/vulcan-lib";
+console.log({ ReviewVotes })
 import { createDummyPost, createDummyLocalgroup } from "./utils";
 import { postsNewNotifications } from "../server/notificationCallbacks";
 import { createNotifications } from "../server/notificationCallbacksHelpers";
