c1e26f796f915b89c6441804a5bf26a50e58149d
Ollie Etherington <ollie@etherington.xyz>
2022-12-01 09:47:46 +0000

    Update schema

--- packages/lesswrong/server/migrations/20221021T091915.schema_hash.ts
+++ packages/lesswrong/server/migrations/20221021T091915.schema_hash.ts
@@ -1,7 +1,7 @@
 /**
  * This migration exists purely to add the first schema hash.
  */
-export const acceptsSchemaHash = "c9c5881673dd7dc007450255e5519b03";
+export const acceptsSchemaHash = "2e6dd768e7ca4ef6f586c863fba6cb03";
 
 export const up = async ({db}: MigrationContext) => {
   // TODO
