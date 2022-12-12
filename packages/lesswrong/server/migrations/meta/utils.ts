a6cfa4dca769379cc45d5762400de88580d87372
Ollie Etherington <ollie@etherington.xyz>
2022-12-12 16:42:45 -0500

    Stricter type for addField helper

--- packages/lesswrong/server/migrations/meta/utils.ts
+++ packages/lesswrong/server/migrations/meta/utils.ts
@@ -4,7 +4,7 @@ import AddFieldQuery from "../../../lib/sql/AddFieldQuery";
 export const addField = async <T extends DbObject>(
   db: SqlClient,
   collection: PgCollection<T>,
-  fieldName: string,
+  fieldName: keyof T & string,
 ): Promise<void> => {
   const {sql} = new AddFieldQuery(collection.getTable(), fieldName).compile();
   await db.none(sql);
