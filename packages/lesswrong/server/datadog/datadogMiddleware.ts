0a2a3f2e93999278d0107b75d005bc9e5d339a35
Will Howard <w.howard256+github@gmail.com>
2022-12-12 16:36:34 -0500

    Add middleware file

---
+++ packages/lesswrong/server/datadog/datadogMiddleware.ts
@@ -0,0 +1,18 @@
+// import { getUserFromReq } from '../vulcan-lib';
+import tracer from './tracer'
+
+// Middleware for attaching user info to the root span of a request
+export const datadogMiddleware = (req, res, next) => {
+  const span = tracer.scope().active()
+  if (span !== null) {
+    // @ts-ignore - there is currently no public API for getting the root span, this is the accepted way (see https://github.com/DataDog/dd-trace-js/issues/725#issuecomment-805277510)
+    const rootSpan = span.context()._trace.started[0]
+    const user = req.user
+    if (user) {
+      rootSpan.setTag('usr', {_id: user._id, slug: user.slug})
+    }
+  }
+
+  res.setHeader( 'Access-Control-Allow-Headers', 'x-datadog-trace-id, x-datadog-parent-id, x-datadog-origin, x-datadog-sampling-priority' );
+  next();
+}
