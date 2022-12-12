a7da4f8b340da901058efcc07d8090274606e509
Ollie Etherington <ollie@etherington.xyz>
2022-12-12 10:12:16 -0500

    If Books is not Postgres, warn instead of error

--- packages/lesswrong/server/migrations/20221207T030042.books_toc_title.ts
+++ packages/lesswrong/server/migrations/20221207T030042.books_toc_title.ts
@@ -1,6 +1,5 @@
 import Books from "../../lib/collections/books/collection";
 import AddFieldQuery from "../../lib/sql/AddFieldQuery";
-import PgCollection from "../../lib/sql/PgCollection";
 
 /**
  * Generated on 2022-12-07T03:00:42.348Z by `yarn makemigrations`
@@ -38,14 +37,11 @@ import PgCollection from "../../lib/sql/PgCollection";
 export const acceptsSchemaHash = "dc1ea5409f03e1b22c4c5835fd70e2a3";
 
 export const up = async ({db}: MigrationContext) => {
-  if (!Books.isPostgres()) {
-    throw new Error('Can only run this migration on postgres')
+  if (Books.isPostgres()) {
+    const booksTable = Books.getTable()
+    const {sql} = new AddFieldQuery(booksTable, "tocTitle").compile();
+    await db.none(sql);
+  } else {
+    console.warn("'Books' is not a Postgres collection");
   }
-  const booksTable = Books.getTable()
-  const {sql} = new AddFieldQuery(booksTable, "tocTitle").compile();
-  await db.none(sql);
-}
-
-export const down = async ({db}: MigrationContext) => {
-  // Not implemented
 }
