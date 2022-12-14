3c90cf32ed173363e7aacc657177fd3af7748c90
Raemon <raemon777@gmail.com>
2022-12-13 17:28:47 -0800

    factor out reviewThreshold and positiveVoteThreshold

--- packages/lesswrong/lib/collections/posts/views.ts
+++ packages/lesswrong/lib/collections/posts/views.ts
@@ -10,6 +10,7 @@ import { viewFieldAllowAny, viewFieldNullOrMissing } from '../../vulcan-lib';
 import { Posts } from './collection';
 import { postStatuses, startHerePostIdSetting } from './constants';
 import uniq from 'lodash/uniq';
+import { getPositiveVoteThreshold, getReviewThreshold } from '../../reviewUtils';
 
 export const DEFAULT_LOW_KARMA_THRESHOLD = -10
 export const MAX_LOW_KARMA_THRESHOLD = -1000
@@ -59,7 +60,6 @@ declare global {
     includeDraftEvents?: boolean
     includeShared?: boolean
     distance?: number,
-    minPositiveVoteCount?: number
   }
 }
 
@@ -1359,7 +1359,8 @@ ensureIndex(Posts,
 Posts.addView("reviewVoting", (terms: PostsViewTerms) => {
   return {
     selector: {
-      positiveReviewVoteCount: { $gte: terms.minPositiveVoteCount },
+      positiveReviewVoteCount: { $gte: getPositiveVoteThreshold() },
+      reviewCount: { $gte: getReviewThreshold() }
     },
     options: {
       // This sorts the posts deterministically, which is important for the
@@ -1382,8 +1383,8 @@ ensureIndex(Posts,
 Posts.addView("reviewFinalVoting", (terms: PostsViewTerms) => {
   return {
     selector: {
-      reviewCount: { $gte: 1 },
-      positiveReviewVoteCount: { $gte: 1 }, // TODO: Ray thinks next year this should change to "has at least 4 points"
+      reviewCount: { $gte: getReviewThreshold() },
+      positiveReviewVoteCount: { $gte: getPositiveVoteThreshold() }
     },
     options: {
       // This sorts the posts deterministically, which is important for the
