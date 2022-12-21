72a16da1b0d1ea26c1a21bb7fe32b6a461efe692
Ollie Etherington <ollie@etherington.xyz>
2022-12-21 12:27:32 +0000

    Define collectionName on MongoCollection

--- packages/lesswrong/lib/sql/PgCollection.ts
+++ packages/lesswrong/lib/sql/PgCollection.ts
@@ -58,7 +58,7 @@ class PgCollection<T extends DbObject> extends MongoCollection<T> {
       result = await client.any(sql, args);
     } catch (error) {
       // If this error gets triggered, you probably generated a malformed query
-      const {collectionName} = this as unknown as CollectionBase<T>;
+      const {collectionName} = this;
       debugData = util.inspect({collectionName, ...debugData}, {depth: null});
       const {sql, args} = query.compile();
       // eslint-disable-next-line no-console
@@ -173,7 +173,7 @@ class PgCollection<T extends DbObject> extends MongoCollection<T> {
           const result = await this.executeQuery(query, {pipeline, options});
           return result as unknown as T[];
         } catch (e) {
-          const {collectionName} = this as unknown as CollectionBase<T>;
+          const {collectionName} = this;
           // If you see this, you probably built a bad aggregation pipeline, or
           // this file has a bug, or you're using an unsupported aggregation
           // pipeline operator
