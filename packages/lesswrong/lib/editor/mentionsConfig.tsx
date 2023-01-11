9303a639fe2b675714a454ca350debd102cf6dfb
James Babcock <jimrandomh@gmail.com>
2023-01-11 12:30:12 -0800

    Configure mentions-plugin for users
    Makes it so you can tag a user with @theirname. Does not yet send them
    any sort of notification.

--- packages/lesswrong/lib/editor/mentionsConfig.tsx
+++ packages/lesswrong/lib/editor/mentionsConfig.tsx
@@ -2,17 +2,28 @@ import {AlgoliaIndexCollectionName, getAlgoliaIndexName, getSearchClient} from '
 import {promisify} from '../utils/asyncUtils'
 import {getSiteUrl} from '../vulcan-lib'
 
-interface SearchHit {
+interface PostSearchHit {
   title: string,
   slug: string,
   _id: string
 }
 
-interface SearchResults {
-  hits: SearchHit[]
+interface PostSearchResults {
+  hits: PostSearchHit[]
+}
+
+interface UserSearchHit {
+  name: string,
+  slug: string,
+  _id: string
+}
+
+interface UserSearchResults {
+  hits: PostSearchHit[]
 }
 
 const postMarker = '#';
+const userMarker = '@';
 const linkPrefix = getSiteUrl()
 
 const initSearchForIndex = (indexName: AlgoliaIndexCollectionName) => {
@@ -22,29 +33,51 @@ const initSearchForIndex = (indexName: AlgoliaIndexCollectionName) => {
   return promisify(search)
 }
 
-async function fetchSuggestions(searchString: string) {
+async function fetchPostSuggestions(searchString: string) {
   const search = initSearchForIndex('Posts')
   const searchResults = await search({
     query: searchString,
     attributesToRetrieve: ['title', 'slug', '_id'],
     hitsPerPage: 20
-  }) as SearchResults
+  }) as PostSearchResults
   
   return searchResults.hits.map(hit => {
     return {
       id: postMarker + hit.title, //what gets displayed in the dropdown results, must have postMarker 
-      link: linkPrefix + 'posts/' + hit._id + '/' + hit.slug,
+      link: `${linkPrefix}posts/${hit._id}/${hit.slug}`,
       text: hit.title,
     }
   })
 }
 
+async function fetchUserSuggestions(searchString: string) {
+  const search = initSearchForIndex('Users')
+  const searchResults = await search({
+    query: searchString,
+    attributesToRetrieve: ['displayName', 'slug', '_id'],
+    hitsPerPage: 20
+  }) as UserSearchResults
+  
+  return searchResults.hits.map(hit => {
+    return {
+      id: userMarker + hit.displayName, //what gets displayed in the dropdown results, must have postMarker 
+      link: `${linkPrefix}users/${hit.slug}`,
+      text: hit.displayName,
+    }
+  })
+}
+
 export const mentionPluginConfiguration = {
-    feeds: [
-      {
-        marker: postMarker,
-        feed: fetchSuggestions,
-        minimumCharacters: 1
-      }
-    ]
-  }
+  feeds: [
+    {
+      marker: postMarker,
+      feed: fetchPostSuggestions,
+      minimumCharacters: 1
+    },
+    {
+      marker: userMarker,
+      feed: fetchUserSuggestions,
+      minimumCharacters: 1
+    }
+  ]
+}
