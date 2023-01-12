c6c9e972005149eb8c07c056a3b8994f36f6f5bf
Vladyslav Sitalo <sitalov@amazon.com>
2023-01-11 17:32:04 -0800

    User mentions: UI (#6457 part 1)

--- packages/lesswrong/lib/editor/mentionsConfig.tsx
+++ packages/lesswrong/lib/editor/mentionsConfig.tsx
@@ -1,18 +1,31 @@
 import {AlgoliaIndexCollectionName, getAlgoliaIndexName, getSearchClient} from '../algoliaUtil'
 import {promisify} from '../utils/asyncUtils'
-import {getSiteUrl} from '../vulcan-lib'
+import {getSiteUrl, Components} from '../vulcan-lib'
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-interface SearchHit {
+
+interface PostSearchHit {
   title: string,
   slug: string,
   _id: string
 }
 
-interface SearchResults {
-  hits: SearchHit[]
+interface UserSearchHit {
+  displayName: string
+  slug: string
+  _id: string
+  username: string
+  groups?: string[]
+  karma?: number
+  createdAt: string
 }
 
-const postMarker = '#';
+const markers = {
+  post: '#',
+  user: '@',
+}
+
 const linkPrefix = getSiteUrl()
 
 const initSearchForIndex = (indexName: AlgoliaIndexCollectionName) => {
@@ -22,29 +35,59 @@ const initSearchForIndex = (indexName: AlgoliaIndexCollectionName) => {
   return promisify(search)
 }
 
-async function fetchSuggestions(searchString: string) {
+async function fetchPostSuggestions(searchString: string) {
   const search = initSearchForIndex('Posts')
   const searchResults = await search({
     query: searchString,
     attributesToRetrieve: ['title', 'slug', '_id'],
-    hitsPerPage: 20
-  }) as SearchResults
-  
-  return searchResults.hits.map(hit => {
-    return {
-      id: postMarker + hit.title, //what gets displayed in the dropdown results, must have postMarker 
-      link: linkPrefix + 'posts/' + hit._id + '/' + hit.slug,
-      text: hit.title,
-    }
-  })
+    hitsPerPage: 20,
+  }) as { hits: PostSearchHit[] }
+
+  return searchResults.hits.map(hit => ({
+    id: markers.post + hit.title, //what gets displayed in the dropdown results, must have postMarker 
+    link: `${linkPrefix}posts/${hit._id}/${hit.slug}`,
+    text: hit.title,
+  }))
+}
+
+async function fetchUserSuggestions(searchString: string) {
+  const search = initSearchForIndex('Users')
+  const searchResults = await search({
+    query: searchString,
+    attributesToRetrieve: ['displayName', 'slug', '_id', 'username', 'groups', 'karma', 'createdAt'],
+    hitsPerPage: 20,
+  }) as { hits: UserSearchHit[] }
+
+  return searchResults.hits.map(hit => ({
+    id: markers.user + hit.displayName,
+    // Query string is intended for later use in detecting the ping
+    link: `${linkPrefix}users/${hit.slug}?mention=user`,
+    text: hit.displayName,
+    karma: hit.karma,
+    createdAt: new Date(hit.createdAt),
+  }))
+}
+
+const renderUserItem = (item: { text: string, karma?: number, createdAt: Date }) => {
+  const itemElement = document.createElement('button')
+
+  ReactDOM.render(<Components.UsersSearchAutocompleteHit {...item} displayName={item.text}/>, itemElement)
+
+  return itemElement
 }
 
 export const mentionPluginConfiguration = {
-    feeds: [
-      {
-        marker: postMarker,
-        feed: fetchSuggestions,
-        minimumCharacters: 1
-      }
-    ]
-  }
+  feeds: [
+    {
+      marker: markers.post,
+      feed: fetchPostSuggestions,
+      minimumCharacters: 1,
+    },
+    {
+      marker: markers.user,
+      feed: fetchUserSuggestions,
+      minimumCharacters: 1,
+      itemRenderer: renderUserItem,
+    },
+  ],
+}
