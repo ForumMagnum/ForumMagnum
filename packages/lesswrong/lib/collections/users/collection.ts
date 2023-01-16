ea9ca4a891a63c86c53b078766306a92be8b0ef1
Ollie Etherington <ollie@etherington.xyz>
2023-01-16 21:59:59 +0000

    Set Users collectionType to 'pg' for the EA Forum

--- packages/lesswrong/lib/collections/users/collection.ts
+++ packages/lesswrong/lib/collections/users/collection.ts
@@ -14,7 +14,7 @@ interface ExtendedUsersCollection extends UsersCollection {
 export const Users: ExtendedUsersCollection = createCollection({
   collectionName: 'Users',
   typeName: 'User',
-  collectionType: forumTypeSetting.get() === "EAForum" ? "switching" : "mongo",
+  collectionType: forumTypeSetting.get() === "EAForum" ? "pg" : "mongo",
   schema,
   resolvers: getDefaultResolvers('Users'),
   mutations: getDefaultMutations('Users', {
