a7ffcac9556832010610bb82e30ecb23a8ead85d
Ollie Etherington <ollie@etherington.xyz>
2023-01-17 12:30:54 +0000

    Add migration to update default values for createdAt

---
+++ packages/lesswrong/server/migrations/20230117T122732.createdAt_default_value.ts
@@ -0,0 +1,14 @@
+export const acceptsSchemaHash = "17b3602e26c300f477a4c80d4b6c6617";
+
+import { Collections } from "../vulcan-lib"
+import { updateDefaultValue } from "./meta/utils"
+
+export const up = async ({db}: MigrationContext) => {
+  for (const collection of Collections) {
+    if (collection.isPostgres()) {
+      await updateDefaultValue(db, collection, "createdAt");
+    }
+  }
+}
+
+export const down = async ({}: MigrationContext) => {}
