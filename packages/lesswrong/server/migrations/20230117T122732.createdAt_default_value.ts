b180a40bca00f1c86cbbc341a69fd3b89c982f10
Ollie Etherington <ollie@etherington.xyz>
2023-01-18 13:55:49 +0000

    Add down step to createdAt_default_value migration

--- packages/lesswrong/server/migrations/20230117T122732.createdAt_default_value.ts
+++ packages/lesswrong/server/migrations/20230117T122732.createdAt_default_value.ts
@@ -1,7 +1,7 @@
 export const acceptsSchemaHash = "17b3602e26c300f477a4c80d4b6c6617";
 
 import { Collections } from "../vulcan-lib"
-import { updateDefaultValue } from "./meta/utils"
+import { updateDefaultValue, dropDefaultValue } from "./meta/utils"
 
 export const up = async ({db}: MigrationContext) => {
   for (const collection of Collections) {
@@ -11,4 +11,10 @@ export const up = async ({db}: MigrationContext) => {
   }
 }
 
-export const down = async (_: MigrationContext) => {}
+export const down = async ({db}: MigrationContext) => {
+  for (const collection of Collections) {
+    if (collection.isPostgres()) {
+      await dropDefaultValue(db, collection, "createdAt");
+    }
+  }
+}
