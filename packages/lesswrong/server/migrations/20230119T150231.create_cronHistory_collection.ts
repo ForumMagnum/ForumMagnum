4e3409c410465c582cb2345fa646e7709b818228
Ollie Etherington <ollie@etherington.xyz>
2023-01-19 15:04:45 +0000

    Update schema for cronHistory collection

---
+++ packages/lesswrong/server/migrations/20230119T150231.create_cronHistory_collection.ts
@@ -0,0 +1,10 @@
+export const acceptsSchemaHash = "c7b9d9a9376057f062950eef371f4c5a";
+
+export const up = async (_: MigrationContext) => {
+  /*
+   * No change is needed here - we created the collection, but the table
+   * will be created by the `migrateCollections` script
+   */
+}
+
+export const down = async (_: MigrationContext) => {}
