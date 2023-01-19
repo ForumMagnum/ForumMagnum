360ce271ed1d473806baaacb8c21adcc0471525b
Vladyslav Sitalo <sitalov@amazon.com>
2023-01-19 12:34:20 -0800

    Switch to using "makemigrations" vs manual migration

---
+++ packages/lesswrong/server/migrations/20230119T202810.user_mentions_newMentionNotifictions.ts
@@ -0,0 +1,28 @@
+import {fillDefaultValues} from '../manualMigrations/migrationUtils'
+import Users from '../../lib/collections/users/collection'
+
+/**
+ * Generated on 2023-01-19T20:28:10.210Z by `yarn makemigrations`
+ * The following schema changes were detected:
+ * -------------------------------------------
+ * ***Diff too large to display***
+ * -------------------------------------------
+ * (run `git diff --no-index schema/accepted_schema.sql schema/schema_to_accept.sql` to see this more clearly)
+ *
+ * - [ ] Write a migration to represent these changes
+ * - [ ] Rename this file to something more readable if you wish
+ * - [ ] Uncomment `acceptsSchemaHash` below
+ * - [ ] Run `yarn acceptmigrations` to update the accepted schema hash (running makemigrations again will also do this)
+ */
+export const acceptsSchemaHash = "778fab1f7c2d942253123778b67715c5";
+
+export const up = async ({db}: MigrationContext) => {
+  await fillDefaultValues({
+    collection: Users,
+    fieldName: 'notificationNewMention',
+  })
+}
+
+export const down = async ({db}: MigrationContext) => {
+  // TODO, not required
+}
