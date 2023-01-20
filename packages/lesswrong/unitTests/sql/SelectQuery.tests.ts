d19a98677746d8f7bcb34bba8a1e52d7f4d5d180
Ollie Etherington <ollie@etherington.xyz>
2023-01-20 12:53:32 +0000

    Fix casting of deep JSON strings

--- packages/lesswrong/unitTests/sql/SelectQuery.tests.ts
+++ packages/lesswrong/unitTests/sql/SelectQuery.tests.ts
@@ -166,6 +166,12 @@ describe("SelectQuery", () => {
       expectedArgs: [3],
     },
     {
+      name: "can build select query with json fields with a string result",
+      getQuery: () => new SelectQuery(testTable, {"c.d.e": "test"}),
+      expectedSql: 'SELECT "TestCollection".* FROM "TestCollection" WHERE ("c"->\'d\'->>\'e\')::TEXT = $1',
+      expectedArgs: ["test"],
+    },
+    {
       name: "can build select query with array fields",
       getQuery: () => new SelectQuery(testTable, {"c.0": 3}),
       expectedSql: 'SELECT "TestCollection".* FROM "TestCollection" WHERE ("c"[0])::INTEGER = $1',
