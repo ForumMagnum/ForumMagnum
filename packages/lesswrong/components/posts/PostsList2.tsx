8e7249f159ba9061033aacf57964e5a19c7b46b3
Ollie Etherington <ollie@etherington.xyz>
2023-01-20 14:25:39 +0000

    Fix HideRepeatedPostContext removing viewed curated posts from main post feed

--- packages/lesswrong/components/posts/PostsList2.tsx
+++ packages/lesswrong/components/posts/PostsList2.tsx
@@ -192,7 +192,7 @@ const PostsList2 = ({
 
       <div className={boxShadow ? classes.posts : null}>
         {orderedResults && orderedResults.map((post, i) => {
-          if (isPostRepeated(post._id)) {
+          if (isPostRepeated(post._id) || post._id in hiddenPosts) {
             return null;
           }
           addPost(post._id);
@@ -211,9 +211,7 @@ const PostsList2 = ({
             tooltipPlacement,
           };
 
-          if (!(post._id in hiddenPosts)) {
-            return <PostsItem2 key={post._id} {...props} />
-          }
+          return <PostsItem2 key={post._id} {...props} />
         })}
       </div>
       {showLoadMore && <SectionFooter>
