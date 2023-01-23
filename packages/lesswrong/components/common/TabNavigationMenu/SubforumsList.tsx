c600c9933a58e72fedde00083269bc0379e1ca43
Will Howard <w.howard256+github@gmail.com>
2023-01-23 14:16:36 +0000

    Add show more to subforum list in sidebar

--- packages/lesswrong/components/common/TabNavigationMenu/SubforumsList.tsx
+++ packages/lesswrong/components/common/TabNavigationMenu/SubforumsList.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import { registerComponent, Components } from "../../../lib/vulcan-lib/components";
 import { AnalyticsContext } from "../../../lib/analyticsEvents";
 import { useMulti } from "../../../lib/crud/withMulti";
@@ -19,6 +19,16 @@ const styles = ((theme: ThemeType): JssStyles => ({
     height: 'unset',
     minHeight: 24,
   },
+  maybeHiddenResultsHidden: {
+    overflow: 'hidden',
+    maxHeight: 0,
+    transition: 'max-height 0.3s ease-in-out',
+  },
+  maybeHiddenResultsVisible: {
+    overflow: 'hidden',
+    maxHeight: 1000,
+    transition: 'max-height 0.3s ease-in-out',
+  },
   title: {
     paddingLeft: 62,
     paddingBottom: 5,
@@ -34,6 +44,8 @@ const styles = ((theme: ThemeType): JssStyles => ({
   },
 }))
 
+const INITIAL_LIMIT = 3
+
 const SubforumsList = ({ onClick, classes }) => {
   const { results } = useMulti({
     terms: {view: 'currentUserSubforums'},
@@ -42,35 +54,52 @@ const SubforumsList = ({ onClick, classes }) => {
     enableTotal: false,
     fetchPolicy: 'cache-and-network',
   })
+  const [showAll, setShowAll] = useState(false)
+
+  const onClickShowMoreOrLess = useCallback((e) => {
+    e.preventDefault() // Prevent ripple
+    setShowAll(!showAll)
+  }, [showAll])
   
   if (!results || !results.length) return <></>
   
+  const initialResults = results.slice(0, INITIAL_LIMIT)
+  const maybeHiddenResults = results.slice(INITIAL_LIMIT)
+  const displayShowMoreOrLess = results.length > INITIAL_LIMIT
+
   // MenuItem takes a component and passes unrecognized props to that component,
   // but its material-ui-provided type signature does not include this feature.
   // Cast to any to work around it, to be able to pass a "to" parameter.
   const MenuItemUntyped = MenuItem as any
   
   const { TabNavigationSubItem } = Components
-
   
+  const getListItem = (subforum) => (
+    <MenuItemUntyped
+      key={subforum._id}
+      onClick={onClick}
+      component={Link}
+      to={tagGetSubforumUrl(subforum)}
+      classes={{ root: classes.menuItem }}
+    >
+      <TabNavigationSubItem className={classes.subItem}>{subforum.name}</TabNavigationSubItem>
+    </MenuItemUntyped>
+  );
+
   return (
     <span>
       <AnalyticsContext pageSubSectionContext="menuSubforumsList">
         <div>
           <div className={classes.title}>Subforums</div>
-          {results.map((subforum) => (
-            <MenuItemUntyped
-              key={subforum._id}
-              onClick={onClick}
-              component={Link}
-              to={tagGetSubforumUrl(subforum)}
-              classes={{ root: classes.menuItem }}
-            >
-              <TabNavigationSubItem className={classes.subItem}>
-                {subforum.name}
-              </TabNavigationSubItem>
+          <div>{initialResults.map((subforum) => getListItem(subforum))}</div>
+          <div className={showAll ? classes.maybeHiddenResultsVisible : classes.maybeHiddenResultsHidden}>
+            {showAll && maybeHiddenResults.map((subforum) => getListItem(subforum))}
+          </div>
+          {displayShowMoreOrLess && (
+            <MenuItemUntyped onClick={onClickShowMoreOrLess} className={classes.menuItem} disableRipple>
+              <TabNavigationSubItem>(show {showAll ? "less" : "more"})</TabNavigationSubItem>
             </MenuItemUntyped>
-          ))}
+          )}
         </div>
       </AnalyticsContext>
     </span>
