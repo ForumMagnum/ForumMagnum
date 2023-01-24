12db254d339b24d39f7c7ad617979bf6baf268c9
Will Howard <w.howard256+github@gmail.com>
2023-01-24 16:58:49 +0000

    remove log

--- packages/lesswrong/components/form-components/FormComponentPostEditorTagging.tsx
+++ packages/lesswrong/components/form-components/FormComponentPostEditorTagging.tsx
@@ -43,7 +43,6 @@ const FormComponentPostEditorTagging = ({value, path, document, formType, update
 }) => {
   const { TagsChecklist, TagMultiselect, FooterTagList, Loading } = Components
   const showSubforumSection = forumTypeSetting.get() === "EAForum";
-  console.log("FormComponentPostEditorTagging: ", showSubforumSection)
   
   const { results, loading } = useMulti({
     terms: {
