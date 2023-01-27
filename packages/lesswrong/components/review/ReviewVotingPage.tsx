04e6ae78f7e90ed593aa5d2425cd5aea8e568535
Raemon <raemon777@gmail.com>
2023-01-26 23:53:53 -0800

    fix type

--- packages/lesswrong/components/review/ReviewVotingPage.tsx
+++ packages/lesswrong/components/review/ReviewVotingPage.tsx
@@ -366,7 +366,7 @@ const ReviewVotingPage = ({classes}: {
 
   const canInitialResort = !!postsResults
 
-  const reSortPosts = useCallback((sortPosts: string, sortReversed: string, tagFilter: string) => {
+  const reSortPosts = useCallback((sortPosts: string, sortReversed: boolean, tagFilter: string) => {
     if (!postsResults) return
 
     const randomPermutation = generatePermutation(postsResults.length, currentUser)
