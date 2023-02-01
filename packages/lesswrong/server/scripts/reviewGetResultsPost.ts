df108a407b65848ca9a50920fe8bd8d2c759462c
Raemon <raemon777@gmail.com>
2023-01-31 23:09:46 -0800

    fix imports

--- packages/lesswrong/server/scripts/reviewGetResultsPost.ts
+++ packages/lesswrong/server/scripts/reviewGetResultsPost.ts
@@ -1,5 +1,6 @@
 import { Globals } from "../vulcan-lib";
 import fs from 'fs'
+import { createVotingPostHtml } from "../../lib/reviewVoteUpdate";
 
 Globals.getReviewPrizesPost = async () => {
   const result = await createVotingPostHtml()
