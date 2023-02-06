fa7bc7543093433753619d13774c78b69a554c43
jcmustin <jonathan.mustin@gmail.com>
2023-02-06 10:01:42 -0800

    Remove typo

--- packages/lesswrong/server/migrations/20230202T222203.add_backfilled.ts
+++ packages/lesswrong/server/migrations/20230202T222203.add_backfilled.ts
@@ -1,4 +1,4 @@
-t export const acceptsSchemaHash = "f3bb9b35520db60be5d6640c1be3377b";
+export const acceptsSchemaHash = "f3bb9b35520db60be5d6640c1be3377b";
 
 import TagRels from "../../lib/collections/tagRels/collection";
 import { addField, dropField } from "./meta/utils"
