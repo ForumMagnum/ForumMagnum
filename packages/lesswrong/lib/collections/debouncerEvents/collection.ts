bae6093cc8e95934815385c7f279341c11fd789b
Ollie Etherington <ollie@etherington.xyz>
2023-02-07 14:39:15 +0000

    Fix invalid DebouncerEvents index

--- packages/lesswrong/lib/collections/debouncerEvents/collection.ts
+++ packages/lesswrong/lib/collections/debouncerEvents/collection.ts
@@ -15,6 +15,6 @@ addUniversalFields({collection: DebouncerEvents})
 
 ensureIndex(DebouncerEvents, { dispatched:1, af:1, delayTime:1 });
 ensureIndex(DebouncerEvents, { dispatched:1, af:1, upperBoundTime:1 });
-ensureIndex(DebouncerEvents, { dispatched:1, eventName:1, af:1, key:1 });
+ensureIndex(DebouncerEvents, { dispatched:1, af:1, key:1 });
 
 export default DebouncerEvents;
