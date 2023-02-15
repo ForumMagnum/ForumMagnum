60fdffb0f02c51c14d4a29c951e80669f62208f5
Sarah Cheng <sarah.cheng@centreforeffectivealtruism.org>
2023-02-15 15:22:02 -0500

    fix a bug that can prevent publishing an event

--- packages/lesswrong/server/repos/UsersRepo.ts
+++ packages/lesswrong/server/repos/UsersRepo.ts
@@ -1,6 +1,7 @@
 import AbstractRepo from "./AbstractRepo";
 import Users from "../../lib/collections/users/collection";
 
+export type MongoNearLocation = { type: "Point", coordinates: number[] }
 export default class UsersRepo extends AbstractRepo<DbUser> {
   constructor() {
     super(Users);
@@ -12,5 +13,21 @@ export default class UsersRepo extends AbstractRepo<DbUser> {
       FROM "Users"
       WHERE "services"->'resume'->'loginTokens' @> ('[{"hashedToken": "' || $1 || '"}]')::JSONB
     `, [hashedToken]);
+  }
+  
+  getUsersWhereLocationIsInNotificationRadius(location: MongoNearLocation): Promise<Array<DbUser>> {
+    return this.any(`
+      SELECT *
+      FROM "Users"
+      WHERE (
+        EARTH_DISTANCE(
+          LL_TO_EARTH(
+            ("nearbyEventsNotificationsMongoLocation"->'coordinates'->0)::FLOAT8,
+            ("nearbyEventsNotificationsMongoLocation"->'coordinates'->1)::FLOAT8
+          ),
+          LL_TO_EARTH($1, $2)
+        ) * 0.000621371
+      ) < "nearbyEventsNotificationsRadius"
+    `, [location.coordinates[0], location.coordinates[1]])
   }
 }
