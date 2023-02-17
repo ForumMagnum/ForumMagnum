14efd229e5d2b07d104fe1944add793594d69fff
Will Howard <w.howard256+github@gmail.com>
2023-02-17 12:41:01 -0500

    revert noop

--- packages/lesswrong/lib/collections/comments/views.ts
+++ packages/lesswrong/lib/collections/comments/views.ts
@@ -227,9 +227,7 @@ Comments.addView("postLWComments", (terms: CommentsViewTerms) => {
 
 Comments.addView("profileRecentComments", (terms: CommentsViewTerms) => {
   return {
-    selector: {
-      deletedPublic: false,
-    },
+    selector: {deletedPublic: false},
     options: {sort: {isPinnedOnProfile: -1, postedAt: -1}, limit: terms.limit || 5},
   };
 })
