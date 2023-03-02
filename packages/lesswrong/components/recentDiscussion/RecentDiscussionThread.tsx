2a9ff7d1a38852adb5d214b22b7ba063f1925a70
Ollie Etherington <ollie@etherington.xyz>
2023-03-02 14:03:36 +0000

    Add border radius to RecentDiscussionThread

--- packages/lesswrong/components/recentDiscussion/RecentDiscussionThread.tsx
+++ packages/lesswrong/components/recentDiscussion/RecentDiscussionThread.tsx
@@ -23,7 +23,7 @@ const styles = (theme: ThemeType): JssStyles => ({
     position: "relative",
     minHeight: 58,
     boxShadow: theme.palette.boxShadow.default,
-    borderRadius: 3,
+    borderRadius: theme.uiStyle === "book" ? 3 : theme.borderRadius.default,
   },
   plainBackground: {
     backgroundColor: theme.palette.panelBackground.recentDiscussionThread,
