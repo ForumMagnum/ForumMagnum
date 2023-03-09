78da7fc4c05578fcbb9896f3e7df96caca162c08
James Babcock <jimrandomh@gmail.com>
2023-03-08 16:12:30 -0800

    Fix spurious "Async before callbacks should not return undefined" console errors on client-navigate4
    The handling of client-side page navigation event hooks (useOnNavigate)
    was using Vulcan's callback mechanism, in a way that was intended for
    callback-chains returning a series of modified versions of a document
    (but there was nothing like that to modify). Some recently-added
    defensive programming added a console error for `undefined` showing up
    in those sorts of callback chains, which produced spurious console
    errors on page-navigation.
    
    Refactor to use a boring array for managing callbacks, fixing the
    spurious console errors.

--- packages/lesswrong/components/hooks/useOnNavigate.tsx
+++ packages/lesswrong/components/hooks/useOnNavigate.tsx
@@ -1,6 +1,5 @@
 import { useEffect } from 'react';
 import { registerComponent } from '../../lib/vulcan-lib/components';
-import { CallbackChainHook  } from '../../lib/vulcan-lib/callbacks';
 import type { RouterLocation } from '../../lib/vulcan-lib/routes';
 import { useSubscribedLocation } from '../../lib/routeUtil';
 import { isClient } from '../../lib/executionEnvironment';
@@ -8,8 +7,8 @@ import { captureEvent } from '../../lib/analyticsEvents';
 import * as _ from 'underscore';
 
 let lastLocation: RouterLocation|null = null;
-
-const routerOnUpdate = new CallbackChainHook<{oldLocation:RouterLocation|null,newLocation:RouterLocation},[]>("router.onUpdate");
+type LocationChange = {oldLocation: RouterLocation|null, newLocation: RouterLocation};
+let onNavigateFunctions: Array<(locationChange: LocationChange)=>void> = [];
 
 const NavigationEventSender = () => {
   const location = useSubscribedLocation();
@@ -25,10 +24,10 @@ const NavigationEventSender = () => {
             from: lastLocation.pathname,
             to: location.pathname,
           });
-          void routerOnUpdate.runCallbacks({
-            iterator: {oldLocation: lastLocation, newLocation: location},
-            properties: [],
-          });
+          let change: LocationChange = {oldLocation: lastLocation, newLocation: location};
+          for(let cb of [...onNavigateFunctions]) {
+            cb(change);
+          }
         }
         lastLocation = _.clone(location);
       }
@@ -43,11 +42,11 @@ const NavigationEventSender = () => {
  * at the *start* of the navigation, ie, when the link is first clicked (but
  * before most of the stuff at the destination has loaded).
  */
-export function useOnNavigate(fn: ({oldLocation,newLocation}: {oldLocation: RouterLocation|null, newLocation: RouterLocation})=>void) {
+export function useOnNavigate(fn: (change: LocationChange)=>void) {
   useEffect(() => {
-    routerOnUpdate.add(fn);
+    onNavigateFunctions.push(fn);
     return () => {
-      routerOnUpdate.remove(fn);
+      onNavigateFunctions = onNavigateFunctions.filter(f=>f!==fn);
     };
   }, [fn]);
 }
