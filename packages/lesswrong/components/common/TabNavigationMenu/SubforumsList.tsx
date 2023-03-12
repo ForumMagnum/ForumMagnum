28792a43c079f8d5c7576412134787cc83e45cd7
Sharang Phadke <sharang.phadke@gmail.com>
2023-03-12 13:32:44 -0400

    actually remove the thing

--- packages/lesswrong/components/common/TabNavigationMenu/SubforumsList.tsx
+++ packages/lesswrong/components/common/TabNavigationMenu/SubforumsList.tsx
@@ -74,7 +74,6 @@ const SubforumsList = ({ onClick, classes }) => {
     <span>
       <AnalyticsContext pageSubSectionContext="menuSubforumsList">
         <div>
-          <div className={classes.title}>Core topics</div>
           {initialResults.map((subforum) => getListItem(subforum))}
           {showAll && maybeHiddenResults.map((subforum) => getListItem(subforum))}
           {displayShowMoreOrLess && (
