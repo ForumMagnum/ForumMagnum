f1293f0d5705e0880ac4f2f57e8728d578d95678
Ollie Etherington <ollie@etherington.xyz>
2023-04-04 14:02:05 +0100

    Generate image fallback ourselves to use correct font and save on network

--- packages/lesswrong/components/users/UsersProfileImage.tsx
+++ packages/lesswrong/components/users/UsersProfileImage.tsx
@@ -5,32 +5,74 @@ import rng from "../../lib/seedrandom";
 
 export type ProfileImageFallback = "initials";
 
-const styles = (_: ThemeType): JssStyles => ({
+const styles = (theme: ThemeType): JssStyles => ({
   root: {
     borderRadius: "50%",
   },
+  initalText: {
+    color: "#222", // For both light and dark mode
+    fill: "#222",
+    lineHeight: 1,
+    fontFamily: theme.palette.fonts.sansSerifStack,
+  },
 });
 
-const colorCutoff = 80;
+const COLOR_CUTOFF = 80;
+
 const colorComponent = (rand: ReturnType<typeof rng>): number =>
-  (Math.abs(rand.int32()) % (255 - colorCutoff)) + colorCutoff;
+  (Math.abs(rand.int32()) % (255 - COLOR_CUTOFF)) + COLOR_CUTOFF;
 
 const userBackground = (displayName: string): string => {
   const rand = rng(displayName);
   const r = colorComponent(rand);
   const g = colorComponent(rand);
   const b = colorComponent(rand);
-  return ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+  return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
 }
 
-const nameTransform = (s: string) => encodeURIComponent(s[0]);
-
-const buildInitialFallbackSrc = (user: UsersMinimumInfo, size: number): string => {
-  const api = "https://ui-avatars.com/api/";
-  const name = user.displayName.split(/[\s-_.]/).map(nameTransform).join("+");
-  const actualSize = size * 2; // Allow for high-DPI screens
-  const background = userBackground(user.displayName);
-  return `${api}?name=${name}&size=${actualSize}&background=${background}&bold=true`;
+const InitialFallback = ({displayName, size, className, classes}: {
+  displayName: string,
+  size: number,
+  className?: string,
+  classes: ClassesType,
+}) => {
+  const initials = displayName.split(/[\s-_.]/).map(
+    (s) => encodeURIComponent(s[0]).toUpperCase(),
+  );
+  const text = initials.join("").slice(0, 3);
+  const background = userBackground(displayName);
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      version="1.1"
+      width={`${size}px`}
+      height={`${size}px`}
+      viewBox={`0 0 ${size} ${size}`}
+      className={classNames(classes.root, className)}
+    >
+      <rect
+        fill={background}
+        width={size}
+        height={size}
+        cx={size / 2}
+        cy={size / 2}
+        r={size / 2}
+      />
+      <text
+        className={classes.initalText}
+        x="50%"
+        y="50%"
+        alignmentBaseline="middle"
+        textAnchor="middle"
+        fontSize={size * 0.45}
+        fontWeight="600"
+        dy=".1em"
+        dominantBaseline="middle"
+      >
+        {text}
+      </text>
+    </svg>
+  );
 }
 
 const UsersProfileImage = ({user, size, fallback, className, classes}: {
@@ -55,11 +97,11 @@ const UsersProfileImage = ({user, size, fallback, className, classes}: {
   if (fallback === "initials") {
     return (
       <picture>
-        <img
-          src={buildInitialFallbackSrc(user, size)}
-          width={`${size}px`}
-          height={`${size}px`}
-          className={classNames(classes.root, className)}
+        <InitialFallback
+          displayName={user.displayName}
+          size={size}
+          className={className}
+          classes={classes}
         />
       </picture>
     );
