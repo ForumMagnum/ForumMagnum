e6dad0bd6597320f470d6bafd32c0124124e3628
Ollie Etherington <ollie@etherington.xyz>
2023-04-07 21:43:12 +0100

    Fix lint error

--- packages/lesswrong/components/users/UsersProfileImage.tsx
+++ packages/lesswrong/components/users/UsersProfileImage.tsx
@@ -58,7 +58,7 @@ const InitialFallback: FC<{
   className?: string,
   classes: ClassesType,
 }> = memo(({displayName, size, className, classes}) => {
-  const initials = displayName.split(/[\s-_.\(\)]/).map((s) => s?.[0]?.toUpperCase());
+  const initials = displayName.split(/[\s-_.()]/).map((s) => s?.[0]?.toUpperCase());
   const text = initials.filter((s) => s?.length).join("").slice(0, 3);
   const background = userBackground(displayName);
   return (
