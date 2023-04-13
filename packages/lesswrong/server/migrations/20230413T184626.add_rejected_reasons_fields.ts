e1fa0ee36b813cc6621c2270f8ae0e22e8cc2b59
Robert Mushkatblat <bobert.mushky@gmail.com>
2023-04-13 11:47:33 -0700

    redo migration

--- packages/lesswrong/server/migrations/20230413T184626.add_rejected_reasons_fields.ts
+++ packages/lesswrong/server/migrations/20230413T184626.add_rejected_reasons_fields.ts
@@ -1,37 +1,33 @@
-import Posts from "../../lib/collections/posts/collection";
-import { addField, dropField } from "./meta/utils";
-import Comments from "../../lib/collections/comments/collection";
-
 /**
- * Generated on 2023-04-12T00:50:55.949Z by `yarn makemigrations`
+ * Generated on 2023-04-13T18:46:26.414Z by `yarn makemigrations`
  * The following schema changes were detected:
  * -------------------------------------------
- * diff --git a/Users/rbloom/git/lesswrongSuite/LessWrong2/schema/accepted_schema.sql b/Users/rbloom/git/lesswrongSuite/LessWrong2/schema/schema_to_accept.sql
- * index 3b73e0d65e..de09d261d5 100644
- * --- a/Users/rbloom/git/lesswrongSuite/LessWrong2/schema/accepted_schema.sql
- * +++ b/Users/rbloom/git/lesswrongSuite/LessWrong2/schema/schema_to_accept.sql
+ * diff --git a/Users/robert/Documents/repos/ForumMagnum/schema/accepted_schema.sql b/Users/robert/Documents/repos/ForumMagnum/schema/schema_to_accept.sql
+ * index 366d0fa827..38f9b34e68 100644
+ * --- a/Users/robert/Documents/repos/ForumMagnum/schema/accepted_schema.sql
+ * +++ b/Users/robert/Documents/repos/ForumMagnum/schema/schema_to_accept.sql
  * @@ -4,5 +4,3 @@
  *  --
- * --- Overall schema hash: 1728cb3d532414ce56d22566ab53c3be
+ * --- Overall schema hash: 64c57945e3105d8daf5be8d51a1ee559
  * -
- * --- Accepted on 2023-04-11T17:26:12.000Z by 20230411T172612.add_hideCommunitySection_to_users.ts
- * +-- Overall schema hash: 88c571d9dc61235c09b0e6e335a1c232
+ * --- Accepted on 2023-04-12T18:58:46.000Z by 20230412T185846.add_modGPTAnalysis.ts
+ * +-- Overall schema hash: 1058503cdf3522b15f94b3499566433a
  *  
  * @@ -108,3 +106,3 @@ CREATE TABLE "CommentModeratorActions" (
  *  
- * --- Schema for "Comments", hash: 1c77433ee8754e841a073c882ee2f7ef
- * +-- Schema for "Comments", hash: 47f6b6206c616886b968bafeb94bc1c4
+ * --- Schema for "Comments", hash: aff61766f86b6129215dc4cd710aa12a
+ * +-- Schema for "Comments", hash: 1a34920ecad5fd219e1d13d6025f4839
  *  CREATE TABLE "Comments" (
- * @@ -159,2 +157,3 @@ CREATE TABLE "Comments" (
- *      "rejected" bool DEFAULT false,
+ * @@ -161,2 +159,3 @@ CREATE TABLE "Comments" (
+ *      "modGPTRecommendation" text,
  * +    "rejectedReason" text,
  *      "rejectedByUserId" varchar(27),
- * @@ -461,3 +460,3 @@ CREATE TABLE "PostRelations" (
+ * @@ -463,3 +462,3 @@ CREATE TABLE "PostRelations" (
  *  
  * --- Schema for "Posts", hash: 3af0a7c4d804abbea3f2a8396a737aeb
  * +-- Schema for "Posts", hash: f4ca610b780ee8fc503c04e05fd7a646
  *  CREATE TABLE "Posts" (
- * @@ -593,2 +592,3 @@ CREATE TABLE "Posts" (
+ * @@ -595,2 +594,3 @@ CREATE TABLE "Posts" (
  *      "rejected" bool DEFAULT false,
  * +    "rejectedReason" text,
  *      "rejectedByUserId" varchar(27),
@@ -44,7 +40,11 @@ import Comments from "../../lib/collections/comments/collection";
  * - [ ] Uncomment `acceptsSchemaHash` below
  * - [ ] Run `yarn acceptmigrations` to update the accepted schema hash (running makemigrations again will also do this)
  */
-export const acceptsSchemaHash = "88c571d9dc61235c09b0e6e335a1c232";
+export const acceptsSchemaHash = "1058503cdf3522b15f94b3499566433a";
+
+import Comments from "../../lib/collections/comments/collection";
+import Posts from "../../lib/collections/posts/collection";
+import { addField, dropField } from "./meta/utils";
 
 export const up = async ({db}: MigrationContext) => {
   if (Posts.isPostgres()) {
@@ -65,4 +65,3 @@ export const down = async ({db}: MigrationContext) => {
     await dropField(db, Comments, 'rejectedReason');
   }
 }
-
