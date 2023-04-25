180a7fad132593dd88072f46a2a30dc7202b2ed1
Ollie Etherington <ollie@etherington.xyz>
2023-04-25 20:46:19 +0100

    Use sans serif font for 'no more recommendations' message

--- packages/lesswrong/components/recommendations/RecommendationsList.tsx
+++ packages/lesswrong/components/recommendations/RecommendationsList.tsx
@@ -2,20 +2,29 @@ import React, { ComponentType } from 'react';
 import { Components, registerComponent } from '../../lib/vulcan-lib';
 import { useRecommendations } from './withRecommendations';
 import type { RecommendationsAlgorithm } from '../../lib/collections/users/recommendationSettings';
+import { isEAForum } from '../../lib/instanceSettings';
 
 export type RecommendationsListItem = ComponentType<{
   post: PostsListWithVotes|PostsListWithVotesAndSequence,
   translucentBackground?: boolean,
 }>;
 
+const styles = (theme: ThemeType) => ({
+  noMoreMessage: {
+    fontFamily: isEAForum ? theme.palette.fonts.sansSerifStack : undefined,
+  },
+});
+
 const RecommendationsList = ({
   algorithm,
   translucentBackground,
   ListItem = Components.PostsItem,
+  classes,
 }: {
   algorithm: RecommendationsAlgorithm,
   translucentBackground?: boolean,
   ListItem?: RecommendationsListItem,
+  classes: ClassesType,
 }) => {
   const {PostsLoading, Typography} = Components;
   const {recommendationsLoading, recommendations} = useRecommendations(algorithm);
@@ -31,12 +40,19 @@ const RecommendationsList = ({
         translucentBackground={translucentBackground}
       />
     )}
-    {recommendations.length===0 &&
-      <Typography variant="body1"><small>There are no more recommendations left.</small></Typography>}
+    {recommendations.length === 0 &&
+      <Typography variant="body1" className={classes.noMoreMessage}>
+        <small>There are no more recommendations left.</small>
+      </Typography>
+    }
   </div>
 }
 
-const RecommendationsListComponent = registerComponent('RecommendationsList', RecommendationsList);
+const RecommendationsListComponent = registerComponent(
+  'RecommendationsList',
+  RecommendationsList,
+  {styles},
+);
 
 declare global {
   interface ComponentTypes {
