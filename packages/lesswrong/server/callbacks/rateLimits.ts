34aab13c288735f59318f2c6dd85364c338ba942
Raemon <raemon777@gmail.com>
2023-04-25 13:36:32 -0700

    wip coauthor statuses

--- packages/lesswrong/server/callbacks/rateLimits.ts
+++ packages/lesswrong/server/callbacks/rateLimits.ts
@@ -119,7 +119,7 @@ const userNumberOfCommentsOnOthersPostsInPastTimeframe = async (user: DbUser, ho
     },
   }).fetch();
   const postIds = comments.map(comment => comment.postId)
-  const postsNotAuthoredByCommenter = await Posts.find({_id: {$in: postIds}, userId: {$ne:user._id}}).fetch()
+  const postsNotAuthoredByCommenter = await Posts.find({_id: {$in: postIds}, $or: [{userId: {$ne:user._id}}]}, {projection: {_id:1}}).fetch()
   const postsNotAuthoredByCommenterIds = postsNotAuthoredByCommenter.map(post => post._id)
   const commentsOnNonauthorPosts = comments.filter(comment => postsNotAuthoredByCommenterIds.includes(comment.postId))
   return commentsOnNonauthorPosts.length
