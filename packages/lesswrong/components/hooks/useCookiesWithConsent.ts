318ceb4308851a630832800f9dff8fc724d3086c
Will Howard <w.howard256+github@gmail.com>
2023-05-05 10:20:35 +0100

    revert infinite loop fix

--- packages/lesswrong/components/hooks/useCookiesWithConsent.ts
+++ packages/lesswrong/components/hooks/useCookiesWithConsent.ts
@@ -1,14 +1,11 @@
 import { useCookies } from "react-cookie";
 import { CookieSetOptions } from "universal-cookie/cjs/types";
 import { ALL_COOKIES, COOKIE_CONSENT_TIMESTAMP_COOKIE, COOKIE_PREFERENCES_COOKIE, CookieType, ONLY_NECESSARY_COOKIES, isCookieAllowed, isValidCookieTypeArray } from "../../lib/cookies/utils";
-import { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import { cookiePreferencesChangedCallbacks } from "../../lib/cookies/callbacks";
 import { getExplicitConsentRequiredAsync, getExplicitConsentRequiredSync } from "../common/CookieBanner/geolocation";
 import { useTracking } from "../../lib/analyticsEvents";
 import moment from "moment";
-import { DatabasePublicSetting } from "../../lib/publicSettings";
-
-export const debugCookieBannerSetting = new DatabasePublicSetting<boolean>('debugCookieBanner', true);
 
 /**
  * Fetches the current cookie preferences and allows the user to update them.
@@ -46,31 +43,16 @@ export function useCookiePreferences(): {
 
   // If the user had not given explicit consent, but the value of COOKIE_PREFERENCES_COOKIE is different to what we are
   // using in the code (fallbackPreferences), update the cookie. This is so that Google Tag Manager handles it correctly.
-  const autoUpdateCount = useRef<number>(0);
-  useEffect(() => {
-    if (explicitConsentRequired === "unknown" || explicitConsentGiven) return;
+  // TODO this is causing an infinite loop somehow, reenable once we figure out why.
+  // useEffect(() => {
+  //   if (explicitConsentRequired === "unknown" || explicitConsentGiven) return;
 
-    if (isValidCookieTypeArray(preferencesCookieValue) && JSON.stringify(cookiePreferences) !== JSON.stringify(preferencesCookieValue)) {
-      autoUpdateCount.current++;
-      // Apoologies for this bit of debugging in prod, but this previously caused an infinie loop
-      // sometimes and I haven't been able to reproduce this locally.
-      if (debugCookieBannerSetting.get() && autoUpdateCount.current > 2) {
-        if (autoUpdateCount.current > 10) return; // Stop logging these if it's really stuck in an infinite loop
-
-        captureEvent("cookieBannerDebug", {
-          subtype: "autoUpdate",
-          autoUpdateCount: autoUpdateCount.current,
-          cookiePreferences,
-          preferencesCookieValue,
-        })
-        return;
-      }
-
-      setCookie(COOKIE_PREFERENCES_COOKIE, cookiePreferences, { path: "/", expires: moment().add(2, 'years').toDate() });
-      void cookiePreferencesChangedCallbacks.runCallbacks({iterator: cookiePreferences, properties: []});
-    }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [explicitConsentRequired, JSON.stringify(cookiePreferences), JSON.stringify(preferencesCookieValue), setCookie]);
+  //   if (JSON.stringify(cookiePreferences) !== JSON.stringify(preferencesCookieValue)) {
+  //     setCookie(COOKIE_PREFERENCES_COOKIE, cookiePreferences, { path: "/", expires: moment().add(2, 'years').toDate()  });
+  //     void cookiePreferencesChangedCallbacks.runCallbacks({iterator: cookiePreferences, properties: []});
+  //   }
+  // // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, [explicitConsentRequired, JSON.stringify(cookiePreferences), JSON.stringify(preferencesCookieValue), setCookie]);
   
   const updateCookiePreferences = useCallback(
     (newPreferences: CookieType[]) => {
