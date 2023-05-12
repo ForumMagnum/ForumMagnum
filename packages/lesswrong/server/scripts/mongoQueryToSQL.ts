49b4938b80f98c09077afe547e51db291263c557
Robert Mushkatblat <bobert.mushky@gmail.com>
2023-05-12 14:22:35 -0700

    add comment

--- packages/lesswrong/server/scripts/mongoQueryToSQL.ts
+++ packages/lesswrong/server/scripts/mongoQueryToSQL.ts
@@ -2,6 +2,9 @@ import SelectQuery from "../../lib/sql/SelectQuery";
 import Table from "../../lib/sql/Table";
 import { getCollectionByTableName, Globals } from "../vulcan-lib";
 
+/**
+ * Translates a mongo find query to SQL for debugging purposes.  Requires a server running because the query builder uses collections, etc.
+ */
 Globals.findToSQL = ({ tableName, selector, options }: { tableName: CollectionNameString, selector: AnyBecauseTodo, options?: MongoFindOptions<DbObject> }) => {
   const table = Table.fromCollection(getCollectionByTableName(tableName));
   const select = new SelectQuery<DbObject>(table, selector, options);
