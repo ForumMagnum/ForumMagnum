de967147ec4d3cb78d6d95e407a15a59ece933d1
Will Howard <w.howard256+github@gmail.com>
2023-05-15 12:21:47 +0100

    Make redirect work

--- packages/lesswrong/server/botRedirect.ts
+++ packages/lesswrong/server/botRedirect.ts
@@ -1,19 +1,61 @@
-import type { Request, Response } from 'express';
+import type { Request, Response, NextFunction } from 'express';
 import { DatabasePublicSetting } from '../lib/publicSettings';
-import { combineUrls } from './vulcan-lib';
+import { combineUrls } from '../lib/vulcan-lib/utils';
+import { PublicInstanceSetting } from '../lib/instanceSettings';
 
-const botSitUrlSetting = new DatabasePublicSetting<string|null>('botSiteUrl', null);
+/** Url of the bot site to redirect to, e.g. https://forum-bots.effectivealtruism.org (must include the http(s)://) */
+const botSiteUrlSetting = new DatabasePublicSetting<string|null>('botSite.url', null);
+/** e.g.
+ * {
+ *   '.*': [ // matches all paths
+ *     '.*python.*',
+ *     ...
+ *   ],
+ *   '/allPosts/?.*|/graphql/?.*': [ // Matches any path starting with /allPosts/ or /graphql/
+ *     '.*python.*',
+ *     ...
+ *   ],
+ * }
+*/
+const botSiteUserAgentRegexesSetting = new DatabasePublicSetting<Record<string, string[]> | null>('botSite.userAgentRegexes', null);
 
-export const botRedirectMiddleware = (req: Request, res: Response, next: AnyBecauseTodo) => {
-  // TODO; inspect for user agent
-  // const blockedFromEverythingBots
-  // const blockedFromGraphqlAndAllPostsBots
-  // Redirect bots to the bot site
-  // TODO; add protocol if not already present
-  const botSiteBaseUrl = botSitUrlSetting.get();
-  if (!botSiteBaseUrl) {
+const botSiteRedirectEnabledSetting = new PublicInstanceSetting<boolean>('botSite.redirectEnabled', false, 'optional');
+
+/**
+ * Middleware function to redirect bot requests to a separate bot site.
+ *
+ * This function inspects the user agent and path of incoming requests. If the user agent matches
+ * a certain pattern and the bot site redirect setting is enabled, it redirects the request to
+ * the bot site. The redirect is a 307 (temporary) redirect to try and prevent search engines
+ * from permanently indexing the bot site if we mess this up
+ */
+export const botRedirectMiddleware = (req: Request, res: Response, next: NextFunction) => {
+  const userAgent = req.headers["user-agent"];
+  const botSiteBaseUrl = botSiteUrlSetting.get();
+
+  if (!botSiteRedirectEnabledSetting.get() || !botSiteBaseUrl || !userAgent) {
     return next();
   }
-  const botSiteUrl = combineUrls(botSiteBaseUrl, req.originalUrl);
-  res.redirect(307, botSiteUrl);
+
+  const userAgentRegexes = botSiteUserAgentRegexesSetting.get();
+
+  if (!userAgentRegexes) {
+    return next();
+  }
+
+  // Check each path and associated user agent regexes
+  for (let path in userAgentRegexes) {
+    const pathRegex = new RegExp(path);
+
+    // If request URL matches path and user agent matches any regex, redirect to bot site
+    if (pathRegex.test(req.url)) {
+      const regexes = userAgentRegexes[path];
+      if (regexes.some(regex => new RegExp(regex).test(userAgent))) {
+        const botSiteUrl = combineUrls(botSiteBaseUrl, req.url);
+        return res.redirect(307, botSiteUrl);
+      }
+    }
+  }
+
+  next();
 }
