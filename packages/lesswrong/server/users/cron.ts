5d57e861cc7aa7ef6db0f676792b12f5fef0287d
Ruben Bloom <darkruby501@users.noreply.github.com>
2023-05-19 16:55:53 -0700

    clean up, fix selector, import correctly

--- packages/lesswrong/server/users/cron.ts
+++ packages/lesswrong/server/users/cron.ts
@@ -1,22 +1,22 @@
 import { addCronJob } from '../cronUtil';
 import Users from "../../lib/collections/users/collection";
-import * as _ from 'underscore';
 import { ModeratorActions } from "../../lib/collections/moderatorActions";
+import { allRateLimits } from "../../lib/collections/moderatorActions/schema";
+import { getSignatureWithNote } from "../../lib/collections/users/helpers";
+import * as _ from 'underscore';
 import moment from 'moment';
-import {allRateLimits} from "../../lib/collections/moderatorActions/schema";
-import {getNewModActionNotes, getSignatureWithNote} from "../../lib/collections/users/helpers";
 
 
 addCronJob({
   name: 'expiredRateLimitsReturnToReviewQueue',
-  interval: 'every 1 minute',
+  interval: 'every 24 hours',
   async job() {
   
   
     const startOfDay = moment(new Date()).subtract(1, 'days').toDate()
     const endOfDay = moment(new Date()).add(1, 'days').toDate()
     
-    const rateLimitsExpiringToday = await ModeratorActions.find({type: "rateLimit", endedAt: [{$gte: startOfDay}, {$lt: endOfDay}, ]}).fetch();
+    const rateLimitsExpiringToday = await ModeratorActions.find({type: {$in: allRateLimits}, endedAt: {$gte: startOfDay, $lt: endOfDay}}).fetch();
     const userIdsWithExpiringRateLimits = rateLimitsExpiringToday.map((action) => action.userId);
     const usersWithExpiringRateLimits = await Users.find({_id: {$in: userIdsWithExpiringRateLimits}}).fetch();
     
