f1ab18e1f3f5dbfec4e34fcabdb4c4615b0993a3
Robert Mushkatblat <bobert.mushky@gmail.com>
2023-05-23 11:41:11 -0700

    try adding rate-limited posts test

---
+++ cypress/e2e/posts/create_five_posts.cy.js
@@ -0,0 +1,26 @@
+/// <reference types="Cypress" />
+
+describe('Posts', function() {
+  beforeEach(function() {
+    cy.task('dropAndSeedDatabase');
+    cy.fixture('users/testUser').as('testUser').then(() => {
+      cy.loginAs(this.testUser);
+    });
+  });
+  
+  it('can create five but not six posts per day', function() {
+    for (let i = 0; i < 5; i++) {
+      cy.visit('/newPost');
+      cy.get('.EditTitle-root').type('Test post 123');
+      cy.get('.ck-editor__editable').type('Test body 123');
+      cy.contains("Submit").click();
+      cy.url().should('include', 'test-post-123');
+      cy.contains("Test post 123");
+      cy.contains("Test body 123");  
+    }
+
+    cy.reload();
+    cy.visit('/newPost');
+    cy.contains("Users cannot post more than 5 posts a day");
+  });
+});
