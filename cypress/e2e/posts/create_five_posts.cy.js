8ac26f9f49370bd6fdb7b4e31f87c9cc45905d69
Robert Mushkatblat <bobert.mushky@gmail.com>
2023-05-23 11:59:50 -0700

    add log

--- cypress/e2e/posts/create_five_posts.cy.js
+++ cypress/e2e/posts/create_five_posts.cy.js
@@ -10,6 +10,7 @@ describe('Posts', function() {
   
   it('can create five but not six posts per day', function() {
     for (let i = 0; i < 5; i++) {
+      cy.log(`creating post ${i}`)
       cy.visit('/newPost');
       cy.get('.EditTitle-root').type('Test post 123');
       cy.get('.ck-editor__editable').type('Test body 123');
@@ -17,6 +18,7 @@ describe('Posts', function() {
       cy.url().should('include', 'test-post-123');
       cy.contains("Test post 123");
       cy.contains("Test body 123");  
+      cy.log(`created post ${i}`)
     }
 
     cy.reload();
