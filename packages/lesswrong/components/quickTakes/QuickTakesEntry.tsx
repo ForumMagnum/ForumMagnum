5e3317423ec316bbe03159aacccf5646a0d08690
Ollie Etherington <ollie@etherington.xyz>
2023-06-02 12:52:12 +0100

    Add closed entry styles

--- packages/lesswrong/components/quickTakes/QuickTakesEntry.tsx
+++ packages/lesswrong/components/quickTakes/QuickTakesEntry.tsx
@@ -1,14 +1,52 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { registerComponent, Components } from "../../lib/vulcan-lib";
 
-const styles = (_theme: ThemeType) => ({
+const styles = (theme: ThemeType) => ({
+  root: {
+    background: theme.palette.panelBackground.default,
+    borderRadius: theme.borderRadius.default,
+    padding: 12,
+    display: "flex",
+    gap: "12px",
+    height: 64,
+  },
+  form: {
+    width: "100%",
+    height: "100%",
+  },
+  entry: {
+    width: "100%",
+    height: "100%",
+    color: theme.palette.grey[1000],
+    background: theme.palette.grey[100],
+    borderRadius: theme.borderRadius.default,
+    fontFamily: theme.palette.fonts.sansSerifStack,
+    fontWeight: 500,
+    fontSize: 14,
+    padding: 10,
+    "&::placeholder": {
+      color: theme.palette.grey[600],
+    },
+  },
 });
 
-const QuickTakesEntry = () => {
-  const {} = Components;
+const QuickTakesEntry = ({currentUser, classes}: {
+  currentUser: UsersCurrent,
+  classes: ClassesType,
+}) => {
+  const onSubmit = useCallback(() => {
+  }, [currentUser]);
+  const {UsersProfileImage} = Components;
   return (
-    <div>
-      Entry
+    <div className={classes.root}>
+      <UsersProfileImage user={currentUser} size={32} />
+      <form onSubmit={onSubmit} className={classes.form}>
+        <input
+          type="text"
+          className={classes.entry}
+          placeholder="Share exploratory, draft-stage, rough thoughts..."
+        />
+      </form>
     </div>
   );
 }
--- packages/lesswrong/components/quickTakes/QuickTakesSection.tsx
+++ packages/lesswrong/components/quickTakes/QuickTakesSection.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { registerComponent, Components } from "../../lib/vulcan-lib";
 import { AnalyticsContext } from "../../lib/analyticsEvents";
+import { useCurrentUser } from "../common/withUser";
 
 const styles = (_theme: ThemeType) => ({
 });
 
 const QuickTakesSection = () => {
+  const currentUser = useCurrentUser();
   const {
     SingleColumnSection, SectionTitle, QuickTakesEntry, QuickTakesList,
   } = Components;
@@ -13,7 +15,7 @@ const QuickTakesSection = () => {
     <AnalyticsContext pageSectionContext="quickTakesSection">
       <SingleColumnSection>
         <SectionTitle title="Quick takes" />
-        <QuickTakesEntry />
+        {currentUser && <QuickTakesEntry currentUser={currentUser} />}
         <QuickTakesList />
       </SingleColumnSection>
     </AnalyticsContext>
