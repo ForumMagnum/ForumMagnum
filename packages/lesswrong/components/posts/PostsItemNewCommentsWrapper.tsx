269b080a4ec6f93d90e21d6774dcb819ba82802a
Robert Mushkatblat <bobert.mushky@gmail.com>
2023-06-09 13:33:51 -0700

    fix text

--- packages/lesswrong/components/posts/PostsItemNewCommentsWrapper.tsx
+++ packages/lesswrong/components/posts/PostsItemNewCommentsWrapper.tsx
@@ -13,10 +13,9 @@ const styles = (theme: ThemeType): JssStyles => ({
   }
 })
 
-const PostsItemNewCommentsWrapper = ({ terms, classes, title, post, treeOptions }: {
+const PostsItemNewCommentsWrapper = ({ terms, classes, post, treeOptions }: {
   terms: CommentsViewTerms,
   classes: ClassesType,
-  title?: string,
   post: PostsList,
   treeOptions: CommentTreeOptions,
 }) => {
@@ -27,21 +26,31 @@ const PostsItemNewCommentsWrapper = ({ terms, classes, title, post, treeOptions 
     fetchPolicy: 'cache-first',
     limit: 5,
   });
+
+  const { loading: debateResponsesLoading, results: debateResponses } = useMulti({
+    collectionName: "Comments",
+    fragmentName: "CommentsList",
+    terms: {
+      view: 'recentDebateResponses',
+      postId: post._id,
+      limit: 2,
+    },
+    skip: !post.debate
+  });
+
   const { Loading, CommentsList, NoContent } = Components
 
-  if (!loading && results && !results.length) {
-    return <NoContent>No comments found</NoContent>
-  } 
-  
-  else {
-    const lastCommentId = results && results[0]?._id
-    let nestedComments = results && unflattenComments(results);
-    if (post.debate) {
+  const getCommentsListNode = (commentsList: CommentsList[] | undefined, loadingState: boolean, title: string, reverseOrder?: boolean) => {
+    const lastCommentId = commentsList && commentsList[0]?._id
+    const nestedComments = commentsList && unflattenComments(commentsList);
+
+    if (reverseOrder) {
       nestedComments?.sort((a, b) => new Date(a.item.postedAt).getTime() - new Date(b.item.postedAt).getTime());
     }
+
     return (
       <div>
-        {title && <div className={classes.title}>{title}</div>}
+        {post.debate && <div className={classes.title}>{title}</div>}
         {nestedComments && <CommentsList
           treeOptions={{
             ...treeOptions,
@@ -51,9 +60,34 @@ const PostsItemNewCommentsWrapper = ({ terms, classes, title, post, treeOptions 
           comments={nestedComments}
           startThreadTruncated={true}
         />}
-        {loading && <Loading/>}
+        {loadingState && <Loading/>}
       </div>
     );
+  };
+
+  const noCommentsFound = !loading && results && !results.length;
+  // If it's not a dialog, we want to return NoContent strictly based on noCommentsFound
+  const noDebateResponsesFound = !post.debate
+    ? true
+    : !debateResponsesLoading && debateResponses && !debateResponses.length;
+
+  if (noCommentsFound && noDebateResponsesFound) {
+    return <NoContent>No comments found</NoContent>
+  } 
+  
+  else {
+    const commentsNode = getCommentsListNode(results, loading, 'User Comments');
+
+    if (post.debate) {
+      const debateResponsesNode = getCommentsListNode(debateResponses, debateResponsesLoading, 'Dialog Responses', true);
+
+      return <div>
+        {debateResponsesNode}
+        {commentsNode}
+      </div>;
+    }
+
+    return commentsNode;
   }
 };
 
