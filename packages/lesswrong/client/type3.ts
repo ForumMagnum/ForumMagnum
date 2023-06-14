c7ef7ef20011387c6265aba137714d8a1b20b859
Will Howard <w.howard256+github@gmail.com>
2023-06-14 16:25:58 +0100

    Apply suggestions from code review
    Accept Ollie's suggestions
    
    Co-authored-by: Ollie Etherington <ollie@etherington.xyz>

--- packages/lesswrong/client/type3.ts
+++ packages/lesswrong/client/type3.ts
@@ -1,9 +1,15 @@
 import { captureEvent } from "../lib/analyticsEvents";
 
-const receiveAnalytics = (type: string, event: Record<string, any>) => {
+const receiveAnalytics = (type: string, event: Record<string, AnyBecauseIsInput>) => {
   // See https://docs.type3.audio/#analytics-events for documentation
   const qualifiedType = `type3.${type}`;
   captureEvent(qualifiedType, event);
 }
 
-(window as any).t3aAnalytics = receiveAnalytics;
+declare global {
+  interface Window {
+    t3aAnalytics: typeof receiveAnalytics,
+  }
+}
+
+window.t3aAnalytics = receiveAnalytics;
