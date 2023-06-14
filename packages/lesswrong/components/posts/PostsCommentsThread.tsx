6de22912926c23119d4cb592769b75425ef8c388
Will Howard <w.howard256+github@gmail.com>
2023-06-14 13:46:48 +0100

    Make comment and post fetching run in parallel on the posts page (#7384)

--- packages/lesswrong/components/posts/PostsCommentsThread.tsx
+++ packages/lesswrong/components/posts/PostsCommentsThread.tsx
@@ -1,19 +1,32 @@
 import React from 'react';
 import { Components, registerComponent } from '../../lib/vulcan-lib';
 import { useMulti } from '../../lib/crud/withMulti';
+import { EagerPostComments } from './PostsPage/PostsPage';
+import isEqual from 'lodash/isEqual';
 
-const PostsCommentsThread = ({ post, terms, newForm=true }: {
+export const postsCommentsThreadMultiOptions = {
+  collectionName: "Comments" as const,
+  fragmentName: 'CommentsList' as const,
+  fetchPolicy: 'cache-and-network' as const,
+  enableTotal: true,
+}
+
+const PostsCommentsThread = ({ post, eagerPostComments, terms, newForm=true }: {
   post?: PostsDetails,
+  eagerPostComments?: EagerPostComments,
   terms: CommentsViewTerms,
   newForm?: boolean,
 }) => {
-  let { loading, results, loadMore, loadingMore, totalCount } = useMulti({
+  // check for deep equality between terms and eagerPostComments.terms
+  const useEagerResults = eagerPostComments && isEqual(terms, eagerPostComments?.terms);
+
+  const lazyResults = useMulti({
     terms,
-    collectionName: "Comments",
-    fragmentName: 'CommentsList',
-    fetchPolicy: 'cache-and-network',
-    enableTotal: true,
+    skip: useEagerResults,
+    ...postsCommentsThreadMultiOptions,
   });
+
+  const { loading, results, loadMore, loadingMore, totalCount } = useEagerResults ? eagerPostComments.queryResponse : lazyResults;
   
   if (loading && !results) {
     return <Components.Loading />;
