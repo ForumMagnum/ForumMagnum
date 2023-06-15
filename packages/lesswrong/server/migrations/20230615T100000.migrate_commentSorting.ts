ebcb73465ff928bcc77ab6331f1e455e3d71e9a3
Will Howard <w.howard256+github@gmail.com>
2023-06-15 11:58:52 +0100

    remove schema hash

--- packages/lesswrong/server/migrations/20230615T100000.migrate_commentSorting.ts
+++ packages/lesswrong/server/migrations/20230615T100000.migrate_commentSorting.ts
@@ -1,7 +1,5 @@
 import Users from "../../lib/collections/users/collection";
 
-export const acceptsSchemaHash = "8f9b37b6b8213a24c21dba39e77f7bbb";
-
 export const up = async ({db}: MigrationContext) => {
   if (!Users.isPostgres()) return
 
