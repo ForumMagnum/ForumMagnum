887cc9492b08184385796820bec11747a6b8db7e
James Babcock <jimrandomh@gmail.com>
2023-06-16 11:17:27 -0700

    Try making server-sent events connection auto-reconnect

--- packages/lesswrong/client/serverSentEventsClient.ts
+++ packages/lesswrong/client/serverSentEventsClient.ts
@@ -1,9 +1,34 @@
 import { onServerSentNotificationEvent } from "../components/hooks/useUnreadNotifications";
 
+let serverSentEventSource: EventSource|null = null;
+
 export function subscribeToNotifications() {
-  const evtSource = new EventSource("/api/notificationEvents");
-  evtSource.onmessage = (event) => {
+  recreateEventSource();
+}
+
+function recreateEventSource() {
+  if (serverSentEventSource) {
+    if (serverSentEventSource.readyState === EventSource.CLOSED) {
+      serverSentEventSource = null;
+    } else if (serverSentEventSource.readyState === EventSource.OPEN) {
+      serverSentEventSource.close();
+      serverSentEventSource = null;
+    } else if (serverSentEventSource.readyState === EventSource.CONNECTING) {
+      // If it's already connected, don't try to reconnect again.
+      return;
+    }
+  }
+  // eslint-disable-next-line no-console
+  console.log("Connecting to server-sent events");
+  serverSentEventSource = new EventSource("/api/notificationEvents");
+  serverSentEventSource.onerror = (errorEvent) => {
+    // eslint-disable-next-line no-console
+    setTimeout(() => {
+      console.log(`Server-sent events error`, errorEvent);
+      recreateEventSource();
+    }, 3000);
+  }
+  serverSentEventSource.onmessage = (event) => {
     onServerSentNotificationEvent(event.data);
   }
 }
-
