488e8e2fdbdb0edee8860ca63a9b2877466c7012
Raemon <raemon777@gmail.com>
2023-06-23 11:26:20 -0700

    return votingSystem canCreate

--- packages/lesswrong/lib/collections/posts/schema.tsx
+++ packages/lesswrong/lib/collections/posts/schema.tsx
@@ -1058,7 +1058,7 @@ const schema: SchemaType<DbPost> = {
     type: String,
     optional: true,
     canRead: ['guests'],
-    canCreate: isLW ? ['members'] : ['admins', 'sunshineRegiment'],
+    canCreate: ['admins', 'sunshineRegiment'],
     canUpdate: [userOwnsAndOnLW, 'admins', 'sunshineRegiment'],
     group: isLW ? formGroups.reactExperiment : formGroups.adminOptions,
     control: "select",
