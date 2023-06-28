99397174c1b7d45f4a118b1a865dffbb6e55dd70
Sarah Cheng <sarah.cheng@centreforeffectivealtruism.org>
2023-06-27 23:43:54 -0400

    lint fix

--- packages/lesswrong/components/icons/closeMenuIcon.tsx
+++ packages/lesswrong/components/icons/closeMenuIcon.tsx
@@ -5,10 +5,10 @@ export const CloseMenuIcon = (props: React.HTMLAttributes<SVGElement>) => {
     <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
       <g id="heroicons-mini/bars-3">
       <g id="Rectangle 4920 (Stroke)">
-        <path fill-rule="evenodd" clip-rule="evenodd" d="M2 4.75C2 4.33579 2.33579 4 2.75 4H17.25C17.6642 4 18 4.33579 18 4.75C18 5.16421 17.6642 5.5 17.25 5.5H2.75C2.33579 5.5 2 5.16421 2 4.75ZM7 10C7 9.58579 7.33579 9.25 7.75 9.25H17.25C17.6642 9.25 18 9.58579 18 10C18 10.4142 17.6642 10.75 17.25 10.75H7.75C7.33579 10.75 7 10.4142 7 10ZM2 15.25C2 14.8358 2.33579 14.5 2.75 14.5H17.25C17.6642 14.5 18 14.8358 18 15.25C18 15.6642 17.6642 16 17.25 16H2.75C2.33579 16 2 15.6642 2 15.25Z" fill="currentColor"/>
+        <path fillRule="evenodd" clipRule="evenodd" d="M2 4.75C2 4.33579 2.33579 4 2.75 4H17.25C17.6642 4 18 4.33579 18 4.75C18 5.16421 17.6642 5.5 17.25 5.5H2.75C2.33579 5.5 2 5.16421 2 4.75ZM7 10C7 9.58579 7.33579 9.25 7.75 9.25H17.25C17.6642 9.25 18 9.58579 18 10C18 10.4142 17.6642 10.75 17.25 10.75H7.75C7.33579 10.75 7 10.4142 7 10ZM2 15.25C2 14.8358 2.33579 14.5 2.75 14.5H17.25C17.6642 14.5 18 14.8358 18 15.25C18 15.6642 17.6642 16 17.25 16H2.75C2.33579 16 2 15.6642 2 15.25Z" fill="currentColor"/>
         <path d="M7 10C7 9.58579 7.33579 9.25 7.75 9.25H17.25C17.6642 9.25 18 9.58579 18 10C18 10.4142 17.6642 10.75 17.25 10.75H7.75C7.33579 10.75 7 10.4142 7 10Z" fill="currentColor"/>
       </g>
-        <path id="Vector" d="M5 8L3 10L5 12" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+        <path id="Vector" d="M5 8L3 10L5 12" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
       </g>
     </svg>
   );
