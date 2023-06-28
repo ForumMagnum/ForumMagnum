e54e64c97579eda3069445219c6eda61acdf42a9
James Babcock <jimrandomh@gmail.com>
2023-06-28 12:55:28 -0700

    Logged-out users don't connect to the server-sent events endpoint for notifications

--- packages/lesswrong/client/serverSentEventsClient.ts
+++ packages/lesswrong/client/serverSentEventsClient.ts
@@ -1,36 +1,61 @@
-import { onServerSentNotificationEvent } from "../components/hooks/useUnreadNotifications";
+import { serverSentEventsAPI, onServerSentNotificationEvent } from "../components/hooks/useUnreadNotifications";
 
+let serverSentEventsActive = false;
 let serverSentEventSource: EventSource|null = null;
 let reconnectPending = false;
 
-export function subscribeToNotifications() {
-  recreateEventSource();
+export function initServerSentEvents() {
+  serverSentEventsAPI.setServerSentEventsActive = setServerSentEventsActive;
 }
 
-function recreateEventSource() {
+function setServerSentEventsActive(active: boolean)
+{
+  if (active) {
+    serverSentEventsActive = true;
+    if (!serverSentEventSource && !reconnectPending)
+      connectServerSentEvents();
+  } else {
+    serverSentEventsActive = false;
+    disconnectServerSentEvents();
+  }
+}
+
+function disconnectServerSentEvents() {
+  if (serverSentEventSource) {
+    serverSentEventSource.close();
+    serverSentEventSource = null;
+  }
+}
+
+function connectServerSentEvents() {
   reconnectPending = false;
+  
+  if (!serverSentEventsActive) {
+    return;
+  }
 
   if (serverSentEventSource) {
     if (serverSentEventSource.readyState === EventSource.CLOSED) {
+      // If there's an EventSource but it's closed, we're recreating it.
       serverSentEventSource = null;
-    } else if (serverSentEventSource.readyState === EventSource.OPEN) {
-      serverSentEventSource.close();
-      serverSentEventSource = null;
-    } else if (serverSentEventSource.readyState === EventSource.CONNECTING) {
-      // If it's already connected, don't try to reconnect again.
+    } else if (serverSentEventSource.readyState === EventSource.OPEN || serverSentEventSource.readyState === EventSource.CONNECTING) {
+      // If it's already connected or connecting, don't try to reconnect again.
       return;
     }
   }
   // eslint-disable-next-line no-console
   console.log("Connecting to server-sent events");
   serverSentEventSource = new EventSource("/api/notificationEvents");
+
   serverSentEventSource.onerror = (errorEvent) => {
+    // eslint-disable-next-line no-console
+    console.log(`Server-sent events error`, errorEvent);
+    disconnectServerSentEvents();
+
     if (!reconnectPending) {
       reconnectPending = true;
       setTimeout(() => {
-        // eslint-disable-next-line no-console
-        console.log(`Server-sent events error`, errorEvent);
-        recreateEventSource();
+        connectServerSentEvents();
       }, 15000);
     }
   }
