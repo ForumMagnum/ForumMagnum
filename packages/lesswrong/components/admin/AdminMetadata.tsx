770bb74d1ba8dc3f13112d7ce3937b489e872b19
James Babcock <jimrandomh@gmail.com>
2023-07-04 18:16:13 -0700

    Remove missing/extra-indexes display on /admin that was mongodb-specific and crashed without mongodb

--- packages/lesswrong/components/admin/AdminMetadata.tsx
+++ packages/lesswrong/components/admin/AdminMetadata.tsx
@@ -29,46 +29,12 @@ const AdminMetadata = ({ classes }: { classes: ClassesType }) => {
   const {missingIndexes, extraIndexes, serverInfo} = adminMetadata;
   
   return (<div>
-    <h2>Server Information</h2>
+    <h3>Server Information</h3>
     <ul>
       {Object.keys(serverInfo).map(key => <li key={key}>
         {key}: {typeof serverInfo[key]==="string" ? serverInfo[key] : JSON.stringify(serverInfo[key])}
       </li>)}
     </ul>
-    
-    <h2>Missing Indexes</h2>
-    { missingIndexes.length === 0
-      ? "No missing indexes"
-      : <table className={classes.indexesTable}><tbody>
-          <tr>
-            <td>Collection</td>
-            <td>Index</td>
-          </tr>
-          {missingIndexes.map((missingIndex: AnyBecauseTodo, i: number) => (
-            <tr key={i}>
-              <td>{missingIndex.collectionName}</td>
-              <td className={classes.indexRow}>{JSON.stringify(missingIndex.index)}</td>
-            </tr>
-          ))}
-        </tbody></table>
-    }
-    
-    <h2>Extra Indexes</h2>
-    { extraIndexes.length === 0
-      ? "No extra indexes"
-      : <table className={classes.indexesTable}><tbody>
-          <tr>
-            <td>Collection</td>
-            <td>Index</td>
-          </tr>
-          {extraIndexes.map((extraIndex: AnyBecauseTodo, i: number) => (
-            <tr key={i}>
-              <td>{extraIndex.collectionName}</td>
-              <td className={classes.indexRow}>{JSON.stringify(extraIndex.index)}</td>
-            </tr>
-          ))}
-        </tbody></table>
-    }
   </div>);
 }
 
