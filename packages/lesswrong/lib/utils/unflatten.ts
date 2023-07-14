6f3a99ba6483c0c18a382fb71ceec5f979c27745
James Babcock <jimrandomh@gmail.com>
2023-07-14 12:41:36 -0700

    Make unflattenComments infer the existence of unloaded ancestor comments and insert placeholders for them
    Co-Authored-By: Ollie Etherington <ollie@etherington.xyz>

--- packages/lesswrong/lib/utils/unflatten.ts
+++ packages/lesswrong/lib/utils/unflatten.ts
@@ -1,12 +1,15 @@
-import * as _ from 'underscore';
+import map from 'lodash/map';
+import keyBy from 'lodash/keyBy';
+import some from 'lodash/some';
 
 interface ThreadableCommentType {
   _id: string
-  parentCommentId: string
+  parentCommentId: string|null
   topLevelCommentId: string
 }
 export interface CommentTreeNode<T extends ThreadableCommentType> {
-  item: T,
+  _id: string
+  item: T|null,
   children: Array<CommentTreeNode<T>>
 }
 
@@ -16,43 +19,102 @@ export interface CommentTreeNode<T extends ThreadableCommentType> {
 // wrap the comments in an object with `item` and `children` fields. This
 // avoids cloning the comment object, which is good because the clone messes
 // with React's ability to detect whether updates are needed.
+//
+// If the comments are a subset of the comments on a post rather than the
+// complete set, there may be implied comments that aren't loaded, which are
+// ancestors of comments that are. In that case, nodes appear in the tree with
+// `item:null`, representing the fact that there exists a comment there but it
+// isn't loaded.
 export function unflattenComments<T extends ThreadableCommentType>(comments: Array<T>): Array<CommentTreeNode<T>>
 {
-  const resultsRestructured = _.map(comments, (comment:T): CommentTreeNode<T> => { return { item:comment, children:[] }});
-  return unflattenCommentsRec(resultsRestructured);
-}
-
-export function addGapIndicators<T extends ThreadableCommentType>(comments: Array<CommentTreeNode<T>>): Array<CommentTreeNode<T & {gapIndicator?: boolean}>> {
-  // Sometimes (such as /shortform page), a comment tree is rendered where some comments are not the direct descendant of the previous comment. 
-  // This function adds extra, empty comment nodes to make this UI more visually clear
-  return comments.map((node: CommentTreeNode<T>): CommentTreeNode<T & {gapIndicator?: boolean}> => {
-    if (node?.item?.parentCommentId !== node?.item?.topLevelCommentId) {
-      return {item: {...node.item, gapIndicator: true}, children: node.children}
+  const usedCommentIds = new Set<string>();
+  
+  // Convert comments into (disconnected) tree nodes
+  const resultsRestructured = map(comments, (comment:T): CommentTreeNode<T> => {
+    usedCommentIds.add(comment._id);
+    return { _id: comment._id, item:comment, children:[] }
+  });
+  
+  function addVirtualComment(id: string) {
+    if (!usedCommentIds.has(id)) {
+      usedCommentIds.add(id);
+      resultsRestructured.push({
+        _id: id,
+        item: null,
+        children: []
+      });
     }
-    return node
-  })
+  }
+  
+  // Check if any of the comments mention a parentCommentId or topLevelCommentId
+  // that wasn't in the set; if so, add a tree node for those.
+  for (let comment of comments) {
+    if (comment.parentCommentId && !usedCommentIds.has(comment.parentCommentId)) {
+      addVirtualComment(comment.parentCommentId);
+    }
+    if (!usedCommentIds.has(comment.topLevelCommentId)) {
+      addVirtualComment(comment.topLevelCommentId);
+    }
+  }
+  
+  let resultsById = keyBy(resultsRestructured, r=>r._id);
+  let nonRootCommentIds = new Set<string>();
+  
+  for (let result of resultsRestructured) {
+    if (result.item) {
+      if (result.item.parentCommentId) {
+        const parent = resultsById[result.item.parentCommentId]
+        parent.children.push(result);
+        nonRootCommentIds.add(result._id);
+        
+        // If the parent is an unloaded comment, we can infer that the parent
+        // is a descendent of the same top-level comment as we are
+        if (!parent.item) {
+          const topLevelComment = resultsById[result.item.topLevelCommentId];
+          if (parent._id != topLevelComment._id) {
+            nonRootCommentIds.add(parent._id);
+            if (!some(topLevelComment.children, c=>c._id===parent._id)) {
+              topLevelComment.children.push(parent);
+            }
+          }
+        }
+      }
+    }
+  }
+  
+  let rootComments: Array<CommentTreeNode<T>> = [];
+  for (let result of resultsRestructured) {
+    if (!nonRootCommentIds.has(result._id)) {
+      rootComments.push(result);
+    }
+  }
+  return rootComments;
 }
 
 // Recursive portion of unflattenComments. Produced by incremental modification
 // of Vulcan's Utils.unflatten.
-function unflattenCommentsRec<T extends ThreadableCommentType>(array: Array<CommentTreeNode<T>>, parent?: CommentTreeNode<T>): Array<CommentTreeNode<T>>
+/*function unflattenCommentsRec<T extends ThreadableCommentType>(array: Array<CommentTreeNode<T>>, parent?: CommentTreeNode<T>): Array<CommentTreeNode<T>>
 {
   let tree: Array<CommentTreeNode<T>> = [];
 
   let children: Array<CommentTreeNode<T>> = [];
 
   if (typeof parent === "undefined") {
-    let commentDict: Partial<Record<string,boolean>> = {}
+    let commentDict = new Set<string>();
     array.forEach((node) => {
-      commentDict[node.item._id] = true
+      commentDict.add(node._id);
     })
     // if there is no parent, we're at the root level
     // so we return all root nodes (i.e. nodes with no parent)
-    children = _.filter(array, (node:CommentTreeNode<T>) => (!node.item.parentCommentId || !commentDict[node.item.parentCommentId]));
+    children = filter(array, (node:CommentTreeNode<T>) =>
+      (node.item && (!node.item.parentCommentId || !commentDict.has(node.item.parentCommentId)))
+    );
   } else {
     // if there *is* a parent, we return all its child nodes
     // (i.e. nodes whose parentId is equal to the parent's id.)
-    children = _.filter(array, (node:CommentTreeNode<T>) => node.item.parentCommentId === parent.item._id);
+    children = filter(array, (node:CommentTreeNode<T>) =>
+      node.item?.parentCommentId === parent._id
+     );
   }
 
   // if we found children, we keep on iterating
@@ -73,7 +135,18 @@ function unflattenCommentsRec<T extends ThreadableCommentType>(array: Array<Comm
   }
 
   return tree;
-}
+}*/
+
+/*export function addGapIndicators<T extends ThreadableCommentType>(comments: Array<CommentTreeNode<T>>): Array<CommentTreeNode<T & {gapIndicator?: boolean}>> {
+  // Sometimes (such as /shortform page), a comment tree is rendered where some comments are not the direct descendant of the previous comment. 
+  // This function adds extra, empty comment nodes to make this UI more visually clear
+  return comments.map((node: CommentTreeNode<T>): CommentTreeNode<T & {gapIndicator?: boolean}> => {
+    if (node?.item?.parentCommentId !== node?.item?.topLevelCommentId) {
+      return {item: {...node.item, gapIndicator: true}, children: node.children}
+    }
+    return node
+  })
+}*/
 
 export function commentTreesEqual<Fragment extends ThreadableCommentType>(a: Array<CommentTreeNode<Fragment>>, b: Array<CommentTreeNode<Fragment>>) {
   if (!!a !== !!b) return false;
---
+++ packages/lesswrong/unitTests/unflatted.tests.ts
@@ -0,0 +1,97 @@
+import { unflattenComments } from '../lib/utils/unflatten';
+import chai from 'chai';
+
+type MinimalComment = {
+  _id: string
+  parentCommentId: string|null
+  topLevelCommentId: string
+}
+
+describe("unflatten comments", () => {
+  it('unflattens comments', () => {
+    // C
+    // |\
+    // C C
+    chai.assert.deepEqual(
+      unflattenComments<MinimalComment>([
+        {_id:"1", parentCommentId:null, topLevelCommentId:"1"},
+        {_id:"2", parentCommentId:"1", topLevelCommentId:"1"},
+        {_id:"3", parentCommentId:"1", topLevelCommentId:"1"},
+      ]),
+      [{
+        _id:"1",
+        item: {_id:"1", parentCommentId:null, topLevelCommentId:"1"},
+        children: [
+          {
+            _id: "2",
+            item: {_id:"2", parentCommentId:"1", topLevelCommentId:"1"},
+            children: [],
+          },
+          {
+            _id: "3",
+            item: {_id:"3", parentCommentId:"1", topLevelCommentId:"1"},
+            children: [],
+          },
+        ],
+      }]
+    );
+    // _
+    // |\
+    // C C
+    chai.assert.deepEqual(
+      unflattenComments<MinimalComment>([
+        {_id:"2", parentCommentId:"1", topLevelCommentId:"1"},
+        {_id:"3", parentCommentId:"1", topLevelCommentId:"1"},
+      ]),
+      [{
+        _id:"1",
+        item: null,
+        children: [
+          {
+            _id: "2",
+            item: {_id:"2", parentCommentId:"1", topLevelCommentId:"1"},
+            children: [],
+          },
+          {
+            _id: "3",
+            item: {_id:"3", parentCommentId:"1", topLevelCommentId:"1"},
+            children: [],
+          },
+        ],
+      }]
+    );
+    // 1_
+    //  |\
+    // 2_ C3
+    //  |
+    // 4C
+    chai.assert.deepEqual(
+      unflattenComments<MinimalComment>([
+        {_id:"3", parentCommentId:"1", topLevelCommentId:"1"},
+        {_id:"4", parentCommentId:"2", topLevelCommentId:"1"},
+      ]),
+      [{
+        _id:"1",
+        item: null,
+        children: [
+          {
+            _id: "3",
+            item: {_id:"3", parentCommentId:"1", topLevelCommentId:"1"},
+            children: [],
+          },
+          {
+            _id: "2",
+            item: null,
+            children: [
+              {
+                _id: "4",
+                item: {_id:"4", parentCommentId:"2", topLevelCommentId:"1"},
+                children: [],
+              }
+            ],
+          },
+        ],
+      }]
+    );
+  });
+});
