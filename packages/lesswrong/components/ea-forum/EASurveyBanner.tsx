2db269d97ed65820b10de5744dae1aff74473f97
Ollie Etherington <ollie@etherington.xyz>
2023-08-01 17:36:08 +0100

    Add basic banner styles

--- packages/lesswrong/components/ea-forum/EASurveyBanner.tsx
+++ packages/lesswrong/components/ea-forum/EASurveyBanner.tsx
@@ -1,15 +1,25 @@
 import React from "react";
 import { registerComponent } from "../../lib/vulcan-lib";
 
-const styles = (_theme: ThemeType) => ({
+const styles = (theme: ThemeType) => ({
   root: {
+    position: "sticky",
+    top: 0,
+    zIndex: 100000,
+    padding: 20,
+    background: theme.palette.primary.main,
+    fontFamily: theme.palette.fonts.sansSerifStack,
+    fontSize: 15,
+    fontWeight: 450,
+    textAlign: "center",
+    color: theme.palette.text.alwaysWhite,
   },
 });
 
-const EASurveyBanner = () => {
+const EASurveyBanner = ({classes}: {classes: ClassesType}) => {
   return (
-    <div>
-      Banner!
+    <div className={classes.root}>
+      Help evaluate the forum by taking the 5 minute EA Forum survey
     </div>
   );
 }
