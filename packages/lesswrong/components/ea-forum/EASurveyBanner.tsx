979cec1d7b831f8582ec57943b4ae0673f7d06bf
Ollie Etherington <ollie@etherington.xyz>
2023-08-01 17:45:47 +0100

    Add button styles in survey banner

--- packages/lesswrong/components/ea-forum/EASurveyBanner.tsx
+++ packages/lesswrong/components/ea-forum/EASurveyBanner.tsx
@@ -1,25 +1,51 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { registerComponent } from "../../lib/vulcan-lib";
+import { TypeformPopupEmbed } from "../common/TypeformEmbeds";
 
 const styles = (theme: ThemeType) => ({
   root: {
     position: "sticky",
     top: 0,
-    zIndex: 100000,
-    padding: 20,
+    zIndex: 10000, // The typeform popup has z-index 10001
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: "25px",
+    padding: 16,
     background: theme.palette.primary.main,
     fontFamily: theme.palette.fonts.sansSerifStack,
     fontSize: 15,
     fontWeight: 450,
-    textAlign: "center",
     color: theme.palette.text.alwaysWhite,
+  },
+  button: {
+    background: theme.palette.text.alwaysWhite,
+    color: theme.palette.text.alwaysBlack,
+    borderRadius: theme.borderRadius.default,
+    fontSize: 15,
+    fontWeight: 500,
+    padding: "8px 12px",
+    cursor: "pointer",
+    "&:hover": {
+      opacity: 0.9,
+    },
   },
 });
 
 const EASurveyBanner = ({classes}: {classes: ClassesType}) => {
+  const onClose = useCallback(() => {
+  }, []);
+
   return (
     <div className={classes.root}>
       Help evaluate the forum by taking the 5 minute EA Forum survey
+      <TypeformPopupEmbed
+        widgetId="Z1wH4v8v"
+        title="EA Forum survey"
+        label="Take the survey"
+        onClose={onClose}
+        className={classes.button}
+      />
     </div>
   );
 }
