0d9e6f6a319378a95e2b1c2949f49afccddfd884
Sarah Cheng <sarah.cheng@centreforeffectivealtruism.org>
2023-08-08 11:58:18 -0400

    actually fix admins who are not mods

--- packages/lesswrong/components/admin/useAdminToggle.ts
+++ packages/lesswrong/components/admin/useAdminToggle.ts
@@ -53,9 +53,9 @@ export const useAdminToggle = (): {
     // If not a member of the realAdmins group, add that group
     // before giving up admin powers so that we'll be able to take
     // the admin powers back
-    let groups = currentUser.groups || [];
+    let groups = currentUser.groups || []
     if (!groups.some(g => g === "realAdmins")) {
-      groups = [...currentUser.groups, "realAdmins"]
+      groups = [...groups, "realAdmins"]
       await updateCurrentUser({ groups })
     }
     await updateCurrentUser({
