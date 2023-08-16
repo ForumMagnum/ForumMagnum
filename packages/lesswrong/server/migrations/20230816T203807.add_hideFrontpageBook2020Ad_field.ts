e4957980411bb7858ffb62a03a91972deae581a1
Raemon <raemon777@gmail.com>
2023-08-16 13:40:13 -0700

    re-run migrations

--- packages/lesswrong/server/migrations/20230816T203807.add_hideFrontpageBook2020Ad_field.ts
+++ packages/lesswrong/server/migrations/20230816T203807.add_hideFrontpageBook2020Ad_field.ts
@@ -1,24 +1,24 @@
 /**
- * Generated on 2023-08-10T05:15:06.537Z by `yarn makemigrations`
+ * Generated on 2023-08-16T20:38:07.177Z by `yarn makemigrations`
  * The following schema changes were detected:
  * -------------------------------------------
  * diff --git a/Users/raymondarnold/Documents/LessWrongSuite/Lesswrong2/schema/accepted_schema.sql b/Users/raymondarnold/Documents/LessWrongSuite/Lesswrong2/schema/schema_to_accept.sql
- * index 25d15814a6..618e73ea04 100644
+ * index 5249d88575..d75708ee3d 100644
  * --- a/Users/raymondarnold/Documents/LessWrongSuite/Lesswrong2/schema/accepted_schema.sql
  * +++ b/Users/raymondarnold/Documents/LessWrongSuite/Lesswrong2/schema/schema_to_accept.sql
  * @@ -4,5 +4,3 @@
  *  --
- * --- Overall schema hash: e7d890dadc54453234507be13698b7b7
+ * --- Overall schema hash: d7266200db426f47cd1ba7abcc6a6db7
  * -
- * --- Accepted on 2023-07-27T12:20:27.000Z by 20230727T122027.add_isPostType_field.ts
- * +-- Overall schema hash: cbb83104f3628956058186f0e2c1b1bf
+ * --- Accepted on 2023-08-07T23:01:23.000Z by 20230807T230123.add_hideHomeRHS_to_Users.ts
+ * +-- Overall schema hash: 4aca6d1717b9bb1f98fa154bd99ea8a5
  *  
  * @@ -1003,3 +1001,3 @@ CREATE TABLE "UserTagRels" (
  *  
- * --- Schema for "Users", hash: d64da8350def59cbeb640f264a7b09a4
- * +-- Schema for "Users", hash: 7641869ff4e3e926d16e9165f4894b85
+ * --- Schema for "Users", hash: 6af340dca1e5f32d30449abd73726b6f
+ * +-- Schema for "Users", hash: 269813160b5b2c5562d86035253bf007
  *  CREATE TABLE "Users" (
- * @@ -1120,2 +1118,3 @@ CREATE TABLE "Users" (
+ * @@ -1121,2 +1119,3 @@ CREATE TABLE "Users" (
  *      "hideFrontpageBook2019Ad" bool,
  * +    "hideFrontpageBook2020Ad" bool,
  *      "sunshineNotes" text DEFAULT '',
@@ -31,7 +31,7 @@
  * - [ ] Uncomment `acceptsSchemaHash` below
  * - [ ] Run `yarn acceptmigrations` to update the accepted schema hash (running makemigrations again will also do this)
  */
-export const acceptsSchemaHash = "cbb83104f3628956058186f0e2c1b1bf";
+export const acceptsSchemaHash = "4aca6d1717b9bb1f98fa154bd99ea8a5";
 
 import Users from "../../lib/collections/users/collection";
 import { addField, dropField } from "./meta/utils";
