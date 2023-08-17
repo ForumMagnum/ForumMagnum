153f1e29798057048f4e19bf9a4ff2c0dfb01f72
Raemon <raemon777@gmail.com>
2023-08-16 19:02:59 -0700

    promise all await

--- packages/lesswrong/server/scripts/bestOfLessWrongTagUpdate.ts
+++ packages/lesswrong/server/scripts/bestOfLessWrongTagUpdate.ts
@@ -6,18 +6,23 @@ import Tags from "../../lib/collections/tags/collection";
 import Users from "../../lib/collections/users/collection";
 
 async function bestOfLessWrongTagUpdate () {
-  const posts = await Posts.find({
-    finalReviewVoteScoreHighKarma: {$gt: 20},
-  }, {sort: {postedAt: -1}}).fetch();
-
   const tag = await Tags.findOne({slug: "best-of-lesswrong"});
   const user = await Users.findOne({displayName: "Raemon"});
   const context = createAdminContext();
 
   if (tag && user && context) {
-    for (let i = 0; i < posts.length; i++) {
-      void addOrUpvoteTag({tagId: tag?._id, postId:posts[i]._id, currentUser: user, context});
-    }
+    const posts = await Posts.find({
+      finalReviewVoteScoreHighKarma: {$gt: 20},
+      [`tagRelevance.${tag._id}`]: {$exists: false}
+    }, {sort: {postedAt: -1}}).fetch();
+
+    // eslint-disable-next-line no-console
+    console.log(posts.length)
+    posts.forEach((post, i) => {
+      // eslint-disable-next-line no-console
+      console.log(i, post.title, moment(post.postedAt).format("YYYY-MM-DD"))
+    })
+    await Promise.all(posts.map((post) => addOrUpvoteTag({tagId: tag?._id, postId:post._id, currentUser: user, context})))
   }
 }
 
