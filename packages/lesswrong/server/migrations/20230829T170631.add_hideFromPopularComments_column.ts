19fa62827e8746bf4558d4a561594c1ea205a38a
Ollie Etherington <ollie@etherington.xyz>
2023-08-29 20:22:05 +0300

    Fix lint

--- packages/lesswrong/server/migrations/20230829T170631.add_hideFromPopularComments_column.ts
+++ packages/lesswrong/server/migrations/20230829T170631.add_hideFromPopularComments_column.ts
@@ -5,12 +5,12 @@ export const acceptsSchemaHash = "7d3553d2dcd4a5e47968398dfee076f2";
 
 export const up = async ({db}: MigrationContext) => {
   if (Posts.isPostgres()) {
-    addField(db, Posts, "hideFromPopularComments");
+    await addField(db, Posts, "hideFromPopularComments");
   }
 }
 
 export const down = async ({db}: MigrationContext) => {
   if (Posts.isPostgres()) {
-    dropField(db, Posts, "hideFromPopularComments");
+    await dropField(db, Posts, "hideFromPopularComments");
   }
 }
