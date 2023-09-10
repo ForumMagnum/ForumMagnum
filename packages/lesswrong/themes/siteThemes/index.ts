75e0657abd1a15ed097b4f65ca16418576e4ac0f
JP Addison <johnpaddison@gmail.com>
2023-09-10 12:14:31 -0400

    remove references to wu from the branch destined for master

--- packages/lesswrong/themes/siteThemes/index.ts
+++ packages/lesswrong/themes/siteThemes/index.ts
@@ -2,14 +2,12 @@ import { ForumTypeString } from '../../lib/instanceSettings';
 import { alignmentForumTheme } from './alignmentForumTheme'
 import { eaForumTheme } from './eaTheme'
 import { lessWrongTheme } from './lesswrongTheme'
-import { wakingUpTheme } from './wakingUpTheme'
 
 export const getSiteTheme = (forumType: ForumTypeString): SiteThemeSpecification => {
   const forumThemes: Record<ForumTypeString, SiteThemeSpecification> = {
     AlignmentForum: alignmentForumTheme,
     EAForum: eaForumTheme,
     LessWrong: lessWrongTheme,
-    WakingUp: wakingUpTheme,
   }
   return forumThemes[forumType];
 }
