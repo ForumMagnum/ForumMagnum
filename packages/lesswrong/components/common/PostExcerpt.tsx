c379f64bcd6d1a9b1e0597a3555cb7a67d163508
Ollie Etherington <ollie@etherington.xyz>
2023-09-13 19:36:08 +0300

    Add 'more' button to post excerpt

--- packages/lesswrong/components/common/PostExcerpt.tsx
+++ packages/lesswrong/components/common/PostExcerpt.tsx
@@ -1,15 +1,36 @@
 import React, { CSSProperties } from "react";
 import { Components, registerComponent } from "../../lib/vulcan-lib";
+import { Link } from "../../lib/reactRouterWrapper";
+import { postGetPageUrl } from "../../lib/collections/posts/helpers";
 import classNames from "classnames";
 
-const styles = (_theme: ThemeType) => ({
+const styles = (theme: ThemeType) => ({
   root: {
+    position: "relative",
     fontSize: "1.1rem",
     lineHeight: "1.5em",
     overflow: "hidden",
     textOverflow: "ellipsis",
     display: "-webkit-box",
     "-webkit-box-orient": "vertical",
+  },
+  more: {
+    position: "absolute",
+    bottom: 0,
+    right: 0,
+    fontFamily: theme.palette.fonts.sansSerifStack,
+    fontWeight: 500,
+    paddingLeft: 30,
+    background: `linear-gradient(
+      90deg,
+      ${theme.palette.inverseGreyAlpha(0.2)} 0%,
+      ${theme.palette.inverseGreyAlpha(1.0)} 34%,
+      ${theme.palette.inverseGreyAlpha(1.0)} 100%
+    )`,
+    "&:hover": {
+      opacity: 1,
+      color: `${theme.palette.primary.light} !important`,
+    },
   },
 });
 
@@ -23,7 +44,6 @@ const PostExcerpt = ({post, lines = 3, className, classes}: {
     return null;
   }
 
-  // TODO: Add a 'More' button linking to the post
   const {ContentStyles, ContentItemBody} = Components;
   return (
     <ContentStyles
@@ -34,6 +54,7 @@ const PostExcerpt = ({post, lines = 3, className, classes}: {
       <ContentItemBody dangerouslySetInnerHTML={{
         __html: post.contents?.htmlHighlight ?? "",
       }} />
+      <Link to={postGetPageUrl(post)} className={classes.more}>(More)</Link>
     </ContentStyles>
   );
 }
