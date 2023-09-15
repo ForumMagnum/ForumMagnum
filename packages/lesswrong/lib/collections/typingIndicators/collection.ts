585ccf4da177c6516f9537fa38eef32f8ebf6dbc
Raemon <raemon777@gmail.com>
2023-09-15 14:19:34 -0700

    remove default mutations

--- packages/lesswrong/lib/collections/typingIndicators/collection.ts
+++ packages/lesswrong/lib/collections/typingIndicators/collection.ts
@@ -5,30 +5,12 @@ import {createCollection} from "../../vulcan-lib";
 import {userOwns} from "../../vulcan-users/permissions";
 import schema from "./schema";
 
-const options: MutationOptions<DbTypingIndicator> = {
-  newCheck: (user: DbUser|null, document: DbTypingIndicator|null) => {
-    if (!user || !document) return false;
-    return true
-  },
-
-  editCheck: (user: DbUser|null, document: DbTypingIndicator|null) => {
-    if (!user || !document) return false;
-    return userOwns(user, document) 
-  },
-
-  removeCheck: (user: DbUser|null, document: DbTypingIndicator|null) => {
-    if (!user || !document) return false;
-    return userOwns(user, document) 
-  },
-}
-
 export const TypingIndicators: TypingIndicatorsCollection = createCollection({
   collectionName: 'TypingIndicators',
   typeName: 'TypingIndicator',
   collectionType: 'pg',
   schema,
   resolvers: getDefaultResolvers('TypingIndicators'),
-  mutations: getDefaultMutations('TypingIndicators', options),
   logChanges: true,
 })
 
