5bc3f67b13a44e1ab53e06abdc1afe7968036337
Michael Keenan <michael.keenan@gmail.com>
2023-09-25 22:30:25 -0700

    Remove Recommendations and Recent Discussion from homepage

--- packages/lesswrong/components/common/LWHome.tsx
+++ packages/lesswrong/components/common/LWHome.tsx
@@ -1,37 +1,19 @@
 import { Components, registerComponent } from '../../lib/vulcan-lib';
 import React from 'react';
 import { AnalyticsContext } from "../../lib/analyticsEvents";
-import { getReviewPhase, reviewIsActive, REVIEW_YEAR } from '../../lib/reviewUtils';
 
 const LWHome = () => {
-  const { RecentDiscussionFeed, HomeLatestPosts, AnalyticsInViewTracker, LWRecommendations, FrontpageReviewWidget, SingleColumnSection, FrontpageBestOfLWWidget } = Components
+  const { HomeLatestPosts, AnalyticsInViewTracker } = Components
 
   return (
       <AnalyticsContext pageContext="homePage">
         <React.Fragment>
-
-          {!reviewIsActive() && <LWRecommendations configName="frontpage" />}
-
-          {reviewIsActive() && getReviewPhase() === "RESULTS" && <SingleColumnSection>
-            <FrontpageBestOfLWWidget reviewYear={REVIEW_YEAR}/>
-          </SingleColumnSection>}
-        
-          {reviewIsActive() && getReviewPhase() !== "RESULTS" && <SingleColumnSection>
-            <FrontpageReviewWidget reviewYear={REVIEW_YEAR}/>
-          </SingleColumnSection>}
-          
           <AnalyticsInViewTracker
               eventProps={{inViewType: "latestPosts"}}
               observerProps={{threshold:[0, 0.5, 1]}}
           >
             <HomeLatestPosts />
           </AnalyticsInViewTracker>
-
-          <RecentDiscussionFeed
-            af={false}
-            commentsLimit={4}
-            maxAgeHours={18}
-          />
         </React.Fragment>
       </AnalyticsContext>
   )
