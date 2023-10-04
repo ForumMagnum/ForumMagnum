6bc43be807d5381c9aea8f6252977780f3ade3f8
Ollie Etherington <ollie@etherington.xyz>
2023-10-04 18:39:00 +0300

    Order EA emojis on posts by date instead of karma

--- packages/lesswrong/server/repos/PostsRepo.ts
+++ packages/lesswrong/server/repos/PostsRepo.ts
@@ -107,12 +107,12 @@ export default class PostsRepo extends AbstractRepo<DbPost> {
         SELECT
           "key",
           (ARRAY_AGG(
-            "displayName" ORDER BY COALESCE("karma", 0) DESC)
+            "displayName" ORDER BY "createdAt" ASC)
           ) AS "displayNames"
         FROM (
           SELECT
             u."displayName",
-            u."karma",
+            v."createdAt",
             (JSONB_EACH(v."extendedVoteType")).*
           FROM "Votes" v
           JOIN "Users" u ON u."_id" = v."userId"
