156c254c5e90607ae60a2704164f2f7a89c6a87b
Raemon <raemon777@gmail.com>
2023-10-06 15:44:21 -0700

    Update editor guidelines

--- packages/lesswrong/components/posts/dialogues/DialogueEditorUI.tsx
+++ packages/lesswrong/components/posts/dialogues/DialogueEditorUI.tsx
@@ -27,7 +27,7 @@ export const DialogueEditorUI = ({classes}: {
     <div>Dialogue Editor</div>
     <ul className={classes.info}>
       <li>You can edit your responses afterwards.</li>
-      <li>You can see each other's responses as you type them</li>
+      <li>You can see each other's responses as you type them.</li>
       <li>Default etiquette is that it's fine to draft your message before the other person finishes.</li>
     </ul>
   </div>;
