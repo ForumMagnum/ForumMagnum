38bfea3250227b9e8ae45044dcfe81c9c3dc729c
Ruben Bloom <darkruby501@users.noreply.github.com>
2023-10-06 12:42:27 -0700

    yarn migrations stuff

--- packages/lesswrong/server/migrations/20231006T193146.dialogue_notifications_and_post_sharing_default.ts
+++ packages/lesswrong/server/migrations/20231006T193146.dialogue_notifications_and_post_sharing_default.ts
@@ -1,8 +1,9 @@
-import {addField, dropField} from "./meta/utils";
 import Users from "../../lib/collections/users/collection";
+import Posts from "../../lib/collections/posts/collection";
+import {addField, dropField, updateDefaultValue} from "./meta/utils";
 
 /**
- * Generated on 2023-10-06T00:16:13.001Z by `yarn makemigrations`
+ * Generated on 2023-10-06T19:31:46.057Z by `yarn makemigrations`
  * The following schema changes were detected:
  * -------------------------------------------
  * ***Diff too large to display***
@@ -14,16 +15,20 @@ import Users from "../../lib/collections/users/collection";
  * - [ ] Uncomment `acceptsSchemaHash` below
  * - [ ] Run `yarn acceptmigrations` to update the accepted schema hash (running makemigrations again will also do this)
  */
-export const acceptsSchemaHash = "b4ce54de2bace9053d82bab6730be031";
+export const acceptsSchemaHash = "ab02d7ee110cc11f4df3b0f16a24905d";
 
 export const up = async ({db}: MigrationContext) => {
-  if (!Users.isPostgres()) return
+  
+  if (!Users.isPostgres() || !Posts.isPostgres()) return
   await addField(db, Users, "notificationDialogueMessages")
   await addField(db, Users, "notificationPublishedDialogueMessages")
+  await updateDefaultValue(db, Posts, "shareWithUsers")
+  
 }
 
 export const down = async ({db}: MigrationContext) => {
-  if (!Users.isPostgres()) return
+  if (!Users.isPostgres() || !Posts.isPostgres()) return
   await dropField(db, Users, "notificationDialogueMessages")
   await dropField(db, Users, "notificationPublishedDialogueMessages")
 }
+
