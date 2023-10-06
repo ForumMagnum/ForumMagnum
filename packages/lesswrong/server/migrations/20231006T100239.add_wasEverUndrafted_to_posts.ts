6b97f9df42abac72f917c75b46c6620e59229cdb
Ollie Etherington <ollie@etherington.xyz>
2023-10-06 13:18:33 +0300

    Set `wasEverUndrafted` value for existing posts

--- packages/lesswrong/server/migrations/20231006T100239.add_wasEverUndrafted_to_posts.ts
+++ packages/lesswrong/server/migrations/20231006T100239.add_wasEverUndrafted_to_posts.ts
@@ -6,6 +6,14 @@ export const acceptsSchemaHash = "32e3d3213b13f5c68390fe80b3f93571";
 export const up = async ({db}: MigrationContext) => {
   if (Posts.isPostgres()) {
     await addField(db, Posts, "wasEverUndrafted");
+    await db.none(`
+      UPDATE "Posts"
+      SET "wasEverUndrafted" = TRUE
+      WHERE
+        "draft" IS NOT TRUE OR
+        "frontpageDate" IS NOT NULL OR
+        "curatedDate" IS NOT NULL
+    `);
   }
 }
 
