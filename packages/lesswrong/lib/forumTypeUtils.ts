82b322814079350c644ab73ef05dec3eb5bfa3b0
Michael Keenan <michael.keenan@gmail.com>
2023-10-10 15:49:37 -0700

    Fix circular dependency problem

--- packages/lesswrong/lib/forumTypeUtils.ts
+++ packages/lesswrong/lib/forumTypeUtils.ts
@@ -1,6 +1,5 @@
 import capitalize from "lodash/fp/capitalize";
 import { forumTypeSetting, ForumTypeString } from "./instanceSettings"
-import { isFriendlyUI } from "../themes/forumTheme";
 
 //Partial Type adds "undefined" erroneously to T, so we need to explicitly tell TS that it can't be undefined.
 type NonUndefined<T> = T extends undefined ? never : T;
@@ -34,6 +33,10 @@ export class DeferredForumSelect<T> {
 
 /**
  * Convert heading to sentence case in Friendly UI sites, leave as is on LW (will usually be "start case" e.g. "Set Topics").
- * In the event of edge cases (e.g. "EA Forum" -> "Ea forum"), it's probably best to do an inline forumTypeSetting check
+ * In the event of edge cases (e.g. "EA Forum" -> "Ea forum"), it's probably best to do an inline forumTypeSetting check.
+ * Ideally we'd use an isFriendlyUI check, but that would create a circular dependency with themeNames.ts.
  */
-export const preferredHeadingCase = isFriendlyUI ? capitalize : (s: string) => s;
+export const preferredHeadingCase = forumSelect({
+  EAForum: capitalize, // e.g. "Set Topics" => "Set topics"
+  default: (s: string) => s,
+});
