fe84b798013ac8da1846c0645adcb4a8d35e3b2a
Ruben Bloom <darkruby501@users.noreply.github.com>
2023-10-24 17:17:45 -0700

    trying to replicate their example

--- public/lesswrong-editor/src/ckeditor5-dialogue-comments/dialogue-comment-box.js
+++ public/lesswrong-editor/src/ckeditor5-dialogue-comments/dialogue-comment-box.js
@@ -19,7 +19,7 @@ class SimpleBoxUI extends Plugin {
         // to be displayed in the toolbar.
         editor.ui.componentFactory.add( 'dialogueMessageInput', locale => {
             // The state of the button will be bound to the widget command.
-            const command = editor.commands.get( 'insertSimpleBox' );
+            const command = editor.commands.get( 'insertRootParagraphBox' );
 
             // The button will be an instance of ButtonView.
             const buttonView = new ButtonView( locale );
@@ -36,11 +36,30 @@ class SimpleBoxUI extends Plugin {
             buttonView.bind( 'isOn', 'isEnabled' ).to( command, 'value', 'isEnabled' );
 
             // Execute the command when the button is clicked (executed).
-            this.listenTo( buttonView, 'execute', () => editor.execute( 'insertSimpleBox' ) );
+            this.listenTo( buttonView, 'execute', () => editor.execute( 'insertRootParagraphBox' ) );
 
             return buttonView;
         } );
+
     }
+}
+
+class InsertSimpleBoxCommand extends Command {
+	execute() {
+		this.editor.model.change( writer => {
+			// Insert <simpleBox>*</simpleBox> at the current selection position
+			// in a way that will result in creating a valid model structure.
+			this.editor.model.insertContent( createSimpleBox( writer ) );
+		} );
+	}
+
+	refresh() {
+		const model = this.editor.model;
+		const selection = model.document.selection;
+		const allowedIn = model.schema.findAllowedParent( selection.getFirstPosition(), 'simpleBox' );
+
+		this.isEnabled = allowedIn !== null;
+	}
 }
 
 class SimpleBoxEditing extends Plugin {
@@ -52,7 +71,8 @@ class SimpleBoxEditing extends Plugin {
         this._defineSchema();
         this._defineConverters();
 
-        this.editor.commands.add( 'insertSimpleBox', new InsertRootParagraphBoxCommand( this.editor ) );
+        this.editor.commands.add( 'insertRootParagraphBox', new InsertRootParagraphBoxCommand( this.editor ) );
+        this.editor.commands.add( 'insertSimpleBox', new InsertSimpleBoxCommand( this.editor ) );
         this.editor.commands.add( 'submitDialogueMessage', new SubmitDialogueMessageCommand( this.editor ) );
 
         this.editor.keystrokes.set( 'Ctrl+Enter', (evt, cancel) => {
@@ -149,6 +169,40 @@ class SimpleBoxEditing extends Plugin {
                 return false;
             }
         });
+
+        schema.register( 'simpleBox', {
+			// Behaves like a self-contained object (e.g. an image).
+			isObject: true,
+
+			// Allow in places where other blocks are allowed (e.g. directly in the root).
+			allowWhere: '$block'
+		} );
+
+		schema.register( 'simpleBoxTitle', {
+			// Cannot be split or left by the caret.
+			isLimit: true,
+
+			allowIn: 'simpleBox',
+
+			// Allow content which is allowed in blocks (i.e. text with attributes).
+			allowContentOf: '$block'
+		} );
+
+		schema.register( 'simpleBoxDescription', {
+			// Cannot be split or left by the caret.
+			isLimit: true,
+
+			allowIn: 'simpleBox',
+
+			// Allow content which is allowed in the root (e.g. paragraphs).
+			allowContentOf: '$root'
+		} );
+
+		// schema.addChildCheck( ( context, childDefinition ) => {
+		// 	if ( context.endsWith( 'simpleBoxDescription' ) && childDefinition.name == 'simpleBox' ) {
+		// 		return false;
+		// 	}
+		// } );
     }
 
     _defineConverters() {
@@ -238,7 +292,96 @@ class SimpleBoxEditing extends Plugin {
             model: 'dialogueMessage',
             view: messageEditingDowncastViewGenerator
         } );
+
+        		// <simpleBox> converters
+		conversion.for( 'upcast' ).elementToElement( {
+			model: 'simpleBox',
+			view: {
+				name: 'section',
+				classes: 'simple-box'
+			}
+		} );
+		conversion.for( 'dataDowncast' ).elementToElement( {
+			model: 'simpleBox',
+			view: {
+				name: 'section',
+				classes: 'simple-box'
+			}
+		} );
+		conversion.for( 'editingDowncast' ).elementToElement( {
+			model: 'simpleBox',
+			view: ( modelElement, { writer: viewWriter } ) => {
+				const section = viewWriter.createContainerElement( 'section', { class: 'simple-box' } );
+
+				return toWidget( section, viewWriter, { label: 'simple box widget' } );
+			}
+		} );
+
+		// <simpleBoxTitle> converters
+		conversion.for( 'upcast' ).elementToElement( {
+			model: 'simpleBoxTitle',
+			view: {
+				name: 'h1',
+				classes: 'simple-box-title'
+			}
+		} );
+		conversion.for( 'dataDowncast' ).elementToElement( {
+			model: 'simpleBoxTitle',
+			view: {
+				name: 'h1',
+				classes: 'simple-box-title'
+			}
+		} );
+		conversion.for( 'editingDowncast' ).elementToElement( {
+			model: 'simpleBoxTitle',
+			view: ( modelElement, { writer: viewWriter } ) => {
+				// Note: You use a more specialized createEditableElement() method here.
+				const h1 = viewWriter.createEditableElement( 'h1', { class: 'simple-box-title' } );
+
+				return toWidgetEditable( h1, viewWriter );
+			}
+		} );
+
+		// <simpleBoxDescription> converters
+		conversion.for( 'upcast' ).elementToElement( {
+			model: 'simpleBoxDescription',
+			view: {
+				name: 'div',
+				classes: 'simple-box-description'
+			}
+		} );
+		conversion.for( 'dataDowncast' ).elementToElement( {
+			model: 'simpleBoxDescription',
+			view: {
+				name: 'div',
+				classes: 'simple-box-description'
+			}
+		} );
+		conversion.for( 'editingDowncast' ).elementToElement( {
+			model: 'simpleBoxDescription',
+			view: ( modelElement, { writer: viewWriter } ) => {
+				// Note: You use a more specialized createEditableElement() method here.
+				const div = viewWriter.createEditableElement( 'div', { class: 'simple-box-description' } );
+
+				return toWidgetEditable( div, viewWriter );
+			}
+		} );
     }
+}
+
+function createSimpleBox( writer ) {
+	const simpleBox = writer.createElement( 'simpleBox' );
+	const simpleBoxTitle = writer.createElement( 'simpleBoxTitle' );
+	const simpleBoxDescription = writer.createElement( 'simpleBoxDescription' );
+
+	writer.append( simpleBoxTitle, simpleBox );
+	writer.append( simpleBoxDescription, simpleBox );
+
+	// There must be at least one paragraph for the description to be editable.
+	// See https://github.com/ckeditor/ckeditor5/issues/1464.
+	writer.appendElement( 'paragraph', simpleBoxDescription );
+
+	return simpleBox;
 }
 
 
