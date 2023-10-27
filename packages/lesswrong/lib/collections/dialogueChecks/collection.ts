c8bf2654c874b0c8e77ec6383fc4311b6108c257
lagerros <jacobklagerros@gmail.com>
2023-10-27 13:52:34 -0700

    add collection

---
+++ packages/lesswrong/lib/collections/dialogueChecks/collection.ts
@@ -0,0 +1,63 @@
+import {ensureIndex} from "../../collectionIndexUtils";
+import {addUniversalFields, getDefaultMutations, getDefaultResolvers, schemaDefaultValue} from "../../collectionUtils";
+import {MutationOptions} from "../../vulcan-core/default_mutations";
+import {createCollection} from "../../vulcan-lib";
+import {userOwns} from "../../vulcan-users/permissions";
+//import schema from "./schema";
+
+const schema: SchemaType<DbDialogueCheck> = {
+  // permissions enforced via collection-level checkAccess
+  userId: {
+    type: String,
+    nullable: false,
+    canRead: ['members'], 
+    canCreate: ['members'],
+  },
+  targetUserId: {
+    type: String,
+    nullable: false,
+    canRead: ['members'],
+    canCreate: ['members'],
+  },
+  Check: {
+    type: Boolean,
+    nullable: false,
+    ...schemaDefaultValue(false),
+    canRead: ['members'],
+    canCreate: ['members'],
+    canUpdate: [userOwns],
+  },
+  CheckedAt: {
+    type: Date,
+    nullable: false,
+    canRead: ['members'],
+    canCreate: ['members'],
+    canUpdate: [userOwns],
+  },
+}
+
+
+export const DialogueChecks: DialogueChecksCollection = createCollection({
+  collectionName: 'DialogueChecks',
+  typeName: 'DialogueCheck',
+  collectionType: 'pg',
+  schema,
+  resolvers: getDefaultResolvers('DialogueChecks'),
+  logChanges: true,
+})
+
+
+// TODO!!!: 
+DialogueChecks.checkAccess = async (user: DbUser|null, document: DbDialogueCheck, context: ResolverContext|null): Promise<boolean> => {
+  // You can access only if:
+  // * you have a document with your userId and the targetUserId, and where Check is true 
+
+  const gossippee = document.userId 
+  // check if there is an existing document where gossipper is user, gossippee is targetUser, and Check is true
+
+  return true // (vote.userId===currentUser._id || userIsAdminOrMod(currentUser));
+};
+
+addUniversalFields({ collection: DialogueChecks })
+
+export default DialogueChecks;
---
+++ packages/lesswrong/lib/collections/dialogueChecks/fragments.ts
@@ -0,0 +1,11 @@
+import { registerFragment } from '../../vulcan-lib';
+
+registerFragment(`
+  fragment DialogueCheckInfo on DialogueCheck {
+    _id
+    userId
+    targetUserId
+    checked
+    checkedAt
+  }
+`);
