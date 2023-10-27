90bd5c8213a9fa2896a3179c27af6e47cdf15615
Michael Keenan <michael.keenan@gmail.com>
2023-10-26 17:38:41 -0700

    Filter out validation error headings less hackily

--- packages/lesswrong/components/vulcan-forms/FormErrors.tsx
+++ packages/lesswrong/components/vulcan-forms/FormErrors.tsx
@@ -8,16 +8,20 @@ const styles = (theme: ThemeType): JssStyles => ({
   }
 })
 
-const FormErrors = ({ errors, classes }: {
+// Validation errors are returned with a kind of "heading" error with id "app.validation_error". It
+// can be used for a message like "Please fix the following errors:", but in practice, we don't want
+// that, so we filter it out by default.
+const FormErrors = ({ errors, classes, filteredErrors = ['app.validation_error'] }: {
   errors: any[]
-  classes: ClassesType
+  classes: ClassesType,
+  filteredErrors?: string[]
 }) => (
   <div className={classNames(classes.root, "form-errors")}>
     {!!errors.length && (
       <Components.Alert className="flash-message" variant="danger">
         <ul>
           {errors.map((error, index) => (
-            error.message !== 'app.validation_error' &&
+            !filteredErrors.includes(error.message) &&
               <li key={index}>
                 <Components.FormError error={error} errorContext="form" />
               </li>
