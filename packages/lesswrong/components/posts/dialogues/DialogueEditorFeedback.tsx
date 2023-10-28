97d31a0df52c2403146754c3909ced1a82b785b1
Rafe Kennedy <dear.rafe@gmail.com>
2023-10-27 17:47:50 -0700

    Review comments

--- packages/lesswrong/components/posts/dialogues/DialogueEditorFeedback.tsx
+++ packages/lesswrong/components/posts/dialogues/DialogueEditorFeedback.tsx
@@ -20,37 +20,39 @@ const styles = (theme: ThemeType): JssStyles => ({
   }
 });
 
-type clickState = 'unclicked' | 'clickedAndItWorked' | 'clickedAndItDidntWork'
+type ClickState = 'unclicked' | 'success' | 'failure'
 
-const feedbackButtonClicked = async (user: UsersCurrent | null, post: PostsEdit, setBeenClicked: Dispatch<SetStateAction<clickState>>) => {
-  const data = { title: post.title,
-    id: post._id,
-    url: getSiteUrl() + "posts/" + post._id,
-    coauthors: post.coauthors.map(c => c.displayName).join(', '),
-    author: post.user?.displayName,
-    requester: user?.displayName
-  }
-  const response = await fetch('https://hooks.slack.com/triggers/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', {
-    method: 'POST',
-    body: JSON.stringify(data),
-  })
-  if (response.status === 200) setBeenClicked('clickedAndItWorked')
-  else setBeenClicked('clickedAndItDidntWork')
-
-}
 
 export const DialogueEditorFeedback = ({ classes, post }: {
   classes: ClassesType,
   post: PostsEdit
 }) => {
-  const [beenClicked, setBeenClicked] = useState<clickState>('unclicked');
+  const [clickState, setClickState] = useState<ClickState>('unclicked');
   const user = useCurrentUser()
+
+  const feedbackButtonClicked = async () => {
+    const data = { title: post.title,
+      id: post._id,
+      url: getSiteUrl() + "posts/" + post._id,
+      coauthors: post.coauthors.map(c => c.displayName).join(', '),
+      author: post.user?.displayName,
+      requester: user?.displayName
+    }
+    const response = await fetch('https://hooks.slack.com/triggers/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', {
+      method: 'POST',
+      body: JSON.stringify(data),
+    })
+    if (response.status === 200) setClickState('success')
+    else setClickState('failure')
+  
+  }
+
   return <div className={classes.root}>
-    <div className={classes.feedbackRow}>{beenClicked === 'unclicked'
-      ? <Button className={classes.button} onClick={async _ => { await feedbackButtonClicked(user, post, setBeenClicked) }}>
+    <div className={classes.feedbackRow}>{clickState === 'unclicked'
+      ? <Button className={classes.button} onClick={async _ => { await feedbackButtonClicked() }}>
           Get feedback or editing help from the LessWrong team.
         </Button>
-      : beenClicked === 'clickedAndItWorked'
+      : clickState === 'success'
         ? <div>Feedback requested!</div>
         : <div>That didn't work! Refresh and try again?</div>}
     </div>
