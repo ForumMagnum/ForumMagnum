571f9dfe8b60cfe3bd434b83a9c1575dae76071c
Michael Keenan <michael.keenan@gmail.com>
2023-11-06 09:36:25 -0800

    Don't restrict viewing posts by unreviewed users

--- packages/lesswrong/lib/collections/posts/permissions.ts
+++ packages/lesswrong/lib/collections/posts/permissions.ts
@@ -53,11 +53,6 @@ Posts.checkAccess = async (currentUser: DbUser|null, post: DbPost, context: Reso
     return true;
   } else if (post.isFuture || post.draft || post.deletedDraft) {
     return false;
-    // TODO: consider getting rid of this clause entirely and instead just relying on default view filter, 
-    // since LW is now allowing people to see rejected content and preventing them from seeing 'not-yet-rejected
-    // content is kinda weird)
-  } else if (post.authorIsUnreviewed && !post.rejected) {
-    return false
   } else {
     const status = _.findWhere(postStatusLabels, {value: post.status});
     if (!status) return false;
