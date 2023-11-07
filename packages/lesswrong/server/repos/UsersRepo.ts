79d4c7bfa531ff6b4d1c20709d129da830114f29
Ricki Heicklen <ricki.heicklen@gmail.com>
2023-11-06 17:23:10 -0800

    changing false to true

--- packages/lesswrong/server/repos/UsersRepo.ts
+++ packages/lesswrong/server/repos/UsersRepo.ts
@@ -306,7 +306,8 @@ export default class UsersRepo extends AbstractRepo<DbUser> {
             v."userId" = $1
             AND u._id != $1
             AND v."votedAt" > NOW() - INTERVAL '1.5 years'
-    
+            AND v."cancelled" IS NOT TRUE
+
         UNION ALL
     
         -- Joining Users with Comments and Votes
@@ -330,6 +331,7 @@ export default class UsersRepo extends AbstractRepo<DbUser> {
             v."userId" = $1
             AND u._id != $1
             AND v."votedAt" > NOW() - INTERVAL '1.5 years'
+            AND v."cancelled" IS NOT TRUE
     )
   
     SELECT
