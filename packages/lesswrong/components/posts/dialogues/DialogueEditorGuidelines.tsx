40c95415349c705b80a7d8addccec5dd1b69e20c
ben-pace <benitopace@gmail.com>
2023-11-08 12:48:17 -0800

    Further mockup edits

--- packages/lesswrong/components/posts/dialogues/DialogueEditorGuidelines.tsx
+++ packages/lesswrong/components/posts/dialogues/DialogueEditorGuidelines.tsx
@@ -17,7 +17,7 @@ const styles = (theme: ThemeType): JssStyles => ({
   info: {
     color: theme.palette.grey[600],
     ...commentBodyStyles(theme),
-    margin: '0 !important',
+    margin: '16px !important',
     paddingBottom: 8
   },
   checkbox: {
@@ -31,14 +31,19 @@ export const DialogueEditorGuidelines = ({classes}: {
   return <div className={classes.root}>
     <div>Dialogue Setup</div>
     <p className={classes.info}>
-      The LW Team’s current recommended way to dialogue is to use When2Meet to find a 2-3 hour chunk of time where you can both show up and dialogue at the same time.
+      The LW Team’s current recommended way to dialogue is to use When2Meet to find a 2-3 hour chunk of time where you can both show up and dialogue at the same time.</p> 
+    <p className={classes.info}>Please click on the checkbox below once you've filled out your availability! You'll get a notification when other dialogue participants fill out theirs.
     </p>
     <iframe 
-  src="https://www.when2meet.com/?22298232-WhYeO" 
+  src="https://www.when2meet.com/?22298384-tLmQw" 
   width="150%" 
   height="600px" 
   style={{border: "none", marginLeft: "-25%", marginRight: "-25%"}}
 />
+<FormControlLabel
+      control={<Checkbox name="availability" />}
+      label="I have filled out my availability."
+    />
   </div>;
 }
 
