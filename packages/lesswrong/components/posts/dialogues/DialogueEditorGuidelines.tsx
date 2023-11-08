edd28ccc7c26bc23aa06edb35272da42cdc49ffc
ben-pace <benitopace@gmail.com>
2023-11-08 12:35:21 -0800

    Mocking up dialogues page

--- packages/lesswrong/components/posts/dialogues/DialogueEditorGuidelines.tsx
+++ packages/lesswrong/components/posts/dialogues/DialogueEditorGuidelines.tsx
@@ -2,6 +2,8 @@
 import React from 'react';
 import { registerComponent } from '../../../lib/vulcan-lib';
 import { commentBodyStyles } from '../../../themes/stylePiping';
+import Checkbox from '@material-ui/core/Checkbox';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
 
 const styles = (theme: ThemeType): JssStyles => ({
   root: {
@@ -17,19 +19,26 @@ const styles = (theme: ThemeType): JssStyles => ({
     ...commentBodyStyles(theme),
     margin: '0 !important',
     paddingBottom: 8
-  }
+  },
+  checkbox: {
+    marginBottom: 0, // Adjust this value to reduce space between checkboxes
+  },
 });
 
 export const DialogueEditorGuidelines = ({classes}: {
   classes: ClassesType,
 }) => {
   return <div className={classes.root}>
-    <div>Dialogue Editor</div>
-    <ul className={classes.info}>
-      <li>You can edit your responses afterwards.</li>
-      <li>You can see each other's responses as you type them.</li>
-      <li>Default etiquette: It's okay to start drafting your message before the other person finishes theirs.</li>
-    </ul>
+    <div>Dialogue Setup</div>
+    <p className={classes.info}>
+      The LW Team’s current recommended way to dialogue is to use When2Meet to find a 2-3 hour chunk of time where you can both show up and dialogue at the same time.
+    </p>
+    <iframe 
+  src="https://www.when2meet.com/?22298232-WhYeO" 
+  width="150%" 
+  height="600px" 
+  style={{border: "none", marginLeft: "-25%", marginRight: "-25%"}}
+/>
   </div>;
 }
 
