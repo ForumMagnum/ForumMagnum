35d1270a5dab13e7e730f8e7ef9a75cc11279d8d
lagerros <jacobklagerros@gmail.com>
2023-11-13 15:32:44 -0800

    Reformat auto-message generation

--- packages/lesswrong/server/callbacks/dialogueMatchPreferenceCallbacks.ts
+++ packages/lesswrong/server/callbacks/dialogueMatchPreferenceCallbacks.ts
@@ -4,6 +4,7 @@ import { createMutator, updateMutator } from "../vulcan-lib";
 
 interface MatchPreferenceFormData extends DbDialogueMatchPreference {
   displayName: string;
+  userId: string
 }
 
 function getParagraphWithText(text: string) {
@@ -13,31 +14,85 @@ function getParagraphWithText(text: string) {
 
 type TopicMatch = "match" | "noMatch" | "uncertain";
 
+function convertTimestamp(timestamp: number) {
+  const date = new Date(timestamp);
+  return date.toUTCString();
+}
+
+const getDialogueMessageHTML = (userId:string, displayName:string, order:string, content:string ) => {
+
+  const time = convertTimestamp(Date.now())
+  const message_id = `${userId}-${time}`
+
+  const html = 
+    `<section class="dialogue-message ContentStyles-debateResponseBody" message-id="${message_id}" user-id="${userId}" display-name="${displayName}" submitted-date="${time}" user-order="${order}">
+      <section class="dialogue-message-header CommentUserName-author UsersNameDisplay-noColor"></section>
+      <div>
+        ${content}
+      </div>
+    </section>`
+
+  return html
+}
+
+const getFormAsHtml = (formData: MatchPreferenceFormData) => {
+  const html = `
+    <p>${formData.topicNotes}</p>
+  `
+  return html
+}
+
+const helperBotDisplayName = "Dialogue Helper Bot"
+const helperBotId = "edmzLyzymdoSuXnym"
+
 const welcomeMessage = (formDataSourceUser: MatchPreferenceFormData, formDataTargetUser: MatchPreferenceFormData) => {
   const userName = formDataSourceUser.displayName;
+  const userId = formDataSourceUser.userId;
   const targetUserName = formDataTargetUser.displayName;
+  const targetUserId = formDataTargetUser.userId;
+  
+
+  const introMessageContent = `
+    <p>Hey ${userName} and ${targetUserName}, you were potentially interested in a dialogue!</p>
+    <p>Here are your form replies on format:</p>
+    <ul>
+      <li><i>Sync:</i> find a 1-3h time to sit down and dialogue</li>
+      <li><i>Async: </i>like a letter exchange over time. Suggested effort: at least 2 longer replies each before publishing</li>
+    </ul>
+    <figure class="table">
+      <table>
+        <tbody>
+          <tr>
+            <td>&nbsp;
+            </td>
+            <td><strong>Sync</strong></td>
+            <td><strong>Async</strong></td>
+            <td>Notes</td>
+          </tr>
+          <tr>
+            <td><i>${userName}</i></td>
+            <td>${formDataSourceUser.syncPreference}</td>
+            <td>${formDataSourceUser.asyncPreference}</td>
+            <td>${formDataSourceUser.formatNotes}</td>
+          </tr>
+          <tr>
+            <td><i>${targetUserName}</i></td>
+            <td>${formDataTargetUser.syncPreference}</td>
+            <td>${formDataTargetUser.asyncPreference}</td>
+            <td>${formDataTargetUser.formatNotes}</td>
+          </tr>
+        </tbody>
+      </table>
+    </figure>
+    <p>And here's what you said on topics:</p>
+  `;
 
   const isYesOrMeh = (value: string) => ["Yes", "Meh"].includes(value);
-
-  let matchLineDangerous
-  let formatMessage
-  let nextAction
-
-  matchLineDangerous = `Hey ${userName} and ${targetUserName}, you were potentially interested in a dialogue... let's see if there's something here.`;
 
   const formatPreferenceMatch = 
     (isYesOrMeh(formDataSourceUser.syncPreference) && isYesOrMeh(formDataTargetUser.syncPreference)) ||
     (isYesOrMeh(formDataSourceUser.asyncPreference) && isYesOrMeh(formDataTargetUser.asyncPreference));
 
-  formatMessage = `
-    <p><strong>Format</strong></p>
-    <ul>
-      <li>Sync: find a 1-3h time to sit down and dialogue</li>
-      <li>Async: more like a letter exchange over time. Suggested effort: at least 2 longer replies each before publishing</li>
-    </ul>
-  `
-  const userFormatNotesDangerous = `"${userName}: sync ${formDataSourceUser.syncPreference}, async ${formDataSourceUser.asyncPreference}. ` + formDataSourceUser.formatNotes + `"`
-  const targetUserFormatNotesDangerous = `"${targetUserName}: sync ${formDataSourceUser.syncPreference}, async ${formDataSourceUser.asyncPreference}. ` + formDataTargetUser.formatNotes + `"`
 
   let topicMatch: TopicMatch = "uncertain" as TopicMatch; // Haven't build the other functionality for now. TODO! 
   let topicMessage: string;
@@ -54,54 +109,32 @@ const welcomeMessage = (formDataSourceUser: MatchPreferenceFormData, formDataTar
       break;
   }
 
-  const userTopicNotesDangerous = `${userName}: "` + formDataSourceUser.topicNotes + `"`
-  const targetUserTopicNotesDangerous = `${targetUserName}: "` + formDataTargetUser.topicNotes + `"`
+  const userContent = getFormAsHtml(formDataSourceUser)
+  const targetUserContent = getFormAsHtml(formDataTargetUser)
 
-  // default
-  nextAction = `<p><strong>Suggestions</strong></p><p>Our auto-checker couldn't tell if you were compatible or not. Feel free to chat to figure it out. And if it doesn't work it's totally okay to just call this a "good try" and then move on :)</p>`
+  let nextAction = `<p>My auto-checker couldn't tell if you were compatible or not. Feel free to chat to figure it out. And if it doesn't work it's totally okay to just call this a "good try" and then move on :)</p>`
 
   if (formatPreferenceMatch && topicMatch === "uncertain") {
     nextAction = `
-      <p><strong>Suggestions</strong></p><p> Your preferences overlapped on format, but our auto-checker couldn't tell if you had topics in common. Feel free to chat to figure it out. And if it doesn't work it's totally okay to just call this a "good try" and then move on :)</p>
+      <p>It seems your preferences overlapped on format, but our auto-checker couldn't tell if you had topics in common. Feel free to chat to figure it out. And if it doesn't work it's totally okay to just call this a "good try" and then move on :)</p>
     `
   } 
   if (!formatPreferenceMatch && topicMatch === "uncertain") {
     nextAction = `
-      <p><strong>Suggestions</strong></p><p> It seems you have different format preferences. So a dialogue might not be the right solution here. That's okay! It's fine to call this a "nice try" and just move on :)
+      <p>It seems you have different format preferences. So a dialogue might not be the right solution here.</p> 
+      <p>That's okay! It's fine to call this a "nice try" and just move on :)
       (We still created this chat for you in case that's not right and you wanted to discuss a bit more)</p>
     `
   } 
 
-  // TODO: handle different cases
-  // if (!topicMatch && formatPreferenceMatch) {
-  //   nextAction = `
-  //     It seems you didn't find a topic, but do overlap on format. Feel free to come up with some more topic ideas! If you can't find any, that's okay! It's fine to call this a "nice try" and just move on :) 
-  //   `
-  // } 
-  // if (topicsInCommon && formatPreferenceMatch) {
-  //   nextAction = `
-  //     It seems you've got overlap on both topic and format! :) 
-  //   `
-  // }
-  // if (topicsInCommon && !formatPreferenceMatch) {
-  //   nextAction = `
-  //     It seems you've got topics in common, but have different preferences on format. So a dialogue might not be the right solution here. That's okay! We still made this chat if you wanna hash it out more :) 
-  //   `
-  // }
+  const introMessage = getDialogueMessageHTML(helperBotId, helperBotDisplayName, "1", introMessageContent);
+  const userMessage = getDialogueMessageHTML(userId, userName, "2", userContent);
+  const targetUserMessage = getDialogueMessageHTML(targetUserId, targetUserName, "3", targetUserContent);
+  const nextActionMessage = getDialogueMessageHTML(helperBotId, helperBotDisplayName, "1", nextAction); 
 
-  // We have a bunch of string concatenation going on
-  // So we set each bit as the text content of its own paragraph
-  const $ = cheerioParse("<div></div>");
+  const message = `<div>${introMessage}${userMessage}${targetUserMessage}${nextActionMessage}</div>`
 
-  $('div').append( getParagraphWithText(matchLineDangerous) )
-  $('div').append( formatMessage )
-  $('div').append( getParagraphWithText(userFormatNotesDangerous) )
-  $('div').append( getParagraphWithText(targetUserFormatNotesDangerous) )
-  $('div').append( topicMessage )
-  $('div').append( getParagraphWithText(userTopicNotesDangerous) )
-  $('div').append( getParagraphWithText(targetUserTopicNotesDangerous) )
-  $('div').append( nextAction )
-  return $('div').html();
+  return message
 }
 
 getCollectionHooks("DialogueMatchPreferences").createBefore.add(async function GenerateDialogue ( userMatchPreferences, { context, currentUser } ) {
@@ -132,13 +165,14 @@ getCollectionHooks("DialogueMatchPreferences").createBefore.add(async function G
 
   const formDataUser1 = {
     ...userMatchPreferences,
+    userId: userId, 
     displayName: currentUser.displayName,
   }
   const formDataUser2 = {
     ...reciprocalMatchPreferences,
     displayName: targetUser.displayName,
+    userId: targetUserId,
   }
-  
  
   const result = await createMutator({
     collection: context.Posts,
@@ -147,7 +181,7 @@ getCollectionHooks("DialogueMatchPreferences").createBefore.add(async function G
       title,
       draft: true,
       collabEditorDialogue: true,
-      coauthorStatuses:[{userId: targetUserId, confirmed: true, requested: false}],
+      coauthorStatuses:[{userId: targetUserId, confirmed: true, requested: false}, {userId: helperBotId, confirmed: true, requested: false}],
       shareWithUsers: [targetUserId],
       sharingSettings: {
         anyoneWithLinkCan: "none",
@@ -156,7 +190,7 @@ getCollectionHooks("DialogueMatchPreferences").createBefore.add(async function G
       contents: {
         originalContents: {
           type: "ckEditorMarkup",
-          data: welcomeMessage( formDataUser1, formDataUser2 )
+          data: welcomeMessage(formDataUser1, formDataUser2)
         }
       } as AnyBecauseHard
     },
