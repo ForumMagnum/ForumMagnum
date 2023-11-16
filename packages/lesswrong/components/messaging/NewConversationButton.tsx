d7c01bd10648163d09d01de1ccdaea64e51e8f98
Will Howard <w.howard256+github@gmail.com>
2023-11-16 17:54:46 +0000

    fix type

--- packages/lesswrong/components/messaging/NewConversationButton.tsx
+++ packages/lesswrong/components/messaging/NewConversationButton.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, useCallback, useEffect } from 'react';
+import React, { MouseEvent, ReactNode, useCallback, useEffect } from 'react';
 import { registerComponent } from '../../lib/vulcan-lib';
 import qs from 'qs';
 import { useDialog } from '../common/withDialog';
@@ -51,7 +51,7 @@ const NewConversationButton = ({ user, currentUser, children, from, includeModer
   }, [conversation, embedConversation, getTemplateParams, navigate, templateQueries])
 
   const handleClick = currentUser
-    ? (e) => {
+    ? (e: MouseEvent) => {
       initiateConversation(user._id)
       e.stopPropagation()
     }
