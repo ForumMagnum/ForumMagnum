013b906b957fdd89bda0165480af6bf08f0639d0
Sarah Cheng <sarah.cheng@centreforeffectivealtruism.org>
2024-01-15 12:34:39 -0500

    [EA Forum only] remove the homepage RHS Wrapped ad

--- packages/lesswrong/components/ea-forum/EAHomeRightHandSide.tsx
+++ packages/lesswrong/components/ea-forum/EAHomeRightHandSide.tsx
@@ -66,32 +66,6 @@ const styles = (theme: ThemeType) => ({
   podcastsSection: {
     rowGap: '6px',
   },
-  wrappedAd: {
-    backgroundColor: theme.palette.wrapped.background,
-    color: theme.palette.text.alwaysWhite,
-    padding: '12px 24px',
-    borderRadius: theme.borderRadius.default,
-    transition: `box-shadow .3s`,
-    '&:hover': {
-      opacity: 1,
-      boxShadow: `0 0 11px ${theme.palette.icon.dim5}`
-    }
-  },
-  wrappedAdHeadingRow: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    columnGap: 40,
-  },
-  wrappedAdHeading: {
-    fontWeight: 600,
-    fontSize: 16,
-    lineHeight: '22px',
-    margin: 0
-  },
-  wrappedAdImg: {
-    width: 50
-  },
   digestAd: {
     maxWidth: 280,
     marginBottom: 32,
@@ -175,27 +149,6 @@ const styles = (theme: ThemeType) => ({
     fontSize: 13,
   },
 });
-
-const WrappedAd = ({classes}: {
-  classes: ClassesType<typeof styles>,
-}) => {
-  const currentUser = useCurrentUser()
-  
-  if (!currentUser) return null
-  
-  const { CloudinaryImage2 } = Components
-  
-  return <AnalyticsContext pageSubSectionContext="wrappedAd">
-    <div className={classes.section}>
-      <Link to="/wrapped" className={classes.wrappedAd}>
-        <div className={classes.wrappedAdHeadingRow}>
-          <h2 className={classes.wrappedAdHeading}>Your 2023 Wrapped</h2>
-          <CloudinaryImage2 publicId="2023_wrapped" imgProps={{w: '100'}} className={classes.wrappedAdImg} />
-        </div>
-      </Link>
-    </div>
-  </AnalyticsContext>
-}
 
 
 /**
@@ -357,8 +310,6 @@ export const EAHomeRightHandSide = ({classes}: {
   return <AnalyticsContext pageSectionContext="homeRhs">
     {!!currentUser && sidebarToggleNode}
     <div className={classes.root}>
-      <WrappedAd classes={classes} />
-
       {digestAdNode}
       
       <AnalyticsContext pageSubSectionContext="resources">
