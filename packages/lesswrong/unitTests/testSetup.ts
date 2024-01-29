d9d0c9515b12ff673ecf776b876f8023d857451f
Rafe Kennedy <dear.rafe@gmail.com>
2024-01-29 13:18:35 -0800

    load settings before unit tests

--- packages/lesswrong/unitTests/testSetup.ts
+++ packages/lesswrong/unitTests/testSetup.ts
@@ -4,12 +4,14 @@ import { AbortSignal } from "node-abort-controller";
 import { setPublicSettings } from '../lib/settingsCache';
 // See https://github.com/openai/openai-node#customizing-the-fetch-client
 import "openai/shims/node";
+import { refreshSettingsCaches } from '../server/loadDatabaseSettings';
 
 // Fix for Reference error AbortSignal in `lru-cache`
 // See https://github.com/isaacs/node-lru-cache/issues/239
 global.AbortSignal = AbortSignal as AnyBecauseHard;
 
 setPublicSettings({});
+void refreshSettingsCaches();
 
 jest.setTimeout(20000);
 
