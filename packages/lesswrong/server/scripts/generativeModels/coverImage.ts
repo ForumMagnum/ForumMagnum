f0482af5bcdf35cb2e5b0b69722d1f4f647a0843
Ricki Heicklen <ricki.heicklen@gmail.com>
2024-01-31 16:20:46 -0800

    changes to coverImage

--- packages/lesswrong/server/scripts/generativeModels/coverImage.ts
+++ packages/lesswrong/server/scripts/generativeModels/coverImage.ts
@@ -1,6 +1,9 @@
 import { imagine_key, openai_key, openai_org, slack_api_token } from './keys.ts'
 import OpenAI from 'openai'
 import axios from 'axios';
+import fs from 'fs';
+import path from 'path'
+import { readFile, writeFile } from 'fs/promises'
 import { Globals } from '../../vulcan-lib/index.ts';
 import Posts from '../../../lib/collections/posts/collection.ts';
 
@@ -35,7 +38,7 @@ const openai = new OpenAI({
   organization: openai_org
 })
 
-const llm_prompt = (essay: string) => `Please generate 5 phrases, each 1 to 7 words long. They should describe simple concrete objects and  will go in this instruction for an image prompt: "Aquarelle book cover inspired by topographic river maps and mathematical diagrams and equations. [BLANK]. Fade to white"
+const llm_prompt = (essay: string) => `Please generate 2 phrases, each 1 to 7 words long. They should describe simple concrete objects and  will go in this instruction for an image prompt: "Aquarelle book cover inspired by topographic river maps and mathematical diagrams and equations. [BLANK]. Fade to white"
 
 Some examples for other essays would be:
 * "Coins and shaking hands"
@@ -83,11 +86,61 @@ const getElements = async (essay: {title: string, content: string}): Promise<str
 
 const prompter = (el: string) => `https://s.mj.run/Bkrf46MPWyo  Aquarelle illustration inspired by topographic river maps and mathematical diagrams and equations. A clear image of ${el} --no text --ar 8:5 --iw 2.0 --s 150 --chaos 10 --v 6.0`
 
-const postPromptImages = async (prompt: string, {threadTs}: {title: string, threadTs?: string}, images: string[]) => {
+async function downloadImage(url : string, filePath : string) : Promise<void> {
+  const response = await axios({
+    url: url,
+    method: 'GET',
+    responseType: 'stream',
+  });
+
+  console.log('Download image response data: ', response.data)
+  response.data.pipe(fs.createWriteStream(filePath));
+
+  return new Promise((resolve, reject) => {
+    response.data.on('end', () => {
+      resolve();
+    });
+
+    response.data.on('error', (err : Error) => {
+      reject(err);
+    });
+  });
+}
+
+async function uploadImageToSlack(filePath : string, threadTs : string) {
+  const formData = {
+    file: fs.readFileSync(filePath),
+    channels: channelId,
+    thread_ts: threadTs
+  };
+  const response = await axios.post('https://slack.com/api/files.upload', formData, {
+    headers: {
+      'Authorization': `Bearer ${slackToken}`,
+      'Content-Type': 'multipart/form-data',
+    },
+    // maxContentLength: Infinity,
+    // maxBodyLength: Infinity
+  });
+  console.log('Upload image response data: ', response.data)
+  console.log('Upload image response statusText: ', response.statusText)
+  return response.data;
+}
+
+const postPromptImages = async (prompt: string, {threadTs}: {title: string, threadTs?: string}, url: string, images: string[], aspect_ratio?: {width : number, height : number}) => {
   if (!threadTs) return
   await postReply(`*Prompt: ${prompt}*`, threadTs);
   return await Promise.all(images.map(async (image) => {
-    await postReply(image, threadTs);
+
+    const imageUrl = image;
+    const filePath = './image.png'; // ${image}
+    await downloadImage(imageUrl, filePath);
+
+    const filePathDummy = 'lw_big.png'
+    // const fullFilePath = path.join(__dirname, filePathDummy);
+ 
+    // console.log("Full file path: ", fullFilePath)
+    await uploadImageToSlack(filePathDummy, threadTs)
+    // await postReply(url, threadTs);
   }))
 }
 
@@ -131,7 +184,7 @@ async function go(essays: Essay[], essayIx: number, el: string) {
         // stop repeating
         clearInterval(intervalId);
         console.log('Completed image details', responseData.data);
-        await postPromptImages(el, essays[essayIx], responseData.data.upscaled_urls)
+        await postPromptImages(el, essays[essayIx], responseData.data.url, responseData.data.upscaled_urls)
         releaseMidjourneyRights()
         console.log('Completed image details', responseData.data);
       } else {
@@ -155,11 +208,13 @@ const headers = {
 };
 
 // Function to post a message
-async function postMessage(text: string, threadTs?: string) {
+async function postMessage(text: string, threadTs?: string, blocks?: any) {
   const data = {
     channel: channelId,
     text: text,
-    ...(threadTs && { thread_ts: threadTs }) // Add thread_ts if provided
+    unfurl_media: true,
+    ...(threadTs && { thread_ts: threadTs }), // Add thread_ts if provided
+    ...(blocks && { blocks: blocks})
   };
 
   try {
@@ -187,12 +242,12 @@ async function makeEssayThread(essay: {title: string, content: string, threadTs?
 }
 
 // Post a reply to the thread
-async function postReply(text: string, threadTs: string) {
-  await postMessage(text, threadTs);
+async function postReply(text: string, threadTs: string, blocks? : any) {
+  await postMessage(text, threadTs, blocks);
 }
 
 async function main () {
-  const limit = 3
+  const limit = 1
   const essays = (await getEssays()).slice(0, limit)
   await Promise.all(essays.map(makeEssayThread))
   const elementss = await Promise.all(essays.slice(0, limit).map(getElements))
@@ -203,3 +258,28 @@ async function main () {
 }
 
 Globals.coverImages = () => main()
+
+
+
+    // const getDimFromAR = (aspect_ratio : {width : number, height : number}) : [number, number] => {
+    //   const SIZE_LIMIT = 1000000
+    //   const multiplier = Math.sqrt(SIZE_LIMIT/(aspect_ratio.width * aspect_ratio.height))
+    //   return [aspect_ratio.width * multiplier, aspect_ratio.height * multiplier]
+    // }
+    // const [width, height] = aspect_ratio ? getDimFromAR(aspect_ratio) : [1200, 750] // this is an aspect ratio of 8:5
+
+      // // Construct the blocks using Block Kit JSON structure
+      // const blocks = [
+      //   {
+      //     "type": "image",
+      //     "title": {
+      //       "type": "plain_text",
+      //       "text": "This is your image"
+      //     },
+      //     "block_id": "image4",
+      //     "image_url": image,
+      //     "alt_text": "Alt text whatever.",
+      //     "image_width": width,
+      //     "image_height": height
+      //   }
+      // ];
