b172aa06856b6250804fa3d11512006a5be0ba40
Will Howard <w.howard256+github@gmail.com>
2024-02-02 14:34:23 +0000

    tabs to spaces

--- public/lesswrong-editor/src/ckeditor5-cta-button/cta-button-form.js
+++ public/lesswrong-editor/src/ckeditor5-cta-button/cta-button-form.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-tabs */
 import Plugin from "@ckeditor/ckeditor5-core/src/plugin";
 import ClickObserver from "@ckeditor/ckeditor5-engine/src/view/observer/clickobserver";
 import ContextualBalloon from "@ckeditor/ckeditor5-ui/src/panel/balloon/contextualballoon";
@@ -17,343 +16,343 @@ import submitHandler from "@ckeditor/ckeditor5-ui/src/bindings/submithandler";
 import './ctaform.css';
 
 class MainFormView extends View {
-	constructor(locale, editor) {
-		super(locale);
+  constructor(locale, editor) {
+    super(locale);
 
-		this.editor = editor;
-		this._createKeyAndFocusTrackers();
+    this.editor = editor;
+    this._createKeyAndFocusTrackers();
 
-		// selectedElement will be set to the button that is clicked on (there can be multiple in the same editor)
-		this.selectedElement = null;
+    // selectedElement will be set to the button that is clicked on (there can be multiple in the same editor)
+    this.selectedElement = null;
 
-		// Equation input
-		const { buttonTextView, linkToView } = this._createInputs();
-		this.buttonTextView = buttonTextView;
-		this.linkToView = linkToView;
+    // Equation input
+    const { buttonTextView, linkToView } = this._createInputs();
+    this.buttonTextView = buttonTextView;
+    this.linkToView = linkToView;
 
-		this.previewEnabled = true;
+    this.previewEnabled = true;
 
-		let children = [];
-		children = [buttonTextView, linkToView];
+    let children = [];
+    children = [buttonTextView, linkToView];
 
-		// Add UI elements to template
-		this.setTemplate({
-			tag: "form",
-			attributes: {
-				class: ["ck", "ck-cta-form"],
-				tabindex: "-1",
-				spellcheck: "false",
-			},
-			children: [
-				{
-					tag: "div",
-					children: [
-						{
-							tag: "div",
-							attributes: {
-								class: ["ck-cta-form-label"],
-							},
-							children: ["Button text"],
-						},
-						buttonTextView
-					]
-				},
-				{
-					tag: "div",
-					children: [
-						{
-							tag: "div",
-							attributes: {
-								class: ["ck-cta-form-label"],
-							},
-							children: ["Link to"],
-						},
-						linkToView
-					]
-				}
-			],
-		});
-	}
+    // Add UI elements to template
+    this.setTemplate({
+      tag: "form",
+      attributes: {
+        class: ["ck", "ck-cta-form"],
+        tabindex: "-1",
+        spellcheck: "false",
+      },
+      children: [
+        {
+          tag: "div",
+          children: [
+            {
+              tag: "div",
+              attributes: {
+                class: ["ck-cta-form-label"],
+              },
+              children: ["Button text"],
+            },
+            buttonTextView
+          ]
+        },
+        {
+          tag: "div",
+          children: [
+            {
+              tag: "div",
+              attributes: {
+                class: ["ck-cta-form-label"],
+              },
+              children: ["Link to"],
+            },
+            linkToView
+          ]
+        }
+      ],
+    });
+  }
 
-	render() {
-		super.render();
+  render() {
+    super.render();
 
-		// Prevent default form submit event & trigger custom 'submit'
-		submitHandler({
-			view: this,
-		});
+    // Prevent default form submit event & trigger custom 'submit'
+    submitHandler({
+      view: this,
+    });
 
-		// Register form elements to focusable elements
-		const childViews = [this.buttonTextView, this.linkToView];
+    // Register form elements to focusable elements
+    const childViews = [this.buttonTextView, this.linkToView];
 
-		childViews.forEach((v) => {
-			this._focusables.add(v);
-			this.focusTracker.add(v.element);
-		});
+    childViews.forEach((v) => {
+      this._focusables.add(v);
+      this.focusTracker.add(v.element);
+    });
 
-		// Listen to keypresses inside form element
-		this.keystrokes.listenTo(this.element);
-	}
+    // Listen to keypresses inside form element
+    this.keystrokes.listenTo(this.element);
+  }
 
-	focus() {
-		this._focusCycler.focusFirst();
-	}
+  focus() {
+    this._focusCycler.focusFirst();
+  }
 
-	_createKeyAndFocusTrackers() {
-		this.focusTracker = new FocusTracker();
-		this.keystrokes = new KeystrokeHandler();
-		this._focusables = new ViewCollection();
+  _createKeyAndFocusTrackers() {
+    this.focusTracker = new FocusTracker();
+    this.keystrokes = new KeystrokeHandler();
+    this._focusables = new ViewCollection();
 
-		this._focusCycler = new FocusCycler({
-			focusables: this._focusables,
-			focusTracker: this.focusTracker,
-			keystrokeHandler: this.keystrokes,
-			actions: {
-				focusPrevious: "shift + tab",
-				focusNext: "tab",
-			},
-		});
-	}
+    this._focusCycler = new FocusCycler({
+      focusables: this._focusables,
+      focusTracker: this.focusTracker,
+      keystrokeHandler: this.keystrokes,
+      actions: {
+        focusPrevious: "shift + tab",
+        focusNext: "tab",
+      },
+    });
+  }
 
-	get buttonText() {
-		return this.buttonTextView.element.value;
-	}
+  get buttonText() {
+    return this.buttonTextView.element.value;
+  }
 
-	set buttonText(value) {
-		this.buttonTextView.element.value = value;
-	}
+  set buttonText(value) {
+    this.buttonTextView.element.value = value;
+  }
 
-	get linkTo() {
-		return this.linkToView.element.value;
-	}
+  get linkTo() {
+    return this.linkToView.element.value;
+  }
 
-	set linkTo(value) {
-		this.linkToView.element.value = value;
-	}
+  set linkTo(value) {
+    this.linkToView.element.value = value;
+  }
 
-	_createInputs() {
-		// Create 'Button text' input
-		const buttonTextView = new InputTextView(this.locale);
-		const buttonTextBind = buttonTextView.bindTemplate;
-		buttonTextView.setTemplate({
-			tag: "input",
-			attributes: {
-				type: "text",
-				class: ["ck-cta-form-input", buttonTextBind.if("hasError", "ck-error")],
-				id: buttonTextBind.to("id"),
-				placeholder: buttonTextBind.to("placeholder"),
-				readonly: false,
-				"aria-invalid": buttonTextBind.if("hasError", true),
-				"aria-describedby": buttonTextBind.to("ariaDescribedById"),
-			},
-			on: {
-				input: buttonTextBind.to("input"),
-			},
-		});
-		buttonTextView.label = "Button text";
-		buttonTextView.on("input", () => {
-			const model = this.editor.model;
-			const selectedElement = this.selectedElement;
+  _createInputs() {
+    // Create 'Button text' input
+    const buttonTextView = new InputTextView(this.locale);
+    const buttonTextBind = buttonTextView.bindTemplate;
+    buttonTextView.setTemplate({
+      tag: "input",
+      attributes: {
+        type: "text",
+        class: ["ck-cta-form-input", buttonTextBind.if("hasError", "ck-error")],
+        id: buttonTextBind.to("id"),
+        placeholder: buttonTextBind.to("placeholder"),
+        readonly: false,
+        "aria-invalid": buttonTextBind.if("hasError", true),
+        "aria-describedby": buttonTextBind.to("ariaDescribedById"),
+      },
+      on: {
+        input: buttonTextBind.to("input"),
+      },
+    });
+    buttonTextView.label = "Button text";
+    buttonTextView.on("input", () => {
+      const model = this.editor.model;
+      const selectedElement = this.selectedElement;
 
-			model.change((writer) => {
-				if (!selectedElement) return;
+      model.change((writer) => {
+        if (!selectedElement) return;
 
-				const inputValue = buttonTextView.element.value.trim();
+        const inputValue = buttonTextView.element.value.trim();
 
-				// Remove the existing text nodes within the selected element
-				const range = writer.createRangeIn(selectedElement);
-				writer.remove(range);
+        // Remove the existing text nodes within the selected element
+        const range = writer.createRangeIn(selectedElement);
+        writer.remove(range);
 
-				// Insert the new text node with the input value
-				const newText = writer.createText(inputValue);
-				writer.insert(newText, writer.createPositionAt(selectedElement, 0));
-			});
-		});
+        // Insert the new text node with the input value
+        const newText = writer.createText(inputValue);
+        writer.insert(newText, writer.createPositionAt(selectedElement, 0));
+      });
+    });
 
-		// Create 'Link to' input
-		const linkToView = new InputTextView(this.locale);
-		const linkToBind = linkToView.bindTemplate;
-		linkToView.setTemplate({
-			tag: "input",
-			attributes: {
-				type: "text",
-				class: ["ck-cta-form-input", linkToBind.if("hasError", "ck-error")],
-				id: linkToBind.to("id"),
-				placeholder: 'https://example.com',
-				readonly: false,
-				"aria-invalid": linkToBind.if("hasError", true),
-				"aria-describedby": linkToBind.to("ariaDescribedById"),
-			},
-			on: {
-				input: linkToBind.to("input"),
-			},
-		});
-		linkToView.label = "Link to";
-		linkToView.on("input", () => {
-			const model = this.editor.model;
-			const selectedElement = this.selectedElement;
+    // Create 'Link to' input
+    const linkToView = new InputTextView(this.locale);
+    const linkToBind = linkToView.bindTemplate;
+    linkToView.setTemplate({
+      tag: "input",
+      attributes: {
+        type: "text",
+        class: ["ck-cta-form-input", linkToBind.if("hasError", "ck-error")],
+        id: linkToBind.to("id"),
+        placeholder: 'https://example.com',
+        readonly: false,
+        "aria-invalid": linkToBind.if("hasError", true),
+        "aria-describedby": linkToBind.to("ariaDescribedById"),
+      },
+      on: {
+        input: linkToBind.to("input"),
+      },
+    });
+    linkToView.label = "Link to";
+    linkToView.on("input", () => {
+      const model = this.editor.model;
+      const selectedElement = this.selectedElement;
 
-			model.change((writer) => {
-				if (!selectedElement) return;
+      model.change((writer) => {
+        if (!selectedElement) return;
 
-				const inputValue = linkToView.element.value.trim();
+        const inputValue = linkToView.element.value.trim();
 
-				// Set the 'href' attribute to the new inputValue
-				writer.setAttribute("href", inputValue, selectedElement);
-			});
-		});
+        // Set the 'href' attribute to the new inputValue
+        writer.setAttribute("href", inputValue, selectedElement);
+      });
+    });
 
-		return {
-			buttonTextView,
-			linkToView,
-		};
-	}
+    return {
+      buttonTextView,
+      linkToView,
+    };
+  }
 }
 
 export default class CTAButtonForm extends Plugin {
-	static get requires() {
-		return [ContextualBalloon];
-	}
+  static get requires() {
+    return [ContextualBalloon];
+  }
 
-	static get pluginName() {
-		return "CTAButtonForm";
-	}
+  static get pluginName() {
+    return "CTAButtonForm";
+  }
 
-	init() {
-		const editor = this.editor;
-		editor.editing.view.addObserver(ClickObserver);
+  init() {
+    const editor = this.editor;
+    editor.editing.view.addObserver(ClickObserver);
 
-		this.formView = this._createFormView();
+    this.formView = this._createFormView();
 
-		this._balloon = editor.plugins.get(ContextualBalloon);
+    this._balloon = editor.plugins.get(ContextualBalloon);
 
-		this._enableUserBalloonInteractions();
-	}
+    this._enableUserBalloonInteractions();
+  }
 
-	destroy() {
-		super.destroy();
+  destroy() {
+    super.destroy();
 
-		this.formView.destroy();
-	}
+    this.formView.destroy();
+  }
 
-	_showUI(selectedElement) {
-		this._addFormView(selectedElement);
+  _showUI(selectedElement) {
+    this._addFormView(selectedElement);
 
-		this._balloon.showStack("main");
-	}
+    this._balloon.showStack("main");
+  }
 
-	_createFormView() {
-		const editor = this.editor;
+  _createFormView() {
+    const editor = this.editor;
 
-		const formView = new MainFormView(editor.locale, editor);
+    const formView = new MainFormView(editor.locale, editor);
 
-		// Close plugin ui, if esc is pressed (while ui is focused)
-		formView.keystrokes.set("esc", (data, cancel) => {
-			this.formView.fire("submit");
-			this._closeFormView();
-		});
+    // Close plugin ui, if esc is pressed (while ui is focused)
+    formView.keystrokes.set("esc", (data, cancel) => {
+      this.formView.fire("submit");
+      this._closeFormView();
+    });
 
-		// Cycle to the next input on enter, unless this is the last input in which case close
-		formView.keystrokes.set("Enter", (data, cancel) => {
-			if (document.activeElement === formView.linkToView.element) {
-				this.formView.fire("submit");
-				this._closeFormView();
-			} else {
-				this.formView._focusCycler.focusNext();
-				cancel();
-			}
-		});
+    // Cycle to the next input on enter, unless this is the last input in which case close
+    formView.keystrokes.set("Enter", (data, cancel) => {
+      if (document.activeElement === formView.linkToView.element) {
+        this.formView.fire("submit");
+        this._closeFormView();
+      } else {
+        this.formView._focusCycler.focusNext();
+        cancel();
+      }
+    });
 
-		return formView;
-	}
+    return formView;
+  }
 
-	_addFormView(selectedElement) {
-		if (this._isFormInPanel) {
-			return;
-		}
+  _addFormView(selectedElement) {
+    if (this._isFormInPanel) {
+      return;
+    }
 
-		const editor = this.editor;
+    const editor = this.editor;
 
-		this._balloon.add({
-			view: this.formView,
-			position: this._getBalloonPositionData(),
-			balloonClassName: "ck-math-balloon",
-		});
+    this._balloon.add({
+      view: this.formView,
+      position: this._getBalloonPositionData(),
+      balloonClassName: "ck-math-balloon",
+    });
 
-		this.formView.buttonTextView.select();
-		this.formView.selectedElement = selectedElement;
+    this.formView.buttonTextView.select();
+    this.formView.selectedElement = selectedElement;
 
-		// Set buttonText to the text content of selectedElement
-		const buttonText = selectedElement.getChild(0).data;
-		this.formView.buttonText = buttonText;
+    // Set buttonText to the text content of selectedElement
+    const buttonText = selectedElement.getChild(0).data;
+    this.formView.buttonText = buttonText;
 
-		// Set linkTo to the 'data-href' attribute of the selectedElement
-		const linkTo = selectedElement.getAttribute("href") || '';
-		this.formView.linkTo = linkTo;
-	}
+    // Set linkTo to the 'data-href' attribute of the selectedElement
+    const linkTo = selectedElement.getAttribute("href") || '';
+    this.formView.linkTo = linkTo;
+  }
 
-	_closeFormView() {
-		if (!this._isFormInPanel) {
-			return;
-		}
+  _closeFormView() {
+    if (!this._isFormInPanel) {
+      return;
+    }
 
-		const editor = this.editor;
+    const editor = this.editor;
 
-		this.stopListening(editor.ui, "update");
-		this.stopListening(this._balloon, "change:visibleView");
+    this.stopListening(editor.ui, "update");
+    this.stopListening(this._balloon, "change:visibleView");
 
-		editor.editing.view.focus();
+    editor.editing.view.focus();
 
-		if (this._isFormInPanel) {
-			this._balloon.remove(this.formView);
+    if (this._isFormInPanel) {
+      this._balloon.remove(this.formView);
 
-			this.editor.editing.view.focus();
-		}
-	}
+      this.editor.editing.view.focus();
+    }
+  }
 
-	_getBalloonPositionData() {
-		const view = this.editor.editing.view;
-		const viewDocument = view.document;
-		const target = view.domConverter.viewRangeToDom(viewDocument.selection.getFirstRange());
-		return { target };
-	}
+  _getBalloonPositionData() {
+    const view = this.editor.editing.view;
+    const viewDocument = view.document;
+    const target = view.domConverter.viewRangeToDom(viewDocument.selection.getFirstRange());
+    return { target };
+  }
 
-	/**
-	 * Check if the current selected element is a CTA button and return it if so
-	 */
-	_getSelectedCTAButton() {
-		const selection = this.editor.model.document.selection;
-		const selectedElement = selection.getSelectedElement();
-		if (selectedElement && selectedElement.is("element", "ctaButton")) {
-			return selectedElement;
-		}
-		return null;
-	}
+  /**
+   * Check if the current selected element is a CTA button and return it if so
+   */
+  _getSelectedCTAButton() {
+    const selection = this.editor.model.document.selection;
+    const selectedElement = selection.getSelectedElement();
+    if (selectedElement && selectedElement.is("element", "ctaButton")) {
+      return selectedElement;
+    }
+    return null;
+  }
 
-	/**
-	 * Handle clicks to open and close the form view
-	 */
-	_enableUserBalloonInteractions() {
-		const viewDocument = this.editor.editing.view.document;
+  /**
+   * Handle clicks to open and close the form view
+   */
+  _enableUserBalloonInteractions() {
+    const viewDocument = this.editor.editing.view.document;
 
-		// Show panel when a CTA button is selected
-		this.listenTo(viewDocument, "click", () => {
-			const selectedElement = this._getSelectedCTAButton();
-			if (selectedElement) {
-				this._showUI(selectedElement);
-			}
-		});
+    // Show panel when a CTA button is selected
+    this.listenTo(viewDocument, "click", () => {
+      const selectedElement = this._getSelectedCTAButton();
+      if (selectedElement) {
+        this._showUI(selectedElement);
+      }
+    });
 
-		// Close on click outside of balloon panel element
-		clickOutsideHandler({
-			emitter: this.formView,
-			activator: () => true,
-			contextElements: [this._balloon.view.element],
-			callback: () => this._closeFormView(),
-		});
-	}
+    // Close on click outside of balloon panel element
+    clickOutsideHandler({
+      emitter: this.formView,
+      activator: () => true,
+      contextElements: [this._balloon.view.element],
+      callback: () => this._closeFormView(),
+    });
+  }
 
-	get _isFormInPanel() {
-		return this._balloon.hasView(this.formView);
-	}
+  get _isFormInPanel() {
+    return this._balloon.hasView(this.formView);
+  }
 }
