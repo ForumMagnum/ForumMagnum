6f052063a663ab9f664362c506ce08fc640d989c
Rafe Kennedy <dear.rafe@gmail.com>
2024-02-06 17:11:04 -0800

    Styling changes

--- packages/lesswrong/components/posts/PostsPage/SplashHeaderImageOptions.tsx
+++ packages/lesswrong/components/posts/PostsPage/SplashHeaderImageOptions.tsx
@@ -6,22 +6,16 @@ import { ImageProvider, ReviewWinnerImageInfo, useImageContext } from './ImageCo
 
 
 const styles = (theme: ThemeType) => ({
-  root: {
+  root: { 
     zIndex: theme.zIndexes.splashHeaderImageOptions,
-  },
-  idk: { 
-    display: 'flex', 
-    flexDirection: 'column',
-    justifyContent: 'space-around',
     marginBottom: '40px', 
-    height: '90vh',
+    height: '80vh',
     overflow: 'scroll'
   },
   imageContainer: {
     width: '200px',
     height: 'auto',
     cursor: 'pointer',
-    // style={{ cursor: 'pointer', display: 'inline-block', maxWidth: '200px' }}
   },
 });
 
@@ -30,35 +24,26 @@ export const SplashHeaderImageOptions = ({ images, post, classes }: {
   post: PostsWithNavigation|PostsWithNavigationAndRevision,
   classes: ClassesType<typeof styles>
 }) => {
-  // const { PostsPodcastPlayer, T3AudioPlayer } = Components;
 
-  const [selectedImage, setSelectedImage] = useState(images[0].splashArtImageUrl);
-  const { imageInfo, setImageInfo } = useImageContext();
-
-  const handleImageClick = (image: ReviewWinnerImageInfo) => {
-    setSelectedImage(image.splashArtImageUrl);
-    setImageInfo(image); // Step 3
-  };
+  const { setImageInfo } = useImageContext();
 
   return (
-      <div className={classes.root}>
-        <div className={classes.idk}>
-        {images.map((image, index) => (
-          <div 
-            className={classes.imageContainer}
-            key={index}
-            onClick={() => handleImageClick(image)}
-          >
-            <img
-              src={image.splashArtImageUrl}
-              alt={`Selectable ${index}`}
-              style={{ maxWidth: '100%', height: 'auto' }} // Ensure the image scales within the div
-              title={`Prompt: ${image.splashArtImagePrompt || 'No prompt'}`}
-            />
-          </div>
-          ))}
-        </div>
+    <div className={classes.root}>
+    {images.map((image, index) => (
+      <div
+        className={classes.imageContainer}
+        key={index}
+        onClick={() => setImageInfo(image)}
+      >
+        <img
+          src={image.splashArtImageUrl}
+          alt={`Selectable ${index}`}
+          style={{ maxWidth: '100%', height: 'auto' }}
+          title={`Prompt: ${image.splashArtImagePrompt || 'No prompt'}`}
+        />
       </div>
+      ))}
+    </div>
   );
 };
 
