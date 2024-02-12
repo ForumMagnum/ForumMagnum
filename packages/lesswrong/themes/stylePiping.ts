382f93079c556faa5e01409332c1179668968ff2
Robert Mushkatblat <bobert.mushky@gmail.com>
2024-02-12 14:57:03 -0800

    change width/etc to minWidth/etc for tables published from ckeditor to fix all tags display

--- packages/lesswrong/themes/stylePiping.ts
+++ packages/lesswrong/themes/stylePiping.ts
@@ -387,8 +387,8 @@ const baseBodyStyles = (theme: ThemeType): JssStyles => ({
   },
   // CKEditor wraps tables in a figure element
   '& figure.table': {
-    width: 'fit-content !important',
-    height: 'fit-content !important',
+    minWidth: 'fit-content !important',
+    minHeight: 'fit-content !important',
   },
   // Many column tables should overflow instead of squishing
   //  - NB: As of Jan 2023, this does not work on firefox, so ff users will have
