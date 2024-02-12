c1fa780721d30427833417de7ad6d6cc761bc8ef
Ollie Etherington <ollie@etherington.xyz>
2024-02-12 18:16:00 +0200

    Add doc comment to `useOptimisticToggle`

--- packages/lesswrong/components/hooks/useOptimisticToggle.ts
+++ packages/lesswrong/components/hooks/useOptimisticToggle.ts
@@ -1,5 +1,11 @@
 import { useCallback, MouseEvent, useState, useEffect } from "react";
 
+/**
+ * We have lots of places that toggle values on the backend and then show
+ * the result somehow in the UI. This can be unpleasantly laggy if we just
+ * display the new value received from the backend, so this hook can be used
+ * to keep track of an "optimistic" client-side value as well.
+ */
 export const useOptimisticToggle = (
   actualValue: boolean,
   toggle: (e: MouseEvent<HTMLDivElement>) => void,
