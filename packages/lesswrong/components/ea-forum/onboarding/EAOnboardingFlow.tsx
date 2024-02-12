f48dc3158a8f12276693bf8a6f16cbb269dfd528
Ollie Etherington <ollie@etherington.xyz>
2024-02-12 17:38:01 +0200

    Login and signup users without refreshing the page

--- packages/lesswrong/components/ea-forum/onboarding/EAOnboardingFlow.tsx
+++ packages/lesswrong/components/ea-forum/onboarding/EAOnboardingFlow.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { Components, registerComponent } from "../../../lib/vulcan-lib";
 import { useCurrentUser } from "../../common/withUser";
 import { EAOnboardingContextProvider } from "./useEAOnboarding";
@@ -13,7 +13,17 @@ const EAOnboardingFlow = ({classes}: {
   classes: ClassesType<typeof styles>,
 }) => {
   const currentUser = useCurrentUser();
+
+  // If `usernameUnset` is true, then we need to show the onboarding flow.
+  // We cache the value in a `useState` as it gets set to false in the very
+  // first stage (which is the only compulsary stage) - without caching the
+  // value this would close the popup.
   const [isOnboarding, setIsOnboarding] = useState(currentUser?.usernameUnset);
+
+  useEffect(() => {
+    // Set `isOnboarding` to true after a new user signs up.
+    setIsOnboarding((currentValue) => currentValue || currentUser?.usernameUnset);
+  }, [currentUser?.usernameUnset]);
 
   const onOnboardingComplete = useCallback(() => {
     setIsOnboarding(false);
