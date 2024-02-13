38f930f0781298a0b64d316e7028477708bd48ee
Sarah Cheng <sarah.cheng@centreforeffectivealtruism.org>
2024-02-13 10:55:41 -0500

    [EA Forum only] increase the read threshold for job ads

--- packages/lesswrong/components/ea-forum/TargetedJobAdSection.tsx
+++ packages/lesswrong/components/ea-forum/TargetedJobAdSection.tsx
@@ -95,11 +95,11 @@ const TargetedJobAdSection = () => {
         (setting: FilterTag) => filterModeIsSubscribed(setting.filterMode)
       )?.map((setting: FilterTag) => setting.tagId)
       let userIsMatch = subscribedTagIds && !difference(subscribedTagIds, userTagSubs).length
-      // or if they have read at least 12 posts in all the relevant topics in the past 6 months
+      // or if they have read at least 18 posts in all the relevant topics in the past 6 months
       userIsMatch = userIsMatch || (
         !!coreTagReads?.length &&
         JOB_AD_DATA[jobName].readCoreTagIds?.every(
-          tagId => coreTagReads.some(tag => tag.tagId === tagId && tag.userReadCount >= 12)
+          tagId => coreTagReads.some(tag => tag.tagId === tagId && tag.userReadCount >= 18)
         )
       )
       
