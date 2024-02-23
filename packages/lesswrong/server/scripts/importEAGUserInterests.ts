d25793ff1c4bd5f44cf5750338e0d5097985f713
Sarah Cheng <sarah.cheng@centreforeffectivealtruism.org>
2024-02-23 10:56:07 -0500

    add some types

--- packages/lesswrong/server/scripts/importEAGUserInterests.ts
+++ packages/lesswrong/server/scripts/importEAGUserInterests.ts
@@ -1,4 +1,5 @@
 import fs from 'mz/fs';
+import { z } from "zod";
 import Papa from 'papaparse';
 import Users from '../../lib/collections/users/collection';
 import { updateMutator, Vulcan } from '../vulcan-lib';
@@ -38,21 +39,23 @@ Vulcan.importEAGUserInterests = wrapVulcanAsyncScript(
 )
 
 
-type UserEAGDetailsRow = {
-  email: string,
-  careerStage?: string,
-  countryOrRegion?: string,
-  nearestCity?: string,
-  willingnessToRelocateBoston?: string,
-  willingnessToRelocateBayArea?: string,
-  willingnessToRelocateDC?: string,
-  willingnessToRelocateLondon?: string,
-  willingnessToRelocateNYC?: string,
-  willingnessToRelocateOxford?: string,
-  willingnessToRelocateRemote?: string,
-  experiencedIn?: string,
-  interestedIn?: string
-}
+const csvSchema = z.object({
+  email: z.string(),
+  careerStage: z.string().optional(),
+  countryOrRegion: z.string().optional(),
+  nearestCity: z.string().optional(),
+  willingnessToRelocateBoston: z.string().optional(),
+  willingnessToRelocateBayArea: z.string().optional(),
+  willingnessToRelocateDC: z.string().optional(),
+  willingnessToRelocateLondon: z.string().optional(),
+  willingnessToRelocateNYC: z.string().optional(),
+  willingnessToRelocateOxford: z.string().optional(),
+  willingnessToRelocateRemote: z.string().optional(),
+  experiencedIn: z.string().optional(),
+  interestedIn: z.string().optional(),
+});
+
+export type UserEAGDetailsRow = z.infer<typeof csvSchema>;
 
 /**
  * Given a CSV with users' EAG data, import it to the "UserEAGDetails" table, replacing any existing user data.
@@ -61,12 +64,13 @@ Vulcan.importEAGUserDetails = wrapVulcanAsyncScript(
   'importEAGUserDetails',
   async (fileName='EAG_users.csv') => {
     const now = new Date()
-    const records: AnyBecauseTodo[] = []
+    const records: Partial<DbUserEAGDetail>[] = []
     
-    const dataStream: AsyncIterable<AnyBecauseTodo> = fs.createReadStream(fileName)
+    const dataStream: AsyncIterable<AnyBecauseIsInput> = fs.createReadStream(fileName)
       .pipe(Papa.parse(Papa.NODE_STREAM_INPUT, {header: true, delimiter: ','}))
 
-    for await (const row of dataStream) {
+    for await (const data of dataStream) {
+      const row: UserEAGDetailsRow = csvSchema.parse(data);
       // do a simple check to see if we have a user with this email address
       const user = await Users.findOne({email: row.email})
       if (!user) continue
