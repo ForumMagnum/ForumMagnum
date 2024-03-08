a9b01ba1c4b6481f89b95fa019c71dde1cf513ec
Ollie Etherington <ollie@etherington.xyz>
2024-03-08 18:23:24 +0000

    Clean up time conversions

--- packages/lesswrong/lib/eaDraftAmnestyWeek.ts
+++ packages/lesswrong/lib/eaDraftAmnestyWeek.ts
@@ -1,10 +1,10 @@
 import moment from "moment";
 
 export const isDraftAmnestyWeek = () => {
-  const now = moment.utc();
-  const start = moment.utc("2024-03-11");
-  const end = moment.utc("2024-03-18");
-  return now.unix() > start.unix() && now.unix() < end.unix();
+  const now = moment.utc().unix();
+  const start = moment.utc("2024-03-11").unix();
+  const end = moment.utc("2024-03-18").unix();
+  return now > start && now < end;
 }
 
 export const draftAmnestyWeekTagId = "A4eiAauprRuqA6kCx";
