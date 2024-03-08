b481ee56366fa37e559383456796d20215c2e604
Sarah Cheng <sarah.cheng@centreforeffectivealtruism.org>
2024-03-07 19:06:46 -0500

    dont show the login prompt for an inaccessible post page

--- packages/lesswrong/components/common/ErrorAccessDenied.tsx
+++ packages/lesswrong/components/common/ErrorAccessDenied.tsx
@@ -3,6 +3,7 @@ import { Components, registerComponent } from '../../lib/vulcan-lib';
 import { useCurrentUser } from './withUser';
 import { useServerRequestStatus } from '../../lib/routeUtil'
 import { isFriendlyUI } from '../../themes/forumTheme';
+import { isEAForum } from '../../lib/instanceSettings';
 
 const styles = (theme: ThemeType) => ({
   root: isFriendlyUI
@@ -10,12 +11,26 @@ const styles = (theme: ThemeType) => ({
       fontFamily: theme.palette.fonts.sansSerifStack,
       fontSize: 14,
       fontWeight: 500,
+      lineHeight: '24px',
+      textWrap: 'pretty',
+      marginTop: 30
     }
     :{},
 });
 
-const ErrorAccessDenied = ({explanation, classes}: {
+/**
+ * Show a simple "you don't have access" message if the user is logged in,
+ * or a "please log in" message if the user is logged out.
+ *
+ * Most of the time, we use this when a page is meant to be accessed by only a subset of
+ * logged in users, so by default this prompts the user to log in if they are logged out.
+ *
+ * However, for pages that are normally meant to be publicly accessible (like the post page),
+ * we skip the login prompt and just display the "you don't have access" message.
+ */
+const ErrorAccessDenied = ({explanation, skipLoginPrompt, classes}: {
   explanation?: string,
+  skipLoginPrompt?: boolean,
   classes: ClassesType<typeof styles>,
 }) => {
   const { SingleColumnSection, Typography } = Components;
@@ -23,7 +38,7 @@ const ErrorAccessDenied = ({explanation, classes}: {
   const currentUser = useCurrentUser();
   if (serverRequestStatus) serverRequestStatus.status = 403
   
-  if (currentUser) {
+  if (currentUser || skipLoginPrompt) {
     const message = `Sorry, you don't have access to this page.${(explanation ? ` ${explanation}` : "")}`
     return <SingleColumnSection>
       <div className={classes.root}>{message}</div>
