09dab254e25a7368043a4fd0087a59c3c2b66f42
Ollie Etherington <ollie@etherington.xyz>
2024-03-11 20:52:12 +0000

    Fix initialization of editable fields for forum events

--- packages/lesswrong/server/migrations/20240311T113830.create_ForumEvents_table.ts
+++ packages/lesswrong/server/migrations/20240311T113830.create_ForumEvents_table.ts
@@ -1,7 +1,7 @@
 import ForumEvents from "../../lib/collections/forumEvents/collection";
 import { createTable, dropTable } from "./meta/utils";
 
-export const acceptsSchemaHash = "a5d908ad079b15fbcbc381bbc31e2f03";
+export const acceptsSchemaHash = "5128a9d63bc9b8e67348df15c918b0d8";
 
 export const up = async ({db}: MigrationContext) => {
   await createTable(db, ForumEvents);
