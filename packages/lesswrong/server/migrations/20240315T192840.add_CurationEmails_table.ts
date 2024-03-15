be24c774eae770bf3620b5f33e1ef261d6caf233
Robert Mushkatblat <bobert.mushky@gmail.com>
2024-03-15 12:30:25 -0700

    redo migration file

--- packages/lesswrong/server/migrations/20240315T192840.add_CurationEmails_table.ts
+++ packages/lesswrong/server/migrations/20240315T192840.add_CurationEmails_table.ts
@@ -1,9 +1,9 @@
 /**
- * Generated on 2024-03-15T01:34:41.920Z by `yarn makemigrations`
+ * Generated on 2024-03-15T19:28:40.933Z by `yarn makemigrations`
  * The following schema changes were detected:
  * -------------------------------------------
  * diff --git a/Users/robert/Documents/repos/ForumMagnum/schema/accepted_schema.sql b/Users/robert/Documents/repos/ForumMagnum/schema/schema_to_accept.sql
- * index feca4cc2cd..a2d474c516 100644
+ * index feca4cc2cd..6a9b2c2fb5 100644
  * --- a/Users/robert/Documents/repos/ForumMagnum/schema/accepted_schema.sql
  * +++ b/Users/robert/Documents/repos/ForumMagnum/schema/schema_to_accept.sql
  * @@ -4,5 +4,3 @@
@@ -11,7 +11,7 @@
  * --- Overall schema hash: ec6d12fa05425118431d110f2e216b80
  * -
  * --- Accepted on 2024-03-12T12:36:30.000Z by 20240312T123630.create_ForumEvents_table.ts
- * +-- Overall schema hash: 0171c92d85515f22e6e3499e92b1aee4
+ * +-- Overall schema hash: 2a56d7da2e4ee362f7616d1350760500
  *  
  * @@ -237,2 +235,13 @@ CREATE TABLE "CronHistories" (
  *  
@@ -29,9 +29,9 @@
  *  -- Schema for "DatabaseMetadata", hash: d05d205da4249541e524b7d8879c6e1c
  * @@ -1734 +1743,17 @@ CREATE OR REPLACE FUNCTION fm_vote_added_emoji(
  *  
- * +-- Function, hash: b0deed77e4cdaf21f56eb03d561c8455
+ * +-- Function, hash: 321634e71748effc28132b851408fb4c
  * +CREATE OR REPLACE FUNCTION fm_has_verified_email(emails jsonb[])
- * +    RETURNS boolean AS $$
+ * +    RETURNS boolean LANGUAGE plpgsql IMMUTABLE AS $$
  * +    DECLARE
  * +      item jsonb;
  * +    BEGIN
@@ -54,7 +54,7 @@
  * - [ ] Uncomment `acceptsSchemaHash` below
  * - [ ] Run `yarn acceptmigrations` to update the accepted schema hash (running makemigrations again will also do this)
  */
-export const acceptsSchemaHash = "0171c92d85515f22e6e3499e92b1aee4";
+export const acceptsSchemaHash = "2a56d7da2e4ee362f7616d1350760500";
 
 import CurationEmails from "../../lib/collections/curationEmails/collection"
 import { createTable, dropTable, updateFunctions } from "./meta/utils"
