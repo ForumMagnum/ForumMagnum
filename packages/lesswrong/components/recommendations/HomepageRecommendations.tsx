6e8b3f5564b961c9738156f394304ab2f253cc71
Ruben Bloom <darkruby501@users.noreply.github.com>
2024-03-21 14:58:18 -0700

    start to make override controls

--- packages/lesswrong/components/recommendations/HomepageRecommendations.tsx
+++ packages/lesswrong/components/recommendations/HomepageRecommendations.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Components, registerComponent } from '../../lib/vulcan-lib';
 import { AnalyticsContext } from "../../lib/analyticsEvents";
 import type { RecommendationsAlgorithm } from '../../lib/collections/users/recommendationSettings';
@@ -13,20 +13,58 @@ export const HomepageRecommendations = ({ recommendationSettings, classes }: {
   recommendationSettings: RecommendationsAlgorithm
   classes: ClassesType<typeof styles>,
 }) => {
-  const { RecommendationsList, SingleColumnSection, SectionTitle } = Components;
+  const { RecommendationsList, SingleColumnSection, SectionTitle, EditableUsersList } = Components;
+  const [ userOverride, setUserOverride ] = useState<string[]| null>
+
+
+
+
+  const recombeeRecommendationOverrideControls = <div>
+    <div>
+      <label htmlFor="userOverride">User Override:</label>
+      <EditableUsersList
+        value={userOverride}
+        setValue={(newUsers: string[]) => {
+          setUserOverride(newUsers)
+        }}
+        label="override with user"
+      />
+    </div>
+    <div>
+      <label htmlFor="sourceType">Source Type:</label>
+      <select id="sourceType">
+        <option value="default">Default</option>
+        <option value="source1">Source 1</option>
+        <option value="source2">Source 2</option>
+      </select>
+    </div>
+    <div>
+      <label htmlFor="rotationRate">Rotation Rate:</label>
+      <input type="number" id="rotationRate" />
+    </div>
+  </div>
+
+
+
+
+
+
 
   return (
-    <SingleColumnSection>
-      <SectionTitle title="Recommendations" />
-      <AnalyticsContext
-        pageSubSectionContext="belowLatestPostsRecommendations"
-        capturePostItemOnMount
-      >
+    <AnalyticsContext
+      pageSubSectionContext="belowLatestPostsRecommendations"
+      capturePostItemOnMount
+    >
+      <SingleColumnSection>
+        <SectionTitle title="Recommendations" />
+
+        {/* check whether user is admin */}
+        userIsAdmin(currentUser) && 
+
         <RecommendationsList algorithm={recommendationSettings} />
-      </AnalyticsContext>
-    </SingleColumnSection>
+      </SingleColumnSection>
+    </AnalyticsContext>
   );
-}
 
 const HomepageRecommendationsComponent = registerComponent('HomepageRecommendations', HomepageRecommendations, {styles});
 
