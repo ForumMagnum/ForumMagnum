2d98b8a41256ac03e93d1a5d42360f2f77688c6b
Ollie Etherington <ollie@etherington.xyz>
2024-03-21 18:55:07 +0100

    Make people directory results horizontally scrollable

--- packages/lesswrong/components/peopleDirectory/PeopleDirectoryResults.tsx
+++ packages/lesswrong/components/peopleDirectory/PeopleDirectoryResults.tsx
@@ -1,15 +1,37 @@
 import React from "react";
 import { Components, registerComponent } from "../../lib/vulcan-lib";
 import { usePeopleDirectory } from "./usePeopleDirectory";
+import { SCROLL_INDICATOR_SIZE } from "../common/HorizScrollBlock";
 
 const styles = (theme: ThemeType) => ({
   root: {
-    display: "grid",
-    padding: "12px 24px",
-    width: "100%",
+    marginLeft: -SCROLL_INDICATOR_SIZE,
+    width: `calc(100% + ${2 * SCROLL_INDICATOR_SIZE}px)`,
+    maxWidth: "unset !important", // Overwrite the HorizScrollBlock default
+  },
+  contents: {
     border: `1px solid ${theme.palette.grey[310]}`,
     borderRadius: theme.borderRadius.default,
     background: theme.palette.grey[0],
+    padding: "12px 24px !important", // Overwrite the HorizScrollBlock default
+  },
+  // We want to show some padding to the right of the grid, but there's a nasty
+  // firefox bug where the padding isn't shown when the table overflows and
+  // we scroll to the right. Adding this `:after` allows us to force the padding
+  // that we want.
+  // See stackoverflow.com/questions/13471910/no-padding-when-using-overflow-auto
+  gridWrapper: {
+    display: "flex",
+    width: "min-content",
+    "&:after": {
+      content: '""',
+      display: "block",
+      minWidth: 24,
+      height: "100%",
+    },
+  },
+  grid: {
+    display: "grid",
     color: theme.palette.grey[1000],
   },
   loading: {
@@ -25,26 +47,35 @@ export const PeopleDirectoryResults = ({classes}: {
   const columnCount = columns
     .filter((column) => !column.hideable || !column.hidden)
     .length;
-  const {PeopleDirectoryHeading, PeopleDirectoryResultRow, Loading} = Components;
+
+  const {
+    HorizScrollBlock, PeopleDirectoryHeading, PeopleDirectoryResultRow, Loading,
+  } = Components;
   return (
-    <div
-      style={{
-        gridTemplateColumns: `repeat(${columnCount}, 1fr)`,
-      }}
-      className={classes.root}
-    >
-      {columns.map((column) =>
-        !column.hideable || !column.hidden
-          ? <PeopleDirectoryHeading key={column.label} column={column} />
-          : null
-      )}
-      {results.map((result) => (
-        <PeopleDirectoryResultRow key={result._id} result={result} />
-      ))}
-      {resultsLoading &&
-        <div className={classes.loading}><Loading /></div>
-      }
-    </div>
+    <HorizScrollBlock className={classes.root} contentsClassName={classes.contents}>
+      <div className={classes.gridWrapper}>
+      <div
+        style={{
+          gridTemplateColumns: `repeat(${columnCount}, 1fr)`,
+        }}
+        className={classes.grid}
+      >
+        {columns.map((column) =>
+          !column.hideable || !column.hidden
+            ? <PeopleDirectoryHeading key={column.label} column={column} />
+            : null
+        )}
+        {results.map((result) => (
+          <PeopleDirectoryResultRow key={result._id} result={result} />
+        ))}
+        {resultsLoading &&
+          <div className={classes.loading}>
+            <Loading />
+          </div>
+        }
+      </div>
+      </div>
+    </HorizScrollBlock>
   );
 }
 
