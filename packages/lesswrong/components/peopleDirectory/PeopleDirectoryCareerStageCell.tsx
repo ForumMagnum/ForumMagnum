a286ab80a031abd05b66c8b2ae7de92dd689860d
Ollie Etherington <ollie@etherington.xyz>
2024-03-21 10:22:15 +0100

    Fix line wrap for career stage cell

--- packages/lesswrong/components/peopleDirectory/PeopleDirectoryCareerStageCell.tsx
+++ packages/lesswrong/components/peopleDirectory/PeopleDirectoryCareerStageCell.tsx
@@ -1,18 +1,23 @@
 import React from "react";
 import { Components, registerComponent } from "../../lib/vulcan-lib";
 import { CAREER_STAGES } from "../../lib/collections/users/schema";
+import { cellTextStyles } from "./PeopleDirectoryTextCell";
 
 const styles = (theme: ThemeType) => ({
   root: {
     display: "flex",
     alignItems: "center",
     gap: "6px",
-    color: theme.palette.grey[600],
-    fontSize: 13,
-    fontWeight: 600,
   },
   icon: {
     width: 16,
+    color: theme.palette.grey[600],
+  },
+  label: {
+    ...cellTextStyles(theme),
+    color: theme.palette.grey[600],
+    fontSize: 13,
+    fontWeight: 600,
   },
 });
 
@@ -23,19 +28,21 @@ export const PeopleDirectoryCareerStageCell = ({user, classes}: {
   const stage = user.careerStage?.[0]
     ? CAREER_STAGES.find(({value}) => value === user.careerStage?.[0])
     : null;
-  const {ForumIcon} = Components;
+  const {LWTooltip, ForumIcon} = Components;
   return (
-    <div className={classes.root}>
-      {stage
-        ? (
-          <>
-            <ForumIcon icon={stage.icon} className={classes.icon} />
-            {stage.label}
-          </>
-        )
-        : "-"
-      }
-    </div>
+    <LWTooltip title={stage?.label}>
+      <div className={classes.root}>
+        {stage
+          ? (
+            <>
+              <ForumIcon icon={stage.icon} className={classes.icon} />
+              <span className={classes.label}>{stage.label}</span>
+            </>
+          )
+          : "-"
+        }
+      </div>
+    </LWTooltip>
   );
 }
 
