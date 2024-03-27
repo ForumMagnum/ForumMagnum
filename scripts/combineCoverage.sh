169e643209a08104f9abc5f33f1d6890df9e25a5
Ollie Etherington <ollie@etherington.xyz>
2024-03-27 12:00:25 +0100

    Upload coverage reports using coveralls parallel mode

--- scripts/combineCoverage.sh
+++ scripts/combineCoverage.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+UNIT=${1:-unit-coverage/lcov.info}
+INT=${2:-integration-coverage/lcov.info}
+OUT=${3:-combined-coverage/lcov.info}
+HTML=${4:-combined-coverage/lcov-report}
+
 mkdir -p ./combined-coverage
 rm -rf ./combined-coverage/*
-lcov --add-tracefile unit-coverage-lcov.info -a integration-coverage-lcov.info -o combined-coverage/lcov.info
-# genhtml combined-coverage/lcov.info -o combined-coverage/lcov-report
+lcov --add-tracefile $UNIT -a $INT -o $OUT
+genhtml $OUT -o $HTML
