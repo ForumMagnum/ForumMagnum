359f39e4ffe1f423830f274719308c2fcba859d3
Raemon <raemon777@gmail.com>
2024-04-02 16:45:20 -0700

    coauthors-for-best-of-lw

--- packages/lesswrong/components/posts/PostsPage/PostsPageSplashHeader.tsx
+++ packages/lesswrong/components/posts/PostsPage/PostsPageSplashHeader.tsx
@@ -279,7 +279,7 @@ const styles = (theme: ThemeType) => ({
       display: 'block',
     }
   },
-  author: {
+  authors: {
     fontSize: '1.6rem',
     fontWeight: '700',
     color: theme.palette.text.reviewWinner.author,
@@ -375,7 +375,7 @@ const PostsPageSplashHeader = ({post, showEmbeddedPlayer, toggleEmbeddedPlayer, 
   toggleEmbeddedPlayer?: () => void,
   classes: ClassesType<typeof styles>,
 }) => {
-  const { FooterTagList, UsersName, CommentBody, PostActionsButton, LWTooltip, LWPopper, ImageCropPreview, ForumIcon, SplashHeaderImageOptions, PostsAudioPlayerWrapper, PostsSplashPageHeaderVote } = Components;
+  const { FooterTagList, UsersName, CommentBody, PostActionsButton, LWTooltip, LWPopper, ImageCropPreview, ForumIcon, SplashHeaderImageOptions, PostsAudioPlayerWrapper, PostsSplashPageHeaderVote, PostsCoauthor } = Components;
   
   const { selectedImageInfo } = useImageContext();
   const { setToCVisible } = useContext(SidebarsContext)!;
@@ -541,8 +541,17 @@ const PostsPageSplashHeader = ({post, showEmbeddedPlayer, toggleEmbeddedPlayer, 
       <h1 className={classNames(classes.title, { [classes.titleSmaller]: post.title.length > 80 })}>
         {post.title}
       </h1>
-      <div className={classes.author}>
-        <UsersName user={post.user} />
+      <div className={classes.authors}>
+        <span>
+          <UsersName user={post.user} />
+          {!!post.coauthors?.length ? ', ' : ''}
+        </span>
+        {post.coauthors?.map((coauthor, i) =>
+          <span key={`coauthor${coauthor._id}`}>
+            <UsersName user={coauthor} />
+            {i < post.coauthors.length - 1 ? ', ' : ''}
+          </span>
+        )}
       </div>
     </div>
   );
