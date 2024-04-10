dd865a05bde2738fda2d1ebb83ddffe4e86b2602
Sarah Cheng <sarah.cheng@centreforeffectivealtruism.org>
2024-04-10 15:59:14 -0400

    add gating

--- packages/lesswrong/components/posts/PostsLoading.tsx
+++ packages/lesswrong/components/posts/PostsLoading.tsx
@@ -1,11 +1,12 @@
 import range from 'lodash/range';
 import { Components, registerComponent } from '../../lib/vulcan-lib';
 import React from 'react';
+import { isFriendlyUI } from '../../themes/forumTheme';
 
 const PostsLoading = ({placeholderCount}: {
   placeholderCount?: number
 }) => {
-  if (placeholderCount) {
+  if (placeholderCount && isFriendlyUI) {
     return <>
       {range(0, placeholderCount)
         .map(i => <Components.FriendlyPlaceholderPostsItem
