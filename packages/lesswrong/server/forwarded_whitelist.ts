543b238a8904fbc2383ed00db489ca6b0b8f9072
James Babcock <jimrandomh@gmail.com>
2024-04-10 12:04:02 -0700

    Null-safety in getClientIP for bun
    bun doesn't have a req.connection.socket here and this crashed

--- packages/lesswrong/server/forwarded_whitelist.ts
+++ packages/lesswrong/server/forwarded_whitelist.ts
@@ -25,10 +25,11 @@ const computeForwardedWhitelist = () => {
 export const getForwardedWhitelist = () => ({
   getClientIP: (req: AnyBecauseTodo) => {
     // From: https://stackoverflow.com/a/19524949 (which contains incorrect sample code!)
-    const ip = (req.headers['x-forwarded-for'] || '').split(',').shift().trim() || 
-      req.connection.remoteAddress || 
-      req.socket.remoteAddress || 
-      req.connection.socket.remoteAddress
+    const ip = (req.headers['x-forwarded-for'] || '').split(',').shift().trim()
+      || req.connection.remoteAddress
+      || req.socket.remoteAddress
+      || req.connection.socket?.remoteAddress
+      || "unknown"
     
     return ip
   }
