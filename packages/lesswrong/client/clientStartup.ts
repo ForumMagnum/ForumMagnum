83995c63929a3ae02053c257800a9ce3b036c41a
Will Howard <w.howard256+github@gmail.com>
2024-04-16 17:41:48 +0100

    improve startup sequence of bundle

--- packages/lesswrong/client/clientStartup.ts
+++ packages/lesswrong/client/clientStartup.ts
@@ -1,10 +1,39 @@
 import { runStartupFunctions } from '../lib/executionEnvironment';
 import { filterConsoleLogSpam } from '../lib/consoleFilters';
 
+let startupCalled = false;
+
 async function clientStartup() {
+  if (startupCalled) {
+    return;
+  }
+  startupCalled = true;
+
   filterConsoleLogSpam();
   require('../client.js');
   await runStartupFunctions();
 }
 
-void clientStartup();
+function startupAfterRendering() {
+  // If for some reason the requestAnimationFrame functions don't fire (e.g. because
+  // the tab is in the background), we still want to start the client reasonably quickly.
+  setTimeout(() => clientStartup(), 500);
+
+  console.log('startupAfterRendering entered', performance.now());
+  requestAnimationFrame(() => {
+    console.log('first requestAnimationFrame entered', performance.now());
+    requestAnimationFrame(() => {
+      console.log('second requestAnimationFrame entered', performance.now());
+      void clientStartup();
+    });
+  });
+}
+
+if (document.readyState === 'loading') {
+  console.log('document.readyState === loading', performance.now());
+  document.addEventListener('DOMContentLoaded', startupAfterRendering);
+} else {
+  console.log('document.readyState !== loading', performance.now());
+  void startupAfterRendering();
+}
+
