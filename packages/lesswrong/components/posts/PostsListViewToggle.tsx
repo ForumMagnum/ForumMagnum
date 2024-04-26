44f10c723ca9437f87860d52a8eb25de93fa69e0
Ollie Etherington <ollie@etherington.xyz>
2024-04-26 16:27:26 +0300

    Add 'new' flag for card view toggle

--- packages/lesswrong/components/posts/PostsListViewToggle.tsx
+++ packages/lesswrong/components/posts/PostsListViewToggle.tsx
@@ -3,8 +3,32 @@ import { Components, registerComponent } from "../../lib/vulcan-lib";
 import { isPostsListViewType, usePostsListView } from "../hooks/usePostsListView";
 import { useTracking } from "../../lib/analyticsEvents";
 
-const styles = (_theme: ThemeType) => ({
+const styles = (theme: ThemeType) => ({
   root: {
+    position: "relative",
+  },
+  flag: {
+    position: "absolute",
+    right: -34,
+    top: 8,
+    width: 34,
+    height: 18,
+    padding: "2px 4px",
+    background: theme.palette.primary.dark,
+    color: theme.palette.text.alwaysWhite,
+    borderRadius: theme.borderRadius.small,
+    fontFamily: theme.palette.fonts.sansSerifStack,
+    fontSize: 11,
+    fontWeight: 600,
+  },
+  flagPoint: {
+    position: "absolute",
+    top: 6,
+    left: -3,
+    width: 6,
+    height: 6,
+    transform: "scaleY(70%) rotate(45deg)",
+    background: theme.palette.primary.dark,
   },
 });
 
@@ -28,14 +52,20 @@ const PostsListViewToggle = ({classes}: {
 
   const {ForumDropdown} = Components;
   return (
-    <ForumDropdown
-      value={view}
-      options={options}
-      onSelect={onSelect}
-      paddingSize={24}
-      useIconLabel
-      className={classes.root}
-    />
+    <div className={classes.root}>
+      <ForumDropdown
+        value={view}
+        options={options}
+        onSelect={onSelect}
+        paddingSize={24}
+        useIconLabel
+        className={classes.root}
+      />
+      <div className={classes.flag}>
+        <div className={classes.flagPoint} />
+        NEW
+      </div>
+    </div>
   );
 }
 
