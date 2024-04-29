ab28b978dabb026509c9944691af120523ea1083
Ollie Etherington <ollie@etherington.xyz>
2024-04-29 20:54:13 +0300

    Reindex posts, comments and sequences for deleted users

--- packages/lesswrong/server/search/elastic/elasticCallbacks.ts
+++ packages/lesswrong/server/search/elastic/elasticCallbacks.ts
@@ -3,6 +3,7 @@ import {
   searchIndexedCollectionNames,
 } from "../../../lib/search/searchUtil";
 import { getCollectionHooks } from "../../mutationCallbacks";
+import { UsersRepo } from "../../repos";
 import ElasticClient from "./ElasticClient";
 import ElasticExporter from "./ElasticExporter";
 import { isElasticEnabled } from "./elasticSettings";
@@ -27,4 +28,37 @@ if (isElasticEnabled) {
     getCollectionHooks(collectionName).createAfter.add(callback);
     getCollectionHooks(collectionName).updateAfter.add(callback);
   }
+
+  getCollectionHooks("Users").editAsync.add(async function reindexDeletedUserContent(
+    newUser: DbUser,
+    oldUser: DbUser,
+  ) {
+    if (newUser.deleted && !oldUser.deleted) {
+      const repo = new UsersRepo();
+      const [
+        postIds,
+        commentIds,
+        sequenceIds,
+      ] = await Promise.all([
+        repo.getAllUserPostIds(newUser._id),
+        repo.getAllUserCommentIds(newUser._id),
+        repo.getAllUserSequenceIds(newUser._id),
+      ]);
+
+      const client = new ElasticClient();
+      const exporter = new ElasticExporter(client);
+
+      for (const id of postIds) {
+        await exporter.updateDocument("Posts", id);
+      }
+
+      for (const id of commentIds) {
+        await exporter.updateDocument("Comments", id);
+      }
+
+      for (const id of sequenceIds) {
+        await exporter.updateDocument("Sequences", id);
+      }
+    }
+  });
 }
