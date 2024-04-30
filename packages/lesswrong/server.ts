c551346d2a7c056a76ea20f73d4600630046e8d7
Ollie Etherington <ollie@etherington.xyz>
2024-04-30 12:25:35 +0300

    Add script to reindex content for deleted users

--- packages/lesswrong/server.ts
+++ packages/lesswrong/server.ts
@@ -58,7 +58,7 @@ import './server/scripts/removeRsvp';
 import './server/scripts/regenerateUnicodeSlugs';
 import './server/scripts/checkPostForSockpuppetVoting';
 import './server/scripts/convertAllPostsToEAEmojis';
-
+import './server/scripts/reindexDeletedUserContent';
 import './server/scripts/oneOffBanSpammers'
 import './server/scripts/ensureEmailInEmails';
 import './server/scripts/exportPostDetails';
