26fff009edefaa1075db099e315d213c7a752a6b
Ollie Etherington <ollie@etherington.xyz>
2024-05-02 14:44:48 +0300

    Initial work on account signup test

--- playwright/account_signup.spec.ts
+++ playwright/account_signup.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect } from "@playwright/test";
 
+/*
 test("has title", async ({ page }) => {
   await page.goto("https://playwright.dev/");
 
@@ -15,4 +16,19 @@ test("get started link", async ({ page }) => {
 
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole("heading", { name: "Installation" })).toBeVisible();
+});
+*/
+
+test("account signup", async ({page}) => {
+  const email = "test573814830@example.com";
+  const password = "hunter2!";
+  await page.goto("/");
+  await page.getByRole("button", {name: "LOGIN"}).click();
+  await page.getByPlaceholder("username or email").fill(email);
+  await page.getByPlaceholder("username or email").fill(email);
+
+  // await page.getByTestId("signup-button").click();
+  // await page.getByTestId("login-email-input").fill(email);
+  // await page.getByTestId("login-password-input").fill(password);
+  // await page.getByTestId("signup-submit").click();
 });
