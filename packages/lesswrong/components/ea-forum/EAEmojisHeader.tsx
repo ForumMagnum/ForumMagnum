c4cbc027d54b6d7ecc09e63a1e10ec3081dfd27d
Ollie Etherington <ollie@etherington.xyz>
2024-05-07 19:13:15 +0300

    Add basic UI for emoji header tooltip

--- packages/lesswrong/components/ea-forum/EAEmojisHeader.tsx
+++ packages/lesswrong/components/ea-forum/EAEmojisHeader.tsx
@@ -19,19 +19,63 @@ const styles = (theme: ThemeType) => ({
     },
   },
   emoji: {
+    padding: 6,
+    userSelect: "none",
     position: "absolute",
     zIndex: 3,
-    marginLeft: -20,
-    marginTop: -20,
+    marginLeft: -28,
+    marginTop: -32,
+    fontSize: 24,
   },
   emojiTooltip: {
-    backgroundColor: `${theme.palette.panelBackground.tooltipBackground2} !important`,
+    padding: 12,
+    borderRadius: theme.borderRadius.default,
+    backgroundColor: theme.palette.dropdown.background,
+    border: `1px solid ${theme.palette.dropdown.border}`,
+    "& .SectionTitle-root": {
+      marginTop: 0,
+      paddingBottom: 4,
+    },
+  },
+  emojiTooltipContent: {
+    display: "flex",
+    flexDirection: "column",
+    gap: "12px",
   },
   emojiCursor: {
     pointerEvents: "none",
     "& .ForumIcon-root": {
       color: `${theme.palette.emojiHeader.placeholder} !important`,
     },
+  },
+  pickerButton: {
+    width: 40,
+    height: 40,
+    borderRadius: theme.borderRadius.default,
+    background: theme.palette.panelBackground.loginInput,
+    fontSize: 24,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    cursor: "pointer",
+    userSelect: "none",
+  },
+  input: {
+    minHeight: 40,
+    padding: 8,
+  },
+  textArea: {
+    minHeight: 92,
+    width: "100%",
+    resize: "none",
+  },
+  button: {
+    flexGrow: 1,
+  },
+  row: {
+    display: "flex",
+    gap: "12px",
+    width: "100%",
   },
 });
 
@@ -40,7 +84,8 @@ const isValidTarget = (e: EventTarget | null): e is HTMLDivElement =>
 
 type BannerEmoji = {
   userId: string,
-  displayName: string,
+  displayName?: string,
+  emoji?: string,
   x: number,
   y: number,
   theta: number,
@@ -49,27 +94,28 @@ type BannerEmoji = {
 const Emoji: FC<{
   emoji: BannerEmoji,
   currentUser: UsersCurrent | null,
-  removeEmoji: () => Promise<void>,
+  removeEmoji?: () => Promise<void>,
   classes: ClassesType<typeof styles>,
 }> = ({
-  emoji: {userId, displayName, x, y, theta},
+  emoji: {userId, x, y, theta, emoji},
   currentUser,
-  removeEmoji: removeEmoji,
+  removeEmoji,
   classes,
 }) => {
+  const [link, setLink] = useState("");
+  const [description, setDescription] = useState("");
   const isCurrentUser = userId === currentUser?._id;
-  const title = isCurrentUser
-    ? "You added an amoji (click to remove)"
-    : `${displayName} added a emoji`;
   const onClick = useCallback(() => {
     if (isCurrentUser) {
-      void removeEmoji();
+      void removeEmoji?.();
     }
   }, [isCurrentUser, removeEmoji]);
-  const {LWTooltip, ForumIcon} = Components;
+  const {
+    LWTooltip, ForumIcon, SectionTitle, EAButton, EAOnboardingInput,
+  } = Components;
   return (
-    <div
-      className={classNames(classes.emoji, !displayName && classes.emojiCursor)}
+    <figure
+      className={classNames(classes.emoji, !emoji && classes.emojiCursor)}
       style={{
         left: `${x * window.innerWidth}px`,
         top: `${y * EMOJIS_HEADER_HEIGHT}px`,
@@ -77,20 +123,83 @@ const Emoji: FC<{
       }}
     >
       <LWTooltip
-        title={title}
-        placement="bottom"
+        title={
+          <div className={classes.emojiTooltipContent}>
+            <div className={classes.row}>
+              <div>
+                <SectionTitle title="Emoji" />
+                <div className={classes.pickerButton}>{emoji || "👍"}</div>
+              </div>
+              <div>
+                <SectionTitle title="Link to" />
+                <EAOnboardingInput
+                  value={link}
+                  setValue={setLink}
+                  placeholder="https://example.com"
+                  className={classes.input}
+                />
+              </div>
+            </div>
+            <div>
+              <SectionTitle title="The good news" />
+              <EAOnboardingInput
+                value={description}
+                setValue={setDescription}
+                placeholder="Show when you hover the emoji"
+                As="textarea"
+                rows={3}
+                className={classNames(classes.input, classes.textArea)}
+              />
+            </div>
+            <div className={classes.row}>
+              <EAButton style="grey" className={classes.button}>
+                Cancel
+              </EAButton>
+              <EAButton className={classes.button}>
+                Add good news
+              </EAButton>
+            </div>
+          </div>
+        }
+        placement="bottom-start"
+        clickable
         popperClassName={classes.emojiTooltip}
       >
-        <ForumIcon icon="AddEmoji" onClick={onClick} />
+        {emoji || <ForumIcon icon="AddEmoji" onClick={onClick} />}
       </LWTooltip>
-    </div>
+    </figure>
+  );
+}
+
+type Point = {x: number, y: number};
+
+const EmojiPlaceholder: FC<{
+  hoverPos: Point,
+  classes: ClassesType<typeof styles>,
+}> = ({hoverPos: pos, classes}) => {
+  // const [insertPos, setInsertPos] = useState<Point | null>(null);
+
+  // if (insertPos) {
+    // return (
+      // <div>
+        // K
+      // </div>
+    // );
+  // }
+
+  return (
+    <Emoji
+      emoji={{displayName: "", userId: "", theta: 0, emoji: "", ...pos}}
+      currentUser={null}
+      classes={classes}
+    />
   );
 }
 
 export const EAEmojisHeader = ({classes}: {
   classes: ClassesType<typeof styles>,
 }) => {
-  const [hoverPos, setHoverPos] = useState<{x: number, y: number} | null>(null);
+  const [hoverPos, setHoverPos] = useState<Point | null>(null);
   const ref = useRef<HTMLDivElement>(null);
 
   const normalizeCoords = useCallback((clientX: number, clientY: number) => {
@@ -126,10 +235,9 @@ export const EAEmojisHeader = ({classes}: {
   }, []);
 
   const onClick = useCallback(() => {
-    console.log("click");
   }, []);
 
-  const removeEmoji = useCallback(async () => {
+  // const removeEmoji = useCallback(async () => {
     // const result = await rawRemoveHeart({
       // variables: {
         // electionName: eaGivingSeason23ElectionName,
@@ -141,7 +249,7 @@ export const EAEmojisHeader = ({classes}: {
     // }
     // await refetch();
   // }, [rawRemoveHeart, refetch]);
-  }, []);
+  // }, []);
 
   return (
     <ForumNoSSR>
@@ -154,13 +262,20 @@ export const EAEmojisHeader = ({classes}: {
           ref={ref}
         >
           {hoverPos &&
-            <Emoji
-              emoji={{displayName: "", userId: "", theta: 0, ...hoverPos}}
-              currentUser={null}
-              removeEmoji={removeEmoji}
-              classes={classes}
-            />
+            <EmojiPlaceholder hoverPos={hoverPos} classes={classes} />
           }
+          <Emoji
+            emoji={{
+              displayName: "",
+              userId: "",
+              theta: 0,
+              x: 0.5,
+              y: 0.5,
+              emoji: "👍",
+            }}
+            currentUser={null}
+            classes={classes}
+          />
         </div>
       </AnalyticsContext>
     </ForumNoSSR>
@@ -170,7 +285,7 @@ export const EAEmojisHeader = ({classes}: {
 const EAEmojisHeaderComponent = registerComponent(
   "EAEmojisHeader",
   EAEmojisHeader,
-  {styles},
+  {styles, stylePriority: 2},
 );
 
 declare global {
