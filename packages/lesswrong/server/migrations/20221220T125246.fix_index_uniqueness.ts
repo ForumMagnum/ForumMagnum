2f0b1b700d0397859e45f9d3d8db116461b0801d
Will Howard <w.howard256+github@gmail.com>
2024-05-10 16:50:09 +0100

    comment out troublesome migration

--- packages/lesswrong/server/migrations/20221220T125246.fix_index_uniqueness.ts
+++ packages/lesswrong/server/migrations/20221220T125246.fix_index_uniqueness.ts
@@ -2,19 +2,20 @@ import { getAllCollections } from "../../lib/vulcan-lib/getCollection";
 import { createIndex, dropIndex } from "./meta/utils";
 
 export const up = async ({db}: MigrationContext) => {
-  for (const collection of getAllCollections()) {
-    const table = collection.getTable();
-    for (const index of table.getIndexes()) {
-      if (index.isUnique() || index.getPartialFilterExpression()) {
-        try {
-          await dropIndex(db, collection, index);
-        } catch {
-          // eslint-disable-next-line no-console
-          console.warn("Index doesn't exist:", index);
-        }
-        // allowConcurrent = false because CONCURRENTLY isn't allowed inside a transaction
-        await createIndex(db, collection, index, true, false);
-      }
-    }
-  }
+  // This migration is no longer needed for new databases, and messes with creating indexes concurrently
+  // for (const collection of getAllCollections()) {
+  //   const table = collection.getTable();
+  //   for (const index of table.getIndexes()) {
+  //     if (index.isUnique() || index.getPartialFilterExpression()) {
+  //       try {
+  //         await dropIndex(db, collection, index);
+  //       } catch {
+  //         // eslint-disable-next-line no-console
+  //         console.warn("Index doesn't exist:", index);
+  //       }
+  //       // allowConcurrent = false because CONCURRENTLY isn't allowed inside a transaction
+  //       await createIndex(db, collection, index, true, false);
+  //     }
+  //   }
+  // }
 }
