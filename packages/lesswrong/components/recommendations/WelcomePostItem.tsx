45edf6e5587a495503cac28520aeb4f5a3648f4e
James Babcock <jimrandomh@gmail.com>
2024-05-12 12:37:18 -0700

    Use the correct fragment for WelcomePostItem

--- packages/lesswrong/components/recommendations/WelcomePostItem.tsx
+++ packages/lesswrong/components/recommendations/WelcomePostItem.tsx
@@ -4,13 +4,17 @@ import { aboutPostIdSetting } from "../../lib/routes";
 import { useCurrentUser } from "../common/withUser";
 import { useItemsRead } from "../hooks/useRecordPostView";
 import moment from "moment";
-import { useDisplayedPost } from "../posts/usePost";
+import { useSingle } from "../../lib/crud/withSingle";
 
 const WelcomePostItem = () => {
   const currentUser = useCurrentUser();
   const welcomePostId = aboutPostIdSetting.get();
 
-  const { document: post } = useDisplayedPost(welcomePostId, null);
+  const { document: post } = useSingle({
+    documentId: welcomePostId,
+    collectionName: "Posts",
+    fragmentName: "PostsListWithVotes",
+  });
 
   const { postsRead } = useItemsRead();
   const isRead = post && !!(
