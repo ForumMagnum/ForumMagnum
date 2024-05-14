33ae6b50cce70245e2604de05b923858acc6b2ce
Ollie Etherington <ollie@etherington.xyz>
2024-05-14 21:17:02 +0300

    Clean up playwright projects

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,12 +18,54 @@ const getWebServers = () => {
   webServers.push({
     command: "yarn start-playwright",
     url: "http://localhost:3456",
-    reuseExistingServer: !process.env.CI,
+    reuseExistingServer: true,
     stdout: "ignore",
     stderr: "pipe",
   });
 
   return webServers;
+}
+
+const getProjects = () => {
+  let projects = [
+    {
+      name: "chromium",
+      use: {...devices["Desktop Chrome"]},
+    },
+  ];
+  if (process.env.CI) {
+    projects = projects.concat([
+      {
+        name: "firefox",
+        use: {...devices["Desktop Firefox"]},
+      },
+      {
+        name: "webkit",
+        use: {...devices["Desktop Safari"]},
+      },
+
+      /* Test against mobile viewports. */
+      // {
+      //   name: "Mobile Chrome",
+      //   use: {...devices["Pixel 5"]},
+      // },
+      // {
+      //   name: "Mobile Safari",
+      //   use: {...devices["iPhone 12"]},
+      // },
+
+      /* Test against branded browsers. */
+      // {
+      //   name: "Microsoft Edge",
+      //   use: {...devices["Desktop Edge"], channel: "msedge"},
+      // },
+      // {
+      //   name: "Google Chrome",
+      //   use: {...devices["Desktop Chrome"], channel: "chrome"},
+      // },
+    ]);
+  }
+  return projects;
 }
 
 /**
@@ -57,44 +99,6 @@ export default defineConfig({
      */
     trace: "on-first-retry",
   },
-
-  /* Configure projects for major browsers */
-  projects: [
-    {
-      name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
-    },
-
-    {
-      name: "firefox",
-      use: { ...devices["Desktop Firefox"] },
-    },
-
-    {
-      name: "webkit",
-      use: { ...devices["Desktop Safari"] },
-    },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: "Mobile Chrome",
-    //   use: { ...devices["Pixel 5"] },
-    // },
-    // {
-    //   name: "Mobile Safari",
-    //   use: { ...devices["iPhone 12"] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: "Microsoft Edge",
-    //   use: { ...devices["Desktop Edge"], channel: "msedge" },
-    // },
-    // {
-    //   name: "Google Chrome",
-    //   use: { ...devices["Desktop Chrome"], channel: "chrome" },
-    // },
-  ],
-
+  projects: getProjects(),
   webServer: getWebServers(),
 });
