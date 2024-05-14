824846e09819fd6c0916e203e72f7eb5cc15aa52
Ollie Etherington <ollie@etherington.xyz>
2024-05-14 18:40:49 +0300

    Translate 'edit post' test

--- playwright/create_post.spec.ts
+++ playwright/create_post.spec.ts
@@ -6,14 +6,35 @@ test("create and edit post", async ({page, context}) => {
   await page.goto("/newPost");
 
   // Create a post with a title and body
-  await page.getByPlaceholder("Post title").fill("Test post 123");
-  await page.getByLabel("Rich Text Editor, main").fill("Test body 123");
+  const title = "Test post 123";
+  const body = "Test body 123";
+  await page.waitForTimeout(100);
+  await page.getByPlaceholder("Post title").fill(title);
+  await page.getByLabel("Rich Text Editor, main").fill(body);
   await page.getByText("Submit").click();
 
   // Submitting navigates to the post page - check our new post is there
   await page.waitForURL("/posts/**/test-post-123**");
-  await expect(page.getByText("Test post 123")).toBeVisible();
-  await expect(page.getByText("Test body 123")).toBeVisible();
+  await expect(page.getByText(title)).toBeVisible();
+  await expect(page.getByText(body)).toBeVisible();
+
+  // Click the edit button
+  await page.locator(".PostActionsButton-root").first().click();
+  await page.getByText("Edit", {exact: true}).click();
+  await page.waitForURL("/editPost**");
+
+  // Edit the post
+  const newTitle = "Edited test post";
+  const newBody = "Edited test body";
+  await page.waitForTimeout(100);
+  await page.getByPlaceholder("Post title").fill(newTitle);
+  await page.getByLabel("Rich Text Editor, main").fill(newBody);
+  await page.getByText("Publish changes").click();
+
+  // Submitting navigates to the post page - check it has our edits
+  await page.waitForURL("/posts/**/edited-test-post**");
+  await expect(page.getByText(newTitle)).toBeVisible();
+  await expect(page.getByText(newBody)).toBeVisible();
 });
 
 test("can create 5 posts per day, but not 6", async ({page, context}) => {
