4cd2a8006dbb5316bbe51441b947b547f9b17e33
Rafe Kennedy <dear.rafe@gmail.com>
2024-06-06 13:02:20 -0700

    Add console.warn

--- packages/lesswrong/server/manualMigrations/2024-06-05-rewriteOldReviewBotComments.ts
+++ packages/lesswrong/server/manualMigrations/2024-06-05-rewriteOldReviewBotComments.ts
@@ -26,7 +26,11 @@ registerMigration({
       if (!comment.contents?.html) throw new Error("oh no!", String(comment.contents));
       const manifoldUrl = comment.contents.html.match(/https:\/\/manifold.markets\/LessWrong\/([A-Za-z\d-])+/)?.[0];
       const year = comment.contents.html.match(`but will be at the end of (\\d+)`)?.[1];
-      if (!manifoldUrl || !year) continue
+      if (!manifoldUrl || !year) {
+        // eslint-disable-next-line no-console
+        console.warn("Skipping comment", comment._id, "because we didn't find a manifold URL or year", comment.contents.html)
+        continue
+      }
 
       const newContent = `<p>The <a href="https://www.lesswrong.com/bestoflesswrong">LessWrong Review</a> runs every year to select the posts that have most stood the test of time. This post is not yet eligible for review, but will be at the end of ${year}. The top fifty or so posts are featured prominently on the site throughout the year.</p><p>Hopefully, the review is better than karma at judging enduring karma. If we have accurate prediction markets on the review results, maybe we can have better incentives on LessWrong today. <a href=${manifoldUrl}>Will this post make the top fifty?</a></p>`;
 
