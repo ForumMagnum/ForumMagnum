20a1826cc67cb89a618cf01075d04509b93d3ac4
James Babcock <jimrandomh@gmail.com>
2024-06-13 13:10:27 -0700

    Lint

--- packages/lesswrong/lib/crud/useQuery.ts
+++ packages/lesswrong/lib/crud/useQuery.ts
@@ -1,15 +1,17 @@
-import { useQuery as useQueryApollo, useSuspenseQuery } from "@apollo/client";
+import { useQuery as useQueryApollo, useSuspenseQuery, WatchQueryFetchPolicy } from "@apollo/client";
 import type { SuspenseQueryHookFetchPolicy, FetchPolicy } from "@apollo/client";
 
 type UseQueryOptions = {
-  fetchPolicy: SuspenseQueryHookFetchPolicy,
+  fetchPolicy?: SuspenseQueryHookFetchPolicy & WatchQueryFetchPolicy,
   ssr: boolean,
 };
 
 export function wrappedUseQuery(query: any, options: UseQueryOptions) {
   if (bundleIsServer) {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
     return useSuspenseQuery(query, options);
   } else {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
     return useQueryApollo(query, options);
   }
 }
