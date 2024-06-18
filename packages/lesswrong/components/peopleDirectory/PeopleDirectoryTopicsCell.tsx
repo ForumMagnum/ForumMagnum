e9c344b4fe91624b419d907d485d33c5cd73691e
Ollie Etherington <ollie@etherington.xyz>
2024-06-18 16:52:39 +0100

    Add '+ n more' message to people directory tags list

--- packages/lesswrong/components/peopleDirectory/PeopleDirectoryTopicsCell.tsx
+++ packages/lesswrong/components/peopleDirectory/PeopleDirectoryTopicsCell.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { Components, registerComponent } from "../../lib/vulcan-lib";
 import { tagStyle, smallTagTextStyle, coreTagStyle } from "../tagging/FooterTag";
+import { textCellStyles } from "./PeopleDirectoryTextCell";
 import { InteractionWrapper } from "../common/useClickableCell";
 import { tagGetUrl } from "@/lib/collections/tags/helpers";
 
@@ -12,32 +13,81 @@ const styles = (theme: ThemeType) => ({
     overflow: "hidden",
     maxHeight: 42,
   },
+  tagWrapper: {},
   tag: {
     ...tagStyle(theme),
     ...smallTagTextStyle(theme),
     ...coreTagStyle(theme),
   },
+  more: {
+    ...textCellStyles(theme),
+    color: theme.palette.grey[600],
+    marginTop: 2,
+    marginLeft: 6,
+  },
 });
+
+const TAG_COUNT = 3;
 
 export const PeopleDirectoryTopicsCell = ({user, classes}: {
   user: SearchUser,
   classes: ClassesType<typeof styles>,
 }) => {
+  const ref = useRef<HTMLDivElement | null>(null);
+
+  // The kind of wrapping we want here can't be done with pure CSS unfortunately
+  // since we want to show a "+ n more" message at the end after two lines of
+  // tags. We do this by iterating over the tags after the initial render and
+  // removing as many as needed until the "+ n more" message is visible.
+  useEffect(() => {
+    const container = ref.current;
+    if (!container) {
+      return;
+    }
+
+    const tags: HTMLElement[] = Array.from(
+      container.querySelectorAll(`.${classes.tagWrapper}`),
+    );
+    for (const tag of tags) {
+      tag.style.display = "block";
+    }
+
+    const more: HTMLElement | null = container.querySelector(`.${classes.more}`);
+    if (!tags.length || !more) {
+      return;
+    }
+
+    const offsets = Array.from(
+      new Set(tags.map((t) => t.getBoundingClientRect().top)),
+    ).sort();
+    if (offsets.length < 2) {
+      return;
+    }
+    while (more.getBoundingClientRect().top > offsets[1] + 15) {
+      const tag = tags.pop();
+      if (tag) {
+        tag.remove();
+      } else {
+        break;
+      }
+    }
+  }, [classes]);
+
   const {TagsTooltip} = Components;
   return (
-    <div className={classes.root}>
-      {user.tagNames?.slice(0, 3).map((tagName) => {
+    <div className={classes.root} ref={ref}>
+      {user.tagNames?.slice(0, TAG_COUNT).map((tagName) => {
         const slug = user.tags?.find(({name}) => name === tagName)?.slug;
         if (!slug) {
           return null;
         }
         return (
-          <InteractionWrapper href={tagGetUrl({slug}, {from: "people_directory"})}>
-            <TagsTooltip
-              tagSlug={slug}
-              hideRelatedTags
-              key={tagName}
-            >
+          <InteractionWrapper
+            key={tagName}
+            href={tagGetUrl({slug}, {from: "people_directory"})}
+            className={classes.tagWrapper}
+          >
+            <TagsTooltip tagSlug={slug} hideRelatedTags>
               <div className={classes.tag}>
                 {tagName}
               </div>
@@ -45,6 +95,11 @@ export const PeopleDirectoryTopicsCell = ({user, classes}: {
           </InteractionWrapper>
         );
       })}
+      {(user.tagNames?.length ?? 0) > TAG_COUNT &&
+        <div className={classes.more}>
+          + {user.tagNames!.length - TAG_COUNT} more
+        </div>
+      }
     </div>
   );
 }
