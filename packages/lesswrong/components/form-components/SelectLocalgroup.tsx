42441a9d7bc8ebf352c93b2e3e4167b924c8dea4
Ollie Etherington <ollie@etherington.xyz>
2024-06-19 12:16:55 +0100

    Better typing for `SelectLocalgroup` props

--- packages/lesswrong/components/form-components/SelectLocalgroup.tsx
+++ packages/lesswrong/components/form-components/SelectLocalgroup.tsx
@@ -2,14 +2,23 @@ import React from 'react';
 import { registerComponent, Components } from '../../lib/vulcan-lib';
 import { useMulti } from '../../lib/crud/withMulti';
 
+type SelectLocalgroupProps = {
+  useDocumentAsUser?: boolean,
+  variant?: "default" | "grey",
+  separator?: string,
+} & (
+  (FormComponentProps<string[]> & {multiselect: true}) |
+  (FormComponentProps<string> & {multiselect?: false})
+);
+
 /**
  * A form input for selecting a localgroup -
  * the options are a list of groups for which the current user is an organizer,
  * or all groups if the user is an admin.
  */
-const SelectLocalgroup = (props: any) => {
+const SelectLocalgroup = (props: SelectLocalgroupProps) => {
   const { MenuItem } = Components;
-  
+
   // Default to currentUser, but use props.document if necessary
   // (ex. you want to be able to select groups for another user).
   // Note that props.document doesn't have isAdmin, so using this option will not show all groups for admins.
@@ -25,7 +34,7 @@ const SelectLocalgroup = (props: any) => {
     },
     skip: !user
   });
-  
+
   if (props.multiselect) {
     const options = groups?.map(group => {
       return {value: group._id, label: group.name}
@@ -38,7 +47,12 @@ const SelectLocalgroup = (props: any) => {
       {group.name}
     </MenuItem>
   })
-  return <Components.MuiTextField select {...props} {...!selectOptions?.length ? {disabled: true} : {}}>
+  const {variant, separator, ...textFieldProps} = props;
+  return <Components.MuiTextField
+    select
+    {...textFieldProps}
+    {...!selectOptions?.length ? {disabled: true} : {}}
+  >
     {selectOptions || []}
   </Components.MuiTextField>
 }
