d7fd4ce4f072e2d146d40e5883e22ae8aef3a3af
Robert Mushkatblat <bobert.mushky@gmail.com>
2024-06-21 11:35:34 -0700

    remove isFuture filter and add comment

--- packages/lesswrong/server/callbacks/postCallbacks.ts
+++ packages/lesswrong/server/callbacks/postCallbacks.ts
@@ -605,7 +605,12 @@ getCollectionHooks("Posts").updateAfter.add(async (post: DbPost, props: UpdateCa
 /* Recombee callbacks */
 
 function isNonRecommendablePost(post: DbPost): boolean {
-  return post.shortform || post.unlisted || post.rejected || post.isEvent || !!post.groupId || post.isFuture || post.disableRecommendation || post.status !== 2;
+  // We explicitly don't check `isFuture` here, because the cron job that "publishes" those posts does a raw update
+  // So it won't trigger any of the callbacks, and if we exclude those posts they'll never get recommended
+  // `Posts.checkAccess` already filters out posts with `isFuture` unless you're a mod or otherwise own the post
+  // So we're not really in any danger of showing those posts to regular users
+  // TODO: figure out how to handle this more gracefully
+  return post.shortform || post.unlisted || post.rejected || post.isEvent || !!post.groupId || post.disableRecommendation || post.status !== 2;
 }
 
 postPublishedCallback.add((post, context) => {
