1edc0469f579e0790650d8544a76f1c7cb24b729
Ollie Etherington <ollie@etherington.xyz>
2024-07-01 16:16:56 +0100

    Better aligment of more posts count

--- packages/lesswrong/components/peopleDirectory/PeopleDirectoryPostsCell.tsx
+++ packages/lesswrong/components/peopleDirectory/PeopleDirectoryPostsCell.tsx
@@ -29,7 +29,7 @@ const styles = (theme: ThemeType) => ({
   },
   more: {
     position: "absolute",
-    bottom: 0,
+    bottom: 2,
     right: 0,
     paddingLeft: FADE_SIZE,
     background: `linear-gradient(to right, transparent, var(${ROW_BACKGROUND_VAR}) ${FADE_SIZE}px)`,
