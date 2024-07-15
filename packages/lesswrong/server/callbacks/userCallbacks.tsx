b9dd3e24c1439ebd2a6fbb6d1e297cbdbeb763b8
Ollie Etherington <ollie@etherington.xyz>
2024-07-15 11:09:33 +0100

    Forum gate shortform title updates

--- packages/lesswrong/server/callbacks/userCallbacks.tsx
+++ packages/lesswrong/server/callbacks/userCallbacks.tsx
@@ -11,7 +11,7 @@ import { getCollectionHooks, UpdateCallbackProperties } from '../mutationCallbac
 import { voteCallbacks, VoteDocTuple } from '../../lib/voting/vote';
 import { encodeIntlError } from '../../lib/vulcan-lib/utils';
 import { sendVerificationEmail } from "../vulcan-lib/apollo-server/authentication";
-import { isEAForum, isLW, verifyEmailsSetting } from "../../lib/instanceSettings";
+import { isEAForum, isLW, isLWorAF, verifyEmailsSetting } from "../../lib/instanceSettings";
 import { mailchimpEAForumListIdSetting, mailchimpForumDigestListIdSetting, recombeeEnabledSetting } from "../../lib/publicSettings";
 import { mailchimpAPIKeySetting } from "../../server/serverSettings";
 import {userGetLocation, getUserEmail, userShortformPostTitle} from "../../lib/collections/users/helpers";
@@ -463,7 +463,7 @@ getCollectionHooks("Users").updateBefore.add(async function UpdateDisplayName(da
     if (await Users.findOne({displayName: data.displayName})) {
       throw new Error("This display name is already taken");
     }
-    if (data.shortformFeedId) {
+    if (data.shortformFeedId && !isLWorAF) {
       void updateMutator({
         collection: Posts,
         documentId: data.shortformFeedId,
