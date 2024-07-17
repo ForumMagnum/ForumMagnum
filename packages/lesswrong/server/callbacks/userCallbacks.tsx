fad3b6c3db059b68459d7194beaa01a7a1bc1623
Will Howard <w.howard256+github@gmail.com>
2024-07-17 22:50:31 +0100

    fix cases in userCallbacks

--- packages/lesswrong/server/callbacks/userCallbacks.tsx
+++ packages/lesswrong/server/callbacks/userCallbacks.tsx
@@ -77,7 +77,7 @@ getCollectionHooks("Users").editSync.add(function maybeSendVerificationEmail (mo
       && (!user.whenConfirmationEmailSent
           || user.whenConfirmationEmailSent.getTime() !== modifier.$set.whenConfirmationEmailSent.getTime()))
   {
-    // FIXME should skip on EAF
+    // Note: This is still gated by verifyEmailsSetting because `canUpdate` of whenConfirmationEmailSent is controlled by verifyEmailsSetting
     void sendVerificationEmail(user);
   }
 });
@@ -154,14 +154,7 @@ getCollectionHooks("Users").editSync.add(function clearKarmaChangeBatchOnSetting
 });
 
 getCollectionHooks("Users").newAsync.add(async function subscribeOnSignup (user: DbUser) {
-  // Regardless of the config setting, try to confirm the user's email address
-  // (But not in unit-test contexts, where this function is unavailable and sending
-  // emails doesn't make sense.)
-  // FIXME use sendVerificationEmailConditional
-  if (!isAnyTest && verifyEmailsSetting.get()) {
-    void sendVerificationEmail(user);
-    await bellNotifyEmailVerificationRequired(user);
-  }
+  await sendVerificationEmailConditional(user);
 });
 
 getCollectionHooks("Users").editAsync.add(async function handleSetShortformPost (newUser: DbUser, oldUser: DbUser) {
