891bd7ffd61972777da3359aaa9ad03c62c6306e
Will Howard <w.howard256+github@gmail.com>
2024-07-30 13:40:19 +0100

    Add styling to error messages

--- packages/lesswrong/components/messaging/MessageUser.tsx
+++ packages/lesswrong/components/messaging/MessageUser.tsx
@@ -5,7 +5,17 @@ import { useCurrentUser } from "../common/withUser";
 import { useSingle } from "@/lib/crud/withSingle";
 import { useInitiateConversation } from "../hooks/useInitiateConversation";
 
-const MessageUserInner = ({ user }: { user: UsersMinimumInfo }) => {
+const styles = (theme: ThemeType) => ({
+  error: {
+    fontFamily: theme.palette.fonts.sansSerifStack,
+    fontSize: 16,
+    fontWeight: 450,
+    marginTop: 16,
+    padding: 16
+  },
+});
+
+const MessageUserInner = ({ user, classes }: { user: UsersMinimumInfo; classes: ClassesType<typeof styles> }) => {
   const { Loading, PermanentRedirect, SingleColumnSection } = Components;
 
   const { conversation, conversationLoading } = useInitiateConversation({
@@ -13,14 +23,18 @@ const MessageUserInner = ({ user }: { user: UsersMinimumInfo }) => {
   });
 
   if (!conversation) {
-    return conversationLoading ? <Loading /> : <SingleColumnSection>Failed to create conversation: Unknown error.</SingleColumnSection>;
+    return conversationLoading ? (
+      <Loading />
+    ) : (
+      <SingleColumnSection className={classes.error}>Failed to create conversation: Unknown error.</SingleColumnSection>
+    );
   }
 
   const url = `/inbox/${conversation._id}?from=magic_link`;
   return <PermanentRedirect url={url} status={302} />;
 };
 
-const MessageUser = () => {
+const MessageUser = ({ classes }: { classes: ClassesType<typeof styles> }) => {
   const currentUser = useCurrentUser();
   const { params } = useLocation();
   const { Loading, SingleColumnSection } = Components;
@@ -33,21 +47,31 @@ const MessageUser = () => {
   });
 
   if (!currentUser) {
-    return <div>Log in to access private messages.</div>;
+    return <SingleColumnSection className={classes.error}>Log in to access private messages.</SingleColumnSection>;
   }
 
   if (!user) {
-    return loading ? <Loading /> : <SingleColumnSection>Failed to create conversation: User could not be found.</SingleColumnSection>;
+    return loading ? (
+      <Loading />
+    ) : (
+      <SingleColumnSection className={classes.error}>
+        Failed to create conversation: User could not be found.
+      </SingleColumnSection>
+    );
   }
 
   if (user._id === currentUser._id) {
-    return <SingleColumnSection>Failed to create conversation: You cannot create a converstaion with yourself.</SingleColumnSection>;
+    return (
+      <SingleColumnSection className={classes.error}>
+        Failed to create conversation: You cannot create a converstaion with yourself.
+      </SingleColumnSection>
+    );
   }
 
-  return <MessageUserInner user={user} />
+  return <MessageUserInner user={user} classes={classes} />;
 };
 
-const MessageUserComponent = registerComponent("MessageUser", MessageUser);
+const MessageUserComponent = registerComponent("MessageUser", MessageUser, { styles });
 
 declare global {
   interface ComponentTypes {
