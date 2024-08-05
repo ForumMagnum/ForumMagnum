2c330bce4e7973e26efea71e0002e8c39e82d1ce
Ollie Etherington <ollie@etherington.xyz>
2024-08-05 15:59:20 +0100

    Add special cases for notification type names

--- packages/lesswrong/components/notifications/NotificationsPopoverNotification.tsx
+++ packages/lesswrong/components/notifications/NotificationsPopoverNotification.tsx
@@ -73,8 +73,19 @@ const styles = (theme: ThemeType) => ({
 });
 
 const formatNotificationType = (type: string): string => {
-  const words = type.replace(/([A-Z])/g, " $1");
-  return words.charAt(0).toUpperCase() + words.slice(1).toLowerCase();
+  switch (type) {
+    case "newRSVP":
+      return "New RSVP";
+    case "newShortform":
+      return "New Quick take";
+    case "coauthorRequestNotification":
+      return "Co-author requested";
+    case "coauthorAcceptNotification":
+      return "Co-author accepted";
+    default:
+      const words = type.replace(/([A-Z])/g, " $1");
+      return words.charAt(0).toUpperCase() + words.slice(1).toLowerCase();
+  }
 }
 
 const NotificationsPopoverNotification = ({notification, onClick, classes}: {
