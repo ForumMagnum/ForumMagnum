636eba4c90e83a1cd3fb5866dfa7d1f865e1d326
Robert Mushkatblat <bobert.mushky@gmail.com>
2024-08-15 17:22:15 -0700

    Temporarily strip out the contents field from recent discussion results before the field is dropped
    Co-authored-by: jimrandomh <jim@jimrandomh.org>

--- packages/lesswrong/server/utils/feedUtil.ts
+++ packages/lesswrong/server/utils/feedUtil.ts
@@ -2,6 +2,7 @@ import _ from 'underscore';
 import { addGraphQLResolvers, addGraphQLQuery, addGraphQLSchema } from '../../lib/vulcan-lib/graphql';
 import { accessFilterMultiple } from '../../lib/utils/schemaUtils';
 import { getDefaultViewSelector, mergeSelectors, replaceSpecialFieldSelectors } from '../../lib/utils/viewUtils';
+import { isLWorAF } from '@/lib/instanceSettings';
 
 type FeedSubquery<ResultType extends DbObject, SortKeyType> = {
   type: string,
@@ -187,9 +188,22 @@ export async function mergeFeedQueries<SortKeyType extends number | Date>({limit
   // is applied), and get its sortKey to use as the page cutoff
   const nonNumericallyPositionedResults = _.filter(withLimitApplied, r => !_.some(numericallyPositionedResults, r2=>r===r2)) as Sortable<SortKeyType>[];
   const nextCutoff = (nonNumericallyPositionedResults.length>0) ? nonNumericallyPositionedResults[nonNumericallyPositionedResults.length-1].sortKey : null;
+
+
+  // Temporary hack to get around issues before we drop the contents field after denormalization
+  // TODO: remove after dropping the field
+  const munged = withLimitApplied.map((item) => {
+    const dbObject = (item as any)[(item as any).type];
+    if (isLWorAF && 'postedAt' in dbObject) {
+      const { contents, ...remainder } = (item as any)[(item as any).type];
+      return { ...item, [(item as any).type]: remainder };  
+    } else {
+      return item;
+    }
+  });
   
   return {
-    results: withLimitApplied,
+    results: munged,
     cutoff: nextCutoff,
     endOffset: (offset||0)+withLimitApplied.length
   };
