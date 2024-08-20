d93077f96c960bbab6490b31e815b4fc52e4a092
James Babcock <jimrandomh@gmail.com>
2024-08-20 11:35:47 -0700

    Re-add migration for disable-sidenotes flag (was disabled in a merge conflict)

--- packages/lesswrong/server/migrations/20240820T183233.sidenotesFlag.ts
+++ packages/lesswrong/server/migrations/20240820T183233.sidenotesFlag.ts
@@ -1,22 +1,22 @@
 /**
- * Generated on 2024-08-01T07:04:11.104Z by `yarn makemigrations`
+ * Generated on 2024-08-20T18:32:33.397Z by `yarn makemigrations`
  * The following schema changes were detected:
  * -------------------------------------------
  * diff --git a/Users/jbabcock/repositories/Lesserwrong/LessWrong2/schema/accepted_schema.sql b/Users/jbabcock/repositories/Lesserwrong/LessWrong2/schema/schema_to_accept.sql
- * index 93be302313..a0789e792e 100644
+ * index 4472b6d717..145d7f7c31 100644
  * --- a/Users/jbabcock/repositories/Lesserwrong/LessWrong2/schema/accepted_schema.sql
  * +++ b/Users/jbabcock/repositories/Lesserwrong/LessWrong2/schema/schema_to_accept.sql
  * @@ -4,5 +4,3 @@
  *  --
- * --- Overall schema hash: a2faca5a61a921ed5828a0be4fb26461
+ * --- Overall schema hash: 5525ab08d75613c64270cbb15da2ae94
  * -
- * --- Accepted on 2024-07-04T13:57:16.000Z by 20240704T135716.add_permanentDeletionRequestedAt.ts
- * +-- Overall schema hash: a930ad918f310c7a443ceb43525dfdd1
+ * --- Accepted on 2024-08-19T09:25:36.000Z by 20240819T092536.add_userSurveyEmailSentAt_to_Users.ts
+ * +-- Overall schema hash: dc66ac034a766a7dd21afb2e072073a3
  *  
  * @@ -1392,3 +1390,3 @@ CREATE INDEX IF NOT EXISTS "idx_PostViews_windowStart" ON "PostViews" USING btre
  *  
- * --- Table "Posts", hash f69f78a31845fa492d917bc3762dad7a
- * +-- Table "Posts", hash d4d79f955c7b9d530d2d76d899bc71c3
+ * --- Table "Posts", hash 844cb570f631ddd17ba43c82ff31b266
+ * +-- Table "Posts", hash 0aca5526a61ad46c3ab55bdc71ffb879
  *  CREATE TABLE "Posts" (
  * @@ -1525,2 +1523,3 @@ CREATE TABLE "Posts" (
  *    "sideCommentVisibility" TEXT,
@@ -31,7 +31,7 @@
  * - [ ] Uncomment `acceptsSchemaHash` below
  * - [ ] Run `yarn acceptmigrations` to update the accepted schema hash (running makemigrations again will also do this)
  */
-export const acceptsSchemaHash = "a930ad918f310c7a443ceb43525dfdd1";
+export const acceptsSchemaHash = "dc66ac034a766a7dd21afb2e072073a3";
 
 import { Posts } from "@/lib/collections/posts";
 import { addField } from "./meta/utils"
@@ -41,5 +41,4 @@ export const up = async ({db}: MigrationContext) => {
 }
 
 export const down = async ({db}: MigrationContext) => {
-  // TODO, not required
 }
