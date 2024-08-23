fc86254787a4d7ec6f5f7f0f04a3e4103307ed78
Raemon <raemon777@gmail.com>
2024-08-22 17:39:15 -0700

    fix broken views file

--- packages/lesswrong/lib/collections/comments/views.ts
+++ packages/lesswrong/lib/collections/comments/views.ts
@@ -9,6 +9,10 @@ import { EA_FORUM_COMMUNITY_TOPIC_ID } from '../tags/collection';
 import pick from 'lodash/pick';
 import { TupleSet, UnionOf } from '@/lib/utils/typeGuardUtils';
 
+const COMMENT_SORTING_MODES = new TupleSet([ 
+  "top", "groupByPost", "new", "newest", "old", "oldest", "magic", "recentComments", "recentDiscussion"
+] as const);
+
 declare global {
   interface CommentsViewTerms extends ViewTermsBase {
     view?: CommentsViewName,
@@ -29,23 +33,21 @@ declare global {
     profileTagIds?: string[],
     shortformFrontpage?: boolean,
     showCommunity?: boolean,
-}
+  }
   
-/**
- * Comment sorting mode, a string which gets translated into a mongodb sort
- * order. Not every mode is shown in the UI in every context. Corresponds to
- * `sortings` (below).
- *
- * new/newest, old/oldest, and recentComments/recentDiscussion are synonyms.
- * In past versions, different subsets of these depending on whether you were
- * using an answers view, a subforum view, or something else.
- */
-const COMMENT_SORTING_MODES = new TupleSet([ 
-  "top", "groupByPost", "new", "newest", "old", "oldest", "magic", "recentComments", "recentDiscussion"
-  ] as const);
-export type CommentSortingMode = UnionOf<typeof COMMENT_SORTING_MODES>;
+  /**
+   * Comment sorting mode, a string which gets translated into a mongodb sort
+   * order. Not every mode is shown in the UI in every context. Corresponds to
+   * `sortings` (below).
+   *
+   * new/newest, old/oldest, and recentComments/recentDiscussion are synonyms.
+   * In past versions, different subsets of these depending on whether you were
+   * using an answers view, a subforum view, or something else.
+   */
+  export type CommentSortingMode = UnionOf<typeof COMMENT_SORTING_MODES>;
+}
 
-export function isCommentSortingMode(value: string): value is CommentSortingMode {
+export const isCommentSortingMode = (value: string): value is CommentSortingMode => {
   return COMMENT_SORTING_MODES.has(value);
 }
 
