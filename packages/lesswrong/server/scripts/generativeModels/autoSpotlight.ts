7ad9121d4671361e2e7a7b3381677adad838d9c8
Raemon <raemon777@gmail.com>
2024-08-27 17:27:28 -0700

    WIP review winner art

--- packages/lesswrong/server/scripts/generativeModels/autoSpotlight.ts
+++ packages/lesswrong/server/scripts/generativeModels/autoSpotlight.ts
@@ -1,29 +1,8 @@
-import Tags from "../../../lib/collections/tags/collection";
-import { anthropicApiKey } from "../../../lib/instanceSettings";
 import { Globals, createAdminContext, createMutator } from "../../vulcan-lib";
-
-import Anthropic from '@anthropic-ai/sdk';
 import Spotlights from "../../../lib/collections/spotlights/collection";
 import { fetchFragment } from "../../fetchFragment";
-import ReviewWinners from "@/lib/collections/reviewWinners/collection";
-import { Posts } from "@/lib/collections/posts";
-import Revisions from "@/lib/collections/revisions/collection";
 import { getAnthropicPromptCachingClientOrThrow } from "@/server/languageModels/anthropicClient";
-import fs from 'fs';
-import path from 'path';
-
-async function queryClaude(prompt: string) {
-  const client = getAnthropicPromptCachingClientOrThrow()
-  return await client.messages.create({
-    system: "The assistant is in CLI simulation mode, and responds to the user's CLI commands only with the output of the command.",
-    model: "claude-3-5-sonnet-20240620",
-    max_tokens: 60,
-    messages: [{
-      role: "user", 
-      content: prompt,
-    }]
-  })
-}
+import { REVIEW_WINNER_CACHE, ReviewWinnerWithPost } from "@/lib/collections/reviewWinners/cache";
 
 async function queryClaudeJailbreak(basePrompt: string, spotlightPrompt: string) {
   const client = getAnthropicPromptCachingClientOrThrow()
@@ -51,7 +30,7 @@ async function queryClaudeJailbreak(basePrompt: string, spotlightPrompt: string)
   })
 }
 
-function createSpotlight (post: PostsPage, summary: string) {
+function createSpotlight (post: PostsWithNavigation, reviewWinner: ReviewWinnerWithPost|undefined, summary: string) {
   const context = createAdminContext();
   const postYear = post.postedAt.getFullYear()
   void createMutator({
@@ -71,7 +50,7 @@ function createSpotlight (post: PostsPage, summary: string) {
   })
 }
 
-async function getPromptInfo({testing}: {testing: boolean}): Promise<{posts: PostsPage[], spotlights: DbSpotlight[]}> {
+async function getPromptInfo(): Promise<{posts: PostsWithNavigation[], spotlights: DbSpotlight[]}> {
   const reviewWinners = await fetchFragment({
     collectionName: "ReviewWinners",
     fragmentName: "ReviewWinnerTopPostsPage",
@@ -83,17 +62,19 @@ async function getPromptInfo({testing}: {testing: boolean}): Promise<{posts: Pos
   // Assume these functions exist to fetch data from the database
   const posts = await fetchFragment({
     collectionName: "Posts",
-    fragmentName: "PostsPage",
+    fragmentName: "PostsWithNavigation",
     currentUser: null,
     selector: { _id: { $in: postIds } },
     skipFiltering: true,
   });
+  console.log("posts", posts[0])
+
   const spotlights = await Spotlights.find({ documentId: { $in: postIds }, draft: false }).fetch();
   return { posts, spotlights };
 }
 
 // get the posts that have spotlights, sorted by post length, and filter out the ones that are too short
-const getPostsForPrompt = ({posts, spotlights, log=false}: {posts: PostsPage[], spotlights: DbSpotlight[], log?: boolean}) => {
+const getPostsForPrompt = ({posts, spotlights, log=false}: {posts: PostsWithNavigation[], spotlights: DbSpotlight[], log?: boolean}) => {
   const postsWithSpotlights = posts.filter(post => spotlights.find(spotlight => spotlight.documentId === post._id))
   const postsWithSpotlightsSortedByPostLength = postsWithSpotlights.sort((a, b) => {
     return (a?.contents?.html?.length ?? 0) - (b?.contents?.html?.length ?? 0)
@@ -106,7 +87,7 @@ const getPostsForPrompt = ({posts, spotlights, log=false}: {posts: PostsPage[], 
   return filteredPosts
 }
 
-const getJailbreakPromptBase = ({posts, spotlights}: {posts: PostsPage[], spotlights: DbSpotlight[]}) => {
+const getJailbreakPromptBase = ({posts, spotlights}: {posts: PostsWithNavigation[], spotlights: DbSpotlight[]}) => {
   let prompt = `A series of essays, each followed by a short description of the essay. The style of the short description is slightly casual. They are a single paragraph, 1-3 sentences long. Try to avoid referencing the essay or author (i.e. they don't say "the author" or "in this post" or similar), just talk about the ideas.
   
   `
@@ -130,7 +111,7 @@ const getJailbreakPromptBase = ({posts, spotlights}: {posts: PostsPage[], spotli
   return prompt
 }
 
-const getSpotlightPrompt = ({post}: {post: PostsPage}) => {
+const getSpotlightPrompt = ({post}: {post: PostsWithNavigation}) => {
   return `
   Post: ${post.title}
   ---
@@ -139,38 +120,33 @@ const getSpotlightPrompt = ({post}: {post: PostsPage}) => {
   A short description of the essay, ~2 sentences, no paragraph breaks or bullet points:`
 }
 
-const getAssistantPrompt = ({posts, spotlights}: {posts: PostsPage[], spotlights: DbSpotlight[]}) => {
-
-}
-
+// TODO: figure out what the type from the response is supposed to be and fix it
+// right now it claims "text" isn't a valid type but seems like it should be
 type AnthropicMessageContent = {
   type: "text"
   text: string
 }
 
-
 async function createSpotlights() {
   console.log("Creating spotlights for review winners");
 
-  const { posts, spotlights } = await getPromptInfo({testing: false})
-
+  const { posts, spotlights } = await getPromptInfo()
+  const reviewWinners = REVIEW_WINNER_CACHE.reviewWinners
   const postsForPrompt = getPostsForPrompt({posts, spotlights})
-
-  const jailbreakPromptBase = getJailbreakPromptBase({posts: postsForPrompt, spotlights})
-  const assistantPrompt = getAssistantPrompt({posts: postsForPrompt, spotlights})
-
   const postsWithoutSpotlights = posts.filter(post => !spotlights.find(spotlight => spotlight.documentId === post._id))
 
+  const jailbreakPromptBase = getJailbreakPromptBase({posts: postsForPrompt, spotlights})
 
   for (const post of postsWithoutSpotlights.slice(0, 3)) {
-    console.log(post.postedAt.getFullYear())
+    const reviewWinner = reviewWinners.find(reviewWinner => reviewWinner._id === post._id)
     const jailbreakSummary = await queryClaudeJailbreak(jailbreakPromptBase, getSpotlightPrompt({post}))
     const summary = jailbreakSummary.content[0] as AnthropicMessageContent
-    const cleanedSummary = summary.text.replace(/---|\n/g, "")
+    const cleanedSummary = summary.text.replace(/---|\n/g, "") // it tended to generate a bunch of newlines and --- lines that we don't want
     console.log(post.title, summary.text)
-    createSpotlight(post, cleanedSummary)
+    createSpotlight(post, reviewWinner, cleanedSummary)
   }
+
+  console.log("Done creating spotlights for review winners");
 }
 
 Globals.createSpotlights = createSpotlights;
-Globals.queryClaude = queryClaude;
