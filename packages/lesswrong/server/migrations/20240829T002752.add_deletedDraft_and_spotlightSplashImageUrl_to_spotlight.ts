f8d7c01d502b845f438ffab6395d77b408167e64
Raemon <raemon777@gmail.com>
2024-08-28 17:33:26 -0700

    redo migrations

--- packages/lesswrong/server/migrations/20240829T002752.add_deletedDraft_and_spotlightSplashImageUrl_to_spotlight.ts
+++ packages/lesswrong/server/migrations/20240829T002752.add_deletedDraft_and_spotlightSplashImageUrl_to_spotlight.ts
@@ -1,27 +1,18 @@
 /**
- * Generated on 2024-08-28T01:10:15.819Z by `yarn makemigrations`
+ * Generated on 2024-08-29T00:27:52.639Z by `yarn makemigrations`
  * The following schema changes were detected:
  * -------------------------------------------
  * diff --git a/Users/raymondarnold/Documents/LessWrongSuite/ForumMagnum/schema/accepted_schema.sql b/Users/raymondarnold/Documents/LessWrongSuite/ForumMagnum/schema/schema_to_accept.sql
- * index 255a3886ea..d13db775f3 100644
+ * index 86e0696033..ab0a21bfef 100644
  * --- a/Users/raymondarnold/Documents/LessWrongSuite/ForumMagnum/schema/accepted_schema.sql
  * +++ b/Users/raymondarnold/Documents/LessWrongSuite/ForumMagnum/schema/schema_to_accept.sql
  * @@ -4,5 +4,3 @@
  *  --
- * --- Overall schema hash: e4f656b97627c2f035fdee90d49cb5dc
+ * --- Overall schema hash: 3da0f2961c017b248168aeaa90955305
  * -
- * --- Accepted on 2024-08-27T18:52:18.000Z by 20240827T185218.create_LlmConversationsAndMessages_tables.ts
- * +-- Overall schema hash: 53940b0988baa29480c0c5358b9c20cb
+ * --- Accepted on 2024-08-28T23:08:39.000Z by 20240828T230839.update_userId_type_on_LlmConversations.ts
+ * +-- Overall schema hash: fbfbc086986d64913d7474fde81eaea8
  *  
- * @@ -2566,3 +2564,3 @@ CREATE INDEX IF NOT EXISTS "idx_SplashArtCoordinates_reviewWinnerArtId_createdAt
- *  
- * --- Table "Spotlights", hash 2a5e479e518ab2873651cd8f97bfe770
- * +-- Table "Spotlights", hash a6522d8dba15bf030259a999acaacc59
- *  CREATE TABLE "Spotlights" (
- * @@ -2585,2 +2583,3 @@ CREATE TABLE "Spotlights" (
- *    "spotlightDarkImageId" TEXT,
- * +  "spotlightSplashImageUrl" TEXT,
- *    "schemaVersion" DOUBLE PRECISION NOT NULL DEFAULT 1,
  * 
  * -------------------------------------------
  * (run `git diff --no-index schema/accepted_schema.sql schema/schema_to_accept.sql` to see this more clearly)
@@ -31,15 +22,17 @@
  * - [ ] Uncomment `acceptsSchemaHash` below
  * - [ ] Run `yarn acceptmigrations` to update the accepted schema hash (running makemigrations again will also do this)
  */
-export const acceptsSchemaHash = "53940b0988baa29480c0c5358b9c20cb";
+export const acceptsSchemaHash = "fbfbc086986d64913d7474fde81eaea8";
 
 import Spotlights from "../../lib/collections/spotlights/collection"
 import { addField, dropField } from "./meta/utils"
 
 export const up = async ({db}: MigrationContext) => {
+  await addField(db, Spotlights, "deletedDraft")
   await addField(db, Spotlights, "spotlightSplashImageUrl")
 }
 
 export const down = async ({db}: MigrationContext) => {
+  await dropField(db, Spotlights, "deletedDraft");
   await dropField(db, Spotlights, "spotlightSplashImageUrl");
 }
