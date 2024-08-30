f3fc6f8e3902a59726a6a1e2dfe9260255c9709c
Raemon <raemon777@gmail.com>
2024-08-30 13:31:23 -0700

    fix lint

--- packages/lesswrong/components/spotlights/SpotlightItem.tsx
+++ packages/lesswrong/components/spotlights/SpotlightItem.tsx
@@ -394,7 +394,7 @@ export const SpotlightItem = ({
       data: { deletedDraft: true }
     });
     refetchAllSpotlights?.();
-  }, [currentUser, spotlight._id, spotlight.deletedDraft, refetchAllSpotlights, updateSpotlight]);
+  }, [currentUser, spotlight._id, refetchAllSpotlights, updateSpotlight]);
 
   // Define fade color with a CSS variable to be accessed in the styles
   const style = {
