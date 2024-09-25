fdd71fd1e8a36640c14e0303ca20a26da1366581
Raemon <raemon777@gmail.com>
2024-09-24 18:52:02 -0700

    default resolvers/mutations

--- packages/lesswrong/lib/collections/petrovDayActions/collection.ts
+++ packages/lesswrong/lib/collections/petrovDayActions/collection.ts
@@ -1,10 +1,12 @@
 import schema from './schema';
 import { createCollection } from '../../vulcan-lib';
-import { addUniversalFields } from '../../collectionUtils'
+import { addUniversalFields, getDefaultMutations, getDefaultResolvers } from '../../collectionUtils'
 
 export const PetrovDayActions: PetrovDayActionsCollection = createCollection({
   collectionName: 'PetrovDayActions',
   typeName: 'PetrovDayAction',
+  resolvers: getDefaultResolvers('PetrovDayActions'),
+  mutations: getDefaultMutations('PetrovDayActions'),
   schema
 });
 
