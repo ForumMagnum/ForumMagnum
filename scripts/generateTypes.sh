ac464fe1060f9bd775f8fe12c1cc6fd78cc8c39f
Ollie Etherington <ollie@etherington.xyz>
2024-10-09 12:21:14 +0100

    Add workflow to check if code generation hasn't been run

--- scripts/generateTypes.sh
+++ scripts/generateTypes.sh
@@ -6,8 +6,5 @@ echo "Using a running server to generating type definitions. Errors and warnings
 echo "appear in the server's log output. If this fails, make sure a local server is"
 echo "running."
 
-echo "...generating SQL schema"
-scripts/serverShellCommand.sh "Globals.generateSQLSchema(\"${PWD}\")"
-echo "...generating Typescript types"
-scripts/serverShellCommand.sh "Globals.generateTypes(\"${PWD}\")"
+scripts/serverShellCommand.sh "Globals.generateTypesAndSQLSchema(\"${PWD}\")"
 graphql-codegen --config codegen.yml
