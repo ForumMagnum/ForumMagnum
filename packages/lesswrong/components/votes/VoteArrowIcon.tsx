5d78e73cf34e5c923355fe80e84d0dc18ede624e
Ruben Bloom <darkruby501@users.noreply.github.com>
2024-10-16 18:21:26 -0700

    main vote styling mostly right but without strongvote transition

--- packages/lesswrong/components/votes/VoteArrowIcon.tsx
+++ packages/lesswrong/components/votes/VoteArrowIcon.tsx
@@ -8,6 +8,8 @@ import Transition from 'react-transition-group/Transition';
 import { useVoteColors } from './useVoteColors';
 import type { VoteColor } from './voteColors';
 import { isEAForum } from '../../lib/instanceSettings';
+import { SoftUpArrowIcon } from '../icons/softUpArrowIcon';
+import { SoftUpArrowIconCap } from '../icons/softUpArrowIconCap';
 
 const styles = (theme: ThemeType): JssStyles => ({
   root: {
@@ -24,15 +26,18 @@ const styles = (theme: ThemeType): JssStyles => ({
     cursor: 'not-allowed',
   },
   smallArrow: {
-    fontSize: '50%',
+    fontSize: '60%',
     opacity: isEAForum ? 0.7 : 0.6
   },
-  up: {},
+  up: {
+    bottom: '2px'
+  },
   right: {
     transform: 'rotate(-270deg)',
   },
   down: {
     transform: 'rotate(-180deg)',
+    top: '-1px'
   },
   left: {
     transform: 'rotate(-90deg)',
@@ -45,8 +50,9 @@ const styles = (theme: ThemeType): JssStyles => ({
     transition: `opacity ${theme.voting.strongVoteDelay}ms cubic-bezier(0.74, -0.01, 1, 1) 0ms`,
   },
   bigArrowSolid: {
-    fontSize: '65%',
-    top: "-45%"
+    // fontSize: '65%',
+    top: "-35%",
+    transform: 'scale(1.4)'
   },
   bigArrowCompleted: {
     fontSize: '90%',
@@ -99,13 +105,54 @@ const VoteArrowIcon = ({
 }: VoteArrowIconProps & {
   classes: ClassesType
 }) => {
-  const Icon = solidArrow ? ArrowDropUpIcon : UpArrowIcon
+  const {mainColor, lightColor} = useVoteColors(color);
+  const Icon = solidArrow
+  ?  <SoftUpArrowIcon
+    style={{
+      color: voted || alwaysColored ? mainColor : "inherit",
+      height: 14,
+      width: 14,
+    }}
+    className={classNames(
+      classes.smallArrow,
+    )}
+  /> 
+  : <UpArrowIcon
+    style={{color: voted || alwaysColored ? mainColor : "inherit"}}
+    className={classNames(
+      classes.smallArrow,
+      
+    )}
+    viewBox='6 6 12 12'
+  /> 
+  
+  
+  const getTransitionIcon = (state: string) => solidArrow
+  ? <SoftUpArrowIconCap
+    style={bigVoteCompleted || bigVoted ? {color: lightColor, height: 14, width: 14} : undefined}
+    className={classNames(
+      classes.bigArrow, 
+      bigVoteCompleted && classes.bigArrowCompleted,
+      solidArrow && classes.bigArrowSolid,
+      classes[state]
+    )}
+  />
+  : <UpArrowIcon
+    style={bigVoteCompleted || bigVoted ? {color: lightColor} : undefined}
+    className={classNames(
+      classes.bigArrow,
+      bigVoteCompleted && classes.bigArrowCompleted,
+      solidArrow && classes.bigArrowSolid,
+      classes[state]
+    )}  
+    viewBox='6 6 12 12'
+  />
+  
 
   if (!enabled) {
     eventHandlers = {};
   }
 
-  const {mainColor, lightColor} = useVoteColors(color);
 
   return (
     <IconButton
@@ -120,23 +167,9 @@ const VoteArrowIcon = ({
       onClick={eventHandlers.handleClick}
       disableRipple
     >
-      <Icon
-        style={{color: voted || alwaysColored ? mainColor : "inherit"}}
-        className={classes.smallArrow}
-        viewBox='6 6 12 12'
-      />
+      {Icon}
       <Transition in={!!(bigVotingTransition || bigVoted)} timeout={strongVoteDelay}>
-        {(state) => (
-          <UpArrowIcon
-            style={bigVoteCompleted || bigVoted ? {color: lightColor} : undefined}
-            className={classNames(
-              classes.bigArrow,
-              bigVoteCompleted && classes.bigArrowCompleted,
-              solidArrow && classes.bigArrowSolid,
-              classes[state]
-            )}
-            viewBox='6 6 12 12'
-          />)}
+        {(state) => (getTransitionIcon(state))}
       </Transition>
     </IconButton>
   )
