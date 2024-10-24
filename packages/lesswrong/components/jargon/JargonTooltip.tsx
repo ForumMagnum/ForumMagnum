7e84f2b9e23aeca26481d9a11208fa96fa513c4b
Raemon <raemon777@gmail.com>
2024-10-24 12:45:41 -0700

    add approval to glossaryTextReplacement

--- packages/lesswrong/components/jargon/JargonTooltip.tsx
+++ packages/lesswrong/components/jargon/JargonTooltip.tsx
@@ -119,6 +119,7 @@ function convertGlossaryItemToTextReplacement(glossaryItem: JargonTermsPost, rep
     caseInsensitive: true,
     props: {
       term: glossaryItem.term,
+      approved: glossaryItem.approved,
       definitionHTML: glossaryItem.contents?.html ?? '',
       altTerms: glossaryItem.altTerms,
       humansAndOrAIEdited: glossaryItem.humansAndOrAIEdited,
