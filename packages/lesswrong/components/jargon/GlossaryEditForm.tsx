0a2377fdfbd91ce2e9c816b512f45ee851b9dbaa
Robert Mushkatblat <bobert.mushky@gmail.com>
2024-10-25 15:04:49 -0700

    fix loading copy

--- packages/lesswrong/components/jargon/GlossaryEditForm.tsx
+++ packages/lesswrong/components/jargon/GlossaryEditForm.tsx
@@ -419,7 +419,7 @@ export const GlossaryEditForm = ({ classes, document, showTitle = true }: {
       }
     </div>
     {mutationLoading && <Loading/>}
-    {mutationLoading && <div>(Loading... warning, this will take 30-60 seconds)</div>}
+    {mutationLoading && <div>(Loading... warning, this will take 10-20 seconds.)</div>}
   </div>
 
   return <div className={classes.root}>
