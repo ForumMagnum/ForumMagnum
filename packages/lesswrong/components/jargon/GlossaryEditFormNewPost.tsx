ef44fb1da63ba3d15147be8274f6118e967ff245
Raemon <raemon777@gmail.com>
2024-11-04 12:09:18 -0800

    GlossaryEditFormNewPost section

--- packages/lesswrong/components/jargon/GlossaryEditFormNewPost.tsx
+++ packages/lesswrong/components/jargon/GlossaryEditFormNewPost.tsx
@@ -3,10 +3,17 @@ import React from 'react';
 import { registerComponent } from '../../lib/vulcan-lib';
 import { useTracking } from "../../lib/analyticsEvents";
 import { useUpdateCurrentUser } from '../hooks/useUpdateCurrentUser';
+import { commentBodyStyles } from '@/themes/stylePiping';
 
 const styles = (theme: ThemeType) => ({
   root: {
-
+    ...commentBodyStyles(theme),
+    color: theme.palette.grey[500],
+  },
+  title: {
+    marginRight: 10,
+    color: theme.palette.grey[900],
+    fontSize: '1.25rem'
   }
 });
 
@@ -16,7 +23,7 @@ export const GlossaryEditFormNewPost = ({classes}: {
   const { captureEvent } = useTracking(); //it is virtuous to add analytics tracking to new components
 
   return <div className={classes.root}>
-
+    <span className={classes.title}>Glossary [Beta]</span> Save this post once, to start generating/editing a jargon glossary
   </div>;
 }
 
