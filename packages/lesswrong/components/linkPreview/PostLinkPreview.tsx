ForumMagnum/ForumMagnum

commit 8897c0d4c9fad637a48cae5b77e11e9cd3e3a448

Forum-gate filled link-circles; also make them work with browser history in spite of browser history-sniffing protection

packages/lesswrong/components/linkPreview/PostLinkPreview.tsx
@@ -12,6 +12,7 @@ import { usePostByLegacyId, usePostBySlug } from '../posts/usePost';
 import { isClient } from '../../lib/executionEnvironment';
 import { isFriendlyUI } from '../../themes/forumTheme';
 import classNames from 'classnames';
+import { visitedLinksHaveFilledInCircle } from '@/lib/betas';
 
 let missingLinkPreviewsLogged = new Set<string>();
 
@@ -210,33 +211,59 @@ const PostLinkPreviewVariantCheck = ({ href, post, targetLocation, comment, comm
 }
 const PostLinkPreviewVariantCheckComponent = registerComponent('PostLinkPreviewVariantCheck', PostLinkPreviewVariantCheck);
 
-export const linkStyle = (theme: ThemeType) => ({
-  link: {
-    '&:after': {
-      content: '""',
-      top: -7,
-      position: "relative",
-      marginLeft: 2,
-      marginRight: 0,
-      width: 4,
-      height: 4,
-      display: "inline-block",
-      background: "transparent",
-      border: `1.2px solid ${theme.palette.link.color ?? theme.palette.primary.main}`,
-      borderRadius: "50%",
-    },
-    // `visited` is a string-classname rather than something that gets
-    // prefixed, because some broadly-applied styles in `stylePiping` also use
-    // it. The .visited class is often the same as the :visited pseudo-
-    // selector, but it will also be set when a post/tag/etc is marked as read
-    // for the logged in user in the DB, independent of their local browser
-    // history.
-    "&:visited:after, &.visited:after": {
-      background: theme.palette.link.visited ?? theme.palette.primary.main,
-      border: `1.2px solid ${theme.palette.link.visited ?? theme.palette.primary.main}`,
-    },
-  }
-})
+export const linkStyle = (theme: ThemeType) => (
+  visitedLinksHaveFilledInCircle
+    ? {
+      link: {
+        '&:after': {
+          content: '""',
+          top: -7,
+          position: "relative",
+          marginLeft: 2,
+          marginRight: 0,
+          width: 4,
+          height: 4,
+          display: "inline-block",
+          
+          // The center of the link-circle is the page-background color, rather
+          // than transparent, because :visited cannot change background
+          // opacity. Technically, this means that if a link appears on a
+          // non-default background, the center of the circle is the wrong
+          // color. I'm able to detect this on even-numbered replies (which
+          // have a gray background) if I use a magnifier/color-picker, but
+          // can't detect it by eye, so this is probably fine.
+          background: theme.palette.background.default,
+          border: `1.2px solid ${theme.palette.link.color ?? theme.palette.primary.main}`,
+          borderRadius: "50%",
+        },
+
+        // Visited styles can be applied for two reasons: based on the :visited
+        // selector (which is applied by the browser based on local browser
+        // history), or based on the .visited class (which is applied by link
+        // components for logged-in users based on the read-status of the
+        // destination, in the DB).
+        //
+        // `visited` is a string-classname rather than something that gets
+        // prefixed, because some broadly-applied styles in `stylePiping` also use
+        // it.
+        //
+        // Because of browser rules intended to prevent history-sniffing, the
+        // attributes that can appear in this block, if it's applied via the
+        // :visited selector rather than the .visited class, are highly
+        // restricted. In particular, the `background` attribute can change
+        // color, but it cannot change opacity.
+        "&:visited:after, &.visited:after": {
+          background: theme.palette.link.visited ?? theme.palette.primary.main,
+          border: `1.2px solid ${theme.palette.link.visited ?? theme.palette.primary.main}`,
+        },
+      }
+    } : {
+      '&:after': {
+        content: '"°"',
+        marginLeft: 1,
+      }
+    }
+);
 
 const styles = (theme: ThemeType) => ({
   ...linkStyle(theme)

packages/lesswrong/lib/betas.ts
@@ -113,6 +113,7 @@ export const hasSurveys = isFriendlyUI && !isBotSiteSetting.get();
 export const hasCollapsedFootnotes = !isLWorAF;
 export const useCurationEmailsCron = isLW;
 export const hasSidenotes = isLWorAF;
+export const visitedLinksHaveFilledInCircle = isLWorAF;
 
 // EA Forum disabled the author's ability to moderate posts. We disregard this
 // check in tests as the tests run in EA Forum mode, but we want to be able to

commit a6ab877e91a69394890ffd2b60e51dc3dcaffd0f

Fix broken link dots
Regression from commit 8897c0d4c9fad637a48cae5b77e11e9cd3e3a448

packages/lesswrong/components/linkPreview/PostLinkPreview.tsx
@@ -258,10 +258,12 @@ export const linkStyle = (theme: ThemeType) => (
         },
       }
     } : {
-      '&:after': {
-        content: '"°"',
-        marginLeft: 1,
-      }
+      link: {
+        '&:after': {
+          content: '"°"',
+          marginLeft: 1,
+        },
+      },
     }
 );
 