ForumMagnum/ForumMagnum

commit c920798bb8e15aebf7e122f4be73dd4ee16ca456

glossary in postNew

packages/lesswrong/lib/collections/posts/schema.tsx
@@ -847,7 +847,7 @@ const schema: SchemaType<"Posts"> = {
     graphQLtype: '[JargonTerm!]!',
     optional: true,
     canRead: ['guests'],
-    control: "GlossaryEditForm",
+    control: "GlossaryEditFormWrapper",
     group: formGroups.glossary,
     hidden: ({currentUser}) => !userCanCreateAndEditJargonTerms(currentUser),
 
@@ -3079,6 +3079,8 @@ const schema: SchemaType<"Posts"> = {
     optional: true,
     hidden: true,
     ...schemaDefaultValue(false),
+    canRead: ['members'],
+    canUpdate: [userOwns],
   },
 };
 

packages/lesswrong/components/hooks/useJargonCounts.ts
@@ -1,7 +1,7 @@
 import { useCallback, useMemo } from "react";
 import { countInstancesOfJargon } from "../jargon/utils";
 
-export function useJargonCounts<T extends JargonTermsPost | JargonTerms>(post: PostsPage | PostsWithNavigationAndRevision | PostsWithNavigation, glossaryTerms: Array<T>) {
+export function useJargonCounts<T extends JargonTermsPost | JargonTerms>(post: PostsEditQueryFragment | PostsPage | PostsWithNavigationAndRevision | PostsWithNavigation, glossaryTerms: Array<T>) {
   // Memoize the normalized post contents
   const normalizedPostContents = useMemo(() => 
     (post.contents?.html ?? "").toLowerCase(),

packages/lesswrong/components/jargon/EditUserJargonSettings.tsx
@@ -1,0 +1,60 @@
+// TODO: Import component in components.ts
+import React from 'react';
+import { Components, registerComponent } from '../../lib/vulcan-lib';
+import { useTracking } from "../../lib/analyticsEvents";
+import { useUpdateCurrentUser } from '../hooks/useUpdateCurrentUser';
+import { useCurrentUser } from '../common/withUser';
+import Checkbox from '@material-ui/core/Checkbox';
+
+const styles = (theme: ThemeType) => ({
+  root: {
+
+  },
+  checkboxContainer: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  generationFlagCheckbox: {
+    marginLeft: 10,
+  },
+});
+
+export const EditUserJargonSettings = ({classes}: {
+  classes: ClassesType<typeof styles>,
+}) => {
+  const { captureEvent } = useTracking(); //it is virtuous to add analytics tracking to new components
+  const currentUser = useCurrentUser();
+  const updateCurrentUser = useUpdateCurrentUser();
+
+  const { LWTooltip, MetaInfo } = Components;
+  return <>
+      <LWTooltip title="Automatically query jargon for all drafts">
+        <div className={classes.checkboxContainer}>
+          <MetaInfo>All drafts</MetaInfo>
+          <Checkbox
+            className={classes.generationFlagCheckbox}
+            checked={currentUser?.generateJargonForDrafts}
+            onChange={(e) => updateCurrentUser({generateJargonForDrafts: e.target.checked})}
+          />
+        </div>
+      </LWTooltip>
+      <LWTooltip title="Automatically query jargon for all published posts">
+        <div className={classes.checkboxContainer}>
+          <MetaInfo>All published posts</MetaInfo>
+          <Checkbox
+            className={classes.generationFlagCheckbox}
+            checked={currentUser?.generateJargonForPublishedPosts}
+            onChange={(e) => updateCurrentUser({generateJargonForPublishedPosts: e.target.checked})}
+          />
+        </div>
+      </LWTooltip>
+  </>;
+}
+
+const EditUserJargonSettingsComponent = registerComponent('EditUserJargonSettings', EditUserJargonSettings, {styles});
+
+declare global {
+  interface ComponentTypes {
+    EditUserJargonSettings: typeof EditUserJargonSettingsComponent
+  }
+}

packages/lesswrong/components/jargon/GlossaryEditForm.tsx
@@ -221,7 +221,6 @@ const styles = (theme: ThemeType) => ({
   },
   checkboxRow: {
     display: 'flex',
-    flexDirection: 'column',
     justifyContent: 'space-between',
     alignItems: 'end',
     paddingTop: 4,
@@ -242,13 +241,18 @@ const styles = (theme: ThemeType) => ({
 
 export const GlossaryEditForm = ({ classes, document, showTitle = true }: {
   classes: ClassesType<typeof styles>,
-  document: PostsPage,
+  document: PostsEditQueryFragment,
   showTitle?: boolean,
 }) => {
-  const { JargonEditorRow, LoadMore, Loading, LWTooltip, WrappedSmartForm, IconRight, IconDown, Row, MetaInfo } = Components;
+  const { JargonEditorRow, LoadMore, Loading, LWTooltip, WrappedSmartForm, IconRight, IconDown, Row, MetaInfo, EditUserJargonSettings } = Components;
 
   const currentUser = useCurrentUser();
   const updateCurrentUser = useUpdateCurrentUser();
+
+  const { mutate: updatePost } = useUpdate({
+    collectionName: "Posts",
+    fragmentName: 'PostsEdit',
+  });
 
   const [formCollapsed, setFormCollapsed] = useState(false);
   const [showMoreTerms, setShowMoreTerms] = useState(false);
@@ -450,38 +454,26 @@ export const GlossaryEditForm = ({ classes, document, showTitle = true }: {
   </div>
 
   const generateJargonFlagsRow = <div className={classes.checkboxRow}>
-    <LWTooltip title={`Automatically query ${JARGON_LLM_MODEL} every ~1000 characters added to the post`}>
+    <LWTooltip title={<div><div>
+        Have jargon automatically generated so when you're ready to publish, you can easily review it.
+      </div>
+      <div>
+        <em>(Queries ${JARGON_LLM_MODEL} every ~5 minutes.)</em>
+      </div>
+      </div>}>
       <div className={classes.checkboxContainer} onClick={() => setClickedAutogenerate(true)}>
-        <MetaInfo>Autogenerate</MetaInfo>
+        <MetaInfo>Autogenerate while drafting</MetaInfo>
         <Checkbox
           className={classes.generationFlagCheckbox}
-          checked={currentUser?.generateJargonForDrafts}
-          onChange={(e) => updateCurrentUser({generateJargonForDrafts: e.target.checked})}
+          checked={document?.generateDraftJargon}
+          onChange={(e) => updatePost({
+            selector: { _id: document._id },
+            data: { generateDraftJargon: e.target.checked },
+          })}
         />
       </div>
     </LWTooltip>
-    {clickedAutogenerate && <>
-      <LWTooltip title="Automatically query jargon for all drafts">
-        <div className={classes.checkboxContainer}>
-          <MetaInfo>All drafts</MetaInfo>
-          <Checkbox
-            className={classes.generationFlagCheckbox}
-            checked={currentUser?.generateJargonForDrafts}
-            onChange={(e) => updateCurrentUser({generateJargonForDrafts: e.target.checked})}
-          />
-        </div>
-      </LWTooltip>
-      <LWTooltip title="Automatically query jargon for all published posts">
-        <div className={classes.checkboxContainer}>
-          <MetaInfo>All published posts</MetaInfo>
-          <Checkbox
-            className={classes.generationFlagCheckbox}
-            checked={currentUser?.generateJargonForPublishedPosts}
-            onChange={(e) => updateCurrentUser({generateJargonForPublishedPosts: e.target.checked})}
-          />
-        </div>
-      </LWTooltip>
-    </>}
+    {clickedAutogenerate && <EditUserJargonSettings />}
   </div>
 
   const footer = <div className={classNames(classes.buttonRow, formCollapsed && classes.formCollapsed)}>
@@ -550,7 +542,6 @@ export const GlossaryEditForm = ({ classes, document, showTitle = true }: {
       </div>
       <LoadMore {...loadMoreProps} />
     </div>
-    {generateJargonFlagsRow}
     {footer}
     {editingPrompt && promptEditor}
   </div>;

packages/lesswrong/components/jargon/GlossaryEditFormNewPost.tsx
@@ -1,0 +1,29 @@
+// TODO: Import component in components.ts
+import React from 'react';
+import { registerComponent } from '../../lib/vulcan-lib';
+import { useTracking } from "../../lib/analyticsEvents";
+import { useUpdateCurrentUser } from '../hooks/useUpdateCurrentUser';
+
+const styles = (theme: ThemeType) => ({
+  root: {
+
+  }
+});
+
+export const GlossaryEditFormNewPost = ({classes}: {
+  classes: ClassesType<typeof styles>,
+}) => {
+  const { captureEvent } = useTracking(); //it is virtuous to add analytics tracking to new components
+
+  return <div className={classes.root}>
+
+  </div>;
+}
+
+const GlossaryEditFormNewPostComponent = registerComponent('GlossaryEditFormNewPost', GlossaryEditFormNewPost, {styles});
+
+declare global {
+  interface ComponentTypes {
+    GlossaryEditFormNewPost: typeof GlossaryEditFormNewPostComponent
+  }
+}

packages/lesswrong/components/jargon/GlossaryEditFormWrapper.tsx
@@ -1,0 +1,30 @@
+// TODO: Import component in components.ts
+import React from 'react';
+import { Components, registerComponent } from '../../lib/vulcan-lib';
+import { useTracking } from "../../lib/analyticsEvents";
+
+const styles = (theme: ThemeType) => ({
+  root: {
+
+  }
+});
+
+export const GlossaryEditFormWrapper = ({classes, document}: {
+  classes: ClassesType<typeof styles>,
+  document: PostsEditQueryFragment,
+}) => {
+  const { GlossaryEditFormNewPost, GlossaryEditForm } = Components;
+
+  if (!document._id) return <GlossaryEditFormNewPost />
+  return <div className={classes.root}>
+    <GlossaryEditForm document={document} />
+  </div>;
+}
+
+const GlossaryEditFormWrapperComponent = registerComponent('GlossaryEditFormWrapper', GlossaryEditFormWrapper, {styles});
+
+declare global {
+  interface ComponentTypes {
+    GlossaryEditFormWrapper: typeof GlossaryEditFormWrapperComponent
+  }
+}

packages/lesswrong/components/jargon/GlossaryEditorPage.tsx
@@ -39,7 +39,7 @@ export const GlossaryEditorPage = ({classes}: {
     },
     itemsPerPage: 50,
     collectionName: "Posts",
-    fragmentName: 'PostsPage',
+    fragmentName: 'PostsEditQueryFragment',
   })
 
   const { GlossaryEditForm, SingleColumnSection, PostsTitle, LoadMore, SectionTitle, ContentStyles, ErrorAccessDenied, Row, UsersNameDisplay } = Components

packages/lesswrong/components/jargon/utils.ts
@@ -1,7 +1,7 @@
 // TODO: maybe sort by first use instead of frequency?
 export function countInstancesOfJargon(
   jargonTerm: JargonTermsPost | JargonTerms,
-  post: PostsWithNavigationAndRevision | PostsWithNavigation | PostsPage,
+  post: PostsWithNavigationAndRevision | PostsWithNavigation | PostsPage | PostsEditQueryFragment,
   normalizedPostContents?: string
 ) {
   const postText = normalizedPostContents ?? (post.contents?.html ?? "").toLowerCase();

commit df8a17a342e715e8c55c460dc84b30e9dabcb232

Fix transfering posts to new accounts
Regression from commit c920798 in PR #9966

packages/lesswrong/lib/collections/posts/schema.tsx
@@ -3108,7 +3108,7 @@ const schema: SchemaType<"Posts"> = {
     optional: true,
     hidden: true,
     canRead: ['members'],
-    canUpdate: [userOwns],
+    canUpdate: [userOwns, "admins"],
     ...schemaDefaultValue(false)
   },
 };