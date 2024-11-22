e118987ec19f6cc4c541e0107a624493b02fd0b7
Robert Mushkatblat <bobert.mushky@gmail.com>
2024-11-22 11:36:08 -0800

    red links mockup first pass

--- packages/lesswrong/components/tagging/TagsTooltip.tsx
+++ packages/lesswrong/components/tagging/TagsTooltip.tsx
@@ -2,6 +2,7 @@ import React, { FC, ReactNode, useCallback, useState } from "react";
 import { Components, registerComponent } from "../../lib/vulcan-lib";
 import { useTagPreview } from "./useTag";
 import { isFriendlyUI } from "../../themes/forumTheme";
+import { Link } from '../../lib/reactRouterWrapper';
 import classNames from "classnames";
 
 type PreviewableTag =
@@ -70,7 +71,56 @@ const styles = (theme: ThemeType) => ({
     paddingRight: 32,
     paddingBottom: 24,
   },
+  redLinkTooltip: {
+    paddingTop: 8,
+    paddingLeft: 16,
+    paddingRight: 16,
+    ...(!isFriendlyUI && {
+      width: 500,
+      paddingBottom: 6,
+    }),
+    [theme.breakpoints.down('xs')]: {
+      width: "100%",
+    }
+  },
+  redLinkTooltipTitle: {
+    fontSize: '13px',
+    marginBottom: 4,
+    color: theme.palette.text.secondary,
+  },
 });
+
+const RedLinkTooltip = ({ classes, ...tagsTooltipProps }: TagsTooltipTag & { classes: ClassesType<typeof styles> }) => {
+  const { Typography, ContentStyles, TagHoverPreview } = Components;
+  const derivedTitle = tagsTooltipProps.tagSlug!.split('_').map((word, idx) => {
+    if (idx === 0) {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    }
+    return word;
+  }).join(' ');
+
+  return <div className={classes.redLinkTooltip}>
+    <Typography variant='title'>
+      {derivedTitle}
+    </Typography>
+    {/* TODO: this is a hardcoded example; when we implement the backend for red links we should fix this */}
+    <ContentStyles contentType='tag'>
+      This red link was used on one 1 other page:
+      <ul>
+        <li>
+          <TagHoverPreview targetLocation={{ params: { slug: 'nash_equilibrium' }, hash: '', query: {} }} href='/tag/nash_equilibrium' noPrefetch>
+            <Link to={`/tag/nash_equilibrium`}>
+              Nash Equilibrium
+            </Link>
+          </TagHoverPreview>
+        </li>
+      </ul>
+    </ContentStyles>
+    <ContentStyles contentType='tag' className={classes.redLinkTooltipTitle}>
+      A red link highlights author's intention to point at a concept that doesn't have a satisfactory explanation written (by their standards).
+    </ContentStyles>
+  </div>
+}
 
 const TagsTooltip = ({
   tagRel,
@@ -83,6 +133,7 @@ const TagsTooltip = ({
   inlineBlock = false,
   className,
   popperClassName,
+  isRedLink,
   children,
   classes,
   ...tagsTooltipProps
@@ -97,13 +148,14 @@ const TagsTooltip = ({
   inlineBlock?: boolean,
   className?: string,
   popperClassName?: string,
+  isRedLink?: boolean,
   children: ReactNode,
   classes: ClassesType<typeof styles>,
 }) => {
   const [everHovered, setEverHovered] = useState(false);
   const {tag, loading} = useTagsTooltipTag(
     tagsTooltipProps, hash,
-    noPrefetch && !everHovered
+    (noPrefetch && !everHovered) || isRedLink
   );
 
   const {HoverOver, Loading, TagRelCard, TagPreview} = Components;
@@ -119,6 +171,7 @@ const TagsTooltip = ({
             postCount={previewPostCount}
             hideRelatedTags={hideRelatedTags}
           />}
+          {isRedLink && <RedLinkTooltip classes={classes} {...tagsTooltipProps} />}
         </PreviewWrapper>
       }
       clickable
