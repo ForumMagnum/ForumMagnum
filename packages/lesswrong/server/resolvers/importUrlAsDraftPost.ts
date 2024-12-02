6d3b5c8682892999f57d2b4ded1b97b1f0b87a31
Ruben Bloom <darkruby501@users.noreply.github.com>
2024-12-02 13:47:03 -0800

    use arXiv API to get paper basic details

--- packages/lesswrong/server/resolvers/importUrlAsDraftPost.ts
+++ packages/lesswrong/server/resolvers/importUrlAsDraftPost.ts
@@ -1,4 +1,5 @@
 import {extract} from '@extractus/article-extractor'
+import { ArxivExtractor } from '../extractors/arxivExtractor'
 
 import Posts from '../../lib/collections/posts/collection'
 import {createMutator} from '../vulcan-lib'
@@ -44,13 +45,33 @@ export async function importUrlAsDraftPost(url: string, context: ResolverContext
     context,
   })
 
-  if (existingPost) {
-    return { _id: existingPost._id, slug: existingPost.slug, title: existingPost.title, url: existingPost.url, postedAt: existingPost.postedAt, createdAt: existingPost.createdAt, userId: existingPost.userId, coauthorStatuses: existingPost.coauthorStatuses, draft: existingPost.draft}
-  }
+  // if (existingPost) {
+  //   return { _id: existingPost._id, slug: existingPost.slug, title: existingPost.title, url: existingPost.url, postedAt: existingPost.postedAt, createdAt: existingPost.createdAt, userId: existingPost.userId, coauthorStatuses: existingPost.coauthorStatuses, draft: existingPost.draft}
+  // }
 
-  const extractedData = await extract(url)
-  if (!extractedData) {
-    throw new Error('Failed to extract data from URL')
+  let extractedData
+  
+  if (url.toLowerCase().includes('arxiv.org')) {
+    const arxivData = await ArxivExtractor.extract(url)
+
+    // take the abstract and prepend the authors list and append a hyperlink to the original with the text "Read the full paper"
+    const annotatedContents = `<p><strong>By ${arxivData.authors.join(", ")}</strong></p>
+<h2>Abstract</h2>
+<blockquote>${arxivData.abstract}</blockquote>
+<p><a href="${arxivData.pdfUrl}">Read the full paper</a></p>`
+
+    extractedData = {
+      title: arxivData.title,
+      content: annotatedContents,
+      published: arxivData.published,
+      // Additional metadata could be stored in customFields if needed
+    }
+    console.log("extracted arxiv data", extractedData)
+  } else {
+    extractedData = await extract(url)
+    if (!extractedData) {
+      throw new Error('Failed to extract data from URL')
+    }
   }
 
   console.log({extractedData})
