969d7cb7e6fdc428c29a6baf8481ccfc603f3829
Will Howard <w.howard256+github@gmail.com>
2024-12-13 17:37:48 +0000

    working acceptably on mobile

--- packages/lesswrong/components/forumEvents/ForumEventResultIcon.tsx
+++ packages/lesswrong/components/forumEvents/ForumEventResultIcon.tsx
@@ -66,73 +66,6 @@ const styles = (theme: ThemeType) => ({
     flexDirection: 'column',
     gap: "10px",
   },
-  pinIcon: {
-    width: 12,
-    height: 12,
-    transform: "rotate(90deg)",
-    color: theme.palette.grey[600],
-    cursor: "pointer",
-    marginBottom: 6,
-    "&:hover": {
-      opacity: 0.7
-    }
-  },
-  pinIconPinned: {
-    transform: "rotate(45deg)",
-    color: theme.palette.grey[1000],
-  },
-  commentBody: {
-    display: "-webkit-box",
-    "-webkit-box-orient": "vertical",
-    "-webkit-line-clamp": 25,
-  },
-  // User profile header, some of these styles are borrowed from EAUserTooltipContent
-  userInfo: {
-    display: "flex",
-    maxWidth: "100%",
-  },
-  displayNameRow: {
-    display: "flex",
-    justifyContent: "space-between",
-    gap: "8px"
-  },
-  displayName: {
-    fontSize: "1.3rem",
-    fontWeight: 600,
-    color: theme.palette.grey["A400"],
-  },
-  headerInfo: {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "space-between",
-    overflow: "hidden",
-    width: "100%",
-    "& *": {
-      whiteSpace: "nowrap",
-      textOverflow: "ellipsis",
-      overflow: "hidden",
-    },
-  },
-  profileImage: {
-    marginRight: 12,
-  },
-  replyButtonRow: {
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center"
-  },
-  replyButton: {
-    fontSize: 14,
-    fontWeight: 600,
-    cursor: "pointer",
-    '&:hover': {
-      opacity: 0.7
-    }
-  },
-  replyForm: {
-    border: theme.palette.border.normal,
-    borderRadius: theme.borderRadius.default
-  }
 });
 
 export type ForumEventVoteDisplay = {
@@ -140,120 +73,6 @@ export type ForumEventVoteDisplay = {
   user: UsersMinimumInfo,
   comment: ShortformComments | null
 }
-
-const ForumEventResultPopper = ({
-  anchorEl,
-  user,
-  comment,
-  captureEvent,
-  setIsPinned,
-  isPinned,
-  newRepliesCount,
-  setNewRepliesCount,
-  classes,
-}: {
-  anchorEl: any,
-  user: UsersMinimumInfo;
-  comment: ShortformComments;
-  captureEvent: Function;
-  setIsPinned: React.Dispatch<React.SetStateAction<boolean>>;
-  isPinned: boolean;
-  newRepliesCount: number;
-  setNewRepliesCount: React.Dispatch<React.SetStateAction<number>>;
-  classes: ClassesType<typeof styles>;
-}) => {
-  const {
-    LWPopper,
-    LWClickAwayListener,
-    ForumIcon,
-    CommentBody,
-    CommentsNewForm,
-    UsersProfileImage
-  } = Components;
-
-  const [replyFormOpen, setReplyFormOpen] = useState(false);
-
-  const replySuccessCallback = useCallback(() => {
-    captureEvent("replyFromBanner")
-    // We only need to show the count, so just track comments the user submits rather than refetching
-    setNewRepliesCount((prev) => prev + 1);
-    setReplyFormOpen(false);
-  }, [captureEvent, setNewRepliesCount])
-
-  const repliesCount = (comment?.descendentCount ?? 0) + newRepliesCount;
-
-  const replyString = repliesCount
-    ? `(${repliesCount} ${repliesCount === 1 ? "reply" : "replies"})`
-    : "";
-
-  const role = formatRole(user.jobTitle, user.organization);
-
-  return (
-    <LWPopper
-      open={true}
-      anchorEl={anchorEl}
-      clickable={true}
-      allowOverflow={false}
-      placement={"right-start"}
-    >
-      <LWClickAwayListener onClickAway={() => setIsPinned(false)}>
-        <div className={classes.popperContent}>
-          <div className={classes.userInfo}>
-            <UsersProfileImage user={user} size={40} className={classes.profileImage} />
-            <div className={classes.headerInfo}>
-              <div className={classes.displayNameRow}>
-                <Link
-                  className={classes.displayName}
-                  to={userGetProfileUrl(user)}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  {user.displayName}
-                </Link>
-                <ForumIcon
-                  icon="Pin"
-                  onClick={() => setIsPinned(!isPinned)}
-                  className={classNames(classes.pinIcon, { [classes.pinIconPinned]: isPinned })}
-                />
-              </div>
-              <div>{role}</div>
-            </div>
-          </div>
-          <CommentBody comment={comment} className={classes.commentBody} />
-          <div className={classes.replyButtonRow}>
-            <div
-              className={classes.replyButton}
-              onClick={() => {
-                setReplyFormOpen(true);
-                // Pin even after the comment is submitted so they can see the result
-                setIsPinned(true);
-              }}
-            >
-              Reply
-            </div>
-            <Link
-              to={commentGetPageUrlFromIds({ postId: comment.postId, commentId: comment._id })}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Go to thread {replyString}
-            </Link>
-          </div>
-          {replyFormOpen && (
-            <CommentsNewForm
-              type="reply"
-              post={comment.post ?? undefined}
-              parentComment={comment}
-              cancelCallback={() => setReplyFormOpen(false)}
-              successCallback={replySuccessCallback}
-              className={classes.replyForm}
-            />
-          )}
-        </div>
-      </LWClickAwayListener>
-    </LWPopper>
-  );
-};
 
 const ForumEventResultIcon = ({
   vote,
@@ -264,7 +83,7 @@ const ForumEventResultIcon = ({
   tooltipDisabled: boolean;
   classes: ClassesType<typeof styles>;
 }) => {
-  const { LWTooltip, UsersProfileImage } = Components;
+  const { LWTooltip, UsersProfileImage, ForumEventResultPopper } = Components;
 
   const isDesktop = useIsAboveScreenWidth(POLL_MAX_WIDTH);
 
@@ -310,7 +129,6 @@ const ForumEventResultIcon = ({
             anchorEl={anchorEl}
             user={user}
             comment={comment}
-            classes={classes}
             captureEvent={captureEvent}
             setIsPinned={setIsPinned}
             isPinned={isPinned}
---
+++ packages/lesswrong/components/forumEvents/ForumEventStickers.tsx
@@ -0,0 +1,424 @@
+import { useMulti } from "@/lib/crud/withMulti";
+import { gql, useMutation } from "@apollo/client";
+import React, { FC, useCallback, useMemo, useRef, useState } from "react";
+import { useLoginPopoverContext } from "../hooks/useLoginPopoverContext";
+import { useCurrentUser } from "../common/withUser";
+import { Components, registerComponent } from "@/lib/vulcan-lib";
+import { useCurrentForumEvent } from "../hooks/useCurrentForumEvent";
+import classNames from "classnames";
+import { ForumIconName } from "../common/ForumIcon";
+import { useTracking } from "@/lib/analyticsEvents";
+import { useHover } from "../common/withHover";
+import { commentGetPageUrlFromIds } from "@/lib/collections/comments/helpers";
+import { postGetPageUrl } from "@/lib/collections/posts/helpers";
+import { Link } from "@/lib/reactRouterWrapper";
+
+const styles = (theme: ThemeType) => ({
+  heartsContainer: {
+    width: "100%",
+    height: "100%",
+    position: "absolute",
+  },
+  heart: {
+    color: theme.palette.givingSeason.heart,
+    position: "absolute",
+    transformOrigin: "center",
+    "& svg": {
+      fontSize: 20,
+      [theme.breakpoints.down('xs')]: {
+        fontSize: 16
+      },
+    }
+  },
+  hoverHeart: {
+    opacity: 0.5,
+    pointerEvents: "none",
+    [theme.breakpoints.down('xs')]: {
+      display: "none"
+    },
+  },
+  clearSticker: {
+    top: -1,
+    right: -3,
+    position: "absolute",
+    backgroundColor: `color-mix(in oklab, ${theme.palette.text.alwaysBlack} 65%, ${theme.palette.text.alwaysWhite} 35%)`,
+    padding: 2,
+    borderRadius: "50%",
+    cursor: "pointer",
+    width: 10,
+    height: 10,
+    fontSize: 9,
+    "&:hover": {
+      backgroundColor: theme.palette.text.alwaysBlack,
+    },
+    [theme.breakpoints.down('xs')]: {
+      top: -2,
+      right: -4,
+    },
+  },
+  cross: {
+    color: theme.palette.text.alwaysWhite,
+    opacity: 0.8,
+    position: "absolute",
+    fontSize: 12,
+    top: -4,
+    left: 1
+  },
+  commentPopper: {
+    margin: "0 8px"
+  }
+});
+
+const addForumEventStickerQuery = gql`
+  mutation AddForumEventSticker($forumEventId: String!, $x: Float!, $y: Float!, $theta: Float!) {
+    AddForumEventSticker(forumEventId: $forumEventId, x: $x, y: $y, theta: $theta)
+  }
+`;
+const removeForumEventStickerQuery = gql`
+  mutation RemoveForumEventSticker($forumEventId: String!) {
+    RemoveForumEventSticker(forumEventId: $forumEventId)
+  }
+`;
+
+// TODO use these on the backend
+type ForumEventStickerData = Record<
+  string,
+  {
+    x: number;
+    y: number;
+    theta: number;
+  }
+>;
+
+type ForumEventStickerDisplay = {
+  x: number;
+  y: number;
+  theta: number;
+  user: UsersMinimumInfo;
+  comment: ShortformComments | null;
+};
+
+function stickerDataToArray({
+  data,
+  users,
+  comments,
+  currentUser
+}: {
+  data: ForumEventStickerData | null;
+  users: UsersMinimumInfo[] | undefined;
+  comments: ShortformComments[] | undefined;
+  currentUser: UsersCurrent | null;
+}): { currentUserHeart: ForumEventStickerDisplay | null, otherHearts: ForumEventStickerDisplay[] } {
+  if (!users || !data) {
+    return { currentUserHeart: null, otherHearts: [] };
+  }
+
+  const allHearts = users
+    .map((user) => {
+      const sticker = data[user._id];
+      if (!sticker) return undefined;
+
+      const comment = comments?.find(comment => comment.userId === user._id) || null;
+      return {
+        x: sticker.x,
+        y: sticker.y,
+        theta: sticker.theta,
+        user,
+        comment,
+      };
+    })
+    .filter((sticker) => !!sticker) as ForumEventStickerDisplay[];
+
+  const currentUserHeart = allHearts.find(heart => currentUser && heart.user._id === currentUser._id) || null;
+  const otherHearts = allHearts.filter(heart => !currentUser || heart.user._id !== currentUser._id);
+
+  return { currentUserHeart, otherHearts };
+}
+
+type ForumEventStickerProps = {
+  x: number;
+  y: number;
+  theta: number;
+  user?: UsersMinimumInfo;
+  comment?: ShortformComments | null;
+  icon?: ForumIconName;
+  tooltipDisabled?: boolean;
+  onClear?: () => void;
+  className?: string;
+};
+const ForumEventSticker = React.forwardRef<
+  HTMLDivElement,
+  ForumEventStickerProps & { classes: ClassesType<typeof styles> }
+>(({ x, y, theta, user, comment, icon = "Heart", tooltipDisabled, onClear, className, classes }, ref) => {
+  const { ForumIcon, ForumEventResultPopper } = Components;
+
+  // TODO de-dup with ForumEventResultIcon
+  // TODO decide on a width to stop showing them
+  const isDesktop = true;
+
+  const { captureEvent } = useTracking();
+  const { currentForumEvent } = useCurrentForumEvent();
+
+  const { eventHandlers, hover, anchorEl } = useHover();
+
+  const [isPinned, setIsPinned] = useState(false);
+  const [newRepliesCount, setNewRepliesCount] = useState(0);
+
+  const popperOpen = hover || isPinned;
+
+  if (!isDesktop) return null;
+
+  return (
+    <div
+      className={classNames(classes.heart, className)}
+      ref={ref}
+      style={{
+        left: `${x * 100}%`,
+        top: `${y * 100}%`,
+        transform: `rotate(${theta}deg) translate(-50%, -50%)`,
+      }}
+      {...eventHandlers}
+    >
+      {/* onPointerDown rather than onClick because the button is very small */}
+      {onClear && (
+        <div className={classes.clearSticker} onPointerDown={onClear}>
+          {/* <ForumIcon icon="Close" /> */}
+          <div className={classes.cross}>&times;</div>
+        </div>
+      )}
+      <ForumIcon icon={icon} />
+      {!tooltipDisabled && user && comment && popperOpen && (
+        <ForumEventResultPopper
+          anchorEl={anchorEl}
+          user={user}
+          comment={comment}
+          captureEvent={captureEvent}
+          setIsPinned={setIsPinned}
+          isPinned={isPinned}
+          newRepliesCount={newRepliesCount}
+          setNewRepliesCount={setNewRepliesCount}
+          className={classes.commentPopper}
+        />
+      )}
+    </div>
+  );
+});
+
+const ForumEventStickers: FC<{
+  interactive: boolean;
+  classes: ClassesType<typeof styles>;
+}> = ({ interactive, classes }) => {
+  const { ForumEventCommentForm } = Components;
+
+  const { currentForumEvent, refetch } = useCurrentForumEvent();
+  const { onSignup } = useLoginPopoverContext();
+  const currentUser = useCurrentUser();
+
+  const stickerData: ForumEventStickerData | null = currentForumEvent?.publicData || null;
+
+  const { results: users } = useMulti({
+    terms: {
+      view: 'usersByUserIds',
+      userIds: stickerData
+        ? Object.keys(stickerData)
+        : [],
+      limit: 1000,
+    },
+    collectionName: "Users",
+    fragmentName: 'UsersMinimumInfo',
+    enableTotal: false,
+    skip: !stickerData,
+  });
+  const { results: comments, refetch: refetchComments } = useMulti({
+    terms: {
+      view: 'forumEventComments',
+      forumEventId: currentForumEvent?._id,
+      limit: 1000,
+    },
+    collectionName: "Comments",
+    fragmentName: 'ShortformComments',
+    enableTotal: false,
+    // Don't run on the first pass, to speed up SSR
+    skip: !currentForumEvent?._id || !users,
+  });
+
+  const { currentUserHeart, otherHearts } = useMemo(
+    () => stickerDataToArray({ data: stickerData, users, comments, currentUser }),
+    [comments, currentUser, stickerData, users]
+  );
+
+  const [commentFormOpen, setCommentFormOpen] = useState(false);
+  const [hoverPos, setHoverPos] = useState<{ x: number; y: number } | null>(
+    null
+  );
+  // Random rotation between -25 and 25 degrees
+  const hoverTheta = useMemo(() => (Math.random() * 50) - 25, []);
+
+  const containerRef = useRef<HTMLDivElement | null>(null);
+  const [userVoteRef, setUserVoteRef] = useState<HTMLDivElement | null>(null);
+
+  const normalizeCoords = useCallback(
+    (clientX: number, clientY: number) => {
+      if (containerRef.current) {
+        const bounds = containerRef.current.getBoundingClientRect();
+        if (
+          clientX > bounds.left &&
+          clientX < bounds.right &&
+          clientY > bounds.top &&
+          clientY < bounds.bottom
+        ) {
+          return {
+            x: (clientX - bounds.left) / bounds.width,
+            y: (clientY - bounds.top) / bounds.height,
+          };
+        }
+      }
+      return null;
+    },
+    [containerRef]
+  );
+
+  const [addSticker] = useMutation(addForumEventStickerQuery);
+  const [removeSticker] = useMutation(removeForumEventStickerQuery);
+
+  const showHoverHeart = !currentUserHeart;
+
+  const saveStickerPos = useCallback(
+    async (event: React.MouseEvent) => {
+      if (!currentForumEvent) return;
+
+      // When a logged-in user is done dragging their vote, attempt to save it
+      if (currentUser) {
+        const coords = normalizeCoords(event.clientX, event.clientY);
+
+        if (!coords || currentUserHeart) return;
+
+        if (currentForumEvent.post) {
+          setCommentFormOpen(true);
+        }
+
+        console.log("Adding sticker", { coords });
+
+        // setCurrentUserVote(newVotePos);
+        await addSticker({
+          variables: {
+            ...coords,
+            theta: hoverTheta,
+            forumEventId: currentForumEvent._id,
+          },
+        });
+        // TODO handle the user's vote changing faster than this
+        refetch?.();
+      } else {
+        onSignup();
+      }
+    },
+    [currentForumEvent, currentUser, normalizeCoords, currentUserHeart, addSticker, hoverTheta, refetch, onSignup]
+  );
+
+  const clearSticker = useCallback(async () => {
+    if (!currentForumEvent) return;
+
+    await removeSticker({ variables: { forumEventId: currentForumEvent!._id } });
+    refetch?.();
+  }, [currentForumEvent, refetch, removeSticker])
+
+  const handleMouseMove = useCallback(
+    (event: React.MouseEvent) => {
+      if (!showHoverHeart) return;
+
+      const coords = normalizeCoords(event.clientX, event.clientY);
+      if (coords) {
+        setHoverPos(coords);
+      } else {
+        setHoverPos(null);
+      }
+    },
+    [normalizeCoords, showHoverHeart]
+  );
+
+  const handleMouseLeave = useCallback(() => {
+    if (!showHoverHeart) return;
+
+    setHoverPos(null);
+  }, [showHoverHeart]);
+
+  if (!currentForumEvent) return null;
+
+  return (
+    // TODO analytics
+    <>
+      <div
+        className={classes.heartsContainer}
+        ref={containerRef}
+        {...(interactive && {
+          onMouseMove: handleMouseMove,
+          onMouseLeave: handleMouseLeave,
+          onClick: saveStickerPos,
+        })}
+      >
+        {showHoverHeart && hoverPos && (
+          <ForumEventSticker
+            x={hoverPos.x}
+            y={hoverPos.y}
+            theta={hoverTheta}
+            icon="Heart"
+            className={classNames(classes.heart, classes.hoverHeart)}
+            classes={classes}
+          />
+        )}
+        {currentUserHeart && (
+          <ForumEventSticker
+            {...currentUserHeart}
+            tooltipDisabled={commentFormOpen}
+            icon="Heart"
+            className={classes.heart}
+            ref={setUserVoteRef}
+            onClear={clearSticker}
+            classes={classes}
+          />
+        )}
+        {otherHearts.map((heart, index) => (
+          <ForumEventSticker
+            key={index}
+            {...heart}
+            icon="Heart"
+            className={classes.heart}
+            classes={classes}
+          />
+        ))}
+      </div>
+      {currentForumEvent.post && (
+        <ForumEventCommentForm
+          open={commentFormOpen}
+          comment={currentUserHeart?.comment || null}
+          forumEventId={currentForumEvent._id}
+          onClose={() => setCommentFormOpen(false)}
+          refetch={refetchComments}
+          anchorEl={userVoteRef}
+          post={currentForumEvent.post}
+          title="Where did you donate this year?"
+          subtitle={(post, comment) => (<>
+            <div>
+              Your response will appear as a comment on{" "}
+              <Link to={comment ? commentGetPageUrlFromIds({postId: comment.postId, commentId: comment._id}) : postGetPageUrl(post)} target="_blank" rel="noopener noreferrer">
+                this post
+              </Link>
+              , and show on hover on this banner.
+            </div>
+          </>)}
+        />
+      )}
+    </>
+  );
+};
+
+const ForumEventStickersComponent = registerComponent( 'ForumEventStickers', ForumEventStickers, {styles});
+
+export default ForumEventStickersComponent;
+
+declare global {
+  interface ComponentTypes {
+    ForumEventStickers: typeof ForumEventStickersComponent;
+  }
+}
