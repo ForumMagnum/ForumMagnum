ForumMagnum/ForumMagnum

commit 29972a4dce92893864075d502d3a9366bba93bcd

update font

packages/lesswrong/components/sunshineDashboard/SunshineCuratedSuggestionsItem.tsx
@@ -18,8 +18,15 @@ const styles = (theme: ThemeType): JssStyles => ({
     position: "relative",
     top: 2
   },
+  postTitle: {
+    ...theme.typography.body2,
+    ...theme.typography.postStyle,
+    fontSize: "1rem",
+    fontWeight: 500,
+  },
   titleWithCurationNotice: {
     color: 'green',
+    fontWeight: 600,
   },
 });
 
@@ -94,7 +101,7 @@ const SunshineCuratedSuggestionsItem = ({classes, post, setCurationPost}: {
           <PostsHighlight post={post} maxLengthWords={600}/>
         </SidebarHoverOver>
         <Link to={postGetPageUrl(post)}
-          className={classNames('sunshine-sidebar-posts-title', {
+          className={classNames(classes.postTitle, {
             [classes.titleWithCurationNotice]: !!(post.curationNotices.length > 0),
           })}
         >

commit 42fa4c9425551780fc456a4c5df95221916cfa85

Fix styles for post titles in sunshine side bar
Regression from commit 29972a4dce92893864075d502d3a9366bba93bcd in PR #10057

packages/lesswrong/components/sunshineDashboard/SunshineCuratedSuggestionsItem.tsx
@@ -9,6 +9,7 @@ import { useHover } from '../common/withHover'
 import withErrorBoundary from '../common/withErrorBoundary'
 import * as _ from 'underscore';
 import classNames from 'classnames';
+import { isFriendlyUI } from '@/themes/forumTheme';
 
 const styles = (theme: ThemeType): JssStyles => ({
   audioIcon: {
@@ -18,13 +19,13 @@ const styles = (theme: ThemeType): JssStyles => ({
     position: "relative",
     top: 2
   },
-  postTitle: {
+  postTitle: isFriendlyUI ? {} : {
     ...theme.typography.body2,
     ...theme.typography.postStyle,
     fontSize: "1rem",
     fontWeight: 500,
   },
-  titleWithCurationNotice: {
+  titleWithCurationNotice: isFriendlyUI ? {} : {
     color: 'green',
     fontWeight: 600,
   },