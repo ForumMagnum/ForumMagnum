6a2fe66a06dc507279d48450c94d789b0c008191
Ollie Etherington <ollie@etherington.xyz>
2024-12-19 13:21:48 +0000

    Fix wrapped notifications

--- packages/lesswrong/server/scripts/sendWrappedNotifications.ts
+++ packages/lesswrong/server/scripts/sendWrappedNotifications.ts
@@ -3,34 +3,39 @@ import ReadStatuses from "../../lib/collections/readStatus/collection";
 import Users from "../../lib/collections/users/collection";
 import { createNotifications } from "../notificationCallbacksHelpers";
 import { Globals } from "../vulcan-lib";
+import { WrappedYear, isWrappedYear } from "@/components/ea-forum/wrapped/hooks";
 
-const sendWrappedNotifications = async () => {
-  // notify all users who read a post in 2023
-  const start = new Date(2023, 0)
-  const end = new Date(2024, 0)
+const sendWrappedNotifications = async (year: WrappedYear) => {
+  if (!isWrappedYear(year)) {
+    throw new Error(`${year} is not a valid wrapped year`);
+  }
+
+  // notify all users who read a post the given year
+  const start = new Date(year, 0)
+  const end = new Date(year + 1, 0)
   const readStatuses = await ReadStatuses.find({
     isRead: true,
     lastUpdated: {$gte: start, $lte: end},
     postId: {$ne: null}
-  }, {projection: {userId: 1}}).fetch()
-  
+  }, {}, {userId: 1}).fetch();
+
   const userIds = uniq(readStatuses.map(rs => rs.userId))
-  // filter out users who are banned, deleted, or have unsubscribed from all emails
+  // filter out users who are banned or deleted
   const users = await Users.find({
     _id: {$in: userIds},
     banned: {$exists: false},
-    unsubscribeFromAll: {$ne: true},
     deleted: {$ne: true},
-  }, {projection: {slug: 1}}).fetch()
+  }, {}, {slug: 1}).fetch();
 
   // eslint-disable-next-line no-console
-  console.log(`Sending Wrapped notifications to ${users.length} users`)
+  console.log(`Sending Wrapped ${year} notifications to ${users.length} users`);
 
   void createNotifications({
     userIds: users.map(u => u._id),
     notificationType: 'wrapped',
     documentId: null,
-    documentType: null
+    documentType: null,
+    extraData: {year},
   })
 }
 
