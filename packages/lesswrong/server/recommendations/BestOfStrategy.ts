e17983fae3e7422a48f134c527c610f92092eb18
Sarah Cheng <sarah.cheng@centreforeffectivealtruism.org>
2025-01-03 13:23:10 -0500

    [EA Forum only] Wrapped: re-add thank you page, links open in new tab, DM success message

--- packages/lesswrong/server/recommendations/BestOfStrategy.ts
+++ packages/lesswrong/server/recommendations/BestOfStrategy.ts
@@ -11,7 +11,7 @@ class BestOfStrategy extends FeatureStrategy {
   constructor() {
     // Since this strategy is used infrequently (currently just in Wrapped?),
     // we want to allow more flexibility for posts to get re-recommended.
-    super({maxRecommendationCount: 4})
+    super({maxRecommendationCount: 5})
   }
   
   async recommend(
