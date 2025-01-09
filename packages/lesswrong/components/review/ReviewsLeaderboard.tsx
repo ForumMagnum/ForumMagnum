f3a792b8eb15db67dc667638c891c350c97e2382
Raemon <raemon777@gmail.com>
2025-01-08 19:12:32 -0800

    - Added current user context to display their review row in the leaderboard. - Introduced a new type definition for ReviewLeaderboardRow for better type safety. - extracting the rendering of user rows into a separate function. - Adjusted styles for the leaderboard, including margin changes for better spacing. - Ensured that the leaderboard correctly handles cases where user data may be missing.

--- packages/lesswrong/components/review/ReviewsLeaderboard.tsx
+++ packages/lesswrong/components/review/ReviewsLeaderboard.tsx
@@ -4,6 +4,7 @@ import groupBy from 'lodash/groupBy';
 import sortBy from 'lodash/sortBy';
 import Card from '@material-ui/core/Card';
 import { ReviewYear } from '../../lib/reviewUtils';
+import { useCurrentUser } from '../common/withUser';
 
 const styles = (theme: ThemeType): JssStyles => ({
   root: {
@@ -27,7 +28,8 @@ const styles = (theme: ThemeType): JssStyles => ({
   },
   showAll: {
     color: theme.palette.primary.main,
-    marginTop: 16,
+    marginTop: 12,
+    marginBottom: 12,
     display: "block"
   },
   reviews: {
@@ -38,11 +40,18 @@ const styles = (theme: ThemeType): JssStyles => ({
   }
 });
 
+type ReviewLeaderboardRow = {
+  user: UsersMinimumInfo,
+  totalKarma: number,
+  reviews: CommentsListWithParentMetadata[]
+}
+
 export const ReviewsLeaderboard = ({classes, reviews, reviewYear}: {
   classes: ClassesType,
   reviews?: CommentsListWithParentMetadata[],
   reviewYear?: ReviewYear
 }) => {
+  const currentUser = useCurrentUser()
   const [truncated, setTruncated] = useState<boolean>(true)
   const { UsersNameDisplay, Row, MetaInfo, LWTooltip, CommentsNode } = Components
 
@@ -58,11 +67,14 @@ export const ReviewsLeaderboard = ({classes, reviews, reviewYear}: {
   const userRows = Object.entries(groupBy(reviews, (review) => review.userId))
   const userRowsMapping = userRows.map(userTuple => {
     const user = userTuple[1][0].user
+    if (!user) {
+      return null
+    }
     return ({
       user: user,
       totalKarma: userTuple[1].reduce((value, review) => value + review.baseScore - getSelfUpvotePower(user), 0),
       reviews: sortBy(userTuple[1], obj => -obj.baseScore)
-  })})
+    })}).filter(userRow => userRow !== null)
 
   const NUM_DEFAULT_REVIEWS = 10
 
@@ -70,6 +82,31 @@ export const ReviewsLeaderboard = ({classes, reviews, reviewYear}: {
   const truncatedRows = truncated ? sortedUserRows.slice(0,NUM_DEFAULT_REVIEWS) : sortedUserRows
 
   const totalKarma = reviews?.reduce((v, r) => v + r.baseScore, 0)
+
+  const reviewLeaderboardRow = (reviewUser: ReviewLeaderboardRow|null) => {
+    if (!reviewUser) {
+      return null
+    }
+    return <div className={classes.row} key={reviewUser.user?._id} >
+      <Row justifyContent="flex-start">
+        <div className={classes.username}>
+          <UsersNameDisplay user={reviewUser.user}/>
+        </div>
+        <div className={classes.karma}>
+          {reviewUser.totalKarma}
+        </div>
+        <div className={classes.reviews}>{reviewUser.reviews.map(review => {
+          return <LWTooltip placement="bottom-start" title={<div className={classes.card}><CommentsNode treeOptions={{showPostTitle: true}} comment={review}/></div>} tooltip={false} key={review._id}>
+            <a href={`/reviews/${reviewYear ?? "all"}#${review._id}`} onClick={() => setTruncated(true)}>
+              <MetaInfo>{review.baseScore - getSelfUpvotePower(review.user)}</MetaInfo>
+            </a>
+          </LWTooltip>
+          })}</div>
+      </Row>
+    </div>
+  }
+
+  const currentUserRow = sortedUserRows.find(userRow => userRow.user?._id === currentUser?._id)
 
   return <div className={classes.root}>
     <Row>
@@ -80,25 +117,12 @@ export const ReviewsLeaderboard = ({classes, reviews, reviewYear}: {
       </div>}
     </Row>
     {truncatedRows.map(reviewUser => {
-      return <div className={classes.row} key={reviewUser.user?._id} >
-        <Row justifyContent="flex-start">
-          <div className={classes.username}>
-            <UsersNameDisplay user={reviewUser.user}/>
-          </div>
-          <div className={classes.karma}>
-            {reviewUser.totalKarma}
-          </div>
-          <div className={classes.reviews}>{reviewUser.reviews.map(review => {
-            return <LWTooltip placement="bottom-start" title={<div className={classes.card}><CommentsNode treeOptions={{showPostTitle: true}} comment={review}/></div>} tooltip={false} key={review._id}>
-              <a href={`/reviews/${reviewYear ?? "all"}#${review._id}`} onClick={() => setTruncated(true)}>
-                <MetaInfo>{review.baseScore - getSelfUpvotePower(review.user)}</MetaInfo>
-              </a>
-            </LWTooltip>
-          })}</div>
-        </Row>
-      </div>
+      return reviewLeaderboardRow(reviewUser)
     })}
     {sortedUserRows.length > NUM_DEFAULT_REVIEWS && <a className={classes.showAll} onClick={() => setTruncated(!truncated)}>{truncated ? <>Show All Reviewers ({NUM_DEFAULT_REVIEWS}/{sortedUserRows.length})</> : "Show Fewer"}</a>}
+    {currentUserRow && truncated && !truncatedRows.find(row => row.user?._id === currentUser?._id) && sortedUserRows.length >= NUM_DEFAULT_REVIEWS && 
+      reviewLeaderboardRow(currentUserRow)
+    }
   </div>
 }
 
