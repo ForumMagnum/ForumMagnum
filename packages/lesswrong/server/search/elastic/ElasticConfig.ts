fb19fabf4a1ed623102f01e8adfc1a4262b01153
Ruben Bloom <darkruby501@users.noreply.github.com>
2025-01-13 12:36:43 -0800

    remove unnecessary karma field specification

--- packages/lesswrong/server/search/elastic/ElasticConfig.ts
+++ packages/lesswrong/server/search/elastic/ElasticConfig.ts
@@ -390,7 +390,6 @@ const elasticSearchConfig: Record<SearchIndexCollectionName, IndexConfig> = {
         scoring: {type: "numeric", pivot: 10},
       },
     ],
-    karmaField: "baseScore",
     tiebreaker: "postCount",
     filters: [
       {term: {deleted: false}},
