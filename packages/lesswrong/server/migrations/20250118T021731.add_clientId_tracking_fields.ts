57ec1a3e1268b95b7b6aebdcbc3b8d1769e3e038
Robert Mushkatblat <bobert.mushky@gmail.com>
2025-01-17 19:10:52 -0800

    Add invalidation and lastSeen/timesSeen fields to clientIds
    Co-authored-by: jimrandomh <jim@jimrandomh.org>

---
+++ packages/lesswrong/server/migrations/20250118T021731.add_clientId_tracking_fields.ts
@@ -0,0 +1,14 @@
+import { ClientIds } from "@/lib/collections/clientIds/collection"
+import { addField, dropField } from "./meta/utils"
+
+export const up = async ({db}: MigrationContext) => {
+  await addField(db, ClientIds, 'invalidated');
+  await addField(db, ClientIds, 'lastSeenAt');
+  await addField(db, ClientIds, 'timesSeen');
+}
+
+export const down = async ({db}: MigrationContext) => {
+  await dropField(db, ClientIds, 'invalidated');
+  await dropField(db, ClientIds, 'lastSeenAt');
+  await dropField(db, ClientIds, 'timesSeen');
+}
