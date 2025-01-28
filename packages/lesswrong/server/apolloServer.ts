06f4207907b4aabb38f3d128e15cca27a268a8fa
James Babcock <jimrandomh@gmail.com>
2025-01-28 10:59:23 -0800

    Remove `cacheControl` option from apollo-server
    When we set up apollo-server, we were passing it `cacheControl: true`.
    This adds some metadata to the response in each graphql request, which
    lists each object-path saying that its max-age (for Apollo Engine, a
    caching middleware that we don't use and have never used) is zero.
    
    Remove that flag. I believe this should have no effect on behavior
    except for making graphql requests slightly smaller.

--- packages/lesswrong/server/apolloServer.ts
+++ packages/lesswrong/server/apolloServer.ts
@@ -241,7 +241,6 @@ export function startWebserver() {
     },
     //tracing: isDevelopment,
     tracing: false,
-    cacheControl: true,
     context: async ({ req, res }: { req: express.Request, res: express.Response }) => {
       const context = await getContextFromReqAndRes({req, res, isSSR: false});
       configureSentryScope(context);
