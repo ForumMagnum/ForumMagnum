2f769f79faea0c2f1e954eafa5b361cb8c3a3d10
James Babcock <jimrandomh@gmail.com>
2025-03-04 16:25:37 -0800

    Don't use apollo-client replacement for cross-site queries

--- packages/lesswrong/lib/crud/useQuery.ts
+++ packages/lesswrong/lib/crud/useQuery.ts
@@ -4,7 +4,7 @@ import { type DocumentNode, type QueryHookOptions, type TypedDocumentNode, useQu
 export function useQuery<TData=any, TVariables=OperationVariables>(
   query: DocumentNode|TypedDocumentNode<TData,TVariables>, options?: QueryHookOptions<TData, TVariables>
 ): Omit<QueryResult<TData, TVariables>, "client"|"startPolling"|"stopPolling"|"subscribeToMore"|"updateQuery"> {
-  if (bundleIsServer) {
+  if (bundleIsServer && !options?.client) {
     // eslint-disable-next-line react-hooks/rules-of-hooks
     return useQueryServer<TData, TVariables>(query, options);
   } else {
