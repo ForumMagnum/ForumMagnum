894272f78ea18341a0f8770a31b9f33e06efc47f
Robert Mushkatblat <bobert.mushky@gmail.com>
2025-04-01 02:25:50 -0700

    update price id

--- packages/lesswrong/server/loot/checkoutSession.ts
+++ packages/lesswrong/server/loot/checkoutSession.ts
@@ -21,7 +21,7 @@ addStaticRoute('/loot-checkout-session', async (props, req, res) => {
 
   const checkoutSession = await stripe.checkout.sessions.create({
     line_items: [{
-      price: 'price_1R8pSiBlb9vL5IMTRWx553H3',
+      price: 'price_1R913oBlb9vL5IMTFXYNpFWR',
       quantity: 1,
     }],
     metadata: {
