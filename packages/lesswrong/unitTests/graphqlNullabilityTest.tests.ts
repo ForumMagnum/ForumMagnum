664d6d12c592fb5c93d8c0dc47f226f80512366c
James Babcock <jimrandomh@gmail.com>
2025-04-03 17:18:56 -0700

    Unit test enforces that if a resolver returns a type with a permissions-filter, it's nullable

--- packages/lesswrong/unitTests/graphqlNullabilityTest.tests.ts
+++ packages/lesswrong/unitTests/graphqlNullabilityTest.tests.ts
@@ -1,30 +1,64 @@
 import chai from 'chai';
 import { getSchema } from "@/lib/schema/allSchemas";
-import { getAllCollections } from "@/server/collections/allCollections";
+import { getAllCollections, getCollection, isValidCollectionName } from "@/server/collections/allCollections";
 import isArray from "lodash/isArray";
+import { getCollectionAccessFilter } from '@/server/permissions/accessFilters';
+import { typeNameToCollectionName } from '@/lib/generated/collectionTypeNames';
+
+type FieldProblem = {
+  collectionName: string
+  fieldName: string
+  message: string
+}
 
 describe("graphql schemas", () => {
   it("Has all permissions-gated fields nullable", () => {
     const collections = getAllCollections();
-    const badFields: string[] = [];
+    const badFields: FieldProblem[] = [];
 
     for (const collection of collections) {
       const schema = getSchema(collection.collectionName);
       for (const [name,field] of Object.entries(schema)) {
         if (field.graphql) {
           const dbNullable = field.database?.nullable;
-          const permissionsGated = field.graphql.canRead && !permissionsAllowGuest(field.graphql.canRead);
+          const fieldPermissionsGated = field.graphql.canRead && !permissionsAllowGuest(field.graphql.canRead);
           const outputType = field.graphql.outputType;
           const graphqlNullable = (typeof outputType !== 'string') || !outputType.endsWith("!");
-          if ((dbNullable || permissionsGated) && !graphqlNullable) {
-            badFields.push(`${collection.collectionName}.${name}`);
+          if (!graphqlNullable) {
+            if (dbNullable) {
+              badFields.push({
+                collectionName: collection.collectionName,
+                fieldName: name,
+                message: "GraphQL outputType is not-nullable, but the corresponding database field is nullable"
+              });
+            }
+            if (fieldPermissionsGated) {
+              badFields.push({
+                collectionName: collection.collectionName,
+                fieldName: name,
+                message: "GraphQL outputType is not-nullable, but the field is permissions-gated"
+              });
+            }
+          }
+          if (typeof outputType === 'string' && outputType.endsWith("!")) {
+            const typeName = outputType.substring(0, outputType.length-1);
+            const maybeCollectionName = (typeNameToCollectionName as any)[typeName] ?? null;
+            if (isValidCollectionName(maybeCollectionName)
+              && collectionHasRowLevelPermissions(maybeCollectionName))
+            {
+              badFields.push({
+                collectionName: collection.collectionName,
+                fieldName: name,
+                message: "GraphQL output is not-nullable, but points to a collection that has an access filter"
+              });
+            }
           }
         }
       }
     }
     
     if (badFields.length > 0) {
-      console.error(`The following fields have a non-nullable graphql type, but have a nullable database type or a restrictive permission that means the field will be missing for some users:\n${badFields.join("\n")}`);
+      console.error(`The following fields have a non-nullable graphql type, but have a nullable database type or a restrictive permission that means the field will be missing for some users:\n${badFields.map(f => `${f.collectionName}.${f.fieldName}: ${f.message}`).join("\n")}`);
       chai.assert.equal(badFields.length, 0);
     }
   })
@@ -43,3 +77,8 @@ export function permissionsAllowGuest(permissions: FieldPermissions) {
   }
   return false;
 }
+
+function collectionHasRowLevelPermissions(collectionName: CollectionNameString) {
+  return !!getCollectionAccessFilter(collectionName);
+}
+
