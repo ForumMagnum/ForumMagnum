71b620e987f0bc30e86da1bd3644966b4073bb51
James Babcock <jimrandomh@gmail.com>
2025-04-15 12:14:02 -0700

    Actually update the scores, don't just recompute them

--- packages/lesswrong/server/scripts/recalculateDocumentScoresOnCollection.ts
+++ packages/lesswrong/server/scripts/recalculateDocumentScoresOnCollection.ts
@@ -5,14 +5,24 @@ import { recalculateDocumentScores } from "../voteServer";
 import { createAnonymousContext } from "../vulcan-lib/createContexts";
 
 export async function recalculateDocumentScoresOnCollection(collectionName: VoteableCollectionName) {
+  const collection = getCollection(collectionName);
   const context = createAnonymousContext();
   await forEachDocumentBatchInCollection({
-    collection: getCollection(collectionName),
+    collection: collection,
     batchSize: 100,
     callback: async (batch) => {
       await executePromiseQueue(batch.map(doc => async () => {
-        await recalculateDocumentScores(doc, collectionName, context)
+        const newScore = await recalculateDocumentScores(doc, collectionName, context)
+        const { baseScore, voteCount, afVoteCount, score, extendedScore, afExtendedScore } = newScore;
+        await collection.rawUpdateOne(
+          { _id: doc._id },
+          {$set: {
+            baseScore, voteCount, afVoteCount, score, extendedScore, afExtendedScore
+          }},
+        );
       }), 5);
     },
   });
+  // eslint-disable-next-line no-console
+  console.log("Done");
 }
