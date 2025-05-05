573ace7dc04af2ded50a66a4c62be9c0c09652ce
James Babcock <jimrandomh@gmail.com>
2025-05-05 02:42:13 -0700

    Lint

--- packages/lesswrong/unitTests/useDebouncedCallback.tests.tsx
+++ packages/lesswrong/unitTests/useDebouncedCallback.tests.tsx
@@ -1,9 +1,8 @@
 /**
  * @jest-environment jsdom
  */
-import React from 'react';
+import React, { act } from 'react';
 import { createRoot } from 'react-dom/client';
-import { act } from 'react';
 import { DebouncedCallbackOptions, useDebouncedCallback } from '../components/hooks/useDebouncedCallback';
 
 jest.useFakeTimers();
