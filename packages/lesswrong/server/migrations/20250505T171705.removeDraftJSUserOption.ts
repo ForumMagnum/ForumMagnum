3bcff5d29a45e18b4ac5880ea4f0425320ec0285
James Babcock <jimrandomh@gmail.com>
2025-05-05 14:16:24 -0700

    Add missing await

--- packages/lesswrong/server/migrations/20250505T171705.removeDraftJSUserOption.ts
+++ packages/lesswrong/server/migrations/20250505T171705.removeDraftJSUserOption.ts
@@ -2,7 +2,7 @@ import Users from "../collections/users/collection";
 import { dropField } from "./meta/utils";
 
 export const up = async ({db}: MigrationContext) => {
-  dropField(db, Users, "reenableDraftJs" as any);
+  await dropField(db, Users, "reenableDraftJs");
 }
 
 export const down = async ({db}: MigrationContext) => {
