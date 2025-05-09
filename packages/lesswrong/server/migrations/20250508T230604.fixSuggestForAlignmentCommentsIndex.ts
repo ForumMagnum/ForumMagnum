1a9f29e856af026a286c6e99fae7c6763155efec
James Babcock <jimrandomh@gmail.com>
2025-05-09 12:51:39 -0700

    Update indexes in migration

--- packages/lesswrong/server/migrations/20250508T230604.fixSuggestForAlignmentCommentsIndex.ts
+++ packages/lesswrong/server/migrations/20250508T230604.fixSuggestForAlignmentCommentsIndex.ts
@@ -1,4 +1,8 @@
+import { updateIndexes } from "./meta/utils";
+import Comments from "../collections/comments/collection";
+
 export const up = async ({db}: MigrationContext) => {
+  await updateIndexes(Comments);
 }
 
 export const down = async ({db}: MigrationContext) => {
