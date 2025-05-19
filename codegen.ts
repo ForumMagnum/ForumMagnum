e3b85cd3d661e686209221e16613a5c91054cac9
Robert Mushkatblat <bobert.mushky@gmail.com>
2025-05-19 12:54:30 -0700

    add some missing scripts

---
+++ codegen.ts
@@ -0,0 +1,94 @@
+import { pascalCase } from 'change-case';
+import type { CodegenConfig } from '@graphql-codegen/cli';
+
+const config: CodegenConfig = {
+  overwrite: true,
+  schema: "./packages/lesswrong/lib/generated/gqlSchema.gql",
+  documents: "./packages/lesswrong/",
+  generates: {
+    // One object for client
+    "./packages/lesswrong/lib/generated/gql-codegen/": {
+      preset: "client",
+      config: {
+        scalars: {
+          Date: {
+            input: 'Date',
+            output: 'string',
+          }
+        },
+        avoidOptionals: {
+          inputValue: false,
+          field: true,
+          object: true,
+          defaultValue: true,
+          resolvers: true,
+          query: true,
+          mutation: true,
+          subscription: true,
+        },
+        namingConvention: (s: string) => pascalCase(s).replace("Fragment", ""),
+        enumsAsTypes: true,
+      },
+      presetConfig: {
+        gqlTagName: 'gql',
+        fragmentMasking: false,
+      },
+    },
+    // One object for server
+    './packages/lesswrong/lib/generated/graphqlCodegenTypes.d.ts': {
+      plugins: [
+        {
+          typescript: {
+            scalars: {
+              Date: {
+                input: 'Date',
+                output: 'string',
+              }
+            }
+          }
+        }, {
+          'typescript-operations': {
+            scalars: {
+              Date: {
+                input: 'Date',
+                output: 'string',
+              }
+            },
+            avoidOptionals: {
+              inputValue: false,
+              field: true,
+              object: true,
+              defaultValue: true,
+              resolvers: true,
+              query: true,
+              mutation: true,
+              subscription: true,
+            },
+          }
+        }
+      ],
+      config: {
+        fragmentMasking: false,
+        inputMaybeValue: 'T | null | undefined',
+        namingConvention: (s: string) => pascalCase(s).replace("Fragment", ""),
+        enumsAsTypes: true,
+        inlineFragmentTypes: 'combine',
+        noExport: true,
+      }
+    },
+    "./client-schema.json": {
+      plugins: ["introspection"],
+      documents: ["./packages/lesswrong/lib/generated/gqlSchema.gql"],
+      config: {
+        minify: true,
+        descriptions: false,
+        schemaDescription: false,
+      },
+    },
+  },
+  hooks: {
+    afterAllFileWrite: ["node ./prune-schema.js > prune_stdout.log 2> prune_stderr.log"]
+  }
+};
+
+export default config;
