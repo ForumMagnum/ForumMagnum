a430d5a2acb809937437d2a01899edbae9546f63
James Babcock <jimrandomh@gmail.com>
2025-05-20 15:46:12 -0700

    Include react-instantsearch styles in our stylesheet rather than downloading from a third party

--- packages/lesswrong/themes/globalStyles/externalStyles.ts
+++ packages/lesswrong/themes/globalStyles/externalStyles.ts
@@ -1,6 +1,5 @@
 
 export const globalExternalStylesheets = [
-  "https://cdn.jsdelivr.net/npm/instantsearch.css@7.0.0/themes/reset-min.css",
   "https://fonts.googleapis.com/css?family=Roboto:300,400,500",
   "https://use.typekit.net/jvr1gjm.css",
   "https://use.typekit.net/tqv5rhd.css"
