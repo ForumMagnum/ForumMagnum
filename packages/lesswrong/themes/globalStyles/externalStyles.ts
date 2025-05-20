047e6cdfdbccddec060b99914cec82188bb55dd0
James Babcock <jimrandomh@gmail.com>
2025-05-20 13:58:57 -0700

    Remove Material Icons font
    The Material Icons font appears to be unused (all of the icons we use
    come from SVGs in the repo or in node_modules). Verified by downloading
    the external stylesheet, which contained a font name and a CSS class,
    and searching the codebase for usages of either, neither of which was
    present.

--- packages/lesswrong/themes/globalStyles/externalStyles.ts
+++ packages/lesswrong/themes/globalStyles/externalStyles.ts
@@ -1,6 +1,5 @@
 
 export const globalExternalStylesheets = [
-  "https://fonts.googleapis.com/icon?family=Material+Icons",
   "https://cdn.jsdelivr.net/npm/instantsearch.css@7.0.0/themes/reset-min.css",
   "https://fonts.googleapis.com/css?family=Roboto:300,400,500",
   "https://use.typekit.net/jvr1gjm.css",
