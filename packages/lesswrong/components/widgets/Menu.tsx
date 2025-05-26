eec64720c98c35f02c993c939b09feb8c3b71198
James Babcock <jimrandomh@gmail.com>
2025-05-26 15:00:59 -0700

    Fix a stray comma when comment menus are open

--- packages/lesswrong/components/widgets/Menu.tsx
+++ packages/lesswrong/components/widgets/Menu.tsx
@@ -39,7 +39,7 @@ export function Menu({open, anchorEl, onClose, onClick, minWidth, className, chi
         <Paper>
           {children}
         </Paper>
-     ,</LWClickAwayListener>
+      </LWClickAwayListener>
     </div>
   </MenuPopper>
 }
