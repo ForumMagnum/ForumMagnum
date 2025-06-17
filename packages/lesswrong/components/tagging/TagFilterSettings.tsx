180905b82b4409b94efcc688749ff2bc0a531243
James Babcock <jimrandomh@gmail.com>
2025-06-17 11:49:23 -0700

    Improve loading state of TagFilterSettings

--- packages/lesswrong/components/tagging/TagFilterSettings.tsx
+++ packages/lesswrong/components/tagging/TagFilterSettings.tsx
@@ -73,7 +73,6 @@ const TagFilterSettingsInner = ({
 }) => {
   const classes = useStyles(styles);
   const currentUser = useCurrentUser()
-  const suggestedTags = useReadSuggestedTags(filterSettings, suggestedTagsQueryRef);
 
   const {
     name: personalBlogpostName,
@@ -81,24 +80,19 @@ const TagFilterSettingsInner = ({
   } = usePersonalBlogpostInfo();
 
   return <>
-      {[...filterSettings.tags, ...suggestedTags].map(tagSettings =>
-        <FilterMode
-          label={tagSettings.tagName}
-          key={tagSettings.tagId}
-          tagId={tagSettings.tagId}
-          mode={tagSettings.filterMode}
-          canRemove={true}
-          onChangeMode={(mode: FilterModeType) => {
-            // If user has clicked on, eg, "Hidden" after it's already selected, return it to default
-            // ... but don't apply that to manually input filter settings
-            const newMode = mode === tagSettings.filterMode && !isCustomFilterMode(mode) ? 0 : mode
-            setTagFilter({tagId: tagSettings.tagId, tagName: tagSettings.tagName, filterMode: newMode})
-          }}
-          onRemove={() => {
-            removeTagFilter(tagSettings.tagId)
-          }}
+      <FilterModeGroup
+        tags={filterSettings.tags}
+        setTagFilter={setTagFilter}
+        removeTagFilter={removeTagFilter}
+      />
+      <SuspenseWrapper name="SuggestedTags">
+        <SuggestedFiltersGroup
+          filterSettings={filterSettings}
+          suggestedTagsQueryRef={suggestedTagsQueryRef}
+          setTagFilter={setTagFilter}
+          removeTagFilter={removeTagFilter}
         />
-      )}
+      </SuspenseWrapper>
   
       {/* Combine these two in one div to make sure that there's never a single element on the second row, if there's overflow */}
       <div className={classes.personalAndPlus}>
@@ -125,6 +119,45 @@ const TagFilterSettingsInner = ({
       </div>
       {flexWrapEndGrow && <div className={classes.flexWrapEndGrow} />}
   </>
+}
+
+const SuggestedFiltersGroup = ({filterSettings, suggestedTagsQueryRef, setTagFilter, removeTagFilter}: {
+  filterSettings: FilterSettings,
+  suggestedTagsQueryRef: QueryRef<ResultOf<typeof TagBasicInfoMultiQuery>>
+  setTagFilter: (args: {tagId: string, tagName?: string, filterMode: FilterModeType}) => void,
+  removeTagFilter: (tagId: string) => void,
+}) => {
+  const suggestedTags = useReadSuggestedTags(filterSettings, suggestedTagsQueryRef);
+  return <FilterModeGroup
+    tags={suggestedTags}
+    setTagFilter={setTagFilter}
+    removeTagFilter={removeTagFilter}
+  />
+}
+
+const FilterModeGroup = ({tags, setTagFilter, removeTagFilter}: {
+  tags: FilterTag[]
+  setTagFilter: (args: {tagId: string, tagName?: string, filterMode: FilterModeType}) => void,
+  removeTagFilter: (tagId: string) => void,
+}) => {
+  return <>{
+    tags.map(tagSettings => <FilterMode
+      label={tagSettings.tagName}
+      key={tagSettings.tagId}
+      tagId={tagSettings.tagId}
+      mode={tagSettings.filterMode}
+      canRemove={true}
+      onChangeMode={(mode: FilterModeType) => {
+        // If user has clicked on, eg, "Hidden" after it's already selected, return it to default
+        // ... but don't apply that to manually input filter settings
+        const newMode = mode === tagSettings.filterMode && !isCustomFilterMode(mode) ? 0 : mode
+        setTagFilter({tagId: tagSettings.tagId, tagName: tagSettings.tagName, filterMode: newMode})
+      }}
+      onRemove={() => {
+        removeTagFilter(tagSettings.tagId)
+      }}
+    />
+  )}</>
 }
 
 /**
