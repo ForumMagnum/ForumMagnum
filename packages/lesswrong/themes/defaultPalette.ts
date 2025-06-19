1ea236695efea2eb6cefaa985978c62dbe0f0176
James Babcock <jimrandomh@gmail.com>
2025-06-18 21:14:16 -0700

    Also fix incorrect color after theme-override-nav for Intercom button

--- packages/lesswrong/themes/defaultPalette.ts
+++ packages/lesswrong/themes/defaultPalette.ts
@@ -493,6 +493,9 @@ export const defaultComponentPalette = (shades: ThemeShadePalette): ThemeCompone
       hoverBackground: shades.grey[800],
     },
   },
+  intercom: {
+    buttonBackground: "unset",
+  },
   sideItemIndicator: {
     sideComment: '#5f9b65',
     inlineReaction: 'lch(68 34.48 85.39 / 76%)',
