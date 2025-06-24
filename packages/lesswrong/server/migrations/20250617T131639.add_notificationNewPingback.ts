a4bdc614e09372b0b7ca028dc4c59538916a95ea
Ollie Etherington <ollie@etherington.xyz>
2025-06-24 10:27:49 +0100

    Update pingback notifications settings for existing users

--- packages/lesswrong/server/migrations/20250617T131639.add_notificationNewPingback.ts
+++ packages/lesswrong/server/migrations/20250617T131639.add_notificationNewPingback.ts
@@ -1,8 +1,17 @@
 import { addField, dropField } from "./meta/utils";
 import Users from "../collections/users/collection";
+import { isEAForum } from "@/lib/instanceSettings";
+import { emailEnabledNotificationSettingOnCreate } from "@/lib/collections/users/notificationFieldHelpers";
 
 export const up = async ({db}: MigrationContext) => {
   await addField(db, Users, "notificationNewPingback");
+  if (isEAForum) {
+    await Users.rawUpdateMany({}, {
+      $set: {
+        notificationNewPingback: emailEnabledNotificationSettingOnCreate,
+      },
+    });
+  }
 }
 
 export const down = async ({db}: MigrationContext) => {
