c769f5b2db4a37c87afe6c460148d9eab3a6556b
James Babcock <jimrandomh@gmail.com>
2025-06-25 15:51:13 -0700

    Fix hang in addBookmarksCount migration

--- packages/lesswrong/server/migrations/20250624T190405.addBookmarksCount.ts
+++ packages/lesswrong/server/migrations/20250624T190405.addBookmarksCount.ts
@@ -4,7 +4,7 @@ import { addField } from "./meta/utils";
 
 export const up = async ({db, dbOutsideTransaction}: MigrationContext) => {
   await addField(db, Users, "bookmarksCount");
-  await new BookmarksRepo(dbOutsideTransaction).updateBookmarkCountForAllUsers();
+  void new BookmarksRepo(dbOutsideTransaction).updateBookmarkCountForAllUsers();
 }
 
 export const down = async ({db}: MigrationContext) => {
