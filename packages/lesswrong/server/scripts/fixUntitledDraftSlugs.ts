1c825383c924ed40ebe0b97ab8c75b41baccc242
James Babcock <jimrandomh@gmail.com>
2025-07-02 14:35:43 -0700

    Fix posts occasionally having the slug "Untitled Draft"
    Fixes a bug where if you edit a post's title to empty and then edit it
    to something nonempty, it doesn't update the slug. Adds a script to fix
    cases where posts have the slug "untitled-draft-..." because that was
    the starting title, and it was affected by this bug.

---
+++ packages/lesswrong/server/scripts/fixUntitledDraftSlugs.ts
@@ -0,0 +1,34 @@
+import { runSqlQuery } from "../sql/sqlClient";
+import { getUnusedSlugByCollectionName } from "../utils/slugUtil";
+import Posts from "../collections/posts/collection";
+import { slugify } from "@/lib/utils/slugify";
+import { updatePost } from "../collections/posts/mutations";
+import { createAnonymousContext } from "../vulcan-lib/createContexts";
+
+export default async function fixUntitledDraftSlugs() {
+  const postIdsWithUntitledDraftSlugs = (await runSqlQuery(`
+    SELECT _id
+    FROM "Posts"
+    WHERE LOWER(title) <> LOWER('Untitled Draft')
+    AND slug LIKE 'untitled-draft%'
+  `)).map(row => row._id);
+  
+  console.log(`Found ${postIdsWithUntitledDraftSlugs.length} posts with slug untitled-draft-* with different titles`);
+  for (const postId of postIdsWithUntitledDraftSlugs) {
+    await regenerateSlugForPostId(postId);
+  }
+}
+
+async function regenerateSlugForPostId(postId: string) {
+  const post = await Posts.findOne({_id: postId});
+  if (!post) throw new Error("Invalid post ID");
+  const { title, slug: oldSlug } = post;
+  const newSlug = await getUnusedSlugByCollectionName("Posts", slugify(title), false, post._id);
+  if (oldSlug !== newSlug) {
+    console.log(`Changing slug of post ${postId} from ${oldSlug} to ${newSlug}`);
+    await updatePost({
+      selector: { _id: postId },
+      data: { slug: newSlug },
+    }, createAnonymousContext());
+  }
+}
