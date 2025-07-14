86c670164cafa222847e41bb6e548ef99ad0a1c7
Ollie Etherington <ollie@etherington.xyz>
2025-07-14 14:12:22 +0100

    Add better 'unsubscribed from all emails' message

--- packages/lesswrong/components/users/emailTokens.ts
+++ packages/lesswrong/components/users/emailTokens.ts
@@ -1,6 +1,8 @@
 import { EmailTokenResult } from "./EmailTokenResult";
+import EmailTokenEmailUnsubscribeResult from "./EmailTokenEmailUnsubscribeResult";
 import type { EmailTokenResultComponentName } from "@/server/emails/emailTokens";
 
 export const emailTokenResultComponents = {
   EmailTokenResult,
+  EmailTokenEmailUnsubscribeResult,
 } satisfies Record<EmailTokenResultComponentName, React.ComponentType<any>>;
