35601365f6531d28fb75653fd3b2e2b53e9deb77
James Babcock <jimrandomh@gmail.com>
2025-07-21 16:57:59 -0700

    Remove unused and push styles out of miscStyles

--- packages/lesswrong/themes/globalStyles/miscStyles.ts
+++ packages/lesswrong/themes/globalStyles/miscStyles.ts
@@ -1,31 +1,4 @@
 export default (): string => `
-/*
- * TODO: These classes are used by 248 posts in the database that were authored
- * while LW2 was using ory-editor, which we have since migrated away from.
- * Getting rid of these styles safely requires updating the formatting of those
- * posts (or at least figuring out what this style actually does).
- */
-
-/* legacyBreakpoints.maxSmall */
-@media screen and (max-width: 715px) {
-  .ory-cell-inner.ory-cell-leaf > div > div > div > div {
-    padding-left:0 !important;
-    padding-top:0 !important;
-    padding-bottom:0 !important;
-  }
-
-  .ory-cell-inner.ory-cell-leaf button {
-    width:20px !important;
-    height:20px !important;
-    margin:0 3px !important;
-  }
-  .ory-cell-inner.ory-cell-leaf > div > div > div > div {
-    display:none !important;
-  }
-}
-
-/* ************************************************************************ */
-
 .editor {
   min-height: 220px;
   border-radius: 2px;
@@ -77,21 +50,6 @@ figure:after {
 
 .form-component-CommentEditor {
   position: static !important;
-}
-
-.image-form-invalid {
-  color: #e65757;
-}
-
-.image-form {
-  height: 34px;
-  width: 220px;
-  padding: 0 12px;
-  font-size: 15px;
-  font-family: inherit;
-  background-color: transparent;
-  border: none;
-  color: #444;
 }
 
 .form-component-EditorFormComponent {
@@ -151,27 +109,12 @@ html {
 
 /* //////////////////////////////////////////////////////////////////////// */
 
-/* TODO: Possibly used by a library, or possibly unused */
-.modal-dialog {
-  margin-top: 100px !important;
-}
-
 .form-cancel {
   margin-left:25px;
 }
 
 .form-component-MuiInput {
   margin-bottom: 0px;
-}
-
-.multi-select-buttons-button {
-  border-radius: 0px !important;
-  text-transform: none !important;
-  min-width: 63px !important;
-}
-
-.multi-select-buttons-label {
-  margin-right: 10px;
 }
 
 .form-component-AlignmentCheckbox {
@@ -190,32 +133,6 @@ html {
 
 h3 {
   margin-top: 0px;
-}
-
-
-/* Deactivate intercom on smaller devices */
-
-/* theme.breakpoints.down('sm') */
-@media screen and (max-width: 959px) {
-  #intercom-container, .intercom-lightweight-app {
-    display: none;
-  }
-}
-
-
-div#mocha {
-  background:white;
-  border-right: 2px solid black;
-  height: 100%;
-  left: 0;
-  margin: 0;
-  overflow: auto;
-  padding: 1rem;
-  position: fixed;
-  resize: horizontal;
-  top: 0;
-  width: 20px;
-  z-index: 10000000;
 }
 
 /*
