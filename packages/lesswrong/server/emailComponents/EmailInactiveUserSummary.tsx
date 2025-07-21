c20251dc5fbbff07acb5ed03e79e461a9921e958
Ollie Etherington <ollie@etherington.xyz>
2025-07-21 14:59:19 +0100

    Hide notifications section when there's nothing to display

--- packages/lesswrong/server/emailComponents/EmailInactiveUserSummary.tsx
+++ packages/lesswrong/server/emailComponents/EmailInactiveUserSummary.tsx
@@ -75,6 +75,17 @@ export const EmailInactiveUserSummary = ({
   unreadNotifications: number,
   recommendedPosts: PostsList[],
 }) => {
+  const showKarmaChange = karmaChange > 0;
+  const showBestReaction = bestReaction && bestReaction.count > 0;
+  const showMostCommentedPost = mostCommentedPost && mostCommentedPost.commentCount > 0;
+  const showUnreadNotifications = unreadNotifications > 0;
+
+  const showNotificationsSection =
+    showKarmaChange ||
+    showBestReaction ||
+    showMostCommentedPost ||
+    showUnreadNotifications;
+
   const classes = useStyles(styles);
   return (
     <div className={classes.root}>
@@ -86,39 +97,44 @@ export const EmailInactiveUserSummary = ({
         things you’ve missed.
       </p>
       <div className={classes.spacing} />
-      <h2 className={classes.heading}>
-        Your notifications
-      </h2>
-      {karmaChange > 0 &&
-        <p>
-          You earned {karmaChange} karma.
-        </p>
+      {showNotificationsSection &&
+        <>
+          <h2 className={classes.heading}>
+            Your notifications
+          </h2>
+          {showKarmaChange &&
+            <p>
+              You earned {karmaChange} karma.
+            </p>
+          }
+          {showBestReaction &&
+            <p>
+              You received {bestReaction.count} :{bestReaction.name}:{" "}
+              react{bestReaction.count > 1 ? 's' : ''}.
+            </p>
+          }
+          {showMostCommentedPost &&
+            <p>
+              You got {mostCommentedPost.commentCount} new{" "}
+              {mostCommentedPost.commentCount === 1 ? "reply" : "replies"}{" "}
+              on your post{" "}
+              <a
+                href={postGetPageUrl(mostCommentedPost.post, true)}
+                className={classes.mostCommentedPost}
+              >
+                {mostCommentedPost.post.title}
+              </a>.
+            </p>
+          }
+          <p>
+            <a href={getSiteUrl()} className={classes.notifications}>
+              See all your notifications on the EA Forum
+              {showUnreadNotifications && ` (${unreadNotifications} unread)`}
+            </a>
+          </p>
+          <div className={classes.spacing} />
+        </>
       }
-      {bestReaction && bestReaction.count > 0 &&
-        <p>
-          You received {bestReaction.count} :{bestReaction.name}: react{bestReaction.count > 1 ? 's' : ''}.
-        </p>
-      }
-      {mostCommentedPost && mostCommentedPost.commentCount > 0 &&
-        <p>
-          You got {mostCommentedPost.commentCount} new{" "}
-          {mostCommentedPost.commentCount === 1 ? "reply" : "replies"}{" "}
-          on your post{" "}
-          <a
-            href={postGetPageUrl(mostCommentedPost.post, true)}
-            className={classes.mostCommentedPost}
-          >
-            {mostCommentedPost.post.title}
-          </a>.
-        </p>
-      }
-      <p>
-        <a href={getSiteUrl()} className={classes.notifications}>
-          See all your notifications on the EA Forum
-          {unreadNotifications > 0 && ` (${unreadNotifications} unread)`}
-        </a>
-      </p>
-      <div className={classes.spacing} />
       {recommendedPosts.length > 0 &&
         <h2 className={classes.heading}>
           Posts you might like
