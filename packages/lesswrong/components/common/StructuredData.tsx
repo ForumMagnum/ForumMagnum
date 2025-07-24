c55544ed3a834d437e7bcbd0a21eacb01104cd5b
Robert Mushkatblat <bobert.mushky@gmail.com>
2025-07-24 16:16:47 -0700

    return structured data as an inlined script tag wherever it's currently inserted

--- packages/lesswrong/components/common/StructuredData.tsx
+++ packages/lesswrong/components/common/StructuredData.tsx
@@ -1,10 +1,16 @@
-import { useContext } from 'react';
-import { SSRResponseContext } from './Helmet';
+import { toEmbeddableJson } from '@/lib/utils/jsonUtils';
+import { SuspenseWrapper } from './SuspenseWrapper';
+
+const StructuredDataInner = ({generate}: {
+  generate: () => Record<string,AnyBecauseHard>
+}) => {
+  return <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: toEmbeddableJson(generate()) }} />;
+}
 
 export const StructuredData = ({generate}: {
   generate: () => Record<string,AnyBecauseHard>
 }) => {
-  const { setStructuredData } = useContext(SSRResponseContext);
-  setStructuredData(generate);
-  return null;
+  return <SuspenseWrapper name="StructuredData">
+    <StructuredDataInner generate={generate} />
+  </SuspenseWrapper>;
 }
