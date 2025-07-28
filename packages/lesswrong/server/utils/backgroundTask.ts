27e4037fed847e7d8d70aaa584d53202474d69c8
Robert Mushkatblat <bobert.mushky@gmail.com>
2025-07-28 15:56:31 -0700

    Delete test route and update comments for backgroundTask
    Co-authored-by: jimrandomh <jim@jimrandomh.org>

--- packages/lesswrong/server/utils/backgroundTask.ts
+++ packages/lesswrong/server/utils/backgroundTask.ts
@@ -2,7 +2,7 @@
  * Run a task (promise), without waiting for the result. If in a serverless
  * context, this function is responsible for making sure the process doesn't
  * exit until the background task is finished. In a non-serverless context,
- * this has no effect.
+ * i.e. either run on a local dev instance or on the client, this has no effect.
  */
 export const backgroundTask = <T>(promise: Promise<T>) => {
   ensureRequestHasBackgroundTaskHandler();
@@ -12,23 +12,21 @@ export const backgroundTask = <T>(promise: Promise<T>) => {
 let pendingBackgroundTasks: Promise<any>[] = [];
 
 function ensureRequestHasBackgroundTaskHandler() {
-  // TODO
-  // According to https://nextjs.org/docs/app/api-reference/functions/after#platform-support
-  // we might be able to get the context of the current request from something
-  // like thread-local storage with something like:
-  //     const RequestContext = globalThis[Symbol.for('@next/request-context')]
-  // But empirically that doesn't work (inside a graphql handler, the most
-  // relevant case, we just get undefined). So I think we have to do this
-  // manually on each entry point.
+  // Checks whether we're running in the context of a NextJS "request".
+  // If so, calls `after` (from @next/server) to ensure that the Vercel function
+  // doesn't exit until all background tasks are complete.
+  // 
+  // Getting the request context is as per https://nextjs.org/docs/app/api-reference/functions/after#platform-support
+  // 
+  // When run locally, this does nothing, as there is no request context, but background
+  // tasks will still complete as long as you don't kill the running server instance.
   const RequestContext = (globalThis as any)[Symbol.for('@next/request-context')];
   if (RequestContext) {
     const contextValue = RequestContext?.get()
     if (contextValue && !contextValue.hasAddedWaitForBackgroundTasks) {
-      console.log("Installing waitForBackgroundTasks handler");
       contextValue.hasAddedWaitForBackgroundTasks = true;
       const { after }: typeof import("next/server") = require("next/server");
       after(async () => {
-        console.log("Waiting for background tasks");
         await waitForBackgroundTasks();
       });
     }
