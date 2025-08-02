7e3e8ba42e65464db4dfb5c758608e5983893450
Ruben Bloom <darkruby501@users.noreply.github.com>
2025-08-02 09:41:06 -0700

    clean up

--- packages/lesswrong/components/common/FeedSelectorCheckbox.tsx
+++ packages/lesswrong/components/common/FeedSelectorCheckbox.tsx
@@ -5,11 +5,8 @@ import { useCookiesWithConsent } from '../hooks/useCookiesWithConsent';
 import { ULTRA_FEED_ENABLED_COOKIE, ULTRA_FEED_PAGE_VISITED_COOKIE } from '../../lib/cookies/cookies';
 import { useUpdateCurrentUser } from '../hooks/useUpdateCurrentUser';
 import { ultraFeedABTest } from '../../lib/abTests';
-import { useABTest } from '../../lib/abTestImpl';
 import { ultraFeedEnabledSetting } from '../../lib/publicSettings';
 import SectionFooterCheckbox from "../form-components/SectionFooterCheckbox";
-import classNames from 'classnames';
-import { userIsAdmin } from '@/lib/vulcan-users/permissions';
 
 const styles = defineStyles("FeedSelectorCheckbox", (theme: ThemeType) => ({
   container: {
@@ -42,12 +39,7 @@ const FeedSelectorCheckbox = ({ currentFeedType }: FeedSelectorCheckboxProps) =>
   const classes = useStyles(styles);
   const currentUser = useCurrentUser();
   const updateCurrentUser = useUpdateCurrentUser();
-  const abTestGroup = useABTest(ultraFeedABTest);
   const [cookies, setCookie] = useCookiesWithConsent([ULTRA_FEED_ENABLED_COOKIE, ULTRA_FEED_PAGE_VISITED_COOKIE]);
-  
-  const cookieValue = cookies[ULTRA_FEED_ENABLED_COOKIE];
-  const hasVisitedFeedPage = cookies[ULTRA_FEED_PAGE_VISITED_COOKIE] === "true";
-  const hasExplicitPreference = cookieValue === "true" || cookieValue === "false";
   
   // Don't show if (1) UltraFeed is disabled, (2) user is not logged in, or (3) user has not explicitly opted in, visited the feed page, or is in the A/B test group
   if (!ultraFeedEnabledSetting.get() || !currentUser) {
