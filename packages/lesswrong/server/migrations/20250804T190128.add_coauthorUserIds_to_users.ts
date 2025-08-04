3d75123ca2f889a368d3a20f3f9fd1e920e71d2e
Ollie Etherington <ollie@etherington.xyz>
2025-08-04 20:17:18 +0100

    Add migration to backfill `coauthorUserIds`

---
+++ packages/lesswrong/server/migrations/20250804T190128.add_coauthorUserIds_to_users.ts
@@ -0,0 +1,45 @@
+/* eslint-disable no-console */
+
+import { addField, dropField } from "./meta/utils"
+import Posts from "../collections/posts/collection";
+import BulkWriter from "../sql/BulkWriter";
+
+export const up = async ({db}: MigrationContext) => {
+  const start = new Date();
+
+  console.log("Adding coauthorUserIds field...");
+  await addField(db, Posts, "coauthorUserIds");
+
+  console.log("Fetching posts to update...");
+  const posts: {
+    _id: string,
+    coauthorStatuses: CoauthorStatusInput[],
+  }[] = await db.any(`
+    SELECT "_id", "coauthorStatuses"
+    FROM "Posts"
+    WHERE CARDINALITY("coauthorStatuses") > 0
+  `);
+
+  console.log(`Creating updates for ${posts.length} posts...`);
+  const operations: MongoBulkWriteOperations<DbPost> = posts.map((post) => ({
+    updateOne: {
+      filter: {_id: post._id},
+      update: {
+        $set: {
+          coauthorUserIds: post.coauthorStatuses!.map(({userId}) => userId),
+        },
+      },
+    },
+  }));
+
+  console.log("Writing updates...");
+  const writer = new BulkWriter(Posts.getTable(), operations);
+  await writer.execute(db);
+
+  const end = new Date();
+  console.log(`Done (${end.getTime() - start.getTime()}ms)`);
+}
+
+export const down = async ({db}: MigrationContext) => {
+  await dropField(db, Posts, "coauthorUserIds");
+}
