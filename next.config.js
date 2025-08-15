d3c6881f8f22a54f3492ed5731608a91b1a2be14
Robert Mushkatblat <bobert.mushky@gmail.com>
2025-08-14 19:32:48 -0700

    add sourcemaps for production browser

--- next.config.js
+++ next.config.js
@@ -33,6 +33,8 @@ function loadTsConfig(configPath) {
 
 /** @type {import('next').NextConfig} */
 module.exports = {
+  productionBrowserSourceMaps: true,
+  
   turbopack: {
     resolveAlias: {
       // Replicate the path mappings from tsconfig-client.json
