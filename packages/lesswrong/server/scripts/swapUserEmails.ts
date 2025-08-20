a0ec10a4bd09bd5a01d4e59015c97f4aa107ffe8
Ollie Etherington <ollie@etherington.xyz>
2025-08-20 14:51:57 +0100

    Swap user emails atomically

--- packages/lesswrong/server/scripts/swapUserEmails.ts
+++ packages/lesswrong/server/scripts/swapUserEmails.ts
@@ -1,32 +1,49 @@
 /* eslint-disable no-console */
 
 import Users from "../collections/users/collection";
+import { getSqlClientOrThrow } from "../sql/sqlClient";
 
 export const swapUserEmails = async (userId1: string, userId2: string) => {
+  // We run the query atomically below, but fetch the users first just to
+  // double check that the entered user ids are valid
   console.log(`Fetching users: ${userId2}, ${userId2}`);
   const [user1, user2] = await Promise.all([
-    Users.findOne({_id: userId1}),
-    Users.findOne({_id: userId2}),
+    Users.findOne({_id: userId1}, {}, {_id: 1, displayName: 1}),
+    Users.findOne({_id: userId2}, {}, {_id: 1, displayName: 1}),
   ]);
   if (!user1 || !user2) {
     throw new Error("Invalid users");
   }
   console.log(`Fetched users: ${user1.displayName}, ${user2.displayName}`);
-  await Promise.all([
-    Users.rawUpdateOne({_id: user1._id}, {
-      $set: {
-        email: user2.email,
-        emails: user2.emails,
-        "services.auth0": user2.services.auth0,
-      },
-    }),
-    Users.rawUpdateOne({_id: user2._id}, {
-      $set: {
-        email: user1.email,
-        emails: user1.emails,
-        "services.auth0": user1.services.auth0,
-      },
-    }),
-  ]);
+
+  const db = getSqlClientOrThrow();
+  await db.none(`
+    WITH u1 AS (
+      SELECT * FROM "Users" WHERE "_id" = $1
+    ),
+    u2 AS (
+      SELECT * FROM "Users" WHERE "_id" = $2
+    )
+    UPDATE "Users" u
+    SET
+      "email" = CASE u."_id"
+        WHEN $1 THEN (SELECT "email" FROM u2)
+        WHEN $2 THEN (SELECT "email" FROM u1)
+      END,
+      "emails" = CASE u."_id"
+        WHEN $1 THEN (SELECT "emails" FROM u2)
+        WHEN $2 THEN (SELECT "emails" FROM u1)
+      END,
+      "services" = JSONB_SET(
+        u."services",
+        '{auth0}',
+        CASE u."_id"
+          WHEN $1 THEN (SELECT "services"->'auth0' FROM u2)
+          WHEN $2 THEN (SELECT "services"->'auth0' FROM u1)
+        END
+      )
+    WHERE u."_id" IN ($1, $2)
+  `, [userId1, userId2]);
+
   console.log("Updated user emails");
 }
