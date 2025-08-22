71b5cdccc0b8e3f92fddd4b69ac6e210723553d6
Robert Mushkatblat <bobert.mushky@gmail.com>
2025-08-21 17:42:36 -0700

    fix sequence post page path param for sequence id

--- packages/lesswrong/components/sequences/SequencesPost.tsx
+++ packages/lesswrong/components/sequences/SequencesPost.tsx
@@ -7,7 +7,7 @@ import PostsPageWrapper from "../posts/PostsPage/PostsPageWrapper";
 
 const SequencesPost = () => {
   const { query, params } = useLocation();
-  const { postId, sequenceId } = params;
+  const { postId, _id: sequenceId } = params;
   
   const version = query.revision
   return <PostsPageWrapper documentId={postId} sequenceId={sequenceId} version={version} />
