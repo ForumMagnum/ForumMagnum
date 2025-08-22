f698f0cd8c7d8aded4dc53f16a88d0eedb0bb0e7
James Babcock <jimrandomh@gmail.com>
2025-08-22 14:28:47 -0700

    Add unhandledRejection nodejs event handler to instrumentation

--- instrumentation.js
+++ instrumentation.js
@@ -1,6 +1,30 @@
 // @ts-check
 
+function installUnhandledRejectionEventHandler() {
+  // Log unhandled promise rejections, eg exceptions escaping from async
+  // callbacks. The default node behavior is to silently ignore these exceptions,
+  // which is terrible and has led to unnoticed bugs in the past.
+  if (process.env.NEXT_RUNTIME === 'nodejs') {
+    process.on("unhandledRejection", (r) => {
+      const { captureException } = require("@sentry/nextjs");
+      captureException(r);
+      
+      //eslint-disable-next-line no-console
+      console.log("Unhandled rejection");
+      //eslint-disable-next-line no-console
+      console.log(r);
+      
+      if (r.stack) {
+        //eslint-disable-next-line no-console
+        console.log(r.stack);
+      }
+    });
+  }
+}
+
 export async function register() {
+  installUnhandledRejectionEventHandler();
+
   if (process.env.NODE_ENV !== "production") {
     return;
   }
