0c886c88763d94838ddad4044eb01521ebec8dca
James Babcock <jimrandomh@gmail.com>
2025-09-09 15:23:41 -0700

    Suppress console warnings from analytics events with empty body during Firefox tab close

--- app/analyticsEvent/route.ts
+++ app/analyticsEvent/route.ts
@@ -3,17 +3,22 @@ import { backgroundTask } from "@/server/utils/backgroundTask";
 import type { NextRequest } from "next/server";
 
 export async function POST(req: NextRequest) {
-  let body: any = null;
+  let bodyText: string|null = null;
+  let bodyJson: any = null;
   try {
-    body = await req.json();
+    bodyText = await req.text();
+    if (bodyText.length > 0) {
+      bodyJson = JSON.parse(bodyText);
+    }
   } catch(e) {
+    //eslint-disable-next-line no-console
     console.error(e);
   }
   
-  if (!body?.events || !body?.now) {
+  if (!bodyJson?.events || !bodyJson?.now) {
     return new Response('analyticsEvent endpoint should be JSON with fields "events" and "now"', { status: 400 });
   }
   
-  backgroundTask(handleAnalyticsEventWriteRequest(body.events, body.now));
+  backgroundTask(handleAnalyticsEventWriteRequest(bodyJson.events, bodyJson.now));
   return new Response("ok", { status: 200 });
 }
