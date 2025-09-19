965c8bd620be5500d17ef3666699f18062fe9dd5
Robert Mushkatblat <bobert.mushky@gmail.com>
2025-09-19 15:26:20 -0700

    add back isReturningVisitor logic to fix ssr mismatch for returning logged out users who should be getting the enriched tab

--- packages/lesswrong/components/next/SharedScripts.tsx
+++ packages/lesswrong/components/next/SharedScripts.tsx
@@ -9,12 +9,14 @@ import { faviconUrlSetting } from '@/lib/instanceSettings';
 
 // These exist as a client component to avoid the RSC rehydration protocol
 // putting them into the initial streamed response chunk twice.
-const SharedScriptsInner = () => {
+const SharedScriptsInner = ({ isReturningVisitor }: { isReturningVisitor: boolean }) => {
   const { public: publicInstanceSettings } = getInstanceSettings();
   return (<>
       {globalExternalStylesheets.map(stylesheet => <link key={stylesheet} rel="stylesheet" type="text/css" href={stylesheet}/>)}
       <script dangerouslySetInnerHTML={{__html: `window.publicInstanceSettings = ${toEmbeddableJson(publicInstanceSettings)}`}}/>
       <script dangerouslySetInnerHTML={{__html: getEmbeddedStyleLoaderScript()}}/>
+      <script dangerouslySetInnerHTML={{__html: `window.isReturningVisitor = ${toEmbeddableJson(isReturningVisitor)}`}}/>
+      
       <meta httpEquiv='delegate-ch' content='sec-ch-dpr https://res.cloudinary.com;' />
       <meta httpEquiv="Accept-CH" content="DPR, Viewport-Width, Width"/>
       {/* HACK: These insertion-point markers are <script> tags (rather than
@@ -28,6 +30,6 @@ const SharedScriptsInner = () => {
   </>)
 };
 
-export const SharedScripts = () => {
-  return useMemo(() => <SharedScriptsInner/>, []);
+export const SharedScripts = ({ isReturningVisitor }: { isReturningVisitor: boolean }) => {
+  return useMemo(() => <SharedScriptsInner isReturningVisitor={isReturningVisitor}/>, [isReturningVisitor]);
 }
