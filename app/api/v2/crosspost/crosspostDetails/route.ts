873051574d745dec9e65ae9b02151e1e682fc34a
Robert Mushkatblat <bobert.mushky@gmail.com>
2025-09-23 12:50:45 -0700

    add eaforum-specific cors response headers to the /graphql route to fix crosspost account linking

--- app/api/v2/crosspost/crosspostDetails/route.ts
+++ app/api/v2/crosspost/crosspostDetails/route.ts
@@ -1,4 +1,5 @@
-import { crosspostDetailsCrosspostHandler, crosspostOptionsHandler } from "@/server/crossposting/handlers";
+import { crosspostDetailsCrosspostHandler } from "@/server/crossposting/handlers";
+import { crosspostOptionsHandler } from "@/server/crossposting/cors";
 import type { NextRequest } from "next/server";
 
 export function POST(req: NextRequest) {
