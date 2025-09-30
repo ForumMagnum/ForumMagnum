1c5e41516c8ab3c9a5ef4e635047def572035fb1
Robert Mushkatblat <bobert.mushky@gmail.com>
2025-09-30 11:30:46 -0700

    add custom index to comment embeddings migration

--- packages/lesswrong/server/migrations/20250929T225026.add_comment_embeddings_table.ts
+++ packages/lesswrong/server/migrations/20250929T225026.add_comment_embeddings_table.ts
@@ -1,8 +1,10 @@
 import CommentEmbeddings from "../collections/commentEmbeddings/collection";
-import { createTable } from "./meta/utils"
+import { queueMigrationTask } from "./meta/migrationTaskQueue";
+import { createTable, updateCustomIndexes } from "./meta/utils"
 
-export const up = async ({db}: MigrationContext) => {
+export const up = async ({db, dbOutsideTransaction}: MigrationContext) => {
   await createTable(db, CommentEmbeddings);
+  queueMigrationTask(() => updateCustomIndexes(dbOutsideTransaction));
 }
 
 export const down = async ({db}: MigrationContext) => {
