957ad37f675fc1f608da37d477828f50d7b2f30d
Raemon <raemon777@gmail.com>
2025-10-01 12:20:36 -0700

    update migration

--- packages/lesswrong/server/migrations/20251001T001728.make_messages_reactable.ts
+++ packages/lesswrong/server/migrations/20251001T001728.make_messages_reactable.ts
@@ -1,16 +1,22 @@
 import Messages from "../collections/messages/collection";
-import { addField } from "./meta/utils";
+import { addField, dropField } from "./meta/utils";
 
 export const up = async ({db}: MigrationContext) => {
   await addField(db, Messages, "voteCount");
   await addField(db, Messages, "baseScore");
   await addField(db, Messages, "score");
   await addField(db, Messages, "extendedScore");
-  await addField(db, Messages, "inactive");
   await addField(db, Messages, "afBaseScore");
   await addField(db, Messages, "afExtendedScore");
   await addField(db, Messages, "afVoteCount");
 }
 
 export const down = async ({db}: MigrationContext) => {
+  await dropField(db, Messages, "voteCount");
+  await dropField(db, Messages, "baseScore");
+  await dropField(db, Messages, "score");
+  await dropField(db, Messages, "extendedScore");
+  await dropField(db, Messages, "afBaseScore");
+  await dropField(db, Messages, "afExtendedScore");
+  await dropField(db, Messages, "afVoteCount");
 }
