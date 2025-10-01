f87f8b86945d13386b1ca596f62355504beb8f13
Ollie Etherington <ollie@etherington.xyz>
2025-10-01 16:58:04 +0100

    Use custom API for `FMCrosspostControl` and remove foreign apollo client

--- packages/lesswrong/components/hooks/useForeignApolloClient.tsx
+++
@@ -1,8 +0,0 @@
-import { createContext, useContext } from "react";
-import type { ApolloClient, NormalizedCacheObject } from '@apollo/client';
-
-const ForeignApolloClientContext = createContext<ApolloClient<NormalizedCacheObject> | undefined>(undefined);
-
-export const ForeignApolloClientProvider = ForeignApolloClientContext.Provider;
-
-export const useForeignApolloClient = () => useContext(ForeignApolloClientContext);
