ecf04a62ccae15c7094e57edc8af7a34a158af6b
Raemon <raemon777@gmail.com>
2025-10-07 15:24:27 -0700

    switch to switch

--- packages/lesswrong/components/comments/AutoEmailSubscribeCheckbox.tsx
+++ packages/lesswrong/components/comments/AutoEmailSubscribeCheckbox.tsx
@@ -1,13 +1,46 @@
 import React, { useCallback, useState, useEffect, useRef } from "react";
 import { useCurrentUser } from "../common/withUser";
-import SectionFooterCheckbox from "../form-components/SectionFooterCheckbox";
+import Switch from '@/lib/vendor/@material-ui/core/src/Switch';
 import { useUpdateCurrentUser } from "../hooks/useUpdateCurrentUser";
 import { useDialog } from "../common/withDialog";
 import LoginPopup from "../users/LoginPopup";
 import { defineStyles, useStyles } from "../hooks/useStyles";
 import { NotificationTypeSettings } from "@/lib/collections/users/notificationFieldHelpers";
+import LWTooltip from "../common/LWTooltip";
+import ToggleSwitch from "../common/ToggleSwitch";
 
 const styles = defineStyles("AutoEmailSubscribeCheckbox", (theme) => ({
+  root: {
+    display: "flex",
+    alignItems: "center",
+  },
+  switchWrapper: {
+    cursor: "pointer",
+    ...theme.typography.commentStyle,
+    ...theme.typography.body2,
+    lineHeight: `18px`,
+    display: "flex",
+    alignItems: "center",
+    "& .ToggleSwitch-root": {
+      width: `24px !important`,
+      height: `16px !important`,
+      minWidth: `24px !important`,
+    },
+    "& .ToggleSwitch-switchOn": {
+      left: -6,
+    }
+  },
+  switch: {
+    marginRight: 8,
+    "& .ToggleSwitch-handle": {
+      width: 12,
+      height: 12,
+    },
+  },
+  label: {
+    ...theme.typography.commentStyle,
+    color: theme.palette.lwTertiary.main
+  },
   disabled: {
     opacity: 0.8,
     filter: "grayscale(100%)",
@@ -27,8 +60,6 @@ const AutoEmailSubscribeCheckbox = () => {
   const setting: NotificationTypeSettings|null = currentUser?.notificationRepliesToMyComments;
   const checked = !!setting?.email.enabled;
   const classes = useStyles(styles);
-
-  const initialEmailEnabled = useRef(checked);
 
   const handleToggle = useCallback(async () => {
     if (!currentUser) {
@@ -57,6 +88,9 @@ const AutoEmailSubscribeCheckbox = () => {
     });
   }, [currentUser, setting, updateCurrentUser, openDialog, checked]);
 
+  // if email notifications are not enabled on first page-load, don't show this UI
+  // (otherwise, continuing showing the UI when the user toggles it on-and-off)
+  const initialEmailEnabled = useRef(checked);
   if (!initialEmailEnabled.current) {
     return null;
   }
@@ -65,8 +99,15 @@ const AutoEmailSubscribeCheckbox = () => {
 
   const label = <span>Email me replies <span className={classes.hideOnMobile}>to all my comments</span></span>
 
-  return <span className={!checked? classes.disabled : ""}>
-    <SectionFooterCheckbox label={label} value={checked} onClick={handleToggle} tooltip={tooltip}  />
+  const switchElement = <span className={classes.switchWrapper} onClick={handleToggle}>
+    <ToggleSwitch className={classes.switch} value={checked} />
+    <span className={classes.label}>{label}</span>
+  </span>
+
+  return <span className={!checked? classes.disabled : classes.root}>
+    <LWTooltip title={tooltip} placement="bottom-start" inlineBlock={false}>
+      {switchElement}
+    </LWTooltip>
   </span>
 };
 
