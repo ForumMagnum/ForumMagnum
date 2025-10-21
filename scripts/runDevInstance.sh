72b0cab87ccfa508abefc9b859cd260cdc1bc8ca
Robert Mushkatblat <bobert.mushky@gmail.com>
2025-10-21 12:02:23 -0700

    uncomment out vercel env pull when starting local instance

--- scripts/runDevInstance.sh
+++ scripts/runDevInstance.sh
@@ -2,6 +2,6 @@
 
 # We use node directly rather than going through yarn so that we can use --inspect-publish-uid=http,
 # which silences most of the annoying debugger console logs on start.
-#vercel env pull .env.local --environment=${1:-development} && \
+vercel env pull .env.local --environment=${1:-development} && \
 node --inspect --inspect-publish-uid=http --no-deprecation ./node_modules/.bin/next dev --turbopack
 
