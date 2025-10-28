3710eb88352f218024c12c3374b15bf50cf1a3ee
Robert Mushkatblat <bobert.mushky@gmail.com>
2025-10-28 14:19:42 -0700

    hydrate apollo cache with post fragments to avoid loading state in supermod detail view

--- packages/lesswrong/components/hooks/useModeratedUserContents.ts
+++ packages/lesswrong/components/hooks/useModeratedUserContents.ts
@@ -1,7 +1,9 @@
 import { useQuery } from "@/lib/crud/useQuery";
 import { usePublishedPosts } from "./usePublishedPosts";
 import { gql } from "@/lib/generated/gql-codegen";
-import { useMemo } from "react";
+import { useEffect, useMemo } from "react";
+import { useApolloClient } from "@apollo/client/react";
+import { PostsListWithVotes } from "@/lib/collections/posts/fragments";
 
 const CommentsListWithParentMetadataMultiQuery = gql(`
   query multiCommentModerationSidebarQuery($selector: CommentSelector, $limit: Int, $enableTotal: Boolean) {
@@ -26,6 +28,43 @@ export function useModeratedUserContents(userId: string, contentLimit = 20) {
   });
 
   const comments = useMemo(() => [...(commentsData?.comments?.results ?? [])], [commentsData]);
+
+  // In ModerationContentDetail, we embed a post page wrapper into the moderation detail view.
+  // Hydrating the apollo cache here lets us avoid a loading spinner when going through a user's posts that way.
+  const apolloClient = useApolloClient();
+  useEffect(() => {
+    for (const post of posts) {
+      const cacheEntryIdentifiers = {
+        fragment: PostsListWithVotes,
+        fragmentName: "PostsListWithVotes",
+        id: 'Post:'+post._id,
+      };
+      
+      const existingCacheEntry = apolloClient.cache.readFragment<PostsListWithVotes>(cacheEntryIdentifiers);
+
+      if (existingCacheEntry) {
+        continue;
+      }
+
+      apolloClient.cache.writeFragment<PostsListWithVotes>({
+        ...cacheEntryIdentifiers,
+        data: {
+          ...post,
+          bannedUserIds: null,
+          contents: {
+            ...post.contents,
+            _id: post.contents?._id ?? '',
+            plaintextDescription: '',
+            htmlHighlight: post.contents?.htmlHighlight ?? '',
+            wordCount: post.contents?.wordCount ?? 0,
+            version: post.contents?.version ?? '',
+          },
+          podcastEpisode: null,
+        },
+        broadcast: false,
+      });
+    }
+  }, [posts]);
 
   return {
     posts,
