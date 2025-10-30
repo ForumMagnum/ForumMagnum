14f2d58b56fd7675beb58315a718f30ea0b9abec
Ruben Bloom <darkruby501@users.noreply.github.com>
2025-10-30 16:35:29 -0700

    remove ineffective fix that will be replaced

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,10 @@
 import React from "react";
-import LWHome from "@/components/common/LWHome";
 import RouteRoot from "@/components/next/RouteRoot";
-import AlignmentForumHome from "@/components/alignment-forum/AlignmentForumHome";
-import { forumSelect } from "@/lib/forumTypeUtils";
-import EAHome from "@/components/ea-forum/EAHome";
 
 export default async function Home() {
-  const background = forumSelect({
-    LessWrong: '#f8f4ee',
-    EAForum: '#f6f8f9',
-    AlignmentForum: undefined, // Uses theme's grey[60]
-    default: '#f8f4ee',
-  });
   
   return <RouteRoot metadata={{ 
     hasLeftNavigationColumn: true,
-    background
   }}>
     <span/>
   </RouteRoot>;
