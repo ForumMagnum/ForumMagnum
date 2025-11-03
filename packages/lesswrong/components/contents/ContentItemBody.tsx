ForumMagnum/ForumMagnum

commit beb7ac4c30e9c996f62a8f55636174e3e048c8e1

determine forum type at runtime rather than build time, and refactor all top-level uses of forumType functions and settings

packages/lesswrong/components/contents/ContentItemBody.tsx
@@ -17,7 +17,7 @@ import { useTracking } from '@/lib/analyticsEvents';
 import ForumEventPostPagePollSection from '../forumEvents/ForumEventPostPagePollSection';
 import repeat from 'lodash/repeat';
 import { captureException } from '@sentry/nextjs';
-import { colorReplacements } from '@/themes/userThemes/darkMode';
+import { getColorReplacements } from '@/themes/userThemes/darkMode';
 import { colorToString, invertColor, parseColor } from '@/themes/colorUtil';
 import { ThemeContext } from '../themes/useTheme';
 
@@ -614,20 +614,20 @@ function transformStylesForDarkMode(styles: Record<string,string>, themeName: Us
 
 function transformAttributeValueForDarkMode(attributeValue: string): string {
   const normalized = attributeValue.trim().toLowerCase();
-  if (!colorReplacements[normalized]) {
+  if (!getColorReplacements()[normalized]) {
     const parsedColor = parseColor(normalized);
     if (parsedColor) {
       const invertedColor = invertColor(parsedColor);
-      colorReplacements[normalized] = colorToString(invertedColor);
+      getColorReplacements()[normalized] = colorToString(invertedColor);
     } else {
       // If unable to parse a color (eg an unsupported color format), use black
       // as a safe dark-mode background color
-      colorReplacements[normalized] = "#000000";
+      getColorReplacements()[normalized] = "#000000";
     }
   }
   
-  if (colorReplacements[normalized]) {
-    return colorReplacements[normalized];
+  if (getColorReplacements()[normalized]) {
+    return getColorReplacements()[normalized];
   } else {
     return attributeValue;
   }

packages/lesswrong/themes/userThemes/darkMode.ts
@@ -3,7 +3,7 @@ import { invertHexColor, invertColor, colorToString, zeroTo255 } from '../colorU
 import { forumSelect } from '../../lib/forumTypeUtils';
 import deepmerge from 'deepmerge';
 
-export const invertedGreyscale = {
+export const getInvertedGreyscale = () => ({
   // Present in @/lib/vendor/@material-ui/core/src/colors/grey
   50: invertHexColor('#fafafa'),
   100: invertHexColor('#f5f5f5'),
@@ -43,7 +43,7 @@ export const invertedGreyscale = {
   620: invertHexColor("#888888"),
   650: invertHexColor('#808080'),
   680: invertHexColor('#666666'),
-};
+});
 
 const greyAlpha = (alpha: number) => `rgba(255,255,255,${alpha})`;
 
@@ -62,7 +62,7 @@ const inverseGreyAlpha = (alpha: number) => {
 // We handle this in ContentItemBody with transformStylesForDarkMode. The
 // colors below have dedicated color-mappings; all other colors use `parseColor`
 // and `invertColor` from `colorUtil.ts`.
-export const colorReplacements: Record<string,string> = {
+export const getColorReplacements = (): Record<string,string> => ({
   "initial":            "rgba(255,255,255,.87)",
   "rgba(255,255,255,.5)": "rgba(0,0,0.5)",
   "hsl(0, 0%, 90%)":    "hsl(0, 0%, 10%)",
@@ -73,7 +73,7 @@ export const colorReplacements: Record<string,string> = {
   "#FFEEBB":            invertHexColor("#ffeebb"),
   "rgb(255, 238, 187)": colorToString(invertColor([255/255.0,238/255.0,187/255.0,1])),
   "rgb(230, 230, 230)": colorToString(invertColor([230/255.0,230/255.0,230/255.0,1])),
-} as const;
+} as const);
 
 const forumComponentPalette = (shadePalette: ThemeShadePalette) =>
   forumSelect({
@@ -151,10 +151,10 @@ const forumOverrides = (palette: ThemePalette): PartialDeep<ThemeType['overrides
     default: {},
   });
 
-export const darkModeTheme: UserThemeSpecification = {
+export const getDarkModeTheme = (): UserThemeSpecification => ({
   shadePalette: {
     dark: true,
-    grey: invertedGreyscale,
+    grey: getInvertedGreyscale(),
     greyAlpha,
     inverseGreyAlpha,
     boxShadowColor: (alpha: number) => greyAlpha(alpha),
@@ -328,4 +328,4 @@ export const darkModeTheme: UserThemeSpecification = {
     overrides: forumOverrides(palette),
     rawCSS: []
   }),
-};
+});

commit bf31235fea872edf5c3ce6afc3561087ef0ba1a4

Fix broken dark-mode colors in tables
Introduced in: beb7ac4. The color replacements in darkMode.ts are used
as a writable cache; a refactoring broke this by making it return a new
object each time, including in between writing a value and using it,
which made it so that colors that weren't part of the initial
manually-specified set could not be replaced.

packages/lesswrong/components/contents/ContentItemBody.tsx
@@ -17,7 +17,7 @@ import { useTracking } from '@/lib/analyticsEvents';
 import ForumEventPostPagePollSection from '../forumEvents/ForumEventPostPagePollSection';
 import repeat from 'lodash/repeat';
 import { captureException } from '@/lib/sentryWrapper';
-import { getColorReplacements } from '@/themes/userThemes/darkMode';
+import { getColorReplacementsCache } from '@/themes/userThemes/darkMode';
 import { colorToString, invertColor, parseColor } from '@/themes/colorUtil';
 import { ThemeContext } from '../themes/useTheme';
 
@@ -597,6 +597,7 @@ function transformStylesForDarkMode(styles: Record<string,string>, themeName: Us
   if (themeName === 'dark' || themeName === 'auto') {
     return Object.fromEntries(Object.entries(styles).map(([attribute,value]) => {
       if (attributesNeedingTransform[attribute]) {
+        console.log(`Transforming ${attribute}:${value}`);
         const darkModeValue = transformAttributeValueForDarkMode(value)
         if (themeName === "auto" && darkModeValue !== value) {
           return [attribute, `light-dark(${value},${darkModeValue})`];
@@ -613,22 +614,26 @@ function transformStylesForDarkMode(styles: Record<string,string>, themeName: Us
 }
 
 function transformAttributeValueForDarkMode(attributeValue: string): string {
+  console.log(`Transforming color ${attributeValue}`);
   const normalized = attributeValue.trim().toLowerCase();
-  if (!getColorReplacements()[normalized]) {
+  if (!getColorReplacementsCache()[normalized]) {
     const parsedColor = parseColor(normalized);
     if (parsedColor) {
       const invertedColor = invertColor(parsedColor);
-      getColorReplacements()[normalized] = colorToString(invertedColor);
+      getColorReplacementsCache()[normalized] = colorToString(invertedColor);
     } else {
       // If unable to parse a color (eg an unsupported color format), use black
       // as a safe dark-mode background color
-      getColorReplacements()[normalized] = "#000000";
+      getColorReplacementsCache()[normalized] = "#000000";
     }
   }
   
-  if (getColorReplacements()[normalized]) {
-    return getColorReplacements()[normalized];
+  if (getColorReplacementsCache()[normalized]) {
+    const result = getColorReplacementsCache()[normalized];
+    console.log(`Returning ${result} (replacement)`);
+    return result;
   } else {
+    console.log(`Returning ${attributeValue}`);
     return attributeValue;
   }
 }

packages/lesswrong/themes/userThemes/darkMode.ts
@@ -62,7 +62,7 @@ const inverseGreyAlpha = (alpha: number) => {
 // We handle this in ContentItemBody with transformStylesForDarkMode. The
 // colors below have dedicated color-mappings; all other colors use `parseColor`
 // and `invertColor` from `colorUtil.ts`.
-export const getColorReplacements = (): Record<string,string> => ({
+const getManualColorReplacements = () => ({
   "initial":            "rgba(255,255,255,.87)",
   "rgba(255,255,255,.5)": "rgba(0,0,0.5)",
   "hsl(0, 0%, 90%)":    "hsl(0, 0%, 10%)",
@@ -74,6 +74,8 @@ export const getColorReplacements = (): Record<string,string> => ({
   "rgb(255, 238, 187)": colorToString(invertColor([255/255.0,238/255.0,187/255.0,1])),
   "rgb(230, 230, 230)": colorToString(invertColor([230/255.0,230/255.0,230/255.0,1])),
 } as const);
+const colorReplacementsCache = getManualColorReplacements();
+export const getColorReplacementsCache = (): Record<string,string> => colorReplacementsCache;
 
 const forumComponentPalette = (shadePalette: ThemeShadePalette) =>
   forumSelect({