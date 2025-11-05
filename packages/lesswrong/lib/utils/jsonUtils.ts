5a46d33701047098cc3f0a1cecb45befcdcb0a9f
Raemon <raemon777@gmail.com>
2025-11-04 18:34:56 -0800

    revert jsonUtils

--- packages/lesswrong/lib/utils/jsonUtils.ts
+++ packages/lesswrong/lib/utils/jsonUtils.ts
@@ -4,10 +4,5 @@
  * but does *not* wrap it in quotes or escape quotes.
  */
 export const toEmbeddableJson = (serializable: unknown): string => {
-  const json = JSON.stringify(serializable);
-  // JSON.stringify(undefined) returns undefined; when embedding into inline
-  // scripts we want the literal `undefined` instead of throwing on .replace
-  if (json === undefined) return "undefined";
-  return json
-    .replace(/<\//g, "<\\/")
+  return JSON.stringify(serializable)
 }
