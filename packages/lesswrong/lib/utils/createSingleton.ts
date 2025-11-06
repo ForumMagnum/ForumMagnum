5d0f6e2fb8287cb5fcaea9b522eae1bb2a76ec04
James Babcock <jimrandomh@gmail.com>
2025-11-06 12:33:45 -0800

    Apollo initialization happens on first use rather than on import

---
+++ packages/lesswrong/lib/utils/createSingleton.ts
@@ -0,0 +1,12 @@
+
+export function createSingleton<T>(get: ()=>T) {
+  let value: T|null = null;
+  return () => {
+    if (value !== null) {
+      return value;
+    } else {
+      value = get();
+      return value;
+    }
+  }
+}
