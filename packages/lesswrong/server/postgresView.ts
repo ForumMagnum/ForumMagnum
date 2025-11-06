08c0c9eaf97bc6972a0db1abf5e2785a1e42c7d2
Robert Mushkatblat <bobert.mushky@gmail.com>
2025-11-06 14:31:40 -0800

    replace ConversationUnreadMessages view with a correlated subquery to fix tail performance when loading conversation inbox for users with a large number of total messages

--- packages/lesswrong/server/postgresView.ts
+++ packages/lesswrong/server/postgresView.ts
@@ -54,36 +54,6 @@ export class PostgresView {
   }
 }
 
-export const conversationUnreadMessagesView = new PostgresView(
-  "ConversationUnreadMessages",
-  `CREATE OR REPLACE VIEW "ConversationUnreadMessages" AS
-    SELECT q."conversationId", q."userId", EXISTS (
-      SELECT *
-      FROM "Notifications" n
-      INNER JOIN "Messages" m ON
-        n."userId" = q."userId"
-        AND n."documentId" = m."_id"
-        AND n."documentType" = 'message'
-        AND n."emailed" IS NOT TRUE
-        AND n."waitingForBatch" IS NOT TRUE
-        AND n."deleted" IS NOT TRUE
-        AND n."viewed" IS NOT TRUE
-        AND m."conversationId" = q."conversationId"
-      ) "hasUnreadMessages"
-    FROM (
-      SELECT "_id" "conversationId", UNNEST("participantIds") "userId"
-      FROM "Conversations"
-    ) q
-  `,
-  [],
-  undefined,
-  [
-    {type: "collection", name: "Notifications"},
-    {type: "collection", name: "Messages"},
-  ],
-);
-
-
 const createUserLoginTokensQuery = `
   CREATE MATERIALIZED VIEW IF NOT EXISTS "UserLoginTokens" AS
   SELECT
@@ -169,7 +139,6 @@ export const uniquePostUpvotersView = new PostgresView(
 );
 
 export const getAllPostgresViews = () => [
-  conversationUnreadMessagesView,
   userLoginTokensView,
   uniquePostUpvotersView,
 ];
