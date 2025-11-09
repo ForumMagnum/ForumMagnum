b550a8fe6510fc6fe81a2f39ced14958beaf497d
Raemon <raemon777@gmail.com>
2025-11-08 21:24:43 -0800

    revert hopefully

--- packages/lesswrong/server/repos/PostsRepo.ts
+++ packages/lesswrong/server/repos/PostsRepo.ts
@@ -1288,9 +1288,7 @@ class PostsRepo extends AbstractRepo<"Posts"> {
     return new Map(result.map(row => [row.postId, row.isRead]));
   }
   
-  async getHomepageCommunityEvents(limit: number, eventType?: string): Promise<Array<HomepageCommunityEventMarker>> {
-    const eventTypeFilter = eventType ? `AND "types" @> ARRAY[$2]::text[]` : '';
-    const params = eventType ? [limit, eventType] : [limit];
+  async getHomepageCommunityEvents(limit: number): Promise<Array<HomepageCommunityEventMarker>> {
     return this.getRawDb().any<HomepageCommunityEventMarker>(`
       -- PostsRepo.getHomepageCommunityEvents
       SELECT 
@@ -1304,9 +1302,8 @@ class PostsRepo extends AbstractRepo<"Posts"> {
       AND "startTime" < NOW() + INTERVAL '5 months'
       AND "googleLocation" -> 'geometry' -> 'location' ->> 'lat' IS NOT NULL
       AND "googleLocation" -> 'geometry' -> 'location' ->> 'lng' IS NOT NULL
-      ${eventTypeFilter}
       LIMIT $1
-    `, params);
+    `, [limit]);
   }
 }
 
