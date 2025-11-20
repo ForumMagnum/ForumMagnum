62b2d969761887a1ec2d86fdc018e4f76a4f278a
James Babcock <jimrandomh@gmail.com>
2025-11-19 19:24:00 -0800

    Drop a bunch of login-token-related DB indexes that are now obsolete

--- packages/lesswrong/server/migrations/20251119T184455.dropUserLoginTokensMaterializedView.ts
+++ packages/lesswrong/server/migrations/20251119T184455.dropUserLoginTokensMaterializedView.ts
@@ -1,8 +1,16 @@
+import Users from "../collections/users/collection";
+import { dropIndex, dropIndexByName } from "./meta/utils";
 
 export const up = async ({db}: MigrationContext) => {
   await db.none(`
     DROP MATERIALIZED VIEW fm_get_user_by_login_token
   `);
+  await dropIndexByName(db, Users, "idx_Users_services_resume__haveLoginTokensToDelete");
+  await dropIndexByName(db, Users, "idx_Users_services_resume__loginTokens");
+  await dropIndexByName(db, Users, "idx_Users_services_resume__loginTokens__hashedToken");
+  await dropIndexByName(db, Users, "idx_Users_services_resume__loginTokens__token");
+  await dropIndexByName(db, Users, "idx_Users_services_resume__loginTokens__when");
+  await dropIndexByName(db, Users, "idx_Users_services_resume__loginTokens__when");
 }
 
 export const down = async ({db}: MigrationContext) => {
