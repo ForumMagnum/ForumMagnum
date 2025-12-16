ba1851eb66e42f0164a76dfe4061484a6009a310
James Babcock <jimrandomh@gmail.com>
2025-12-15 18:49:21 -0800

    Fix type error

--- packages/lesswrong/lib/utils/eventUtils.ts
+++ packages/lesswrong/lib/utils/eventUtils.ts
@@ -1,4 +1,4 @@
 
-export function isSpecialClick(ev: React.MouseEvent<HTMLAnchorElement>) {
+export function isSpecialClick<T>(ev: React.MouseEvent<T>|MouseEvent) {
   return ev.metaKey || ev.altKey || ev.ctrlKey || ev.shiftKey || ev.button !== 0;
 }
