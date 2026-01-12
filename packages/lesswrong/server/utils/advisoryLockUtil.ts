9fab1f091223b0d5df4a690234491c2b3a71533f
Robert Mushkatblat <bobert.mushky@gmail.com>
2026-01-12 14:18:40 -0800

    update comment and fix stub

--- packages/lesswrong/server/utils/advisoryLockUtil.ts
+++ packages/lesswrong/server/utils/advisoryLockUtil.ts
@@ -6,6 +6,11 @@ import { cyrb53Rand } from '@/server/perfMetrics';
  * WARNING: this is not safe to use in most regular production code,
  * because it causes connection pinning in RDS proxy (which in turn
  * can cause function instances to become totally connection starved).
+ * This does require some additional things to happen, like having
+ * a pretty substantial number of advisory locks taken out concurrently
+ * from an individual function instance (i.e. as many as the number of
+ * connections available to each function instance), but that can happen
+ * with surprising ease if you have any accidental N+1s or something.
  */
 export function getSessionLockOrAbort(lockName: string, callback: () => Promise<void>) {
   const db = getSqlClientOrThrow();
