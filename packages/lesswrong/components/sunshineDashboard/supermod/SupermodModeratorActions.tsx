a84a4426c07994bbd7876dc61c48170fe3faf789
Raemon <raemon777@gmail.com>
2026-01-15 17:42:17 -0800

    rate limit item

--- packages/lesswrong/components/sunshineDashboard/supermod/SupermodModeratorActions.tsx
+++ packages/lesswrong/components/sunshineDashboard/supermod/SupermodModeratorActions.tsx
@@ -2,10 +2,10 @@ import React, { useState } from 'react';
 import { defineStyles, useStyles } from '@/components/hooks/useStyles';
 import ModerationPermissionButtons from './ModerationPermissionButtons';
 import ModeratorActionItem from '../ModeratorUserInfo/ModeratorActionItem';
-import { UserRateLimitsList } from '../UserRateLimitsList';
 import { persistentDisplayedModeratorActions } from '@/lib/collections/moderatorActions/constants';
 import type { InboxAction } from './inboxReducer';
-import moment from 'moment';
+import UserRateLimitItem from '../UserRateLimitItem';
+
 const styles = defineStyles('SupermodModeratorActions', (theme: ThemeType) => ({
   modActionsRow: {
     display: 'flex',
@@ -28,31 +28,7 @@ const styles = defineStyles('SupermodModeratorActions', (theme: ThemeType) => ({
     cursor: 'pointer',
   },
   rateLimitForm: {
-    marginTop: 16,
-    [theme.breakpoints.up('md')]: {
-      border: theme.palette.border.normal,
-      paddingLeft: 10,
-      marginTop: 16,
-      '& .form-section-default > div': {
-        display: "flex",
-        flexWrap: "wrap",
-      },
-      '& .input-type, & .input-intervalUnit, & .input-intervalLength, & .input-actionsPerInterval': {
-        width: "calc(33% - 12px)",
-        overflow: "hidden",
-        marginRight: 12
-      },
-      '& .input-endedAt': {
-        marginRight: 12
-      },
-      '& .input-endedAt .DatePicker-wrapper': {
-        marginTop: 5
-      },
-      '& .form-submit': {
-        display: "flex",
-        justifyContent: "flex-end"
-      }
-    }
+    padding: 10
   },
 }));
 
@@ -60,15 +36,6 @@ const SupermodModeratorActions = ({user, dispatch}: {user: SunshineUsersList, di
   const classes = useStyles(styles);
   const activeModeratorActions = user.moderatorActions?.filter(action => action.active && persistentDisplayedModeratorActions.has(action.type)) ?? [];
   const [showRateLimitForm, setShowRateLimitForm] = useState(false);
-
-  const prefilledCustomFormProps = {
-    userId: user._id,
-    type: 'allComments' as const,
-    intervalLength: 1,
-    intervalUnit: 'days' as const,
-    actionsPerInterval: 3,
-    endedAt: moment().add(3, 'weeks').toDate()
-  };
 
   return (
     <div>
@@ -83,25 +50,16 @@ const SupermodModeratorActions = ({user, dispatch}: {user: SunshineUsersList, di
         <ModerationPermissionButtons user={user} dispatch={dispatch} />
         <div
           className={classes.rateLimitButton}
-          onClick={() => setShowRateLimitForm(true)}
+          onClick={() => setShowRateLimitForm(!showRateLimitForm)}
         >
-          Limit
+          Limits
         </div>
       </div>
       {showRateLimitForm && (
         <div className={classes.rateLimitForm}>
-          <UserRateLimitsForm
-            prefilledProps={prefilledCustomFormProps}
-            onSuccess={() => {
-              setShowRateLimitForm(false);
-            }}
-            onCancel={() => {
-              setShowRateLimitForm(false);
-            }}
-          />
+          <UserRateLimitItem userId={user._id} />
         </div>
       )}
-      {user.userRateLimits && <UserRateLimitsList rateLimits={user.userRateLimits} />}
     </div>
   );
 }
