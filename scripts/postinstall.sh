54cbc08286007c92260f7eaba24ed450ab175037
James Babcock <jimrandomh@gmail.com>
2026-01-22 16:06:30 -0800

    Add yarn install in hocuspocus dir to postinstall

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -113,6 +113,10 @@ fi
 
 #echo 'Running yarn install in ckEditor/'
 #(cd ckEditor && yarn install)
+
+echo 'Running yarn install in fly/hocuspocusServer'
+(cd fly/hocuspocusServer && yarn install)
+
 echo
 echo 'For development, you might need to run (cd ckEditor && yarn install)'
 echo 'To start a local development server, run "yarn start development"'
