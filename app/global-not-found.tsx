4e44b774b7e1f54aed4ee80ff2d5ca4f9ddb0e90
James Babcock <jimrandomh@gmail.com>
2026-01-29 16:44:59 -0800

    Rewrite global-not-found

--- app/global-not-found.tsx
+++ app/global-not-found.tsx
@@ -1,10 +1,5 @@
-import Error404 from '@/components/common/Error404';
-import RouteRoot from '@/components/layout/RouteRoot';
 import React from 'react';
 
 export default function NotFound() {
-  return <RouteRoot delayedStatusCode>
-    global-not-found.tsx
-    <Error404 />
-  </RouteRoot>
+  return <div>global-not-found.tsx</div>
 }
