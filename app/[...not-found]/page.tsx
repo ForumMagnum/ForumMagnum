def15ca9287a358985783a8db3d6a2e94fe78db3
James Babcock <jimrandomh@gmail.com>
2026-01-29 17:03:39 -0800

    Fix 404 page for nonexistent routes

---
+++ app/[...not-found]/page.tsx
@@ -0,0 +1,9 @@
+import Error404 from '@/components/common/Error404';
+import RouteRoot from '@/components/layout/RouteRoot';
+import React from 'react';
+
+export default function NotFound() {
+  return <RouteRoot>
+    <Error404 />
+  </RouteRoot>
+}
--- packages/lesswrong/components/common/Error404.tsx
+++ packages/lesswrong/components/common/Error404.tsx
@@ -1,9 +1,12 @@
 "use client";
-
 import React from 'react';
 import SingleColumnSection from "./SingleColumnSection";
 import { defineStyles, useStyles } from '../hooks/useStyles';
 import { StatusCodeSetter } from '../next/StatusCodeSetter';
+import { canonicalizePath } from '@/lib/generated/routeManifest';
+import PermanentRedirect from './PermanentRedirect';
+import { useLocation } from '@/lib/routeUtil';
+import qs from 'qs';
 
 const styles = defineStyles('Error404', (theme: ThemeType) => ({
   root: {
@@ -13,7 +16,21 @@ const styles = defineStyles('Error404', (theme: ThemeType) => ({
 
 const Error404 = () => {
   const classes = useStyles(styles);
-  
+
+  // Before NextJS, we were using react-router, which wasn't case-sensitive by default.
+  // To solve the problem of any existing links going to non-canonically-capitalized paths,
+  // we have a codegen step that generates a trie which we use to find a matching canonical
+  // path (if one exists). Use that on the 404 page, so that if you go to a route that
+  // exists but with the wrong capitalization, it will serve a redirect to the corrected
+  // page.
+  const location = useLocation();
+  const { pathname, query, hash } = location;
+  const canonicalPathname = canonicalizePath(pathname);
+  if (canonicalPathname && canonicalPathname !== location.pathname) {
+    const redirectTarget = `${canonicalPathname}${query ? `?${qs.stringify(query)}` : ''}${hash ? `#${hash}` : ''}`;
+    return <PermanentRedirect url={redirectTarget}/>
+  }
+
   return (
     <SingleColumnSection className={classes.root}>
       <StatusCodeSetter status={404}/>
