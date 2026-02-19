a3a7d969579959c7f03d6b8929a17128f9b7d186
James Babcock <jimrandomh@gmail.com>
2026-02-19 10:38:12 -0800

    When run with a command provided, `yarn repl` avoids REPL-ish I/O

--- scripts/repl.ts
+++ scripts/repl.ts
@@ -2,10 +2,19 @@ import { initGlobals } from "./scriptUtil";
 import * as tsNode from 'ts-node';
 import omit from "lodash/omit";
 import { loadReplEnv, ProjectEnv } from "./runWithVercelEnv";
+import { PassThrough, Writable } from "stream";
 
 import { initConsole } from "../packages/lesswrong/server/serverStartup";
 
 /* eslint-disable no-console */
+
+function createDevNullWritable() {
+  return new Writable({
+    write(_chunk, _encoding, callback) {
+      callback();
+    },
+  });
+}
 
 async function replMain() {
   const projectEnvSettings = await loadReplEnv();
@@ -18,47 +27,56 @@ async function replMain() {
   initGlobals(environment==="prod", { bundleIsCodegen: codegen });
   initConsole();
   
-  const repl = tsNode.createRepl();
+  const shouldRunSingleCommand = !!projectEnvSettings.command;
+  const repl = tsNode.createRepl(
+    shouldRunSingleCommand
+      ? {
+        // In one-shot command mode, avoid reading from stdin or printing a prompt.
+        stdin: new PassThrough(),
+        stdout: createDevNullWritable(),
+        stderr: process.stderr,
+      }
+      : undefined
+  );
   const service = tsNode.create({...repl.evalAwarePartialHost, swc: true, compilerOptions: { module: 'commonjs' }});
   repl.setService(service);
   repl.start();
-  (async () => {
-    let defaultExport: (() => any)|undefined = undefined;
+  let defaultExport: (() => any)|undefined = undefined;
 
-    if (projectEnvSettings.file) {
-      // Import the specified file
-      repl.evalCode(`import * as __repl_import from ${JSON.stringify(projectEnvSettings.file)};\n`);
-      // Get a list of its exports
-      const __repl_import = repl.evalCode("__repl_import\n");
-      defaultExport = __repl_import["default"];
-      // Copy them into the global namespace
-      const importsExceptDefault = omit(__repl_import, "default");
-      repl.evalCode(`const {${Object.keys(importsExceptDefault).join(", ")}} = __repl_import;\n`);
-    }
-    if (projectEnvSettings.command) {
-      try {
-        const result = await repl.evalCode(projectEnvSettings.command);
-        if (result !== undefined) {
-          console.log(result);
-        }
-      } catch(e) {
-        console.error(e);
-      } finally {
-        process.exit(0);
+  if (projectEnvSettings.file) {
+    // Import the specified file
+    repl.evalCode(`import * as __repl_import from ${JSON.stringify(projectEnvSettings.file)};\n`);
+    // Get a list of its exports
+    const __repl_import = repl.evalCode("__repl_import\n");
+    defaultExport = __repl_import["default"];
+    // Copy them into the global namespace
+    const importsExceptDefault = omit(__repl_import, "default");
+    repl.evalCode(`const {${Object.keys(importsExceptDefault).join(", ")}} = __repl_import;\n`);
+  }
+
+  if (projectEnvSettings.command) {
+    try {
+      const result = await repl.evalCode(projectEnvSettings.command);
+      if (result !== undefined) {
+        console.log(result);
       }
-    } else if (defaultExport && typeof defaultExport==='function') {
-      try {
-        const result = await defaultExport();
-        if (result !== undefined) {
-          console.log(result);
-        }
-      } catch(e) {
-        console.error(e);
-      } finally {
-        process.exit(0);
-      }
+    } catch(e) {
+      console.error(e);
+    } finally {
+      process.exit(0);
     }
-  })();
+  } else if (defaultExport && typeof defaultExport==='function') {
+    try {
+      const result = await defaultExport();
+      if (result !== undefined) {
+        console.log(result);
+      }
+    } catch(e) {
+      console.error(e);
+    } finally {
+      process.exit(0);
+    }
+  }
 }
 
 void replMain();
