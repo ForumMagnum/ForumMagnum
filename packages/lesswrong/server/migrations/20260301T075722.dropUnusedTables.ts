e9542ff13a0652b5d870c65497378db35aae035b
James Babcock <jimrandomh@gmail.com>
2026-03-02 13:16:40 -0800

    Split part of drop-unused migration for separate future deploy

--- packages/lesswrong/server/migrations/20260301T075722.dropUnusedTables.ts
+++ packages/lesswrong/server/migrations/20260301T075722.dropUnusedTables.ts
@@ -2,11 +2,7 @@ import { dropField, dropTable } from "./meta/utils";
 
 export const up = async ({db}: MigrationContext) => {
   await dropTable(db, "AdvisorRequests");
-  await dropTable(db, "ForumEvents");
   await dropTable(db, "GardenCodes");
-  await dropField(db, "Comments", "forumEventId");
-  await dropField(db, "Comments", "forumEventMetadata");
-  await dropField(db, "Comments", "subforumStickyPriority");
   await dropTable(db, "ElectionCandidates");
   await dropTable(db, "ElectionVotes");
   await dropTable(db, "FeaturedResources");
@@ -14,16 +10,22 @@ export const up = async ({db}: MigrationContext) => {
   await dropTable(db, "SurveyQuestions");
   await dropTable(db, "SurveyResponses");
   await dropTable(db, "SurveySchedules");
-  await dropField(db, "Users", "optedOutOfSurveys");
-  await dropField(db, "Users", "inactiveSurveyEmailSentAt");
-  await dropField(db, "Users", "userSurveyEmailSentAt");
   await dropTable(db, "UserJobAds");
-  await dropField(db, "Users", "hideJobAdUntil");
   await dropTable(db, "UserEAGDetails");
   await dropTable(db, "DigestPosts");
   await dropTable(db, "Digests");
-  await dropField(db, "Users", "subscribedToDigest");
-  await dropField(db, "Users", "subscribedToNewsletter");
+  
+  // Split into a separate PR/separate migration to avoid brief downtime during deploy
+  //await dropTable(db, "ForumEvents");
+  //await dropField(db, "Comments", "forumEventId");
+  //await dropField(db, "Comments", "forumEventMetadata");
+  //await dropField(db, "Comments", "subforumStickyPriority");
+  //await dropField(db, "Users", "optedOutOfSurveys");
+  //await dropField(db, "Users", "inactiveSurveyEmailSentAt");
+  //await dropField(db, "Users", "userSurveyEmailSentAt");
+  //await dropField(db, "Users", "hideJobAdUntil");
+  //await dropField(db, "Users", "subscribedToDigest");
+  //await dropField(db, "Users", "subscribedToNewsletter");
 }
 
 export const down = async ({db}: MigrationContext) => {
