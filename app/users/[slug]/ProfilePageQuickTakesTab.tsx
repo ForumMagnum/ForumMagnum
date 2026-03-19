e9c5afb99bca147ee6645030bfbd70bca945935b
Raemon <raemon777@gmail.com>
2026-03-18 18:05:04 -0700

    list of QuickTakeItems on profile page

--- app/users/[slug]/ProfilePageQuickTakesTab.tsx
+++ app/users/[slug]/ProfilePageQuickTakesTab.tsx
@@ -3,10 +3,25 @@ import { profileStyles } from "./profileStyles";
 import { UltraFeedContextProvider } from "@/components/ultraFeed/UltraFeedContextProvider";
 import { UltraFeedObserverProvider } from "@/components/ultraFeed/UltraFeedObserver";
 import { OverflowNavObserverProvider } from "@/components/ultraFeed/OverflowNavObserverContext";
-import UserContentFeed from "@/components/users/UserContentFeed";
+import QuickTakesListItem from "@/components/quickTakes/QuickTakesListItem";
+import LoadMore from "@/components/common/LoadMore";
+import Loading from "@/components/vulcan-core/Loading";
+import { useQueryWithLoadMore } from "@/components/hooks/useQueryWithLoadMore";
+import { gql } from "@/lib/generated/gql-codegen";
 import { z } from "zod";
 
-const profilePageQuickTakesTabUnsharedStyles = defineStyles("ProfilePageQuickTakesTabUnshared", () => ({
+const ProfilePageQuickTakesTabQuery = gql(`
+  query ProfilePageQuickTakesTab($selector: CommentSelector, $limit: Int, $enableTotal: Boolean) {
+    comments(selector: $selector, limit: $limit, enableTotal: $enableTotal) {
+      results {
+        ...FrontpageShortformComments
+      }
+      totalCount
+    }
+  }
+`);
+
+const profilePageQuickTakesTabUnsharedStyles = defineStyles("ProfilePageQuickTakesTabUnshared", (theme) => ({
   tabPanel: {
     display: "flex",
     flexDirection: "column",
@@ -15,6 +30,14 @@ const profilePageQuickTakesTabUnsharedStyles = defineStyles("ProfilePageQuickTak
     "@media (max-width: 630px)": {
       order: 1,
     },
+  },
+  quickTakesList: {
+    display: "flex",
+    flexDirection: "column",
+    gap: 12,
+  },
+  quickTakeItem: {
+    border: theme.palette.border.commentBorder,
   },
 }));
 
@@ -42,6 +65,15 @@ export function ProfilePageQuickTakesTabContents({user, settings}: {
   void settings;
   const sharedClasses = useStyles(profileStyles);
   const classes = useStyles(profilePageQuickTakesTabUnsharedStyles);
+  const {data, loading, loadMoreProps} = useQueryWithLoadMore(ProfilePageQuickTakesTabQuery, {
+    variables: {
+      selector: {shortform: {userId: user._id}},
+      limit: 20,
+      enableTotal: true,
+    },
+    fetchPolicy: "cache-and-network",
+  });
+  const quickTakes = data?.comments?.results;
 
   return (
     <div className={classes.tabPanel}>
@@ -49,7 +81,15 @@ export function ProfilePageQuickTakesTabContents({user, settings}: {
         <UltraFeedObserverProvider incognitoMode={false}>
           <OverflowNavObserverProvider>
             <div className={sharedClasses.profileFeedTopMargin}>
-              <UserContentFeed userId={user._id} externalSortMode="recent" externalFilter="quickTakes" removeSideMargins={true} />
+              <div className={classes.quickTakesList}>
+                {quickTakes?.map((quickTake: FrontpageShortformComments) => (
+                  <div key={quickTake._id} className={classes.quickTakeItem}>
+                    <QuickTakesListItem key={quickTake._id} quickTake={quickTake} />
+                  </div>
+                ))}
+                {loading && <Loading />}
+                {!loadMoreProps.hidden && <LoadMore {...loadMoreProps} />}
+              </div>
             </div>
           </OverflowNavObserverProvider>
         </UltraFeedObserverProvider>
