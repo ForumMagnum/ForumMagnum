680414d51eaf560247f9b15fb3c4dc084dfda228
James Babcock <jimrandomh@gmail.com>
2026-03-30 15:15:49 -0700

    Add tooltip to LLM content block heading

--- packages/lesswrong/components/contents/LLMContentBlockDisplay.tsx
+++ packages/lesswrong/components/contents/LLMContentBlockDisplay.tsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { type Ref } from 'react';
+import { TooltipRef } from '../common/FMTooltip';
 
 export default function LLMContentBlockDisplay({modelName, children}: {
-  modelName: string,
+  modelName?: string,
   children: React.ReactNode,
 }) {
   return <div className="llm-content-block" data-model-name={modelName}>
-    <div className="llm-content-block-header">
-      {modelName}
-    </div>
+    <TooltipRef
+     title={<span>Written or edited by {modelName ?? "Unknown Model"}</span>}
+     distance={8} placement="top-start"
+    >
+      {(ref: Ref<HTMLDivElement>) => <div className="llm-content-block-header" ref={ref}>
+        {modelName ?? "Unknown Model"}
+      </div>}
+    </TooltipRef>
     {children}
   </div>
 }
