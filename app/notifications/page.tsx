67ec897d34fe14b8d4d98314f79e4ea15a1b7a8b
Robert Mushkatblat <bobert.mushky@gmail.com>
2026-04-01 02:42:24 -0700

    add assert route attributs to new notifications page

--- app/notifications/page.tsx
+++ app/notifications/page.tsx
@@ -4,10 +4,19 @@ import merge from "lodash/merge";
 import NotificationsPage from '@/components/notifications/NotificationsPage';
 import RouteRoot from "@/components/layout/RouteRoot";
 import { getDefaultMetadata, getPageTitleFields } from "@/server/pageMetadata/sharedMetadata";
+import { assertRouteAttributes } from "@/lib/routeChecks/assertRouteAttributes";
 
 export async function generateMetadata(): Promise<Metadata> {
   return merge({}, await getDefaultMetadata(), getPageTitleFields('Notifications'));
 }
+
+assertRouteAttributes("/notifications", {
+  whiteBackground: false,
+  hasLinkPreview: false,
+  hasPingbacks: false,
+  hasLeftNavigationColumn: false,
+  hasMarkdownVersion: false,
+});
 
 export default function Page() {
   return <RouteRoot>
